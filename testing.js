bc8e39194f6276faa5904ed656d2952ea96dbc48
GitHub <noreply@github.com>
2016-06-23 23:02:21 +0000

    8d5a312 chore(api): clean up compiler/testing api (#9520)

--- testing.js
+++ testing.js
@@ -11,7 +11,6 @@ function __export(m) {
 }
 __export(require('./testing/schema_registry_mock'));
 __export(require('./testing/view_resolver_mock'));
-__export(require('./testing/xhr_mock'));
 __export(require('./testing/test_component_builder'));
 __export(require('./testing/directive_resolver_mock'));
 //# sourceMappingURL=testing.js.map
