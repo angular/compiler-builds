b4aa854909d7d11080fffcb26c968a9b5583009f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-05 18:37:28 +0000

    d2e5198 docs(contributing): fix styleguide link and typos (#13198)

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6cf7a1b');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-d2e5198');
 export { TextAst, BoundTextAst, AttrAst, BoundElementPropertyAst, BoundEventAst, ReferenceAst, VariableAst, ElementAst, EmbeddedTemplateAst, BoundDirectivePropertyAst, DirectiveAst, ProviderAst, ProviderAstType, NgContentAst, PropertyBindingType, templateVisitAll } from './src/template_parser/template_ast';
 export { TEMPLATE_TRANSFORMS } from './src/template_parser/template_parser';
 export { CompilerConfig, RenderTypes } from './src/config';
