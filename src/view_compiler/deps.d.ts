e69fcff493e238dd253aa8287462f9f1414d8a03
Matias Niemelä <matias@yearofmoo.com>
2017-01-09 21:07:03 +0000

    99aa49a feat(language-service): support TS2.2 plugin model

---
+++ src/view_compiler/deps.d.ts
@@ -0,0 +1,24 @@
+/**
+ * This is currently not read, but will probably be used in the future.
+ * We keep it as we already pass it through all the rigth places...
+ */
+export declare class ComponentViewDependency {
+    compType: any;
+    constructor(compType: any);
+}
+/**
+ * This is currently not read, but will probably be used in the future.
+ * We keep it as we already pass it through all the rigth places...
+ */
+export declare class ComponentFactoryDependency {
+    compType: any;
+    constructor(compType: any);
+}
+/**
+ * This is currently not read, but will probably be used in the future.
+ * We keep it as we already pass it through all the rigth places...
+ */
+export declare class DirectiveWrapperDependency {
+    dirType: any;
+    constructor(dirType: any);
+}
---
+++ src/view_compiler/deps.js
@@ -0,0 +1,59 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ *  This is currently not read, but will probably be used in the future.
+  * We keep it as we already pass it through all the rigth places...
+ */
+export var ComponentViewDependency = (function () {
+    /**
+     * @param {?} compType
+     */
+    function ComponentViewDependency(compType) {
+        this.compType = compType;
+    }
+    return ComponentViewDependency;
+}());
+function ComponentViewDependency_tsickle_Closure_declarations() {
+    /** @type {?} */
+    ComponentViewDependency.prototype.compType;
+}
+/**
+ *  This is currently not read, but will probably be used in the future.
+  * We keep it as we already pass it through all the rigth places...
+ */
+export var ComponentFactoryDependency = (function () {
+    /**
+     * @param {?} compType
+     */
+    function ComponentFactoryDependency(compType) {
+        this.compType = compType;
+    }
+    return ComponentFactoryDependency;
+}());
+function ComponentFactoryDependency_tsickle_Closure_declarations() {
+    /** @type {?} */
+    ComponentFactoryDependency.prototype.compType;
+}
+/**
+ *  This is currently not read, but will probably be used in the future.
+  * We keep it as we already pass it through all the rigth places...
+ */
+export var DirectiveWrapperDependency = (function () {
+    /**
+     * @param {?} dirType
+     */
+    function DirectiveWrapperDependency(dirType) {
+        this.dirType = dirType;
+    }
+    return DirectiveWrapperDependency;
+}());
+function DirectiveWrapperDependency_tsickle_Closure_declarations() {
+    /** @type {?} */
+    DirectiveWrapperDependency.prototype.dirType;
+}
+//# sourceMappingURL=deps.js.map
