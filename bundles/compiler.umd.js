593f6382020843991536d7c17bae41d21bb3e712
GitHub <noreply@github.com>
2017-02-23 06:22:15 +0000

    6b7937f test: fix flaky e2e inbox-app test due to net::ERR_SSL_PROTOCOL_ERROR from fetching google fonts (#14665)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-beta.8-175dbce');
+  var VERSION = new _angular_core.Version('4.0.0-beta.8-6b7937f');
 
   /**
    * @license
