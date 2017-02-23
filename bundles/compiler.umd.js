be8eb9c8196da7498d7d906c9b3754d92f088b98
Igor Minar <iminar@google.com>
2017-02-23 01:12:07 +0000

    a1d4769 feat: add a PLATFORM_ID token that provides a platform id Object. (#14647)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-beta.8-88bc143');
+  var VERSION = new _angular_core.Version('4.0.0-beta.8-a1d4769');
 
   /**
    * @license
