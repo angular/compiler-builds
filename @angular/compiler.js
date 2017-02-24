51802597aa9a386a4ff05365eff2eca53c0086e3
Igor Minar <iminar@google.com>
2017-02-24 02:24:44 +0000

    ee747f7 refactor(core): change abstract classes for interfaces (#12324)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-a23634d
+ * @license Angular v4.0.0-beta.8-ee747f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵreflector, View
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-a23634d');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-ee747f7');
 
 /**
  * @license
