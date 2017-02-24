66f65e51cf8a0a1f9dd4bdde68f175dc0e65dd89
Igor Minar <iminar@google.com>
2017-02-24 08:53:30 +0000

    32c2fd5 refactor(animations): change NoOp -> Noop (#14690)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-1f3198c
+ * @license Angular v4.0.0-beta.8-32c2fd5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵreflector, View
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-1f3198c');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-32c2fd5');
 
 /**
  * @license
