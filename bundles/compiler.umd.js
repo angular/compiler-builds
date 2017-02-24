45b5ac20a630ef3ea710fcd3a0f8f7586062c05c
Igor Minar <iminar@google.com>
2017-02-24 17:58:03 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-beta.8-32c2fd5');
+  var VERSION = new _angular_core.Version('4.0.0-beta.8-93ddd38');
 
   /**
    * @license
