0f3ff5b35c1e7b01d9d16f3c4a0711fb05ae7958
GitHub <noreply@github.com>
2017-02-25 02:43:51 +0000

    f32f4de docs: more changelog fixes

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-rc.1-26ed262');
+  var VERSION = new _angular_core.Version('4.0.0-rc.1-f32f4de');
 
   /**
    * @license
