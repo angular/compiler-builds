74d2c2606a2a859ef8004a676d737437cbb333d8
Igor Minar <iminar@google.com>
2017-02-27 19:48:11 +0000

    f356041 docs: update version in changelog to reflect RC.1 (not 0) (#14740)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-rc.1-77682a3');
+  var VERSION = new _angular_core.Version('4.0.0-rc.1-f356041');
 
   /**
    * @license
