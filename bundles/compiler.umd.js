1df16d267bf89d4dfc19bf35c7b3c08c89c6248f
Igor Minar <iminar@google.com>
2017-03-01 16:24:57 +0000

    5753de5 fix(core): fix `isComponentView()` and `isEmbeddedView()` tests (#14789)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-rc.1-bc9e1de');
+  var VERSION = new _angular_core.Version('4.0.0-rc.1-5753de5');
 
   /**
    * @license
