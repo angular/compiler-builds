c47f24178de2274d978a8931598205254a0f936a
Igor Minar <iminar@google.com>
2017-03-01 23:04:36 +0000

    a6996a9 fix(tsc-wrapped): validate metadata in static members of a class (#14772)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-rc.1-6bae737');
+  var VERSION = new _angular_core.Version('4.0.0-rc.1-a6996a9');
 
   /**
    * @license
