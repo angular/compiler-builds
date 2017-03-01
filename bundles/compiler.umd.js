e0ba7350c014d82d132e118e5c68f538ae454be7
Igor Minar <iminar@google.com>
2017-03-01 19:36:58 +0000

    47bdc2b fix(platform-server): don't setup Testability and TestabilityRegistry on the server (#14510)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -7,7 +7,7 @@
   /**
    * @stable
    */
-  var VERSION = new _angular_core.Version('4.0.0-rc.1-49b462e');
+  var VERSION = new _angular_core.Version('4.0.0-rc.1-47bdc2b');
 
   /**
    * @license
