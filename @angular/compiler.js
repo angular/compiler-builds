cfa2c864c3920c2dbc12b0234dc1b82135ca6053
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:17 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵstringify, ɵre
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-d4205bb');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-2f250c1');
 
 /**
  * @license
