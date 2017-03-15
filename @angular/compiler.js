ca6cf36df40abdb29a86ec9cf704ec51f55163ef
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 22:42:14 +0000

    a9b1880 feat(aio): top bar typography, sidenav size

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ec548ad
+ * @license Angular v4.0.0-rc.3-a9b1880
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵstringify, ɵre
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ec548ad');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-a9b1880');
 
 /**
  * @license
