d8756c3f3f4dde631c6cd1fb6a214216a7b46182
GitHub <noreply@github.com>
2017-03-15 20:54:24 +0000

    795638e ci: remove unused import from integration test (#15175)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-322bf7a
+ * @license Angular v4.0.0-rc.3-795638e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵstringify, ɵre
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-322bf7a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-795638e');
 
 /**
  * @license
