0312f01ff897d8190e805686da435b362c8910b3
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 05:07:16 +0000

    923d0c5 fix: correct UMD resolutions for platform-browser_animations (#15190)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6772c91
+ * @license Angular v4.0.0-rc.3-923d0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵstringify, ɵre
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-6772c91');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
 
 /**
  * @license
