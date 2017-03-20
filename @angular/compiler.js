04df73a8193581755d359f4f851713a2a4a40580
Miško Hevery <misko@hevery.com>
2017-03-20 18:02:18 +0000

    5da021c feat(router): add `ParamMap.keys` to get a list of parameters

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-f634c62
+ * @license Angular v4.0.0-rc.5-5da021c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-f634c62');
+const VERSION = new Version('4.0.0-rc.5-5da021c');
 
 /**
  * @license
