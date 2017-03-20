47f0dac6ee7040fabbcf5b4d6bad10c8322b8095
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 12:27:53 +0000

    6d4f5ce build(aio): update to dgeni-packages 0.17.0

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d1e82b0
+ * @license Angular v4.0.0-rc.5-6d4f5ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -8,7 +8,7 @@ import { InjectionToken, Version, Inject, Optional, ɵConsole, ɵstringify, ɵre
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-d1e82b0');
+const VERSION = new Version('4.0.0-rc.5-6d4f5ce');
 
 /**
  * @license
