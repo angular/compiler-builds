3827429da6ca06c5b8c614614753495cf1be7583
Victor Berchet <victor@suumit.com>
2017-03-27 17:01:53 +0000

    816b389 docs: in doc comments, replace [aA]ngular2 with Angular (#15463)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6605dd1
+ * @license Angular v4.0.0-816b389
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-6605dd1');
+const VERSION = new Version('4.0.0-816b389');
 
 /**
  * @license
