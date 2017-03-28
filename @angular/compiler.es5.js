9d5e060540c244fcf5fea8944b6480d4d4554c9e
Victor Berchet <victor@suumit.com>
2017-03-28 17:08:58 +0000

    8e03f65 refactor(router): improve flatten fn

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-b7fa5de
+ * @license Angular v4.0.0-8e03f65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.0.0-b7fa5de');
+var VERSION = new Version('4.0.0-8e03f65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
