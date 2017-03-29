3ff5c6345f3b4f4475dfa31f37b0f94f4d578ea1
Victor Berchet <victor@suumit.com>
2017-03-29 22:27:47 +0000

    19cb503 docs(http): remove deprecated stuff and outdated plunkrs (#15598)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9f2acf5
+ * @license Angular v4.0.0-19cb503
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9f2acf5');
+const VERSION = new Version('4.0.0-19cb503');
 
 /**
  * @license
