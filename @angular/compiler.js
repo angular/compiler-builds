b5040b47a12e43cb03b25268f784cdfb079e5943
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:52:39 +0000

    5c38012 fix(aio): contributor.html typos

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-46f8a6d
+ * @license Angular v4.1.0-beta.0-5c38012
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-46f8a6d');
+const VERSION = new Version('4.1.0-beta.0-5c38012');
 
 /**
  * @license
