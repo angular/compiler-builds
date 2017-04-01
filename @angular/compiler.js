93eacf389eaa5cfe8d0335d516f36f04c487a1f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:30:36 +0000

    03fb766 feat(aio): shell code styling

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-2454a3b
+ * @license Angular v4.1.0-beta.0-03fb766
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-2454a3b');
+const VERSION = new Version('4.1.0-beta.0-03fb766');
 
 /**
  * @license
