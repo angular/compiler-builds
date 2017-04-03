d807c99d7db767549f121b11f597d634a6c056b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 22:56:45 +0000

    0e38bf9 build(aio): new migration

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-309bae0');
+const VERSION = new Version('4.1.0-beta.0-0e38bf9');
 
 /**
  * @license
