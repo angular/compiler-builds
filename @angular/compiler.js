d36a3777866a53f3cf0d044522cf9f118e77bd10
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 02:46:01 +0000

    f4f621a feat(animations): wip: introduce a wave of new animation features

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-c933b75
+ * @license Angular v4.1.0-beta.0-f4f621a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-c933b75');
+const VERSION = new Version('4.1.0-beta.0-f4f621a');
 
 /**
  * @license
