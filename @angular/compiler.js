1e73931faab1d673611d72e0953c339404174dbe
Hans <hans@hansl.ca>
2017-04-10 22:18:11 +0000

    258d539 fix(language-service): detect when there isn't a tsconfig.json

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-09d9f5f
+ * @license Angular v4.1.0-beta.0-258d539
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-09d9f5f');
+const VERSION = new Version('4.1.0-beta.0-258d539');
 
 /**
  * @license
