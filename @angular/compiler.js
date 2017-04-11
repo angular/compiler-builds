c397a8ca93c8e4d5d26f068680ec39b2f1d1e935
Hans <hans@hansl.ca>
2017-04-11 21:41:27 +0000

    0799f18 fix(aio): v2 - when browser lacks service worker, disable sw feature

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-91b2e39
+ * @license Angular v4.1.0-beta.0-0799f18
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-91b2e39');
+const VERSION = new Version('4.1.0-beta.0-0799f18');
 
 /**
  * @license
