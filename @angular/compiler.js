ec1fd6449af46753105a50835b05a375039b3f2b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-12 13:48:05 +0000

    2798c1b feat(aio): code block styles with header

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-38d75d4
+ * @license Angular v4.1.0-beta.0-2798c1b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-38d75d4');
+const VERSION = new Version('4.1.0-beta.0-2798c1b');
 
 /**
  * @license
