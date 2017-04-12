b1ff58ecd38e651f2bc456647325592d6f1c6368
Hans Larsen <hans@hansl.ca>
2017-04-12 01:30:21 +0000

    38d75d4 feat(animations): Update types for TypeScript nullability support

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-04fb29b
+ * @license Angular v4.1.0-beta.0-38d75d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-04fb29b');
+const VERSION = new Version('4.1.0-beta.0-38d75d4');
 
 /**
  * @license
