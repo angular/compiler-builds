150343e32cb1b13039dae40e4552a1c7b57aa7a3
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:10:38 +0000

    9394835 fix(platform-server): handle innerText (#15818)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-d263595
+ * @license Angular v4.1.0-beta.1-9394835
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-d263595');
+const VERSION = new Version('4.1.0-beta.1-9394835');
 
 /**
  * @license
