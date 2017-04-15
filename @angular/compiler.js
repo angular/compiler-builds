e866d31d6876672034e6f43a25106c4a5db6cf9e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:44:29 +0000

    49dfc9f feat(aio): scroll bar styling

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-49d97e1
+ * @license Angular v4.1.0-beta.1-49dfc9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-49d97e1');
+const VERSION = new Version('4.1.0-beta.1-49dfc9f');
 
 /**
  * @license
