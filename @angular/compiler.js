e1d91b99337df831ad0efcd0acbad99f816ae3d5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 21:33:01 +0000

    abecf3e build(aio): move the `transforms` folder into the `tools` folder

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-7c418a7
+ * @license Angular v4.1.0-beta.1-abecf3e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-7c418a7');
+const VERSION = new Version('4.1.0-beta.1-abecf3e');
 
 /**
  * @license
