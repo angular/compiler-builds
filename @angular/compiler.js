848da4381151218b91fb0b878bb13375c592fa50
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:26:27 +0000

    4a3c66f feat(aio): fixed sidenav, moved footer inside of sidenav container (#16028)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-9a50844
+ * @license Angular v4.1.0-beta.1-4a3c66f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-9a50844');
+const VERSION = new Version('4.1.0-beta.1-4a3c66f');
 
 /**
  * @license
