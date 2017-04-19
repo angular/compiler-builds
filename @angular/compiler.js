eee48294fe1c7cde902498ba2218148cadf9ff90
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:42:04 +0000

    93d8f8c docs(aio): add VS Code advice to readme.md

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8fa4453
+ * @license Angular v4.1.0-beta.1-93d8f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8fa4453');
+const VERSION = new Version('4.1.0-beta.1-93d8f8c');
 
 /**
  * @license
