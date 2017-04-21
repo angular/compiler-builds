736517492d429986168d60d49050839d33c8cedb
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:48:51 +0000

    cf034f7 feat(aio): api page styles

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-87ec8d1
+ * @license Angular v4.1.0-rc.0-cf034f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-87ec8d1');
+const VERSION = new Version('4.1.0-rc.0-cf034f7');
 
 /**
  * @license
