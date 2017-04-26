c8c0b082ddff1abcb32bf254c9b410976006438e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 09:08:33 +0000

    35a2dfc fix(aio): resource nav

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-d8e2829');
+const VERSION = new Version('4.1.0-rc.0-35a2dfc');
 
 /**
  * @license
