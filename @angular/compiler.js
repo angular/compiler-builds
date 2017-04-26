5aea5a8283130c6c566343ef7c6326cb6f6a1e9a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 08:36:58 +0000

    ac5e6ba fix(aio): AppComponent should scroll only once when location changes

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-710b4a3
+ * @license Angular v4.1.0-rc.0-ac5e6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-710b4a3');
+const VERSION = new Version('4.1.0-rc.0-ac5e6ba');
 
 /**
  * @license
