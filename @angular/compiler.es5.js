f81bd131e2886ef7a1cecbf30d941d5a7706cc72
GitHub <noreply@github.com>
2017-04-26 19:29:00 +0000

    a28f5ee docs: update 4.1.0 changelog, remove aio

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-c0c5013');
+var VERSION = new Version('4.1.0-a28f5ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-c0c5013
+ * @license Angular v4.1.0-a28f5ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-c0c5013');
+const VERSION = new Version('4.1.0-a28f5ee');
 
 /**
  * @license
