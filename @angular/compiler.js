c9d4ac718c5e2f8db5cc1b7ae0ffd7d3fd372abe
Miško Hevery <misko@hevery.com>
2017-04-27 22:49:27 +0000

    3f46645 feat(aio): add Table of Contents (toc) component. (#16078)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-2a7f636
+ * @license Angular v4.1.0-3f46645
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-2a7f636');
+const VERSION = new Version('4.1.0-3f46645');
 
 /**
  * @license
