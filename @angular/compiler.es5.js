50b25f07e91b84b814fec60aefa644b8a26d7eae
Miško Hevery <misko@hevery.com>
2017-04-28 23:27:23 +0000

    a4de214 fix(core): don’t set `ng-version` for dynamically created components (#16394)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-a2b2afb
+ * @license Angular v4.1.0-a4de214
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-a2b2afb');
+var VERSION = new Version('4.1.0-a4de214');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
