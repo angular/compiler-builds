6bf62082244043cc0eb1e4774f2c53f96c6bfe20
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:32:22 +0000

    21c96a5 feat(aio): sidenav styling extended

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1c8772a
+ * @license Angular v4.1.0-21c96a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-1c8772a');
+const VERSION = new Version('4.1.0-21c96a5');
 
 /**
  * @license
