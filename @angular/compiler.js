d3316af84753a2cb6dc12cdfad5980f72c9e6488
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:30:43 +0000

    cb5bc76 feat(aio): move search results under search box

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-79ed0e7
+ * @license Angular v4.1.0-cb5bc76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-79ed0e7');
+const VERSION = new Version('4.1.0-cb5bc76');
 
 /**
  * @license
