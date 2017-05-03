37619312b3168c97fb8ed3d4414e2c957375ae76
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-03 08:18:27 +0000

    ea9d8a6 docs(aio): move pipes guide under components

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-9650ff0
+ * @license Angular v4.1.0-ea9d8a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-9650ff0');
+const VERSION = new Version('4.1.0-ea9d8a6');
 
 /**
  * @license
