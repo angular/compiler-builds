73550c1a12a1f3949abf70ffd46e3a28bde7593e
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:07:38 +0000

    55b8de9 build(aio): lint examples

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-eb56ab3');
+const VERSION = new Version('4.1.0-55b8de9');
 
 /**
  * @license
