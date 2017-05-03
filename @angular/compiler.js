600e73e278c7cc41b5bd88f46d3c52109007bea5
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:49:27 +0000

    9ca2b4c feat(aio): set “avoid” class by convention (filename has “.avoid.”)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8760bf7');
+const VERSION = new Version('4.1.0-9ca2b4c');
 
 /**
  * @license
