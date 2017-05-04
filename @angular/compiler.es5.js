86400f7b62a1e314c662d567a0762f7fabb568f9
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:50:30 +0000

    6748aea build(aio): update to dgeni-packages 0.19.0

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-b3e63c0
+ * @license Angular v4.1.0-6748aea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-b3e63c0');
+var VERSION = new Version('4.1.0-6748aea');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
