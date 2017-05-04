5385f2b83fc12115ef6ce429fad6fe749e9aa759
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:48:26 +0000

    77b8a76 feat(aio): toc styling

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-55b8de9
+ * @license Angular v4.1.0-77b8a76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-55b8de9');
+const VERSION = new Version('4.1.0-77b8a76');
 
 /**
  * @license
