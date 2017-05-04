995259e504cfd9ae7050dabdfefa53fa3d413ad4
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:49:27 +0000

    b3e63c0 fix(upgrade): initialize all inputs in time for `ngOnChanges()`

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-77b8a76
+ * @license Angular v4.1.0-b3e63c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-77b8a76');
+const VERSION = new Version('4.1.0-b3e63c0');
 
 /**
  * @license
