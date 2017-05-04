e1f49641ab83b686e4ef2e4443323a65cf2c9f19
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 22:18:39 +0000

    5f6d0f2 revert: fix: public API golden files (#16414)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-b9723f9
+ * @license Angular v4.1.1-5f6d0f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-b9723f9');
+const VERSION = new Version('4.1.1-5f6d0f2');
 
 /**
  * @license
