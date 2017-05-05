5488c11d8546a0ee2ec831faa0b78486ff03856d
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 20:40:35 +0000

    9da6340 fix(http): flatten metadata for @angular/http/testing

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-547c363
+ * @license Angular v4.2.0-beta.0-9da6340
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-547c363');
+const VERSION = new Version('4.2.0-beta.0-9da6340');
 
 /**
  * @license
