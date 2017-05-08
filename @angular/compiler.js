4120e84f60d0ead54c29741ea5c9bc7a02369b78
Miško Hevery <misko@hevery.com>
2017-05-08 18:07:03 +0000

    98dd609 docs(animations): remove duplicate word (#16508)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-6dc6777
+ * @license Angular v4.2.0-beta.0-98dd609
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-6dc6777');
+const VERSION = new Version('4.2.0-beta.0-98dd609');
 
 /**
  * @license
