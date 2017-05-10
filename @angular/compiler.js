f7418baa599befe5dab918aa8e04144b781575e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 10:57:18 +0000

    f5335d1 build(aio): fail the doc-gen if there is an invalid `{@link ...}` tag

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-dc7d242
+ * @license Angular v4.2.0-beta.0-f5335d1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-dc7d242');
+const VERSION = new Version('4.2.0-beta.0-f5335d1');
 
 /**
  * @license
