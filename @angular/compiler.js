556f6c796cd0a40b696d566133ae6d7f4e23000d
Jason Aden <jasonaden@google.com>
2017-05-10 23:44:31 +0000

    162dffb docs: add changelog for 4.2.0-beta.1

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-e242e20
+ * @license Angular v4.2.0-beta.1-162dffb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-e242e20');
+const VERSION = new Version('4.2.0-beta.1-162dffb');
 
 /**
  * @license
