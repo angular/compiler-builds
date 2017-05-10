3bc1edebb3a272a1be52df2c438c3ddc82fae4de
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 12:09:53 +0000

    56833a6 docs(aio): more content fixes

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-efa2928
+ * @license Angular v4.2.0-beta.0-56833a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-efa2928');
+const VERSION = new Version('4.2.0-beta.0-56833a6');
 
 /**
  * @license
