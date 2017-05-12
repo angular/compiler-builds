63f4d6325d76ad8a39e800a5d9033d8c73e8b1f7
Igor Minar <iminar@google.com>
2017-05-12 19:36:36 +0000

    0626464 build: use subshells when changing directories

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-21d213d');
+var VERSION = new Version('4.2.0-beta.1-0626464');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-21d213d
+ * @license Angular v4.2.0-beta.1-0626464
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-21d213d');
+const VERSION = new Version('4.2.0-beta.1-0626464');
 
 /**
  * @license
