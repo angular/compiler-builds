aacdcaf42d71369a0cab941c82ffe4f98b5c9e57
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-12 13:47:15 +0000

    b016984 docs(aio): resize images

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-90bc5b2
+ * @license Angular v4.2.0-beta.1-b016984
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-90bc5b2');
+const VERSION = new Version('4.2.0-beta.1-b016984');
 
 /**
  * @license
