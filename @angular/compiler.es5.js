310565c7a6c40c9f49f7fc3752d2fd5b06742478
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-20 11:24:35 +0000

    c8b08f3 feat(aio): implement ScrollSpy service (to highlight the active section in ToC)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-3d382dc
+ * @license Angular v4.2.0-rc.0-c8b08f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-3d382dc');
+var VERSION = new Version('4.2.0-rc.0-c8b08f3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-3d382dc
+ * @license Angular v4.2.0-rc.0-c8b08f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-3d382dc');
+const VERSION = new Version('4.2.0-rc.0-c8b08f3');
 
 /**
  * @license
