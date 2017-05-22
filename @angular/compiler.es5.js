f455292c8114d782aecc89ec1e6470d69f6fabe5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-22 13:08:34 +0000

    ce18fdb fix(aio): use the current version in the version picker

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-670771d
+ * @license Angular v4.2.0-rc.0-ce18fdb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -29,7 +29,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.0-670771d');
+var VERSION = new Version('4.2.0-rc.0-ce18fdb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
