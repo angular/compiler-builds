bff45a2b6d630f369fe6d1b7ca2637175c9490e0
Chuck Jazdzewski <chuckj@google.com>
2017-05-24 20:29:52 +0000

    966eb2f aio: add h1 title to floating table of contents (#16959)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-b0c5018
+ * @license Angular v4.2.0-rc.0-966eb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-b0c5018
+ * @license Angular v4.2.0-rc.0-966eb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-b0c5018');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-966eb2f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
