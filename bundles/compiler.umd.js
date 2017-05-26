091e5ba00113b978e4686904f354cdbeb0e85d56
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:00:44 +0000

    b37a048 docs(aio): restore missing files (#16995)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-e10d763
+ * @license Angular v4.2.0-rc.0-b37a048
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-e10d763
+ * @license Angular v4.2.0-rc.0-b37a048
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-e10d763');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-b37a048');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
