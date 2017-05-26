032bc0401fcab2bfe91eac867365cc0fc12a62e3
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 22:53:55 +0000

    51d7a65 docs: add changelog for 4.2.0-rc.1

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-d108479
+ * @license Angular v4.2.0-rc.1-51d7a65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.0-d108479
+ * @license Angular v4.2.0-rc.1-51d7a65
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-d108479');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-51d7a65');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
