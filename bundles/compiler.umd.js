987baa115fe3a91a4d9bdd8034782e119daf7a8f
GitHub <noreply@github.com>
2017-05-27 19:04:06 +0000

    6220b49 build(aio): upgrade to ngo with top-level NewExpression and CallExpression prefixing (#17063)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-aa683a7
+ * @license Angular v4.2.0-rc.1-6220b49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-aa683a7
+ * @license Angular v4.2.0-rc.1-6220b49
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-aa683a7');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-6220b49');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
