fbe01a901782a2b1131f7e4644792f2a9c96bcb3
Victor Berchet <victor@suumit.com>
2017-05-30 22:33:18 +0000

    0d894a1 fix(aio): fix ToC styling (#17110)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-e513808
+ * @license Angular v4.2.0-rc.1-0d894a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-e513808');
+var VERSION = new Version('4.2.0-rc.1-0d894a1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
