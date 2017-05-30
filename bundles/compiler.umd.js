54d8f291539ee35b959211f0c9c6c1249c6131d7
Victor Berchet <victor@suumit.com>
2017-05-30 18:05:22 +0000

    535d9da fix(common): always use 'other' case for locales with no plural rules (#16990)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-18bf772
+ * @license Angular v4.2.0-rc.1-535d9da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-18bf772
+ * @license Angular v4.2.0-rc.1-535d9da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-18bf772');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-535d9da');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
