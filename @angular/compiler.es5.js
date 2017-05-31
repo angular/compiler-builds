d507acb0ac5b720933d382132aac7209d03fe61a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-31 06:02:12 +0000

    dfbbbb5 build(aio): the the captured h1 as the title for the search index

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-2f35392
+ * @license Angular v4.2.0-rc.1-dfbbbb5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-2f35392');
+var VERSION = new Version('4.2.0-rc.1-dfbbbb5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
