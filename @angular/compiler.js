911e249918cc078f70a4d7adcbb6eca682b8b074
Victor Berchet <victor@suumit.com>
2017-05-31 22:53:30 +0000

    665e707 fix(animations): always change to desired animation state even if no transition fires (#17025)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-c20f60b
+ * @license Angular v4.2.0-rc.1-665e707
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.1-c20f60b');
+const VERSION = new Version('4.2.0-rc.1-665e707');
 
 /**
  * @license
