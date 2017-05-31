cee7c53b3728387a9c896a66642a96e6fed95236
GitHub <noreply@github.com>
2017-05-31 11:05:19 +0000

    1f9a3dd fix(aio): fix typo in web worker check (#17133)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-11505fa');
+var VERSION = new Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-11505fa
+ * @license Angular v4.2.0-rc.1-1f9a3dd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-11505fa');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-1f9a3dd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
