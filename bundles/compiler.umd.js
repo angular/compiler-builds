68b4749f2179db70116bc0c04e1fff2ac4c06ef9
Victor Berchet <victor@suumit.com>
2017-06-01 16:58:55 +0000

    1338995 fix(aio): fix home learn more button alignment (#17154)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-47e4fca
+ * @license Angular v4.2.0-rc.1-1338995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-47e4fca');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-1338995');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
