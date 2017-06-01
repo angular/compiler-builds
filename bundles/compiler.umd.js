4e876f803d4e2b04e7adc2c920ac747f0c1e91d7
Victor Berchet <victor@suumit.com>
2017-06-01 21:20:48 +0000

    068133e fix(animations): do not retain deleted nodes during an non-removal animation (#17153)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-598fdad
+ * @license Angular v4.2.0-rc.1-068133e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-598fdad
+ * @license Angular v4.2.0-rc.1-068133e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-598fdad');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-068133e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
