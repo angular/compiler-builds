74ffd7e1b0597c553366b056ad4f59fb73d7eab1
GitHub <noreply@github.com>
2017-06-01 20:43:38 +0000

    598fdad docs: update the release schedule (#17162)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.1-ad6a57e
+ * @license Angular v4.2.0-rc.1-598fdad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.1-ad6a57e');
+var VERSION = new Version('4.2.0-rc.1-598fdad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
