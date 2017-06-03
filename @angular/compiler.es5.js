4871b9c4c1d8d8ea8015f083328ee324d754a72d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-03 00:22:07 +0000

    b55adee perf(animations): only apply `:leave` flags if animations are set to run

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-35f714e
+ * @license Angular v4.2.0-rc.2-b55adee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-35f714e');
+var VERSION = new Version('4.2.0-rc.2-b55adee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
