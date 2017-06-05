543c95a652c1ad781b56d00f14183298471bf1b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-05 18:36:37 +0000

    7c78282 docs: fix typo in 'Routes' docs

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-02d74ca
+ * @license Angular v4.2.0-rc.2-7c78282
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-02d74ca');
+var VERSION = new Version('4.2.0-rc.2-7c78282');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
