c1ea81c4e9580591f82688d02322f4f140c68226
Igor Minar <iminar@google.com>
2017-06-08 08:13:47 +0000

    8be2e4c fix(aio): make the ToC left hand-side bar shorter (#17280)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a4a2901
+ * @license Angular v4.2.0-rc.2-8be2e4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-a4a2901');
+const VERSION = new Version('4.2.0-rc.2-8be2e4c');
 
 /**
  * @license
