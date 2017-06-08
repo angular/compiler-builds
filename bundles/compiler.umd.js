d8fceeaa7982876424774a5575f6f0982c5d23e0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 21:16:47 +0000

    f370fd3 docs: "TypeScript to JavaScript" code tab renamed “ES6 + Decorators”

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-6a46cab
+ * @license Angular v4.2.0-rc.2-f370fd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-6a46cab
+ * @license Angular v4.2.0-rc.2-f370fd3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-6a46cab');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-f370fd3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
