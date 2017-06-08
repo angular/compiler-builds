7045b6b1fb62f20155382d8cd60adfed797d1a85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 20:13:43 +0000

    7467354 docs(aio): document the non-null assertion operator

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-rc.2-391ed63
+ * @license Angular v4.2.0-rc.2-7467354
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-rc.2-391ed63');
+var VERSION = new Version('4.2.0-rc.2-7467354');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
