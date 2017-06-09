8d0133e4bf979bbdc3d3c052adb3299e6a8d1409
Igor Minar <iminar@google.com>
2017-06-09 21:53:36 +0000

    0020dad docs(aio): update examples to 4.2 (#17367)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-65d49d5');
+var VERSION = new Version('4.2.0-0020dad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-65d49d5
+ * @license Angular v4.2.0-0020dad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-65d49d5');
+var VERSION = new _angular_core.Version('4.2.0-0020dad');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
