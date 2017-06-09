d94cff125e21d5c35fb7331d690c99d77cc717ec
Igor Minar <iminar@google.com>
2017-06-09 21:54:19 +0000

    db5e506 build(aio): update typescript to 2.3.2 (#17382)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-0020dad
+ * @license Angular v4.2.0-db5e506
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-0020dad');
+const VERSION = new Version('4.2.0-db5e506');
 
 /**
  * @license
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-0020dad
+ * @license Angular v4.2.0-db5e506
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-0020dad
+ * @license Angular v4.2.0-db5e506
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-0020dad');
+var VERSION = new _angular_core.Version('4.2.0-db5e506');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
