b20ee2b2cf1c68fada44d85e428c808639dae05a
Igor Minar <iminar@google.com>
2017-06-09 21:42:59 +0000

    65d49d5 fix(aio): temporarily remove link to source from the API pages (#17371)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-33df13a');
+var VERSION = new Version('4.2.0-65d49d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-33df13a');
+const VERSION = new Version('4.2.0-65d49d5');
 
 /**
  * @license
