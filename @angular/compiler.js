90002a7642fce69498c2c8a5956b5b86c759433a
Igor Minar <iminar@google.com>
2017-06-09 22:09:34 +0000

    d56b7ed docs(aio): reorganise the sidenav menu (#16934)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-c2d31fb
+ * @license Angular v4.2.0-d56b7ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-c2d31fb');
+const VERSION = new Version('4.2.0-d56b7ed');
 
 /**
  * @license
