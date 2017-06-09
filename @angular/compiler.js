8a6f9d04cca9e9bbdd47d4309d92c1652101ea8b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 09:54:45 +0000

    8524187 fix(aio): restrain scrolling inside ToC (when cursor over ToC)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-f7422a9
+ * @license Angular v4.2.0-8524187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-f7422a9');
+const VERSION = new Version('4.2.0-8524187');
 
 /**
  * @license
