47a6358102d2c6ebc6d7a8b0e6edec41a91e6003
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 20:07:44 +0000

    ce18c68 fix(aio): temporarily remove the focus style of top-bar nav items

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-2d56239
+ * @license Angular v4.2.0-ce18c68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-2d56239
+ * @license Angular v4.2.0-ce18c68
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-2d56239');
+var VERSION = new _angular_core.Version('4.2.0-ce18c68');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
