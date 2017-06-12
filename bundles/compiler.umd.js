642dc92626cf5dd9451734d398c5d26064fd235b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 22:44:52 +0000

    ed73d4f refactor(compiler): don’t always compile `.ngfactory.ts` files by default

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-451257a
+ * @license Angular v4.2.1-ed73d4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-451257a
+ * @license Angular v4.2.1-ed73d4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-451257a');
+var VERSION = new _angular_core.Version('4.2.1-ed73d4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
