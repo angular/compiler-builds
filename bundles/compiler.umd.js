5f943ae369c4af61a497bb6e738559f2a60e2169
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:38:22 +0000

    185075d fix(animations): compute removal node height correctly

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-3d5f520
+ * @license Angular v4.2.1-185075d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-3d5f520
+ * @license Angular v4.2.1-185075d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-3d5f520');
+var VERSION = new _angular_core.Version('4.2.1-185075d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
