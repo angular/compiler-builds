2f217d81a661b04c8b4f8e401b256059997c56a0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-12 13:31:55 +0000

    3d5f520 test(aio): fix Chrome getting disconnected during unit tests

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-d961911
+ * @license Angular v4.2.1-3d5f520
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-d961911');
+const VERSION = new Version('4.2.1-3d5f520');
 
 /**
  * @license
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-d961911
+ * @license Angular v4.2.1-3d5f520
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-d961911
+ * @license Angular v4.2.1-3d5f520
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-d961911');
+var VERSION = new _angular_core.Version('4.2.1-3d5f520');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
