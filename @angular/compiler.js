f55d750fdfcea16c3e2ee9e15913b8781f6dd6ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 22:20:50 +0000

    d8d21c7 ci: update github templates (#17466)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-b4cd20c
+ * @license Angular v4.2.2-d8d21c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-b4cd20c');
+const VERSION = new Version('4.2.2-d8d21c7');
 
 /**
  * @license
