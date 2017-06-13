994ccf12374ac7ff3f77ecc0ca6cfeb9b4d3bc77
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:53:46 +0000

    bffccf4 fix(aio): fix buttons in "Home" and "Features"

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-7caa0a8
+ * @license Angular v4.2.2-bffccf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-7caa0a8');
+var VERSION = new Version('4.2.2-bffccf4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
