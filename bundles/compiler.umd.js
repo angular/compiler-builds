b4705ebc14c5ee7bb8eef3042b7ceb2c3ea30b3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 21:30:52 +0000

    f840afb ci: disable platform-server integration test as it is currently broken

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-93d1b4e
+ * @license Angular v4.2.2-f840afb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-93d1b4e
+ * @license Angular v4.2.2-f840afb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-93d1b4e');
+var VERSION = new _angular_core.Version('4.2.2-f840afb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
