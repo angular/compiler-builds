2ebf5e66b64df9b7d8a4eb77a9c2dd1deb9433e7
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:13:19 +0000

    2c3e948 docs(aio): update about page

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-ba0f6de
+ * @license Angular v4.2.2-2c3e948
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-ba0f6de');
+const VERSION = new Version('4.2.2-2c3e948');
 
 /**
  * @license
