1b1291c5164e6c8c84924ee8a8502b85943fcfc6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-15 21:47:02 +0000

    96aa3bb docs(aio): update about page

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-64fa100
+ * @license Angular v4.2.2-96aa3bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-64fa100');
+var VERSION = new Version('4.2.2-96aa3bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
