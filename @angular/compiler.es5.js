e55d1560dcf563fc5b40a587507e3f8af6fe7de5
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:05:15 +0000

    01173b9 fix(aio): do not log messages in production

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-06a0cf2');
+var VERSION = new Version('4.2.2-01173b9');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
