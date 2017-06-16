5cd979dcb25636a977112b51ed17d5687bb7ead4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-16 07:27:01 +0000

    956a7e9 docs(aio): re-add biography entry for devversion

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-2cc931e
+ * @license Angular v4.2.2-956a7e9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-2cc931e');
+const VERSION = new Version('4.2.2-956a7e9');
 
 /**
  * @license
