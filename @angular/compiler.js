3745627d856e76e9da5970ba2286d7e14745873d
Hans Larsen <hans@hansl.ca>
2017-06-16 19:34:08 +0000

    71a8ef5 fix(aio): improve no-javascript screen

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-623b9c6
+ * @license Angular v4.2.3-71a8ef5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-623b9c6');
+const VERSION = new Version('4.2.3-71a8ef5');
 
 /**
  * @license
