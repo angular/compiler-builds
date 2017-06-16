ef9e081b46a84771cafac4666db87e28e415fd7f
Hans Larsen <hans@hansl.ca>
2017-06-16 17:16:17 +0000

    8a547ee docs: add changelog for 4.2.3

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-b8c39cd
+ * @license Angular v4.2.3-8a547ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-b8c39cd');
+var VERSION = new Version('4.2.3-8a547ee');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
