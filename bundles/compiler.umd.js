0868109e8592e8f4bf1fe4849088c3a4a98b3e2c
Hans Larsen <hans@hansl.ca>
2017-06-16 17:19:50 +0000

    1ddbddb docs: add changelog for 4.2.3

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-232bd93');
+var VERSION = new _angular_core.Version('4.2.2-1ddbddb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
