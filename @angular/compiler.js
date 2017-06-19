a3b67da37581d487a8b46c72a62c0fef23b9f9cf
Hans Larsen <hans@hansl.ca>
2017-06-19 19:00:18 +0000

    3607a48 docs: remove aio from changelog

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-ab81c1c
+ * @license Angular v4.2.3-3607a48
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-ab81c1c');
+const VERSION = new Version('4.2.3-3607a48');
 
 /**
  * @license
