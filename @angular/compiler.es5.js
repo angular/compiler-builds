84306ce8b77654e785a7fd953e0c7b11c2a1ffb2
Hans Larsen <hans@hansl.ca>
2017-06-19 18:46:30 +0000

    afbb6bb docs: remove aio from changelog

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-4f37f86');
+var VERSION = new Version('4.2.2-afbb6bb');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-4f37f86');
+const VERSION = new Version('4.2.2-afbb6bb');
 
 /**
  * @license
