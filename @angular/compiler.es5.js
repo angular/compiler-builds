223fa40b5b203ca41777523b5eabb43350dbc2a8
Hans Larsen <hans@hansl.ca>
2017-06-19 23:02:30 +0000

    2e8efda fix(aio): leave results panel open when opening search result on new page

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-09512c7
+ * @license Angular v4.2.3-2e8efda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-09512c7');
+var VERSION = new Version('4.2.3-2e8efda');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-09512c7
+ * @license Angular v4.2.3-2e8efda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-09512c7');
+const VERSION = new Version('4.2.3-2e8efda');
 
 /**
  * @license
