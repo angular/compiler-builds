681d3610ce29b9bcb6e9ef081078d89d0b3868d4
Hans <hans@hansl.ca>
2017-06-19 22:44:34 +0000

    2447bd1 fix(aio): leave results panel open when opening search result on new page

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-d3c92a3
+ * @license Angular v4.2.2-2447bd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-d3c92a3
+ * @license Angular v4.2.2-2447bd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-d3c92a3');
+var VERSION = new _angular_core.Version('4.2.2-2447bd1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
