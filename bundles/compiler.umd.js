7eeca8b2d969eee9360b1631a0a42abf9d4eac94
Hans <hans@hansl.ca>
2017-06-19 18:04:18 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-3093c55');
+var VERSION = new _angular_core.Version('4.2.2-4f37f86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
