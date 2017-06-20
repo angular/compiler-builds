564ff773fe9dccf8170323795f88d5d3cb66b276
Hans Larsen <hans@hansl.ca>
2017-06-20 16:38:37 +0000

    4ab7353 fix(forms): roll back breaking change with min/max directives

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-2e8efda
+ * @license Angular v4.2.3-4ab7353
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-2e8efda');
+const VERSION = new Version('4.2.3-4ab7353');
 
 /**
  * @license
