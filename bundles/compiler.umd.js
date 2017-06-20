65f26f0bb8133d8a15292a9886da604478a8c7b9
Hans Larsen <hans@hansl.ca>
2017-06-20 20:33:41 +0000

    f997937 docs(aio): update resource listing

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.3-77860a0
+ * @license Angular v4.2.3-f997937
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.3-77860a0');
+var VERSION = new _angular_core.Version('4.2.3-f997937');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
