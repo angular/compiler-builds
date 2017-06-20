4d13e8bba666d2a390b6bd424cc2b06a57f63f10
Hans <hans@hansl.ca>
2017-06-20 20:25:22 +0000

    009651e refactor(core): remove toString() method from DefaultKeyValueDiffer

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-c59c390
+ * @license Angular v4.2.2-009651e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-c59c390
+ * @license Angular v4.2.2-009651e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-c59c390');
+var VERSION = new _angular_core.Version('4.2.2-009651e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
