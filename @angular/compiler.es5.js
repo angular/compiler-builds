288e383a8e1f4c11ba25a3f04db0a701cad8879c
Hans <hans@hansl.ca>
2017-06-20 20:29:30 +0000

    00874c2 docs(aio): update resource listing

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.2-009651e');
+var VERSION = new Version('4.2.2-00874c2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
