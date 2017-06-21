c3519889ad7743091b6b9d972256c125d4600a42
Hans <hans@hansl.ca>
2017-06-21 23:51:43 +0000

    37cdc4f docs(aio): ward's changes

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4352dd2
+ * @license Angular v4.2.2-37cdc4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-4352dd2
+ * @license Angular v4.2.2-37cdc4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-4352dd2');
+var VERSION = new _angular_core.Version('4.2.2-37cdc4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
