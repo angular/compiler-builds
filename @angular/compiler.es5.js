0a33ab5226d4de505c32c5bcdd61bbc627ff5d0f
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:03:36 +0000

    5c576d3 docs: add documentation for LTS versions

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-68f939e
+ * @license Angular v4.3.0-beta.0-5c576d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-68f939e');
+var VERSION = new Version('4.3.0-beta.0-5c576d3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
