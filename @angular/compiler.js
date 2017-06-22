1d9ae608fe3f44147cb1d3b32b0e4a3efad69945
Hans Larsen <hans@hansl.ca>
2017-06-22 00:48:11 +0000

    1fc0d05 docs: add changelog for 4.3.0-beta.0

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-02a38d3
+ * @license Angular v4.3.0-beta.0-1fc0d05
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-02a38d3');
+const VERSION = new Version('4.3.0-beta.0-1fc0d05');
 
 /**
  * @license
