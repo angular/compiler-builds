382e61f795cad83df348289d134b8496f3189f58
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:06:20 +0000

    8c129d7 docs: Fix some non-code being formatted as code in the router docs

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-5c576d3
+ * @license Angular v4.3.0-beta.0-8c129d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-5c576d3');
+var VERSION = new Version('4.3.0-beta.0-8c129d7');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
