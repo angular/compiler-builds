89938f581653a4cdd000735192a25b66c886337e
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:21:10 +0000

    14d2de1 fix(aio): fix links on `/about` in Firefox

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-849200b
+ * @license Angular v4.3.0-beta.0-14d2de1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-849200b');
+var VERSION = new Version('4.3.0-beta.0-14d2de1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
