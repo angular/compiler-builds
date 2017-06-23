0243972271162780245736f3d25cc7544398f220
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:19:17 +0000

    849200b docs: remove unnecessary newline

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-87206e1
+ * @license Angular v4.3.0-beta.0-849200b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-87206e1');
+const VERSION = new Version('4.3.0-beta.0-849200b');
 
 /**
  * @license
