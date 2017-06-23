26b5bb6a8c71da9f79d134f9299c830bca8aef98
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:40:52 +0000

    3ce9d51 fix(aio): prefix `location.assign` with `window.`

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.0-6191d53
+ * @license Angular v4.3.0-beta.0-3ce9d51
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.0-6191d53');
+var VERSION = new Version('4.3.0-beta.0-3ce9d51');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
