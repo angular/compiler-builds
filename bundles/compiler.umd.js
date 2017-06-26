0a7ed0a68834fbcc53bd94fbc78c86bfc2ba6fd6
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:51:16 +0000

    97bb374 fix(aio): Typo in Setup Anatomy documentation page

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-f365a0f
+ * @license Angular v4.3.0-beta.0-97bb374
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-f365a0f
+ * @license Angular v4.3.0-beta.0-97bb374
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-f365a0f');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-97bb374');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
