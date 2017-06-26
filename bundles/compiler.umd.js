db5162736675d6f6bf31dd5bf94ca2715cc3ecdd
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 18:40:26 +0000

    233044e docs(aio): fix typo

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-8786ba9
+ * @license Angular v4.3.0-beta.0-233044e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-8786ba9
+ * @license Angular v4.3.0-beta.0-233044e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-8786ba9');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-233044e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
