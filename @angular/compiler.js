3de9cd0fc42a8f1550a6b21f024a020e746adc05
Matias Niemelä <matias@yearofmoo.com>
2017-06-26 21:00:24 +0000

    693f79e docs: fix spelling of case variants in naming.md

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-448d9f9
+ * @license Angular v4.3.0-beta.0-693f79e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-448d9f9');
+const VERSION = new Version('4.3.0-beta.0-693f79e');
 
 /**
  * @license
