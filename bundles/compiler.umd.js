18c3de2d861eeaf1ca2c90168242b225fbe399b2
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 00:28:34 +0000

    68fc65d ci: add npm postinstall back to the lint step so node_modules doesn't get out of date

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-693f79e
+ * @license Angular v4.3.0-beta.0-68fc65d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-693f79e
+ * @license Angular v4.3.0-beta.0-68fc65d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-693f79e');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-68fc65d');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
