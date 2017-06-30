17a44c09ca0cbd6081f2f43fd65897ada2dd7a38
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 20:29:15 +0000

    6b6a8e5 docs(aio): Add resources contributor info

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-69ec333
+ * @license Angular v4.3.0-beta.1-6b6a8e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-69ec333');
+var VERSION = new Version('4.3.0-beta.1-6b6a8e5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
