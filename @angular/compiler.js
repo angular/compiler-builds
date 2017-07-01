f2c64e4a9f40e43676a0a904538df7ef8bf66d5b
GitHub <noreply@github.com>
2017-07-01 18:01:27 +0000

    8a1a989 feat(router): add router-level events for GuardsCheck and Resolve (#17601)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-b479ed9
+ * @license Angular v4.3.0-beta.1-8a1a989
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-b479ed9');
+const VERSION = new Version('4.3.0-beta.1-8a1a989');
 
 /**
  * @license
