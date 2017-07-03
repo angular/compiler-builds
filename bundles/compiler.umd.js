1f6479c63d488d2e261a4c5179a278b915c3cfae
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-03 10:11:17 +0000

    9263da5 ci(aio): Fix the payload script only check for changes in aio/scripts

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-dc88e0a
+ * @license Angular v4.3.0-beta.1-9263da5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-dc88e0a
+ * @license Angular v4.3.0-beta.1-9263da5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-dc88e0a');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-9263da5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
