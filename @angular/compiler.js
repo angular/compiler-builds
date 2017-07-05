73f4532ca77432d4b8f6119904b6ae7d55564f54
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:01:57 +0000

    c6b75b0 docs: add animations package to CONTRIBUTING.md (#15413)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-0d7e1a9
+ * @license Angular v4.3.0-beta.1-c6b75b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-0d7e1a9');
+const VERSION = new Version('4.3.0-beta.1-c6b75b0');
 
 /**
  * @license
