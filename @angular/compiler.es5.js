0a975ec0bebf09029311720de041ee9e751580a9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-06 14:16:26 +0000

    71ee0c5 ci(aio): raise polyfill payload limits

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-578bdeb
+ * @license Angular v4.3.0-beta.1-71ee0c5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-578bdeb');
+var VERSION = new Version('4.3.0-beta.1-71ee0c5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
