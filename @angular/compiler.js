d413d49e4144976ee5da9bb7268cde72be63e1e0
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:36:01 +0000

    e1174f3 fix(aio): home & marketing styles cleanup (#17926)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-cb16e9c
+ * @license Angular v4.3.0-beta.1-e1174f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-cb16e9c');
+const VERSION = new Version('4.3.0-beta.1-e1174f3');
 
 /**
  * @license
