cfba6121cd61af094768c218a9e316cf69afc169
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:14:37 +0000

    c81ad9d feat(common): two missing features in HttpClient (#17996)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-37797e2
+ * @license Angular v4.3.0-beta.1-c81ad9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-37797e2');
+const VERSION = new Version('4.3.0-beta.1-c81ad9d');
 
 /**
  * @license
