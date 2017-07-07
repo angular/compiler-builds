6d17ba7d2ed4e58a136ce9df112aec3af9b52ff2
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:04:58 +0000

    3b2d2c4 fix(language-service): do not crash when hovering over a label definitions (#17974)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-227dbbc
+ * @license Angular v4.3.0-beta.1-3b2d2c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-227dbbc');
+var VERSION = new Version('4.3.0-beta.1-3b2d2c4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-227dbbc
+ * @license Angular v4.3.0-beta.1-3b2d2c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-227dbbc');
+const VERSION = new Version('4.3.0-beta.1-3b2d2c4');
 
 /**
  * @license
