cd762aa97d4202d87bd9a72d39a8c86e87ac84eb
Jason Aden <jasonaden@google.com>
2017-07-08 05:15:27 +0000

    b69f0fa docs: add changelog for 4.2.6

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.5-8b8b4cb');
+var VERSION = new Version('4.2.6-b69f0fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-8b8b4cb
+ * @license Angular v4.2.6-b69f0fa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.5-8b8b4cb');
+var VERSION = new _angular_core.Version('4.2.6-b69f0fa');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
