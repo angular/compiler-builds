7cbd196eadf3a99f7558d00c37fea22a0d6e9085
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 13:13:57 +0000

    bd4ec5c revert:build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-406dd45');
+var VERSION = new Version('4.2.6-bd4ec5c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.6-406dd45');
+var VERSION = new _angular_core.Version('4.2.6-bd4ec5c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
