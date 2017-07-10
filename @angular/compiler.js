55766412f0c064968cf4806697abd76c2d1a3bd6
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:53:27 +0000

    ab13f1f build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.6-b69f0fa');
+const VERSION = new Version('4.2.6-ab13f1f');
 
 /**
  * @license
