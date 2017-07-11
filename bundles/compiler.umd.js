5c7ed88d5a40b093898b46f2568a2af97b0fa326
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:19:37 +0000

    09261e6 docs(aio): fix dangling links

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-c6aacf5
+ * @license Angular v4.2.6-09261e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.6-c6aacf5
+ * @license Angular v4.2.6-09261e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.6-c6aacf5');
+var VERSION = new _angular_core.Version('4.2.6-09261e6');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
