01779c9fb70e35d4fe505efee2e828b697f60724
Igor Minar <iminar@google.com>
2017-07-11 19:14:16 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-4d45fe6');
+var VERSION = new Version('4.3.0-rc.0-b6aad07');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
