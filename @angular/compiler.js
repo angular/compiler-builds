d8b6a1cf43caff32e19043d62a4fd16cfa25ac2b
Igor Minar <iminar@google.com>
2017-07-11 19:24:40 +0000

    db3bcc9 docs: fix CHANGELOG typo (#18026)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b9525ec
+ * @license Angular v4.3.0-rc.0-db3bcc9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-b9525ec');
+const VERSION = new Version('4.3.0-rc.0-db3bcc9');
 
 /**
  * @license
