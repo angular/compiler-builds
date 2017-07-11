c9695a4c28b98ebde16b393ce32b28fca6ca0514
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-11 11:13:29 +0000

    c6aacf5 build(aio): remove unused script and GitHub token

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.6-bd4ec5c
+ * @license Angular v4.2.6-c6aacf5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.6-bd4ec5c');
+var VERSION = new Version('4.2.6-c6aacf5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
