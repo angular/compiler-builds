75fb83b5f53fbd4ad0031efddbddf74e9945650d
Igor Minar <iminar@google.com>
2017-07-11 19:42:46 +0000

    1b1a6ba docs: fix 404 link to SFEIR website (#17913)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-df06e8b
+ * @license Angular v4.3.0-rc.0-1b1a6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-df06e8b');
+var VERSION = new Version('4.3.0-rc.0-1b1a6ba');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
