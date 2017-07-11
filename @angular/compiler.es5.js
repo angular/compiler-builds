4250a6df2b9dc0e4c10b2ada290a72c4b21b54dd
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:24:32 +0000

    e8bbf86 build(aio): add terms from heading to the search index

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-b30c5fc
+ * @license Angular v4.3.0-rc.0-e8bbf86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-b30c5fc');
+var VERSION = new Version('4.3.0-rc.0-e8bbf86');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
