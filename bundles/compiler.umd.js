516f44e1970bf244e136b4a4ce8f62627f4121f9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-11 14:29:18 +0000

    e28f097 ci: add aio/content to aio group

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e8bbf86
+ * @license Angular v4.3.0-rc.0-e28f097
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-e8bbf86
+ * @license Angular v4.3.0-rc.0-e28f097
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-e8bbf86');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-e28f097');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
