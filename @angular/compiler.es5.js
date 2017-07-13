cb1e1f8c642e9a38c1938650bebaa0899fb7294c
Igor Minar <iminar@google.com>
2017-07-13 19:34:53 +0000

    ffda3e4 build(aio): simplify the primary overload's anchor

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-a7ea008
+ * @license Angular v4.3.0-rc.0-ffda3e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-a7ea008');
+var VERSION = new Version('4.3.0-rc.0-ffda3e4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
