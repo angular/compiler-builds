86ac9a7534865ddc0053494874b25c0ff396c34f
Igor Minar <iminar@google.com>
2017-07-13 22:51:24 +0000

    45ffe54 fix(tsc-wrapped): support as and class expressions (#16904)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-bc1ea8c
+ * @license Angular v4.3.0-rc.0-45ffe54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-bc1ea8c');
+const VERSION = new Version('4.3.0-rc.0-45ffe54');
 
 /**
  * @license
