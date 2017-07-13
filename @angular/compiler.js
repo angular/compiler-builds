bf5f47004dd5cbbce772759c636a33639e6a0ae5
Igor Minar <iminar@google.com>
2017-07-13 22:48:32 +0000

    bc1ea8c docs: rewrite 'Server Communication' doc guide to use HttpClient (#18079)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-1bf7ba8
+ * @license Angular v4.3.0-rc.0-bc1ea8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-1bf7ba8');
+const VERSION = new Version('4.3.0-rc.0-bc1ea8c');
 
 /**
  * @license
