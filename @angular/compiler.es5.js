84820ff255681a338d637f9f9e170e8f750e5d78
Igor Minar <iminar@google.com>
2017-07-13 22:12:42 +0000

    ce0f4f0 fix(tsc-wrapped): report errors for invalid ast forms (#17994)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-bc3b2ac
+ * @license Angular v4.3.0-rc.0-ce0f4f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-bc3b2ac');
+var VERSION = new Version('4.3.0-rc.0-ce0f4f0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-bc3b2ac
+ * @license Angular v4.3.0-rc.0-ce0f4f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-bc3b2ac');
+const VERSION = new Version('4.3.0-rc.0-ce0f4f0');
 
 /**
  * @license
