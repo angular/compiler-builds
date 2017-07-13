f054d1535fc019d10c938f7e1086ecff04cc9308
Igor Minar <iminar@google.com>
2017-07-13 19:33:33 +0000

    a7ea008 ci: fast-forward to master/HEAD on all circle jobs (#18101)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-edb8375
+ * @license Angular v4.3.0-rc.0-a7ea008
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-edb8375
+ * @license Angular v4.3.0-rc.0-a7ea008
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-edb8375');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-a7ea008');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
