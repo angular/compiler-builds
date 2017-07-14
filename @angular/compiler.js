5b69deae2ebbf47faeda0dae494c07d410d5d32b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 20:02:13 +0000

    3ecc5e5 fix(common): rename HttpXsrfModule to HttpClientXsrfModule

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-f7686d4
+ * @license Angular v4.3.0-rc.0-3ecc5e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-f7686d4');
+const VERSION = new Version('4.3.0-rc.0-3ecc5e5');
 
 /**
  * @license
