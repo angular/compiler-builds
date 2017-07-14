20e17bb9b2aa408a0a355aec93e5f000b17cbc2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 16:53:28 +0000

    8e56c3c docs: fix type error in HttpClient sample

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-7955cac
+ * @license Angular v4.3.0-rc.0-8e56c3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-7955cac');
+var VERSION = new Version('4.3.0-rc.0-8e56c3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
