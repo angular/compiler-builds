1b64b48faf5911769d50bafbadbae575071e959a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-15 08:09:42 +0000

    0bf0c35 build(aio): render type parameters of API function exports

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-97e6901
+ * @license Angular v4.3.0-0bf0c35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-97e6901');
+const VERSION = new Version('4.3.0-0bf0c35');
 
 /**
  * @license
