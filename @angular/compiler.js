ec277f5716fdd24c46ef194bc470db4f26b52d30
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:44:43 +0000

    9f99f4f docs: fix HttpClient sample

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c6ad212
+ * @license Angular v4.3.0-9f99f4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-c6ad212');
+const VERSION = new Version('4.3.0-9f99f4f');
 
 /**
  * @license
