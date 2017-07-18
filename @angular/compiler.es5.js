29f695ff62c2fe357e6c42f3f189f03a82e3a681
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:01:23 +0000

    92179bc fix(aio): do not wrap `<code-tabs>` tab labels

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-cdb069a
+ * @license Angular v4.3.0-92179bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-cdb069a');
+var VERSION = new Version('4.3.0-92179bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-cdb069a
+ * @license Angular v4.3.0-92179bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-cdb069a
+ * @license Angular v4.3.0-92179bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-cdb069a');
+var VERSION = new _angular_core.Version('4.3.0-92179bc');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
