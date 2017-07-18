ad3546f52680bd71321a74234da46e76b2e8cb4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:17:49 +0000

    3b588fe docs: fix typo

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-e03adb9');
+var VERSION = new _angular_core.Version('4.3.0-3b588fe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
