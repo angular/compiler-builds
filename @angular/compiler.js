57855ceeb72f9239da09e5df426ebc5a0cac8e39
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 17:12:45 +0000

    f0a1109 fix(aio): add quote to module

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-c39e7d1
+ * @license Angular v4.3.0-f0a1109
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-c39e7d1');
+const VERSION = new Version('4.3.0-f0a1109');
 
 /**
  * @license
