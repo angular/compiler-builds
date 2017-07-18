ac4920b9eea5e381cf8d62d5cc51571736606d94
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:30:17 +0000

    8076482 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-a5205c6');
+const VERSION = new Version('4.3.0-8076482');
 
 /**
  * @license
