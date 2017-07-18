8115b0bf6d309e38538166aaea85450a864c628f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:39:48 +0000

    d22f8f5 fix(animations): always camelcase style property names that contain auto styles

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-8076482
+ * @license Angular v4.3.0-d22f8f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-8076482
+ * @license Angular v4.3.0-d22f8f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-8076482');
+var VERSION = new _angular_core.Version('4.3.0-d22f8f5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
