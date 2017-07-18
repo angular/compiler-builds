7d1e78ddbe1c9f81bef0ee842b2c3c3c30e02542
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:40:05 +0000

    5c62e30 fix(common): send flushed body as error instead of null

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-256bc8a');
+var VERSION = new Version('4.3.0-5c62e30');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-256bc8a
+ * @license Angular v4.3.0-5c62e30
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-256bc8a');
+var VERSION = new _angular_core.Version('4.3.0-5c62e30');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
