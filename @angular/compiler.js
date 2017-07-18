bdff4ada1e9a4d83f71c79ec700f1d66879c0843
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 16:54:15 +0000

    799bffb docs(aio): fix cheatsheet layout for narrow screens

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-fda607c
+ * @license Angular v4.3.0-799bffb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-fda607c');
+const VERSION = new Version('4.3.0-799bffb');
 
 /**
  * @license
