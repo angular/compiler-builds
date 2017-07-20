91e9722772966c4d68456abe998281ced617c130
Miško Hevery <misko@hevery.com>
2017-07-20 17:26:52 +0000

    8d28191 fix(aio): invalid formatting in architecure.md (#18159)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-31cb418
+ * @license Angular v5.0.0-beta.0-8d28191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-31cb418');
+var VERSION = new Version('5.0.0-beta.0-8d28191');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-31cb418
+ * @license Angular v5.0.0-beta.0-8d28191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-31cb418');
+const VERSION = new Version('5.0.0-beta.0-8d28191');
 
 /**
  * @license
