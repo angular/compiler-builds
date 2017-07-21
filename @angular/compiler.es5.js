b5cddaf7d6e0dd46ca91a5bdcf3b08ba4a14f2f7
Miško Hevery <misko@hevery.com>
2017-07-21 17:47:59 +0000

    e0a9625 test: fix bad merge (#18267)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-a6c635e');
+var VERSION = new Version('5.0.0-beta.0-e0a9625');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
