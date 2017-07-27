803736e074efad455c339f53ff2c3ba290a91ca3
Miško Hevery <misko@hevery.com>
2017-07-27 00:37:53 +0000

    fae47d8 refactor(forms): move value accessor tests into own spec (#18356)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-086f4aa
+ * @license Angular v5.0.0-beta.0-fae47d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-086f4aa');
+const VERSION = new Version('5.0.0-beta.0-fae47d8');
 
 /**
  * @fileoverview added by tsickle
