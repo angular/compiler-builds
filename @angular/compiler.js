fea0ad4002dd59881672eaefef3adf42a2005a20
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-01 18:05:12 +0000

    49cd851 feat(router): add events tracking activation of individual routes

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-82b067f
+ * @license Angular v5.0.0-beta.1-49cd851
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-82b067f');
+const VERSION = new Version('5.0.0-beta.1-49cd851');
 
 /**
  * @fileoverview added by tsickle
