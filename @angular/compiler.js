74453a8bc5c6e450cb2084d41dc8d3fa2ec08270
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-08-02 20:42:58 +0000

    24db1ed docs: add changelog for 4.3.3

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.2-da8bb1b
+ * @license Angular v4.3.3-24db1ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.2-da8bb1b');
+const VERSION = new Version('4.3.3-24db1ed');
 
 /**
  * @license
