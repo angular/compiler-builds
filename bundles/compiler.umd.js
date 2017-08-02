b8a65b633794aa28c54eb5df748c907014e7742f
Victor Berchet <victor@suumit.com>
2017-08-02 23:47:19 +0000

    afe3393 test(common): skip some DatePipe tests in old Chrome where Intl is buggy (#15784)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-b65fe3e
+ * @license Angular v4.3.3-afe3393
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-b65fe3e
+ * @license Angular v4.3.3-afe3393
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-b65fe3e');
+var VERSION = new _angular_core.Version('4.3.3-afe3393');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
