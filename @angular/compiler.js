0cebb7301b1b1be693a49a2a4e405e2b50b24441
Victor Berchet <victor@suumit.com>
2017-08-03 00:08:43 +0000

    65d0888 fix(aio): fix layout of the webpack guide (#18493)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-afe3393
+ * @license Angular v4.3.3-65d0888
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-afe3393');
+const VERSION = new Version('4.3.3-65d0888');
 
 /**
  * @license
