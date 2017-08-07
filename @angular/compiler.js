3b0b79bdd1d943f28689243a4957d5da320d52d1
Victor Berchet <victor@suumit.com>
2017-08-07 19:21:44 +0000

    c0c03dc fix(animations): revert container/queried animations accordingly during cancel (#18516)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-f9f8924
+ * @license Angular v5.0.0-beta.2-c0c03dc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-f9f8924');
+const VERSION = new Version('5.0.0-beta.2-c0c03dc');
 
 /**
  * @fileoverview added by tsickle
