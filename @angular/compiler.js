2a2aa17534ec0b94bcadf51a72d1cb1bd3ed13ac
Victor Berchet <victor@suumit.com>
2017-08-07 18:04:15 +0000

    3212f8c docs(aio): typo & update the bio (#18559)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-c421cca
+ * @license Angular v4.3.3-3212f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-c421cca');
+const VERSION = new Version('4.3.3-3212f8c');
 
 /**
  * @license
