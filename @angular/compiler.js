fb92d690a4f4dc281de6f6636c082b59d4da2bce
Victor Berchet <victor@suumit.com>
2017-08-08 20:03:05 +0000

    6f2038c fix(compiler-cli): fix and re-enble expression lowering (#18570)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-f0a5501
+ * @license Angular v5.0.0-beta.2-6f2038c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-f0a5501');
+const VERSION = new Version('5.0.0-beta.2-6f2038c');
 
 /**
  * @fileoverview added by tsickle
