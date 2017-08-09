2ed71d7c8db4e07433d245f07c7e29ec0a24e191
Victor Berchet <victor@suumit.com>
2017-08-09 23:44:45 +0000

    cea0241 docs: add changelog for 5.0.0-beta.3

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.2-ff5c58b
+ * @license Angular v5.0.0-beta.3-cea0241
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.2-ff5c58b');
+var VERSION = new Version('5.0.0-beta.3-cea0241');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
