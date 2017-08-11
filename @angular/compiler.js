cdcfb20fac1f1305fca6c9177c9b7b2dc5a50d75
Hans <hans@hansl.ca>
2017-08-11 03:49:03 +0000

    679608d refactor(compiler-cli): use the transformer based compiler by default

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-06faac8
+ * @license Angular v5.0.0-beta.3-679608d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -24,7 +24,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-06faac8');
+const VERSION = new Version('5.0.0-beta.3-679608d');
 
 /**
  * @fileoverview added by tsickle
