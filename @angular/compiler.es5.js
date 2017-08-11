4b84275065cf6167879dde1252ffaef9f0463f48
Hans Larsen <hans@hansl.ca>
2017-08-11 20:58:06 +0000

    d7be4f1 perf(aio): update to new version of build-optimizer

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-06e479f
+ * @license Angular v4.3.4-d7be4f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-06e479f');
+var VERSION = new Version('4.3.4-d7be4f1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
