7f0dc7dc0cff6fa2334accbc17b99b134590860a
Hans Larsen <hans@hansl.ca>
2017-08-15 23:22:24 +0000

    1cb6076 build(aio): switch from `@angular/http` to `@angular/common/http`

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-259fc91
+ * @license Angular v4.3.4-1cb6076
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-259fc91');
+var VERSION = new Version('4.3.4-1cb6076');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
