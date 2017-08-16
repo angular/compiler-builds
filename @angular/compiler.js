e4ea3b45a607288e16d5c73cc125b28d98f556da
Hans Larsen <hans@hansl.ca>
2017-08-16 18:04:37 +0000

    a1624f2 fix(forms): re-assigning options should not clear select

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-b2f4d53
+ * @license Angular v4.3.4-a1624f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.4-b2f4d53');
+const VERSION = new Version('4.3.4-a1624f2');
 
 /**
  * @license
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-b2f4d53
+ * @license Angular v4.3.4-a1624f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.4-b2f4d53
+ * @license Angular v4.3.4-a1624f2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.4-b2f4d53');
+var VERSION = new _angular_core.Version('4.3.4-a1624f2');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
