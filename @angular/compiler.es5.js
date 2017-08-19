1e8449e56b28a95c3bb0bbce49089ef97821b1cc
Miško Hevery <misko@hevery.com>
2017-08-19 00:04:38 +0000

    3b571a4 build(aio): do not auto-link code elements already inside a link (#18776)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.5-f9b2905
+ * @license Angular v4.3.5-3b571a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-f9b2905');
+var VERSION = new Version('4.3.5-3b571a4');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-f9b2905
+ * @license Angular v4.3.5-3b571a4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.5-f9b2905');
+const VERSION = new Version('4.3.5-3b571a4');
 
 /**
  * @license
