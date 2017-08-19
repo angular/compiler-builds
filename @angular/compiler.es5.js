021f5a2a825ee540cb7e6546aec2af0d08bb6613
Miško Hevery <misko@hevery.com>
2017-08-19 04:37:49 +0000

    e228f2c fix(compiler-cli): use forward slashes for ts.resolveModuleName (#18784)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-f2a2a6b
+ * @license Angular v5.0.0-beta.4-e228f2c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -326,7 +326,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-f2a2a6b');
+var VERSION = new Version('5.0.0-beta.4-e228f2c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
