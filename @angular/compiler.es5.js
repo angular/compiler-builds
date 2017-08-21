5cd5acc3b2a67239d9fb7f4be8091e7e9c20c451
Miško Hevery <misko@hevery.com>
2017-08-21 18:25:44 +0000

    5a1b9a3 style(animations): format integration spec (#18805)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-7062811
+ * @license Angular v5.0.0-beta.4-5a1b9a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -326,7 +326,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-7062811');
+var VERSION = new Version('5.0.0-beta.4-5a1b9a3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
