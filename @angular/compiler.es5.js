7cc3a0df15fd5b74b7ca79869d2fed13a71be064
Miško Hevery <misko@hevery.com>
2017-08-23 21:12:16 +0000

    f09a266 docs: add changelog for 4.3.6

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.5-641be64
+ * @license Angular v4.3.6-f09a266
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.5-641be64');
+var VERSION = new Version('4.3.6-f09a266');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-641be64
+ * @license Angular v4.3.6-f09a266
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.5-641be64');
+const VERSION = new Version('4.3.6-f09a266');
 
 /**
  * @license
