daaf03156b1e8a5e9e8fe850f6b5927a3219b642
Misko Hevery <misko@hevery.com>
2017-08-25 21:56:48 +0000

    3d2ee6c build: fix build; missing firebase-tools

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.4-1c2f3d4
+ * @license Angular v5.0.0-beta.4-3d2ee6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -326,7 +326,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.4-1c2f3d4');
+var VERSION = new Version('5.0.0-beta.4-3d2ee6c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
