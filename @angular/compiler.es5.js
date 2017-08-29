4bcaa759ce8b807d7a708eb0d7e78d56bde2295e
Miško Hevery <misko@hevery.com>
2017-08-29 16:55:17 +0000

    4878936 feat(common): generate `closure-locale.ts` to tree shake locale data (#18907)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.5-72c7b6e
+ * @license Angular v5.0.0-beta.5-4878936
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -326,7 +326,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-72c7b6e');
+var VERSION = new Version('5.0.0-beta.5-4878936');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-72c7b6e
+ * @license Angular v5.0.0-beta.5-4878936
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-72c7b6e
+ * @license Angular v5.0.0-beta.5-4878936
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -362,7 +362,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-72c7b6e');
+var VERSION = new Version('5.0.0-beta.5-4878936');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
