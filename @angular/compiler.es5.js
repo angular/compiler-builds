c77896d8d02b305b04a1bdbd32282c9b3adea800
Jason Aden <jasonaden@google.com>
2017-08-30 00:46:09 +0000

    8821723 fix(common): fix XSSI prefix stripping by using JSON.parse always (#18466)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.6-dfe2bad
+ * @license Angular v4.3.6-8821723
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.6-dfe2bad');
+var VERSION = new Version('4.3.6-8821723');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
