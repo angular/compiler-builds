0d12564492f3326d4ee87773a2518eb0faad62c2
Victor Berchet <victor@suumit.com>
2017-08-31 01:06:32 +0000

    30d53a8 fix(common): use v4 plurals when importing `DeprecatedI18NPipesModule` (#18955)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-043f104
+ * @license Angular v5.0.0-beta.5-30d53a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-043f104
+ * @license Angular v5.0.0-beta.5-30d53a8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -362,7 +362,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.5-043f104');
+var VERSION = new Version('5.0.0-beta.5-30d53a8');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
