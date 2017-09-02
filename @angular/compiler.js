7fc71f5201af334a37a050aeab26acadb64c87a4
Miško Hevery <misko@hevery.com>
2017-09-02 04:20:55 +0000

    7d137d7 fix(core): complete EventEmitter in QueryList on component destroy (#18902)

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-b8b551c
+ * @license Angular v4.3.6-7d137d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-b8b551c');
+const VERSION = new Version('4.3.6-7d137d7');
 
 /**
  * @license
