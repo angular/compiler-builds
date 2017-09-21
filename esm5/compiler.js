a69295945cdcf86c88090ce3d33bb6622e91581a
Igor Minar <igor@angularjs.org>
2017-09-21 00:13:18 +0000

    4586fcc refactor(upgrade): use correct property name (#19180)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -322,7 +322,7 @@ var Version = (function () {
 /**
  * @stable
  */
-var VERSION = new Version('5.0.0-beta.7-9e84719');
+var VERSION = new Version('5.0.0-beta.7-4586fcc');
 
 /**
  * @license
