ea2cc766b9b10c643544abc8c59c3c0597af4039
Jason Aden <jasonaden@google.com>
2017-09-22 22:56:20 +0000

    125ded2 fix(router): fix param & query param types `any` -> `string | string []`

---
+++ src/config.d.ts
@@ -0,0 +1,19 @@
+import { MissingTranslationStrategy, ViewEncapsulation } from './core';
+export declare class CompilerConfig {
+    defaultEncapsulation: ViewEncapsulation | null;
+    enableLegacyTemplate: boolean;
+    useJit: boolean;
+    jitDevMode: boolean;
+    missingTranslation: MissingTranslationStrategy | null;
+    preserveWhitespaces: boolean;
+    constructor({defaultEncapsulation, useJit, jitDevMode, missingTranslation, enableLegacyTemplate, preserveWhitespaces}?: {
+        defaultEncapsulation?: ViewEncapsulation;
+        useJit?: boolean;
+        jitDevMode?: boolean;
+        missingTranslation?: MissingTranslationStrategy;
+        enableLegacyTemplate?: boolean;
+        preserveWhitespaces?: boolean;
+        fullTemplateTypeCheck?: boolean;
+    });
+}
+export declare function preserveWhitespacesDefault(preserveWhitespacesOption: boolean | null, defaultSetting?: boolean): boolean;
