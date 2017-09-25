65ef508f023b904b2b43ad77ee43a00e872a21e2
Victor Berchet <victor@suumit.com>
2017-09-25 19:34:32 +0000

    5c99b01 build(aio): run the upload server as a non-previleged user

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.3-04bc525');
+var VERSION = new _angular_core.Version('4.4.3-5c99b01');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
