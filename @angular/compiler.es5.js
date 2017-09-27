ffb344aaa574b2d0e84b794cd4651a92121b7cbe
Victor Berchet <victor@suumit.com>
2017-09-27 22:52:36 +0000

    0cefb0b docs(aio): add copy about NgForOf (#18686)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-55a7443
+ * @license Angular v4.4.3-0cefb0b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-55a7443');
+var VERSION = new Version('4.4.3-0cefb0b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-55a7443
+ * @license Angular v4.4.3-0cefb0b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-55a7443');
+const VERSION = new Version('4.4.3-0cefb0b');
 
 /**
  * @license
