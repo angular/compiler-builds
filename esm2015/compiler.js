8d539d21a6142fcd6bb996f8942b291fc815a79f
Victor Berchet <victor@suumit.com>
2017-09-28 20:59:45 +0000

    14e8e88 Revert "refactor(core): Change `abstract get` to `readonly` (#19226)"

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-bed8ac7
+ * @license Angular v5.0.0-beta.7-14e8e88
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -561,7 +561,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-bed8ac7');
+const VERSION = new Version('5.0.0-beta.7-14e8e88');
 
 /**
  * @fileoverview added by tsickle
