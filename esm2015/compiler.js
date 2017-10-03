271f2daeef324c4dbb397e2398553ef3c205c3f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-03 15:27:44 +0000

    7cecaf2 docs(aio): add ngVikings to the events page

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-eef7d8a
+ * @license Angular v5.0.0-rc.0-7cecaf2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -561,7 +561,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.0-eef7d8a');
+const VERSION = new Version('5.0.0-rc.0-7cecaf2');
 
 /**
  * @fileoverview added by tsickle
