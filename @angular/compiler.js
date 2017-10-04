a7633ee51ab13d584ebed8a6bb56286ec273f6a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:32:59 +0000

    f98eb35 docs(aio): document Custom Async validators

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-95f3b1d');
+const VERSION = new Version('4.4.4-f98eb35');
 
 /**
  * @license
