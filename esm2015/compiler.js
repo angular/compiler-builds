3d6327af2afd7f3d58b1ebf6f66bf25268ded13d
Chuck Jazdzewski <chuckj@google.com>
2017-10-06 22:56:59 +0000

    f83989b fix(compiler-cli): produce smaller source maps for templates (#19578)

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-03227e6
+ * @license Angular v5.0.0-rc.1-f83989b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -561,7 +561,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-03227e6');
+const VERSION = new Version('5.0.0-rc.1-f83989b');
 
 /**
  * @fileoverview added by tsickle
