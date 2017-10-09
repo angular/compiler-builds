d0d91fe9ebfea61cfe4b8040c3a5ea002ea04056
Chuck Jazdzewski <chuckj@google.com>
2017-10-09 22:12:48 +0000

    32ca685 build(aio): do not fail on first `yarn setup` (#19603)

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-f393d86
+ * @license Angular v5.0.0-rc.1-32ca685
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -561,7 +561,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-f393d86');
+const VERSION = new Version('5.0.0-rc.1-32ca685');
 
 /**
  * @fileoverview added by tsickle
