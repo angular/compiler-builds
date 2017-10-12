d863e48c411af39c56d0ea374989cb81ba3e04e6
Tobias Bosch <tbosch1009@gmail.com>
2017-10-12 23:29:52 +0000

    653a211 Revert "Revert "Revert "perf(compiler): skip type check and emit in bazel in some cases. (#19646)"""

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-522ec9a
+ * @license Angular v5.0.0-rc.2-653a211
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.0.0-rc.2-522ec9a
+ * @license Angular v5.0.0-rc.2-653a211
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -656,7 +656,7 @@ var Version = (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.2-522ec9a');
+var VERSION = new Version('5.0.0-rc.2-653a211');
 
 /**
  * @fileoverview added by tsickle
@@ -28434,9 +28434,9 @@ var MapPlaceholderNames = (function (_super) {
  * found in the LICENSE file at https://angular.io/license
  */
 var GeneratedFile = (function () {
-    function GeneratedFile(srcFileName, genFileName, sourceOrStmts) {
-        this.srcFileName = srcFileName;
-        this.genFileName = genFileName;
+    function GeneratedFile(srcFileUrl, genFileUrl, sourceOrStmts) {
+        this.srcFileUrl = srcFileUrl;
+        this.genFileUrl = genFileUrl;
         if (typeof sourceOrStmts === 'string') {
             this.source = sourceOrStmts;
             this.stmts = null;
@@ -28455,7 +28455,7 @@ var GeneratedFile = (function () {
      * @return {?}
      */
     function (other) {
-        if (this.genFileName !== other.genFileName) {
+        if (this.genFileUrl !== other.genFileUrl) {
             return false;
         }
         if (this.source) {
@@ -28478,9 +28478,9 @@ var GeneratedFile = (function () {
 function toTypeScript(file, preamble) {
     if (preamble === void 0) { preamble = ''; }
     if (!file.stmts) {
-        throw new Error("Illegal state: No stmts present on GeneratedFile " + file.genFileName);
+        throw new Error("Illegal state: No stmts present on GeneratedFile " + file.genFileUrl);
     }
-    return new TypeScriptEmitter().emitStatements(file.genFileName, file.stmts, preamble);
+    return new TypeScriptEmitter().emitStatements(file.genFileUrl, file.stmts, preamble);
 }
 
 /**
