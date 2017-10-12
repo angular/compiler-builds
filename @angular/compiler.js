3d40adffa558a9086d33b0285aee66584a905393
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 20:55:30 +0000

    32a814b docs: removing unnecessary commits from change log

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-912068e
+ * @license Angular v4.4.5-32a814b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-912068e');
+const VERSION = new Version('4.4.5-32a814b');
 
 /**
  * @license
