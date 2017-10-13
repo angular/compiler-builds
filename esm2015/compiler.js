a3669ef7c34cf71a4dbb3e0543b1bb43f4409dc5
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:35:24 +0000

    e81d1fc docs(animations): add missing bracket to fadeAnimation

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.2-02394d2
+ * @license Angular v5.0.0-rc.2-e81d1fc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -561,7 +561,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.2-02394d2');
+const VERSION = new Version('5.0.0-rc.2-e81d1fc');
 
 /**
  * @fileoverview added by tsickle
