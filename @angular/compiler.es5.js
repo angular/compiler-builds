4e9312e84fa3230da141404c3ee6198ea20c72bf
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 23:17:58 +0000

    630c19f build: remove required BrowserStack run as it fails with “Access denied” (#19769)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-af8c2fa
+ * @license Angular v4.4.5-630c19f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-af8c2fa');
+var VERSION = new Version('4.4.5-630c19f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
