59b0b3f1eb8a649ea8c3f2b5aee5e07b300fd5e4
Tobias Bosch <tbosch1009@gmail.com>
2017-10-17 18:07:16 +0000

    0789601 build: fix broken path for animations in .pullapprove (#19453)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-ce0ac46
+ * @license Angular v4.4.5-0789601
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-ce0ac46');
+var VERSION = new Version('4.4.5-0789601');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
