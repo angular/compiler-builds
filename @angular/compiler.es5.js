d5df5ab27aa839d1f5b579b01f046ac4edc30e00
Tobias Bosch <tbosch1009@gmail.com>
2017-10-19 00:46:11 +0000

    7231f5e docs: add changelog for 4.4.6

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-269f5ac');
+var VERSION = new Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -55,7 +55,7 @@ function __extends(d, b) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.5-269f5ac');
+var VERSION = new _angular_core.Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
