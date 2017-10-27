22ae201687d3d56a545ba20f3a94147b3fdfd7df
Matias Niemelä <matias@yearofmoo.com>
2017-10-27 03:15:58 +0000

    394d951 docs: add changelog for 5.0.0-rc.7

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.6-31797d3
+ * @license Angular v5.0.0-rc.7-394d951
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -567,7 +567,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.6-31797d3');
+const VERSION = new Version('5.0.0-rc.7-394d951');
 
 /**
  * @fileoverview added by tsickle
