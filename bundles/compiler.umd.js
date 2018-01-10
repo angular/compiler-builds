641f59e9883e776a7a2fd2ce9e00e5804580c3f7
Kara Erickson <karakara@google.com>
2018-01-10 20:17:24 +0000

    fc3e7e0 refactor(core): create consistent naming scheme across classes (#21403)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-908129e
+ * @license Angular v5.2.0-fc3e7e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-908129e
+ * @license Angular v5.2.0-fc3e7e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -676,7 +676,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-908129e');
+var VERSION = new Version('5.2.0-fc3e7e0');
 
 /**
  * @fileoverview added by tsickle
