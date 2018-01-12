2b6d0ce7fb7cacc9c31bd6d485f5142cc9db5d0e
Alex Eagle <alexeagle@google.com>
2018-01-12 22:08:38 +0000

    3e3c22f fix(aio): correctly handle redirects (#21416)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-f6db952');
+var VERSION = new Version('5.2.0-3e3c22f');
 
 /**
  * @fileoverview added by tsickle
