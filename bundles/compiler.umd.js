ce6dc3a62fddfd89cc5d0d697c8b07b3290ba1b6
Alex Eagle <alexeagle@google.com>
2018-01-16 15:39:11 +0000

    3bcc0e6 refactor(core): refactor WrappedValue (#20997)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d6ba9f9
+ * @license Angular v5.2.0-3bcc0e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.0-d6ba9f9
+ * @license Angular v5.2.0-3bcc0e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -676,7 +676,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-d6ba9f9');
+var VERSION = new Version('5.2.0-3bcc0e6');
 
 /**
  * @fileoverview added by tsickle
