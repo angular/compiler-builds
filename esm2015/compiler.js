303401db3cdea1522319187d1e130fd308bda352
Alex Eagle <alexeagle@google.com>
2018-01-17 15:34:32 +0000

    7e3f9a4 fix(core): fix chained http call (#20924)

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-fb4d84d
+ * @license Angular v5.2.0-7e3f9a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-fb4d84d');
+const VERSION = new Version('5.2.0-7e3f9a4');
 
 /**
  * @fileoverview added by tsickle
