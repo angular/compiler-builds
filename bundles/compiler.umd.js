efac883e648f30bd42acd4389127cc8b2ece5d9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:29:29 +0000

    2707012 fix(forms): publish missing types (#19941)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-7e51e52
+ * @license Angular v6.0.0-beta.2-2707012
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.2-7e51e52
+ * @license Angular v6.0.0-beta.2-2707012
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -683,7 +683,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.2-7e51e52');
+var VERSION = new Version('6.0.0-beta.2-2707012');
 
 /**
  * @fileoverview added by tsickle
