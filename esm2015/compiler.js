b7d60ee1ff2559bc57158cef9126cf8a50b121e8
Miško Hevery <misko@hevery.com>
2018-02-07 21:40:41 +0000

    47b73fd fix(core): ensure initial value of QueryList length (#21980) (#21982)

--- esm2015/compiler.js
+++ esm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -581,7 +581,7 @@ class Version {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.4-07769e5');
+const VERSION = new Version('5.2.4-47b73fd');
 
 /**
  * @fileoverview added by tsickle
--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-07769e5
+ * @license Angular v5.2.4-47b73fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -633,7 +633,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-07769e5');
+var VERSION = new Version('5.2.4-47b73fd');
 
 /**
  * @fileoverview added by tsickle
