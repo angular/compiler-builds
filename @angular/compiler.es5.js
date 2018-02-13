817139e40b33751eea08b7840d7d626baefd5575
Miško Hevery <misko@hevery.com>
2018-02-13 18:38:20 +0000

    2c5cf19 fix(core): use appropriate inert document strategy for Firefox & Safari (#22077)

--- @angular/compiler.es5.js
+++ @angular/compiler.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.6-7231f5e
+ * @license Angular v4.4.6-2c5cf19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.6-7231f5e');
+var VERSION = new Version('4.4.6-2c5cf19');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
