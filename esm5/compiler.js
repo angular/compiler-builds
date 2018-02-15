233eabe579440e30578c4e70565acdb4bee15f19
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 00:19:18 +0000

    b54ad05 fix(aio): improve printing styles (#19651)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-5bb9fca
+ * @license Angular v6.0.0-beta.4-b54ad05
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -659,7 +659,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-5bb9fca');
+var VERSION = new Version('6.0.0-beta.4-b54ad05');
 
 /**
  * @fileoverview added by tsickle
