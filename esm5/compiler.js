7a5d2e4a5072ae9f10ed256ece2860e3b3e755cc
Victor Berchet <victor@suumit.com>
2018-02-21 23:22:15 +0000

    a7ebf5a fix(core): properly handle function without prototype in reflector (#22284)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b42921b
+ * @license Angular v6.0.0-beta.4-a7ebf5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -659,7 +659,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.4-b42921b');
+var VERSION = new Version('6.0.0-beta.4-a7ebf5a');
 
 /**
  * @fileoverview added by tsickle
