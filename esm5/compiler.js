36db1447f1a9d5276df9143272e962cfb8e7e3fe
Kara Erickson <karakara@google.com>
2018-03-08 05:16:57 +0000

    5d4fa7f test(ivy): add canonical examples of bindings on elements (#22403)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-8fb34bc
+ * @license Angular v6.0.0-beta.7-5d4fa7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -659,7 +659,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-8fb34bc');
+var VERSION = new Version('6.0.0-beta.7-5d4fa7f');
 
 /**
  * @fileoverview added by tsickle
