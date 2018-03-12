96fc5921ee677be1b08f0b0cd97cb8d351d5a649
Kara Erickson <karakara@google.com>
2018-03-12 21:57:25 +0000

    6e00410 fix(compiler-cli): annotate Ivy fields as @nocollapse in closure mode (#22691)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-f95730b
+ * @license Angular v6.0.0-beta.7-6e00410
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-f95730b
+ * @license Angular v6.0.0-beta.7-6e00410
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -702,7 +702,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-f95730b');
+var VERSION = new Version('6.0.0-beta.7-6e00410');
 
 /**
  * @fileoverview added by tsickle
