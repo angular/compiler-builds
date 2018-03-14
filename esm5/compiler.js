a6a9aecda3d95f3bdc5422810f0a89f2e5d717ab
Kara Erickson <karakara@google.com>
2018-03-14 16:16:16 +0000

    37fedd0 feat(core): add task tracking to Testability (#16863)

--- esm5/compiler.js
+++ esm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b1365d1
+ * @license Angular v6.0.0-beta.7-37fedd0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -659,7 +659,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-b1365d1');
+var VERSION = new Version('6.0.0-beta.7-37fedd0');
 
 /**
  * @fileoverview added by tsickle
