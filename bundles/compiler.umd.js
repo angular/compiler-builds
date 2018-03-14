c665a759b459bf861278df21e6274d50889cec6e
Kara Erickson <karakara@google.com>
2018-03-14 06:57:01 +0000

    b1365d1 refactor(ivy): remove directiveRefresh instruction (#22745)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4ac606b
+ * @license Angular v6.0.0-beta.7-b1365d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v6.0.0-beta.7-4ac606b
+ * @license Angular v6.0.0-beta.7-b1365d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -702,7 +702,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4ac606b');
+var VERSION = new Version('6.0.0-beta.7-b1365d1');
 
 /**
  * @fileoverview added by tsickle
