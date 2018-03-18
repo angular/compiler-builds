2142a6346f8a6bb1a03fd3daa57370e0aa5a7b63
Misko Hevery <misko@hevery.com>
2018-03-18 19:18:41 +0000

    e27cfd6 refactor(ivy): split up directiveCreate for tree shaking (#22838)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4f21d37
+ * @license Angular v6.0.0-beta.7-e27cfd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-4f21d37');
+var VERSION = new Version('6.0.0-beta.7-e27cfd6');
 
 /**
  * @fileoverview added by tsickle
