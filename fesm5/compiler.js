a1900bbf046aea4d9f60ae65b1e2b9ace262598d
Igor Minar <igor@angular.io>
2018-03-20 22:49:37 +0000

    3cc5c2e build: update to rxjs@6.0.0-beta.0 (#22887)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e6c731f
+ * @license Angular v6.0.0-beta.7-3cc5c2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-e6c731f');
+var VERSION = new Version('6.0.0-beta.7-3cc5c2e');
 
 /**
  * @fileoverview added by tsickle
