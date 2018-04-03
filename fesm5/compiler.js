006e0c519a419dfd5169d9b9160c4127adf81be5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 20:01:16 +0000

    4506230 docs: update contents to recommend new way of defining providers (#23076)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-18ac228
+ * @license Angular v6.0.0-rc.1-4506230
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-18ac228');
+var VERSION = new Version('6.0.0-rc.1-4506230');
 
 /**
  * @fileoverview added by tsickle
