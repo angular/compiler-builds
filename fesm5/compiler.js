140f56657edde375d0962fdd39364a61c2f3422b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:01:58 +0000

    d37064c test(router): fix typo in expectation (#23137)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-23cc3ef
+ * @license Angular v6.0.0-rc.1-d37064c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -660,7 +660,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-23cc3ef');
+var VERSION = new Version('6.0.0-rc.1-d37064c');
 
 /**
  * @fileoverview added by tsickle
