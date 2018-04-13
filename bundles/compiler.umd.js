4e10973b6bb3f359e455fd0a8eff9338a7be006c
Igor Minar <igor@angular.io>
2018-04-13 20:45:56 +0000

    21f3301 refactor: ensure all 'TODO's are consistent (#23252)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-a3204f8
+ * @license Angular v5.2.9-21f3301
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -44,7 +44,7 @@ var __assign = Object.assign || function __assign(t) {
 };
 
 /**
- * @license Angular v5.2.9-a3204f8
+ * @license Angular v5.2.9-21f3301
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -677,7 +677,7 @@ var Version = /** @class */ (function () {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.9-a3204f8');
+var VERSION = new Version('5.2.9-21f3301');
 
 /**
  * @fileoverview added by tsickle
