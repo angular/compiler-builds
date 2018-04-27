b2fb7ad85b80aa54c4a939243e321587509666b4
Igor Minar <igor@angular.io>
2018-04-27 15:26:58 +0000

    693a7ba test: add i18n to cli-hello-world integration test (#23527)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from './util';
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-91bf598');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.5-693a7ba');
 //# sourceMappingURL=version.js.map
