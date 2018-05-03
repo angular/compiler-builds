59c53acf68d9b9c844e92307bfeb4a87a0478068
Igor Minar <igor@angular.io>
2018-05-03 18:18:48 +0000

    ecde152 build: update to rxjs@6.0.0 (#23679)

--- esm2015/src/version.js
+++ esm2015/src/version.js
@@ -13,5 +13,5 @@ import { Version } from './util';
 /**
  *
  */
-export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-983e5f2');
+export const /** @type {?} */ VERSION = new Version('6.0.0-rc.6-ecde152');
 //# sourceMappingURL=version.js.map
