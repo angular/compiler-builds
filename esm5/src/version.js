30168fbece5f0dddb2902de4edd741fcb19f6e0e
Igor Minar <igor@angular.io>
2018-05-03 23:02:43 +0000

    947ea17 build: update the scripts/release/post-check script for 6.0.x

--- esm5/src/version.js
+++ esm5/src/version.js
@@ -13,5 +13,5 @@ import { Version } from './util';
 /**
  *
  */
-export var /** @type {?} */ VERSION = new Version('6.0.0-a190c45');
+export var /** @type {?} */ VERSION = new Version('6.0.0-947ea17');
 //# sourceMappingURL=version.js.map
