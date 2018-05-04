830bc6d80680d69f0992702a927c1dffae38132c
Igor Minar <igor@angular.io>
2018-05-04 22:32:20 +0000

    0187501 test: fix firebase deployment script test

--- @angular/compiler.js
+++ @angular/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-b19216d
+ * @license Angular v4.4.7-0187501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Attribute, COMPILER_OPTIONS, CUSTOM_ELEME
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.7-b19216d');
+const VERSION = new Version('4.4.7-0187501');
 
 /**
  * @license
