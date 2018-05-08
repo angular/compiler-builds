b956927adb19586486269ad434cf2b6c7d95b42e
Igor Minar <igor@angular.io>
2018-05-08 21:15:58 +0000

    fc03427 fix(core): call ngOnDestroy on all services that have it (#23755)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+140.sha-77ff72f
+ * @license Angular v6.0.0-rc.5+141.sha-fc03427
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1138,7 +1138,7 @@ var Version = /** @class */ (function () {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+140.sha-77ff72f');
+var VERSION = new Version('6.0.0-rc.5+141.sha-fc03427');
 
 /**
  * @license
