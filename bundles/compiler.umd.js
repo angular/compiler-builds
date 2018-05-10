7fcf5a192acbbcdcd9ed244518b7e10dfa2cdfc8
Igor Minar <igor@angular.io>
2018-05-10 23:42:44 +0000

    b1cda36 fix(elements): always check to create strategy (#23825)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+162.sha-fe3679a
+ * @license Angular v6.0.0-rc.5+164.sha-b1cda36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+162.sha-fe3679a');
+var VERSION = new Version('6.0.0-rc.5+164.sha-b1cda36');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+162.sha-fe3679a
+ * @license Angular v6.0.0-rc.5+164.sha-b1cda36
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+162.sha-fe3679a');
+const VERSION = new Version('6.0.0-rc.5+164.sha-b1cda36');
 
 /**
  * @license
