c02d18bd48d8abadc1c6b235063523262dc0dff3
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 21:07:28 +0000

    2a528fc build(aio): display types of API const docs correctly (#23850)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+55.sha-752b83a
+ * @license Angular v6.0.0+56.sha-2a528fc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0+55.sha-752b83a');
+const VERSION = new Version('6.0.0+56.sha-2a528fc');
 
 /**
  * @license
