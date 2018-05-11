c29f99dc724d2d8155c57c881177d221f3cc043f
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 16:46:19 +0000

    8a27a03 docs: update version to 6 in language-service (#20795)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+167.sha-4ecae64
+ * @license Angular v6.0.0-rc.5+168.sha-8a27a03
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+167.sha-4ecae64');
+const VERSION = new Version('6.0.0-rc.5+168.sha-8a27a03');
 
 /**
  * @license
