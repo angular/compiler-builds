5f3aaf8b56759ce86a0125a2e302cefa29fee6db
Matias Niemelä <matias@yearofmoo.com>
2018-05-11 17:00:11 +0000

    56be337 fix(aio): make background transparent in 144x144 PWA icon (#23851)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+53.sha-1b83b3f
+ * @license Angular v6.0.0+54.sha-56be337
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0+53.sha-1b83b3f');
+var VERSION = new Version('6.0.0+54.sha-56be337');
 
 /**
  * @license
