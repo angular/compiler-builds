5a3e3266d7e60f0a1d78ca78923d0bd2925e7054
Igor Minar <igor@angular.io>
2018-05-11 00:07:45 +0000

    089fe83 build: update to latest TypeScript rules (#23828)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+164.sha-b1cda36
+ * @license Angular v6.0.0-rc.5+165.sha-089fe83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+164.sha-b1cda36');
+var VERSION = new Version('6.0.0-rc.5+165.sha-089fe83');
 
 /**
  * @license
