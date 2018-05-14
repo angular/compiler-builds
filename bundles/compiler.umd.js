c563dd7fb75db9029e63b5fb47af6c5a787adb57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-14 20:06:07 +0000

    e9d1709 build: only match version tags for BUILD_SCM_VERSION (#23903)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+59.sha-b5d3de5
+ * @license Angular v6.0.0+60.sha-e9d1709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0+59.sha-b5d3de5');
+var VERSION = new Version('6.0.0+60.sha-e9d1709');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+59.sha-b5d3de5
+ * @license Angular v6.0.0+60.sha-e9d1709
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0+59.sha-b5d3de5');
+const VERSION = new Version('6.0.0+60.sha-e9d1709');
 
 /**
  * @license
