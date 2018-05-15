d8a1ca2200735188900cdc66448e127de268890a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:39:09 +0000

    c6b618d fix(service-worker): deprecate `versionedFiles` in asset-group resources (#23584)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+61.sha-ad6052e
+ * @license Angular v6.0.1+12.sha-c6b618d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0+61.sha-ad6052e');
+const VERSION = new Version('6.0.1+12.sha-c6b618d');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+61.sha-ad6052e
+ * @license Angular v6.0.1+12.sha-c6b618d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0+61.sha-ad6052e');
+var VERSION = new Version('6.0.1+12.sha-c6b618d');
 
 /**
  * @license
