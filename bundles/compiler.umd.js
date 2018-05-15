d1f00623b13825e2bbfd8025fb06a502b84a5681
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 20:09:00 +0000

    a8f6542 release: cut the v6.0.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.1+12.sha-c6b618d
+ * @license Angular v6.0.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.1+12.sha-c6b618d');
+var VERSION = new Version('6.0.2');
 
 /**
  * @license
