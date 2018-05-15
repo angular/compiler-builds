69e122e7c7cb52d427e834714610740014a63413
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:02:57 +0000

    ad6052e test: switch to ts_web_test_suite (#23859)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+60.sha-e9d1709
+ * @license Angular v6.0.0+61.sha-ad6052e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0+60.sha-e9d1709');
+var VERSION = new Version('6.0.0+61.sha-ad6052e');
 
 /**
  * @license
