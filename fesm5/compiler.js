e6a15ac770066729af5be604f2fe55a2059cffe3
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:15 +0000

    daee41a docs(aio): update HTTP error test example (#22844)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+3.sha-68bd45b
+ * @license Angular v6.0.2+4.sha-daee41a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+3.sha-68bd45b');
+var VERSION = new Version('6.0.2+4.sha-daee41a');
 
 /**
  * @license
