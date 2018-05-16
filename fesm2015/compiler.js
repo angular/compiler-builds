0289f2461a9d6d199947dc85b2b4ee8608944ffc
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:07:54 +0000

    eb90039 docs(aio): Expose server and CLI configuration for universal in guide (#23842)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+6.sha-52cbe89
+ * @license Angular v6.0.2+9.sha-eb90039
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+6.sha-52cbe89');
+const VERSION = new Version('6.0.2+9.sha-eb90039');
 
 /**
  * @license
