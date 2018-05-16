bbb914de47c8db4fb5dab467adf6a7279dd4c19e
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 21:48:51 +0000

    6a24c02 docs(aio): Remove Intertech with no courses scheduled (#22867) PR Close #22867

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+187.sha-b7c417f
+ * @license Angular v6.0.0-rc.5+188.sha-6a24c02
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1135,7 +1135,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+187.sha-b7c417f');
+var VERSION = new Version('6.0.0-rc.5+188.sha-6a24c02');
 
 /**
  * @license
