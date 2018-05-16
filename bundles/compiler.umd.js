7bfc738460acb34233b48fccf22a8200b4951006
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:02:22 +0000

    5510341 docs(aio): add Angular Conf Australia to events (#22929)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+191.sha-547efb5
+ * @license Angular v6.0.0-rc.5+192.sha-5510341
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+191.sha-547efb5');
+var VERSION = new Version('6.0.0-rc.5+192.sha-5510341');
 
 /**
  * @license
