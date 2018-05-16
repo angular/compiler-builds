a8431454c9e69f189ac14b9b165255451341c08c
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 21:58:51 +0000

    68bd45b docs(bazel): add a link to the Bazel doc (#22940)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+2.sha-0f64077
+ * @license Angular v6.0.2+3.sha-68bd45b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+2.sha-0f64077');
+const VERSION = new Version('6.0.2+3.sha-68bd45b');
 
 /**
  * @license
