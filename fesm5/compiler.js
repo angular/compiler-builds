6ac9b5f9c9d34411270ca260c87fbbe97aab7fb1
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:03:02 +0000

    f3361ab test: don't run unit tests on Firefox (#23942)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+5.sha-41c2030
+ * @license Angular v6.0.2+7.sha-f3361ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+5.sha-41c2030');
+var VERSION = new Version('6.0.2+7.sha-f3361ab');
 
 /**
  * @license
