c70197206d53d325a7766278a5a3d4386c095a8f
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:00:44 +0000

    547efb5 docs(aio): fix path to observables guide (#23858) PR Close #23858

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+190.sha-091b11a
+ * @license Angular v6.0.0-rc.5+191.sha-547efb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+190.sha-091b11a');
+const VERSION = new Version('6.0.0-rc.5+191.sha-547efb5');
 
 /**
  * @license
