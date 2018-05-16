2221f503a2b3280d3cbce8b33922819aa170e177
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:11:54 +0000

    20d7637 docs(aio): Expose server and CLI configuration for universal in guide (#23842)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+194.sha-8ee25e6
+ * @license Angular v6.0.0-rc.5+195.sha-20d7637
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+194.sha-8ee25e6');
+var VERSION = new Version('6.0.0-rc.5+195.sha-20d7637');
 
 /**
  * @license
