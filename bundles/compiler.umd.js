8b913acc0fcec47a91a19c12a905317600a48e04
Matias Niemelä <matias@yearofmoo.com>
2018-05-16 22:13:35 +0000

    43ee10f docs(aio): changed 'onVoted' output property to 'voted' to be in line with the styleguide (#23832)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+8.sha-5d318ff
+ * @license Angular v6.0.2+10.sha-43ee10f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+8.sha-5d318ff');
+var VERSION = new Version('6.0.2+10.sha-43ee10f');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+8.sha-5d318ff
+ * @license Angular v6.0.2+10.sha-43ee10f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+8.sha-5d318ff');
+const VERSION = new Version('6.0.2+10.sha-43ee10f');
 
 /**
  * @license
