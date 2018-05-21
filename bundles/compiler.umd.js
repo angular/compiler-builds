87c708bea39b7730e41a0e4bfe8aa64dbb561e55
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:45:33 +0000

    95344d6 fix(aio): avoid unnecessary re-calculations in live-examples (#23960)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+17.sha-eec231d');
+var VERSION = new Version('6.0.2+16.sha-95344d6');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+17.sha-eec231d
+ * @license Angular v6.0.2+16.sha-95344d6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+17.sha-eec231d');
+const VERSION = new Version('6.0.2+16.sha-95344d6');
 
 /**
  * @license
