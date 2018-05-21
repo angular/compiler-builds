638a4432a2023bb0c57916af7bf49abe9ea7ee94
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:35:24 +0000

    4007d00 docs(aio): add mhartington to gde (#23777)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+10.sha-43ee10f
+ * @license Angular v6.0.2+12.sha-4007d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+10.sha-43ee10f');
+var VERSION = new Version('6.0.2+12.sha-4007d00');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+10.sha-43ee10f
+ * @license Angular v6.0.2+12.sha-4007d00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+10.sha-43ee10f');
+const VERSION = new Version('6.0.2+12.sha-4007d00');
 
 /**
  * @license
