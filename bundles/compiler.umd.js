70ceca0021b733247fade7c8bac8153e6f1dc5ac
Matias Niemelä <matias@yearofmoo.com>
2018-05-21 20:42:57 +0000

    de267e9 docs(aio): add ant design of angular in resources (#23953)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+203.sha-f8c6947
+ * @license Angular v6.0.0-rc.5+204.sha-de267e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+203.sha-f8c6947');
+var VERSION = new Version('6.0.0-rc.5+204.sha-de267e9');
 
 /**
  * @license
