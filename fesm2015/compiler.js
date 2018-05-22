793aa5ff0d77f64f0acd4691d9850d40399dcada
Matias Niemelä <matias@yearofmoo.com>
2018-05-22 20:59:16 +0000

    7fee1fd docs(aio): fix typo (#23925)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+21.sha-0ee5b7e
+ * @license Angular v6.0.2+22.sha-7fee1fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+21.sha-0ee5b7e');
+const VERSION = new Version('6.0.2+22.sha-7fee1fd');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+21.sha-0ee5b7e
+ * @license Angular v6.0.2+22.sha-7fee1fd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+21.sha-0ee5b7e');
+var VERSION = new Version('6.0.2+22.sha-7fee1fd');
 
 /**
  * @license
