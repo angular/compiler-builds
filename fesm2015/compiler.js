e9111cb2b83a1f2c31738e5949dbc0e64e23c72e
Matias Niemelä <matias@yearofmoo.com>
2018-05-23 00:04:40 +0000

    13cb75d release: cut the v6.0.3 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+215.sha-23a98b9
+ * @license Angular v6.0.0-rc.5+216.sha-13cb75d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+215.sha-23a98b9');
+const VERSION = new Version('6.0.0-rc.5+216.sha-13cb75d');
 
 /**
  * @license
