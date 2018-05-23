d70c768a91ae7539a9ec6073877abe58467c56b2
Matias Niemelä <matias@yearofmoo.com>
2018-05-23 00:02:34 +0000

    1542893 release: cut the v6.0.3 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+23.sha-469b1e4
+ * @license Angular v6.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.2+23.sha-469b1e4');
+var VERSION = new Version('6.0.3');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.2+23.sha-469b1e4
+ * @license Angular v6.0.3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.2+23.sha-469b1e4');
+const VERSION = new Version('6.0.3');
 
 /**
  * @license
