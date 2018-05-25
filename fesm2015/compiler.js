89b6b2bf6bd9d840a5ae8af6c882897569b4122a
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:17:50 +0000

    f206eb9 docs(aio): add Juri Strumpflohner to GDE resources (#24086)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+1.sha-3d6e82e
+ * @license Angular v6.0.3+2.sha-f206eb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+1.sha-3d6e82e');
+const VERSION = new Version('6.0.3+2.sha-f206eb9');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+1.sha-3d6e82e
+ * @license Angular v6.0.3+2.sha-f206eb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+1.sha-3d6e82e');
+var VERSION = new Version('6.0.3+2.sha-f206eb9');
 
 /**
  * @license
