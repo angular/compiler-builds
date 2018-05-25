69afe32794c6632e40a13224b341e211807e6cd8
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:16:44 +0000

    3d6e82e docs(aio): remove outdated rangle link (#24108) PR Close #24108

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3
+ * @license Angular v6.0.3+1.sha-3d6e82e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3');
+var VERSION = new Version('6.0.3+1.sha-3d6e82e');
 
 /**
  * @license
