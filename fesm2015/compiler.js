b38f42d8e72760e8f20354a2fc2b881b2f132f66
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:18:48 +0000

    3e39fef refactor(aio): improve logging output in `update-preview-server.sh` (#24071)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+222.sha-36cc72e
+ * @license Angular v6.0.0-rc.5+223.sha-3e39fef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+222.sha-36cc72e');
+const VERSION = new Version('6.0.0-rc.5+223.sha-3e39fef');
 
 /**
  * @license
