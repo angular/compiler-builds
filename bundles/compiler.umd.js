68623f5459c89d1d4a7db8f9b6cff52e9e72006b
Matias Niemelä <matias@yearofmoo.com>
2018-05-25 18:18:39 +0000

    a29c756 refactor(aio): improve logging output in `update-preview-server.sh` (#24071)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+2.sha-f206eb9
+ * @license Angular v6.0.3+3.sha-a29c756
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+2.sha-f206eb9');
+var VERSION = new Version('6.0.3+3.sha-a29c756');
 
 /**
  * @license
