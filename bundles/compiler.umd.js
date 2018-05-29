e77eee68aa56394550620b342bdaea78ab3efa29
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:14:00 +0000

    e43d3fa build: pick up rules_typescript karma fix (#24184)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+228.sha-729c797
+ * @license Angular v6.0.0-rc.5+231.sha-e43d3fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+228.sha-729c797');
+var VERSION = new Version('6.0.0-rc.5+231.sha-e43d3fa');
 
 /**
  * @license
