91d3a7088fbc7b97dd4cb8e930e36251fc794c65
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:20:34 +0000

    5a02ae2 docs: update lts and labs practices (#23922)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+10.sha-7698afe
+ * @license Angular v6.0.3+18.sha-5a02ae2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+10.sha-7698afe');
+var VERSION = new Version('6.0.3+18.sha-5a02ae2');
 
 /**
  * @license
