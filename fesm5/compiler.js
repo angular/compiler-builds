cbf2823df53ee98caf397fd301573d8e3151e94d
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 23:04:16 +0000

    41cd8f3 refactor(core): use Partial<T> for MetadataOverride (#24103)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+244.sha-3fd3c2a
+ * @license Angular v6.0.0-rc.5+245.sha-41cd8f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1135,7 +1135,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+244.sha-3fd3c2a');
+var VERSION = new Version('6.0.0-rc.5+245.sha-41cd8f3');
 
 /**
  * @license
