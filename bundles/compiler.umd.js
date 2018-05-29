b1b4a13561b518663da06037e3b99087003400b3
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 22:21:26 +0000

    c2e1311 refactor(aio): rename method (loadContainingCustomElements --> loadContainedCustomElements) (#23944)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+234.sha-d74078f
+ * @license Angular v6.0.0-rc.5+241.sha-c2e1311
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
+var VERSION = new Version('6.0.0-rc.5+241.sha-c2e1311');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+234.sha-d74078f
+ * @license Angular v6.0.0-rc.5+241.sha-c2e1311
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
+const VERSION = new Version('6.0.0-rc.5+241.sha-c2e1311');
 
 /**
  * @license
