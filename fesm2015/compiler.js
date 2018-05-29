56b321ac05081b6e45b25a8862cc8924269db73c
Matias Niemelä <matias@yearofmoo.com>
2018-05-29 21:16:49 +0000

    d74078f docs(http): correct spelling error (#23675)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+233.sha-ddd6124
+ * @license Angular v6.0.0-rc.5+234.sha-d74078f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+233.sha-ddd6124');
+const VERSION = new Version('6.0.0-rc.5+234.sha-d74078f');
 
 /**
  * @license
