47d5a66e6a8cbec6a8474aea584b21e938032113
Victor Berchet <victor@suumit.com>
2018-05-30 23:36:05 +0000

    a634a5a Revert "docs: update docs to use HttpClientModule instead of HttpModule (#22727)"

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+34.sha-4122502
+ * @license Angular v6.0.3+35.sha-a634a5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+34.sha-4122502');
+const VERSION = new Version('6.0.3+35.sha-a634a5a');
 
 /**
  * @license
