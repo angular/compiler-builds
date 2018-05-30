a1b1052f8e21a1f626a7cfb59de693952da3e3e8
Victor Berchet <victor@suumit.com>
2018-05-30 22:01:08 +0000

    e9f2203 fix(platform-server): avoid clash between server and client style encapsulation attributes (#24158)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+29.sha-f04aef4
+ * @license Angular v6.0.3+33.sha-e9f2203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+29.sha-f04aef4');
+const VERSION = new Version('6.0.3+33.sha-e9f2203');
 
 /**
  * @license
