131074c3252fdf44e1e814a9661b61cc70b47798
Victor Berchet <victor@suumit.com>
2018-05-30 19:27:17 +0000

    d249852 build: update rules_webtesting (#24198)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+27.sha-707dd59
+ * @license Angular v6.0.3+28.sha-d249852
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+27.sha-707dd59');
+const VERSION = new Version('6.0.3+28.sha-d249852');
 
 /**
  * @license
