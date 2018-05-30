3cdee00856c5ad2b9619f77509260b273364098f
Victor Berchet <victor@suumit.com>
2018-05-30 17:49:14 +0000

    3ed7fc6 docs: update docs to use HttpClientModule instead of HttpModule (#22727)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+254.sha-5b25c07
+ * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+254.sha-5b25c07');
+var VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+254.sha-5b25c07
+ * @license Angular v6.0.0-rc.5+253.sha-3ed7fc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+254.sha-5b25c07');
+const VERSION = new Version('6.0.0-rc.5+253.sha-3ed7fc6');
 
 /**
  * @license
