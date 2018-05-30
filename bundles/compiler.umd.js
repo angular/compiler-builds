66cd14470b4fe15b552bd50fc9ba7d4f1119adeb
Victor Berchet <victor@suumit.com>
2018-05-30 19:26:42 +0000

    707dd59 build: update brotli version in WORKSPACE (#24194)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+26.sha-4d25705
+ * @license Angular v6.0.3+27.sha-707dd59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+26.sha-4d25705');
+var VERSION = new Version('6.0.3+27.sha-707dd59');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+26.sha-4d25705
+ * @license Angular v6.0.3+27.sha-707dd59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+26.sha-4d25705');
+const VERSION = new Version('6.0.3+27.sha-707dd59');
 
 /**
  * @license
