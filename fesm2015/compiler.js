dc9ddb1769caa75add1dc14a4b2c3182c2d3dfeb
Matias Niemelä <matias@yearofmoo.com>
2018-05-30 06:04:39 +0000

    e53179e refactor(ivy): move parent from LNode to TNode (#24189)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+246.sha-31795b6
+ * @license Angular v6.0.0-rc.5+247.sha-e53179e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+246.sha-31795b6');
+const VERSION = new Version('6.0.0-rc.5+247.sha-e53179e');
 
 /**
  * @license
