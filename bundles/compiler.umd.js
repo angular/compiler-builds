c27e7ab1f0e4fd4848ddbd4718178e7dbcffba0f
Victor Berchet <victor@suumit.com>
2018-05-31 00:53:25 +0000

    2f377db style(compiler-cli): fix typo error (#23897) PR Close #23897

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+42.sha-e2e7b49
+ * @license Angular v6.0.3+43.sha-2f377db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+42.sha-e2e7b49');
+var VERSION = new Version('6.0.3+43.sha-2f377db');
 
 /**
  * @license
