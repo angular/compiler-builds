359b2cb503c9c664222839a17ea87c7866da24a9
Victor Berchet <victor@suumit.com>
2018-05-31 00:56:11 +0000

    a10bdef docs(aio): Add GDE Kim Maida to contributors

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+43.sha-2f377db
+ * @license Angular v6.0.3+44.sha-a10bdef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -415,7 +415,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+43.sha-2f377db');
+var VERSION = new Version('6.0.3+44.sha-a10bdef');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+43.sha-2f377db
+ * @license Angular v6.0.3+44.sha-a10bdef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+43.sha-2f377db');
+const VERSION = new Version('6.0.3+44.sha-a10bdef');
 
 /**
  * @license
