40b3d388eb6e6d5771bf5d143066457ccdcc9895
Victor Berchet <victor@suumit.com>
2018-05-31 23:34:41 +0000

    68a799e build(bazel): re-enable packages/upgrade/test:test_web test with static_files in ts_web_test_suite (#24214)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+292.sha-5f178f3
+ * @license Angular v6.0.0-rc.5+293.sha-68a799e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+292.sha-5f178f3');
+const VERSION = new Version('6.0.0-rc.5+293.sha-68a799e');
 
 /**
  * @license
