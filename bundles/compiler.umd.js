4b3d6cf6e0adf3b153df1b5dc574b001624bc513
Victor Berchet <victor@suumit.com>
2018-05-31 17:32:08 +0000

    3647cb7 build: sync g3 exclude list from copybara to ngbot (#24224) PR Close #24224

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+284.sha-49d5de6
+ * @license Angular v6.0.0-rc.5+285.sha-3647cb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+284.sha-49d5de6');
+var VERSION = new Version('6.0.0-rc.5+285.sha-3647cb7');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+284.sha-49d5de6
+ * @license Angular v6.0.0-rc.5+285.sha-3647cb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+284.sha-49d5de6');
+const VERSION = new Version('6.0.0-rc.5+285.sha-3647cb7');
 
 /**
  * @license
