6720646ad969d8d46df358c41e97556399687106
Victor Berchet <victor@suumit.com>
2018-06-02 17:58:42 +0000

    0561b66 fix(ivy): query nodes from different TemplateRefs inserted into one ViewContainerRef (#24254)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+298.sha-5cbcb56
+ * @license Angular v6.0.0-rc.5+299.sha-0561b66
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.0-rc.5+298.sha-5cbcb56');
+var VERSION = new Version('6.0.0-rc.5+299.sha-0561b66');
 
 /**
  * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+298.sha-5cbcb56
+ * @license Angular v6.0.0-rc.5+299.sha-0561b66
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+298.sha-5cbcb56');
+const VERSION = new Version('6.0.0-rc.5+299.sha-0561b66');
 
 /**
  * @license
