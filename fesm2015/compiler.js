90bde0690c165c49b275781aac7c2b835b9d341c
Victor Berchet <victor@suumit.com>
2018-06-04 17:58:44 +0000

    efe49c1 docs(aio): clean up frequent ng-modules (#24025)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+54.sha-a3d9878
+ * @license Angular v6.0.3+55.sha-efe49c1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+54.sha-a3d9878');
+const VERSION = new Version('6.0.3+55.sha-efe49c1');
 
 /**
  * @license
