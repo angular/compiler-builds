445bfb464483065f1698ea1dc960bd8312b51a18
Victor Berchet <victor@suumit.com>
2018-06-04 17:55:33 +0000

    ccaa199 docs(aio): remove an extraneous apostrophe (#24293) PR Close #24293

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+304.sha-0690622
+ * @license Angular v6.0.0-rc.5+305.sha-ccaa199
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+304.sha-0690622');
+const VERSION = new Version('6.0.0-rc.5+305.sha-ccaa199');
 
 /**
  * @license
