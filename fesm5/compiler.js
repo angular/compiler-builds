8340321b74d281cf4a843788242c5394a2fa3dae
Victor Berchet <victor@suumit.com>
2018-06-04 19:38:15 +0000

    0639cb9 fix(aio): remove unnecessary scrollbar in `code-tabs` (#24207)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+55.sha-efe49c1
+ * @license Angular v6.0.3+57.sha-0639cb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+55.sha-efe49c1');
+var VERSION = new Version('6.0.3+57.sha-0639cb9');
 
 /**
  * @license
