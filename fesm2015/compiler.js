14a2c1e936947c0851bb9cb8c1d900c85ccdf6f9
Victor Berchet <victor@suumit.com>
2018-06-05 21:03:38 +0000

    ea3127e build(bazel): ran format (#24279)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+58.sha-faf60d9
+ * @license Angular v6.0.3+62.sha-ea3127e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.3+58.sha-faf60d9');
+const VERSION = new Version('6.0.3+62.sha-ea3127e');
 
 /**
  * @license
