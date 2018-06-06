be021bc7bd53f68df6849007e4d2e4800bebc5c6
Victor Berchet <victor@suumit.com>
2018-06-06 19:27:12 +0000

    41698bf release: cut the v6.0.4 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.3+67.sha-23c50e2
+ * @license Angular v6.0.4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -329,7 +329,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.3+67.sha-23c50e2');
+var VERSION = new Version('6.0.4');
 
 /**
  * @license
