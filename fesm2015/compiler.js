5c581c6e3d335e6e504cc3494ccc2b51b1f5eda2
Victor Berchet <victor@suumit.com>
2018-06-06 05:09:21 +0000

    22b58a7 feat(ivy): add support of ApplicationRef.bootstrapModuleFactory (#23811)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+322.sha-86b13cc
+ * @license Angular v6.0.0-rc.5+323.sha-22b58a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.0-rc.5+322.sha-86b13cc');
+const VERSION = new Version('6.0.0-rc.5+323.sha-22b58a7');
 
 /**
  * @license
