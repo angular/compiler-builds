5e85abdbe931038224f9c55067d2698babd4b741
Miško Hevery <misko@hevery.com>
2018-06-07 23:10:28 +0000

    7031972 docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.4+1.sha-cfa5b65');
+const VERSION = new Version('6.0.4+2.sha-7031972');
 
 /**
  * @license
