10f372977ad30af44629ec30f32aa45aaf0cc1d2
Miško Hevery <misko@hevery.com>
2018-06-07 20:43:20 +0000

    bd02b27 feat(core): expose a Compiler API for accessing module ids from NgModule types (#24258)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+12.sha-e3759f7
+ * @license Angular v6.1.0-beta.0+13.sha-bd02b27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+12.sha-e3759f7');
+const VERSION = new Version('6.1.0-beta.0+13.sha-bd02b27');
 
 /**
  * @license
