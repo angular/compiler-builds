27895297e18a0be1a5ab663ea9d13176c600c835
Miško Hevery <misko@hevery.com>
2018-06-07 22:16:39 +0000

    ca79e11 feat(ivy): a generic visitor which allows prefixing nodes for ngtsc (#24230)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+14.sha-f781f74
+ * @license Angular v6.1.0-beta.0+15.sha-ca79e11
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+14.sha-f781f74');
+const VERSION = new Version('6.1.0-beta.0+15.sha-ca79e11');
 
 /**
  * @license
