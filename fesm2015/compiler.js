ed0131aa3ef7973c816d8fad3d360cbabc2e1dad
Miško Hevery <misko@hevery.com>
2018-06-07 21:05:39 +0000

    f781f74 refactor(ivy): remove need for LContainer.template (#24335)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+13.sha-bd02b27
+ * @license Angular v6.1.0-beta.0+14.sha-f781f74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.0+13.sha-bd02b27');
+const VERSION = new Version('6.1.0-beta.0+14.sha-f781f74');
 
 /**
  * @license
