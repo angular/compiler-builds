697d09f7bfa923f6871131da71754e59ad426cd7
Miško Hevery <misko@hevery.com>
2018-06-09 00:08:41 +0000

    0139173 fix(animations): always render end-state styles for orphaned DOM nodes (#24236)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+6.sha-f3c9954
+ * @license Angular v6.0.4+7.sha-0139173
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.4+6.sha-f3c9954');
+const VERSION = new Version('6.0.4+7.sha-0139173');
 
 /**
  * @license
