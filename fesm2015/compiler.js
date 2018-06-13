41c415a9539211256735287b089899b3ca8e8afe
Miško Hevery <misko@hevery.com>
2018-06-13 23:10:31 +0000

    b91b9ef release: cut the v6.0.5 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+22.sha-9a9a7de
+ * @license Angular v6.0.5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.4+22.sha-9a9a7de');
+const VERSION = new Version('6.0.5');
 
 /**
  * @license
