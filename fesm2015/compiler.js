146da5a05bf7b2f3cfab4df484332336f71742cc
Miško Hevery <misko@hevery.com>
2018-06-14 00:17:23 +0000

    fbe6871 build(aio): add github links to API doc members (#24000)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5
+ * @license Angular v6.0.5+21.sha-fbe6871
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.5');
+const VERSION = new Version('6.0.5+21.sha-fbe6871');
 
 /**
  * @license
