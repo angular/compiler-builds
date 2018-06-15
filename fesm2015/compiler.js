47ad2fbf0b47261a3d58ecb8b9b339d11c3a3ea2
Miško Hevery <misko@hevery.com>
2018-06-15 17:17:55 +0000

    aa66e84 docs: push animation api doc changes back to stable (#24525)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+26.sha-e1bc3f5
+ * @license Angular v6.0.5+27.sha-aa66e84
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.5+26.sha-e1bc3f5');
+const VERSION = new Version('6.0.5+27.sha-aa66e84');
 
 /**
  * @license
