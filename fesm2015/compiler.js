1be4add9646e7ebb943c135f557e462ef2093afe
Miško Hevery <misko@hevery.com>
2018-06-15 00:04:39 +0000

    f5b3661 fix(core): Injector correctly honors the @Self flag (#24520)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+24.sha-3147a92
+ * @license Angular v6.0.5+25.sha-f5b3661
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.5+24.sha-3147a92');
+const VERSION = new Version('6.0.5+25.sha-f5b3661');
 
 /**
  * @license
