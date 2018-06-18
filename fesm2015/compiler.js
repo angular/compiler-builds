1ebfd9d194d0a7a0e0254b89b104d1b869375655
Miško Hevery <misko@hevery.com>
2018-06-18 22:28:00 +0000

    74f07f4 docs: Remove outdated Webpack guide and example (#24478)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.5+27.sha-aa66e84
+ * @license Angular v6.0.5+28.sha-74f07f4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.5+27.sha-aa66e84');
+const VERSION = new Version('6.0.5+28.sha-74f07f4');
 
 /**
  * @license
