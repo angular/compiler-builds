6ce7bea8f5ac20e3526fb5464f1f1663b2b4bb40
Miško Hevery <misko@hevery.com>
2018-06-21 20:53:04 +0000

    7b2b1af fix(ivy): support inputs/outputs in decorator metadata in JIT (#24565)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+8.sha-10da6a4
+ * @license Angular v6.1.0-beta.2+10.sha-7b2b1af
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1135,7 +1135,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+8.sha-10da6a4');
+var VERSION = new Version('6.1.0-beta.2+10.sha-7b2b1af');
 
 /**
  * @license
@@ -17956,7 +17956,7 @@ var R3ResolvedDependencyType;
  */
 function compileFactoryFunction(meta) {
     // Each dependency becomes an invocation of an inject*() function.
-    var args = meta.deps.map(function (dep) { return compileInjectDependency(dep, meta.injectFn, meta.useOptionalParam); });
+    var args = meta.deps.map(function (dep) { return compileInjectDependency(dep, meta.injectFn); });
     // The overall result depends on whether this is construction or function invocation.
     var expr = meta.useNew ? new InstantiateExpr(meta.fnOrClass, args) :
         new InvokeFunctionExpr(meta.fnOrClass, args);
@@ -17965,7 +17965,7 @@ function compileFactoryFunction(meta) {
     var retExpr = meta.extraResults === undefined ? expr : literalArr(__spread([expr], meta.extraResults));
     return fn([], [new ReturnStatement(retExpr)], INFERRED_TYPE, undefined, meta.name + "_Factory");
 }
-function compileInjectDependency(dep, injectFn, useOptionalParam) {
+function compileInjectDependency(dep, injectFn) {
     // Interpret the dependency according to its resolved type.
     switch (dep.resolved) {
         case R3ResolvedDependencyType.Token:
@@ -17987,18 +17987,7 @@ function compileInjectDependency(dep, injectFn, useOptionalParam) {
             // parameters describing how to inject the dependency must be passed to the inject function
             // that's being used.
             if (flags !== 0 /* Default */ || dep.optional) {
-                // Either the dependency is optional, or non-default flags are in use. Either of these cases
-                // necessitates adding an argument for the default value if such an argument is required
-                // by the inject function (useOptionalParam === true).
-                if (useOptionalParam) {
-                    // The inject function requires a default value parameter.
-                    injectArgs.push(dep.optional ? NULL_EXPR : literal(undefined));
-                }
-                // The last parameter is always the InjectFlags, which only need to be specified if they're
-                // non-default.
-                if (flags !== 0 /* Default */) {
-                    injectArgs.push(literal(flags));
-                }
+                injectArgs.push(literal(flags));
             }
             return importExpr(injectFn).callFn(injectArgs);
         }
@@ -18102,7 +18091,6 @@ function compilePipe(outputCtx, pipe, reflector) {
         fnOrClass: outputCtx.importExpr(pipe.type.reference), deps: deps,
         useNew: true,
         injectFn: Identifiers$1.directiveInject,
-        useOptionalParam: false,
     });
     definitionMapValues.push({ key: 'factory', value: templateFactory, quoted: false });
     // e.g. `pure: true`
@@ -19329,7 +19317,6 @@ function baseDirectiveFields(meta, constantPool, bindingParser) {
         deps: meta.deps,
         useNew: true,
         injectFn: Identifiers$1.directiveInject,
-        useOptionalParam: false,
         extraResults: queryDefinitions,
     }));
     // e.g. `hostBindings: (dirIndex, elIndex) => { ... }
@@ -24048,7 +24035,6 @@ function compileInjectable(meta) {
                 fnOrClass: fnOrClass,
                 useNew: useNew,
                 injectFn: Identifiers.inject,
-                useOptionalParam: true,
                 deps: meta.deps,
             });
         }
@@ -24095,7 +24081,6 @@ function compileInjectable(meta) {
             fnOrClass: meta.type,
             useNew: true,
             injectFn: Identifiers.inject,
-            useOptionalParam: true,
             deps: meta.deps,
         });
     }
