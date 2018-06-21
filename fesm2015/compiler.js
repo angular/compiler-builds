75c189da9f323533b15d9a77be4a3532ec3f1781
Miško Hevery <misko@hevery.com>
2018-06-21 00:25:12 +0000

    1f532aa feat(docs-infra): Add GitHub and Twitter external icon links to topnav toolbar (#24542)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6
+ * @license Angular v6.0.6+1.sha-1f532aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.6');
+const VERSION = new Version('6.0.6+1.sha-1f532aa');
 
 /**
  * @license
