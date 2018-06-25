1fbe2fd5c48779b99858c697196d9c7d9f488097
Miško Hevery <misko@hevery.com>
2018-06-25 16:13:59 +0000

    0b356d4 fix(animations): set animations styles properly on platform-server (#24624)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.6+7.sha-a886659
+ * @license Angular v6.0.6+8.sha-0b356d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.6+7.sha-a886659');
+const VERSION = new Version('6.0.6+8.sha-0b356d4');
 
 /**
  * @license
