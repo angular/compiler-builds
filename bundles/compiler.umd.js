525b4f972d03110adf073ff80c695d14ba7f26db
Jason Aden <jasonaden@google.com>
2018-06-25 17:14:26 +0000

    941d2cd test(aio): fix upgrade-phonecat examples e2e tests (#24583)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+26.sha-d244523
+ * @license Angular v6.1.0-beta.2+25.sha-941d2cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1221,7 +1221,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+26.sha-d244523');
+var VERSION = new Version('6.1.0-beta.2+25.sha-941d2cd');
 
 /**
  * @license
