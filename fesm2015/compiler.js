43f1309329c3b9102f8dc769ba2c3eea763d3cce
Miško Hevery <misko@hevery.com>
2018-06-28 22:40:53 +0000

    d89f57f build(docs-infra): render short description of parameters in API docs (#24537)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+3.sha-ac5b69f
+ * @license Angular v6.0.7+4.sha-d89f57f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.7+3.sha-ac5b69f');
+const VERSION = new Version('6.0.7+4.sha-d89f57f');
 
 /**
  * @license
