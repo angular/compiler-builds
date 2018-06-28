0dbeaba5aee3ba24243dc4b1be486e4b924c7f5f
Miško Hevery <misko@hevery.com>
2018-06-28 22:34:44 +0000

    ad68332 docs: consistent spacing in tutorial html files (#23105) (#24497)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7
+ * @license Angular v6.0.7+1.sha-ad68332
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.7');
+const VERSION = new Version('6.0.7+1.sha-ad68332');
 
 /**
  * @license
