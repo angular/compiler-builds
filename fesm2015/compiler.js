fa7d60e6767446782b107d9f07837f28dfbe1d12
Misko Hevery <misko@hevery.com>
2018-06-29 14:29:45 +0000

    0f1de35 docs(core): rephrase doc for Injector.get (#24670)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+4.sha-d89f57f
+ * @license Angular v6.0.7+5.sha-0f1de35
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -291,7 +291,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.7+4.sha-d89f57f');
+const VERSION = new Version('6.0.7+5.sha-0f1de35');
 
 /**
  * @license
