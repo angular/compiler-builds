7fed7446aa1fabedf5e3cc8d7d26c2b50296a778
Miško Hevery <misko@hevery.com>
2018-07-03 21:02:00 +0000

    e3064d5 feat: typescript 2.9 support (#24652)

--- src/aot/util.d.ts
+++ src/aot/util.d.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 export declare function ngfactoryFilePath(filePath: string, forceSourceFile?: boolean): string;
 export declare function stripGeneratedFileSuffix(filePath: string): string;
 export declare function isGeneratedFile(filePath: string): boolean;
