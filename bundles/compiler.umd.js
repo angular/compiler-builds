33df933c0a62fda542c9ec56107ed3fe4920075b
Miško Hevery <misko@hevery.com>
2018-07-06 18:20:35 +0000

    0437598 docs(common): fix in the documentation of PUT (#24528) PR Close #24528

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+22.sha-2d1102f
+ * @license Angular v6.0.7+21.sha-0437598
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -416,7 +416,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+22.sha-2d1102f');
+var VERSION = new Version('6.0.7+21.sha-0437598');
 
 /**
  * @license
