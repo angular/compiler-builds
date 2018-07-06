7b84a85f52891997ecdccc37a9d997953afa4688
Miško Hevery <misko@hevery.com>
2018-07-06 18:16:57 +0000

    25b532e docs: clarify faqs about services (#24079)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+19.sha-b51ce62
+ * @license Angular v6.0.7+18.sha-25b532e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -292,7 +292,7 @@ class Version {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.0.7+19.sha-b51ce62');
+const VERSION = new Version('6.0.7+18.sha-25b532e');
 
 /**
  * @license
