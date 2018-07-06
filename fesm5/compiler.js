ef35b730c46978c605bae1e70b863eb99ec87440
Miško Hevery <misko@hevery.com>
2018-07-06 21:35:29 +0000

    d571a51 build: upgrade karma and related dependencies (#19904)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+56.sha-f936b8c
+ * @license Angular v6.1.0-beta.3+55.sha-d571a51
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+56.sha-f936b8c');
+var VERSION = new Version('6.1.0-beta.3+55.sha-d571a51');
 
 /**
  * @license
