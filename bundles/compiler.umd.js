58ce9cf06c3c71ff03d12ee48cfc9b36a1a20cb6
Miško Hevery <misko@hevery.com>
2018-07-06 18:19:18 +0000

    2d1102f docs: add workspace and related cli terms (#24633)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+20.sha-bc2bf18
+ * @license Angular v6.0.7+22.sha-2d1102f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -416,7 +416,7 @@ var Version = /** @class */ (function () {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+20.sha-bc2bf18');
+var VERSION = new Version('6.0.7+22.sha-2d1102f');
 
 /**
  * @license
