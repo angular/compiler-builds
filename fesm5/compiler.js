ceab2d298ad41ab1831e76c44a40a39477283a09
Miško Hevery <misko@hevery.com>
2018-07-09 22:48:33 +0000

    6881404 fix(language-service): do not overwrite native `Reflect` (#24299)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+61.sha-3980640
+ * @license Angular v6.1.0-beta.3+62.sha-6881404
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __assign, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+61.sha-3980640');
+var VERSION = new Version('6.1.0-beta.3+62.sha-6881404');
 
 /**
  * @license
