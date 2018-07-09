504db95e96345e61b4ce1aff7cb6c8e2b10c8a56
Pete Bacon Darwin <pete@bacondarwin.com>
2018-07-09 12:03:50 +0000

    da016d0 feat(ivy): implement ngcc `Esm5FileParser`

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-beta.3+83.sha-15ca58a
+ * @license Angular v6.1.0-beta.3+84.sha-da016d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __spread, __assign, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+83.sha-15ca58a');
+var VERSION = new Version('6.1.0-beta.3+84.sha-da016d0');
 
 /**
  * @license
