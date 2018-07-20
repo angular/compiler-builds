a44b898e398e5450679879bcb1615c134b8be2a9
Victor Berchet <victor@suumit.com>
2018-07-20 19:24:25 +0000

    2a5a05b fix(docs-infra): fix table header layout in API pages (#24919)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+25.sha-4a29100
+ * @license Angular v6.0.9+26.sha-2a5a05b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -286,7 +286,7 @@ class Version {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.9+25.sha-4a29100');
+const VERSION = new Version('6.0.9+26.sha-2a5a05b');
 
 /**
  * @license
