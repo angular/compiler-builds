cbde7cf7780533a4e297ba05180d8b746fa695d6
Victor Berchet <victor@suumit.com>
2018-07-23 19:13:46 +0000

    060867d build: Fix windows tests (#24927)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.0.9+38.sha-4d840cb
+ * @license Angular v6.0.9+39.sha-060867d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __spread, __assign, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -324,7 +324,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.0.9+38.sha-4d840cb');
+var VERSION = new Version('6.0.9+39.sha-060867d');
 
 /**
  * @license
