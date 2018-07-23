4e12d863a5089376b278be2e26c848d35412c236
Victor Berchet <victor@suumit.com>
2018-07-23 16:42:40 +0000

    6b859da fix(core): stop reusing provider definitions across NgModuleRef instances (#25022)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0-rc.3+43.sha-7960d18
+ * @license Angular v6.1.0-rc.3+44.sha-6b859da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+43.sha-7960d18');
+var VERSION = new Version('6.1.0-rc.3+44.sha-6b859da');
 
 /**
  * @license
