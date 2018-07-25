f9f200d97f50745115c9a5965ccdec0ef10c1479
Victor Berchet <victor@suumit.com>
2018-07-25 15:24:48 +0000

    ae7b96b docs: refactor style guide example 03-06 (#24996)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+50.sha-01b6aad
+ * @license Angular v6.0.9+51.sha-ae7b96b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -286,7 +286,7 @@ class Version {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.9+50.sha-01b6aad');
+const VERSION = new Version('6.0.9+51.sha-ae7b96b');
 
 /**
  * @license
