70199afc69a85099a4eff702c61be2d0365e1d76
Victor Berchet <victor@suumit.com>
2018-07-25 05:34:33 +0000

    01b6aad Revert "docs: refactor style guide example 03-06 (#24996)"

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.9+49.sha-40cdab2
+ * @license Angular v6.0.9+50.sha-01b6aad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -286,7 +286,7 @@ class Version {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.0.9+49.sha-40cdab2');
+const VERSION = new Version('6.0.9+50.sha-01b6aad');
 
 /**
  * @license
