5eacb7470db95184393bb339e57678421b51864b
Igor Minar <igor@angular.io>
2018-08-01 20:29:07 +0000

    1dd2eaa docs: fix typos and missing word in tutorial (#20764)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+73.sha-166ddaa
+ * @license Angular v6.1.0+80.sha-1dd2eaa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __spread, __values, __assign, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+73.sha-166ddaa');
+var VERSION = new Version('6.1.0+80.sha-1dd2eaa');
 
 /**
  * @license
