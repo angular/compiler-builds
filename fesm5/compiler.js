0533f9d741b705ad01ead3ef7153f09f68cef605
Igor Minar <igor@angular.io>
2018-08-01 20:09:17 +0000

    3ab31a4 docs: update to account for CLI changes (#25223)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+23.sha-43dcf77
+ * @license Angular v6.1.0+24.sha-3ab31a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __assign, __spread, __values, __extends, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+23.sha-43dcf77');
+var VERSION = new Version('6.1.0+24.sha-3ab31a4');
 
 /**
  * @license
