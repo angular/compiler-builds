529f2fc47cc01f8430c483dc132c98d9663dd2d0
Igor Minar <igor@angular.io>
2018-08-01 20:22:21 +0000

    fe5e8b7 docs(aio): update Kendo UI description in resource.json (#24845)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+33.sha-3d3a1a4
+ * @license Angular v6.1.0+37.sha-fe5e8b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+33.sha-3d3a1a4');
+var VERSION = new Version('6.1.0+37.sha-fe5e8b7');
 
 /**
  * @license
