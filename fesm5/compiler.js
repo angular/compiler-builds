145c0974349ceb8fc8dda656ceb0d14009a9657f
Igor Minar <igor@angular.io>
2018-08-01 20:13:22 +0000

    0452712 docs: refactor lazy loading modules example (#25071)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+30.sha-ec31f6b
+ * @license Angular v6.1.0+31.sha-0452712
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __spread, __values, __assign, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+30.sha-ec31f6b');
+var VERSION = new Version('6.1.0+31.sha-0452712');
 
 /**
  * @license
