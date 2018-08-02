107b81bf0446bd9a0bacfb8dc0975ea99fdfbbe4
Igor Minar <igor@angular.io>
2018-08-02 00:37:47 +0000

    20b4530 docs: update reactiveconf 2018 in events (#24739)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.0+87.sha-06a1974
+ * @license Angular v6.1.0+88.sha-20b4530
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0+87.sha-06a1974');
+var VERSION = new Version('6.1.0+88.sha-20b4530');
 
 /**
  * @license
