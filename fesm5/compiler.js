591fdf98b59ce56e7e6f8ae3654dd30ddbe6979d
Kara Erickson <karakara@google.com>
2018-08-06 19:22:01 +0000

    ae9b4e6 fix(router): take base uri into account in `setUpLocationSync()` (#20244)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.1+6.sha-478eca3
+ * @license Angular v6.1.1+7.sha-ae9b4e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.1+6.sha-478eca3');
+var VERSION = new Version('6.1.1+7.sha-ae9b4e6');
 
 /**
  * @license
