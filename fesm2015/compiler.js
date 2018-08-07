3357933c84e9bf2cb523d8c7bfd1b507fd892af0
Kara Erickson <karakara@google.com>
2018-08-07 20:05:16 +0000

    fefc860 fix(ivy): fix bug with banana-in-a-box expressions in nested templates (#25321)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.0+44.sha-02e201a
+ * @license Angular v7.0.0-beta.0+45.sha-fefc860
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1079,7 +1079,7 @@ class Version {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.0.0-beta.0+44.sha-02e201a');
+const VERSION = new Version('7.0.0-beta.0+45.sha-fefc860');
 
 /**
  * @license
@@ -14932,13 +14932,28 @@ class _AstToIrVisitor {
     }
     visitPropertyWrite(ast, mode) {
         const receiver = this._visit(ast.receiver, _Mode.Expression);
+        let varExpr = null;
         if (receiver === this._implicitReceiver) {
-            const varExpr = this._getLocal(ast.name);
-            if (varExpr) {
-                throw new Error('Cannot assign to a reference or variable!');
+            const localExpr = this._getLocal(ast.name);
+            if (localExpr) {
+                if (localExpr instanceof ReadPropExpr) {
+                    // If the local variable is a property read expression, it's a reference
+                    // to a 'context.property' value and will be used as the target of the
+                    // write expression.
+                    varExpr = localExpr;
+                }
+                else {
+                    // Otherwise it's an error.
+                    throw new Error('Cannot assign to a reference or variable!');
+                }
             }
         }
-        return convertToStatementIfNeeded(mode, receiver.prop(ast.name).set(this._visit(ast.value, _Mode.Expression)));
+        // If no local expression could be produced, use the original receiver's
+        // property as the target.
+        if (varExpr === null) {
+            varExpr = receiver.prop(ast.name);
+        }
+        return convertToStatementIfNeeded(mode, varExpr.set(this._visit(ast.value, _Mode.Expression)));
     }
     visitSafePropertyRead(ast, mode) {
         return this.convertSafeAccess(ast, this.leftMostSafeNode(ast), mode);
