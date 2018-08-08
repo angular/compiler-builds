a2ae4f198ec3009a10e406658d66869d558078bb
Kara Erickson <karakara@google.com>
2018-08-08 21:53:11 +0000

    638ff76 docs: add ngrx book to the docs (#23389)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
+ * @license Angular v7.0.0-beta.1+4.sha-638ff76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __values, __extends, __read } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+7.sha-a3f1e2c');
+var VERSION = new Version('7.0.0-beta.1+4.sha-638ff76');
 
 /**
  * @license
