5b23820b4ea92f5c2d06959bfb20c7904bd93248
Ben Lesh <ben@benlesh.com>
2018-08-14 05:59:54 +0000

    c869b14 build: drop unused re-export of json_marshal. (#25449)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.1+29.sha-e0314b5
+ * @license Angular v7.0.0-beta.1+30.sha-c869b14
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __values, __extends, __read } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+29.sha-e0314b5');
+var VERSION = new Version('7.0.0-beta.1+30.sha-c869b14');
 
 /**
  * @license
