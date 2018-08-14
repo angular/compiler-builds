f277bce3f34143da463a87b23e05250cb0a12eb5
Ben Lesh <ben@benlesh.com>
2018-08-14 05:49:13 +0000

    5f08bdf ci: github robot should enforce that all requested reviews are submitted (#25336)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+11.sha-151e4b9
+ * @license Angular v6.1.2+13.sha-5f08bdf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __values, __extends, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.2+11.sha-151e4b9');
+var VERSION = new Version('6.1.2+13.sha-5f08bdf');
 
 /**
  * @license
