cc15858f9a7973e8ff71cfa9fec01ae86e3a404f
Ben Lesh <ben@benlesh.com>
2018-08-14 20:13:34 +0000

    e906bf4 docs: add Accelebrate to resources (#23204)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+12.sha-f1ed022
+ * @license Angular v6.1.2+14.sha-e906bf4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.2+12.sha-f1ed022');
+var VERSION = new Version('6.1.2+14.sha-e906bf4');
 
 /**
  * @license
