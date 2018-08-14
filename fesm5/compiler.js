76ea0eaea7a93f9ea5082ce666e7c71111c463a5
Ben Lesh <ben@benlesh.com>
2018-08-14 05:38:43 +0000

    151e4b9 docs: add link to Yarn in README (#24856)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.2+9.sha-cb05f9b
+ * @license Angular v6.1.2+11.sha-151e4b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __assign, __spread, __values, __extends, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.2+9.sha-cb05f9b');
+var VERSION = new Version('6.1.2+11.sha-151e4b9');
 
 /**
  * @license
