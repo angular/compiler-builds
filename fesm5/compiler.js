6ea475987b3d7d2eeeac4b9d762062cb81d8c72a
Jason Aden <jasonaden@google.com>
2018-08-21 00:52:33 +0000

    46b0ce9 refactor(bazel): allow and ignore extra args for _ts_expected_outs (#25558)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.3+19.sha-78750a7
+ * @license Angular v6.1.3+20.sha-46b0ce9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __assign, __spread, __values, __extends, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.3+19.sha-78750a7');
+var VERSION = new Version('6.1.3+20.sha-46b0ce9');
 
 /**
  * @license
