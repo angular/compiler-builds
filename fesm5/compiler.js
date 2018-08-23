f4852b2f21cca3efe4b78de99953f164491513a9
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 22:40:31 +0000

    4e82a76 docs(elements): fix typo (tranformation --> transformation) (#25600)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.4+3.sha-51d5b43
+ * @license Angular v6.1.4+4.sha-4e82a76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.4+3.sha-51d5b43');
+var VERSION = new Version('6.1.4+4.sha-4e82a76');
 
 /**
  * @license
