577bef121913a72e040cbde486d6cd20cc94eabf
Matias Niemelä <matias@yearofmoo.com>
2018-08-23 03:07:54 +0000

    cc0d0a9 fix(docs-infra): fix closure warning issue for improper internal flag (#25628)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.4+1.sha-82f26fe
+ * @license Angular v6.1.4+2.sha-cc0d0a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1125,7 +1125,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.4+1.sha-82f26fe');
+var VERSION = new Version('6.1.4+2.sha-cc0d0a9');
 
 /**
  * @license
@@ -20383,7 +20383,7 @@ var StaticSymbolResolver = /** @class */ (function () {
             }
         }
     };
-    /* @internal */
+    /** @internal */
     StaticSymbolResolver.prototype.ignoreErrorsFor = function (cb) {
         var recorder = this.errorRecorder;
         this.errorRecorder = function () { };
