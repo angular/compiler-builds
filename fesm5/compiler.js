1cd92875021d7e63a5c4da392f6bcf90703e6124
Misko Hevery <misko@hevery.com>
2018-09-04 20:55:58 +0000

    4fe369e docs: add pwa keyword to service worker page (#25725) PR Close #25725

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+11.sha-d8930bb
+ * @license Angular v6.1.6+12.sha-4fe369e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __assign, __spread, __values, __extends, __read } from 'tslib';
 
 /**
  * @license
@@ -1130,7 +1130,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+11.sha-d8930bb');
+var VERSION = new Version('6.1.6+12.sha-4fe369e');
 
 /**
  * @license
