0898acfa5feda5c61bdaf95b46de96ca284a5589
Misko Hevery <misko@hevery.com>
2018-09-05 19:25:23 +0000

    92298e5 build(docs-infra): do not render internals in package API pages (#25723)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+19.sha-23a96dc
+ * @license Angular v6.1.6+17.sha-92298e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __assign, __spread, __extends, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1130,7 +1130,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+19.sha-23a96dc');
+var VERSION = new Version('6.1.6+17.sha-92298e5');
 
 /**
  * @license
