c0bcb96596fe7fd8fe47bff5ec04a9e0b657f88a
Igor Minar <igor@angular.io>
2018-09-06 23:20:07 +0000

    b9a5ce1 docs(service-worker): update http-server command (#25845) PR Close #25845

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v6.1.6+28.sha-d1063c6
+ * @license Angular v6.1.7+8.sha-b9a5ce1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __assign, __spread, __extends, __values, __read } from 'tslib';
+import { __extends, __spread, __assign, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -1130,7 +1130,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.6+28.sha-d1063c6');
+var VERSION = new Version('6.1.7+8.sha-b9a5ce1');
 
 /**
  * @license
