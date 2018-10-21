5aa081ac3bdb22055b838ce8888a27aed7441f64
Igor Minar <igor@angular.io>
2018-10-21 04:25:49 +0000

    100c7ef build: fix ivy package-dist generation (#26629)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+137.sha-4c2ce4e
+ * @license Angular v7.0.0-rc.1+156.sha-100c7ef.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0-rc.1+137.sha-4c2ce4e');
+    var VERSION = new Version('7.0.0-rc.1+156.sha-100c7ef.with-local-changes');
 
     /**
      * @license
