e8185d3107150a9cd7dc326a135d16fc12fc3280
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-10-23 21:57:11 +0000

    b52c97c ci: disable ivy CI jobs on the patch branch (#26702)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0+67.sha-929739c.with-local-changes
+ * @license Angular v7.0.0+68.sha-b52c97c.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.0.0+67.sha-929739c.with-local-changes');
+    var VERSION = new Version('7.0.0+68.sha-b52c97c.with-local-changes');
 
     /**
      * @license
