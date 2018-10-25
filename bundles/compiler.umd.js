286501865d071f12ab23ab1d86c7cf66adb0dca7
Matias Niemelä <matias@yearofmoo.com>
2018-10-25 00:15:48 +0000

    1880c95 ci: only publish builds if relevant `aio` jobs pass (#26722)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+2.sha-13a803d
+ * @license Angular v7.1.0-beta.0+3.sha-1880c95
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+2.sha-13a803d');
+    var VERSION = new Version('7.1.0-beta.0+3.sha-1880c95');
 
     /**
      * @license
