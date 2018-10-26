4eaaa8115ee813f5d1965ecafcefefe4080e562a
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 21:53:45 +0000

    d50d400 build: fix formatting issue in host_binding_spec file

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+35.sha-19fcfc3
+ * @license Angular v7.1.0-beta.0+36.sha-d50d400
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1130,7 +1130,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.1.0-beta.0+35.sha-19fcfc3');
+var VERSION = new Version('7.1.0-beta.0+36.sha-d50d400');
 
 /**
  * @license
