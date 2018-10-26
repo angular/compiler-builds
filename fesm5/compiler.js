0d46782d9c36c326f97cb9a96784d208002a8312
Matias Niemelä <matias@yearofmoo.com>
2018-10-26 17:39:53 +0000

    7c730fe build(docs-infra): include inherited members in search index (#26676)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+18.sha-aefa06f
+ * @license Angular v7.1.0-beta.0+20.sha-7c730fe
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1130,7 +1130,7 @@ var Version = /** @class */ (function () {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.1.0-beta.0+18.sha-aefa06f');
+var VERSION = new Version('7.1.0-beta.0+20.sha-7c730fe');
 
 /**
  * @license
