074033301e6ebab7ccd27962b8a9cb37732370fa
Matias Niemelä <matias@yearofmoo.com>
2018-10-29 17:30:47 +0000

    ef6f160 ci(docs-infra): notify caretaker about `aio_monitoring` failures (#26649)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+44.sha-95993e1
+ * @license Angular v7.1.0-beta.0+53.sha-ef6f160
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+44.sha-95993e1');
+    var VERSION = new Version('7.1.0-beta.0+53.sha-ef6f160');
 
     /**
      * @license
