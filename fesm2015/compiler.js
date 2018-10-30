3db72c4fd8db6385da01bef96dcfcb39ab82422f
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 22:59:35 +0000

    332394d build(bazel): generalize fix for AMD name for flat module out file (#26837)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+73.sha-62b4ff52
+ * @license Angular v7.1.0-beta.0+74.sha-332394d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1084,7 +1084,7 @@ class Version {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('7.1.0-beta.0+73.sha-62b4ff52');
+const VERSION = new Version('7.1.0-beta.0+74.sha-332394d');
 
 /**
  * @license
