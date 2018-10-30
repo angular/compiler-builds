0b2ed602c1d72172a41e8b1763aa3e0c95340fc2
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:53:28 +0000

    87a7f2d docs: fix typo (#26773) PR Close #26773

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+69.sha-8fc4ae5
+ * @license Angular v7.1.0-beta.0+70.sha-87a7f2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION = new Version('7.1.0-beta.0+69.sha-8fc4ae5');
+    var VERSION = new Version('7.1.0-beta.0+70.sha-87a7f2d');
 
     /**
      * @license
