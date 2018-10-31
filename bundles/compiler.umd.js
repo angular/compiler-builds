fcc965df138190e33f20b4dfbd9a1ac787be0927
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:52:04 +0000

    2c25d29 docs: release notes for the v7.0.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
+ * @license Angular v7.1.0-beta.0+79.sha-2c25d29
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13725,7 +13725,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.0+78.sha-ec29fd3');
+    var VERSION$1 = new Version('7.1.0-beta.0+79.sha-2c25d29');
 
     /**
      * @license
