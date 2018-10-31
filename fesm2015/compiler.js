c887dc75d713d274ff26ab145d4408e21f10b1a2
Matias Niemelä <matias@yearofmoo.com>
2018-10-31 23:28:24 +0000

    ec29fd3 build: remove problematic `yarn` prefix for running bazel

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+77.sha-d042c4a
+ * @license Angular v7.1.0-beta.0+78.sha-ec29fd3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12882,7 +12882,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0-beta.0+77.sha-d042c4a');
+const VERSION$1 = new Version('7.1.0-beta.0+78.sha-ec29fd3');
 
 /**
  * @license
