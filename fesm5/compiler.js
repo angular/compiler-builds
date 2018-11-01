5a3129214180f8f4ac2be0f51e162faf0e24c7bd
Kara Erickson <karakara@google.com>
2018-11-01 21:38:38 +0000

    bc68b59 test(ivy): ngcc - test compiling a CLI generated project (#26403)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.1+19.sha-eb5d308
+ * @license Angular v7.1.0-beta.1+31.sha-bc68b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -13671,7 +13671,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+19.sha-eb5d308');
+var VERSION$1 = new Version('7.1.0-beta.1+31.sha-bc68b59');
 
 /**
  * @license
