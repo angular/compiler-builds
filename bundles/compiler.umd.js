2309ad80eec37e9044dcfdf9132f57b34931fc69
Kara Erickson <karakara@google.com>
2018-11-01 21:46:26 +0000

    3b9bc73 fix(ivy): host bindings after dirs without host bindings should work (#26801)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+31.sha-bc68b59
+ * @license Angular v7.1.0-beta.1+37.sha-3b9bc73
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13728,7 +13728,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+31.sha-bc68b59');
+    var VERSION$1 = new Version('7.1.0-beta.1+37.sha-3b9bc73');
 
     /**
      * @license
