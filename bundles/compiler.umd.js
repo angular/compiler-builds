9145b22ab15ee3f9ded7e91bdde2566b6c067b69
Kara Erickson <karakara@google.com>
2018-11-01 23:29:15 +0000

    496372d fix(router): remove type bludgeoning of context and outlet when running CanDeactivate (#26496)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+51.sha-e9e804f
+ * @license Angular v7.1.0-beta.1+52.sha-496372d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13728,7 +13728,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+51.sha-e9e804f');
+    var VERSION$1 = new Version('7.1.0-beta.1+52.sha-496372d');
 
     /**
      * @license
