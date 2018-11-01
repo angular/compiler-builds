69eb402ead69b8abc6258be71ad283b06edba1a7
Kara Erickson <karakara@google.com>
2018-11-01 21:13:07 +0000

    eb5d308 build: update `canonical-path` dependency (#26719)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+18.sha-683d53d
+ * @license Angular v7.1.0-beta.1+19.sha-eb5d308
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13728,7 +13728,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+18.sha-683d53d');
+    var VERSION$1 = new Version('7.1.0-beta.1+19.sha-eb5d308');
 
     /**
      * @license
