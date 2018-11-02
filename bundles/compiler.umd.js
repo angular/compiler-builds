5332df24e5f573700f1c38d138c69607085ba14d
Kara Erickson <karakara@google.com>
2018-11-02 18:10:01 +0000

    affcbbd feat(docs-infra): add getting started widgets (#26059)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+61.sha-c016066
+ * @license Angular v7.1.0-beta.1+53.sha-affcbbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13728,7 +13728,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+61.sha-c016066');
+    var VERSION$1 = new Version('7.1.0-beta.1+53.sha-affcbbd');
 
     /**
      * @license
