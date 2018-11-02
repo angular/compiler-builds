f6f9079e68509c0e16c4e443a909ed1cd2bc39a8
Kara Erickson <karakara@google.com>
2018-11-02 20:46:57 +0000

    516af6c docs: minor edits and corrections to cli intro (#26654)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+63.sha-9c2d0d0
+ * @license Angular v7.1.0-beta.1+64.sha-516af6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13671,7 +13671,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+63.sha-9c2d0d0');
+var VERSION$1 = new Version('7.1.0-beta.1+64.sha-516af6c');
 
 /**
  * @license
