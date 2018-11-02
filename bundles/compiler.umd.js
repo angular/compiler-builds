f896c60d3fce5b61501d96f1e66d0390f82f3dc3
Kara Erickson <karakara@google.com>
2018-11-02 18:01:11 +0000

    fe1c0d7 feat(docs-infra): add getting started widgets (#26059)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+9.sha-dc05385
+ * @license Angular v7.0.2+10.sha-fe1c0d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13703,7 +13703,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.0.2+9.sha-dc05385');
+    var VERSION$1 = new Version('7.0.2+10.sha-fe1c0d7');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2+9.sha-dc05385
+ * @license Angular v7.0.2+10.sha-fe1c0d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12854,7 +12854,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.0.2+9.sha-dc05385');
+const VERSION$1 = new Version('7.0.2+10.sha-fe1c0d7');
 
 /**
  * @license
