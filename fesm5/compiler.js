f4c350e0a0c1d874673ab31cf2c0d236bb501bbc
Kara Erickson <karakara@google.com>
2018-11-05 18:15:28 +0000

    e6a0c45 style(compiler): typo fix (#26934) PR Close #26934

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+66.sha-7d2a746
+ * @license Angular v7.1.0-beta.1+67.sha-e6a0c45
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -7667,8 +7667,8 @@ var Token = /** @class */ (function () {
     };
     Token.prototype.isNumber = function () { return this.type == TokenType.Number; };
     Token.prototype.isString = function () { return this.type == TokenType.String; };
-    Token.prototype.isOperator = function (operater) {
-        return this.type == TokenType.Operator && this.strValue == operater;
+    Token.prototype.isOperator = function (operator) {
+        return this.type == TokenType.Operator && this.strValue == operator;
     };
     Token.prototype.isIdentifier = function () { return this.type == TokenType.Identifier; };
     Token.prototype.isKeyword = function () { return this.type == TokenType.Keyword; };
@@ -13671,7 +13671,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+66.sha-7d2a746');
+var VERSION$1 = new Version('7.1.0-beta.1+67.sha-e6a0c45');
 
 /**
  * @license
