f35d220023f59df366fb7db2ec6e7a3373b7eec7
Kara Erickson <karakara@google.com>
2018-11-05 18:19:44 +0000

    d0037b2 fix(ivy): correct ngtsc path handling in Windows (#26703)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+69.sha-d568d7a
+ * @license Angular v7.1.0-beta.1+70.sha-d0037b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13671,7 +13671,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+69.sha-d568d7a');
+var VERSION$1 = new Version('7.1.0-beta.1+70.sha-d0037b2');
 
 /**
  * @license
