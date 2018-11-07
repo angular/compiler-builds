b05e6e40ec323392c27628fd38c5ee5f7a2dc54d
Kara Erickson <karakara@google.com>
2018-11-07 20:37:41 +0000

    b57ce92 docs: release notes for the v7.0.3 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+85.sha-9e26216
+ * @license Angular v7.1.0-beta.1+86.sha-b57ce92
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13688,7 +13688,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+85.sha-9e26216');
+var VERSION$1 = new Version('7.1.0-beta.1+86.sha-b57ce92');
 
 /**
  * @license
