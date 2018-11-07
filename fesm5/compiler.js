7d260ed5515323ec0256ddee2cc83876225378c0
Kara Erickson <karakara@google.com>
2018-11-07 01:27:26 +0000

    297c54e fix(ivy): support env where requestAnimationFrame is not available (#26779)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+77.sha-7dbc103
+ * @license Angular v7.1.0-beta.1+79.sha-297c54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13671,7 +13671,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.1+77.sha-7dbc103');
+var VERSION$1 = new Version('7.1.0-beta.1+79.sha-297c54e');
 
 /**
  * @license
