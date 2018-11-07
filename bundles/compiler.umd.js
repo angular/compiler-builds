fbbe6ada71f7f41340f867a36b69f59125c94a3a
Kara Erickson <karakara@google.com>
2018-11-07 18:58:23 +0000

    53e4e0c fix(ivy): Enable AoT tests to run on CI; disable failing tests (#26975)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+81.sha-931a363
+ * @license Angular v7.1.0-beta.1+83.sha-53e4e0c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13728,7 +13728,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.1+81.sha-931a363');
+    var VERSION$1 = new Version('7.1.0-beta.1+83.sha-53e4e0c');
 
     /**
      * @license
