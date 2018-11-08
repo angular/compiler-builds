d4ccc4fe9cfe0981225ec08eefacf470499c878c
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:28:19 +0000

    791d192 docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+2.sha-15e671e
+ * @license Angular v7.1.0-beta.2+3.sha-791d192
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13688,7 +13688,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-beta.2+2.sha-15e671e');
+var VERSION$1 = new Version('7.1.0-beta.2+3.sha-791d192');
 
 /**
  * @license
