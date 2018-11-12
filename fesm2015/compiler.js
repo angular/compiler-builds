aaa6f4729d0887c1fbcb6403fa5924d491ac408a
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:16:56 +0000

    53197de docs: Fix wrong quoting in SSR guide (#27057) PR Close #27057

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+14.sha-7a1b876
+ * @license Angular v7.0.3+17.sha-53197de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12854,7 +12854,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.0.3+14.sha-7a1b876');
+const VERSION$1 = new Version('7.0.3+17.sha-53197de');
 
 /**
  * @license
