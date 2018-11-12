f37d049fe1cbe7f794ace723252b366cf9bcc11c
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:31:58 +0000

    b278ea1 docs: fix typo in toh-pt0.md (#27044) PR Close #27044

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+24.sha-1810cdf
+ * @license Angular v7.1.0-beta.2+25.sha-b278ea1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12911,7 +12911,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0-beta.2+24.sha-1810cdf');
+const VERSION$1 = new Version('7.1.0-beta.2+25.sha-b278ea1');
 
 /**
  * @license
