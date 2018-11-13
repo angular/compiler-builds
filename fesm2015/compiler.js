3d9af7698a48f38abed2894f63f0c487bf33bbb6
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:12:46 +0000

    bff5c24 docs: fix typo (#26878) PR Close #26878

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+17.sha-53197de
+ * @license Angular v7.0.3+18.sha-bff5c24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12854,7 +12854,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.0.3+17.sha-53197de');
+const VERSION$1 = new Version('7.0.3+18.sha-bff5c24');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+17.sha-53197de
+ * @license Angular v7.0.3+18.sha-bff5c24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13646,7 +13646,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.0.3+17.sha-53197de');
+var VERSION$1 = new Version('7.0.3+18.sha-bff5c24');
 
 /**
  * @license
