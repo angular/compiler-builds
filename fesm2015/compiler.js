6c84fcc93319664d389cfbe371ed82172eccca1f
Andrew Kushnir <akushnir@google.com>
2018-11-13 22:24:25 +0000

    d97994b fix(ivy): clone ts.SourceFile in ivy_switch when triggered (#27032)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+41.sha-c31e78f
+ * @license Angular v7.1.0-beta.2+42.sha-d97994b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12984,7 +12984,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0-beta.2+41.sha-c31e78f');
+const VERSION$1 = new Version('7.1.0-beta.2+42.sha-d97994b');
 
 /**
  * @license
