5a0ff2ffc4dfbd8542fe4308eaa6acd095b4f24e
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:46:14 +0000

    9c7428e build(bazel): turn on --nolegacy-external-runfiles (#26770)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+23.sha-4348c47
+ * @license Angular v7.0.3+25.sha-9c7428e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13646,7 +13646,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.0.3+23.sha-4348c47');
+var VERSION$1 = new Version('7.0.3+25.sha-9c7428e');
 
 /**
  * @license
