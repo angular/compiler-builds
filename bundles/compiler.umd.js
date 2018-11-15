34ef9816fa8041e6c56a516132245f5a57e9adbd
Filipe Silva <filipematossilva@gmail.com>
2018-11-15 11:32:44 +0000

    d3f9b3a ci: test ts-api-guardian on windows

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+45.sha-a752971
+ * @license Angular v7.1.0-beta.2+46.sha-d3f9b3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13829,7 +13829,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-beta.2+45.sha-a752971');
+    var VERSION$1 = new Version('7.1.0-beta.2+46.sha-d3f9b3a');
 
     /**
      * @license
