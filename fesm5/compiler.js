cc058dc8d15b4acab3d697ac6374d0c77fd63e0a
Miško Hevery <misko@hevery.com>
2018-11-16 17:48:22 +0000

    848f414 fix(ivy): DI should  work when no element injector on starting node (#27133)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+5.sha-65943b4
+ * @license Angular v7.1.0-rc.0+6.sha-848f414
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13772,7 +13772,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-rc.0+5.sha-65943b4');
+var VERSION$1 = new Version('7.1.0-rc.0+6.sha-848f414');
 
 /**
  * @license
