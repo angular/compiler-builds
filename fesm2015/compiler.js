290c3fad2d8c8d370761df0ef438baf2fa5a7aaf
Misko Hevery <misko@hevery.com>
2018-11-16 05:45:55 +0000

    65943b4 docs: add link to workspace config page (#26927)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+2.sha-3ec7c50
+ * @license Angular v7.1.0-rc.0+5.sha-65943b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -12984,7 +12984,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0-rc.0+2.sha-3ec7c50');
+const VERSION$1 = new Version('7.1.0-rc.0+5.sha-65943b4');
 
 /**
  * @license
