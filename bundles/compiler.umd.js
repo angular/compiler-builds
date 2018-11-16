bd29c6966ea69482905d0868dc6be4c772f8a22d
Misko Hevery <misko@hevery.com>
2018-11-16 20:45:54 +0000

    b07bd30 feat(bazel): Bazel workspace schematics (#26971)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+7.sha-4f965ad
+ * @license Angular v7.1.0-rc.0+8.sha-b07bd30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13829,7 +13829,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+7.sha-4f965ad');
+    var VERSION$1 = new Version('7.1.0-rc.0+8.sha-b07bd30');
 
     /**
      * @license
