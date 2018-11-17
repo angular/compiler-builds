b064c43dfa354dc02f25806610f4278d900a7f52
Misko Hevery <misko@hevery.com>
2018-11-17 02:17:11 +0000

    4222b63 Revert "refactor(ivy): ensure directive host bindings use the styling algorithm (#27134)"

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+12.sha-92e80af
+ * @license Angular v7.1.0-rc.0+14.sha-4222b63
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15411,7 +15411,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+12.sha-92e80af');
+    var VERSION$1 = new Version('7.1.0-rc.0+14.sha-4222b63');
 
     /**
      * @license
