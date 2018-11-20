818b82b6f5f5e34d5552dfc9333fba77484230c8
Misko Hevery <misko@hevery.com>
2018-11-20 21:05:40 +0000

    4390e10 fix(ivy): don't update parent pointers in the ivy switch transform (#27170)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-rc.0+27.sha-975c269.with-local-changes
+ * @license Angular v7.1.0-rc.0+30.sha-4390e10.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __spread, __assign, __read, __values } from 'tslib';
 
 /**
  * @license
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-rc.0+27.sha-975c269.with-local-changes');
+var VERSION$1 = new Version('7.1.0-rc.0+30.sha-4390e10.with-local-changes');
 
 /**
  * @license
