d4f8c3b1bc5d05e1cef32bbfc091c01939f05a57
Misko Hevery <misko@hevery.com>
2018-11-20 20:06:02 +0000

    ca40565 fix(ivy): hack implementation of host styles (#27180)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+26.sha-34306c3.with-local-changes
+ * @license Angular v7.1.0-rc.0+28.sha-ca40565.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-rc.0+26.sha-34306c3.with-local-changes');
+var VERSION$1 = new Version('7.1.0-rc.0+28.sha-ca40565.with-local-changes');
 
 /**
  * @license
