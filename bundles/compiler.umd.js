ac39a7884a4afbc84976cf4bf9d10a4cb9c9f76c
Misko Hevery <misko@hevery.com>
2018-11-21 23:40:59 +0000

    bf71b10 release: cut the v7.1.0 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+34.sha-573fb78
+ * @license Angular v7.1.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+34.sha-573fb78');
+    var VERSION$1 = new Version('7.1.0');
 
     /**
      * @license
