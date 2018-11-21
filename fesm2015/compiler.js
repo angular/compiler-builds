8a37b2c90e10c6b04ad709c1fdc451320b7cbd12
Miško Hevery <misko@hevery.com>
2018-11-21 17:46:18 +0000

    573fb78 docs: add formatting productivity hints to `DEVELOPER.md` (#27211)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+37.sha-0d9b27f
+ * @license Angular v7.1.0-rc.0+34.sha-573fb78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0-rc.0+37.sha-0d9b27f');
+const VERSION$1 = new Version('7.1.0-rc.0+34.sha-573fb78');
 
 /**
  * @license
