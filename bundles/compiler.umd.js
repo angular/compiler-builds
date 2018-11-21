7704e50aac52433d971caff188ba0cb6ddf4cd5a
Miško Hevery <misko@hevery.com>
2018-11-21 16:01:41 +0000

    826afc9 Revert "feat(bazel): Bazel workspace schematics (#26971)" (#27202)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.4+4.sha-a02eda5
+ * @license Angular v7.0.4+11.sha-826afc9.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13703,7 +13703,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.0.4+4.sha-a02eda5');
+    var VERSION$1 = new Version('7.0.4+11.sha-826afc9.with-local-changes');
 
     /**
      * @license
