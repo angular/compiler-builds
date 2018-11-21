38f061cf6d4322ff7629953a33d212f1d40d049b
Miško Hevery <misko@hevery.com>
2018-11-21 16:10:34 +0000

    a72250b build(bazel): Add Bazel builders (#27141)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+31.sha-026b7e3
+ * @license Angular v7.1.0-rc.0+32.sha-a72250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0-rc.0+31.sha-026b7e3');
+var VERSION$1 = new Version('7.1.0-rc.0+32.sha-a72250b');
 
 /**
  * @license
