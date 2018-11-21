f396a4e12c2861248b5046e5a117c577d6603ef8
Miško Hevery <misko@hevery.com>
2018-11-21 17:45:02 +0000

    573fb78 docs: add formatting productivity hints to `DEVELOPER.md` (#27211)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+33.sha-d666370
+ * @license Angular v7.1.0-rc.0+34.sha-573fb78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0-rc.0+33.sha-d666370');
+    var VERSION$1 = new Version('7.1.0-rc.0+34.sha-573fb78');
 
     /**
      * @license
