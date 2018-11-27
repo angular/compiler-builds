b510e9b177be069682b5f71663d81107df5b2071
Jason Aden <jasonaden@google.com>
2018-11-27 18:50:09 +0000

    a7ba05a fix(ivy): fix directive instantiation at slots above 2^12 (#27280)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+18.sha-d62da4d
+ * @license Angular v7.1.0+19.sha-a7ba05a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14552,7 +14552,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+18.sha-d62da4d');
+const VERSION$1 = new Version('7.1.0+19.sha-a7ba05a');
 
 /**
  * @license
