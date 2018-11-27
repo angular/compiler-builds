1dc1e216a83a50b55541ba18d23706d4aab41d20
Jason Aden <jasonaden@google.com>
2018-11-27 22:09:23 +0000

    bdf5f3e fix(core): export a value for InjectFlags (#27279)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+4.sha-a71038b
+ * @license Angular v7.1.0+5.sha-bdf5f3e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+4.sha-a71038b');
+    var VERSION$1 = new Version('7.1.0+5.sha-bdf5f3e');
 
     /**
      * @license
