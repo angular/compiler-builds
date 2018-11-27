c2ba37d66246ee61cbc0f3035305c0f5e9925b99
Jason Aden <jasonaden@google.com>
2018-11-27 18:45:10 +0000

    d767e0b fix(ivy): consider providers for view/content queries (#27156)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+11.sha-f45aedc
+ * @license Angular v7.1.0+13.sha-d767e0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14552,7 +14552,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+11.sha-f45aedc');
+const VERSION$1 = new Version('7.1.0+13.sha-d767e0b');
 
 /**
  * @license
