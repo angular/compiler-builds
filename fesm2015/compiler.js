bb7a78a929ef2769b52b6cda11caa332fa93cb9d
Jason Aden <jasonaden@google.com>
2018-11-27 22:12:01 +0000

    39e426c fix(ivy): TestBed.get(Compiler) throws "Error: Runtime compiler is not loaded" (#27223)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+20.sha-d0e8020
+ * @license Angular v7.1.0+34.sha-39e426c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14552,7 +14552,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+20.sha-d0e8020');
+const VERSION$1 = new Version('7.1.0+34.sha-39e426c');
 
 /**
  * @license
