05256e553b700dbb9d445073092cd27426a7e926
Igor Minar <igor@angular.io>
2018-11-30 06:34:51 +0000

    2cce8d4 docs: fix typo (#27294)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+9.sha-a149c3a
+ * @license Angular v7.1.1+10.sha-2cce8d4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.1+9.sha-a149c3a');
+    var VERSION$1 = new Version('7.1.1+10.sha-2cce8d4');
 
     /**
      * @license
