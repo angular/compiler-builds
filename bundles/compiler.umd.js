3a349d46c597a878e0287e1d1ac2a21c5bae3835
Igor Minar <igor@angular.io>
2018-11-30 06:12:46 +0000

    23bc8ed test: move remaining fixmeIvy to test level (#27354)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+56.sha-36e7bf1
+ * @license Angular v7.1.0+57.sha-23bc8ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15520,7 +15520,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+56.sha-36e7bf1');
+    var VERSION$1 = new Version('7.1.0+57.sha-23bc8ed');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+56.sha-36e7bf1
+ * @license Angular v7.1.0+57.sha-23bc8ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14552,7 +14552,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+56.sha-36e7bf1');
+const VERSION$1 = new Version('7.1.0+57.sha-23bc8ed');
 
 /**
  * @license
