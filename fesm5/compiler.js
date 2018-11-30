5907415dd40b9ececdc0d8914bfac7ac9bf8e888
Igor Minar <igor@angular.io>
2018-11-30 06:09:59 +0000

    419fa17 docs: move past events on events page (#27342)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+49.sha-e024f2f
+ * @license Angular v7.1.0+51.sha-419fa17
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15463,7 +15463,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+49.sha-e024f2f');
+var VERSION$1 = new Version('7.1.0+51.sha-419fa17');
 
 /**
  * @license
