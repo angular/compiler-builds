15eef74e9fd7ff1497378a6a70625a6b7a0d12b4
Igor Minar <igor@angular.io>
2018-11-30 06:17:47 +0000

    eb99753 test(ivy): enable @angular/service-worker targets to run on CI (#27310)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+60.sha-1402175
+ * @license Angular v7.1.0+63.sha-eb99753
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15463,7 +15463,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+60.sha-1402175');
+var VERSION$1 = new Version('7.1.0+63.sha-eb99753');
 
 /**
  * @license
