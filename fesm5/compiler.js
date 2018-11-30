3ea564297af8485110d1da06ef10b066697aa711
Igor Minar <igor@angular.io>
2018-11-30 06:15:45 +0000

    1402175 test(upgrade): re-enable tests that have been fixed (#27305)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+59.sha-65775a3
+ * @license Angular v7.1.0+60.sha-1402175
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15463,7 +15463,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+59.sha-65775a3');
+var VERSION$1 = new Version('7.1.0+60.sha-1402175');
 
 /**
  * @license
