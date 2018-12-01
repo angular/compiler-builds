81e0023c9a31b88069558312d9ab1a9d110229eb
Igor Minar <igor@angular.io>
2018-12-01 00:32:13 +0000

    f5f323d build: only stamp version info when releasing (#27362)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+101.sha-35e02ad
+ * @license Angular v7.1.0+102.sha-f5f323d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14554,7 +14554,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+101.sha-35e02ad');
+const VERSION$1 = new Version('7.1.0+102.sha-f5f323d');
 
 /**
  * @license
