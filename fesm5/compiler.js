953ef8710c9fdc1cebe4883906349dfe4375147e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 23:08:50 +0000

    75723d5 feat(ivy): ngtsc support for static resolution of array.slice() (#27158)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+113.sha-06d4a0c
+ * @license Angular v7.1.0+114.sha-75723d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15485,7 +15485,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+113.sha-06d4a0c');
+var VERSION$1 = new Version('7.1.0+114.sha-75723d5');
 
 /**
  * @license
