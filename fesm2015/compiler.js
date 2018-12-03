389e14221178cc11600c1fc16bbbee7688192737
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 20:29:46 +0000

    7d89cff fix(ivy): Ensure ComponentFactory uses both injectors to retrieve rendererFactory (#27341)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+99.sha-01fd0cd
+ * @license Angular v7.1.0+109.sha-7d89cff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14554,7 +14554,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+99.sha-01fd0cd');
+const VERSION$1 = new Version('7.1.0+109.sha-7d89cff');
 
 /**
  * @license
