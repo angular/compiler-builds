500dac2eedcd0d1b29e75690be0b7981aaaefc10
Igor Minar <igor@angular.io>
2018-12-03 16:53:41 +0000

    931a636 docs: fix typo in toh-pt6.md (#27403)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+106.sha-a082f64
+ * @license Angular v7.1.0+107.sha-931a636
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14554,7 +14554,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+106.sha-a082f64');
+const VERSION$1 = new Version('7.1.0+107.sha-931a636');
 
 /**
  * @license
