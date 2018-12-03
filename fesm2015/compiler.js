802db2ea595623c18ccd6d649e943886187ecce8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 17:43:02 +0000

    6f5c124 fix(ivy): proper accounting of host vars in case of inherited Directives (#27392)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+107.sha-931a636
+ * @license Angular v7.1.0+108.sha-6f5c124
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14554,7 +14554,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+107.sha-931a636');
+const VERSION$1 = new Version('7.1.0+108.sha-6f5c124');
 
 /**
  * @license
