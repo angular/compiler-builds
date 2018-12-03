9a09a1bb1984bdec9c8dc589c07172e654aa941a
Igor Minar <igor@angular.io>
2018-12-03 16:51:45 +0000

    d847051 test(common): add PercentPipe round and trim tests (#27365)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+103.sha-a1470c9
+ * @license Angular v7.1.0+105.sha-d847051
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15476,7 +15476,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+103.sha-a1470c9');
+var VERSION$1 = new Version('7.1.0+105.sha-d847051');
 
 /**
  * @license
