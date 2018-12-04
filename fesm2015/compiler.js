881a2b6dbdc4c2ab0284f9ee437b3365ed01086e
Igor Minar <igor@angular.io>
2018-12-04 22:38:06 +0000

    d3c08e7 fix(compiler-cli): flatModuleIndex files not generated on windows with multiple input files (#27200)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+117.sha-7ad6b03
+ * @license Angular v7.1.0+121.sha-d3c08e7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14563,7 +14563,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+117.sha-7ad6b03');
+const VERSION$1 = new Version('7.1.0+121.sha-d3c08e7');
 
 /**
  * @license
