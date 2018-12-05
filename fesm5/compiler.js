90fddfd5c12d4ad3679008a1fb663bd1e8761ea4
Igor Minar <igor@angular.io>
2018-12-05 07:42:17 +0000

    4b9948c fix(core): More precise return type for `InjectableDecorator` (#27360)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+149.sha-faca8b1
+ * @license Angular v7.1.0+150.sha-4b9948c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15497,7 +15497,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+149.sha-faca8b1');
+var VERSION$1 = new Version('7.1.0+150.sha-4b9948c');
 
 /**
  * @license
