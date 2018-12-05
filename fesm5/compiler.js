23850a96634b6a37e0eebe17ddcd42638a4f55f5
Igor Minar <igor@angular.io>
2018-12-05 04:40:01 +0000

    9b4d959 fix(bazel): do not throw if ts compile action does not create esm5 outputs (#27401)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+23.sha-8087b6b
+ * @license Angular v7.1.1+24.sha-9b4d959
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.1+23.sha-8087b6b');
+var VERSION$1 = new Version('7.1.1+24.sha-9b4d959');
 
 /**
  * @license
