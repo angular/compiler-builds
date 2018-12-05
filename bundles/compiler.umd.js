a7682d0c6b05ece1fdf5180d3d8a70b257fbde48
Igor Minar <igor@angular.io>
2018-12-05 21:14:13 +0000

    24ebdbd ci: test ts-api-guardian on windows (#27205)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-821fecb
+ * @license Angular v7.1.0+164.sha-24ebdbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15558,7 +15558,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+163.sha-821fecb');
+    var VERSION$1 = new Version('7.1.0+164.sha-24ebdbd');
 
     /**
      * @license
