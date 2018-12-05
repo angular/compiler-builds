58896fa115dd28ef98ac9482cefe8af02e40326f
Igor Minar <igor@angular.io>
2018-12-05 04:46:32 +0000

    5c57920 fix(docs-infra): log the successful creation of previews (#27436)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+24.sha-9b4d959
+ * @license Angular v7.1.1+25.sha-5c57920
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.1+24.sha-9b4d959');
+const VERSION$1 = new Version('7.1.1+25.sha-5c57920');
 
 /**
  * @license
