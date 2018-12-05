1014d2b132d03076842e4020898ded238bb7e99b
Igor Minar <igor@angular.io>
2018-12-05 21:52:32 +0000

    4b5cd72 refactor(docs-infra): remove unused custom element (#27250)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+164.sha-24ebdbd
+ * @license Angular v7.1.0+176.sha-4b5cd72
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15501,7 +15501,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+164.sha-24ebdbd');
+var VERSION$1 = new Version('7.1.0+176.sha-4b5cd72');
 
 /**
  * @license
