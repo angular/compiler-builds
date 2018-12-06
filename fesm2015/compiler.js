f5ae79048caa68c6ab2a2ac7d9b8644c7ccae5f2
Igor Minar <igor@angular.io>
2018-12-06 17:47:02 +0000

    84f2928 fix(compiler-cli): ngtsc shim files not being generated on case-insensitive platforms (#27466)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+53.sha-d7c72fc
+ * @license Angular v7.1.1+56.sha-84f2928
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.1+53.sha-d7c72fc');
+const VERSION$1 = new Version('7.1.1+56.sha-84f2928');
 
 /**
  * @license
