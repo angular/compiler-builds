8ec2c1de705c28548657626c9e58482c2637f0b4
Igor Minar <igor@angular.io>
2018-12-06 05:20:17 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14573,7 +14573,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+185.sha-a315ded');
+const VERSION$1 = new Version('7.1.0+194.sha-6740d0d');
 
 /**
  * @license
