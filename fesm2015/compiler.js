1a57b5864ca833d7e1d1c96a370b9a442c017b0f
Miško Hevery <misko@hevery.com>
2018-12-06 21:43:36 +0000

    3fa2a5f fix(ivy): add missing AttributeMarker export

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+163.sha-6fa5383
+ * @license Angular v7.1.0+202.sha-3fa2a5f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14573,7 +14573,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.0+163.sha-6fa5383');
+const VERSION$1 = new Version('7.1.0+202.sha-3fa2a5f');
 
 /**
  * @license
