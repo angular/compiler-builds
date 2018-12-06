e43b6cf502a888dfa6fb090c65dabba5dd71f97e
Igor Minar <igor@angular.io>
2018-12-06 22:10:45 +0000

    c71d7b5 fix(ivy): setting up animation properties correctly (FW-643) (#27496)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+205.sha-4da739a
+ * @license Angular v7.1.0+206.sha-c71d7b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -13407,11 +13407,11 @@ function getSerializedI18nContent(message) {
 function mapBindingToInstruction(type) {
     switch (type) {
         case 0 /* Property */:
+        case 4 /* Animation */:
             return Identifiers$1.elementProperty;
         case 2 /* Class */:
             return Identifiers$1.elementClassProp;
         case 1 /* Attribute */:
-        case 4 /* Animation */:
             return Identifiers$1.elementAttribute;
         default:
             return undefined;
@@ -13924,10 +13924,11 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
             var instruction = mapBindingToInstruction(input.type);
             if (input.type === 4 /* Animation */) {
                 var value_1 = input.value.visit(_this._valueConverter);
-                // setAttribute without a value doesn't make any sense
+                // setProperty without a value doesn't make any sense
                 if (value_1.name || value_1.value) {
+                    _this.allocateBindingSlots(value_1);
                     var name_2 = prepareSyntheticAttributeName(input.name);
-                    _this.updateInstruction(input.sourceSpan, Identifiers$1.elementAttribute, function () {
+                    _this.updateInstruction(input.sourceSpan, Identifiers$1.elementProperty, function () {
                         return [
                             literal(elementIndex), literal(name_2), _this.convertPropertyBinding(implicit, value_1)
                         ];
@@ -14846,9 +14847,9 @@ function compileComponentFromMetadata(meta, constantPool, bindingParser) {
     if (meta.encapsulation !== ViewEncapsulation.Emulated) {
         definitionMap.set('encapsulation', literal(meta.encapsulation));
     }
-    // e.g. `animations: [trigger('123', [])]`
+    // e.g. `animation: [trigger('123', [])]`
     if (meta.animations !== null) {
-        definitionMap.set('data', literalMap([{ key: 'animations', value: meta.animations, quoted: false }]));
+        definitionMap.set('data', literalMap([{ key: 'animation', value: meta.animations, quoted: false }]));
     }
     // On the type side, remove newlines from the selector as it will need to fit into a TypeScript
     // string literal, which must be on one line.
@@ -15502,7 +15503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+205.sha-4da739a');
+var VERSION$1 = new Version('7.1.0+206.sha-c71d7b5');
 
 /**
  * @license
