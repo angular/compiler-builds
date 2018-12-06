917721a571b404fc6f36f8e833f79865a72d8018
Igor Minar <igor@angular.io>
2018-12-06 22:12:00 +0000

    8fa7e93 test(ivy): enable router tests with pending timers (FW-768) (#27509)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+207.sha-cad6714
+ * @license Angular v7.1.0+208.sha-8fa7e93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15562,7 +15562,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+207.sha-cad6714');
+    var VERSION$1 = new Version('7.1.0+208.sha-8fa7e93');
 
     /**
      * @license
