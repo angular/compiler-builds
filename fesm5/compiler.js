a4a8a7f505ab4259f202226d78f8bb5a48cbc8f7
Igor Minar <igor@angular.io>
2018-12-06 18:03:10 +0000

    d63d53f ci: test ts-api-guardian on windows (#27205)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+199.sha-7c52d10
+ * @license Angular v7.1.0+200.sha-d63d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15501,7 +15501,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+199.sha-7c52d10');
+var VERSION$1 = new Version('7.1.0+200.sha-d63d53f');
 
 /**
  * @license
