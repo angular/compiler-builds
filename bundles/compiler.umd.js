0acfc914e67215ce6c7038c11deb80e43e04487c
Igor Minar <igor@angular.io>
2018-12-06 22:03:42 +0000

    4da739a fix(bazel): Respect existing angular installation (#27495)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+203.sha-8e9858f
+ * @license Angular v7.1.0+205.sha-4da739a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15559,7 +15559,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.0+203.sha-8e9858f');
+    var VERSION$1 = new Version('7.1.0+205.sha-4da739a');
 
     /**
      * @license
