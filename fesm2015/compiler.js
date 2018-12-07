2ce451be1fcc63c9f8b4e0340456c581ec159c54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:00:05 +0000

    6316051 build(docs-infra): upgrade cli command docs sources to 5eef7122c (#27539)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+3.sha-aa48810
+ * @license Angular v7.2.0-beta.1+5.sha-6316051
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14577,7 +14577,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.1+3.sha-aa48810');
+const VERSION$1 = new Version('7.2.0-beta.1+5.sha-6316051');
 
 /**
  * @license
