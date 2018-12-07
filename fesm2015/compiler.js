357b656c633f98a39c61fbf40ce7cbdd68753daa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:16:21 +0000

    7a5136b build: update source-map-support to get rid of Node's DeprecationWarning (#27538)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+5.sha-a9f39a4
+ * @license Angular v7.1.2+6.sha-7a5136b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.2+5.sha-a9f39a4');
+const VERSION$1 = new Version('7.1.2+6.sha-7a5136b');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+5.sha-a9f39a4
+ * @license Angular v7.1.2+6.sha-7a5136b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.2+5.sha-a9f39a4');
+var VERSION$1 = new Version('7.1.2+6.sha-7a5136b');
 
 /**
  * @license
