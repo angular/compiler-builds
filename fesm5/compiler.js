92153d98372dee3b402771ee60934020e1efbc9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 19:20:20 +0000

    f034114 fix(bazel): tsickle dependency not working with typescript 3.1.x (#27402)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+10.sha-055dc2d
+ * @license Angular v7.2.0-beta.1+13.sha-f034114
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15510,7 +15510,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.1+10.sha-055dc2d');
+var VERSION$1 = new Version('7.2.0-beta.1+13.sha-f034114');
 
 /**
  * @license
