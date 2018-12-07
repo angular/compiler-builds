c5f1fb8d51e4d4a733cea3d922197a217aba711c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 17:56:55 +0000

    90c273a ci: update ngbot config to add merge linked labels (#27512) PR Close #27512

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+3.sha-74346e9
+ * @license Angular v7.1.2+1.sha-90c273a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.2+3.sha-74346e9');
+    var VERSION$1 = new Version('7.1.2+1.sha-90c273a');
 
     /**
      * @license
