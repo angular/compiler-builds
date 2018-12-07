e3b550b9a36ab07f4c1ede659a96fafde01a06db
Igor Minar <igor@angular.io>
2018-12-07 06:08:57 +0000

    6bd28e6 docs: release notes for the v7.1.2 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+208.sha-8fa7e93
+ * @license Angular v7.2.0-beta.1+1.sha-6bd28e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15505,7 +15505,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.0+208.sha-8fa7e93');
+var VERSION$1 = new Version('7.2.0-beta.1+1.sha-6bd28e6');
 
 /**
  * @license
