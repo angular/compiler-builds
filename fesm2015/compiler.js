bd78496434792f508f07cdf28af2b8c6a6238d86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:20:25 +0000

    255e672 fix(ivy): define correct SVG namespace (FW-757) (#27529)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+13.sha-f034114
+ * @license Angular v7.2.0-beta.1+15.sha-255e672
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14582,7 +14582,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.1+13.sha-f034114');
+const VERSION$1 = new Version('7.2.0-beta.1+15.sha-255e672');
 
 /**
  * @license
