5788e64a56bcdf444080a7ad06763b207fce81bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 19:00:37 +0000

    2182be4 test(ivy): enable more of @angular/core tests on node and in a browser (#27572)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+17.sha-9e7a8f6
+ * @license Angular v7.2.0-beta.1+19.sha-2182be4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15510,7 +15510,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.1+17.sha-9e7a8f6');
+var VERSION$1 = new Version('7.2.0-beta.1+19.sha-2182be4');
 
 /**
  * @license
