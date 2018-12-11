e1ef0c244fcb1fc5526190ef90b98c607895f8a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 18:32:07 +0000

    05cdfb9 fix(ivy): make ViewRef.detectChanges work with embedded views (FW-749) (#27521)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+12.sha-b0c7561
+ * @license Angular v7.2.0-beta.1+22.sha-05cdfb9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15567,7 +15567,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.1+12.sha-b0c7561');
+    var VERSION$1 = new Version('7.2.0-beta.1+22.sha-05cdfb9');
 
     /**
      * @license
