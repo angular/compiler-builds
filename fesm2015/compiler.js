71766b92127b17a71cd69b6779fedd1c238b7cdc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 19:42:43 +0000

    01114dc build: fix type-check errors introduced during rules_ts 0.21 (#27586) (#27590)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+9.sha-575ef9f
+ * @license Angular v7.1.2+11.sha-01114dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.2+9.sha-575ef9f');
+const VERSION$1 = new Version('7.1.2+11.sha-01114dc');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+9.sha-575ef9f
+ * @license Angular v7.1.2+11.sha-01114dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.2+9.sha-575ef9f');
+var VERSION$1 = new Version('7.1.2+11.sha-01114dc');
 
 /**
  * @license
