b75eaac100ec38b2c9011103136a72f85627a296
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 00:52:36 +0000

    93078e3 fix(bazel): Read latest versions from latest-versions.ts & use semver check (#27591)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+1.sha-29ab729
+ * @license Angular v7.1.3+2.sha-93078e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.3+1.sha-29ab729');
+const VERSION$1 = new Version('7.1.3+2.sha-93078e3');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+1.sha-29ab729
+ * @license Angular v7.1.3+2.sha-93078e3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+1.sha-29ab729');
+var VERSION$1 = new Version('7.1.3+2.sha-93078e3');
 
 /**
  * @license
