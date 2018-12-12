78559bb43b4a5fc2cb170c5570ea925443670f53
Miško Hevery <misko@hevery.com>
2018-12-12 21:29:42 +0000

    37c05bd fix(ivy): avoid destroy renderer method invocation for child views (#27592)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+6.sha-28ceca0
+ * @license Angular v7.2.0-beta.2+9.sha-37c05bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15569,7 +15569,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+6.sha-28ceca0');
+    var VERSION$1 = new Version('7.2.0-beta.2+9.sha-37c05bd');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+6.sha-28ceca0
+ * @license Angular v7.2.0-beta.2+9.sha-37c05bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.2+6.sha-28ceca0');
+const VERSION$1 = new Version('7.2.0-beta.2+9.sha-37c05bd');
 
 /**
  * @license
