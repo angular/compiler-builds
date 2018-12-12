012f26960eda13f5b8d0da17ac8156cfe557dd71
Miško Hevery <misko@hevery.com>
2018-12-12 21:32:16 +0000

    7fabe44 fix(ivy): add support for optional nullable injection tokens (#27552)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+9.sha-37c05bd
+ * @license Angular v7.2.0-beta.2+10.sha-7fabe44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.2+9.sha-37c05bd');
+const VERSION$1 = new Version('7.2.0-beta.2+10.sha-7fabe44');
 
 /**
  * @license
