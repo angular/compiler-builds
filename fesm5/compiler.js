222df038f9ad504df907572ea2867f2e225d6c98
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:18:41 +0000

    26c79ef build(bazel): fix postinstall-patches.js script run during yarn postinstall when running yarn_install (#27610)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+4.sha-0b8db92
+ * @license Angular v7.1.3+5.sha-26c79ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+4.sha-0b8db92');
+var VERSION$1 = new Version('7.1.3+5.sha-26c79ef');
 
 /**
  * @license
