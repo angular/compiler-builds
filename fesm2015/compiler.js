6056af46c06527810a00b92e10f63c2fff8969c9
Miško Hevery <misko@hevery.com>
2018-12-13 19:49:06 +0000

    e269274 build: ts-api-guardian tsconfig warning with bazel (#27583)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+18.sha-8313ffc
+ * @license Angular v7.2.0-beta.2+19.sha-e269274
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14584,7 +14584,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.2+18.sha-8313ffc');
+const VERSION$1 = new Version('7.2.0-beta.2+19.sha-e269274');
 
 /**
  * @license
