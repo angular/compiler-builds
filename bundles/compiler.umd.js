1b050c4b439c295d645b07071c7170e795a1d29b
Miško Hevery <misko@hevery.com>
2018-12-13 19:39:18 +0000

    a808c7d build: ts-api-guardian tsconfig warning with bazel (#27583)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+16.sha-89ace1a
+ * @license Angular v7.1.3+17.sha-a808c7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+16.sha-89ace1a');
+    var VERSION$1 = new Version('7.1.3+17.sha-a808c7d');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+16.sha-89ace1a
+ * @license Angular v7.1.3+17.sha-a808c7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+16.sha-89ace1a');
+var VERSION$1 = new Version('7.1.3+17.sha-a808c7d');
 
 /**
  * @license
