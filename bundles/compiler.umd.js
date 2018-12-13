cb14ef515db2b09ee6bfa68af79d4562bb79efea
Miško Hevery <misko@hevery.com>
2018-12-13 19:39:52 +0000

    52fd4a2 build: disable postinstall-patch.js log output (#27619)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+14.sha-8c32a2e
+ * @license Angular v7.1.3+15.sha-52fd4a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+14.sha-8c32a2e');
+    var VERSION$1 = new Version('7.1.3+15.sha-52fd4a2');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+14.sha-8c32a2e
+ * @license Angular v7.1.3+15.sha-52fd4a2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+14.sha-8c32a2e');
+var VERSION$1 = new Version('7.1.3+15.sha-52fd4a2');
 
 /**
  * @license
