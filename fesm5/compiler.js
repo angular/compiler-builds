6f85177823009de41916d98eb7c246309d8ca5f0
Miško Hevery <misko@hevery.com>
2018-12-13 19:46:26 +0000

    ffe1b4d build: disable postinstall-patch.js log output (#27619)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+16.sha-e5c10c3
+ * @license Angular v7.2.0-beta.2+17.sha-ffe1b4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15512,7 +15512,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.2+16.sha-e5c10c3');
+var VERSION$1 = new Version('7.2.0-beta.2+17.sha-ffe1b4d');
 
 /**
  * @license
