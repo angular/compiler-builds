98cc502a4972023bd541912c2da33ee50137e980
Miško Hevery <misko@hevery.com>
2018-12-14 23:51:00 +0000

    c220328 docs: adding ngAtlanta 2019 (#27522)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+26.sha-fa3af8b
+ * @license Angular v7.1.3+25.sha-c220328
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14561,7 +14561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.1.3+26.sha-fa3af8b');
+const VERSION$1 = new Version('7.1.3+25.sha-c220328');
 
 /**
  * @license
