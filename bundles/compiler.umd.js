33bca325ade625215b9f4548d316e0ef2c9ff4cc
Miško Hevery <misko@hevery.com>
2018-12-14 23:27:17 +0000

    522919a fix(bazel): emit full node stack traces when Angular compilation crashes (#27678)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+30.sha-c6ae729
+ * @license Angular v7.2.0-beta.2+41.sha-522919a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15578,7 +15578,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+30.sha-c6ae729');
+    var VERSION$1 = new Version('7.2.0-beta.2+41.sha-522919a');
 
     /**
      * @license
