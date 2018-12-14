421358505f137b98cc386abe08d2e511ab4f12ae
Miško Hevery <misko@hevery.com>
2018-12-14 19:05:01 +0000

    d0d351c docs(common): update currencyPipe default digitsInfo values (#27417)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-beta.2+36.sha-7fae911
+ * @license Angular v7.2.0-beta.2+39.sha-d0d351c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15521,7 +15521,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.2+36.sha-7fae911');
+var VERSION$1 = new Version('7.2.0-beta.2+39.sha-d0d351c');
 
 /**
  * @license
