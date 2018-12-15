6c3c0d10204b45a336f15bd38da627db0aa15c23
Miško Hevery <misko@hevery.com>
2018-12-15 00:06:30 +0000

    f1c9d6a fix(animations): do not truncate decimals for delay (#24455)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+48.sha-b00aeef
+ * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15578,7 +15578,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+48.sha-b00aeef');
+    var VERSION$1 = new Version('7.2.0-beta.2+47.sha-f1c9d6a');
 
     /**
      * @license
