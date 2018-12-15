c423d581d1f2b4c47ca1c60e9467a98bffa7cb0e
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:01 +0000

    df123e0 fix(animations): mark actual descendant node as disabled (#26180)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+44.sha-b39efdd
+ * @license Angular v7.2.0-beta.2+46.sha-df123e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15578,7 +15578,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+44.sha-b39efdd');
+    var VERSION$1 = new Version('7.2.0-beta.2+46.sha-df123e0');
 
     /**
      * @license
