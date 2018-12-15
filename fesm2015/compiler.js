2ff338f635239e5a9d44694371998b26d5a64b5f
Miško Hevery <misko@hevery.com>
2018-12-15 00:07:57 +0000

    f9545d1 docs: adding ngAtlanta 2019 (#27522)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
+ * @license Angular v7.2.0-beta.2+43.sha-f9545d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14593,7 +14593,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.2+47.sha-f1c9d6a');
+const VERSION$1 = new Version('7.2.0-beta.2+43.sha-f9545d1');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+47.sha-f1c9d6a
+ * @license Angular v7.2.0-beta.2+43.sha-f9545d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15521,7 +15521,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.2+47.sha-f1c9d6a');
+var VERSION$1 = new Version('7.2.0-beta.2+43.sha-f9545d1');
 
 /**
  * @license
