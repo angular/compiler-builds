f21de2eeecfbde2dff233f0640120d36857f98cd
Miško Hevery <misko@hevery.com>
2018-12-17 18:04:58 +0000

    ce4cff6 docs: Add Smart UI to UI Components section in resources (#27515)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+30.sha-78bd3c7
+ * @license Angular v7.1.3+31.sha-ce4cff6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+30.sha-78bd3c7');
+    var VERSION$1 = new Version('7.1.3+31.sha-ce4cff6');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+30.sha-78bd3c7
+ * @license Angular v7.1.3+31.sha-ce4cff6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+30.sha-78bd3c7');
+var VERSION$1 = new Version('7.1.3+31.sha-ce4cff6');
 
 /**
  * @license
