886e449a1b5afaee667100444f97c5e850313b0f
Miško Hevery <misko@hevery.com>
2018-12-18 18:01:20 +0000

    f57916c fix(bazel): devserver entry_module should have underscore name (#27719)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+79.sha-8fdd702
+ * @license Angular v7.2.0-beta.2+76.sha-f57916c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15575,7 +15575,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+79.sha-8fdd702');
+    var VERSION$1 = new Version('7.2.0-beta.2+76.sha-f57916c');
 
     /**
      * @license
