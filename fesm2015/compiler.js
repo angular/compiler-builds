ac57c8961659d66f780c5a1eac8b6316ed8a9fc1
Miško Hevery <misko@hevery.com>
2018-12-18 18:02:16 +0000

    8fdd702 docs: Fixed typo (#27669) PR Close #27669

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-d766ad0
+ * @license Angular v7.2.0-beta.2+79.sha-8fdd702
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14590,7 +14590,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-beta.2+77.sha-d766ad0');
+const VERSION$1 = new Version('7.2.0-beta.2+79.sha-8fdd702');
 
 /**
  * @license
