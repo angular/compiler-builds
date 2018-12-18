6151fc05ac4be4897e57bfbbdf59306e0e2f542c
Miško Hevery <misko@hevery.com>
2018-12-18 01:11:39 +0000

    183f278 fix(bazel): Set module_name and enable ng test (#27715)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+32.sha-d1a14f6
+ * @license Angular v7.1.3+33.sha-183f278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15529,7 +15529,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.1.3+32.sha-d1a14f6');
+    var VERSION$1 = new Version('7.1.3+33.sha-183f278');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+32.sha-d1a14f6
+ * @license Angular v7.1.3+33.sha-183f278
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15472,7 +15472,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.1.3+32.sha-d1a14f6');
+var VERSION$1 = new Version('7.1.3+33.sha-183f278');
 
 /**
  * @license
