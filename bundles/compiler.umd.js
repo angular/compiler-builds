90fc54b6dafa6ae883539067ccb23937266c2ed3
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:41:20 +0000

    b1560f7 build: fix our copy of Array#find typing (#27742)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+2.sha-a3debf6
+ * @license Angular v7.2.0-rc.0+1.sha-b1560f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+2.sha-a3debf6');
+    var VERSION$1 = new Version('7.2.0-rc.0+1.sha-b1560f7');
 
     /**
      * @license
