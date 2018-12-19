552e85e14719c856beab6c7c3389df2b59ec7705
Igor Minar <igor@angular.io>
2018-12-19 18:24:33 +0000

    01a5c74 turns out that github silently converts '/ in team names to '-'

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * @license Angular v7.2.0-beta.2+78.sha-01a5c74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15575,7 +15575,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+77.sha-4d71c4c');
+    var VERSION$1 = new Version('7.2.0-beta.2+78.sha-01a5c74');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * @license Angular v7.2.0-beta.2+78.sha-01a5c74
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15518,7 +15518,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-beta.2+77.sha-4d71c4c');
+var VERSION$1 = new Version('7.2.0-beta.2+78.sha-01a5c74');
 
 /**
  * @license
