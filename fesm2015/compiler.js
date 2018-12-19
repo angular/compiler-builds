071c714224309fb614606970c6ec4e006f46ef1f
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 23:50:20 +0000

    b08f3ac test(bazel): Make sure CLI project created with Bazel works with original workflow (#27741)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
+ * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+5.sha-3ab25ab');
+const VERSION$1 = new Version('7.2.0-rc.0+6.sha-b08f3ac');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
+ * @license Angular v7.2.0-rc.0+6.sha-b08f3ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+5.sha-3ab25ab');
+var VERSION$1 = new Version('7.2.0-rc.0+6.sha-b08f3ac');
 
 /**
  * @license
