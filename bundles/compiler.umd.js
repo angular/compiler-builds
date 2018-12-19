edea79f2149507e683a76b4b9097a52029aae17e
Miško Hevery <misko@hevery.com>
2018-12-19 00:12:34 +0000

    24fa0b7 docs: release notes for the v7.1.4 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+91.sha-12c3176
+ * @license Angular v7.2.0-beta.2+92.sha-24fa0b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-beta.2+91.sha-12c3176');
+    var VERSION$1 = new Version('7.2.0-beta.2+92.sha-24fa0b7');
 
     /**
      * @license
