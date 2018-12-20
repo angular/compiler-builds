ad5d464d0d101248abf1556b1c635655f34791a7
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:46:55 +0000

    07ada7f fix(router): ensure URL is updated after second redirect with UrlUpdateStrategy="eager" (#27523)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+13.sha-880e8a5
+ * @license Angular v7.2.0-rc.0+14.sha-07ada7f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14621,7 +14621,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+13.sha-880e8a5');
+const VERSION$1 = new Version('7.2.0-rc.0+14.sha-07ada7f');
 
 /**
  * @license
