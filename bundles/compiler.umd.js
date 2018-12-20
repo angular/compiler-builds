35b1a24a85b14e2c66e15fcb02ef2e375e732308
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 21:47:56 +0000

    dd59e9a docs(upgrade): add gotchas/tips/example for multiple downgraded modules (#27217)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-2013ef7
+ * @license Angular v7.2.0-rc.0+9.sha-dd59e9a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+4.sha-2013ef7');
+    var VERSION$1 = new Version('7.2.0-rc.0+9.sha-dd59e9a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-2013ef7
+ * @license Angular v7.2.0-rc.0+9.sha-dd59e9a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+4.sha-2013ef7');
+const VERSION$1 = new Version('7.2.0-rc.0+9.sha-dd59e9a');
 
 /**
  * @license
