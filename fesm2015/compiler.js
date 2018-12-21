8ccf586056b007366b8d9428ed969b2de54344de
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:43:36 +0000

    c5ce4e6 docs: fix code example to ensure consistency in file (#26577)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+24.sha-bba5e26
+ * @license Angular v7.2.0-rc.0+23.sha-c5ce4e6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14621,7 +14621,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+24.sha-bba5e26');
+const VERSION$1 = new Version('7.2.0-rc.0+23.sha-c5ce4e6');
 
 /**
  * @license
