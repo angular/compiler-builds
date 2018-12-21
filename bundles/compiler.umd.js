dee611a94b1edaaaac17985f3e679abba3c4603a
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:44:34 +0000

    bba5e26 docs(forms): clarify the pattern validator behavior (#27560)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+21.sha-cdd737e
+ * @license Angular v7.2.0-rc.0+24.sha-bba5e26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15609,7 +15609,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+21.sha-cdd737e');
+    var VERSION$1 = new Version('7.2.0-rc.0+24.sha-bba5e26');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+21.sha-cdd737e
+ * @license Angular v7.2.0-rc.0+24.sha-bba5e26
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __values, __read } from 'tslib';
+import { __extends, __spread, __assign, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15552,7 +15552,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+21.sha-cdd737e');
+var VERSION$1 = new Version('7.2.0-rc.0+24.sha-bba5e26');
 
 /**
  * @license
