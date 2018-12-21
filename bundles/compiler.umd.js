44e2c0dae4d83a21d702f2084c55d275b60054a1
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 21:05:46 +0000

    509aa61 fix(ivy): apply all overrides from TestBed, not the last one only (#27734)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+23.sha-c5ce4e6
+ * @license Angular v7.2.0-rc.0+22.sha-509aa61
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15609,7 +15609,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+23.sha-c5ce4e6');
+    var VERSION$1 = new Version('7.2.0-rc.0+22.sha-509aa61');
 
     /**
      * @license
