740844be827252e5ef0372823c094c6099e3c60e
Matias Niemelä <matias@yearofmoo.com>
2018-12-26 12:23:17 +0000

    9b91bee docs: fix typo explictly (#27798) Closes #27796

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+27.sha-13eb57a
+ * @license Angular v7.2.0-rc.0+29.sha-9b91bee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14764,7 +14764,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+27.sha-13eb57a');
+const VERSION$1 = new Version('7.2.0-rc.0+29.sha-9b91bee');
 
 /**
  * @license
