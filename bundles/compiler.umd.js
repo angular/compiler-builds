777f0f23c0e77a886c99934ef655016c7fa8d2c0
Ben Lesh <ben@benlesh.com>
2018-12-26 19:55:06 +0000

    707c682 docs(elements): add "Firefox" for support of custom elements (#27789)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+32.sha-3f64e87
+ * @license Angular v7.2.0-rc.0+33.sha-707c682
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15752,7 +15752,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+32.sha-3f64e87');
+    var VERSION$1 = new Version('7.2.0-rc.0+33.sha-707c682');
 
     /**
      * @license
