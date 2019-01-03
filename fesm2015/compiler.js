c077bc4169873ac2c13d46f2d274f543b519b9c2
Ben Lesh <ben@benlesh.com>
2019-01-03 18:12:08 +0000

    9a15a42 docs: add more info on what is being cached (#27343)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+19.sha-679235a
+ * @license Angular v7.2.0-rc.0+22.sha-9a15a42
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+19.sha-679235a');
+const VERSION$1 = new Version('7.2.0-rc.0+22.sha-9a15a42');
 
 /**
  * @license
