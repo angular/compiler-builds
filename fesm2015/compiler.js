12d778d213d2961adaf3358d9851248f5077e957
Ben Lesh <ben@benlesh.com>
2019-01-03 18:15:24 +0000

    27431e0 test(docs-infra): increase timeout for redirection to external URL (#27903)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+35.sha-c4f7727
+ * @license Angular v7.2.0-rc.0+38.sha-27431e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14764,7 +14764,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+35.sha-c4f7727');
+const VERSION$1 = new Version('7.2.0-rc.0+38.sha-27431e0');
 
 /**
  * @license
