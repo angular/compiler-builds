a2bc040de0e8a947f5146526309a97fdd2d20ad2
Ben Lesh <ben@benlesh.com>
2019-01-03 18:17:19 +0000

    755c809 docs: improve description of bundle budgets (#27833)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+39.sha-59f64dd
+ * @license Angular v7.2.0-rc.0+40.sha-755c809
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14764,7 +14764,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+39.sha-59f64dd');
+const VERSION$1 = new Version('7.2.0-rc.0+40.sha-755c809');
 
 /**
  * @license
