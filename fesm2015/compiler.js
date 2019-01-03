801cff744bb03e295aba5872b9742c8146c01087
Ben Lesh <ben@benlesh.com>
2019-01-03 18:19:48 +0000

    7df9040 docs: add missing comma to environment config in build guide (#27476)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+44.sha-1dc95c4
+ * @license Angular v7.2.0-rc.0+42.sha-7df9040
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14764,7 +14764,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+44.sha-1dc95c4');
+const VERSION$1 = new Version('7.2.0-rc.0+42.sha-7df9040');
 
 /**
  * @license
