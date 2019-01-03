665d795c18369795927b21ae433e1c14a276d396
Ben Lesh <ben@benlesh.com>
2019-01-03 18:14:23 +0000

    59f64dd docs(service-worker): update service worker configuration doc (#27868)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+38.sha-27431e0
+ * @license Angular v7.2.0-rc.0+39.sha-59f64dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15695,7 +15695,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+38.sha-27431e0');
+var VERSION$1 = new Version('7.2.0-rc.0+39.sha-59f64dd');
 
 /**
  * @license
