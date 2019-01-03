3d94f9b7ca8f812989d93a3990db662d1677da5b
Ben Lesh <ben@benlesh.com>
2019-01-03 18:12:37 +0000

    5aecb2d docs: fix typo in TOH http section (#26127)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+22.sha-9a15a42
+ * @license Angular v7.2.0-rc.0+23.sha-5aecb2d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+22.sha-9a15a42');
+    var VERSION$1 = new Version('7.2.0-rc.0+23.sha-5aecb2d');
 
     /**
      * @license
