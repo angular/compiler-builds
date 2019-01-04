4b1fbbfdcb477049ecc7ec2d9e73e75bfe4378f7
Kara Erickson <karakara@google.com>
2019-01-04 21:01:30 +0000

    13d23f3 fix(ivy): ngtsc program emit ignoring custom transformers (#27837)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+59.sha-48555f9
+ * @license Angular v7.2.0-rc.0+60.sha-13d23f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15723,7 +15723,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+59.sha-48555f9');
+var VERSION$1 = new Version('7.2.0-rc.0+60.sha-13d23f3');
 
 /**
  * @license
