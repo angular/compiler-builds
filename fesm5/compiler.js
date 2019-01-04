dcd5cd58c8f7f81b6cd8ca1ffbff259f34b63286
Kara Erickson <karakara@google.com>
2019-01-04 20:51:48 +0000

    3f2ebbd fix(ivy): support bindings in ICUs in nested contexts (#27914)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
+ * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15722,7 +15722,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+57.sha-7eb2c41');
+var VERSION$1 = new Version('7.2.0-rc.0+56.sha-3f2ebbd');
 
 /**
  * @license
