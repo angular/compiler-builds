6a0092069ae434c816ce64aa60cd867be97b535e
Kara Erickson <karakara@google.com>
2019-01-04 20:26:35 +0000

    970e3a0 test: remove bower and polymer benchmarks (#27931)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+25.sha-791d865
+ * @license Angular v7.2.0-rc.0+26.sha-970e3a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+25.sha-791d865');
+const VERSION$1 = new Version('7.2.0-rc.0+26.sha-970e3a0');
 
 /**
  * @license
