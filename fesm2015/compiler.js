8779efc62c7784dcdb28cafbdca834ca7b7a2f3b
Kara Erickson <karakara@google.com>
2019-01-04 23:23:37 +0000

    929334b fix(ivy): should not throw when getting VCRef.parentInjector on the root view (#27909)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+63.sha-fb7816f
+ * @license Angular v7.2.0-rc.0+64.sha-929334b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+63.sha-fb7816f');
+const VERSION$1 = new Version('7.2.0-rc.0+64.sha-929334b');
 
 /**
  * @license
