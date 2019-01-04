309b1e764fd5fcfb528a968afab29d84563ad4e3
Kara Erickson <karakara@google.com>
2019-01-04 20:50:25 +0000

    7eb2c41 fix(ivy): ng-content tags in re-inserted templates should walk declaration tree (#27783)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+55.sha-38b4d15
+ * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15779,7 +15779,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+55.sha-38b4d15');
+    var VERSION$1 = new Version('7.2.0-rc.0+57.sha-7eb2c41');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+55.sha-38b4d15
+ * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15722,7 +15722,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+55.sha-38b4d15');
+var VERSION$1 = new Version('7.2.0-rc.0+57.sha-7eb2c41');
 
 /**
  * @license
