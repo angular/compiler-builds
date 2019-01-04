af0b246fcef6de1b29dfaae60c0ba32103a1d8dd
Kara Erickson <karakara@google.com>
2019-01-04 20:56:36 +0000

    48555f9 fix(ivy): ensure unique template and listener function identifiers (#27766)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-176b3f1
+ * @license Angular v7.2.0-rc.0+59.sha-48555f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -13922,10 +13922,10 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
                 if (name_1 === NON_BINDABLE_ATTR) {
                     isNonBindableMode = true;
                 }
-                else if (name_1 == 'style') {
+                else if (name_1 === 'style') {
                     stylingBuilder.registerStyleAttr(value);
                 }
-                else if (name_1 == 'class') {
+                else if (name_1 === 'class') {
                     stylingBuilder.registerClassAttr(value);
                 }
                 else if (attr.i18n) {
@@ -13955,7 +13955,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         var allOtherInputs = [];
         element.inputs.forEach(function (input) {
             if (!stylingBuilder.registerBoundInput(input)) {
-                if (input.type == 0 /* Property */) {
+                if (input.type === 0 /* Property */) {
                     if (input.i18n) {
                         i18nAttrs.push(input);
                     }
@@ -14051,7 +14051,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
             this.processStylingInstruction(implicit, stylingBuilder.buildElementStylingInstruction(element.sourceSpan, this.constantPool), true);
             // Generate Listeners (outputs)
             element.outputs.forEach(function (outputAst) {
-                _this.creationInstruction(outputAst.sourceSpan, Identifiers$1.listener, _this.prepareListenerParameter(element.name, outputAst));
+                _this.creationInstruction(outputAst.sourceSpan, Identifiers$1.listener, _this.prepareListenerParameter(element.name, outputAst, elementIndex));
             });
         }
         // the code here will collect all update-level styling instructions and add them to the
@@ -14120,8 +14120,8 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
             this.i18n.appendTemplate(template.i18n, templateIndex);
         }
         var tagName = sanitizeIdentifier(template.tagName || '');
-        var contextName = tagName ? this.contextName + "_" + tagName : '';
-        var templateName = contextName ? contextName + "_Template_" + templateIndex : "Template_" + templateIndex;
+        var contextName = (tagName ? this.contextName + '_' + tagName : '') + "_" + templateIndex;
+        var templateName = contextName + "_Template";
         var parameters = [
             literal(templateIndex),
             variable(templateName),
@@ -14139,7 +14139,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
             parameters.push(this.prepareRefsParameter(template.references));
             parameters.push(importExpr(Identifiers$1.templateRefExtractor));
         }
-        // handle property bindings e.g. p(1, 'forOf', ɵbind(ctx.items));
+        // handle property bindings e.g. p(1, 'ngForOf', ɵbind(ctx.items));
         var context = variable(CONTEXT_NAME);
         template.inputs.forEach(function (input) {
             var value = input.value.visit(_this._valueConverter);
@@ -14156,7 +14156,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         // Nested templates must not be visited until after their parent templates have completed
         // processing, so they are queued here until after the initial pass. Otherwise, we wouldn't
         // be able to support bindings in nested templates to local refs that occur after the
-        // template definition. e.g. <div *ngIf="showing"> {{ foo }} </div>  <div #foo></div>
+        // template definition. e.g. <div *ngIf="showing">{{ foo }}</div>  <div #foo></div>
         this._nestedTemplateFns.push(function () {
             var _a;
             var templateFunctionExpr = templateVisitor.buildTemplateFunction(template.children, template.variables, _this._ngContentSelectors.length + _this._ngContentSelectorsOffset, template.i18n);
@@ -14173,7 +14173,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         });
         // Generate listeners for directive output
         template.outputs.forEach(function (outputAst) {
-            _this.creationInstruction(outputAst.sourceSpan, Identifiers$1.listener, _this.prepareListenerParameter('ng_template', outputAst));
+            _this.creationInstruction(outputAst.sourceSpan, Identifiers$1.listener, _this.prepareListenerParameter('ng_template', outputAst, templateIndex));
         });
     };
     TemplateDefinitionBuilder.prototype.visitBoundText = function (text) {
@@ -14400,7 +14400,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         }));
         return this.constantPool.getConstLiteral(asLiteral(refsParam), true);
     };
-    TemplateDefinitionBuilder.prototype.prepareListenerParameter = function (tagName, outputAst) {
+    TemplateDefinitionBuilder.prototype.prepareListenerParameter = function (tagName, outputAst, index) {
         var _this = this;
         var eventName = outputAst.name;
         if (outputAst.type === 1 /* Animation */) {
@@ -14408,7 +14408,7 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         }
         var evNameSanitized = sanitizeIdentifier(eventName);
         var tagNameSanitized = sanitizeIdentifier(tagName);
-        var functionName = this.templateName + "_" + tagNameSanitized + "_" + evNameSanitized + "_listener";
+        var functionName = this.templateName + "_" + tagNameSanitized + "_" + evNameSanitized + "_" + index + "_listener";
         return function () {
             var listenerScope = _this._bindingScope.nestedScope(_this._bindingScope.bindingLevel);
             var bindingExpr = convertActionBinding(listenerScope, variable(CONTEXT_NAME), outputAst.handler, 'b', function () { return error('Unexpected interpolation'); });
@@ -14714,7 +14714,7 @@ function createCssSelector(tag, attributes) {
         var value = attributes[name];
         cssSelector.addAttribute(name, value);
         if (name.toLowerCase() === 'class') {
-            var classes = value.trim().split(/\s+/g);
+            var classes = value.trim().split(/\s+/);
             classes.forEach(function (className) { return cssSelector.addClassName(className); });
         }
     });
@@ -14812,10 +14812,11 @@ function prepareSyntheticAttributeName(name) {
 function isSingleElementTemplate(children) {
     return children.length === 1 && children[0] instanceof Element$1;
 }
+function isTextNode(node) {
+    return node instanceof Text$3 || node instanceof BoundText || node instanceof Icu$1;
+}
 function hasTextChildrenOnly(children) {
-    return !children.find(function (child) {
-        return !(child instanceof Text$3 || child instanceof BoundText || child instanceof Icu$1);
-    });
+    return children.every(isTextNode);
 }
 
 /**
@@ -15722,7 +15723,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+54.sha-176b3f1');
+var VERSION$1 = new Version('7.2.0-rc.0+59.sha-48555f9');
 
 /**
  * @license
