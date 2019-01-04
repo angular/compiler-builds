abb647f9438bbcc4d7627a08ab0bad034c065d04
Kara Erickson <karakara@google.com>
2019-01-04 20:55:13 +0000

    176b3f1 ci: make `integration_test` job logs less verbose (#27934)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
+ * @license Angular v7.2.0-rc.0+54.sha-176b3f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14791,7 +14791,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+56.sha-3f2ebbd');
+const VERSION$1 = new Version('7.2.0-rc.0+54.sha-176b3f1');
 
 /**
  * @license
