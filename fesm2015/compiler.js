4aa9006ecaca83c1613adebeb9ec607f40e68820
Kara Erickson <karakara@google.com>
2019-01-05 03:05:53 +0000

    7adcbb3 build: enforce minimum Bazel version of 0.21.0 in WORKSPACE so cache works with `yarn bazel` (#27935)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+66.sha-411de25
+ * @license Angular v7.2.0-rc.0+68.sha-7adcbb3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+66.sha-411de25');
+const VERSION$1 = new Version('7.2.0-rc.0+68.sha-7adcbb3');
 
 /**
  * @license
