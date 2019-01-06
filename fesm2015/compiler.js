939a3c8543a0b5a88738fa0c58dc6c58c833db10
Kara Erickson <karakara@google.com>
2019-01-06 08:04:00 +0000

    6b0c1a7 test(ivy): turn on platform-browser-dynamic tests on CI (#27940)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-4c9aff5
+ * @license Angular v7.2.0-rc.0+71.sha-6b0c1a7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+70.sha-4c9aff5');
+const VERSION$1 = new Version('7.2.0-rc.0+71.sha-6b0c1a7');
 
 /**
  * @license
