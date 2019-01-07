a05f8d74c71472dc68a2f441e8b5aa9ac9158d2d
Kara Erickson <karakara@google.com>
2019-01-07 23:56:04 +0000

    575cf3d ci: remove required ci status for travis from angular-robot.yaml (#27970)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+38.sha-9e04284
+ * @license Angular v7.2.0-rc.0+51.sha-575cf3d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+38.sha-9e04284');
+    var VERSION$1 = new Version('7.2.0-rc.0+51.sha-575cf3d');
 
     /**
      * @license
