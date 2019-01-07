3a89f0f428512beb231b56910d25b602c9ddff42
Kara Erickson <karakara@google.com>
2019-01-07 17:06:54 +0000

    654055b ci: add brandonroberts to @angular/framework-global-approvers-for-docs-only-changes (#27949)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+2.sha-e94e7ee
+ * @license Angular v7.2.0+1.sha-654055b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+2.sha-e94e7ee');
+const VERSION$1 = new Version('7.2.0+1.sha-654055b');
 
 /**
  * @license
