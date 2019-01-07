ecce40d13babd61e3fed3b31c54d99a72e6da261
Kara Erickson <karakara@google.com>
2019-01-07 23:16:36 +0000

    216bbd0 test(bazel): re-enable ng_package golden testing on ci (#27829)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+33.sha-bbe656e
+ * @license Angular v7.2.0-rc.0+37.sha-216bbd0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+33.sha-bbe656e');
+var VERSION$1 = new Version('7.2.0-rc.0+37.sha-216bbd0');
 
 /**
  * @license
