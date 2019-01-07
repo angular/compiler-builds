8980075512c9aec5ec9e834e327bb2a990fc2460
Kara Erickson <karakara@google.com>
2019-01-07 23:10:28 +0000

    a75c734 fix(ivy): restore NG defs after running tests in TestBed (#27786)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+9.sha-e775313
+ * @license Angular v7.2.0+10.sha-a75c734
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+9.sha-e775313');
+var VERSION$1 = new Version('7.2.0+10.sha-a75c734');
 
 /**
  * @license
