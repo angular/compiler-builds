d9a488cbf0e3a55c53ad28611d4ca38e173917bc
Kara Erickson <karakara@google.com>
2019-01-07 17:10:11 +0000

    e08feb7 refactor(ivy): unify native node handling around render parent and element / ICU containers (#27925)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+1.sha-654055b
+ * @license Angular v7.2.0+6.sha-e08feb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+1.sha-654055b');
+    var VERSION$1 = new Version('7.2.0+6.sha-e08feb7');
 
     /**
      * @license
