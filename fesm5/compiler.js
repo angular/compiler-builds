733759135542170978a2c51cd966b4f6f58a5481
Kara Erickson <karakara@google.com>
2019-01-08 22:22:15 +0000

    1a7f92c test(ivy): re-enable passing animation tests (#27997)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+43.sha-a084024
+ * @license Angular v7.2.0+44.sha-1a7f92c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15779,7 +15779,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+43.sha-a084024');
+var VERSION$1 = new Version('7.2.0+44.sha-1a7f92c');
 
 /**
  * @license
