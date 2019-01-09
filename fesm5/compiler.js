5accee6a43dde1e623a15e1c7e03feabd35b7a8e
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:51:47 +0000

    e1e4887 refactor(ivy): merge canInsertNativeNode and getParentNative (#28011)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+73.sha-91a8a4f
+ * @license Angular v7.2.0+78.sha-e1e4887
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15810,7 +15810,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+73.sha-91a8a4f');
+var VERSION$1 = new Version('7.2.0+78.sha-e1e4887');
 
 /**
  * @license
