0234bef54b5616a4ec0cf8c865a2cce0e794ff08
Andrew Kushnir <akushnir@google.com>
2019-01-09 19:29:24 +0000

    d6cfe2e build: use cross platform workspace_status_command (#27431)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+67.sha-d6cfe2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15867,7 +15867,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+56.sha-c3aa24c');
+    var VERSION$1 = new Version('7.2.0+67.sha-d6cfe2e');
 
     /**
      * @license
