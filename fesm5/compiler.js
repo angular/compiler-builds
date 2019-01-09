08d16aca2f126c52c43226f241d792e0c3891959
Andrew Kushnir <akushnir@google.com>
2019-01-09 20:59:11 +0000

    a06469c docs(forms): remove duplicated link (#27884) PR Close #27884

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+70.sha-ecc304a
+ * @license Angular v7.2.0-rc.0+72.sha-a06469c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+70.sha-ecc304a');
+var VERSION$1 = new Version('7.2.0-rc.0+72.sha-a06469c');
 
 /**
  * @license
