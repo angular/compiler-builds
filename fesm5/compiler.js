565cce6cca19212e378259c42c8d6b23736b3bc7
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:10:04 +0000

    9b2b9b3 docs(core): add missing closing backtick (`) (#27908) PR Close #27908

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+74.sha-9b2b9b3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __extends, __assign, __spread, __read, __values } from 'tslib';
+import { __extends, __assign, __spread, __values, __read } from 'tslib';
 
 /**
  * @license
@@ -15810,7 +15810,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+56.sha-c3aa24c');
+var VERSION$1 = new Version('7.2.0+74.sha-9b2b9b3');
 
 /**
  * @license
