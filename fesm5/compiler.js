fb80293bdf1108e72d75e72ea63cdb60e3b7a552
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:01:52 +0000

    582395b docs: fix typo PageNotFoundCompponent (#27799) closes: #27795

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+74.sha-9b2b9b3
+ * @license Angular v7.2.0+72.sha-582395b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15810,7 +15810,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+74.sha-9b2b9b3');
+var VERSION$1 = new Version('7.2.0+72.sha-582395b');
 
 /**
  * @license
