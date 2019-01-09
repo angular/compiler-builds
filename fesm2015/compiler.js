c2a4f641c3db45e3497f3813fcad04c6db766820
Kara Erickson <karakara@google.com>
2019-01-09 01:32:53 +0000

    1de4031 docs: use static zip function as an Observable creator. (#26790)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+54.sha-1c39ad3
+ * @license Angular v7.2.0+55.sha-1de4031
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14846,7 +14846,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+54.sha-1c39ad3');
+const VERSION$1 = new Version('7.2.0+55.sha-1de4031');
 
 /**
  * @license
