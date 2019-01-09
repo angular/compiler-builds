53c560d1f3ebdbc26eba92e7f5e49ff31be64d6e
Andrew Kushnir <akushnir@google.com>
2019-01-09 20:18:02 +0000

    76580b9 ci: add brandonroberts to @angular/docs-infra codeowners (#28023)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+67.sha-d6cfe2e
+ * @license Angular v7.2.0+71.sha-76580b9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14877,7 +14877,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+67.sha-d6cfe2e');
+const VERSION$1 = new Version('7.2.0+71.sha-76580b9');
 
 /**
  * @license
