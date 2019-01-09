f0c1d549ffaacb37c48c93954309e86ce9e6ed55
Kara Erickson <karakara@google.com>
2019-01-09 01:05:56 +0000

    1c39ad3 feat(ivy): reference external classes by their exported name (#27743)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+47.sha-b621a69
+ * @license Angular v7.2.0+54.sha-1c39ad3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14846,7 +14846,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+47.sha-b621a69');
+const VERSION$1 = new Version('7.2.0+54.sha-1c39ad3');
 
 /**
  * @license
