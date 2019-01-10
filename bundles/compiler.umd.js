b3966116791069f992d3734c2557b646a02b005e
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:29:55 +0000

    a64a7a4 test(upgrade): log more info to help debug CI flakes (#28045)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+77.sha-9bda446
+ * @license Angular v7.2.0-rc.0+78.sha-a64a7a4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+77.sha-9bda446');
+    var VERSION$1 = new Version('7.2.0-rc.0+78.sha-a64a7a4');
 
     /**
      * @license
