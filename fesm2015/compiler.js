616911fe40b2640f71b6d418de45ce4256c29273
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:58:53 +0000

    69ca7cd fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+74.sha-e5f5ad3
+ * @license Angular v7.2.0-rc.0+75.sha-69ca7cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+74.sha-e5f5ad3');
+const VERSION$1 = new Version('7.2.0-rc.0+75.sha-69ca7cd');
 
 /**
  * @license
