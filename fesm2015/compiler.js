f69b432659d13a62a4af5196d91d8b3413a9e1ae
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:21:43 +0000

    6003145 fix(ivy): properly rewrite imports in generated factory shims (#27998)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+85.sha-5a0deb8
+ * @license Angular v7.2.0+87.sha-6003145
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14886,7 +14886,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+85.sha-5a0deb8');
+const VERSION$1 = new Version('7.2.0+87.sha-6003145');
 
 /**
  * @license
