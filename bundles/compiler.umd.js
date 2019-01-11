26e325e83985ec860d3200ef553481c0d0b219e9
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:41:10 +0000

    0465dc6 build: update `version` in `package.json` (#28075)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+87.sha-0d9afd8
+ * @license Angular v7.2.0-rc.0+93.sha-0465dc6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+87.sha-0d9afd8');
+    var VERSION$1 = new Version('7.2.0-rc.0+93.sha-0465dc6');
 
     /**
      * @license
