3283d6e2a7c71e86fc252be81816785c81a28e13
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:27:42 +0000

    2530c9d docs: fix typo (#27865) PR Close #27865

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+85.sha-7bafe18
+ * @license Angular v7.2.0-rc.0+83.sha-2530c9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+85.sha-7bafe18');
+    var VERSION$1 = new Version('7.2.0-rc.0+83.sha-2530c9d');
 
     /**
      * @license
