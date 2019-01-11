217156a43d795225ee422b1628937176a4ac0de7
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:48:18 +0000

    df292c2 fix(ivy): TestBed should not clobber compilation of global-scope modules (#28033)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+128.sha-091a8a6
+ * @license Angular v7.2.0+126.sha-df292c2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14897,7 +14897,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+128.sha-091a8a6');
+const VERSION$1 = new Version('7.2.0+126.sha-df292c2');
 
 /**
  * @license
