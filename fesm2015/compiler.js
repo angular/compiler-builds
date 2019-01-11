3430df049b6bfd70c59512150027a87d6326504c
Andrew Kushnir <akushnir@google.com>
2019-01-11 22:48:42 +0000

    fe4d3a1 test(ivy): re-enable passing upgrade tests (#28088)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+133.sha-b73d678
+ * @license Angular v7.2.0+134.sha-fe4d3a1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+133.sha-b73d678');
+const VERSION$1 = new Version('7.2.0+134.sha-fe4d3a1');
 
 /**
  * @license
