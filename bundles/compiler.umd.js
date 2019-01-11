0482fc1e787ba0c6c1f3a218f236082741d37fd1
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:48:10 +0000

    091a8a6 fix(ivy): add missing @nocollapse annotation to __NG_ELEMENT_ID__ fields (#28050)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+122.sha-8c3f98f
+ * @license Angular v7.2.0+128.sha-091a8a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15887,7 +15887,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+122.sha-8c3f98f');
+    var VERSION$1 = new Version('7.2.0+128.sha-091a8a6');
 
     /**
      * @license
