ed8a2e165975171420bbcf30aaa2b187c9da897d
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:20 +0000

    26a8c09 docs(router): fixed a typo in CanLoad title text (#27894)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+108.sha-acb2cac
+ * @license Angular v7.2.0+110.sha-26a8c09
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14897,7 +14897,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+108.sha-acb2cac');
+const VERSION$1 = new Version('7.2.0+110.sha-26a8c09');
 
 /**
  * @license
