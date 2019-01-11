076406a90f403e00d68910cb7082c617f8b7f198
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:28:01 +0000

    d505468 docs: re-write interpolation section and add example (#25170)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+112.sha-f854eb7
+ * @license Angular v7.2.0+113.sha-d505468
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15830,7 +15830,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+112.sha-f854eb7');
+var VERSION$1 = new Version('7.2.0+113.sha-d505468');
 
 /**
  * @license
