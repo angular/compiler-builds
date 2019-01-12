62bbb62a3e03d2859a44ab48649ed84f3bf810d3
Keen Yee Liau <kyliau@umich.edu>
2019-01-12 00:42:50 +0000

    ef5bdcb docs(bazel): Getting started with Bazel + CLI

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+139.sha-6eeb19c
+ * @license Angular v7.2.0+144.sha-ef5bdcb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15823,7 +15823,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+139.sha-6eeb19c');
+    var VERSION$1 = new Version('7.2.0+144.sha-ef5bdcb');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+139.sha-6eeb19c
+ * @license Angular v7.2.0+144.sha-ef5bdcb
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14833,7 +14833,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+139.sha-6eeb19c');
+const VERSION$1 = new Version('7.2.0+144.sha-ef5bdcb');
 
 /**
  * @license
