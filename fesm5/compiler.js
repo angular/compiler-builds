c9b50f6ec4721ea266078a72137fca9a72e6bd59
Andrew Kushnir <akushnir@google.com>
2019-01-12 00:40:29 +0000

    31556fa docs: add library doc to guides in aio (#27581)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+96.sha-01d475d
+ * @license Angular v7.2.0-rc.0+97.sha-31556fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+96.sha-01d475d');
+var VERSION$1 = new Version('7.2.0-rc.0+97.sha-31556fa');
 
 /**
  * @license
