6e6d0e24eb94bbddd9e103bd6a6a6a6e545d987c
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:26:11 +0000

    5e6e96d docs: fix GitHub pages deployment command (#26896)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+150.sha-638375b
+ * @license Angular v7.2.0+152.sha-5e6e96d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14833,7 +14833,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+150.sha-638375b');
+const VERSION$1 = new Version('7.2.0+152.sha-5e6e96d');
 
 /**
  * @license
