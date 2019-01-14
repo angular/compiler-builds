3585368eb4d60b9f0840cd038fb7d4f6e33de694
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:32:25 +0000

    c5b67b9 docs: update link in universal guide (#26628)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+148.sha-78bc21c
+ * @license Angular v7.2.0+149.sha-c5b67b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14833,7 +14833,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+148.sha-78bc21c');
+const VERSION$1 = new Version('7.2.0+149.sha-c5b67b9');
 
 /**
  * @license
