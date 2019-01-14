bd2aab664af9676667c8d15c07c8c44b4f06a223
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:20:33 +0000

    2558e68 docs: add browser polyfills for IE11 with hash-based routing (#27135)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+101.sha-5f616ca
+ * @license Angular v7.2.0-rc.0+104.sha-2558e68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+101.sha-5f616ca');
+    var VERSION$1 = new Version('7.2.0-rc.0+104.sha-2558e68');
 
     /**
      * @license
