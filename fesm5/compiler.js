21f587354b74b117ff262ba5215b4186ecfef814
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:31:44 +0000

    78bc21c docs: fix scripts section and some minor issues in universal documentation (#26444)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+147.sha-48a03fc
+ * @license Angular v7.2.0+148.sha-78bc21c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+147.sha-48a03fc');
+var VERSION$1 = new Version('7.2.0+148.sha-78bc21c');
 
 /**
  * @license
