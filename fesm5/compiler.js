3cf1c43dc556cbb1a713fd0c495213a13f1a8f50
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:19:04 +0000

    5a8c14b test(docs-infra): increase timeout for all redirection tests (#28103)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+97.sha-31556fa
+ * @license Angular v7.2.0-rc.0+98.sha-5a8c14b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+97.sha-31556fa');
+var VERSION$1 = new Version('7.2.0-rc.0+98.sha-5a8c14b');
 
 /**
  * @license
