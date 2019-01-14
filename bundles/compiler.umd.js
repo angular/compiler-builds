fbf2e54eeb32cc887de0b82554eabadf67e271c5
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:23:48 +0000

    961d553 docs: change in(what should I import? answer) NgModule FAQs (#27677)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+104.sha-2558e68
+ * @license Angular v7.2.0-rc.0+109.sha-961d553
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+104.sha-2558e68');
+    var VERSION$1 = new Version('7.2.0-rc.0+109.sha-961d553');
 
     /**
      * @license
