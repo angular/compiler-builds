f6ef748797429cb7bf8f3634c53d13bc89dfdc6c
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:41:49 +0000

    2c3b084 perf(platform-server): use shared `DomElementSchemaRegistry` instance (#28150) (#28151)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+126.sha-918d906
+ * @license Angular v7.2.0-rc.0+127.sha-2c3b084
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+126.sha-918d906');
+    var VERSION$1 = new Version('7.2.0-rc.0+127.sha-2c3b084');
 
     /**
      * @license
