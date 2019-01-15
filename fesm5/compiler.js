3ce30bf63f941fabeafa6397bcc4cd0ab3f9d4eb
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:33:46 +0000

    60fecc1 fixup! refactor(ivy): simplify context discover based on feedback (#28101)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+174.sha-fa53150
+ * @license Angular v7.2.0+176.sha-60fecc1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15774,7 +15774,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+174.sha-fa53150');
+var VERSION$1 = new Version('7.2.0+176.sha-60fecc1');
 
 /**
  * @license
