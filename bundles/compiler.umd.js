6e80842345b7a1db1c31d4f7e3f6c68572a0a28d
Andrew Kushnir <akushnir@google.com>
2019-01-15 01:32:09 +0000

    88d4155 test(bazel): Use local_repository to load Angular (#28142)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+109.sha-961d553
+ * @license Angular v7.2.0-rc.0+121.sha-88d4155
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15592,7 +15592,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0-rc.0+109.sha-961d553');
+    var VERSION$1 = new Version('7.2.0-rc.0+121.sha-88d4155');
 
     /**
      * @license
