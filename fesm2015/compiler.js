52f5d2bb6b034da2800f50f0de024bd0ae8f3baf
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:11:10 +0000

    918d906 docs(router): add clarification for Router config (#28159)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+124.sha-9faad4b
+ * @license Angular v7.2.0-rc.0+126.sha-918d906
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14606,7 +14606,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0-rc.0+124.sha-9faad4b');
+const VERSION$1 = new Version('7.2.0-rc.0+126.sha-918d906');
 
 /**
  * @license
