9026b784770cecab3d9637ac77d31a486f4e9377
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:08:28 +0000

    2e5752e docs: update the triage and labels doc with the latest processes (#28127)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+180.sha-da8ee29
+ * @license Angular v7.2.0+181.sha-2e5752e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14837,7 +14837,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+180.sha-da8ee29');
+const VERSION$1 = new Version('7.2.0+181.sha-2e5752e');
 
 /**
  * @license
