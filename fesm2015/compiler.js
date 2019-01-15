753689df6d34dd2fb2ca15d39d6977d8d187ecc7
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:02:43 +0000

    e62eeed fix(ivy): ensure component/directive `class` selectors are properly understood (#27849)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+177.sha-06e5bf1
+ * @license Angular v7.2.0+178.sha-e62eeed
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14840,7 +14840,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.0+177.sha-06e5bf1');
+const VERSION$1 = new Version('7.2.0+178.sha-e62eeed');
 
 /**
  * @license
