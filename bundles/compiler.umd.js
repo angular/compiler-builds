eb6b3cf7a12c000550671b7ed0b55a12ad5bf17c
Andrew Kushnir <akushnir@google.com>
2019-01-16 21:57:18 +0000

    1e5e62e docs: release notes for the v7.2.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+198.sha-bac71ef
+ * @license Angular v7.2.0+199.sha-1e5e62e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15830,7 +15830,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.0+198.sha-bac71ef');
+    var VERSION$1 = new Version('7.2.0+199.sha-1e5e62e');
 
     /**
      * @license
