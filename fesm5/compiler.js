bb3b5f4a2f99dc3fec0eb263715247fa8a3cbe37
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:01:06 +0000

    332dcb2 test(upgrade): clean up global state after test (#28181)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+127.sha-2c3b084
+ * @license Angular v7.2.0-rc.0+128.sha-332dcb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15535,7 +15535,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0-rc.0+127.sha-2c3b084');
+var VERSION$1 = new Version('7.2.0-rc.0+128.sha-332dcb2');
 
 /**
  * @license
