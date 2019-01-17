89c0ee4082cf6cbc021787b0eb88abc91cc0a71f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:19:44 +0000

    e172e97 fix(ivy): ensure animation @trigger ordering is correctly delivered to the renderer (#28165)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+12.sha-5a582a8
+ * @license Angular v8.0.0-beta.0+11.sha-e172e97
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14833,7 +14833,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.0+12.sha-5a582a8');
+const VERSION$1 = new Version('8.0.0-beta.0+11.sha-e172e97');
 
 /**
  * @license
