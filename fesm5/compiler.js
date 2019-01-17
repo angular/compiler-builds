6d5e8673760a9a23c7f91d1b96ecd114fe95f3aa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:19:20 +0000

    896cf35 fix(ivy): ensure renderer begin/end methods are only called during change detection (#28192)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
+ * @license Angular v8.0.0-beta.0+9.sha-896cf35
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15773,7 +15773,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.0+8.sha-1f7d3b9');
+var VERSION$1 = new Version('8.0.0-beta.0+9.sha-896cf35');
 
 /**
  * @license
