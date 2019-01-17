4c98103afa2f5091823a875ff3fe624d54c5a88d
Andrew Kushnir <akushnir@google.com>
2019-01-17 00:41:53 +0000

    a570fdf fix(bazel): Fix integration test after v8 bump (#28194)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+199.sha-1e5e62e
+ * @license Angular v8.0.0-beta.0+1.sha-a570fdf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15773,7 +15773,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.0+199.sha-1e5e62e');
+var VERSION$1 = new Version('8.0.0-beta.0+1.sha-a570fdf');
 
 /**
  * @license
