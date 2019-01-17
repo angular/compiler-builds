c1cbcc7145cbc69b57a32c0bb59aa444a8361f21
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 01:57:26 +0000

    4ea6c27 build(bazel): Use @angular/cli from root package.json (#28139)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1
+ * @license Angular v7.2.1+3.sha-4ea6c27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1');
+    var VERSION$1 = new Version('7.2.1+3.sha-4ea6c27');
 
     /**
      * @license
