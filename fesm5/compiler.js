9375346803cf99f3486b16e250e01613af1f5376
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:36:26 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+5.sha-80967ce
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15773,7 +15773,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.0+5.sha-80967ce');
+var VERSION$1 = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
 /**
  * @license
