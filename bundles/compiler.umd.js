a674a6d250ec7b778a9a6ad0c9898d3368a33b29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:00:50 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15830,7 +15830,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+6.sha-50cf2ac');
+    var VERSION$1 = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14839,7 +14839,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.0+6.sha-50cf2ac');
+const VERSION$1 = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
 /**
  * @license
