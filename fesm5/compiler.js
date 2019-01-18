b50cd6f02795b52f878ce8058ad0432ed31bde07
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 17:23:52 +0000

    73616ab fix(bazel): increase node memory limit for ng_module rule to prevent OOM for big modules (#28237)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+16.sha-c1c8746
+ * @license Angular v8.0.0-beta.0+15.sha-73616ab
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15767,7 +15767,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.0+16.sha-c1c8746');
+var VERSION$1 = new Version('8.0.0-beta.0+15.sha-73616ab');
 
 /**
  * @license
