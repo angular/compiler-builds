cadaa7fa1ca0e465587dfbe46cd41611e282d3a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-18 18:27:32 +0000

    45bf911 feat(forms): add `markAllAsTouched()` to `AbstractControl` (#26812)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+20.sha-ab2bf83
+ * @license Angular v8.0.0-beta.0+21.sha-45bf911
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15769,7 +15769,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.0+20.sha-ab2bf83');
+var VERSION$1 = new Version('8.0.0-beta.0+21.sha-45bf911');
 
 /**
  * @license
