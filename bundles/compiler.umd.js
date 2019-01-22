c0d939d88f540112c3823965851c06292dd1bc1a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:52:48 +0000

    cf71668 build: hide vscode settings e.g. debug launch config (#28299)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+15.sha-54565ed
+ * @license Angular v7.2.1+16.sha-cf71668
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+15.sha-54565ed');
+    var VERSION$1 = new Version('7.2.1+16.sha-cf71668');
 
     /**
      * @license
