7ab98dd17359528f38a6f3dce676d2d32714d496
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:35:17 +0000

    54565ed docs: fix crisis-detail milestone files (#27025)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+14.sha-ab08385
+ * @license Angular v7.2.1+15.sha-54565ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+14.sha-ab08385');
+    var VERSION$1 = new Version('7.2.1+15.sha-54565ed');
 
     /**
      * @license
