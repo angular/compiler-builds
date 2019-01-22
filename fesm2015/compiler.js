0beb10faaca00b490572d7efb5e1869134abbe5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:13:47 +0000

    058aafc fix(ivy): fix styling context resolution for host bindings on containers (#28221)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+29.sha-6bd20e8
+ * @license Angular v8.0.0-beta.0+31.sha-058aafc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14835,7 +14835,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.0+29.sha-6bd20e8');
+const VERSION$1 = new Version('8.0.0-beta.0+31.sha-058aafc');
 
 /**
  * @license
