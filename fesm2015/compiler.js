11efdb67d63e89597be4f2d2075c7d2a64249c4f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:35:47 +0000

    37f8263 docs: correct array to map (#27379)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+51.sha-e0fbe86
+ * @license Angular v8.0.0-beta.0+54.sha-37f8263
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14835,7 +14835,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.0+51.sha-e0fbe86');
+const VERSION$1 = new Version('8.0.0-beta.0+54.sha-37f8263');
 
 /**
  * @license
