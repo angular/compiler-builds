25a2ba9668f2fd0ee1d311a207fc08f45d090e5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:34:48 +0000

    7439b46 docs: fix explanation of Injectable decorator in architecture guide (#27480)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+34.sha-9a965c9
+ * @license Angular v8.0.0-beta.0+35.sha-7439b46
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15769,7 +15769,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.0+34.sha-9a965c9');
+var VERSION$1 = new Version('8.0.0-beta.0+35.sha-7439b46');
 
 /**
  * @license
