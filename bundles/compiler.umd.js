ce4dca754b0dfdc466a69bb5156dd6365f1f4537
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 18:11:12 +0000

    eb8ccf6 fix(docs-infra): removal of the use of the ChildNode.remove() method that it isn't supported by IE (#28188)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+11.sha-607fd8e
+ * @license Angular v7.2.1+13.sha-eb8ccf6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+11.sha-607fd8e');
+    var VERSION$1 = new Version('7.2.1+13.sha-eb8ccf6');
 
     /**
      * @license
