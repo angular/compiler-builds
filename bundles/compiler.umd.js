4d25d35bae1cb9d29128fa13a26a21b129b8b15f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:00:41 +0000

    5c5fe34 release: cut the v7.2.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+20.sha-32737a6
+ * @license Angular v7.2.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.1+20.sha-32737a6');
+    var VERSION$1 = new Version('7.2.2');
 
     /**
      * @license
