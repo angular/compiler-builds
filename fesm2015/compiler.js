e93e80adeddf641d49696d34943c267903280f2b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:07:59 +0000

    fe9b3ea docs: release notes for the v7.2.2 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+65.sha-50df897
+ * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14835,7 +14835,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.0+65.sha-50df897');
+const VERSION$1 = new Version('8.0.0-beta.0+66.sha-fe9b3ea');
 
 /**
  * @license
