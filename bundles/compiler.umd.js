ae164ef94aba58ae1907506cfe55766111e6fb22
Jason Aden <jasonaden@google.com>
2019-01-23 23:57:53 +0000

    d4ecffe refactor: remove obsolete font-smoothing property (#28174)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+18.sha-cbd6264
+ * @license Angular v8.0.0-beta.1+20.sha-d4ecffe
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15919,7 +15919,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+18.sha-cbd6264');
+    var VERSION$1 = new Version('8.0.0-beta.1+20.sha-d4ecffe');
 
     /**
      * @license
