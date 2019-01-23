b2f598ce2171a8da3ad5d86843b0da44b9ac8562
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:46:51 +0000

    8f1198f release: cut the v8.0.0-beta.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
+ * @license Angular v8.0.0-beta.1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15826,7 +15826,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.0+66.sha-fe9b3ea');
+    var VERSION$1 = new Version('8.0.0-beta.1');
 
     /**
      * @license
