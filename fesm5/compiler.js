3120c643d64fa5827080afd78e7ce9327c26f8fd
Jason Aden <jasonaden@google.com>
2019-01-25 21:12:12 +0000

    b2811e5 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+28.sha-e2c98fb
+ * @license Angular v8.0.0-beta.1+29.sha-b2811e5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15862,7 +15862,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.1+28.sha-e2c98fb');
+var VERSION$1 = new Version('8.0.0-beta.1+29.sha-b2811e5');
 
 /**
  * @license
