533df4797a648ab39987cff7507a702c925f60f4
Jason Aden <jasonaden@google.com>
2019-01-25 21:06:01 +0000

    a1b9995 refactor(router): initalize browserUrlTree to empty tree (#28376)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+15.sha-35f7ff0
+ * @license Angular v7.2.2+16.sha-a1b9995
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.2+15.sha-35f7ff0');
+    var VERSION$1 = new Version('7.2.2+16.sha-a1b9995');
 
     /**
      * @license
