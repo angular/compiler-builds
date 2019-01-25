e11e1ae11282aa526881c2aa6dedb84c47bd4ed9
Jason Aden <jasonaden@google.com>
2019-01-25 21:27:25 +0000

    fdc2b0b fix(ivy): queries should register matches from top to bottom (#28319)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+43.sha-3d5a919
+ * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15862,7 +15862,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.1+43.sha-3d5a919');
+var VERSION$1 = new Version('8.0.0-beta.1+35.sha-fdc2b0b');
 
 /**
  * @license
