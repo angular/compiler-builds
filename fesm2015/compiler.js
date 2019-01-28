d1f5ca3aeec16396c530e07e164f6c5dbd303a82
Jason Aden <jasonaden@google.com>
2019-01-28 20:25:03 +0000

    2f19ad9 fix(docs-infra): upgrade codelyzer to 4.5.0 (#28389)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+16.sha-a1b9995
+ * @license Angular v7.2.2+25.sha-2f19ad9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.2+16.sha-a1b9995');
+const VERSION$1 = new Version('7.2.2+25.sha-2f19ad9');
 
 /**
  * @license
