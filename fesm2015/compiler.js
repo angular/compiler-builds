4f060745c6e1e63acb78696cdba808212a821d6f
Jason Aden <jasonaden@google.com>
2019-01-28 20:35:40 +0000

    12b8a6e fix(bazel): Builder should invoke local bazel/iblaze (#28303)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+28.sha-260ac20
+ * @license Angular v7.2.2+29.sha-12b8a6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.2+28.sha-260ac20');
+const VERSION$1 = new Version('7.2.2+29.sha-12b8a6e');
 
 /**
  * @license
