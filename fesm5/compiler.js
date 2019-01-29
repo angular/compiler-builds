ff2f8c796fb4a34b503466ad0c09df867f5b3f28
Jason Aden <jasonaden@google.com>
2019-01-29 18:01:23 +0000

    22f76df feat(compiler-cli): expose ngtsc as a TscPlugin (#28431)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+83.sha-e18a52e
+ * @license Angular v8.0.0-beta.1+84.sha-22f76df
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15861,7 +15861,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.1+83.sha-e18a52e');
+var VERSION$1 = new Version('8.0.0-beta.1+84.sha-22f76df');
 
 /**
  * @license
