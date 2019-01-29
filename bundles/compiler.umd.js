125f0a3565de9b686dc0f5870d7fba4dd1f0d059
Jason Aden <jasonaden@google.com>
2019-01-29 20:06:11 +0000

    b87bf39 fix(docs-infra): add hamburger button to CLI COMMANDS section (#28418)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+85.sha-227f7e4
+ * @license Angular v8.0.0-beta.1+86.sha-b87bf39
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15918,7 +15918,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.1+85.sha-227f7e4');
+    var VERSION$1 = new Version('8.0.0-beta.1+86.sha-b87bf39');
 
     /**
      * @license
