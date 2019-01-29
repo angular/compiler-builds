b71c9d6a9604fd0d43d4eda946854b911e8024ff
Jason Aden <jasonaden@google.com>
2019-01-29 23:32:39 +0000

    dabcb3e docs: fix Redundant "See also:" link in ContentChild (#28334)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+89.sha-ec414b4
+ * @license Angular v8.0.0-beta.1+94.sha-dabcb3e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14927,7 +14927,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.1+89.sha-ec414b4');
+const VERSION$1 = new Version('8.0.0-beta.1+94.sha-dabcb3e');
 
 /**
  * @license
