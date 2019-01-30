6ea01b009e9ac68f4e647a4fbf0e8e1d918285db
Jason Aden <jasonaden@google.com>
2019-01-30 01:06:04 +0000

    d11c2f9 docs: fix typo in lifecycle-hooks.md (#28206)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+44.sha-0ff48a1
+ * @license Angular v7.2.2+47.sha-d11c2f9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.2+44.sha-0ff48a1');
+const VERSION$1 = new Version('7.2.2+47.sha-d11c2f9');
 
 /**
  * @license
