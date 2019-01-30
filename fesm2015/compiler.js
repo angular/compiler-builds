e2a8fdb69f849495106f1c633de12485be69ae00
Jason Aden <jasonaden@google.com>
2019-01-30 01:23:43 +0000

    ad49962 docs: fix typo for Browserslist in build guide (#28328)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+95.sha-41e68f7
+ * @license Angular v8.0.0-beta.1+100.sha-ad49962
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14927,7 +14927,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.1+95.sha-41e68f7');
+const VERSION$1 = new Version('8.0.0-beta.1+100.sha-ad49962');
 
 /**
  * @license
