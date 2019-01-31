b42ff627f3ae82c3d5098dc0f4496e7b84975d05
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 21:10:28 +0000

    9efb39c test(ivy): ComponentFactoryResolver can resolve any component factory in Ivy (#28465)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+4.sha-c0b3835
+ * @license Angular v8.0.0-beta.2+6.sha-9efb39c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+4.sha-c0b3835');
+const VERSION$1 = new Version('8.0.0-beta.2+6.sha-9efb39c');
 
 /**
  * @license
