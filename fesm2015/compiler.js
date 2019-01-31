58705ce420f459ddc59239f6b7b18f82c13ab2a7
Matias Niemelä <matias@yearofmoo.com>
2019-01-31 22:51:21 +0000

    1b6d8a7 fix(ivy): queries should match container node itself before matching its views (#28473)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+6.sha-9efb39c
+ * @license Angular v8.0.0-beta.2+8.sha-1b6d8a7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+6.sha-9efb39c');
+const VERSION$1 = new Version('8.0.0-beta.2+8.sha-1b6d8a7');
 
 /**
  * @license
