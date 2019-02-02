a42ff91cd94080d497d9df67c6bfdf328f1ef0ec
Matias Niemelä <matias@yearofmoo.com>
2019-02-02 01:38:55 +0000

    72c3695 ci: pin ChromeDriver to a version compatible with docker image's Chrome (#28494)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+12.sha-8930f60
+ * @license Angular v8.0.0-beta.2+17.sha-72c3695
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.2+12.sha-8930f60');
+var VERSION$1 = new Version('8.0.0-beta.2+17.sha-72c3695');
 
 /**
  * @license
