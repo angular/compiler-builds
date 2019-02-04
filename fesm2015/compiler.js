aa851f203b4849d929f3f3189839bf471557b5ff
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:53:45 +0000

    4dfcbc6 build(docs-infra): update terser version in lockfile (#28512)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+17.sha-72c3695
+ * @license Angular v8.0.0-beta.2+21.sha-4dfcbc6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+17.sha-72c3695');
+const VERSION$1 = new Version('8.0.0-beta.2+21.sha-4dfcbc6');
 
 /**
  * @license
