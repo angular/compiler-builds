214eddf350b969be9e760dafa074362858220041
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 20:16:30 +0000

    2109c30 docs: fix shadow variable in tutorial example (#28499) PR Close #28499

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+16.sha-c64e666
+ * @license Angular v7.2.3+18.sha-2109c30
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.3+16.sha-c64e666');
+const VERSION$1 = new Version('7.2.3+18.sha-2109c30');
 
 /**
  * @license
