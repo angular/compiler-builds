101da34b19552a5018a53ab1e6a952a3e74d1882
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:09:34 +0000

    f2621db fix(core): remove createInjector() from public API (#28509)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+27.sha-463a789
+ * @license Angular v8.0.0-beta.2+35.sha-f2621db
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+27.sha-463a789');
+const VERSION$1 = new Version('8.0.0-beta.2+35.sha-f2621db');
 
 /**
  * @license
