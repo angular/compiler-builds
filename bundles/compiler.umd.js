95d86212aa6148b14fbe2efb3f96436d308187a9
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 16:15:34 +0000

    c64e666 test: log cli version in `cli-hello-world[-ivy]` integration projects (#28421)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+9.sha-5f9d574
+ * @license Angular v7.2.3+16.sha-c64e666
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.3+9.sha-5f9d574');
+    var VERSION$1 = new Version('7.2.3+16.sha-c64e666');
 
     /**
      * @license
