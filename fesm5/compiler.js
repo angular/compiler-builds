536a348a706696c29a942181e977c0f636e184ab
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:18:22 +0000

    fe6e76c ci: keep job alive when yarn request takes more than 10 minutes (#28458)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+20.sha-c1aa1bf
+ * @license Angular v7.2.3+24.sha-fe6e76c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.3+20.sha-c1aa1bf');
+var VERSION$1 = new Version('7.2.3+24.sha-fe6e76c');
 
 /**
  * @license
