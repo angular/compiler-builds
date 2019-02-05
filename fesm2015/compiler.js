e82ff294369de60bd82b103703214b1a1555bdba
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:55:08 +0000

    744b020 build: fix failing compiler-cli tests on windows (#28352)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
+ * @license Angular v8.0.0-beta.2+48.sha-744b020
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+39.sha-9ef8d2b');
+const VERSION$1 = new Version('8.0.0-beta.2+48.sha-744b020');
 
 /**
  * @license
