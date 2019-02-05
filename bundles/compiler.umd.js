69e7974beeb5bcea1512c2dc98392d8bec7440f3
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 18:41:20 +0000

    9ef8d2b ci: keep job alive when yarn request takes more than 10 minutes (#28458)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+38.sha-07fb4b5
+ * @license Angular v8.0.0-beta.2+39.sha-9ef8d2b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.2+38.sha-07fb4b5');
+    var VERSION$1 = new Version('8.0.0-beta.2+39.sha-9ef8d2b');
 
     /**
      * @license
