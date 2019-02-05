ac93203ce3b9cf0b645374f856c2c3417592d5ee
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 19:44:35 +0000

    a6fd118 build: switch to typescript's es2015 typings (#28134)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+24.sha-fe6e76c
+ * @license Angular v7.2.3+25.sha-a6fd118
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.3+24.sha-fe6e76c');
+    var VERSION$1 = new Version('7.2.3+25.sha-a6fd118');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.3+24.sha-fe6e76c
+ * @license Angular v7.2.3+25.sha-a6fd118
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.3+24.sha-fe6e76c');
+const VERSION$1 = new Version('7.2.3+25.sha-a6fd118');
 
 /**
  * @license
