bddd453162cb17eb55f48978ff55a1adc2da7772
Matias Niemelä <matias@yearofmoo.com>
2019-02-05 22:18:19 +0000

    4142db0 style: run format (#28532)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+50.sha-8d11627
+ * @license Angular v8.0.0-beta.2+57.sha-4142db0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+50.sha-8d11627');
+const VERSION$1 = new Version('8.0.0-beta.2+57.sha-4142db0');
 
 /**
  * @license
