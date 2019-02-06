5408c2554a44be76ed4c9466b162534bb930f63d
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:56:16 +0000

    d42f32c feat: optionally save complete performance log in chrome benchpress tests (#27551)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+6.sha-baf103c
+ * @license Angular v8.0.0-beta.3+10.sha-d42f32c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+6.sha-baf103c');
+    var VERSION$1 = new Version('8.0.0-beta.3+10.sha-d42f32c');
 
     /**
      * @license
