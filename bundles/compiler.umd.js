7228237bb03ba418f8caa22448c1bb45aa1df60a
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:47:31 +0000

    5a2c3ff fix(ivy): proper component resolution in case of inheritance (#28439)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
+ * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+1.sha-ed0cf7e');
+    var VERSION$1 = new Version('8.0.0-beta.3+2.sha-5a2c3ff');
 
     /**
      * @license
