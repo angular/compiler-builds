6a6b3b7ca6d63702fd3464b3e92e1dc1024a782a
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 01:29:19 +0000

    979582a build: add some debug info for yarn installs (#28553)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+59.sha-63b744b
+ * @license Angular v8.0.0-beta.2+60.sha-979582a
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.2+59.sha-63b744b');
+const VERSION$1 = new Version('8.0.0-beta.2+60.sha-979582a');
 
 /**
  * @license
