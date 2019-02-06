99134b3bb76b56e362fd92ad9fd69a7846ab1d3b
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 05:18:44 +0000

    5ebc0da fix(ivy): remove query results from destroyed embedded views (#28445)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+8.sha-d4add54
+ * @license Angular v8.0.0-beta.3+14.sha-5ebc0da
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+8.sha-d4add54');
+    var VERSION$1 = new Version('8.0.0-beta.3+14.sha-5ebc0da');
 
     /**
      * @license
