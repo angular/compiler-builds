ee334058fe2a6bd1095b515aa8631a586652ada9
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:31:27 +0000

    851cf16 docs(service-worker): note about isStable/setInterval (#28102)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+26.sha-0e6f799
+ * @license Angular v8.0.0-beta.3+28.sha-851cf16
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+26.sha-0e6f799');
+const VERSION$1 = new Version('8.0.0-beta.3+28.sha-851cf16');
 
 /**
  * @license
