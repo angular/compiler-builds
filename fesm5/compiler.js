7f3a2135cb06546e7493513f5d95d50f229c09c2
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:46:28 +0000

    ed0cf7e fix(router): set href when routerLink is used on an 'area' element (#28441)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+61.sha-5a6f0d0
+ * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.2+61.sha-5a6f0d0');
+var VERSION$1 = new Version('8.0.0-beta.3+1.sha-ed0cf7e');
 
 /**
  * @license
