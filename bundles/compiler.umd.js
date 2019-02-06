e516a9f8ebb76d2b5d4dd285aa4d8bac8a2f2cae
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 20:46:07 +0000

    47f5b5f docs(service-worker): note about isStable/setInterval (#28102)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+21.sha-a0e3a8e
+ * @license Angular v7.2.4+23.sha-47f5b5f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+21.sha-a0e3a8e');
+    var VERSION$1 = new Version('7.2.4+23.sha-47f5b5f');
 
     /**
      * @license
