1ed58ee078641c964bda582c53dce90e4dd9edaf
Misko Hevery <misko@hevery.com>
2019-02-07 19:41:59 +0000

    50732e1 fix(router): redirect to root url when returned as UrlTree from guard (#28271)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+16.sha-62a13e7
+ * @license Angular v8.0.0-beta.3+35.sha-50732e1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+16.sha-62a13e7');
+const VERSION$1 = new Version('8.0.0-beta.3+35.sha-50732e1');
 
 /**
  * @license
