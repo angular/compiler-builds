06c502ea937577b0a85bd1b8517b9fb02cc344d4
Miško Hevery <misko@hevery.com>
2019-02-07 20:54:41 +0000

    264ef72 build: update render3 section of integration test readme (#28372)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+35.sha-50732e1
+ * @license Angular v8.0.0-beta.3+42.sha-264ef72
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+35.sha-50732e1');
+var VERSION$1 = new Version('8.0.0-beta.3+42.sha-264ef72');
 
 /**
  * @license
