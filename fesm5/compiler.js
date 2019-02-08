da13b0cab28a5249c0f8f0b8ea28ce2742b04ce3
Miško Hevery <misko@hevery.com>
2019-02-08 01:06:53 +0000

    bd9da26 docs: update HttpHeader documentation (#26233)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+29.sha-1e58a21
+ * @license Angular v7.2.4+30.sha-bd9da26
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.4+29.sha-1e58a21');
+var VERSION$1 = new Version('7.2.4+30.sha-bd9da26');
 
 /**
  * @license
