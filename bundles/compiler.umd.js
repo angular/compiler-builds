f827f06aa7dbb4f4052952ebbc13ff42be1714e5
Miško Hevery <misko@hevery.com>
2019-02-08 04:22:29 +0000

    94f042b refactor(ivy): remove unneeded detach property (#28595) PR Close #28595

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+35.sha-50732e1
+ * @license Angular v8.0.0-beta.3+53.sha-94f042b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+35.sha-50732e1');
+    var VERSION$1 = new Version('8.0.0-beta.3+53.sha-94f042b');
 
     /**
      * @license
