11dd3fbaaad3dfd4b544c26b0174983f147911a2
Miško Hevery <misko@hevery.com>
2019-02-08 01:10:31 +0000

    6f9dd1b refactor(common): Remove deprecated FileType (#28583)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+48.sha-c3fadad
+ * @license Angular v8.0.0-beta.3+49.sha-6f9dd1b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+48.sha-c3fadad');
+var VERSION$1 = new Version('8.0.0-beta.3+49.sha-6f9dd1b');
 
 /**
  * @license
