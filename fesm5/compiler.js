6af1e4c8497a7ef47b1f0f8676cfb166ef7fd480
Miško Hevery <misko@hevery.com>
2019-02-08 17:34:17 +0000

    ceada77 ci: add all required statuses to the requiredStatuses list for angular-robot (#28613)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+56.sha-d91ea2c
+ * @license Angular v8.0.0-beta.3+57.sha-ceada77
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+56.sha-d91ea2c');
+var VERSION$1 = new Version('8.0.0-beta.3+57.sha-ceada77');
 
 /**
  * @license
