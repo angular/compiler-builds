37291f4ae802c1b84669f3b0a6bc0f839c36b701
Miško Hevery <misko@hevery.com>
2019-02-08 22:07:26 +0000

    7115e7c build: remove outdated polymer tree benchmark (#28568)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+62.sha-44de68c
+ * @license Angular v8.0.0-beta.3+68.sha-7115e7c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+62.sha-44de68c');
+    var VERSION$1 = new Version('8.0.0-beta.3+68.sha-7115e7c');
 
     /**
      * @license
