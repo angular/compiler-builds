638f599b8a61b7da994b84dc0fff769d2b64f540
Misko Hevery <misko@hevery.com>
2019-02-08 06:19:50 +0000

    faf0b25 docs(aio): add Happy Angular Podcast to resources (#28407)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+54.sha-9067025
+ * @license Angular v8.0.0-beta.3+55.sha-faf0b25
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15890,7 +15890,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+54.sha-9067025');
+var VERSION$1 = new Version('8.0.0-beta.3+55.sha-faf0b25');
 
 /**
  * @license
