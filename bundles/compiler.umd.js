ade79755c9765eeea44a81b6a7b35022a14bc244
Miško Hevery <misko@hevery.com>
2019-02-08 01:03:11 +0000

    c3fadad docs: update HttpHeader documentation (#26233)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+47.sha-b65fe62
+ * @license Angular v8.0.0-beta.3+48.sha-c3fadad
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15947,7 +15947,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+47.sha-b65fe62');
+    var VERSION$1 = new Version('8.0.0-beta.3+48.sha-c3fadad');
 
     /**
      * @license
