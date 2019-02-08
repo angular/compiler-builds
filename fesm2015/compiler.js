89b70afb1fce3b66d756f439ebb8dceee4ab8ca4
Miško Hevery <misko@hevery.com>
2019-02-08 17:47:19 +0000

    1ff3699 test(upgrade): change flaky test to not be affected by other tests (#28617)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+59.sha-0e4705a
+ * @license Angular v8.0.0-beta.3+60.sha-1ff3699
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14967,7 +14967,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+59.sha-0e4705a');
+const VERSION$1 = new Version('8.0.0-beta.3+60.sha-1ff3699');
 
 /**
  * @license
