d862c39e0ce900e212877c88dd44d1bafacc613a
Miško Hevery <misko@hevery.com>
2019-02-11 17:32:26 +0000

    8bd7d5b docs: clarify library types integration (#28410)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+45.sha-aa163be
+ * @license Angular v7.2.4+46.sha-8bd7d5b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+45.sha-aa163be');
+    var VERSION$1 = new Version('7.2.4+46.sha-8bd7d5b');
 
     /**
      * @license
