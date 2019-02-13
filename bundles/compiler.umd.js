ec5fc7c729eca9ef08c7ed35442714719df90bb5
Miško Hevery <misko@hevery.com>
2019-02-13 18:22:37 +0000

    0c7581d refactor(bazel): use multi_sass_binary rule (#28669)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+129.sha-91b7152
+ * @license Angular v8.0.0-beta.3+131.sha-0c7581d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16231,7 +16231,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+129.sha-91b7152');
+    var VERSION$1 = new Version('8.0.0-beta.3+131.sha-0c7581d');
 
     /**
      * @license
