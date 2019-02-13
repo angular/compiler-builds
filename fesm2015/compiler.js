b3d463e28ec9948a33f15b0d0edef3b897bcdc63
Miško Hevery <misko@hevery.com>
2019-02-13 20:54:07 +0000

    6fa4235 test(ivy): enable passing Material tests (#28687)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+152.sha-dba2a40
+ * @license Angular v8.0.0-beta.3+146.sha-6fa4235
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15244,7 +15244,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+152.sha-dba2a40');
+const VERSION$1 = new Version('8.0.0-beta.3+146.sha-6fa4235');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+152.sha-dba2a40
+ * @license Angular v8.0.0-beta.3+146.sha-6fa4235
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16176,7 +16176,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+152.sha-dba2a40');
+var VERSION$1 = new Version('8.0.0-beta.3+146.sha-6fa4235');
 
 /**
  * @license
