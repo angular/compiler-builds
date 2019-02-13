c0d602e4f48ca260fb623e94bb6d3b67e84a8338
Miško Hevery <misko@hevery.com>
2019-02-13 20:53:35 +0000

    dba2a40 build: remove legacy e2e tests job (#28645)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+144.sha-2f27a80
+ * @license Angular v8.0.0-beta.3+152.sha-dba2a40
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15244,7 +15244,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+144.sha-2f27a80');
+const VERSION$1 = new Version('8.0.0-beta.3+152.sha-dba2a40');
 
 /**
  * @license
