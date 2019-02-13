7463ff7fd52f44a7d5ff5b98c22392af2dfa5848
Misko Hevery <misko@hevery.com>
2019-02-13 06:24:49 +0000

    ac58d01 refactor(ivy): remove unused notImplement function (#28630)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+125.sha-d68a98f
+ * @license Angular v8.0.0-beta.3+126.sha-ac58d01
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15253,7 +15253,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+125.sha-d68a98f');
+const VERSION$1 = new Version('8.0.0-beta.3+126.sha-ac58d01');
 
 /**
  * @license
