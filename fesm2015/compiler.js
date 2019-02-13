90d1c8733aa4b7d0bd5c345dd6da7ace8553b9b7
Miško Hevery <misko@hevery.com>
2019-02-13 18:20:57 +0000

    91b7152 feat(compiler-cli): no longer re-export external symbols by default (#28633)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+130.sha-06ec95f
+ * @license Angular v8.0.0-beta.3+129.sha-91b7152
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15253,7 +15253,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+130.sha-06ec95f');
+const VERSION$1 = new Version('8.0.0-beta.3+129.sha-91b7152');
 
 /**
  * @license
