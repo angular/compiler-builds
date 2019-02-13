86ef6b8f02b2383a0ae3030ffabc8c9cde6ab94c
Miško Hevery <misko@hevery.com>
2019-02-13 18:24:56 +0000

    49fb8c3 fix(bazel): Install angular repo before yarn_install (#28670)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+133.sha-2ea030c
+ * @license Angular v8.0.0-beta.3+132.sha-49fb8c3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16174,7 +16174,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+133.sha-2ea030c');
+var VERSION$1 = new Version('8.0.0-beta.3+132.sha-49fb8c3');
 
 /**
  * @license
