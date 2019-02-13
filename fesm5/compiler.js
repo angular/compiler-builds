3f98b1fd87ff12c314cb224392381d861cd58dc7
Miško Hevery <misko@hevery.com>
2019-02-13 04:52:57 +0000

    6050cd0 ci: downgrade to sauce-connect v4.5.1 (#28659)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+107.sha-0cb02d9
+ * @license Angular v8.0.0-beta.3+108.sha-6050cd0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15917,7 +15917,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+107.sha-0cb02d9');
+var VERSION$1 = new Version('8.0.0-beta.3+108.sha-6050cd0');
 
 /**
  * @license
