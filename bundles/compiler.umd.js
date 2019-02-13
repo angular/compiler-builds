d6b9f120fd5f67522e70f2fcaecccb99ad06e4f2
Miško Hevery <misko@hevery.com>
2019-02-13 20:52:55 +0000

    2f27a80 test(ivy): ivy change detection doesn't descend into CD-detached view trees (#28680)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+143.sha-77eee42
+ * @license Angular v8.0.0-beta.3+144.sha-2f27a80
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16233,7 +16233,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+143.sha-77eee42');
+    var VERSION$1 = new Version('8.0.0-beta.3+144.sha-2f27a80');
 
     /**
      * @license
