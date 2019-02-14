eb8f9e665252287a887144b9d9b3e1357088b1ff
Misko Hevery <misko@hevery.com>
2019-02-14 04:00:28 +0000

    1df3aef fix(forms): mark form as pristine before emitting value and status change events (#28395)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+162.sha-3842dd6
+ * @license Angular v8.0.0-beta.3+167.sha-1df3aef
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15271,7 +15271,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.3+162.sha-3842dd6');
+const VERSION$1 = new Version('8.0.0-beta.3+167.sha-1df3aef');
 
 /**
  * @license
