08f6ac4c96db77e4cc6e246337c3ad9da7bd398f
Misko Hevery <misko@hevery.com>
2019-02-14 05:43:59 +0000

    65c2dea refactor(bazel): Fix warning about overridden tsconfig options (#28674)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+167.sha-1df3aef
+ * @license Angular v8.0.0-beta.3+173.sha-65c2dea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16260,7 +16260,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+167.sha-1df3aef');
+    var VERSION$1 = new Version('8.0.0-beta.3+173.sha-65c2dea');
 
     /**
      * @license
