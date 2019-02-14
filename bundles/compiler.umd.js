2d28e19e13f0d2e956d42dc5ac99b1197be8c772
Miško Hevery <misko@hevery.com>
2019-02-14 19:51:26 +0000

    63e5d27 fix(ivy): rethrow event handler errors in tests (#28707)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+178.sha-7cbc36f
+ * @license Angular v8.0.0-beta.3+177.sha-63e5d27
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16260,7 +16260,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+178.sha-7cbc36f');
+    var VERSION$1 = new Version('8.0.0-beta.3+177.sha-63e5d27');
 
     /**
      * @license
