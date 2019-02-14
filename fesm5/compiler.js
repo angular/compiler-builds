71e67ece371de1e593c527f964a37a449e9856b8
Miško Hevery <misko@hevery.com>
2019-02-14 19:52:01 +0000

    b6dffa9 docs: incorporate review feedback (#26827)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+79.sha-e2b3cc8
+ * @license Angular v7.2.4+81.sha-b6dffa9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.4+79.sha-e2b3cc8');
+var VERSION$1 = new Version('7.2.4+81.sha-b6dffa9');
 
 /**
  * @license
