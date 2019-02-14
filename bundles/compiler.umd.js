8d733c49a4b50147fc8e07629367334545da0e9f
Misko Hevery <misko@hevery.com>
2019-02-14 03:49:56 +0000

    8cec4b3 build: make api-extractor work in google3 (#28588)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+168.sha-b0afc4c
+ * @license Angular v8.0.0-beta.3+166.sha-8cec4b3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16260,7 +16260,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.3+168.sha-b0afc4c');
+    var VERSION$1 = new Version('8.0.0-beta.3+166.sha-8cec4b3');
 
     /**
      * @license
