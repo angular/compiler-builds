15c8cb4270a473cccb83330010b42fd34ae81e97
Miško Hevery <misko@hevery.com>
2019-02-15 22:43:20 +0000

    fcc8c56 docs: release notes for the v7.2.5 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+181.sha-8accc98
+ * @license Angular v8.0.0-beta.3+182.sha-fcc8c56
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16207,7 +16207,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.3+181.sha-8accc98');
+var VERSION$1 = new Version('8.0.0-beta.3+182.sha-fcc8c56');
 
 /**
  * @license
