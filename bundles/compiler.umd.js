0c5330bf0f636d47f7f8e7c153be671bc8aefc9f
Miško Hevery <misko@hevery.com>
2019-02-15 21:49:58 +0000

    a2ba444 release: cut the v release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+82.sha-ac6b2b4
+ * @license Angular v7.2.4+83.sha-a2ba444
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+82.sha-ac6b2b4');
+    var VERSION$1 = new Version('7.2.4+83.sha-a2ba444');
 
     /**
      * @license
