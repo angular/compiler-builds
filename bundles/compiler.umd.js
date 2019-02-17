43ed30afea4fcaa8093422a5d60ff196c8649f36
Misko Hevery <misko@hevery.com>
2019-02-17 05:18:21 +0000

    bf25165 build(bazel): specify tsconfig-test dependency (#28696)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+83.sha-a2ba444
+ * @license Angular v7.2.5+1.sha-bf25165
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.4+83.sha-a2ba444');
+    var VERSION$1 = new Version('7.2.5+1.sha-bf25165');
 
     /**
      * @license
