9329e1fad9973a318d440babe6f7f8a92ff59cf0
Misko Hevery <misko@hevery.com>
2019-02-17 05:23:43 +0000

    c1094cf refactor(ivy): remove duplicated getRootView function (#28732)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4
+ * @license Angular v8.0.0-beta.4+2.sha-c1094cf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15275,7 +15275,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4');
+const VERSION$1 = new Version('8.0.0-beta.4+2.sha-c1094cf');
 
 /**
  * @license
