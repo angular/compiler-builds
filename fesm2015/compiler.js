27a36e05e643cc5014d21a9825c895ae6cfd8d03
Igor Minar <igor@angular.io>
2019-02-19 21:12:12 +0000

    f6ca619 test: bazel integration test should use angular npm packages (#28762)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+12.sha-dd3cab6
+ * @license Angular v7.2.5+16.sha-f6ca619
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.5+12.sha-dd3cab6');
+const VERSION$1 = new Version('7.2.5+16.sha-f6ca619');
 
 /**
  * @license
