bb98efaed87e8c88ccc05a29041a0b7f4a575a81
Igor Minar <igor@angular.io>
2019-02-19 21:07:32 +0000

    80c7aff build(docs-infra): improve caching configuration for Firebase hosting (#28785)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+19.sha-6788d86
+ * @license Angular v8.0.0-beta.4+20.sha-80c7aff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15275,7 +15275,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4+19.sha-6788d86');
+const VERSION$1 = new Version('8.0.0-beta.4+20.sha-80c7aff');
 
 /**
  * @license
