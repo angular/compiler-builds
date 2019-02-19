7367f34d62597e803cec45907b34bee335c57a6c
Igor Minar <igor@angular.io>
2019-02-19 20:59:37 +0000

    cdcbb53 docs: capitalize karma (#28775)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+7.sha-1df6b16
+ * @license Angular v7.2.5+9.sha-cdcbb53
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.5+7.sha-1df6b16');
+var VERSION$1 = new Version('7.2.5+9.sha-cdcbb53');
 
 /**
  * @license
