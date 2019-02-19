fb21a1eeade763c8f6444cbc94fcd2ea2f7062c2
Igor Minar <igor@angular.io>
2019-02-19 21:56:20 +0000

    ae16378 docs: fix wrong filename in text (#28832)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+29.sha-d8ae899
+ * @license Angular v8.0.0-beta.4+32.sha-ae16378
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16264,7 +16264,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+29.sha-d8ae899');
+    var VERSION$1 = new Version('8.0.0-beta.4+32.sha-ae16378');
 
     /**
      * @license
