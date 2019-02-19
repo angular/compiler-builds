384517748762ef28fe3ba2bdcae47e82ef3eed71
Igor Minar <igor@angular.io>
2019-02-19 19:26:26 +0000

    980bb52 docs: warn that directives don't support namespaces (#25855)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+7.sha-3c7ce82
+ * @license Angular v8.0.0-beta.4+9.sha-980bb52
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15275,7 +15275,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4+7.sha-3c7ce82');
+const VERSION$1 = new Version('8.0.0-beta.4+9.sha-980bb52');
 
 /**
  * @license
