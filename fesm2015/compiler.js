81c9c5ebb4963188d85d9abc5ac98638087decb4
Ben Lesh <ben@benlesh.com>
2019-02-20 21:45:49 +0000

    a7e1c0c feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+54.sha-81c3104
+ * @license Angular v8.0.0-beta.4+56.sha-a7e1c0c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4+54.sha-81c3104');
+const VERSION$1 = new Version('8.0.0-beta.4+56.sha-a7e1c0c');
 
 /**
  * @license
