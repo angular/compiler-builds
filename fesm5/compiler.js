ea13bc3cf79b64163beef337e5751cf2c105272b
Igor Minar <igor@angular.io>
2019-02-20 02:16:33 +0000

    1eccf64 build: Add .bzl/BUILD files to @angular/bazel package (#28769)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+43.sha-2d80419
+ * @license Angular v8.0.0-beta.4+44.sha-1eccf64
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16210,7 +16210,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.4+43.sha-2d80419');
+var VERSION$1 = new Version('8.0.0-beta.4+44.sha-1eccf64');
 
 /**
  * @license
