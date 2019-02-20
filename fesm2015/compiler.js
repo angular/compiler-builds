f7e679cf31b5deec2984323169c67aaf69f1c47b
Igor Minar <igor@angular.io>
2019-02-20 22:11:11 +0000

    05c25cc build: update deps in ./scripts/release/post-check

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+2.sha-cfb2d17
+ * @license Angular v8.0.0-beta.5+3.sha-05c25cc
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+2.sha-cfb2d17');
+const VERSION$1 = new Version('8.0.0-beta.5+3.sha-05c25cc');
 
 /**
  * @license
