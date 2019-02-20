605de6075b4f326be5c0dccac919c2444537592f
Igor Minar <igor@angular.io>
2019-02-20 17:36:30 +0000

    069146e ci: exclude **/third_party/** from google3 check (#28870)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+24.sha-fc895ba
+ * @license Angular v7.2.5+25.sha-069146e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.5+24.sha-fc895ba');
+    var VERSION$1 = new Version('7.2.5+25.sha-069146e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+24.sha-fc895ba
+ * @license Angular v7.2.5+25.sha-069146e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14832,7 +14832,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.5+24.sha-fc895ba');
+const VERSION$1 = new Version('7.2.5+25.sha-069146e');
 
 /**
  * @license
