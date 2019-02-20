31b94e707fab8da308a76af574a1e1fd1570a54d
Igor Minar <igor@angular.io>
2019-02-20 06:47:42 +0000

    96b597c fix(bazel): Add postinstall step to generate summaries (#28850)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+45.sha-669b441
+ * @license Angular v8.0.0-beta.4+46.sha-96b597c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4+45.sha-669b441');
+const VERSION$1 = new Version('8.0.0-beta.4+46.sha-96b597c');
 
 /**
  * @license
