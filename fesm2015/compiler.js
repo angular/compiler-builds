cbe91ac369445913aa33ba3d8b756e34f5e26ffe
Igor Minar <igor@angular.io>
2019-02-20 06:47:46 +0000

    669b441 test(ivy): update material golden file to turn on passing tests (#28848)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+44.sha-1eccf64
+ * @license Angular v8.0.0-beta.4+45.sha-669b441
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.4+44.sha-1eccf64');
+const VERSION$1 = new Version('8.0.0-beta.4+45.sha-669b441');
 
 /**
  * @license
