a43d7a08a301379eba70759630038dedc6c1b9bf
Igor Minar <igor@angular.io>
2019-02-20 00:55:16 +0000

    2d80419 fix(bazel): Schematics should upgrade rxjs to 6.4.0 (#28841)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+42.sha-fd4e1d6
+ * @license Angular v8.0.0-beta.4+43.sha-2d80419
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16267,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+42.sha-fd4e1d6');
+    var VERSION$1 = new Version('8.0.0-beta.4+43.sha-2d80419');
 
     /**
      * @license
