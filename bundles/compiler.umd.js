8d2858a62f98aa3f7a877fb073ed15bbaa7610d3
Igor Minar <igor@angular.io>
2019-02-20 21:30:44 +0000

    27eb8f2 build: update deps in ./scripts/release/post-check

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+29.sha-843fc7d
+ * @license Angular v7.2.5+31.sha-27eb8f2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.5+29.sha-843fc7d');
+    var VERSION$1 = new Version('7.2.5+31.sha-27eb8f2');
 
     /**
      * @license
