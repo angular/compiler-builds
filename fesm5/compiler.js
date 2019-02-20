8aab6cd559d887d6e24d465afe5f09a867816d87
Igor Minar <igor@angular.io>
2019-02-20 17:44:36 +0000

    04ae125 fix(docs-infra): prevent extra space at the bottom of the page in Chrome (#28864)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+50.sha-1832e0f
+ * @license Angular v8.0.0-beta.4+51.sha-04ae125
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16210,7 +16210,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.4+50.sha-1832e0f');
+var VERSION$1 = new Version('8.0.0-beta.4+51.sha-04ae125');
 
 /**
  * @license
