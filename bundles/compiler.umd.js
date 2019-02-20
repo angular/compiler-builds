61f4ee10b2e0f70348b6dc46ac9329b33f43324d
Igor Minar <igor@angular.io>
2019-02-20 19:15:53 +0000

    b9eb662 test(bazel): Upgrade CLI dependencies for schematics test (#28872)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+51.sha-04ae125
+ * @license Angular v8.0.0-beta.4+52.sha-b9eb662
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16267,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.4+51.sha-04ae125');
+    var VERSION$1 = new Version('8.0.0-beta.4+52.sha-b9eb662');
 
     /**
      * @license
