1bbdeaa903f4d1b4e7eed1ed309d0e1d6a445870
Igor Minar <igor@angular.io>
2019-02-21 01:09:30 +0000

    13b96ac ci: run saucelabs unit tests as cronjob (#28787)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+5.sha-623fd3f
+ * @license Angular v8.0.0-beta.5+6.sha-13b96ac
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+5.sha-623fd3f');
+const VERSION$1 = new Version('8.0.0-beta.5+6.sha-13b96ac');
 
 /**
  * @license
