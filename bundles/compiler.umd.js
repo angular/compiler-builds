a7f1ba0a17717485cd8b71bbd51a0c7e74ba79e4
Igor Minar <igor@angular.io>
2019-02-21 08:37:35 +0000

    df627e6 fix(ivy): correct absolute path processing for templateUrl and styleUrls (#28789)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+10.sha-be121bb
+ * @license Angular v8.0.0-beta.5+12.sha-df627e6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16267,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+10.sha-be121bb');
+    var VERSION$1 = new Version('8.0.0-beta.5+12.sha-df627e6');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+10.sha-be121bb
+ * @license Angular v8.0.0-beta.5+12.sha-df627e6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15278,7 +15278,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+10.sha-be121bb');
+const VERSION$1 = new Version('8.0.0-beta.5+12.sha-df627e6');
 
 /**
  * @license
