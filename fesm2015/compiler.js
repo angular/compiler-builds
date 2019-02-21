2de8570e62282f535e3a5c81721a5f7a6cdeec57
Igor Minar <igor@angular.io>
2019-02-21 08:39:29 +0000

    95d9aa2 fix(ivy): allow HTML comments to be present inside <ng-content> (#28849)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+14.sha-9ae14db
+ * @license Angular v8.0.0-beta.5+13.sha-95d9aa2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15282,7 +15282,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+14.sha-9ae14db');
+const VERSION$1 = new Version('8.0.0-beta.5+13.sha-95d9aa2');
 
 /**
  * @license
