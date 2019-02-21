4aae925ae9401b0ff1df3c947f49c23c478ceed7
Ben Lesh <ben@benlesh.com>
2019-02-21 21:00:00 +0000

    3f2b51b ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+19.sha-d8704a3
+ * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15286,7 +15286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+19.sha-d8704a3');
+const VERSION$1 = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
 
 /**
  * @license
