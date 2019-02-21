3fe1ad2a461c7a869984950c50cd20cb44501610
Igor Minar <igor@angular.io>
2019-02-21 00:50:44 +0000

    623fd3f build: remove legacy-unit-tests-local job (#28703)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+3.sha-05c25cc
+ * @license Angular v8.0.0-beta.5+5.sha-623fd3f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16267,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+3.sha-05c25cc');
+    var VERSION$1 = new Version('8.0.0-beta.5+5.sha-623fd3f');
 
     /**
      * @license
