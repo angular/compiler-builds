e960eba83f48174a009e51bedc03943b04bb8a9f
Igor Minar <igor@angular.io>
2019-02-21 01:01:51 +0000

    be6af26 ci: run saucelabs unit tests as cronjob (#28787)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+31.sha-27eb8f2
+ * @license Angular v7.2.6+4.sha-be6af26
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.5+31.sha-27eb8f2');
+    var VERSION$1 = new Version('7.2.6+4.sha-be6af26');
 
     /**
      * @license
