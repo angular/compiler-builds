a3701f1ea06f7ae4b18eaeb07c4411231d1c16c5
Igor Minar <igor@angular.io>
2019-02-21 01:10:26 +0000

    d0b6622 test(ivy): turn on more passing Material tests (#28876)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+6.sha-13b96ac
+ * @license Angular v8.0.0-beta.5+7.sha-d0b6622
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16267,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+6.sha-13b96ac');
+    var VERSION$1 = new Version('8.0.0-beta.5+7.sha-d0b6622');
 
     /**
      * @license
