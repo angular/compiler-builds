3beb1811c33ec2aaa0688b3b95fdb0e4d6f0f301
Igor Minar <igor@angular.io>
2019-02-21 08:43:12 +0000

    72d043f fix(ivy): check the presence of .css resource for styleUrls (#28770)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+13.sha-95d9aa2
+ * @license Angular v8.0.0-beta.5+11.sha-72d043f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -13149,8 +13149,7 @@
             var parsedElement;
             if (preparsedElement.type === PreparsedElementType.NG_CONTENT) {
                 // `<ng-content>`
-                if (element.children &&
-                    !element.children.every(function (node) { return isEmptyTextNode(node) || isCommentNode(node); })) {
+                if (element.children && !element.children.every(isEmptyTextNode)) {
                     this.reportError("<ng-content> element cannot have content.", element.sourceSpan);
                 }
                 var selector = preparsedElement.selectAttr;
@@ -13346,9 +13345,6 @@
     }
     function isEmptyTextNode(node) {
         return node instanceof Text$3 && node.value.trim().length == 0;
-    }
-    function isCommentNode(node) {
-        return node instanceof Comment;
     }
 
     /**
@@ -16271,7 +16267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+13.sha-95d9aa2');
+    var VERSION$1 = new Version('8.0.0-beta.5+11.sha-72d043f');
 
     /**
      * @license
