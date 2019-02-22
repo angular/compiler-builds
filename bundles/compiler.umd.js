a307adb54a397d0291d5cc3b593143c68523d20e
Ben Lesh <ben@benlesh.com>
2019-02-22 20:19:48 +0000

    9dac04f fix(ivy): ngOnChanges hooks should be inherited from grand-superclasses (#28888)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+42.sha-6ccf743.with-local-changes
+ * @license Angular v8.0.0-beta.5+44.sha-9dac04f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16277,7 +16277,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+42.sha-6ccf743.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
 
     /**
      * @license
