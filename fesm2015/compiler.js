aa69b6729fda510e8c5906025789deb6ab36da7e
Ben Lesh <ben@benlesh.com>
2019-02-22 20:21:30 +0000

    43181ea fix(ivy): host listeners being inherited twice (#28902)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+44.sha-9dac04f.with-local-changes
+ * @license Angular v8.0.0-beta.5+45.sha-43181ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15287,7 +15287,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.5+45.sha-43181ea.with-local-changes');
 
 /**
  * @license
