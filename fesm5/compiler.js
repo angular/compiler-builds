6f5ae63f729843edc116022c599777569309bf06
Igor Minar <igor@angular.io>
2019-02-22 00:48:10 +0000

    21e78ad build: no //package/bazel/src/api-extractor on the patch branch (#28901)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+7.sha-2817764.with-local-changes
+ * @license Angular v7.2.6+9.sha-21e78ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15761,7 +15761,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.6+7.sha-2817764.with-local-changes');
+var VERSION$1 = new Version('7.2.6+9.sha-21e78ad.with-local-changes');
 
 /**
  * @license
