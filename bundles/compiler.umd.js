8408f82d20e220972d24a40359ae899ec91fff70
Ben Lesh <ben@benlesh.com>
2019-02-22 18:06:36 +0000

    40f1f94 refactor: remove unused and duplicate CSS properties from styleguide examples (#28252)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+19.sha-2270467.with-local-changes
+ * @license Angular v7.2.6+20.sha-40f1f94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15818,7 +15818,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.6+19.sha-2270467.with-local-changes');
+    var VERSION$1 = new Version('7.2.6+20.sha-40f1f94.with-local-changes');
 
     /**
      * @license
