31fede7e6179c129571725160b7f70b48cc3b525
Igor Minar <igor@angular.io>
2019-02-22 02:17:24 +0000

    1145bdb fix(bazel): Pin browsers for schematics (#28913)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+22.sha-f4d6525.with-local-changes
+ * @license Angular v8.0.0-beta.5+23.sha-1145bdb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16218,7 +16218,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.5+22.sha-f4d6525.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.5+23.sha-1145bdb.with-local-changes');
 
 /**
  * @license
