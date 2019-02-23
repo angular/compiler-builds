7f0bfc2cec158ebddf5ff0af8b2b5fd6b3aa2121
Igor Minar <igor@angular.io>
2019-02-23 01:49:55 +0000

    350802b build: update to latest bazel rules (#28931)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+61.sha-10ba91b.with-local-changes
+ * @license Angular v8.0.0-beta.5+62.sha-350802b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16220,7 +16220,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.5+61.sha-10ba91b.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.5+62.sha-350802b.with-local-changes');
 
 /**
  * @license
