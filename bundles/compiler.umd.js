76a175e901be9d80e213e34ee297d17c6dfb3396
Ben Lesh <ben@benlesh.com>
2019-02-25 21:23:31 +0000

    7a11242 docs: add Nebular to UI Components section in resources (#28838) PR Close #28838

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+73.sha-65de0d6.with-local-changes
+ * @license Angular v8.0.0-beta.5+74.sha-7a11242.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16277,7 +16277,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.5+73.sha-65de0d6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.5+74.sha-7a11242.with-local-changes');
 
     /**
      * @license
