7a6ba5d05789c3f63db908ea31e5f19c03012a61
Ben Lesh <ben@benlesh.com>
2019-02-25 18:53:11 +0000

    65d3dda docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16220,7 +16220,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.5+71.sha-65d3dda.with-local-changes');
 
 /**
  * @license
