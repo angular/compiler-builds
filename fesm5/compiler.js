261fdf831a6f627aa79c89c7c35502c6b4f21906
Ben Lesh <ben@benlesh.com>
2019-02-25 22:35:28 +0000

    4ef1a3c docs: add Leonardo Zizzamia to GDE resources (#25935)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+36.sha-642c015.with-local-changes
+ * @license Angular v7.2.6+38.sha-4ef1a3c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15765,7 +15765,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.6+36.sha-642c015.with-local-changes');
+var VERSION$1 = new Version('7.2.6+38.sha-4ef1a3c.with-local-changes');
 
 /**
  * @license
