11de075938d35bb87096e8bcfb2654e09c46408c
Ben Lesh <ben@benlesh.com>
2019-02-25 21:12:24 +0000

    0977d95 docs(core): export the `...Decorator` interfaces to ensure they are documented (#28836) (#28961)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.6+34.sha-d374787.with-local-changes
+ * @license Angular v7.2.6+35.sha-0977d95.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15765,7 +15765,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.6+34.sha-d374787.with-local-changes');
+var VERSION$1 = new Version('7.2.6+35.sha-0977d95.with-local-changes');
 
 /**
  * @license
