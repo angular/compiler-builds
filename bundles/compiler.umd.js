9e2d239003d79fca7aacfb9c4769c20ef133b162
Andrew Kushnir <akushnir@google.com>
2019-02-27 22:28:12 +0000

    91a161a refactor(ivy): simplify logic of projectable nodes insertion (#29008)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+16.sha-daf8251.with-local-changes
+ * @license Angular v8.0.0-beta.6+18.sha-91a161a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16292,7 +16292,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+16.sha-daf8251.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+18.sha-91a161a.with-local-changes');
 
     /**
      * @license
