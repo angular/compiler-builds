33691c54397b0b795fad79f905b1ed824d274af3
Ben Lesh <ben@benlesh.com>
2019-02-27 01:21:40 +0000

    41de05e test(ivy): update select root causes (#28989)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+3.sha-dad5a25.with-local-changes
+ * @license Angular v8.0.0-beta.6+4.sha-41de05e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15288,7 +15288,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.6+3.sha-dad5a25.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.6+4.sha-41de05e.with-local-changes');
 
 /**
  * @license
