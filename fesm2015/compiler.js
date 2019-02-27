f6339f93287e1028521dd8a9f2e9ed687c0a925a
Ben Lesh <ben@benlesh.com>
2019-02-27 18:58:11 +0000

    2dd44d7 ci(core): fix `legacy-unit-tests-saucelabs` job (#29009)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+8.sha-b8d8749.with-local-changes
+ * @license Angular v8.0.0-beta.6+10.sha-2dd44d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15288,7 +15288,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.6+8.sha-b8d8749.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.6+10.sha-2dd44d7.with-local-changes');
 
 /**
  * @license
