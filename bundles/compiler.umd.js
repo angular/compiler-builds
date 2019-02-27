8a5e7f7b0b2d3ff08330e332b886d3eaae9d03d4
Ben Lesh <ben@benlesh.com>
2019-02-27 18:49:21 +0000

    f16fca4 build(docs-infra): upgrade cli command docs sources to 3b1fe6437 (#29003)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+8.sha-b8d8749.with-local-changes
+ * @license Angular v8.0.0-beta.6+9.sha-f16fca4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16278,7 +16278,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+8.sha-b8d8749.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+9.sha-f16fca4.with-local-changes');
 
     /**
      * @license
