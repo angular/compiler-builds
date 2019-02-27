02b2a97e8914e8162edc7625251c688940ef7748
Ben Lesh <ben@benlesh.com>
2019-02-27 01:17:24 +0000

    dc33519 fix(bazel): Add SHA256 for rules_sass (#28994)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6.with-local-changes
+ * @license Angular v8.0.0-beta.6+1.sha-dc33519.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16278,7 +16278,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+1.sha-dc33519.with-local-changes');
 
     /**
      * @license
