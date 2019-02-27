4e5cdea948396d818c29a9ef5ff60d84b88830ff
Ben Lesh <ben@benlesh.com>
2019-02-27 01:06:27 +0000

    f380263 release: cut the v8.0.0-beta.6 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+86.sha-5a4c402.with-local-changes
+ * @license Angular v8.0.0-beta.6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16221,7 +16221,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.5+86.sha-5a4c402.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.6.with-local-changes');
 
 /**
  * @license
