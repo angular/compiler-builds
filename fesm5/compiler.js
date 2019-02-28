2c1ae78e3f4d7eeab9ec9359be2faea53baa8625
Andrew Kushnir <akushnir@google.com>
2019-02-28 18:56:41 +0000

    6ee47d5 ci: speed up `publish_artifacts` CircleCI job for PRs (#29028)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+7.sha-9226b42.with-local-changes
+ * @license Angular v7.2.7+8.sha-6ee47d5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15765,7 +15765,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.7+7.sha-9226b42.with-local-changes');
+var VERSION$1 = new Version('7.2.7+8.sha-6ee47d5.with-local-changes');
 
 /**
  * @license
