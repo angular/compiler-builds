880fe1ef661d1cc8a66b38fd127dc75715c6a70f
Andrew Kushnir <akushnir@google.com>
2019-03-01 18:30:37 +0000

    ac76e5d docs: add how to create a minimal repo in bug report template (#29022)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+38.sha-0bc26fc.with-local-changes
+ * @license Angular v8.0.0-beta.6+39.sha-ac76e5d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16320,7 +16320,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+38.sha-0bc26fc.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+39.sha-ac76e5d.with-local-changes');
 
     /**
      * @license
