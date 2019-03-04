51d807d8281da23422ddf6e1a1c030018c454457
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:31:53 +0000

    11c926c docs: add AngularConnect announcement for after ng-conf (#29096)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+17.sha-c938fb4.with-local-changes
+ * @license Angular v7.2.7+19.sha-11c926c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15765,7 +15765,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.7+17.sha-c938fb4.with-local-changes');
+var VERSION$1 = new Version('7.2.7+19.sha-11c926c.with-local-changes');
 
 /**
  * @license
