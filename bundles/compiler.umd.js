eafd13694f59ce9600ca8f059b4b68f97f285384
Andrew Kushnir <akushnir@google.com>
2019-03-04 18:49:21 +0000

    3403027 build: update @angular-devkit/build-angular to version 0.13.4 (#29069) PR Close #29069

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+59.sha-d2f015f.with-local-changes
+ * @license Angular v8.0.0-beta.6+61.sha-3403027.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16275,7 +16275,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+59.sha-d2f015f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+61.sha-3403027.with-local-changes');
 
     /**
      * @license
