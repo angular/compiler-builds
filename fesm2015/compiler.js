68352739b123b15df2c364845897966c851c8f2a
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:00:34 +0000

    5669333 fix(service-worker): detect new version even if files are identical to an old one (#26006)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+26.sha-c976b88.with-local-changes
+ * @license Angular v7.2.7+27.sha-5669333.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14843,7 +14843,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.7+26.sha-c976b88.with-local-changes');
+const VERSION$1 = new Version('7.2.7+27.sha-5669333.with-local-changes');
 
 /**
  * @license
