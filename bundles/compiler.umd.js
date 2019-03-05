25e9c5a2f5e1bb8ae1dc6ff57b947ca3e32f8770
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:11:25 +0000

    36b6110 docs: order upcoming events in chronological order (#29115)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+30.sha-24a1e14.with-local-changes
+ * @license Angular v7.2.7+31.sha-36b6110.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15823,7 +15823,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.7+30.sha-24a1e14.with-local-changes');
+    var VERSION$1 = new Version('7.2.7+31.sha-36b6110.with-local-changes');
 
     /**
      * @license
