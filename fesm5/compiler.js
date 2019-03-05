03273517f0d603dc3392df88e66354e1f4fe82ae
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:05:26 +0000

    72ecc45 fix(router): removed obsolete TODO comment (#29085)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+72.sha-7d17496.with-local-changes
+ * @license Angular v8.0.0-beta.6+73.sha-72ecc45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16228,7 +16228,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.6+72.sha-7d17496.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.6+73.sha-72ecc45.with-local-changes');
 
 /**
  * @license
