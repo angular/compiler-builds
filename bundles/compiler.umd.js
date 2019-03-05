4f9284f24112c82758ab0d282167bdf71ae90ab2
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:13:58 +0000

    4486dab docs: order upcoming events in chronological order (#29115)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+76.sha-01577b0.with-local-changes
+ * @license Angular v8.0.0-beta.6+77.sha-4486dab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16285,7 +16285,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+76.sha-01577b0.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+77.sha-4486dab.with-local-changes');
 
     /**
      * @license
