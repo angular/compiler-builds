cc361c95e7a07b4af0d96cc64fc26cbd96e085f1
Andrew Kushnir <akushnir@google.com>
2019-03-06 18:40:19 +0000

    54286b8 docs: release notes for the v7.2.8 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+90.sha-84406e4.with-local-changes
+ * @license Angular v8.0.0-beta.6+91.sha-54286b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16295,7 +16295,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.6+91.sha-54286b8.with-local-changes');
 
     /**
      * @license
