ecae51c02bdd4af74c6e8677f9e943cef77ef672
Andrew Kushnir <akushnir@google.com>
2019-03-06 00:51:52 +0000

    6945f79 build(docs-infra): ivy prebuild script should not run ngcc for all formats (#29122)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+31.sha-36b6110.with-local-changes
+ * @license Angular v7.2.7+33.sha-6945f79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14843,7 +14843,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.7+31.sha-36b6110.with-local-changes');
+const VERSION$1 = new Version('7.2.7+33.sha-6945f79.with-local-changes');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.7+31.sha-36b6110.with-local-changes
+ * @license Angular v7.2.7+33.sha-6945f79.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.7+31.sha-36b6110.with-local-changes');
+var VERSION$1 = new Version('7.2.7+33.sha-6945f79.with-local-changes');
 
 /**
  * @license
