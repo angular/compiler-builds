4dd4d4ecd8b732646b622c57732216269b8dcb48
Andrew Kushnir <akushnir@google.com>
2019-03-06 05:33:20 +0000

    84406e4 fix(ivy): avoid innerHTML usage in exports test (to make it work in IE11) (#29127)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+89.sha-c29d2a4.with-local-changes
+ * @license Angular v8.0.0-beta.6+90.sha-84406e4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15289,7 +15289,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.6+89.sha-c29d2a4.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.6+90.sha-84406e4.with-local-changes');
 
 /**
  * @license
