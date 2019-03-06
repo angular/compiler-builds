6c9acd7e30c56c646ab039f23b1341242a057793
Andrew Kushnir <akushnir@google.com>
2019-03-06 19:21:07 +0000

    f96efd1 test(ivy): enable passing MatChipList tests (#29130)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+5.sha-dc6192c.with-local-changes
+ * @license Angular v8.0.0-beta.7+8.sha-f96efd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15289,7 +15289,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.7+5.sha-dc6192c.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.7+8.sha-f96efd1.with-local-changes');
 
 /**
  * @license
