90aad86a7223dbecbc20bfa49924c9f34960b756
Kara Erickson <karakara@google.com>
2019-03-07 23:51:02 +0000

    190330a docs: clarify intro and examples (#29012)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+5.sha-e8c8ec7.with-local-changes
+ * @license Angular v7.2.8+8.sha-190330a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.8+5.sha-e8c8ec7.with-local-changes');
+var VERSION$1 = new Version('7.2.8+8.sha-190330a.with-local-changes');
 
 /**
  * @license
