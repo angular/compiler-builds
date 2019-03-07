fffa5b6c06c0090f454d567ccac878fbc1d092e1
Kara Erickson <karakara@google.com>
2019-03-07 19:07:36 +0000

    9a19592 build: remove now redundant `bundle_dts = True` attribute (#29128)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+18.sha-479ae51.with-local-changes
+ * @license Angular v8.0.0-beta.7+22.sha-9a19592.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16295,7 +16295,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.7+18.sha-479ae51.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.7+22.sha-9a19592.with-local-changes');
 
     /**
      * @license
