73aa935bfe96b2b1da76a40fab06c305a981de3e
Kara Erickson <karakara@google.com>
2019-03-08 20:10:26 +0000

    37c5a26 perf(ivy): switch ngtsc to use single-file emit (#29147)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+43.sha-142ac41.with-local-changes
+ * @license Angular v8.0.0-beta.7+44.sha-37c5a26.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16273,7 +16273,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.7+43.sha-142ac41.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.7+44.sha-37c5a26.with-local-changes');
 
 /**
  * @license
