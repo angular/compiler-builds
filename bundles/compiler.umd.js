58d49df8e9ab7e5ab131f5619063c8d77b538fb2
Kara Erickson <karakara@google.com>
2019-03-08 22:36:21 +0000

    c37ec8b fix(ivy): produce ts.Diagnostics for NgModule scope errors (#29191)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+50.sha-fc30530.with-local-changes
+ * @license Angular v8.0.0-beta.7+51.sha-c37ec8b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16330,7 +16330,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.7+50.sha-fc30530.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.7+51.sha-c37ec8b.with-local-changes');
 
     /**
      * @license
