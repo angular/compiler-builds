33ae6598a83c2b515c8fff736e085e6f607fa293
Kara Erickson <karakara@google.com>
2019-03-11 18:06:01 +0000

    6b6fdff fixup! docs: update Universal guide (#28296)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+56.sha-4b9eb61.with-local-changes
+ * @license Angular v8.0.0-beta.7+58.sha-6b6fdff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16273,7 +16273,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.7+56.sha-4b9eb61.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.7+58.sha-6b6fdff.with-local-changes');
 
 /**
  * @license
