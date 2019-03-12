81421e4819016dacede776bf894ede2862993727
Kara Erickson <karakara@google.com>
2019-03-12 18:32:41 +0000

    7be198a docs: change the file name of the public API according with Angular CLI (#29200)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+13.sha-b4ee746.with-local-changes
+ * @license Angular v7.2.8+14.sha-7be198a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.8+13.sha-b4ee746.with-local-changes');
+var VERSION$1 = new Version('7.2.8+14.sha-7be198a.with-local-changes');
 
 /**
  * @license
