82f4db5a6d83b55ed278d976a5f7353abdba33b6
Kara Erickson <karakara@google.com>
2019-03-12 17:52:46 +0000

    defc30c docs: add Sam Julien to GDE group (#29190)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+64.sha-869e1cd.with-local-changes
+ * @license Angular v8.0.0-beta.7+65.sha-defc30c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16330,7 +16330,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.7+64.sha-869e1cd.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.7+65.sha-defc30c.with-local-changes');
 
     /**
      * @license
