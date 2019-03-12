2bc916a1320f686e386a39dcc10c843a434f092c
Kara Erickson <karakara@google.com>
2019-03-12 21:22:57 +0000

    3f32c0e test(ivy): diagnose root causes of failures in CdkTable and MatTable (#29253)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+84.sha-b7c17ff.with-local-changes
+ * @license Angular v8.0.0-beta.7+85.sha-3f32c0e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15338,7 +15338,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.7+84.sha-b7c17ff.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.7+85.sha-3f32c0e.with-local-changes');
 
 /**
  * @license
