873e9d100946aa4abac21439e5d92c300c9ff085
Kara Erickson <karakara@google.com>
2019-03-12 17:54:16 +0000

    76119b8 docs: update enableIvy link (#29238) PR Close #29238

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+65.sha-defc30c.with-local-changes
+ * @license Angular v8.0.0-beta.7+66.sha-76119b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16273,7 +16273,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.7+65.sha-defc30c.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.7+66.sha-76119b8.with-local-changes');
 
 /**
  * @license
