faf60cd156184d9dda5f2b0b12499ea994c499be
Kara Erickson <karakara@google.com>
2019-03-13 17:54:57 +0000

    9a7f560 fix(bazel): fix strict null checks compile error in packages/bazel/src/schematics/ng-add/index.ts (#29282)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+7.sha-955e4e7.with-local-changes
+ * @license Angular v8.0.0-beta.8+8.sha-9a7f560.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16354,7 +16354,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+7.sha-955e4e7.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+8.sha-9a7f560.with-local-changes');
 
     /**
      * @license
