5619cc9bf55091a1d3746b9053d9064e95302ca4
Kara Erickson <karakara@google.com>
2019-03-13 01:21:07 +0000

    1625d86 docs: de-emphasize monorepo (#28377)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+3.sha-29fae6d.with-local-changes
+ * @license Angular v8.0.0-beta.8+5.sha-1625d86.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16349,7 +16349,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+3.sha-29fae6d.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+5.sha-1625d86.with-local-changes');
 
     /**
      * @license
