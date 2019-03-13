9577f16c8953d6283c0d2319b4cf06e28dc6764f
Kara Erickson <karakara@google.com>
2019-03-13 01:21:48 +0000

    940fbf7 fix(ivy): unable to inject class and style attributes (#29192)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+83.sha-c5daaa9.with-local-changes
+ * @license Angular v8.0.0-beta.8+1.sha-940fbf7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15338,7 +15338,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.7+83.sha-c5daaa9.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.8+1.sha-940fbf7.with-local-changes');
 
 /**
  * @license
