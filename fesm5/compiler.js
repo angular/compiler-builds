56e5ca98ab566ec9ad5f909467b00246e1da0b10
Kara Erickson <karakara@google.com>
2019-03-13 17:58:41 +0000

    bb18cba build: user bazelrc should be able to overwrite existing flags (#29279)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.8+20.sha-16c59ae.with-local-changes
+ * @license Angular v7.2.9+1.sha-bb18cba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.8+20.sha-16c59ae.with-local-changes');
+var VERSION$1 = new Version('7.2.9+1.sha-bb18cba.with-local-changes');
 
 /**
  * @license
