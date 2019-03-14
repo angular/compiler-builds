42314a038e384b7175e688593910a0522c60bdf6
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:23:19 +0000

    487d415 build(docs-infra): update AIO dependencies within their current ranges (#29301)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+31.sha-90df7de.with-local-changes
+ * @license Angular v8.0.0-beta.8+32.sha-487d415.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16327,7 +16327,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+31.sha-90df7de.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+32.sha-487d415.with-local-changes');
 
     /**
      * @license
