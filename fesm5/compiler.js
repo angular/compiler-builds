29a74df7ccf6d82aef90c597b60d905163dd22d4
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:39:50 +0000

    da78b02 docs: clarify commands to integrate routing (#26530)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+17.sha-d787925.with-local-changes
+ * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.9+17.sha-d787925.with-local-changes');
+var VERSION$1 = new Version('7.2.9+18.sha-da78b02.with-local-changes');
 
 /**
  * @license
