e58a5c4978bb58acd9df3da018953c9f110c2e03
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 20:22:18 +0000

    6ab8c0b build(bazel): update bazel managed yarn to 1.13.0 (same as CI version) (#29311)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+29.sha-7b70760.with-local-changes
+ * @license Angular v8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16327,7 +16327,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+29.sha-7b70760.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+28.sha-6ab8c0b.with-local-changes');
 
     /**
      * @license
