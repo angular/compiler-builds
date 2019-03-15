f17e93a60b29c65b88286bbf72394e48fcef29cc
Matias Niemelä <matias@yearofmoo.com>
2019-03-15 18:35:29 +0000

    fc8048d build: update to nodejs rules 0.27 (#29210)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+33.sha-7c297e0.with-local-changes
+ * @license Angular v8.0.0-beta.8+42.sha-fc8048d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15316,7 +15316,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.8+33.sha-7c297e0.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.8+42.sha-fc8048d.with-local-changes');
 
 /**
  * @license
