a3ca7b8ffef1c6773def81744b72f4d989556191
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 19:27:15 +0000

    105cfaf fix(compiler-cli): incorrect metadata bundle for multiple unnamed re-exports (#29360)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+58.sha-2ab194c.with-local-changes
+ * @license Angular v8.0.0-beta.8+59.sha-105cfaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16327,7 +16327,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+58.sha-2ab194c.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+59.sha-105cfaf.with-local-changes');
 
     /**
      * @license
