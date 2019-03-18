ee3218c1a0a5d54f66bc5ec53fd20c9fc41d1b66
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 21:54:31 +0000

    d87b035 refactor(ivy): Update query-related comments (#29342)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+61.sha-b759d63.with-local-changes
+ * @license Angular v8.0.0-beta.8+62.sha-d87b035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15316,7 +15316,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.8+61.sha-b759d63.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.8+62.sha-d87b035.with-local-changes');
 
 /**
  * @license
