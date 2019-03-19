27c38102809ce863299634dc5c9b339bfe5a5e71
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:29:21 +0000

    280ea13 docs(core): fix typo in NgModule imports example description (#29272)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+26.sha-8e57965.with-local-changes
+ * @license Angular v7.2.9+28.sha-280ea13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.9+26.sha-8e57965.with-local-changes');
+var VERSION$1 = new Version('7.2.9+28.sha-280ea13.with-local-changes');
 
 /**
  * @license
