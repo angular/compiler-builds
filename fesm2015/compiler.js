c6bca1d7a04967ed5ff4db4594942f1852a384da
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:34:32 +0000

    fd1d161 docs: edit $any() section and add example in Template Syntax (#28157)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+28.sha-280ea13.with-local-changes
+ * @license Angular v7.2.9+29.sha-fd1d161.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14843,7 +14843,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.9+28.sha-280ea13.with-local-changes');
+const VERSION$1 = new Version('7.2.9+29.sha-fd1d161.with-local-changes');
 
 /**
  * @license
