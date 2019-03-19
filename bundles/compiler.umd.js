c8a8d4a8ec0e6ba3d2ae1a2b9c4ac745700d67db
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 05:27:44 +0000

    0244a24 feat(ivy): avoid unnecessary recompilations in TestBed (#29294)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+69.sha-ae4a86e.with-local-changes
+ * @license Angular v8.0.0-beta.8+71.sha-0244a24.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16327,7 +16327,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.8+69.sha-ae4a86e.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.8+71.sha-0244a24.with-local-changes');
 
     /**
      * @license
