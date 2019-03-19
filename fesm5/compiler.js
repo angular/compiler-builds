a50910980d08073712034aebd0e7cafa7dd0fa05
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:25:58 +0000

    4a665ca docs: fix typo in section introduction (#29394) PR Close #29394

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+73.sha-e9fab63.with-local-changes
+ * @license Angular v8.0.0-beta.8+74.sha-4a665ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16270,7 +16270,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.8+73.sha-e9fab63.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.8+74.sha-4a665ca.with-local-changes');
 
 /**
  * @license
