7ec3da2a4b6e96e976c2f2fae482c3258ff0d000
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 17:51:00 +0000

    d6e27a4 test(ivy): remove passing snack bar test from blocklist (#29423)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes
+ * @license Angular v8.0.0-beta.9+15.sha-d6e27a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15338,7 +15338,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.9+14.sha-1a4d4a0.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.9+15.sha-d6e27a4.with-local-changes');
 
 /**
  * @license
