d93393829c0357ec7d2b7c7fe0c77c21c56279ea
Matias Niemelä <matias@yearofmoo.com>
2019-03-20 18:04:01 +0000

    0d04450 ci: add devversion to fw-dev-infra (#29418)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes
+ * @license Angular v8.0.0-beta.9+20.sha-0d04450.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15338,7 +15338,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.9+20.sha-0d04450.with-local-changes');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes
+ * @license Angular v8.0.0-beta.9+20.sha-0d04450.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16292,7 +16292,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.9+19.sha-ddfdf3c.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.9+20.sha-0d04450.with-local-changes');
 
 /**
  * @license
