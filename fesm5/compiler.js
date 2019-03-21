ce6a4d3da0205699b31b548998a065847553695a
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 07:01:40 +0000

    41737bb docs: fix html tags in changelog (#29411)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+47.sha-afe3e72.with-local-changes
+ * @license Angular v8.0.0-beta.9+48.sha-41737bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16294,7 +16294,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.9+47.sha-afe3e72.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.9+48.sha-41737bb.with-local-changes');
 
 /**
  * @license
