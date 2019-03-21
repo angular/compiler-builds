eff62c74f281b640fd76cdb49bddc1186659e36c
Miško Hevery <misko@hevery.com>
2019-03-21 22:55:29 +0000

    dfcf759 docs: drop Coding Conventions section from style guide (#29331)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+61.sha-6f3052b.with-local-changes
+ * @license Angular v8.0.0-beta.9+68.sha-dfcf759.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15340,7 +15340,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.9+61.sha-6f3052b.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.9+68.sha-dfcf759.with-local-changes');
 
 /**
  * @license
