a8bd832e8a87fa6c032c39ee3f06aead2c79e1b4
Miško Hevery <misko@hevery.com>
2019-03-21 22:36:06 +0000

    6f3052b ci: disable sauce-connect ssl bumping (#29447)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+60.sha-c18fa7b.with-local-changes
+ * @license Angular v8.0.0-beta.9+61.sha-6f3052b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16294,7 +16294,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.9+60.sha-c18fa7b.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.9+61.sha-6f3052b.with-local-changes');
 
 /**
  * @license
