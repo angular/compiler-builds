108d20218b8edbf5c83d79f9b72ea61a56de4725
Matias Niemelä <matias@yearofmoo.com>
2019-03-21 19:48:24 +0000

    c18fa7b build(docs-infra): upgrade cli command docs sources to 18d979cdc (#29437)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+59.sha-693b350.with-local-changes
+ * @license Angular v8.0.0-beta.9+60.sha-c18fa7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15340,7 +15340,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.9+59.sha-693b350.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.9+60.sha-c18fa7b.with-local-changes');
 
 /**
  * @license
