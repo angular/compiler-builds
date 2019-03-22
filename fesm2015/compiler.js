d3fe5f012a92862afcbfec0304d36cea8d9e461c
Miško Hevery <misko@hevery.com>
2019-03-22 20:29:12 +0000

    66b72bf fix(ivy): ViewContainerRef.destroy should properly clean the DOM (#29414)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+73.sha-f3e0cc8.with-local-changes
+ * @license Angular v8.0.0-beta.9+77.sha-66b72bf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15340,7 +15340,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.9+73.sha-f3e0cc8.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.9+77.sha-66b72bf.with-local-changes');
 
 /**
  * @license
