fb7cc0a34ffe24b0ebdde357271aa28a904fc73c
Misko Hevery <misko@hevery.com>
2019-03-25 18:02:18 +0000

    c34c223 fix(ivy): avoid type coercion in binding-related instruction asserts (#29470)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+83.sha-bef5043.with-local-changes
+ * @license Angular v8.0.0-beta.9+90.sha-c34c223.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16294,7 +16294,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.9+83.sha-bef5043.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.9+90.sha-c34c223.with-local-changes');
 
 /**
  * @license
