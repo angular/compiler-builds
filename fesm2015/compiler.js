343ae658acdce159e3ab28d7368005a7cf9bf0af
Miško Hevery <misko@hevery.com>
2019-03-27 17:19:13 +0000

    6c33058 docs: add library structure (#29376)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+4.sha-06056b4.with-local-changes
+ * @license Angular v8.0.0-beta.10+11.sha-6c33058.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15340,7 +15340,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.10+4.sha-06056b4.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.10+11.sha-6c33058.with-local-changes');
 
 /**
  * @license
