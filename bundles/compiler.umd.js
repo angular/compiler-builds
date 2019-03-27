baed94cf64c50e714a4a4b7e18445e32675938a8
Miško Hevery <misko@hevery.com>
2019-03-27 17:23:54 +0000

    29d2986 docs: replaced hero with heroes component property (#29487) PR Close #29487

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+4.sha-4a73dfc.with-local-changes
+ * @license Angular v7.2.11+5.sha-29d2986.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15823,7 +15823,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.11+4.sha-4a73dfc.with-local-changes');
+    var VERSION$1 = new Version('7.2.11+5.sha-29d2986.with-local-changes');
 
     /**
      * @license
