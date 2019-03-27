71cf2d55edf20221244ed9d22b9242f905f7b1d7
Miško Hevery <misko@hevery.com>
2019-03-27 17:01:47 +0000

    a2f8f55 refactor(ivy): rename flushHooksUpTo to select (#29527)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+1.sha-a28b3e3.with-local-changes
+ * @license Angular v8.0.0-beta.10+2.sha-a2f8f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3425,7 +3425,7 @@
         Identifiers.elementStart = { name: 'ɵelementStart', moduleName: CORE$1 };
         Identifiers.elementEnd = { name: 'ɵelementEnd', moduleName: CORE$1 };
         Identifiers.elementProperty = { name: 'ɵelementProperty', moduleName: CORE$1 };
-        Identifiers.flushHooksUpTo = { name: 'ɵflushHooksUpTo', moduleName: CORE$1 };
+        Identifiers.select = { name: 'ɵselect', moduleName: CORE$1 };
         Identifiers.componentHostSyntheticProperty = { name: 'ɵcomponentHostSyntheticProperty', moduleName: CORE$1 };
         Identifiers.componentHostSyntheticListener = { name: 'ɵcomponentHostSyntheticListener', moduleName: CORE$1 };
         Identifiers.elementAttribute = { name: 'ɵelementAttribute', moduleName: CORE$1 };
@@ -14066,8 +14066,8 @@
              */
             this._updateCodeFns = [];
             /**
-             * Memorizes the last node index for which a flushHooksUpTo instruction has been generated.
-             * Initialized to 0 to avoid generating a useless flushHooksUpTo(0).
+             * Memorizes the last node index for which a select instruction has been generated.
+             * Initialized to 0 to avoid generating a useless select(0).
              */
             this._lastNodeIndexWithFlush = 0;
             /** Temporary variable declarations generated from visiting pipes, literals, etc. */
@@ -14804,7 +14804,7 @@
         };
         TemplateDefinitionBuilder.prototype.updateInstruction = function (nodeIndex, span, reference, paramsOrFn) {
             if (this._lastNodeIndexWithFlush < nodeIndex) {
-                this.instructionFn(this._updateCodeFns, span, Identifiers$1.flushHooksUpTo, [literal(nodeIndex)]);
+                this.instructionFn(this._updateCodeFns, span, Identifiers$1.select, [literal(nodeIndex)]);
                 this._lastNodeIndexWithFlush = nodeIndex;
             }
             this.instructionFn(this._updateCodeFns, span, reference, paramsOrFn || []);
@@ -16351,7 +16351,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+1.sha-a28b3e3.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+2.sha-a2f8f55.with-local-changes');
 
     /**
      * @license
