b8cf863dd5ba42a5be78e827637033e2e5a6638d
Jason Aden <jasonaden@google.com>
2019-03-28 22:38:06 +0000

    78ba503 fix(ivy): ngcc - write `.d.ts.map` files to the correct folder (#29556)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+38.sha-632669f.with-local-changes
+ * @license Angular v8.0.0-beta.10+41.sha-78ba503.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16352,7 +16352,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+38.sha-632669f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+41.sha-78ba503.with-local-changes');
 
     /**
      * @license
