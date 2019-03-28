4d9a6d0998243785082403eb2c9a54941a12ac16
Jason Aden <jasonaden@google.com>
2019-03-28 22:51:14 +0000

    6c76dfc docs: copy-edit ivy opt-in page (#29507)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+41.sha-78ba503.with-local-changes
+ * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15341,7 +15341,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.10+41.sha-78ba503.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
 
 /**
  * @license
