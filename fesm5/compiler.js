12e104365bb994151a8eb122c62df7a795a8c8ad
Miško Hevery <misko@hevery.com>
2019-03-28 17:59:24 +0000

    71ec998 fix(ivy): allow TestBed to recompile AOT-compiled components in case of template overrides (#29555)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+32.sha-931d356.with-local-changes
+ * @license Angular v8.0.0-beta.10+34.sha-71ec998.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16295,7 +16295,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.10+32.sha-931d356.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.10+34.sha-71ec998.with-local-changes');
 
 /**
  * @license
