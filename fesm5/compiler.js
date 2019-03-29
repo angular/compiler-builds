dc3087fc113471414332431ef617a6f0276fffc4
Jason Aden <jasonaden@google.com>
2019-03-29 18:06:50 +0000

    c67f6a7 docs: fix typo in renderer description (#29581)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+55.sha-12c9bd2.with-local-changes
+ * @license Angular v8.0.0-beta.10+57.sha-c67f6a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16295,7 +16295,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.10+55.sha-12c9bd2.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.10+57.sha-c67f6a7.with-local-changes');
 
 /**
  * @license
