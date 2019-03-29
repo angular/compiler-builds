ea9349aa97e7eda283638752fe1334b0fd5a234c
Jason Aden <jasonaden@google.com>
2019-03-29 17:55:58 +0000

    22b89ea docs: move text that mention what is expected to fail up higher into the section (#27329)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+47.sha-eb0e29b.with-local-changes
+ * @license Angular v8.0.0-beta.10+51.sha-22b89ea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15341,7 +15341,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.10+47.sha-eb0e29b.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.10+51.sha-22b89ea.with-local-changes');
 
 /**
  * @license
