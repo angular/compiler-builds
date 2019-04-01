625d660ac15603b3efff9e0835745fb6d976c659
Jason Aden <jasonaden@google.com>
2019-04-01 19:09:23 +0000

    39345b6 style(compiler-cli): ensure FFR type is implemented correctly (#29539)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+79.sha-717aa7c.with-local-changes
+ * @license Angular v8.0.0-beta.10+81.sha-39345b6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16352,7 +16352,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+79.sha-717aa7c.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+81.sha-39345b6.with-local-changes');
 
     /**
      * @license
