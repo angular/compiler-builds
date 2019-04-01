83c34079c78b95999b3c286c9fccaa2bf428bf1f
Jason Aden <jasonaden@google.com>
2019-04-01 18:24:20 +0000

    a52123f docs: add missing picture of Jason Bedard (#29596)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+25.sha-60b5bf7.with-local-changes
+ * @license Angular v7.2.11+37.sha-a52123f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.11+25.sha-60b5bf7.with-local-changes');
+var VERSION$1 = new Version('7.2.11+37.sha-a52123f.with-local-changes');
 
 /**
  * @license
