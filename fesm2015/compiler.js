23b59ca476e23b190628e1597a8b96d58b354b23
Jason Aden <jasonaden@google.com>
2019-04-01 20:13:58 +0000

    f9497bf fix(router): adjust setting navigationTransition when a new navigation cancels an existing one (#29636)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+57.sha-c67f6a7.with-local-changes
+ * @license Angular v8.0.0-beta.10+84.sha-f9497bf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15341,7 +15341,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.10+57.sha-c67f6a7.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.10+84.sha-f9497bf.with-local-changes');
 
 /**
  * @license
