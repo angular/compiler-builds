7918f8a97c3234b0a3d98b7291b81af2a88d94d5
Jason Aden <jasonaden@google.com>
2019-04-02 18:03:45 +0000

    91f2b1e refactor(platform-browser): rename _singleTagWhitelist (#29592)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.11+57.sha-6bf8e37.with-local-changes
+ * @license Angular v7.2.11+56.sha-91f2b1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.11+57.sha-6bf8e37.with-local-changes');
+var VERSION$1 = new Version('7.2.11+56.sha-91f2b1e.with-local-changes');
 
 /**
  * @license
