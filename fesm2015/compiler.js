282d9f715bb8b9362175e344aa948d63602c0f6b
Jason Aden <jasonaden@google.com>
2019-04-02 17:55:52 +0000

    1c07061 refactor: change error message (#29594)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+99.sha-7c1f73a.with-local-changes
+ * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -11601,9 +11601,9 @@ class DomElementSchemaRegistry extends ElementSchemaRegistry {
      * Tag and property name are statically known and cannot change at runtime, i.e. it is not
      * possible to bind a value into a changing attribute or tag name.
      *
-     * The filtering is white list based. All attributes in the schema above are assumed to have the
-     * 'NONE' security context, i.e. that they are safe inert string values. Only specific well known
-     * attack vectors are assigned their appropriate context.
+     * The filtering is based on a list of allowed tags|attributes. All attributes in the schema
+     * above are assumed to have the 'NONE' security context, i.e. that they are safe inert
+     * string values. Only specific well known attack vectors are assigned their appropriate context.
      */
     securityContext(tagName, propName, isAttribute) {
         if (isAttribute) {
@@ -15341,7 +15341,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.10+99.sha-7c1f73a.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
 
 /**
  * @license
