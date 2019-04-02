4161e249b4971bfb44af65fd4f83e1526a42590b
Jason Aden <jasonaden@google.com>
2019-04-02 23:06:14 +0000

    7c8f4e3 feat(core): template-var-assignment update schematic (#29608)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+109.sha-03d914a.with-local-changes
+ * @license Angular v8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16352,7 +16352,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.10+109.sha-03d914a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.10+113.sha-7c8f4e3.with-local-changes');
 
     /**
      * @license
