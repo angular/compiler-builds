7fb43eba0b8a7c39a48919ffcec0a8f765ee6801
Jason Aden <jasonaden@google.com>
2019-04-02 23:17:23 +0000

    6b39c9c fix(compiler-cli): ngcc - cope with processing entry-points multiple times (#29657)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+116.sha-c579949.with-local-changes
+ * @license Angular v8.0.0-beta.10+117.sha-6b39c9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16295,7 +16295,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.10+116.sha-c579949.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.10+117.sha-6b39c9c.with-local-changes');
 
 /**
  * @license
