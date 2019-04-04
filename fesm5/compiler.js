30ed84bd048f4d9a75fb4f68d6ed7c86fb4c8e83
Igor Minar <igor@angular.io>
2019-04-04 21:44:02 +0000

    0c47dc0 docs: add universal terms to glossary (#28492)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+17.sha-2bd9214.with-local-changes
+ * @license Angular v8.0.0-beta.11+18.sha-0c47dc0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16342,7 +16342,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.11+18.sha-0c47dc0.with-local-changes');
 
 /**
  * @license
