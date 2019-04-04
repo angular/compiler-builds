c26517c9b62546c1f0e49766a583538f4c4fa844
Igor Minar <igor@angular.io>
2019-04-04 21:44:23 +0000

    2bd9214 fix(bazel): remove karma-jasmine from ts_web_test_suite (#29695)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+13.sha-c3aea73.with-local-changes
+ * @license Angular v8.0.0-beta.11+17.sha-2bd9214.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15388,7 +15388,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.11+13.sha-c3aea73.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.11+17.sha-2bd9214.with-local-changes');
 
 /**
  * @license
