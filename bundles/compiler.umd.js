580db17ff7a6f946c8769e770e0e7d2ed82c5f65
Jason Aden <jasonaden@google.com>
2019-04-04 18:04:29 +0000

    9e33dc3 fix(bazel): use //:tsconfig.json as the default for ng_module (#29670) (#29711)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.12+4.sha-0930820.with-local-changes
+ * @license Angular v7.2.12+5.sha-9e33dc3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15823,7 +15823,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.12+4.sha-0930820.with-local-changes');
+    var VERSION$1 = new Version('7.2.12+5.sha-9e33dc3.with-local-changes');
 
     /**
      * @license
