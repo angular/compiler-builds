124c3257683f824274ae1883a56dd453d93ad4c1
Igor Minar <igor@angular.io>
2019-04-08 20:11:00 +0000

    84be7c5 refactor(ivy): rename `styling_instructions` to `styling` (#29770)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+37.sha-38a3a5a.with-local-changes
+ * @license Angular v8.0.0-beta.11+45.sha-84be7c5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16399,7 +16399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+37.sha-38a3a5a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+45.sha-84be7c5.with-local-changes');
 
     /**
      * @license
