fccc062f3798978f9eef9e3efd29d525bbcc854a
Igor Minar <igor@angular.io>
2019-04-10 19:43:52 +0000

    3e46985 docs: showing hamburger menu for getting started pages on desktop (#29791)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+62.sha-8c37cdc.with-local-changes
+ * @license Angular v8.0.0-beta.11+63.sha-3e46985.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15388,7 +15388,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.11+62.sha-8c37cdc.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.11+63.sha-3e46985.with-local-changes');
 
 /**
  * @license
