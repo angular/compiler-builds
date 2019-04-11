a8366efb3f72a16151a3d6cdb039d2bf5717f145
Igor Minar <igor@angular.io>
2019-04-11 15:41:26 +0000

    5b32f55 refactor(core): static-query schematic should check templates (#29713)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes
+ * @license Angular v8.0.0-beta.11+81.sha-5b32f55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16416,7 +16416,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.11+81.sha-5b32f55.with-local-changes');
 
     /**
      * @license
