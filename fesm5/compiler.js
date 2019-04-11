f71049ff876f61a137bce506dc7570f452da4c10
Igor Minar <igor@angular.io>
2019-04-11 18:18:48 +0000

    54058ba fix(core): resolve ts compile issues due to lenient tsconfig (#29843)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+83.sha-91c7b45.with-local-changes
+ * @license Angular v8.0.0-beta.11+84.sha-54058ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16368,7 +16368,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.11+83.sha-91c7b45.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.11+84.sha-54058ba.with-local-changes');
 
 /**
  * @license
