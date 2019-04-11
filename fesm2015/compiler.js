49f2bfb5f4bb475d0ac189c97d3ddecb82d51c28
Igor Minar <igor@angular.io>
2019-04-11 15:38:25 +0000

    cc2e4b6 fix(bazel): docs formatting (#29817)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+78.sha-6227d0b.with-local-changes
+ * @license Angular v8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15405,7 +15405,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.11+78.sha-6227d0b.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.11+79.sha-cc2e4b6.with-local-changes');
 
 /**
  * @license
