afec4e81443e3087a1a5fbd0299c77968eb7760f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-13 16:49:05 +0000

    ea70d41 test: ensure correct TS version before running postinstall (#29885)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+92.sha-911e0b8.with-local-changes
+ * @license Angular v8.0.0-beta.12+1.sha-ea70d41.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15419,7 +15419,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.11+92.sha-911e0b8.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.12+1.sha-ea70d41.with-local-changes');
 
 /**
  * @license
