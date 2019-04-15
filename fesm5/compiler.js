0f3b9a6d0ca2bfbf482ad0280f59cf03d351227f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-15 16:28:32 +0000

    d1d0f4a fix(docs-infra): add missing ng-conf image (#29902)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+1.sha-ea70d41.with-local-changes
+ * @license Angular v8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16373,7 +16373,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.12+1.sha-ea70d41.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.12+4.sha-d1d0f4a.with-local-changes');
 
 /**
  * @license
