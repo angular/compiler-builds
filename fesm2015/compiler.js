16777cf229f1f761a1c22b4a0fb4644564acddb1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 23:03:35 +0000

    ab60362 release: cut the v8.0.0-beta.13 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+25.sha-86a3f90.with-local-changes
+ * @license Angular v8.0.0-beta.13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15419,7 +15419,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.12+25.sha-86a3f90.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.13.with-local-changes');
 
 /**
  * @license
