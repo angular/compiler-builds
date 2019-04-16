7c6d6a38c06689773c7b9125929f316192274b27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 17:04:42 +0000

    5d824c4 fix(ivy): DebugNode.properties not preserving type of values (#29914)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+18.sha-2deac0a.with-local-changes
+ * @license Angular v8.0.0-beta.12+20.sha-5d824c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16430,7 +16430,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.12+18.sha-2deac0a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.12+20.sha-5d824c4.with-local-changes');
 
     /**
      * @license
