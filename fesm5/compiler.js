201f4090ae59243ee18b73333d492b510e93e142
Ben Lesh <ben@benlesh.com>
2019-04-17 19:30:57 +0000

    a48d288 build(language-service): no need to bootstrap init_node_spec.js (#29937)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+2.sha-d9c39dc.with-local-changes
+ * @license Angular v8.0.0-beta.13+3.sha-a48d288.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16373,7 +16373,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.13+2.sha-d9c39dc.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.13+3.sha-a48d288.with-local-changes');
 
 /**
  * @license
