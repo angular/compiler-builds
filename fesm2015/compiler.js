e8324e8c632b2e88912b77f48eb96ac75a7928dd
Ben Lesh <ben@benlesh.com>
2019-04-18 00:42:32 +0000

    78146c1 docs: update extra options available for RouterModule.forRoot() method (#29846)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+20.sha-e1f51ea.with-local-changes
+ * @license Angular v8.0.0-beta.13+21.sha-78146c1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15419,7 +15419,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.13+20.sha-e1f51ea.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.13+21.sha-78146c1.with-local-changes');
 
 /**
  * @license
