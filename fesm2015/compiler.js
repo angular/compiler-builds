1055767994fb443aab2c3075f33ca0132f3f26c8
Ben Lesh <ben@benlesh.com>
2019-04-19 21:48:39 +0000

    a181e8e refactor: improve merge script formatting (#29991)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+54.sha-6ae0084.with-local-changes
+ * @license Angular v8.0.0-beta.13+55.sha-a181e8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15422,7 +15422,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.13+54.sha-6ae0084.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.13+55.sha-a181e8e.with-local-changes');
 
 /**
  * @license
