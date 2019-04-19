40c134006e933a28197461f43e382b6ba25f2e2b
Ben Lesh <ben@benlesh.com>
2019-04-19 17:15:28 +0000

    5650e38 build(docs-infra): make tsconfig path detection in `switch-to-ivy` more robust (#29989)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+29.sha-f9bb53a.with-local-changes
+ * @license Angular v8.0.0-beta.13+33.sha-5650e38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16430,7 +16430,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+29.sha-f9bb53a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+33.sha-5650e38.with-local-changes');
 
     /**
      * @license
