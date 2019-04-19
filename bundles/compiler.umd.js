215a10fefb265cd8882e7f5d7d7821f43df711a6
Ben Lesh <ben@benlesh.com>
2019-04-19 21:29:09 +0000

    a593b3f ci(docs-infra): do not build with Ivy on 7.2.x (#29993)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+18.sha-2bb4263.with-local-changes
+ * @license Angular v7.2.13+19.sha-a593b3f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15823,7 +15823,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('7.2.13+18.sha-2bb4263.with-local-changes');
+    var VERSION$1 = new Version('7.2.13+19.sha-a593b3f.with-local-changes');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+18.sha-2bb4263.with-local-changes
+ * @license Angular v7.2.13+19.sha-a593b3f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14843,7 +14843,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('7.2.13+18.sha-2bb4263.with-local-changes');
+const VERSION$1 = new Version('7.2.13+19.sha-a593b3f.with-local-changes');
 
 /**
  * @license
