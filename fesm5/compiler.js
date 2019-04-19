50a8c06f42d5a77e942f1dee516c743a4aae1281
Ben Lesh <ben@benlesh.com>
2019-04-19 18:14:22 +0000

    d7f7826 build: publish ts-api-guardian rule in the npm package (#29977)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+35.sha-9b93bd6.with-local-changes
+ * @license Angular v8.0.0-beta.13+36.sha-d7f7826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16373,7 +16373,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.13+35.sha-9b93bd6.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.13+36.sha-d7f7826.with-local-changes');
 
 /**
  * @license
