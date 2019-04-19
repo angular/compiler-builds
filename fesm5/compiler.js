604d71963099a7d774f2fdbeed8f78ce43c2ff10
Ben Lesh <ben@benlesh.com>
2019-04-19 17:15:35 +0000

    0c4430b build(docs-infra): make tsconfig path detection in `switch-to-ivy` more robust (#29989)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.13+12.sha-4994527.with-local-changes
+ * @license Angular v7.2.13+17.sha-0c4430b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.13+12.sha-4994527.with-local-changes');
+var VERSION$1 = new Version('7.2.13+17.sha-0c4430b.with-local-changes');
 
 /**
  * @license
