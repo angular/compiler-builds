bf7b2c5cb8ddac66bc703e4672d28e5df9d7f3dd
Ben Lesh <ben@benlesh.com>
2019-04-22 23:46:31 +0000

    57c8f78 docs: add Differential Loading section to deployment guide (#29932)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+75.sha-54289ae.with-local-changes
+ * @license Angular v8.0.0-beta.13+74.sha-57c8f78.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18002,7 +18002,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.13+75.sha-54289ae.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.13+74.sha-57c8f78.with-local-changes');
 
 /**
  * @license
