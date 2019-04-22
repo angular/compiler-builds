19cad4171e784f1c5ed580c445c67bd76f228414
Ben Lesh <ben@benlesh.com>
2019-04-22 23:53:21 +0000

    0cab437 docs: fixed typo on HttpParamsOptions (#29930) PR Close #29930

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+74.sha-57c8f78.with-local-changes
+ * @license Angular v8.0.0-beta.13+76.sha-0cab437.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18058,7 +18058,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.13+74.sha-57c8f78.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.13+76.sha-0cab437.with-local-changes');
 
     /**
      * @license
