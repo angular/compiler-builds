5cd2248f199b34f5a3b59146a047b0f81768720a
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:23:34 +0000

    24c61cb docs: add redirect link to deprecation summary page (#29951)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes
+ * @license Angular v8.0.0-beta.14+15.sha-24c61cb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18131,7 +18131,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+15.sha-24c61cb.with-local-changes');
 
     /**
      * @license
