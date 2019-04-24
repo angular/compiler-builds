6fefc3113c35286a3d178370c18c464d337434e9
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:07:43 +0000

    0fa7621 refactor(ivy): ngcc - simplify `NewEntryPointFileWriter` code (#30085)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+10.sha-0b5f480.with-local-changes
+ * @license Angular v8.0.0-beta.14+12.sha-0fa7621.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18131,7 +18131,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+10.sha-0b5f480.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+12.sha-0fa7621.with-local-changes');
 
     /**
      * @license
