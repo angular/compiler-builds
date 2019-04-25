97edb7d230364cbf01757023a8b3f0cb3ea0ed74
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:21:59 +0000

    c61df39 feat(router): deprecate loadChildren:string (#30073)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+38.sha-abcb2cf.with-local-changes
+ * @license Angular v8.0.0-beta.14+39.sha-c61df39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18131,7 +18131,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+38.sha-abcb2cf.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+39.sha-c61df39.with-local-changes');
 
     /**
      * @license
