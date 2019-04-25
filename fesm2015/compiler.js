0a7c52f1afcdca8c1fbcd21991e1381bd74195ed
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:20:41 +0000

    abcb2cf refactor(upgrade): rename `module` constant to avoid webpack bug (#30058)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+37.sha-a9379e0.with-local-changes
+ * @license Angular v8.0.0-beta.14+38.sha-abcb2cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17038,7 +17038,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-beta.14+37.sha-a9379e0.with-local-changes');
+const VERSION$1 = new Version('8.0.0-beta.14+38.sha-abcb2cf.with-local-changes');
 
 /**
  * @license
