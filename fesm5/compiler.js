41f88bfa12de3740283768f52db7bd89f9a000da
Andrew Kushnir <akushnir@google.com>
2019-04-25 21:56:13 +0000

    3a9d247 feat: remove deprecated DOCUMENT token from platform-browser (#28117)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+71.sha-6200732.with-local-changes
+ * @license Angular v8.0.0-beta.14+72.sha-3a9d247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18084,7 +18084,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-beta.14+71.sha-6200732.with-local-changes');
+var VERSION$1 = new Version('8.0.0-beta.14+72.sha-3a9d247.with-local-changes');
 
 /**
  * @license
