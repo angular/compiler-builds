d4ae9c5eb2c03584a2dc5f1e5c35f3418558cb84
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:26:21 +0000

    d316a18 fix(ivy): don't include query fields in type constructors (#30094)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+40.sha-28fd5ab.with-local-changes
+ * @license Angular v8.0.0-beta.14+42.sha-d316a18.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18131,7 +18131,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-beta.14+40.sha-28fd5ab.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-beta.14+42.sha-d316a18.with-local-changes');
 
     /**
      * @license
