946f04932ab7f9089ccaa0e45156938e73126f80
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:39:09 +0000

    6c86ae7 fix(ivy): output should not be subscribe twice when 2 listeners (#30144)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes
+ * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+11.sha-6c86ae7.with-local-changes');
 
 /**
  * @license
