2f4f83e72866d10713ce538297aecc8f22bd80d4
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:33:26 +0000

    8dd9192 refactor(ivy): undeprecate inject (#30132)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+7.sha-6711f22.with-local-changes
+ * @license Angular v8.0.0-rc.0+6.sha-8dd9192.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18140,7 +18140,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+7.sha-6711f22.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+6.sha-8dd9192.with-local-changes');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+7.sha-6711f22.with-local-changes
+ * @license Angular v8.0.0-rc.0+6.sha-8dd9192.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18084,7 +18084,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+7.sha-6711f22.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+6.sha-8dd9192.with-local-changes');
 
 /**
  * @license
