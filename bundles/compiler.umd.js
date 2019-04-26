798db8ef894b095a34a2575db2fd4d1031c2e036
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:33:45 +0000

    876ceb3 docs: remove note about ivy being coupled to dynamic import (#30151)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+6.sha-b9f0720.with-local-changes
+ * @license Angular v8.0.0-rc.0+9.sha-876ceb3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18140,7 +18140,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+6.sha-b9f0720.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
 
     /**
      * @license
