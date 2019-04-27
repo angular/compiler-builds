f3ffcb4dce9f74cb14bbd04e25dee7f3a27d2887
Alex Eagle <alexeagle@angular.io>
2019-04-27 00:10:00 +0000

    e5c3695 Revert "refactor: use new Http library in playground (#29355)"

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+18.sha-c99d379.with-local-changes
+ * @license Angular v8.0.0-rc.0+38.sha-e5c3695.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17105,7 +17105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+18.sha-c99d379.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+38.sha-e5c3695.with-local-changes');
 
 /**
  * @license
