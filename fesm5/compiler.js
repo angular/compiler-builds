299c43d8c9e10caa5638747fcfda093ba97eb64b
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:53:29 +0000

    164d160 refactor(core): static-query migrations fails if options cannot be transformed (#30178)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+63.sha-6dc884f.with-local-changes
+ * @license Angular v8.0.0-rc.0+65.sha-164d160.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+63.sha-6dc884f.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+65.sha-164d160.with-local-changes');
 
 /**
  * @license
