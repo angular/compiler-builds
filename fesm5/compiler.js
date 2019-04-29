b6489ae312f1715b6fc761446c9438526e02d188
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:40:25 +0000

    f015dbe docs: add architect terms (#26963)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+57.sha-029a939.with-local-changes
+ * @license Angular v8.0.0-rc.0+58.sha-f015dbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+57.sha-029a939.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+58.sha-f015dbe.with-local-changes');
 
 /**
  * @license
