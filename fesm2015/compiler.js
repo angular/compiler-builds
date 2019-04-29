ad0e1d948825f18ebc3474033cec2f5feb47bf36
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:52:59 +0000

    029a939 refactor(ivy): ngcc - remove the last remnants of `path` and `canonical-path` (#29643)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+44.sha-1195dab.with-local-changes
+ * @license Angular v8.0.0-rc.0+57.sha-029a939.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17105,7 +17105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+44.sha-1195dab.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+57.sha-029a939.with-local-changes');
 
 /**
  * @license
