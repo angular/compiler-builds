203d55f4048e27651dec925dfe3a9b23898b496e
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:48:56 +0000

    1195dab build(docs-infra): update the aio app to framework and cli 8.0.0-rc.1 (#30183)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+43.sha-2945f47.with-local-changes
+ * @license Angular v8.0.0-rc.0+44.sha-1195dab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+43.sha-2945f47.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+44.sha-1195dab.with-local-changes');
 
 /**
  * @license
