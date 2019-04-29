d7d01fca108354aa9fcda767c2a3e948b9765e09
Andrew Kushnir <akushnir@google.com>
2019-04-29 16:57:40 +0000

    4b2fcfd fix: disable injectable-pipe migration (#30180)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.1+27.sha-b4d291a.with-local-changes
+ * @license Angular v8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18199,7 +18199,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.1+27.sha-b4d291a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.1+27.sha-b4d291a.with-local-changes
+ * @license Angular v8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.1+27.sha-b4d291a.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.1+28.sha-4b2fcfd.with-local-changes');
 
 /**
  * @license
