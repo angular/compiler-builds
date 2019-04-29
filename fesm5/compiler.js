537eece294902bdaa9ca05f32b00a4365da953e2
Andrew Kushnir <akushnir@google.com>
2019-04-29 19:44:50 +0000

    71c5d80 build: add size-tracking bazel test (#30070)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2.with-local-changes
+ * @license Angular v8.0.0-rc.2+1.sha-71c5d80.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18143,7 +18143,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.2.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.2+1.sha-71c5d80.with-local-changes');
 
 /**
  * @license
