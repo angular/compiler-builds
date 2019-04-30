0d273be7195d65d79787e30c5305dcb6d17f109d
Kara Erickson <karakara@google.com>
2019-04-30 03:17:33 +0000

    00ffc03 fix(ivy): support forward refs in provider deps (#30201)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+68.sha-e53cf81.with-local-changes
+ * @license Angular v8.0.0-rc.0+69.sha-00ffc03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18147,7 +18147,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+68.sha-e53cf81.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+69.sha-00ffc03.with-local-changes');
 
 /**
  * @license
