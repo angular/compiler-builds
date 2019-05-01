b3fdd3a0fadaa5daa9b49236cac7ec0f0f92e70f
Kara Erickson <karakara@google.com>
2019-05-01 23:57:29 +0000

    b70d20b fix(docs-infra): update app code to work with Ivy (#28530)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+77.sha-5b80ab3.with-local-changes
+ * @license Angular v8.0.0-rc.0+79.sha-b70d20b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18203,7 +18203,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+77.sha-5b80ab3.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+79.sha-b70d20b.with-local-changes');
 
     /**
      * @license
