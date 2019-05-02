310cc204fc9223d45ef80347410df69974b44316
Kara Erickson <karakara@google.com>
2019-05-02 17:52:19 +0000

    3fe3a84 fix(core): fix interpolate identifier in AOT (#30243)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+13.sha-28e4187.with-local-changes
+ * @license Angular v8.0.0-rc.2+14.sha-3fe3a84.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2913,7 +2913,7 @@
             name: 'ɵinlineInterpolate',
             moduleName: CORE,
         };
-        Identifiers.interpolate = { name: 'ɵɵinterpolate', moduleName: CORE };
+        Identifiers.interpolate = { name: 'ɵinterpolate', moduleName: CORE };
         Identifiers.EMPTY_ARRAY = { name: 'ɵEMPTY_ARRAY', moduleName: CORE };
         Identifiers.EMPTY_MAP = { name: 'ɵEMPTY_MAP', moduleName: CORE };
         Identifiers.Renderer = { name: 'Renderer', moduleName: CORE };
@@ -18199,7 +18199,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.2+13.sha-28e4187.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.2+14.sha-3fe3a84.with-local-changes');
 
     /**
      * @license
