a0ef3c37712a621fe096eaea0435c56f1ab03359
Kara Erickson <karakara@google.com>
2019-05-06 16:51:01 +0000

    39f2c9f docs(forms): fix ControlValueAccessor registerOnChange code example (#30276)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+81.sha-1ec092b.with-local-changes
+ * @license Angular v8.0.0-rc.0+85.sha-39f2c9f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18147,7 +18147,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+81.sha-1ec092b.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+85.sha-39f2c9f.with-local-changes');
 
 /**
  * @license
