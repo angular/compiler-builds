d24d30798d9761a3a70d964ee19d96b15cc01a4a
Kara Erickson <karakara@google.com>
2019-05-06 17:37:36 +0000

    0926119 docs: add explanation on what lazy loading is (#29667)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+84.sha-60a8888.with-local-changes
+ * @license Angular v8.0.0-rc.0+86.sha-0926119.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17109,7 +17109,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+84.sha-60a8888.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+86.sha-0926119.with-local-changes');
 
 /**
  * @license
