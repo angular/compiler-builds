dcc825617e94c938ec21f322807126735dee780b
Kara Erickson <karakara@google.com>
2019-05-07 21:43:51 +0000

    48de6e8 docs: release notes for the v7.2.15 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+98.sha-3b551e0.with-local-changes
+ * @license Angular v8.0.0-rc.0+99.sha-48de6e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17116,7 +17116,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+98.sha-3b551e0.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+99.sha-48de6e8.with-local-changes');
 
 /**
  * @license
