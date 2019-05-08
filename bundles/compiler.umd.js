e5ff60c69e902e14938ada24445f619ce2798f80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 21:43:20 +0000

    dd8651d fix(core): CSS sanitizer now allows parens in file names (#30322)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+13.sha-5bbbe3f.with-local-changes
+ * @license Angular v8.0.0-rc.3+14.sha-dd8651d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18199,7 +18199,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.3+13.sha-5bbbe3f.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.3+14.sha-dd8651d.with-local-changes');
 
     /**
      * @license
