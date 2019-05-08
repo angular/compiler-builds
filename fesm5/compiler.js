ccf4f5fe4cc0466672decaff55323b8660fd9958
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 21:12:40 +0000

    9a807bd docs(router): Move ActivatedRoute example to mini-app (#29755)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+117.sha-eccc41c.with-local-changes
+ * @license Angular v8.0.0-rc.0+118.sha-9a807bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18142,7 +18142,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+117.sha-eccc41c.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+118.sha-9a807bd.with-local-changes');
 
 /**
  * @license
