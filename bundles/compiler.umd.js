bffe2f05b2002e25f890595c06aae2bace0d17f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 20:17:47 +0000

    509352f fix(core): static-query migration should gracefully exit if AOT compiler throws (#30269)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+113.sha-29786e8.with-local-changes
+ * @license Angular v8.0.0-rc.0+116.sha-509352f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18198,7 +18198,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+113.sha-29786e8.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+116.sha-509352f.with-local-changes');
 
     /**
      * @license
