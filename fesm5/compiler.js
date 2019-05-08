d7ccfbfd549c6ed73da844667fc84e57b6d8694a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 16:44:28 +0000

    d9b9ed5 test(ivy): move host binding tests to acceptance (#30308)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+107.sha-7c8a62d.with-local-changes
+ * @license Angular v8.0.0-rc.0+108.sha-d9b9ed5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18138,7 +18138,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+107.sha-7c8a62d.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+108.sha-d9b9ed5.with-local-changes');
 
 /**
  * @license
