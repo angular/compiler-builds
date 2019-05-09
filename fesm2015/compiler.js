e6d6f24fc962ae7b037154f62b32059be2fb218d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:54:25 +0000

    0ffdb48 fix(core): static-query migration should handle queries on accessors (#30327)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+124.sha-d70b1ff.with-local-changes
+ * @license Angular v8.0.0-rc.0+126.sha-0ffdb48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17089,7 +17089,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+124.sha-d70b1ff.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+126.sha-0ffdb48.with-local-changes');
 
 /**
  * @license
