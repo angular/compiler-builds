c68e9084e5c9903ef1c447e6ae140063331b1929
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:57:44 +0000

    806f811 docs: remove reference to e2e applications (#30324)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+18.sha-dd299f9.with-local-changes
+ * @license Angular v8.0.0-rc.3+19.sha-806f811.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18208,7 +18208,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.3+18.sha-dd299f9.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.3+19.sha-806f811.with-local-changes');
 
     /**
      * @license
