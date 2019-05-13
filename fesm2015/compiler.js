61016f6834c7c4540b4aa454c28bd321bae7bb8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-13 18:16:51 +0000

    24e6c1e release: ts_api_guardian (#30120)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.3+37.sha-42fc5c9.with-local-changes
+ * @license Angular v8.0.0-rc.3+38.sha-24e6c1e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17115,7 +17115,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.3+37.sha-42fc5c9.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.3+38.sha-24e6c1e.with-local-changes');
 
 /**
  * @license
