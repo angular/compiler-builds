24a5924f7f04c49b1b62e6dcca6d577a55d0f39b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 22:08:12 +0000

    dbb150a Revert "fix(core): CSS sanitizer now allows parens in file names (#30322)" (#30463)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+176.sha-9e5377a.with-local-changes
+ * @license Angular v8.0.0-rc.0+177.sha-dbb150a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18126,7 +18126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+176.sha-9e5377a.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+177.sha-dbb150a.with-local-changes');
 
 /**
  * @license
