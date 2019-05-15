4288209ba3b307205a42d27ea9db4e114d57a8da
Jason Aden <jasonaden@google.com>
2019-05-15 21:50:01 +0000

    32daa93 fix(router): type cast correctly for IE 11 bug breaking URL Unification when comparing objects (#30464)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.4+4.sha-766615c.with-local-changes
+ * @license Angular v8.0.0-rc.4+1.sha-32daa93.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18152,7 +18152,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.4+4.sha-766615c.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.4+1.sha-32daa93.with-local-changes');
 
 /**
  * @license
