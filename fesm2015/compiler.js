fa42966cb6b65bc92503eb6e4c606ea8c6f92d55
Jason Aden <jasonaden@google.com>
2019-05-15 21:46:57 +0000

    35c1750 test(ivy): port TemplateRef tests over to acceptance tests (#30443)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+184.sha-53f3564.with-local-changes
+ * @license Angular v8.0.0-rc.0+185.sha-35c1750.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17092,7 +17092,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+184.sha-53f3564.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+185.sha-35c1750.with-local-changes');
 
 /**
  * @license
