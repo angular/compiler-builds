54c3e0b938b6094301ae37257b548fa78033bebe
Jason Aden <jasonaden@google.com>
2019-05-15 21:45:45 +0000

    35c1750 test(ivy): port TemplateRef tests over to acceptance tests (#30443)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+184.sha-53f3564.with-local-changes
+ * @license Angular v8.0.0-rc.0+185.sha-35c1750.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18182,7 +18182,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+184.sha-53f3564.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+185.sha-35c1750.with-local-changes');
 
     /**
      * @license
