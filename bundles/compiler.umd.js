b8e82b28f9e50ea4272f3e6992dbe967a852f288
Jason Aden <jasonaden@google.com>
2019-05-17 20:55:43 +0000

    660a091 ci: increase codefresh memory limit to decrease flakyness of known big tests (#30472)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+248.sha-9d5abfb.with-local-changes
+ * @license Angular v8.0.0-rc.0+253.sha-660a091.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18200,7 +18200,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+253.sha-660a091.with-local-changes');
 
     /**
      * @license
