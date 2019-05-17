c70deedb60d0de56dee2c34057a8b46857e97f85
Jason Aden <jasonaden@google.com>
2019-05-17 21:45:38 +0000

    c1135ee build: update version to 8.0.0-rc.4 in `package.json` (#30493)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+254.sha-661a57d.with-local-changes
+ * @license Angular v8.0.0-rc.0+257.sha-c1135ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18200,7 +18200,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+254.sha-661a57d.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+257.sha-c1135ee.with-local-changes');
 
     /**
      * @license
