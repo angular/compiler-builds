be0cf7fa68e750a76b0ba2625d444a3ddf99a9f8
Jason Aden <jasonaden@google.com>
2019-05-17 03:41:13 +0000

    98ded94 fixup! test(ivy): make the test run with benchpress (#30449)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes
+ * @license Angular v8.0.0-rc.0+238.sha-98ded94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18182,7 +18182,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+238.sha-98ded94.with-local-changes');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes
+ * @license Angular v8.0.0-rc.0+238.sha-98ded94.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17092,7 +17092,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+233.sha-1f6fcb6.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+238.sha-98ded94.with-local-changes');
 
 /**
  * @license
