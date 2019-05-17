a61a28803065c54a89a0d30782d9b4ebab800978
Jason Aden <jasonaden@google.com>
2019-05-17 20:54:11 +0000

    9d5abfb docs: add kamil mysliwiec to GDE resources (#30508)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes
+ * @license Angular v8.0.0-rc.0+248.sha-9d5abfb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17110,7 +17110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+247.sha-fd7dd4d.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+248.sha-9d5abfb.with-local-changes');
 
 /**
  * @license
