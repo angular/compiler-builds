c87648636992499a285a783df1d842ceeb92cb3d
Jason Aden <jasonaden@google.com>
2019-05-17 17:15:44 +0000

    0937062 fix(ivy): type-checking should infer string type for interpolations (#30177)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+241.sha-f03475c.with-local-changes
+ * @license Angular v8.0.0-rc.0+243.sha-0937062.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17110,7 +17110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+241.sha-f03475c.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+243.sha-0937062.with-local-changes');
 
 /**
  * @license
