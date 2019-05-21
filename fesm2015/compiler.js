4de667a55ae5b31fabdc2085e043a58b642bd318
Jason Aden <jasonaden@google.com>
2019-05-21 16:29:33 +0000

    e946594 docs: move instructions to see changes after the error is fixed in the tutorial (#30529)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+266.sha-abe4433.with-local-changes
+ * @license Angular v8.0.0-rc.0+267.sha-e946594.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17110,7 +17110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+266.sha-abe4433.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+267.sha-e946594.with-local-changes');
 
 /**
  * @license
