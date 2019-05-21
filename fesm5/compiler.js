86e376e4c4b8c037d1f42af5d93626a866dbf575
Jason Aden <jasonaden@google.com>
2019-05-21 20:58:21 +0000

    7c0667d docs: delete double dashes on ng command in testing guide (#30551)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+287.sha-66f269c.with-local-changes
+ * @license Angular v8.0.0-rc.0+283.sha-7c0667d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18144,7 +18144,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+287.sha-66f269c.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+283.sha-7c0667d.with-local-changes');
 
 /**
  * @license
