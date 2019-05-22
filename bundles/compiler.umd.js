d14fc138319c69279f9800ce75c386621fdf7486
Jason Aden <jasonaden@google.com>
2019-05-22 23:41:30 +0000

    3c54818 docs: update node and typescript versions mentioned in docs (#30601)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+292.sha-ae91f45.with-local-changes
+ * @license Angular v8.0.0-rc.0+293.sha-3c54818.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18200,7 +18200,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+292.sha-ae91f45.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+293.sha-3c54818.with-local-changes');
 
     /**
      * @license
