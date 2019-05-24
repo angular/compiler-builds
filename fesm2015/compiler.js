27d76e1cc93b88057c7869f26a0179ef5584e159
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:22:07 +0000

    661c6e6 fix(ivy): fix PR collision with static: true and new test (#30666)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
+ * @license Angular v8.0.0-rc.0+345.sha-661c6e6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17167,7 +17167,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
 
 /**
  * @license
