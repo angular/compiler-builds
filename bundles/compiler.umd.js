a54a603b63d5742c714b8ada135fe64dd82d884e
Igor Minar <igor@angular.io>
2019-05-28 17:01:34 +0000

    4761d40 docs: fix typo in a declaration alert (#30545)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+9.sha-002fbdd.with-local-changes
+ * @license Angular v8.0.0-rc.5+8.sha-4761d40.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18208,7 +18208,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.5+9.sha-002fbdd.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.5+8.sha-4761d40.with-local-changes');
 
     /**
      * @license
