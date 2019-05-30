643b2c86cc1335b9e20bcbf4cf30cdf48e0904ea
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:25:23 +0000

    e3f3b10 docs(ivy): improve debug documentation (#30562)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+374.sha-62332b8.with-local-changes
+ * @license Angular v8.0.0-rc.0+373.sha-e3f3b10.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18320,7 +18320,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+374.sha-62332b8.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+373.sha-e3f3b10.with-local-changes');
 
     /**
      * @license
