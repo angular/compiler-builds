ac9c93703db406a4e1065b72cc3cb0581e4cbed1
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:18:11 +0000

    b19a05c docs(common): fix typo in ngSwitch directive description (#30483)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+4.sha-ae6fa92.with-local-changes
+ * @license Angular v8.0.0+8.sha-b19a05c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17115,7 +17115,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0+4.sha-ae6fa92.with-local-changes');
+const VERSION$1 = new Version('8.0.0+8.sha-b19a05c.with-local-changes');
 
 /**
  * @license
