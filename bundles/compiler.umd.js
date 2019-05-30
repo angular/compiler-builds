3c2737ca486d9eb04a5579a3006e1464a3b61c6c
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:22:13 +0000

    af4925f docs: clarify usage of using /deep/ in component styles guide (#30452)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+360.sha-7757826.with-local-changes
+ * @license Angular v8.0.0-rc.0+361.sha-af4925f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18320,7 +18320,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+360.sha-7757826.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+361.sha-af4925f.with-local-changes');
 
     /**
      * @license
