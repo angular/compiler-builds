4623375615116183b48470e957ad8aa0062d372d
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:27:11 +0000

    e3f3b10 docs(ivy): improve debug documentation (#30562)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+374.sha-62332b8.with-local-changes
+ * @license Angular v8.0.0-rc.0+373.sha-e3f3b10.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17225,7 +17225,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+374.sha-62332b8.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+373.sha-e3f3b10.with-local-changes');
 
 /**
  * @license
