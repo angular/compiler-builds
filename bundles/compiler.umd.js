571471034c3fab04caedd9beb87f2c21d2f973b2
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 19:27:52 +0000

    5e0f982 feat(ivy): use i18n locale data to determine the plural form of ICU expressions (#29249)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes
+ * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18320,7 +18320,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0-rc.0+378.sha-5e0f982.with-local-changes');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes
+ * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17225,7 +17225,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes');
+const VERSION$1 = new Version('8.0.0-rc.0+378.sha-5e0f982.with-local-changes');
 
 /**
  * @license
