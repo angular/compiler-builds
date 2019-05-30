61715ea32f2a24ec727bd2081da076958e028051
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:22:45 +0000

    cfc608a docs: merge duplicated platform-webworker content in Deprecation Guide (#30651)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+6.sha-1c67b90.with-local-changes
+ * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18208,7 +18208,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+6.sha-1c67b90.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+10.sha-cfc608a.with-local-changes');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+6.sha-1c67b90.with-local-changes
+ * @license Angular v8.0.0+10.sha-cfc608a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18152,7 +18152,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0+6.sha-1c67b90.with-local-changes');
+var VERSION$1 = new Version('8.0.0+10.sha-cfc608a.with-local-changes');
 
 /**
  * @license
