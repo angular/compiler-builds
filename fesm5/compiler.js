ac0807407d25f5cdd820f9875ce8c388001b07d3
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 19:28:11 +0000

    7a0f8ac fix(ivy): generate explicit type annotation for NgModuleFactory calls in ngfactories (#30708)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+378.sha-5e0f982.with-local-changes
+ * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18264,7 +18264,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0-rc.0+378.sha-5e0f982.with-local-changes');
+var VERSION$1 = new Version('8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes');
 
 /**
  * @license
