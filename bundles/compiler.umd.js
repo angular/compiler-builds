aecdc7f999c337b824a6a8e64755f4a21ecfbed3
Miško Hevery <misko@hevery.com>
2019-05-31 17:14:21 +0000

    f4cd374 fix(docs-infra): update tooltip of events link (#30643)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+7.sha-1f79c82.with-local-changes
+ * @license Angular v8.1.0-beta.0+9.sha-f4cd374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18330,7 +18330,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0+7.sha-1f79c82.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-beta.0+9.sha-f4cd374.with-local-changes');
 
     /**
      * @license
