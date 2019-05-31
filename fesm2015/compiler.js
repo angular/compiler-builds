093d790a8589cf9528637fbe78bf7587924aa972
Miško Hevery <misko@hevery.com>
2019-05-31 22:42:47 +0000

    8ce288a fix(docs-infra): add v7 in versions dropdown (#30775)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+12.sha-f936590.with-local-changes
+ * @license Angular v8.1.0-beta.0+13.sha-8ce288a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17231,7 +17231,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-beta.0+12.sha-f936590.with-local-changes');
+const VERSION$1 = new Version('8.1.0-beta.0+13.sha-8ce288a.with-local-changes');
 
 /**
  * @license
