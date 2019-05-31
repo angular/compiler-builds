29bf09ccf451088eb0ce16d3fed2830c75334af4
Miško Hevery <misko@hevery.com>
2019-05-31 17:20:27 +0000

    876cd60 docs(docs-infra): remove deprecated `Buffer` usage from stackblitz builder (#30369)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+10.sha-aca339e.with-local-changes
+ * @license Angular v8.1.0-beta.0+11.sha-876cd60.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17231,7 +17231,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-beta.0+10.sha-aca339e.with-local-changes');
+const VERSION$1 = new Version('8.1.0-beta.0+11.sha-876cd60.with-local-changes');
 
 /**
  * @license
