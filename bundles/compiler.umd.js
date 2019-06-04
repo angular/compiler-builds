3e6c9e1db5219854b2c4497bc8c1f493a0e5dd12
Misko Hevery <misko@hevery.com>
2019-06-04 23:56:49 +0000

    0d4f8c7 fix(ivy): allow empty cases for ICU expressions (#30846)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+33.sha-4ecff42.with-local-changes
+ * @license Angular v8.1.0-beta.0+35.sha-0d4f8c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18317,7 +18317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0+33.sha-4ecff42.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-beta.0+35.sha-0d4f8c7.with-local-changes');
 
     /**
      * @license
