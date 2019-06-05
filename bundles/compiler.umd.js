4ee82ac1ca2e0d9aca5ccbaa07ba0d84afaeb807
Miško Hevery <misko@hevery.com>
2019-06-05 20:01:23 +0000

    540b014 release: cut the v8.1.0-next.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+40.sha-8852b79.with-local-changes
+ * @license Angular v8.1.0-next.1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18329,7 +18329,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-beta.0+40.sha-8852b79.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1.with-local-changes');
 
     /**
      * @license
