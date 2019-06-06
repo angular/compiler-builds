46a8023ecbd5cef861889c61446fe5272ee2ff25
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:48 +0000

    65f2010 fix(ivy): ngcc - use spaces in overwritten `package.json` content for readability (#30831)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+38.sha-5a7bcd1.with-local-changes
+ * @license Angular v8.0.0+39.sha-65f2010.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17112,7 +17112,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0+38.sha-5a7bcd1.with-local-changes');
+const VERSION$1 = new Version('8.0.0+39.sha-65f2010.with-local-changes');
 
 /**
  * @license
