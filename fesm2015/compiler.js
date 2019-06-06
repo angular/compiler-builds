6ad678e6675bc38210e593c5bc9dd5b26d3f6210
Miško Hevery <misko@hevery.com>
2019-06-06 21:07:53 +0000

    be82270 feat(docs-infra): white background and corresponding figure updates (#28396)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+41.sha-4cef2c1.with-local-changes
+ * @license Angular v8.0.0+42.sha-be82270.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17112,7 +17112,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.0+41.sha-4cef2c1.with-local-changes');
+const VERSION$1 = new Version('8.0.0+42.sha-be82270.with-local-changes');
 
 /**
  * @license
