5b578368f29e6e2e08f87eff097e874c0968f839
Misko Hevery <misko@hevery.com>
2019-06-06 04:50:29 +0000

    05a43ca fix(bazel): exclude components schematics from build (#30825)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+5.sha-30efb6b.with-local-changes
+ * @license Angular v8.1.0-next.1+4.sha-05a43ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18331,7 +18331,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.1+5.sha-30efb6b.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1+4.sha-05a43ca.with-local-changes');
 
     /**
      * @license
