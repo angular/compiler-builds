c03a2decd572c8851fd85cd287db98008a5986d6
Misko Hevery <misko@hevery.com>
2019-06-06 04:51:30 +0000

    30efb6b fix(ivy): don't project removed placeholders with runtime i18n (#30783)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+1.sha-17bfedd.with-local-changes
+ * @license Angular v8.1.0-next.1+5.sha-30efb6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16546,8 +16546,10 @@ var TemplateDefinitionBuilder = /** @class */ (function () {
         attrs.forEach(function (input) {
             if (input instanceof BoundAttribute) {
                 var value_4 = input.value.visit(_this._valueConverter);
-                _this.allocateBindingSlots(value_4);
-                _this.updateInstruction(templateIndex, template.sourceSpan, Identifiers$1.property, function () { return [literal(input.name), _this.convertPropertyBinding(context, value_4, true)]; });
+                if (value_4 !== undefined) {
+                    _this.allocateBindingSlots(value_4);
+                    _this.updateInstruction(templateIndex, template.sourceSpan, Identifiers$1.property, function () { return [literal(input.name), _this.convertPropertyBinding(context, value_4, true)]; });
+                }
             }
         });
     };
@@ -18273,7 +18275,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0-next.1+1.sha-17bfedd.with-local-changes');
+var VERSION$1 = new Version('8.1.0-next.1+5.sha-30efb6b.with-local-changes');
 
 /**
  * @license
