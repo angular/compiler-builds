9293eea1f9f46755eac8e95c26785cc8dada915f
Miško Hevery <misko@hevery.com>
2019-06-06 21:07:45 +0000

    11a4454 perf(ivy): remove check for function type in renderStringify (#30838)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
+ * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18330,7 +18330,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.1+10.sha-11a4454.with-local-changes');
 
     /**
      * @license
