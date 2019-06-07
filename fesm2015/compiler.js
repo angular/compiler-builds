0cf4e8af3e53321548ed95388cc65effcd66f398
Misko Hevery <misko@hevery.com>
2019-06-07 16:22:13 +0000

    297222f build(docs-infra): fail hard if the CLI source is not what we expect (#30901)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+18.sha-c038675.with-local-changes
+ * @license Angular v8.1.0-next.1+14.sha-297222f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15622,9 +15622,7 @@ class TemplateDefinitionBuilder {
     }
     updateInstruction(nodeIndex, span, reference, paramsOrFn) {
         if (this._lastNodeIndexWithFlush < nodeIndex) {
-            if (nodeIndex > 0) {
-                this.instructionFn(this._updateCodeFns, span, Identifiers$1.select, [literal(nodeIndex)]);
-            }
+            this.instructionFn(this._updateCodeFns, span, Identifiers$1.select, [literal(nodeIndex)]);
             this._lastNodeIndexWithFlush = nodeIndex;
         }
         this.instructionFn(this._updateCodeFns, span, reference, paramsOrFn || []);
@@ -17236,7 +17234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-next.1+18.sha-c038675.with-local-changes');
+const VERSION$1 = new Version('8.1.0-next.1+14.sha-297222f.with-local-changes');
 
 /**
  * @license
