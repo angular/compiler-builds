27bb3bc78dedc7e0a6e55a28bdebde55259a4d95
Misko Hevery <misko@hevery.com>
2019-06-07 16:22:48 +0000

    4e8614b fix(common): expose the `HttpUploadProgressEvent` interface as public API (#30852)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+47.sha-08c38a1.with-local-changes
+ * @license Angular v8.0.0+49.sha-4e8614b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18149,7 +18149,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.0+47.sha-08c38a1.with-local-changes');
+var VERSION$1 = new Version('8.0.0+49.sha-4e8614b.with-local-changes');
 
 /**
  * @license
