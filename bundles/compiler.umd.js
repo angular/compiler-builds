f7b118fb742e021b0a0256b50b9999df106bcffa
Misko Hevery <misko@hevery.com>
2019-06-07 16:12:26 +0000

    8aef446 build(docs-infra): upgrade cli command docs sources to d09f130ce (#30909)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+46.sha-9aeef0a.with-local-changes
+ * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18205,7 +18205,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+46.sha-9aeef0a.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+45.sha-8aef446.with-local-changes');
 
     /**
      * @license
