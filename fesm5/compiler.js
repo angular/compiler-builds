dda4a1e37547d711c08e67dad3d41bb10a449501
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-10 19:46:56 +0000

    c265475 test: update resolve path for windows

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+33.sha-4f1008b.with-local-changes
+ * @license Angular v8.1.0-beta.0+34.sha-c265475.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18274,7 +18274,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0-beta.0+33.sha-4f1008b.with-local-changes');
+var VERSION$1 = new Version('8.1.0-beta.0+34.sha-c265475.with-local-changes');
 
 /**
  * @license
