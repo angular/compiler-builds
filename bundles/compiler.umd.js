f1fcc1b784932a13371bb4981bc3760d3e6f8ce0
Igor Minar <igor@angular.io>
2019-06-11 00:35:39 +0000

    dcf9f05 docs: use `Node.js` consistently (#30934) PR Close #30934

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
+ * @license Angular v8.0.0+55.sha-dcf9f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18205,7 +18205,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+51.sha-6a48485.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+55.sha-dcf9f05.with-local-changes');
 
     /**
      * @license
