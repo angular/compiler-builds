ea3f2ab8670c9c2c6482eec6c0b1da85ee8473a7
Igor Minar <igor@angular.io>
2019-06-11 00:27:00 +0000

    41e56c9 fixup! refactor(docs-infra): make archive redirection tests DRY

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.15+1.sha-b62a9d0.with-local-changes
+ * @license Angular v7.2.15+5.sha-41e56c9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15766,7 +15766,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('7.2.15+1.sha-b62a9d0.with-local-changes');
+var VERSION$1 = new Version('7.2.15+5.sha-41e56c9.with-local-changes');
 
 /**
  * @license
