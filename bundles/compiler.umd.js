2c7ae386569b2986f706d19612ad37c5b429135e
Andrew Kushnir <akushnir@google.com>
2019-06-11 21:46:58 +0000

    338e58c docs: use const in dynamic-component-loader example (#30888)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+60.sha-56a3dcf.with-local-changes
+ * @license Angular v8.0.0+62.sha-338e58c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18205,7 +18205,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.0+60.sha-56a3dcf.with-local-changes');
+    var VERSION$1 = new Version('8.0.0+62.sha-338e58c.with-local-changes');
 
     /**
      * @license
