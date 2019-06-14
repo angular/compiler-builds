947da93bc1fbe8bb86ecde191f9888803576d64b
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:07 +0000

    f41242f docs: rewrite inputs/outputs section of Template Syntax (#27685)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
+ * @license Angular v8.1.0-next.2+19.sha-f41242f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17299,7 +17299,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
+const VERSION$1 = new Version('8.1.0-next.2+19.sha-f41242f.with-local-changes');
 
 /**
  * @license
