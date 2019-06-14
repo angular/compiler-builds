8e417d6ee22f356966d8b0b1bf8408ecbb03c436
Andrew Kushnir <akushnir@google.com>
2019-06-14 20:36:37 +0000

    6fbfb5a feat(ivy): ngcc - recognize static properties on the outer symbol in ES5 (#30795)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+23.sha-16aa6ce.with-local-changes
+ * @license Angular v8.1.0-next.2+24.sha-6fbfb5a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18391,7 +18391,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.2+23.sha-16aa6ce.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.2+24.sha-6fbfb5a.with-local-changes');
 
     /**
      * @license
