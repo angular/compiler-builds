944460cc309527f064d9a0421925303479229d73
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:13:28 +0000

    8f5c396 fix(ivy): don't throw when attempting to destroy a destroyed ComponentRef (#31022)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+10.sha-0c3bb6a.with-local-changes
+ * @license Angular v8.1.0-next.2+11.sha-8f5c396.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17299,7 +17299,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-next.2+10.sha-0c3bb6a.with-local-changes');
+const VERSION$1 = new Version('8.1.0-next.2+11.sha-8f5c396.with-local-changes');
 
 /**
  * @license
