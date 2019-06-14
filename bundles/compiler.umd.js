2c3736affd8818e21377c4a47a3bd288b6c7550c
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:11:52 +0000

    85d38ae docs: rewrite property binding section and add example (#25770)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+14.sha-4ad323a.with-local-changes
+ * @license Angular v8.1.0-next.2+15.sha-85d38ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18391,7 +18391,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.2+14.sha-4ad323a.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.2+15.sha-85d38ae.with-local-changes');
 
     /**
      * @license
