eab23431d043adc313a6ec40074c8ddb04a40200
Andrew Kushnir <akushnir@google.com>
2019-06-17 23:53:47 +0000

    c52ade287 fix(docs-infra): fix search result header color (#30924)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+29.sha-4adf95e.with-local-changes
+ * @license Angular v8.1.0-next.2+30.sha-c52ade2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17299,7 +17299,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-next.2+29.sha-4adf95e.with-local-changes');
+const VERSION$1 = new Version('8.1.0-next.2+30.sha-c52ade2.with-local-changes');
 
 /**
  * @license
