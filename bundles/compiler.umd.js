8d0a11c66df28c8c0fb58402c8d5acdb511da80a
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:21:38 +0000

    87665fe32 build(docs-infra): upgrade cli command docs sources to ab6f36229 (#31102)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+36.sha-87168ac.with-local-changes
+ * @license Angular v8.1.0-next.2+39.sha-87665fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18394,7 +18394,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.2+36.sha-87168ac.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.2+39.sha-87665fe.with-local-changes');
 
     /**
      * @license
