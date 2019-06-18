70f8d812b20dcf0006aa818d37ba2e85dd48d5ca
Andrew Kushnir <akushnir@google.com>
2019-06-18 21:36:27 +0000

    bf9de8ca5 test: update side-effects test to be more descriptive (#31005)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+34.sha-df37c47.with-local-changes
+ * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17173,7 +17173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.1+34.sha-df37c47.with-local-changes');
+const VERSION$1 = new Version('8.0.1+35.sha-bf9de8c.with-local-changes');
 
 /**
  * @license
