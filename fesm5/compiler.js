611bc4b5d5bc5829b1d5b1f202ea6fd30d4ca18d
Andrew Kushnir <akushnir@google.com>
2019-06-18 21:21:37 +0000

    7a53b9090 ci: add jasonaden to forms owners (#31117)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+44.sha-db08fd2.with-local-changes
+ * @license Angular v8.1.0-next.2+45.sha-7a53b90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18338,7 +18338,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0-next.2+44.sha-db08fd2.with-local-changes');
+var VERSION$1 = new Version('8.1.0-next.2+45.sha-7a53b90.with-local-changes');
 
 /**
  * @license
