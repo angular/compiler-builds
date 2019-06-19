74366048116b396aef5062e4ab1fac32173f29c6
Kara Erickson <karakara@google.com>
2019-06-19 22:23:42 +0000

    d8def6d72 refactor(docs-infra): move auto-link filters to dedicated directory (#31051)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+37.sha-2fe8f2b.with-local-changes
+ * @license Angular v8.0.2+2.sha-d8def6d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18210,7 +18210,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.1+37.sha-2fe8f2b.with-local-changes');
+var VERSION$1 = new Version('8.0.2+2.sha-d8def6d.with-local-changes');
 
 /**
  * @license
