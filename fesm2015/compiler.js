60c1e8fb2e4935f5aeca53297f260780a631e37a
Kara Erickson <karakara@google.com>
2019-06-19 23:18:37 +0000

    c36edffe2 docs: fix bad tag (#31115)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+3.sha-b68f2d6.with-local-changes
+ * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17173,7 +17173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.2+3.sha-b68f2d6.with-local-changes');
+const VERSION$1 = new Version('8.0.2+7.sha-c36edff.with-local-changes');
 
 /**
  * @license
