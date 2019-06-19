fa2d52d72b52bddfb5b4c4bdfc1050ba13512737
Kara Erickson <karakara@google.com>
2019-06-19 22:50:48 +0000

    ab86a58f2 docs: remove inappropriate link (#31091)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+3.sha-4ec5081.with-local-changes
+ * @license Angular v8.1.0-next.3+4.sha-ab86a58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18394,7 +18394,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+3.sha-4ec5081.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+4.sha-ab86a58.with-local-changes');
 
     /**
      * @license
