e9782f8a4985fcaab145b7d71564e85650857567
Kara Erickson <karakara@google.com>
2019-06-19 23:19:46 +0000

    9c222350f ci: re-enable payload size tracking (#31138)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
+ * @license Angular v8.0.2+8.sha-9c22235.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18210,7 +18210,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.0.2+7.sha-c36edff.with-local-changes');
+var VERSION$1 = new Version('8.0.2+8.sha-9c22235.with-local-changes');
 
 /**
  * @license
