560a9626534b719f983c79b449aa7980b57aeb6c
Kara Erickson <karakara@google.com>
2019-06-20 20:14:56 +0000

    bf7d04626 style: fix bazel lint in zone.js (#31172)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+19.sha-5eb7426.with-local-changes
+ * @license Angular v8.1.0-next.3+20.sha-bf7d046.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18434,7 +18434,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+19.sha-5eb7426.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+20.sha-bf7d046.with-local-changes');
 
     /**
      * @license
