841a5b59df9c47a95a50e5b40fbd5ad897eb9a7c
Kara Erickson <karakara@google.com>
2019-06-20 18:45:43 +0000

    b356fb01d build(docs-infra): upgrade @angular/cli to 8.1.0-beta.2 (#31137)

--- src/render3/view/template.d.ts
+++ src/render3/view/template.d.ts
@@ -301,6 +301,12 @@ export interface ParseTemplateOptions {
      * but the new line should increment the current line for source mapping.
      */
     escapedString?: boolean;
+    /**
+     * An array of characters that should be considered as leading trivia.
+     * Leading trivia are characters that are not important to the developer, and so should not be
+     * included in source-map segments.  A common example is whitespace.
+     */
+    leadingTriviaChars?: string[];
 }
 /**
  * Parse a template into render3 `Node`s and additional metadata, with no other dependencies.
