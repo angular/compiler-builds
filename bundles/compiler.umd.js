e7122e721c15ca94fc03a52d5759f819b4ae956c
Kara Erickson <karakara@google.com>
2019-06-21 22:52:57 +0000

    7fe95b188 ci: send failure notifications from `saucelabs_tests` to `dev-infra-ci-failures` (#31202)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+18.sha-ff59345.with-local-changes
+ * @license Angular v8.0.2+22.sha-7fe95b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18266,7 +18266,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.0.2+18.sha-ff59345.with-local-changes');
+    var VERSION$1 = new Version('8.0.2+22.sha-7fe95b1.with-local-changes');
 
     /**
      * @license
