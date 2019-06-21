d42b3e3722a2862987842b61ce0ef81792c0b7c3
Kara Erickson <karakara@google.com>
2019-06-21 17:43:04 +0000

    1699b5486 docs: add Mosaic library to resources (#31026)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+10.sha-07f6b2c.with-local-changes
+ * @license Angular v8.0.2+11.sha-1699b54.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17173,7 +17173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.2+10.sha-07f6b2c.with-local-changes');
+const VERSION$1 = new Version('8.0.2+11.sha-1699b54.with-local-changes');
 
 /**
  * @license
