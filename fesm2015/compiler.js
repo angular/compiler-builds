da6e0f57555bc63ffa208867783a8316122a1936
Kara Erickson <karakara@google.com>
2019-06-24 22:19:25 +0000

    980bcaf17 fix(bazel): remove unsupported Css pre-processors from ng new (#31234)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+25.sha-f470e69.with-local-changes
+ * @license Angular v8.0.2+24.sha-980bcaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17173,7 +17173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.2+25.sha-f470e69.with-local-changes');
+const VERSION$1 = new Version('8.0.2+24.sha-980bcaf.with-local-changes');
 
 /**
  * @license
