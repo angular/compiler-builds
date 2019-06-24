eef8503e9d68bbdfbd15e17a0c710099a9a799eb
Kara Erickson <karakara@google.com>
2019-06-24 18:50:57 +0000

    fcb03abc7 fix(ivy): injecting incorrect Renderer2 into child components (#31063)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+40.sha-a4fc98c.with-local-changes
+ * @license Angular v8.1.0-next.3+44.sha-fcb03ab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18423,7 +18423,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+40.sha-a4fc98c.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+44.sha-fcb03ab.with-local-changes');
 
     /**
      * @license
