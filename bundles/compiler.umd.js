2cf662cf2d3614b85e632ecfdd216abae1015c11
Kara Erickson <karakara@google.com>
2019-06-25 18:20:28 +0000

    a1d436e6a docs: fix formatting (#31120)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+65.sha-00ecfc7.with-local-changes
+ * @license Angular v8.1.0-next.3+68.sha-a1d436e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18428,7 +18428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-next.3+65.sha-00ecfc7.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-next.3+68.sha-a1d436e.with-local-changes');
 
     /**
      * @license
