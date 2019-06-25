7133a0e30b9c551581896821c53b807e3e3d2b78
Kara Erickson <karakara@google.com>
2019-06-25 18:00:45 +0000

    00242e8d6 docs(core): change from CSS Event to DOM event (#31229) PR Close #31229

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
+ * @license Angular v8.0.2+30.sha-00242e8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17173,7 +17173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.0.2+33.sha-0a46b2a.with-local-changes');
+const VERSION$1 = new Version('8.0.2+30.sha-00242e8.with-local-changes');
 
 /**
  * @license
