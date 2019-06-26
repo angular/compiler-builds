19c2b451eb52f84c4747716cff56c1904b9aa2fa
Kara Erickson <karakara@google.com>
2019-06-26 00:36:42 +0000

    7186f9c01 refactor(ivy): implement a virtual file-system layer in ngtsc + ngcc (#30921)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+80.sha-280e856.with-local-changes
+ * @license Angular v8.1.0-next.3+83.sha-7186f9c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18380,7 +18380,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0-next.3+80.sha-280e856.with-local-changes');
+var VERSION$1 = new Version('8.1.0-next.3+83.sha-7186f9c.with-local-changes');
 
 /**
  * @license
