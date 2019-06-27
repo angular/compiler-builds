ece92245f3eb3c43d699a81b2a3aa5e68c9949a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 19:55:01 +0000

    d17100608 fix(ivy): ngtsc - NgtscCompilerHost should cope with directories that look like files (#31289)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+11.sha-19e8570.with-local-changes
+ * @license Angular v8.1.0-rc.0+12.sha-d171006.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17352,7 +17352,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-rc.0+11.sha-19e8570.with-local-changes');
+const VERSION$1 = new Version('8.1.0-rc.0+12.sha-d171006.with-local-changes');
 
 /**
  * @license
