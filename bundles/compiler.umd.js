716615a5bdb9e7a6ae0b16b78fea3d359864d6a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 17:10:00 +0000

    f2360aab9 fix(ivy): incorrect namespace for root node created through ViewContainerRef (#31232)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+3.sha-6c0cca0.with-local-changes
+ * @license Angular v8.1.0-rc.0+2.sha-f2360aa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-rc.0+3.sha-6c0cca0.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-rc.0+2.sha-f2360aa.with-local-changes');
 
     /**
      * @license
