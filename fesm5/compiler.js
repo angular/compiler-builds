72afb0138a1ecf89a5175fd84c7fdbf55d62faed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-27 23:08:54 +0000

    579f1295a ci: add branch info to CircleCI failure notifications (#31319)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+10.sha-2914b10.with-local-changes
+ * @license Angular v8.1.0-rc.0+11.sha-579f129.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18385,7 +18385,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0-rc.0+10.sha-2914b10.with-local-changes');
+var VERSION$1 = new Version('8.1.0-rc.0+11.sha-579f129.with-local-changes');
 
 /**
  * @license
