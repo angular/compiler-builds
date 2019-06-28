0ac5447463e5e89a9bc561e2d46f50859898e482
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:48:54 +0000

    2b5d52fbd docs: add tests for lazy loading angularjs example (#30622)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+21.sha-f57e77e.with-local-changes
+ * @license Angular v8.1.0-rc.0+20.sha-2b5d52f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17376,7 +17376,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-rc.0+21.sha-f57e77e.with-local-changes');
+const VERSION$1 = new Version('8.1.0-rc.0+20.sha-2b5d52f.with-local-changes');
 
 /**
  * @license
