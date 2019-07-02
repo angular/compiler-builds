7dbfe94e04de59d55d9dde87188ea85418ed69d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:53:14 +0000

    d0c32e03b docs(core): in template syntax guide, make SVG example more clear (#31356)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+29.sha-0fe6110.with-local-changes
+ * @license Angular v8.1.0-rc.0+32.sha-d0c32e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0-rc.0+29.sha-0fe6110.with-local-changes');
+    var VERSION$1 = new Version('8.1.0-rc.0+32.sha-d0c32e0.with-local-changes');
 
     /**
      * @license
