9f0bbdfc3691c714be7a7d2edc8c4cd233e49dd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:49:59 +0000

    95a9d6759 test(zone.js): fix typos (#31358)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+41.sha-1db3ac4.with-local-changes
+ * @license Angular v8.1.0-rc.0+44.sha-95a9d67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17452,7 +17452,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0-rc.0+41.sha-1db3ac4.with-local-changes');
+const VERSION$1 = new Version('8.1.0-rc.0+44.sha-95a9d67.with-local-changes');
 
 /**
  * @license
