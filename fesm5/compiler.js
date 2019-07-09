ee6a3f6dc5721c25018a7b15011e2ffb5edd0cff
Jason Aden <jasonaden@google.com>
2019-07-09 17:00:01 +0000

    98a68ad3e fix(ivy): handle namespaced imports correctly (#31367)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+14.sha-36d3062.with-local-changes
+ * @license Angular v8.2.0-next.0+15.sha-98a68ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18479,7 +18479,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.0-next.0+14.sha-36d3062.with-local-changes');
+var VERSION$1 = new Version('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
 
 /**
  * @license
