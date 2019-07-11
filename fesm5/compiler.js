dfa32677197181e1466c590f63706f755fe7439a
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 16:07:19 +0000

    1cba5d42d fix(ivy): handle rooted resource paths correctly (#31511)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+6.sha-fe17938.with-local-changes
+ * @license Angular v8.2.0-next.1+8.sha-1cba5d4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18513,7 +18513,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.0-next.1+6.sha-fe17938.with-local-changes');
+var VERSION$1 = new Version('8.2.0-next.1+8.sha-1cba5d4.with-local-changes');
 
 /**
  * @license
