0a59886e60259288f95f19cea328d4e9be295327
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:39:27 +0000

    58260fc4a build: remove redundant `@types/source-map` dependency (#31468)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
+ * @license Angular v8.1.0+36.sha-58260fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0+34.sha-3ade93f.with-local-changes');
+    var VERSION$1 = new Version('8.1.0+36.sha-58260fc.with-local-changes');
 
     /**
      * @license
