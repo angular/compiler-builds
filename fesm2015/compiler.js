9ae6ba16e8ed23fa75dd220a9ecea051da654b1b
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 17:23:18 +0000

    a55c6df07 feat(docs-infra): add prominent archive mode banner styling (#31245)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+18.sha-e85fa5d.with-local-changes
+ * @license Angular v8.2.0-next.1+21.sha-a55c6df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17483,7 +17483,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.0-next.1+18.sha-e85fa5d.with-local-changes');
+const VERSION$1 = new Version('8.2.0-next.1+21.sha-a55c6df.with-local-changes');
 
 /**
  * @license
