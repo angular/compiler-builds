ca2641fc262199d4a3efb9b28df3d0692423c301
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:54:45 +0000

    a395cb1a5 docs: add Renderer to deprecations doc (#31419)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17352,7 +17352,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0+12.sha-1863254.with-local-changes');
+const VERSION$1 = new Version('8.1.0+14.sha-a395cb1.with-local-changes');
 
 /**
  * @license
