27aec5d9dea44e2fada397ae6bc327ee987556e3
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 22:45:26 +0000

    d545bbeee fix(ivy): handle ModuleWithProviders in R3TestBed correctly (#31415)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+34.sha-4bb283c.with-local-changes
+ * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18569,7 +18569,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+34.sha-4bb283c.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
 
     /**
      * @license
