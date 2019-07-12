e9ebea359f63c772b17b4d09615641ec0745c968
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:56:23 +0000

    63e458dd3 fix(ivy): handle ICUs with placeholders in case other nested ICUs are present (#31516)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
+ * @license Angular v8.2.0-next.1+36.sha-63e458d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15882,18 +15882,19 @@
     var SerializerVisitor = /** @class */ (function () {
         function SerializerVisitor() {
             /**
-             * Flag that indicates that we are processing elements of an ICU.
+             * Keeps track of ICU nesting level, allowing to detect that we are processing elements of an ICU.
              *
-             * This flag is needed due to the fact that placeholders in ICUs and in other messages are
-             * represented differently in Closure:
+             * This is needed due to the fact that placeholders in ICUs and in other messages are represented
+             * differently in Closure:
              * - {$placeholder} in non-ICU case
              * - {PLACEHOLDER} inside ICU
              */
-            this.insideIcu = false;
+            this.icuNestingLevel = 0;
         }
         SerializerVisitor.prototype.formatPh = function (value) {
-            var formatted = formatI18nPlaceholderName(value, /* useCamelCase */ !this.insideIcu);
-            return this.insideIcu ? "{" + formatted + "}" : "{$" + formatted + "}";
+            var isInsideIcu = this.icuNestingLevel > 0;
+            var formatted = formatI18nPlaceholderName(value, /* useCamelCase */ !isInsideIcu);
+            return isInsideIcu ? "{" + formatted + "}" : "{$" + formatted + "}";
         };
         SerializerVisitor.prototype.visitText = function (text, context) { return text.value; };
         SerializerVisitor.prototype.visitContainer = function (container, context) {
@@ -15902,10 +15903,10 @@
         };
         SerializerVisitor.prototype.visitIcu = function (icu, context) {
             var _this = this;
-            this.insideIcu = true;
+            this.icuNestingLevel++;
             var strCases = Object.keys(icu.cases).map(function (k) { return k + " {" + icu.cases[k].visit(_this) + "}"; });
             var result = "{" + icu.expressionPlaceholder + ", " + icu.type + ", " + strCases.join(' ') + "}";
-            this.insideIcu = false;
+            this.icuNestingLevel--;
             return result;
         };
         SerializerVisitor.prototype.visitTagPlaceholder = function (ph, context) {
@@ -18569,7 +18570,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+36.sha-63e458d.with-local-changes');
 
     /**
      * @license
