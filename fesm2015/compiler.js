0ce28dc9d97b749b48af18addbc9233ef81e4477
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:57:35 +0000

    fa69e99bd build: add tsconfig-test to dependency for tsconfig in core/test/strict_types (#31471)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+36.sha-58260fc.with-local-changes
+ * @license Angular v8.1.0+41.sha-fa69e99.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17352,7 +17352,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.1.0+36.sha-58260fc.with-local-changes');
+const VERSION$1 = new Version('8.1.0+41.sha-fa69e99.with-local-changes');
 
 /**
  * @license
