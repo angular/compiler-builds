1b878c40bfe42a6dc120f88e322be1f65ecf07c5
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 21:56:01 +0000

    13dbb98a1 fix(compiler-cli): Return original sourceFile instead of redirected sourceFile from getSourceFile (#26036)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+49.sha-78a8098.with-local-changes
+ * @license Angular v8.1.0+50.sha-13dbb98.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18385,7 +18385,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.1.0+49.sha-78a8098.with-local-changes');
+var VERSION$1 = new Version('8.1.0+50.sha-13dbb98.with-local-changes');
 
 /**
  * @license
