682401ce5e26729fcbde719b641418d5b22f3ce5
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 16:54:01 +0000

    d6c80871f docs: remove and update resource list (#31469)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
+ * @license Angular v8.2.0-next.1+50.sha-d6c8087.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18570,7 +18570,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+50.sha-d6c8087.with-local-changes');
 
     /**
      * @license
