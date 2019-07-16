ad903736cdd682ae07bfaec55c4e3f1dad968834
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 17:24:00 +0000

    6692c5dd1 docs: add note about current working directory for cli projects (#31507)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+52.sha-177cf26.with-local-changes
+ * @license Angular v8.1.0+53.sha-6692c5d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18441,7 +18441,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.0+52.sha-177cf26.with-local-changes');
+    var VERSION$1 = new Version('8.1.0+53.sha-6692c5d.with-local-changes');
 
     /**
      * @license
