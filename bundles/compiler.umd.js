b173b59adf9bd6b1906dd164e3b27fd7e9fb1123
Matias Niemelä <matias@yearofmoo.com>
2019-07-16 17:21:01 +0000

    8853f13f8 build(docs-infra): upgrade cli command docs sources to a8fe15cb6 (#31585)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+55.sha-3166cff.with-local-changes
+ * @license Angular v8.2.0-next.1+56.sha-8853f13.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18570,7 +18570,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.1+55.sha-3166cff.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.1+56.sha-8853f13.with-local-changes');
 
     /**
      * @license
