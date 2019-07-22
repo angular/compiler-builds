a4b4c22926a3d9f3c0c542ec6298b1c8d05ba009
Miško Hevery <misko@hevery.com>
2019-07-22 17:06:08 +0000

    8f084d721 docs: clarify toh (#28571)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+35.sha-cb848b9.with-local-changes
+ * @license Angular v8.2.0-next.2+36.sha-8f084d7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18520,7 +18520,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+35.sha-cb848b9.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+36.sha-8f084d7.with-local-changes');
 
     /**
      * @license
