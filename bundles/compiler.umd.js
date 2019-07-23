9902865b0a92fef99085f37d89c4ff3f66cdd71b
Kara Erickson <karakara@google.com>
2019-07-23 06:27:50 +0000

    0e68c7edf fix(ivy): support projecting containers created by query reads (#31790)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+45.sha-54ef63b.with-local-changes
+ * @license Angular v8.2.0-next.2+46.sha-0e68c7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18541,7 +18541,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+45.sha-54ef63b.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+46.sha-0e68c7e.with-local-changes');
 
     /**
      * @license
