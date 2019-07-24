01548fc47c935229e7a5863210f0f049355d25b5
Misko Hevery <misko@hevery.com>
2019-07-24 04:32:55 +0000

    a445826da fix(zone.js): handle new api of electron 4 (#31669)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+55.sha-f69e4e6.with-local-changes
+ * @license Angular v8.2.0-next.2+65.sha-a445826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18508,7 +18508,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.0-next.2+55.sha-f69e4e6.with-local-changes');
+var VERSION$1 = new Version('8.2.0-next.2+65.sha-a445826.with-local-changes');
 
 /**
  * @license
