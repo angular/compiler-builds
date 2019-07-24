d1a771cb65e0d99b96aa64ff474e2675b5d60b63
Miško Hevery <misko@hevery.com>
2019-07-24 21:59:46 +0000

    716af1059 fix(zone.js): move property patch to legacy (#31660)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+73.sha-87ce4e9.with-local-changes
+ * @license Angular v8.2.0-next.2+77.sha-716af10.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18564,7 +18564,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+73.sha-87ce4e9.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+77.sha-716af10.with-local-changes');
 
     /**
      * @license
