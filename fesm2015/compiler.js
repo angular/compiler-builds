3aca17241dc5b2f17aa49ce5da4e6078ea507cf7
Misko Hevery <misko@hevery.com>
2019-07-24 17:59:50 +0000

    b696413a7 fix(ivy): support attribute selectors in dynamic component creation (#31812)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+55.sha-f69e4e6.with-local-changes
+ * @license Angular v8.2.0-next.2+70.sha-b696413.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17463,7 +17463,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.0-next.2+55.sha-f69e4e6.with-local-changes');
+const VERSION$1 = new Version('8.2.0-next.2+70.sha-b696413.with-local-changes');
 
 /**
  * @license
