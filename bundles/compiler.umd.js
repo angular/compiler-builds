e9d77dc3b6eaca1e66524b015bdeea6f47842f4a
Miško Hevery <misko@hevery.com>
2019-07-24 22:06:41 +0000

    a86850e3f refactor(docs-infra): remove obsolete `format` attribute from code examples (#31674)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+83.sha-2c402d5.with-local-changes
+ * @license Angular v8.2.0-next.2+81.sha-a86850e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18564,7 +18564,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-next.2+83.sha-2c402d5.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-next.2+81.sha-a86850e.with-local-changes');
 
     /**
      * @license
