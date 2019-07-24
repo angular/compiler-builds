f46de88dd8b3d113dd28f0997b2bd97eb832ed0e
Miško Hevery <misko@hevery.com>
2019-07-24 22:18:06 +0000

    9e10d0307 fix(zone.js): handle MSPointer event correctly (#31722)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+30.sha-53286d1.with-local-changes
+ * @license Angular v8.1.2+31.sha-9e10d03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18452,7 +18452,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.2+30.sha-53286d1.with-local-changes');
+    var VERSION$1 = new Version('8.1.2+31.sha-9e10d03.with-local-changes');
 
     /**
      * @license
