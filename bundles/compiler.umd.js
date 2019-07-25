32f3cbc64c28bde8a27534afc7391ad69a2d3ab0
Misko Hevery <misko@hevery.com>
2019-07-25 23:23:46 +0000

    55fab0a5c build: lock material unit tests job to specific commit (#31569) (#31847)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+33.sha-fdf2f69.with-local-changes
+ * @license Angular v8.1.2+35.sha-55fab0a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18452,7 +18452,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.1.2+33.sha-fdf2f69.with-local-changes');
+    var VERSION$1 = new Version('8.1.2+35.sha-55fab0a.with-local-changes');
 
     /**
      * @license
