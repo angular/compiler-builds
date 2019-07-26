aafbcc92dfe13f60d0417d9dbe529782d7c36190
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:21:02 +0000

    155f40c17 docs: fix header mistake (#31811)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+3.sha-8c446b0.with-local-changes
+ * @license Angular v8.2.0-rc.0+4.sha-155f40c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17468,7 +17468,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.0-rc.0+3.sha-8c446b0.with-local-changes');
+const VERSION$1 = new Version('8.2.0-rc.0+4.sha-155f40c.with-local-changes');
 
 /**
  * @license
