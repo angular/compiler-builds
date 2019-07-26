1d1b7bb5802eeee4f794c92845ad408c64865db3
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:43:42 +0000

    397d0ba9a test(ivy): fix broken testcase in Windows (#31860)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
+ * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17468,7 +17468,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+const VERSION$1 = new Version('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
 /**
  * @license
