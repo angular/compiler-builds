5eed96e826299047da0a3881730c2ca051b3684f
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:17:11 +0000

    800e90e4e fix(zone.js): don't rely on global node typings outside of node/ directory (#31783)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+10.sha-14890e9.with-local-changes
+ * @license Angular v8.2.0-rc.0+12.sha-800e90e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18569,7 +18569,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0-rc.0+10.sha-14890e9.with-local-changes');
+    var VERSION$1 = new Version('8.2.0-rc.0+12.sha-800e90e.with-local-changes');
 
     /**
      * @license
