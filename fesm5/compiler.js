1cf43495a2ad98adc057469388a0ef14144afbde
Andrew Kushnir <akushnir@google.com>
2019-07-31 18:56:00 +0000

    aebf65d0e docs: add ReactiveConf 2019 to events (#31895) PR Close #31895

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
+ * @license Angular v8.2.0+1.sha-aebf65d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18513,7 +18513,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.0-rc.0+13.sha-03e8a31.with-local-changes');
+var VERSION$1 = new Version('8.2.0+1.sha-aebf65d.with-local-changes');
 
 /**
  * @license
