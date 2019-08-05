808f3e9a353829e44e91ad44a335654f2fc0106a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:18:40 +0000

    83d67b4a8 docs: add doc for compiler options (#31829)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+15.sha-c00e7ff.with-local-changes
+ * @license Angular v8.2.0+19.sha-83d67b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18569,7 +18569,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0+15.sha-c00e7ff.with-local-changes');
+    var VERSION$1 = new Version('8.2.0+19.sha-83d67b4.with-local-changes');
 
     /**
      * @license
