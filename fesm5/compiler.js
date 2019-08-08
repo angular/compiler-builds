35fb1b855872d650f58a2205a4724e6b57bbc36a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:33:20 +0000

    8af2cc1ef docs: use proper case in styleguide sentence (#32039)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+52.sha-29d3b68.with-local-changes
+ * @license Angular v9.0.0-next.0+54.sha-8af2cc1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18513,7 +18513,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.0+52.sha-29d3b68.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.0+54.sha-8af2cc1.with-local-changes');
 
 /**
  * @license
