9eeb49bfa565ddf09156a8c06e121570e8910a9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 20:09:48 +0000

    cfaefa996 fix(ivy): set LOCALE_ID when using the injector (#31983)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+33.sha-a48376b.with-local-changes
+ * @license Angular v8.2.0+34.sha-cfaefa9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18569,7 +18569,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.0+33.sha-a48376b.with-local-changes');
+    var VERSION$1 = new Version('8.2.0+34.sha-cfaefa9.with-local-changes');
 
     /**
      * @license
