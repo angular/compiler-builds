f4795c172e8586b2452bb7b42a4861106ac55c5a
Kara Erickson <karakara@google.com>
2019-08-09 18:06:06 +0000

    695f322dc docs: clarify pipe naming (#31806)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+7.sha-9896d43.with-local-changes
+ * @license Angular v9.0.0-next.1+5.sha-695f322.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18513,7 +18513,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.1+7.sha-9896d43.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.1+5.sha-695f322.with-local-changes');
 
 /**
  * @license
