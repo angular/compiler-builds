8751e7533a35e1fc69d751bf652db6815e1b5806
Kara Erickson <karakara@google.com>
2019-08-10 00:47:59 +0000

    753080133 Revert "docs(forms): use a number as input value for formControlName (#30606)" (#32088)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+17.sha-3df54be.with-local-changes
+ * @license Angular v9.0.0-next.1+19.sha-7530801.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18661,7 +18661,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.1+17.sha-3df54be.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.1+19.sha-7530801.with-local-changes');
 
 /**
  * @license
