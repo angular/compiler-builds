3f6346390c1a8baf29481a85a3e190690c7d4174
Kara Erickson <karakara@google.com>
2019-08-12 14:29:29 +0000

    7b9414565 style: fix inline comment typo (#32090)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+20.sha-37de490.with-local-changes
+ * @license Angular v9.0.0-next.1+21.sha-7b94145.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18830,7 +18830,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.1+20.sha-37de490.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.1+21.sha-7b94145.with-local-changes');
 
     /**
      * @license
