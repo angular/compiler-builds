b183d385551d228e2fbfe1ff929e3e0146b534da
Kara Erickson <karakara@google.com>
2019-08-12 23:29:18 +0000

    48487cb70 ci: add ivy commits to generated CHANGELOG (#32114)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+1.sha-c68371e.with-local-changes
+ * @license Angular v9.0.0-next.2+2.sha-48487cb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+1.sha-c68371e.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+2.sha-48487cb.with-local-changes');
 
     /**
      * @license
