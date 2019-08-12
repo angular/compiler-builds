ba29cd7cc41c4a5c65d912049617e97a2261eca7
Kara Erickson <karakara@google.com>
2019-08-12 20:50:39 +0000

    914900a56 refactor(ivy): remove load instruction (#32067)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+23.sha-4ea3e7e.with-local-changes
+ * @license Angular v9.0.0-next.1+24.sha-914900a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3466,7 +3466,6 @@ var Identifiers$1 = /** @class */ (function () {
     Identifiers.i18nApply = { name: 'ɵɵi18nApply', moduleName: CORE$1 };
     Identifiers.i18nPostprocess = { name: 'ɵɵi18nPostprocess', moduleName: CORE$1 };
     Identifiers.i18nLocalize = { name: 'ɵɵi18nLocalize', moduleName: CORE$1 };
-    Identifiers.load = { name: 'ɵɵload', moduleName: CORE$1 };
     Identifiers.pipe = { name: 'ɵɵpipe', moduleName: CORE$1 };
     Identifiers.projection = { name: 'ɵɵprojection', moduleName: CORE$1 };
     Identifiers.projectionDef = { name: 'ɵɵprojectionDef', moduleName: CORE$1 };
@@ -18660,7 +18659,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.1+23.sha-4ea3e7e.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.1+24.sha-914900a.with-local-changes');
 
 /**
  * @license
