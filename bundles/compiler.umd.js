1dea4b432d653c96ac5cd7aa7c7ae2b0216d9922
Kara Erickson <karakara@google.com>
2019-08-12 21:06:31 +0000

    c0194e011 docs: release notes for the v8.2.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+24.sha-914900a.with-local-changes
+ * @license Angular v9.0.0-next.1+25.sha-c0194e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.1+24.sha-914900a.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.1+25.sha-c0194e0.with-local-changes');
 
     /**
      * @license
