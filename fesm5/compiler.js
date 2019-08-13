291f53e1555aa1d2086a289f5b55f1ea1cd01d29
Kara Erickson <karakara@google.com>
2019-08-13 18:41:39 +0000

    a5f39aeda refactor(language-service): Return ts.Diagnostic[] for getDiagnostics (#32115)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+8.sha-a91ab15.with-local-changes
+ * @license Angular v9.0.0-next.2+9.sha-a5f39ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18659,7 +18659,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.2+8.sha-a91ab15.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.2+9.sha-a5f39ae.with-local-changes');
 
 /**
  * @license
