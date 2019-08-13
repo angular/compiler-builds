daf27d369d0f31441d69d4125ab41d5c4ff88bba
Kara Erickson <karakara@google.com>
2019-08-13 17:12:16 +0000

    ebcf79d7f docs(zone.js): update DEVELOPER.md for changelog instruction (#32016)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+2.sha-48487cb.with-local-changes
+ * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+2.sha-48487cb.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+2.sha-48487cb.with-local-changes
+ * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17596,7 +17596,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.2+2.sha-48487cb.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
 
 /**
  * @license
