c657384572e6a1e34dc7d5cf5e3a1c255a5a8158
Kara Erickson <karakara@google.com>
2019-08-13 19:28:18 +0000

    3d1b82be6 docs: correct description of animation example (#32009)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+8.sha-7b3bd21.with-local-changes
+ * @license Angular v8.2.2+5.sha-3d1b82b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18648,7 +18648,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.2+8.sha-7b3bd21.with-local-changes');
+var VERSION$1 = new Version('8.2.2+5.sha-3d1b82b.with-local-changes');
 
 /**
  * @license
