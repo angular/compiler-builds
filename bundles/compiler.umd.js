c65d93f17fccedbb8135709d36e62874b8142da6
Andrew Kushnir <akushnir@google.com>
2019-08-14 21:31:43 +0000

    d6bbc4d76 docs(router): fix router description (#32136)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+31.sha-5a562d8.with-local-changes
+ * @license Angular v9.0.0-next.2+30.sha-d6bbc4d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+31.sha-5a562d8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+30.sha-d6bbc4d.with-local-changes');
 
     /**
      * @license
