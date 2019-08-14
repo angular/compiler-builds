6e2992f1c9625156736446f6bd2319ef80d504e9
Andrew Kushnir <akushnir@google.com>
2019-08-14 19:21:25 +0000

    9e6c67713 test(language-service): Remove test for external template (#32017)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+24.sha-8438811.with-local-changes
+ * @license Angular v9.0.0-next.2+23.sha-9e6c677.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17596,7 +17596,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.2+24.sha-8438811.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.2+23.sha-9e6c677.with-local-changes');
 
 /**
  * @license
