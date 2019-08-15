2a3b280c115c89ba7e1e7fa16b3f706b84d06b0e
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:05:12 +0000

    373d9660d docs: fix typo of Typescript to TypeScript (#32153)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+41.sha-1c65161.with-local-changes
+ * @license Angular v9.0.0-next.2+42.sha-373d966.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17596,7 +17596,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.2+41.sha-1c65161.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.2+42.sha-373d966.with-local-changes');
 
 /**
  * @license
