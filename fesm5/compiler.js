9e750e466af3871917ae7a45600ed560a91b29f5
Andrew Kushnir <akushnir@google.com>
2019-08-15 22:25:08 +0000

    4c3b791ff perf(ivy): avoid first template pass checks during view creation (#32120)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+46.sha-964d726.with-local-changes
+ * @license Angular v9.0.0-next.2+47.sha-4c3b791.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18659,7 +18659,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.2+46.sha-964d726.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.2+47.sha-4c3b791.with-local-changes');
 
 /**
  * @license
