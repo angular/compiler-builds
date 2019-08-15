665995772469b7986798eee93c37dad8078fa950
Andrew Kushnir <akushnir@google.com>
2019-08-15 22:26:42 +0000

    4c3b791ff perf(ivy): avoid first template pass checks during view creation (#32120)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+46.sha-964d726.with-local-changes
+ * @license Angular v9.0.0-next.2+47.sha-4c3b791.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+46.sha-964d726.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+47.sha-4c3b791.with-local-changes');
 
     /**
      * @license
