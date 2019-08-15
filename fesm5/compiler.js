f7cd5bacded9660ef27e8cd7d13687fd63853bb0
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:05:45 +0000

    699c705a8 docs: clarify hierarchical injectors (#28700)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+19.sha-6f2e8af.with-local-changes
+ * @license Angular v8.2.2+18.sha-699c705.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18648,7 +18648,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.2+19.sha-6f2e8af.with-local-changes');
+var VERSION$1 = new Version('8.2.2+18.sha-699c705.with-local-changes');
 
 /**
  * @license
