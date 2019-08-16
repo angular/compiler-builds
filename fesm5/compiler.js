6abe14029d6062e537c9f713cb565188a73f1a7c
Andrew Kushnir <akushnir@google.com>
2019-08-16 22:46:27 +0000

    5f76de1d7 test(language-service): Fix diagnostic tests (#32161)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+50.sha-abb44f7.with-local-changes
+ * @license Angular v9.0.0-next.2+52.sha-5f76de1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18659,7 +18659,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.2+50.sha-abb44f7.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.2+52.sha-5f76de1.with-local-changes');
 
 /**
  * @license
