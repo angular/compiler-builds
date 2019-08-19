d70bc928d8f99a81b48d8543d673c35bba2fed01
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:35:29 +0000

    994264c0b refactor(ivy): simplify `walkTNodeTree` method for readability (#31065)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+55.sha-e85ec23.with-local-changes
+ * @license Angular v9.0.0-next.2+58.sha-994264c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18828,7 +18828,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+55.sha-e85ec23.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+58.sha-994264c.with-local-changes');
 
     /**
      * @license
