cc0bc60747e2b14e9bc763658d9791775f5fe944
Andrew Kushnir <akushnir@google.com>
2019-08-20 23:58:42 +0000

    388578fec Revert "ci: remove material-unit-tests failure blocklist (#32138)" (#32226)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+70.sha-dd6070a.with-local-changes
+ * @license Angular v9.0.0-next.2+71.sha-388578f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17593,7 +17593,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.2+70.sha-dd6070a.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.2+71.sha-388578f.with-local-changes');
 
 /**
  * @license
