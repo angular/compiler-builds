d28704188b30cb613d6e50e2c2c9f20ce9d10d00
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:34:01 +0000

    cfed0c0cf fix(ivy): Support selector-less directive as base classes (#32125)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+66.sha-f8b995d.with-local-changes
+ * @license Angular v9.0.0-next.2+68.sha-cfed0c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17900,9 +17900,6 @@
         var _a = baseDirectiveFields(meta, constantPool, bindingParser), definitionMap = _a.definitionMap, statements = _a.statements;
         addFeatures(definitionMap, meta);
         var expression = importExpr(Identifiers$1.defineDirective).callFn([definitionMap.toLiteralMap()]);
-        if (!meta.selector) {
-            throw new Error("Directive " + meta.name + " has no selector, please add it!");
-        }
         var type = createTypeForDef(meta, Identifiers$1.DirectiveDefWithMeta);
         return { expression: expression, type: type, statements: statements };
     }
@@ -18828,7 +18825,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.2+66.sha-f8b995d.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.2+68.sha-cfed0c0.with-local-changes');
 
     /**
      * @license
