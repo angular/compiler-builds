9be6646bdfedbd577dfac2d92766998af8955535
atscott <atscott@google.com>
2019-08-21 22:41:15 +0000

    5c94833b8 test(language-service): Add test for CRLF line endings (#32245)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+1.sha-02a567e.with-local-changes
+ * @license Angular v9.0.0-next.3+4.sha-5c94833.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18664,7 +18664,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.3+1.sha-02a567e.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.3+4.sha-5c94833.with-local-changes');
 
 /**
  * @license
