a877bc7cdd76078ee58c7ef365180a339e9d8755
atscott <atscott@google.com>
2019-08-21 22:56:26 +0000

    a84b5ef81 build: use bazel workers for ngc and tsc in local development (#32246)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+3.sha-b85ac03.with-local-changes
+ * @license Angular v9.0.0-next.3+5.sha-a84b5ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18833,7 +18833,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+3.sha-b85ac03.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+5.sha-a84b5ef.with-local-changes');
 
     /**
      * @license
