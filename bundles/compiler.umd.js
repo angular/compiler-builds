c6828482ca52066e88f7c7c73ecd8e6edcf50277
atscott <atscott@google.com>
2019-08-21 22:42:14 +0000

    b85ac0313 docs(upgrade): position old setup guide as legacy until it can be removed (#32193)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+2.sha-7fed0fa.with-local-changes
+ * @license Angular v9.0.0-next.3+3.sha-b85ac03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18833,7 +18833,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+2.sha-7fed0fa.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+3.sha-b85ac03.with-local-changes');
 
     /**
      * @license
