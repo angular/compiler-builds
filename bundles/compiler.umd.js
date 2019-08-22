68f56105a6431928244405ee2c68293e328f5503
atscott <atscott@google.com>
2019-08-22 19:51:42 +0000

    6d1115465 refactor(language-service): Remove redudant 'TemplateInfo' type (#32250)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+12.sha-6b245a3.with-local-changes
+ * @license Angular v9.0.0-next.3+13.sha-6d11154.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18833,7 +18833,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.3+12.sha-6b245a3.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.3+13.sha-6d11154.with-local-changes');
 
     /**
      * @license
