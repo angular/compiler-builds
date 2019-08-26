08f5039fafda97f239749a509312fbf037c4a24e
atscott <atscott@google.com>
2019-08-26 16:52:23 +0000

    0fb78d6c9 build(docs-infra): upgrade cli command docs sources to ea386e045 (#32298)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+13.sha-0878d67.with-local-changes
+ * @license Angular v8.2.3+15.sha-0fb78d6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.3+13.sha-0878d67.with-local-changes');
+    var VERSION$1 = new Version('8.2.3+15.sha-0fb78d6.with-local-changes');
 
     /**
      * @license
