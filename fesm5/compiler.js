db137c53dab6967ee3d39a423e2c9d2b29f3ae97
atscott <atscott@google.com>
2019-08-27 16:27:54 +0000

    f57c17de2 build: remove the deprecated `aio` commit message scope (#32341)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
+ * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18664,7 +18664,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
 /**
  * @license
