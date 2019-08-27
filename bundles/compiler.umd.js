264630efbc84a3a8c7d12f89b064739adc85b31f
atscott <atscott@google.com>
2019-08-27 16:24:41 +0000

    73dd43170 build: remove the deprecated `aio` commit message scope (#32341)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+20.sha-514271b.with-local-changes
+ * @license Angular v8.2.3+21.sha-73dd431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.3+20.sha-514271b.with-local-changes');
+    var VERSION$1 = new Version('8.2.3+21.sha-73dd431.with-local-changes');
 
     /**
      * @license
