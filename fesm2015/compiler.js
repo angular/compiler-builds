7a7ea01b8c5b2c40dbd59cdf528e129f0a68cab4
Miško Hevery <misko@hevery.com>
2019-08-29 00:32:01 +0000

    60a056d5d refactor(core): undecorated classes migration should not decorate classes if not needed (#32319)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+12.sha-581b837.with-local-changes
+ * @license Angular v9.0.0-next.4+11.sha-60a056d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17635,7 +17635,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.4+12.sha-581b837.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.4+11.sha-60a056d.with-local-changes');
 
 /**
  * @license
