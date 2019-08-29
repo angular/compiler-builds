c7ec514aaa36acfa7f8b35b120e018df3b638f5c
Miško Hevery <misko@hevery.com>
2019-08-29 19:51:34 +0000

    d5101dff3 fix(ivy): ngcc - improve the "ngcc version changed" error message (#32396)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+30.sha-63dff9c.with-local-changes
+ * @license Angular v9.0.0-next.4+31.sha-d5101df.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18867,7 +18867,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.4+30.sha-63dff9c.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.4+31.sha-d5101df.with-local-changes');
 
     /**
      * @license
