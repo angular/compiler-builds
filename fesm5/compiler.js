0ce26b732a7f9ccf96d661eb6212600e0f5688ca
Miško Hevery <misko@hevery.com>
2019-08-29 00:47:00 +0000

    18ce58c2b refactor(language-service): cleanup tests for Hover (#32378)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+17.sha-97fc45f.with-local-changes
+ * @license Angular v9.0.0-next.4+21.sha-18ce58c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18699,7 +18699,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.4+17.sha-97fc45f.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.4+21.sha-18ce58c.with-local-changes');
 
 /**
  * @license
