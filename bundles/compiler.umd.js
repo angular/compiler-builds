3c706ddfe8688edee0f2221c937d816217f0a1fe
Miško Hevery <misko@hevery.com>
2019-08-29 00:44:40 +0000

    97fc45f32 fix(language-service): Invalidate Reflector caches when program changes (#32357)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+19.sha-d4703d9.with-local-changes
+ * @license Angular v9.0.0-next.4+17.sha-97fc45f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18868,7 +18868,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.4+19.sha-d4703d9.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.4+17.sha-97fc45f.with-local-changes');
 
     /**
      * @license
