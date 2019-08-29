2f1d822ed3febc2194c44e53df3d4bf33e1745d0
Miško Hevery <misko@hevery.com>
2019-08-29 00:44:33 +0000

    d4703d931 refactor(ivy): remove global state access from inputs-related functions (#32370)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+16.sha-4b12511.with-local-changes
+ * @license Angular v9.0.0-next.4+19.sha-d4703d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18699,7 +18699,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.4+16.sha-4b12511.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.4+19.sha-d4703d9.with-local-changes');
 
 /**
  * @license
