cbe2b12ea6fdedc0bc64da63fabedd56d26a0c3d
Misko Hevery <misko@hevery.com>
2019-08-30 05:09:18 +0000

    1537791f0 perf(core): Make `PlatformLocation` tree-shakable (#32154)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+39.sha-3758978.with-local-changes
+ * @license Angular v9.0.0-next.4+44.sha-1537791.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18698,7 +18698,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.4+39.sha-3758978.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.4+44.sha-1537791.with-local-changes');
 
 /**
  * @license
