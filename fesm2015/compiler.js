f69ed5ca0db855244ffe38c5594e180ec8e3eeac
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:58:37 +0000

    43619fc84 revert: docs: create Issue and Pull Request markdown doc, explaining automatic locking policy (#32405)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+51.sha-ad13520.with-local-changes
+ * @license Angular v9.0.0-next.4+52.sha-43619fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17634,7 +17634,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.4+51.sha-ad13520.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.4+52.sha-43619fc.with-local-changes');
 
 /**
  * @license
