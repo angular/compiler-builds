698275306732a43b183c8f9033f3f6b9b6cd3afd
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:21:40 +0000

    350ea47de feat(bazel): update to the latest `@microsoft/api-extractor` (#32185)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+46.sha-ba5e07e.with-local-changes
+ * @license Angular v9.0.0-next.4+50.sha-350ea47.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18867,7 +18867,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.4+46.sha-ba5e07e.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.4+50.sha-350ea47.with-local-changes');
 
     /**
      * @license
