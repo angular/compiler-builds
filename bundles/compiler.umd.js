c1b3b83bb5ffc5a0b013e1fd1dcc85d4cda122fc
Misko Hevery <misko@hevery.com>
2019-08-30 20:04:09 +0000

    b5f85638f fix(zone.js): a path traversal attack in test (#32392)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+9.sha-efd13d3.with-local-changes
+ * @license Angular v8.2.4+10.sha-b5f8563.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.4+9.sha-efd13d3.with-local-changes');
+    var VERSION$1 = new Version('8.2.4+10.sha-b5f8563.with-local-changes');
 
     /**
      * @license
