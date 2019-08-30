8377002ceffe958b247f8370a3c62158442165f2
Misko Hevery <misko@hevery.com>
2019-08-30 20:11:58 +0000

    60f963986 ci: update codeowners for docs guides/examples/images (#32360)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+56.sha-c56c241.with-local-changes
+ * @license Angular v9.0.0-next.4+59.sha-60f9639.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17634,7 +17634,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.4+56.sha-c56c241.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.4+59.sha-60f9639.with-local-changes');
 
 /**
  * @license
