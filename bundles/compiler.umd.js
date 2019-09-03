78fea5324e3bae834fbf1142c5f6e62232fc1b2d
Miško Hevery <misko@hevery.com>
2019-09-03 21:17:24 +0000

    04d4fea3e docs: correct @ngtools/webpack url (#32407)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+78.sha-89434e0.with-local-changes
+ * @license Angular v9.0.0-next.4+79.sha-04d4fea.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19046,7 +19046,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.4+78.sha-89434e0.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.4+79.sha-04d4fea.with-local-changes');
 
     /**
      * @license
