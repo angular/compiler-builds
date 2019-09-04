e37f4fcebe0154ab9fd3ebf944617555ec0e4e94
Miško Hevery <misko@hevery.com>
2019-09-04 21:03:42 +0000

    8be20f3a3 build: fix incorrect formating which caused a broken build

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+19.sha-5112669.with-local-changes
+ * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.4+19.sha-5112669.with-local-changes');
+    var VERSION$1 = new Version('8.2.4+20.sha-8be20f3.with-local-changes');
 
     /**
      * @license
