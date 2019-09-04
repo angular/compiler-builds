7de20e43d0141f2631af6fb6978c4fd2ec779d44
Miško Hevery <misko@hevery.com>
2019-09-04 18:59:24 +0000

    cfa09b84d docs: fix typo in structural directive example (#32459) Fixes #32458

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+80.sha-e213080.with-local-changes
+ * @license Angular v9.0.0-next.4+81.sha-cfa09b8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18877,7 +18877,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.4+80.sha-e213080.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.4+81.sha-cfa09b8.with-local-changes');
 
 /**
  * @license
