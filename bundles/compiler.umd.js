66bbb80cc5d22bf4fec84a2e6c0d432f8750ad3d
Miško Hevery <misko@hevery.com>
2019-09-04 19:12:12 +0000

    01e0f58dd docs: Updating the Ignite UI for Angular url (#32417)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+82.sha-a383a5a.with-local-changes
+ * @license Angular v9.0.0-next.4+85.sha-01e0f58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19046,7 +19046,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.4+82.sha-a383a5a.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.4+85.sha-01e0f58.with-local-changes');
 
     /**
      * @license
