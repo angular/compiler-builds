f961a22728a41ba3cbe3f58448a2cd5e0f664fb9
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:01 +0000

    896a8a441 docs: added note in animation keyframe offset (#32350)

--- bundles/compiler-testing.umd.min.js
+++ bundles/compiler-testing.umd.min.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+20.sha-8be20f3.with-local-changes
+ * @license Angular v8.2.5+3.sha-896a8a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
