a7f0f95d2dc0a34cc614f889f91904145ebf5a4c
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:31:00 +0000

    62d92f809 fix(ivy): unable to bind to properties that start with class or style (#32421)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+20.sha-497d6b1.with-local-changes
+ * @license Angular v9.0.0-next.5+19.sha-62d92f8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18885,7 +18885,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.5+20.sha-497d6b1.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.5+19.sha-62d92f8.with-local-changes');
 
 /**
  * @license
