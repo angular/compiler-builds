639abd0f371b847bff05ffd629dafbeb0be13757
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 22:31:01 +0000

    da42a7648 fix(ivy): node placed in incorrect order inside ngFor with ng-container (#32324)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+18.sha-098feec.with-local-changes
+ * @license Angular v9.0.0-next.5+16.sha-da42a76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19053,7 +19053,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.5+18.sha-098feec.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.5+16.sha-da42a76.with-local-changes');
 
     /**
      * @license
