f8a1ca03f5810af8cb0d0c87aafad742f25fda89
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 23:07:05 +0000

    a9ff48e67 fix(core): improve the "missing `$localize`" error message (#32491)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+16.sha-da42a76.with-local-changes
+ * @license Angular v9.0.0-next.5+17.sha-a9ff48e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17803,7 +17803,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.5+16.sha-da42a76.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.5+17.sha-a9ff48e.with-local-changes');
 
 /**
  * @license
