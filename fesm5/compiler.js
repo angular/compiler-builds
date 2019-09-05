f854b5a182fa52d4c1731fd852c5d226a572a283
Matias Niemelä <matias@yearofmoo.com>
2019-09-05 17:53:06 +0000

    36d613dd6 docs: update default path for xi18n (#32480)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+6.sha-094538c.with-local-changes
+ * @license Angular v9.0.0-next.5+11.sha-36d613d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18888,7 +18888,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.5+6.sha-094538c.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.5+11.sha-36d613d.with-local-changes');
 
 /**
  * @license
