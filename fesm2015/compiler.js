11391865935ff452fc0f5bce57ffc773973e0aba
Matias Niemelä <matias@yearofmoo.com>
2019-09-06 17:39:06 +0000

    bc061b78b fix(ivy): warn instead of throwing for unknown properties (#32463)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+21.sha-4c3674f.with-local-changes
+ * @license Angular v9.0.0-next.5+22.sha-bc061b7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17804,7 +17804,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.5+21.sha-4c3674f.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.5+22.sha-bc061b7.with-local-changes');
 
 /**
  * @license
