3aa757f48ab5b285fb1b35d5bb980d14b3c3d3b4
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 23:29:34 +0000

    9166baf70 refactor(core): Migrate TestBed.get to TestBed.inject (#32382)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+46.sha-a391aeb.with-local-changes
+ * @license Angular v9.0.0-next.5+48.sha-9166baf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19054,7 +19054,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.5+46.sha-a391aeb.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.5+48.sha-9166baf.with-local-changes');
 
     /**
      * @license
