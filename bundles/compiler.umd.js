5c3f9df58ffe9e1ebff53fafb8ffcf1eb3afe327
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:32:46 +0000

    38f4dcd5e docs: port accessibility app from stackblitz to aio (#32377)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+16.sha-6e380cf.with-local-changes
+ * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.5+16.sha-6e380cf.with-local-changes');
+    var VERSION$1 = new Version('8.2.5+17.sha-38f4dcd.with-local-changes');
 
     /**
      * @license
