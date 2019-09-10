72b5312be0d5f97a394431a3ab980e4afab560ea
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:46:40 +0000

    2565f6795 docs: add mdb angular boilerplate to docs resources (#30448)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
+ * @license Angular v8.2.5+18.sha-2565f67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18648,7 +18648,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.5+17.sha-38f4dcd.with-local-changes');
+var VERSION$1 = new Version('8.2.5+18.sha-2565f67.with-local-changes');
 
 /**
  * @license
