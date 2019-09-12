7803b7facbf36d2c0be3b9e7c82474cd0b271167
Kara Erickson <karakara@google.com>
2019-09-12 20:24:26 +0000

    5a830c49c refactor(ivy): improve micro-benchmark profiling (#32647)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+36.sha-bd67958.with-local-changes
+ * @license Angular v9.0.0-next.6+37.sha-5a830c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17808,7 +17808,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.6+36.sha-bd67958.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.6+37.sha-5a830c4.with-local-changes');
 
 /**
  * @license
