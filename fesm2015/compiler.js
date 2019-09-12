2401494fe11bc60975c155d89a2f0eb1d6f98989
Kara Erickson <karakara@google.com>
2019-09-12 17:52:43 +0000

    1771d6ff2 fix(language-service): Lazily instantiate MetadataResolver (#32631)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+27.sha-527ce3b.with-local-changes
+ * @license Angular v9.0.0-next.6+21.sha-1771d6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17808,7 +17808,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.6+27.sha-527ce3b.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
 
 /**
  * @license
