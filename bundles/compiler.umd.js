344bae977128611ba338123e61b6759738135a71
Kara Erickson <karakara@google.com>
2019-09-12 19:37:24 +0000

    581b99143 docs: add undecorated classes migration faq (#32478)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+9.sha-4014aab.with-local-changes
+ * @license Angular v8.2.6+10.sha-581b991.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.6+9.sha-4014aab.with-local-changes');
+    var VERSION$1 = new Version('8.2.6+10.sha-581b991.with-local-changes');
 
     /**
      * @license
