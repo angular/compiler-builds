6356af9a582bc5a1f8fce95153cde6b6cd3bbb77
Kara Erickson <karakara@google.com>
2019-09-13 17:34:30 +0000

    bfb399586 fix(ivy): DebugNode throws exceptions when querying some properties (#32622)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+48.sha-5328bb2.with-local-changes
+ * @license Angular v9.0.0-next.6+49.sha-bfb3995.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18893,7 +18893,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.6+48.sha-5328bb2.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.6+49.sha-bfb3995.with-local-changes');
 
 /**
  * @license
