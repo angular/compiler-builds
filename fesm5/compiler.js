28055359c6e8fb7c086d22601381d48fc214b709
Kara Erickson <karakara@google.com>
2019-09-13 16:16:23 +0000

    e1065eec5 refactor(language-service): let -> const (#32668)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+45.sha-082b0d3.with-local-changes
+ * @license Angular v9.0.0-next.6+46.sha-e1065ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18889,7 +18889,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.6+45.sha-082b0d3.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.6+46.sha-e1065ee.with-local-changes');
 
 /**
  * @license
