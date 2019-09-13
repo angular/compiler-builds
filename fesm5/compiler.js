1ad875dc874a6a5443ccd6f8a743a51b9db78c4b
Kara Erickson <karakara@google.com>
2019-09-13 16:09:35 +0000

    c442c0334 refactor(language-service): remove non-null assertion on MockHost member (#32666)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+42.sha-bbb2798.with-local-changes
+ * @license Angular v9.0.0-next.6+44.sha-c442c03.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18889,7 +18889,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.6+42.sha-bbb2798.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.6+44.sha-c442c03.with-local-changes');
 
 /**
  * @license
