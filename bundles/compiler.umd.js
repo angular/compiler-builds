2ea0a8243c12a1de67b598074e767fd89011a0a9
Kara Erickson <karakara@google.com>
2019-09-16 18:07:25 +0000

    f213c7a64 docs: add autoprefixer backward compatability instruction (#32109)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.6+13.sha-947c076.with-local-changes
+ * @license Angular v8.2.6+14.sha-f213c7a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.6+13.sha-947c076.with-local-changes');
+    var VERSION$1 = new Version('8.2.6+14.sha-f213c7a.with-local-changes');
 
     /**
      * @license
