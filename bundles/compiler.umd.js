027af26b4f4a1ab9b53846480dcc793a34e475d3
Andrew Kushnir <akushnir@google.com>
2019-09-17 20:52:01 +0000

    a0d04c628 fix(ivy): ensure that `window.ng` utilities are published when NgModules are used (#32725)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+71.sha-b741a1c.with-local-changes
+ * @license Angular v9.0.0-next.6+72.sha-a0d04c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19037,7 +19037,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.6+71.sha-b741a1c.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.6+72.sha-a0d04c6.with-local-changes');
 
     /**
      * @license
