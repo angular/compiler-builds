a98c3fa9fca3adaff442e4bebc4f4538613e73df
Andrew Kushnir <akushnir@google.com>
2019-09-19 21:40:46 +0000

    04ddcfecd docs: edit copy on getting started step 1 (#32773)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+11.sha-f3859ff.with-local-changes
+ * @license Angular v9.0.0-next.7+12.sha-04ddcfe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19037,7 +19037,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.7+11.sha-f3859ff.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.7+12.sha-04ddcfe.with-local-changes');
 
     /**
      * @license
