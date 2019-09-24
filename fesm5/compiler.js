ca60668149d4b965e5b6261ec4448c14bb35a70e
Andrew Kushnir <akushnir@google.com>
2019-09-24 21:22:02 +0000

    104c786b9 docs: removed duplicated spaces in upgrade guide (#32813)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.7+3.sha-e63390d.with-local-changes
+ * @license Angular v8.2.7+5.sha-104c786.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18648,7 +18648,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.7+3.sha-e63390d.with-local-changes');
+var VERSION$1 = new Version('8.2.7+5.sha-104c786.with-local-changes');
 
 /**
  * @license
