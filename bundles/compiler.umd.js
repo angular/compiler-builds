0777828a40ff764535925ab16ef104175fdfdd5b
Andrew Kushnir <akushnir@google.com>
2019-09-25 18:52:31 +0000

    0ea4875b1 fix(ngcc): make the build-marker error more clear (#32712)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+4.sha-c134646.with-local-changes
+ * @license Angular v9.0.0-next.8+5.sha-0ea4875.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19037,7 +19037,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.8+4.sha-c134646.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.8+5.sha-0ea4875.with-local-changes');
 
     /**
      * @license
