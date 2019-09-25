be1d8f8ead178fb767059117c75afe112f41ebc9
Andrew Kushnir <akushnir@google.com>
2019-09-25 17:49:12 +0000

    4dddee742 docs: release notes for the v8.2.8 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+25.sha-39bc6f7.with-local-changes
+ * @license Angular v9.0.0-next.7+26.sha-4dddee7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18868,7 +18868,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.7+25.sha-39bc6f7.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.7+26.sha-4dddee7.with-local-changes');
 
 /**
  * @license
