4307078b0da26ef562eead5733b077f755466cf7
atscott <atscott@google.com>
2019-09-27 23:19:40 +0000

    877f9ad7b docs: add missing space between "Component" and "will" in guide (#32885)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+19.sha-a23ee2b.with-local-changes
+ * @license Angular v9.0.0-next.8+20.sha-877f9ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17794,7 +17794,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.8+19.sha-a23ee2b.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.8+20.sha-877f9ad.with-local-changes');
 
 /**
  * @license
