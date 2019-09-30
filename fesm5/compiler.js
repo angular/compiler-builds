f8a72241951e705bae7316b22b8159d30dfa9ddd
atscott <atscott@google.com>
2019-09-30 21:29:35 +0000

    002a97d85 fix(ngcc): ensure private exports are added for `ModuleWithProviders` (#32902)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+23.sha-f438ae8.with-local-changes
+ * @license Angular v9.0.0-next.8+24.sha-002a97d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18875,7 +18875,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.8+23.sha-f438ae8.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.8+24.sha-002a97d.with-local-changes');
 
 /**
  * @license
