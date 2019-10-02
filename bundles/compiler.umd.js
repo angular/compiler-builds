fe5851e4aba8c8ee46f0ee5c99541a479e3c42f6
atscott <atscott01@gmail.com>
2019-10-02 20:42:01 +0000

    954b8e53a refactor(bazel): ng_module action description should include compile mode (#32955)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+2.sha-a3b6b10.with-local-changes
+ * @license Angular v9.0.0-next.9+3.sha-954b8e5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19060,7 +19060,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.9+2.sha-a3b6b10.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.9+3.sha-954b8e5.with-local-changes');
 
     /**
      * @license
