33f921d40e7d93f71b85f1cf593e4202261b120e
atscott <atscott01@gmail.com>
2019-10-02 18:17:26 +0000

    d96167fc5 docs: add lib build note (#32940)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+22.sha-57457fb.with-local-changes
+ * @license Angular v8.2.8+24.sha-d96167f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17585,7 +17585,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.8+22.sha-57457fb.with-local-changes');
+const VERSION$1 = new Version('8.2.8+24.sha-d96167f.with-local-changes');
 
 /**
  * @license
