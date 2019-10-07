4d4c4e8cb8fd9e5a29c3b6113173a304057ee648
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:44:13 +0000

    40d87dd80 docs: fix accessibility lint rules (#32661)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+62.sha-0ce8621.with-local-changes
+ * @license Angular v9.0.0-next.9+63.sha-40d87dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19119,7 +19119,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.9+62.sha-0ce8621.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.9+63.sha-40d87dd.with-local-changes');
 
     /**
      * @license
