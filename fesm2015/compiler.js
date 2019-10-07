be466da861ad53c5b085eedc1b8dbdd1dbac4e69
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:27:48 +0000

    0ce862119 docs: add dotnettricks to training (#32771)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+60.sha-c1bb886.with-local-changes
+ * @license Angular v9.0.0-next.9+62.sha-0ce8621.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17869,7 +17869,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.9+60.sha-c1bb886.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.9+62.sha-0ce8621.with-local-changes');
 
 /**
  * @license
