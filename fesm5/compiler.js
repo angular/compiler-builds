5da96560c13d5d229b6820162efe8f10c6bf75c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 18:15:38 +0000

    393398e6f build(docs-infra): update in-memory-we-api and karma-jasmine-html-reporter version (#32892)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+55.sha-e007918.with-local-changes
+ * @license Angular v9.0.0-next.9+56.sha-393398e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18950,7 +18950,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.9+55.sha-e007918.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.9+56.sha-393398e.with-local-changes');
 
 /**
  * @license
