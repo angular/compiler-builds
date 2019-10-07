94b78d34a94ced296714385913927375effd126e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 20:37:29 +0000

    c507dda21 docs: correct sentence in AoT compiler guide (#33020)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+63.sha-40d87dd.with-local-changes
+ * @license Angular v9.0.0-next.9+64.sha-c507dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17869,7 +17869,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.9+63.sha-40d87dd.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.9+64.sha-c507dda.with-local-changes');
 
 /**
  * @license
