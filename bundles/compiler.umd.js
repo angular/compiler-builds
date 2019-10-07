1aabd235c2ffe3a152a19870559b125ee5c27cdd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 17:44:36 +0000

    bad343433 fix(ivy): avoid exposing `ng` with Closure Compiler enhanced optimizations (#33010)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+52.sha-d18289f.with-local-changes
+ * @license Angular v9.0.0-next.9+51.sha-bad3434.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19119,7 +19119,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.9+52.sha-d18289f.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.9+51.sha-bad3434.with-local-changes');
 
     /**
      * @license
