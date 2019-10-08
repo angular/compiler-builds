4dc0e5c25b88ce83269fa28acdc11a902d2104b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 16:50:10 +0000

    64823f561 build: add history-server to @angular/bazel schematics (#32889)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+65.sha-2265cb5.with-local-changes
+ * @license Angular v9.0.0-next.9+71.sha-64823f5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19119,7 +19119,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.9+65.sha-2265cb5.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.9+71.sha-64823f5.with-local-changes');
 
     /**
      * @license
