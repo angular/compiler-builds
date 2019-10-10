6a3462cf0c7baf728490c8c3e3b8e0be0bf0e3f3
Miško Hevery <misko@hevery.com>
2019-10-10 22:34:34 +0000

    1ae77da60 docs: update Stackblitz example for lazy-loading (#33070)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+22.sha-35a95a8.with-local-changes
+ * @license Angular v9.0.0-next.10+27.sha-1ae77da.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17883,7 +17883,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.10+22.sha-35a95a8.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.10+27.sha-1ae77da.with-local-changes');
 
 /**
  * @license
