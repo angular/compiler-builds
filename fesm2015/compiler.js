5cc0be716cb4a8fc3742fee116db3ad7e804854b
Miško Hevery <misko@hevery.com>
2019-10-11 23:02:08 +0000

    d4d07233d perf(ivy): guard host binding execution with a TNode flag (#33102)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+37.sha-15e3b5f.with-local-changes
+ * @license Angular v9.0.0-next.10+41.sha-d4d0723.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17883,7 +17883,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.10+37.sha-15e3b5f.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.10+41.sha-d4d0723.with-local-changes');
 
 /**
  * @license
