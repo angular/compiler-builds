900cd5d823db1a9f8d9a76eac3f8e12bc23ecc19
Miško Hevery <misko@hevery.com>
2019-10-14 23:58:58 +0000

    64aae3a9d test(language-service): test project cleanup (#33157)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+79.sha-10dcbcf.with-local-changes
+ * @license Angular v9.0.0-next.10+77.sha-64aae3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19002,7 +19002,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.10+79.sha-10dcbcf.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.10+77.sha-64aae3a.with-local-changes');
 
 /**
  * @license
