113735df83c5c7d6f5027afdbe7bbdbdc1946dc0
Miško Hevery <misko@hevery.com>
2019-10-14 16:46:49 +0000

    1a34fbce2 fix(ngcc): rename the executable from `ivy-ngcc` to `ngcc` (#33140)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+49.sha-25af147.with-local-changes
+ * @license Angular v9.0.0-next.10+48.sha-1a34fbc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+49.sha-25af147.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+48.sha-1a34fbc.with-local-changes');
 
     /**
      * @license
