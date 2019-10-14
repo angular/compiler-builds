cf68242d8dae4364cd1fe25fead8c4a7c4ace301
Miško Hevery <misko@hevery.com>
2019-10-14 20:26:30 +0000

    68f06c8dd build: using the xlarge resource class for zonejs tests on CI (#33148)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+55.sha-868b3f9.with-local-changes
+ * @license Angular v9.0.0-next.10+57.sha-68f06c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18965,7 +18965,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.10+55.sha-868b3f9.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.10+57.sha-68f06c8.with-local-changes');
 
 /**
  * @license
