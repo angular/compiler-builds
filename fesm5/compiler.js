41c0416de5ee8f6ff94c8933176a71e2b358d073
Miško Hevery <misko@hevery.com>
2019-10-14 16:42:37 +0000

    84ba1f012 test(language-service): Remove redundant marker methods in MockHost (#33115)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+44.sha-d8249d1.with-local-changes
+ * @license Angular v9.0.0-next.10+46.sha-84ba1f0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2740,7 +2740,7 @@ var ConstantPool = /** @class */ (function () {
             case 2 /* Component */:
                 return 'ɵcmp';
             case 1 /* Directive */:
-                return 'ngDirectiveDef';
+                return 'ɵdir';
             case 0 /* Injector */:
                 return 'ngInjectorDef';
             case 3 /* Pipe */:
@@ -18965,7 +18965,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.10+44.sha-d8249d1.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.10+46.sha-84ba1f0.with-local-changes');
 
 /**
  * @license
