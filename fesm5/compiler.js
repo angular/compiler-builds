a99e0e68f186860f7b713ce5aeec80eb8cf54d2b
Miško Hevery <misko@hevery.com>
2019-10-14 23:59:47 +0000

    5265b7413 docs: fix typo in guide (#33155)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+76.sha-4c0726d.with-local-changes
+ * @license Angular v9.0.0-next.10+78.sha-5265b74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19002,7 +19002,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.10+76.sha-4c0726d.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.10+78.sha-5265b74.with-local-changes');
 
 /**
  * @license
