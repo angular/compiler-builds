1c68bb9c64b4ccd7aef2b32965f3602dca99d167
Miško Hevery <misko@hevery.com>
2019-10-15 00:09:21 +0000

    ac745c835 docs: clarify static router-outlet name attribute (#32973)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+81.sha-8321bd8.with-local-changes
+ * @license Angular v9.0.0-next.10+83.sha-ac745c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19171,7 +19171,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+81.sha-8321bd8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+83.sha-ac745c8.with-local-changes');
 
     /**
      * @license
