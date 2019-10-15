0e6d53899e9985436f3025495b253eec33c5e155
Miško Hevery <misko@hevery.com>
2019-10-15 00:03:45 +0000

    72494c441 refactor(core): replace instanceof Array (#33077)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+77.sha-64aae3a.with-local-changes
+ * @license Angular v9.0.0-next.10+80.sha-72494c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19171,7 +19171,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.10+77.sha-64aae3a.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.10+80.sha-72494c4.with-local-changes');
 
     /**
      * @license
