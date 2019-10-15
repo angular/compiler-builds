79785ee05d4fe8628309b17e56b561e9c74b283e
Miško Hevery <misko@hevery.com>
2019-10-15 21:29:54 +0000

    ad72c9044 fix(ivy): i18n - add XLIFF aliases for legacy message id support (#33160)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+94.sha-11e04b1.with-local-changes
+ * @license Angular v9.0.0-next.10+95.sha-ad72c90.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -8148,10 +8148,11 @@ var I18nMetaVisitor = /** @class */ (function () {
             // generate (or restore) message id if not specified in template
             message.id = typeof meta !== 'string' && meta.id || decimalDigest(message);
         }
-        if (this.i18nLegacyMessageIdFormat === 'xlf') {
+        if (this.i18nLegacyMessageIdFormat === 'xlf' || this.i18nLegacyMessageIdFormat === 'xliff') {
             message.legacyId = computeDigest(message);
         }
-        else if (this.i18nLegacyMessageIdFormat === 'xlf2' || this.i18nLegacyMessageIdFormat === 'xmb') {
+        else if (this.i18nLegacyMessageIdFormat === 'xlf2' || this.i18nLegacyMessageIdFormat === 'xliff2' ||
+            this.i18nLegacyMessageIdFormat === 'xmb') {
             message.legacyId = computeDecimalDigest(message);
         }
         else if (typeof meta !== 'string') {
@@ -19002,7 +19003,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.10+94.sha-11e04b1.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.10+95.sha-ad72c90.with-local-changes');
 
 /**
  * @license
