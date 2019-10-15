f80abbc922aaa84cc13265ca3ed2a9bf79247c16
Miško Hevery <misko@hevery.com>
2019-10-15 21:21:48 +0000

    11e04b189 fix(ivy): avoid DOM element assertions if procedural renderer is used (#33156)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+93.sha-ec6a9f2.with-local-changes
+ * @license Angular v9.0.0-next.10+94.sha-11e04b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17920,7 +17920,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.10+93.sha-ec6a9f2.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.10+94.sha-11e04b1.with-local-changes');
 
 /**
  * @license
