1a3de04a41ac31aeb384294017679b79ec36fd9b
Matias Niemelä <matias@yearofmoo.com>
2019-10-16 21:03:12 +0000

    37cbcfaa7 fix(upgrade): remove unused version export (#33180)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.11+1.sha-ef0b8f4.with-local-changes
+ * @license Angular v8.2.11+2.sha-37cbcfa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17585,7 +17585,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('8.2.11+1.sha-ef0b8f4.with-local-changes');
+const VERSION$1 = new Version('8.2.11+2.sha-37cbcfa.with-local-changes');
 
 /**
  * @license
