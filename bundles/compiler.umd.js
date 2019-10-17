507d83c666b40fcd913fb19676b74aad54d96bba
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:34:07 +0000

    4800fa1c0 refactor(core): tweek micro-benchmarks to make them more consistent (#33207)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+26.sha-0e260d2.with-local-changes
+ * @license Angular v9.0.0-next.11+31.sha-4800fa1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19172,7 +19172,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+26.sha-0e260d2.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+31.sha-4800fa1.with-local-changes');
 
     /**
      * @license
