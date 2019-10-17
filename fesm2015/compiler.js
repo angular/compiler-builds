11b47f5c96f2dd446c48d1c4dce7d580ac605a83
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:25:55 +0000

    1a8bd22fa refactor(core): rename ngLocaleIdDef to ɵloc (#33212)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+39.sha-bb53b65.with-local-changes
+ * @license Angular v9.0.0-next.11+41.sha-1a8bd22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17924,7 +17924,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.11+39.sha-bb53b65.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.11+41.sha-1a8bd22.with-local-changes');
 
 /**
  * @license
