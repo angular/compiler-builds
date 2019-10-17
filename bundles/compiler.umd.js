b18ca4c44d4bed3e9ef8eeafbc1bb7f0f7d5dcf2
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:43:31 +0000

    43487f676 docs: correct vscode clang-format setup instructions (#33190)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+37.sha-6f203c9.with-local-changes
+ * @license Angular v9.0.0-next.11+38.sha-43487f6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19175,7 +19175,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+37.sha-6f203c9.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+38.sha-43487f6.with-local-changes');
 
     /**
      * @license
