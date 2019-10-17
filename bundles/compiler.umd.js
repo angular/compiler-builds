be08a092bdc6b2d02b90804fd497bb4af163c6d6
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 17:08:31 +0000

    082aed6e4 revert: feat: add a flag in bootstrap to enable coalesce event change detection to improve performance (#30533) (#33230)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+23.sha-d192a7b.with-local-changes
+ * @license Angular v9.0.0-next.11+24.sha-082aed6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19172,7 +19172,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+23.sha-d192a7b.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+24.sha-082aed6.with-local-changes');
 
     /**
      * @license
