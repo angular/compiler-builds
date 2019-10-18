786164fb396940a40430d4d8d6df39b9004abe08
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:04:41 +0000

    6958d11d95 feat(ivy): type checking of event bindings (#33125)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+67.sha-c44a1a7.with-local-changes
+ * @license Angular v9.0.0-next.11+65.sha-6958d11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19008,7 +19008,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.11+67.sha-c44a1a7.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.11+65.sha-6958d11.with-local-changes');
 
 /**
  * @license
