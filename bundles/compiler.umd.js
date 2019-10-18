1c6a8c84c13412c079df4ad87bfb846bbf632e24
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:02:53 +0000

    c44a1a78a9 docs: add ayaz hafiz as a contributor (#32975)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+66.sha-f6667f8.with-local-changes
+ * @license Angular v9.0.0-next.11+67.sha-c44a1a7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19187,7 +19187,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.11+66.sha-f6667f8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.11+67.sha-c44a1a7.with-local-changes');
 
     /**
      * @license
