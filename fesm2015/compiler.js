8550378f61803b32eb80513f505d0ccd883cb4fb
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 17:08:30 +0000

    e5081bcf25 build: update the integration/payloads limit

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
+ * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17926,7 +17926,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.12+11.sha-d7dc6cb.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.12+17.sha-e5081bc.with-local-changes');
 
 /**
  * @license
