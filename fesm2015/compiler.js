fa15f59da77dc4a5cd938266599216f7f5179cd7
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:40:46 +0000

    d4db746898 feat(ivy): give shim generation its own compiler options (#33256)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+6.sha-29bc3a7.with-local-changes
+ * @license Angular v9.0.0-next.12+7.sha-d4db746.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17926,7 +17926,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.12+6.sha-29bc3a7.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.12+7.sha-d4db746.with-local-changes');
 
 /**
  * @license
