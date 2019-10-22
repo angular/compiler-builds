50b3ee86caed79f679a448206b9d724e916b30e3
Kara Erickson <karakara@google.com>
2019-10-22 19:24:10 +0000

    1b8b04cf26 docs: add ModuleWithProviders migration doc (#33085)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+46.sha-7f7dc7c.with-local-changes
+ * @license Angular v9.0.0-next.12+49.sha-1b8b04c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19079,7 +19079,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.12+46.sha-7f7dc7c.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.12+49.sha-1b8b04c.with-local-changes');
 
 /**
  * @license
