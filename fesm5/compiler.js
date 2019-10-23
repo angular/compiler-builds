3edf3b58b2f904d2fec37d1f0b14cb504ee69248
Andrew Kushnir <akushnir@google.com>
2019-10-23 19:04:07 +0000

    1b607529a6 docs: fix spelling error in angular component (#32971)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+63.sha-2ecc4c7.with-local-changes
+ * @license Angular v9.0.0-next.12+64.sha-1b60752.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19079,7 +19079,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.12+63.sha-2ecc4c7.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.12+64.sha-1b60752.with-local-changes');
 
 /**
  * @license
