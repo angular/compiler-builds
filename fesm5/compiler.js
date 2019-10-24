f6478e7d8fa62c26a25506926c80ce445b3993e8
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:19:22 +0000

    3d11355fec test(compiler): add expression absolute span tests for `TemplateAst`s (#33253)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+4.sha-f1269d9.with-local-changes
+ * @license Angular v9.0.0-next.13+7.sha-3d11355.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14088,7 +14088,8 @@ var TemplateParseVisitor = /** @class */ (function () {
                 }
                 hasInlineTemplates = true;
                 var parsedVariables_1 = [];
-                _this._bindingParser.parseInlineTemplateBinding(templateKey, templateValue, attr.sourceSpan, attr.sourceSpan.start.offset, templateMatchableAttrs, templateElementOrDirectiveProps, parsedVariables_1);
+                var absoluteOffset = (attr.valueSpan || attr.sourceSpan).start.offset;
+                _this._bindingParser.parseInlineTemplateBinding(templateKey, templateValue, attr.sourceSpan, absoluteOffset, templateMatchableAttrs, templateElementOrDirectiveProps, parsedVariables_1);
                 templateElementVars.push.apply(templateElementVars, __spread(parsedVariables_1.map(function (v) { return VariableAst.fromParsedVariable(v); })));
             }
             if (!hasBinding && !hasTemplateBinding) {
@@ -19079,7 +19080,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.13+4.sha-f1269d9.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.13+7.sha-3d11355.with-local-changes');
 
 /**
  * @license
