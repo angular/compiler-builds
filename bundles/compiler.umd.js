56325c5bc043b2fd7928fa19ed5552db0e36ec5a
Andrew Kushnir <akushnir@google.com>
2019-10-24 17:07:54 +0000

    2f3812a1ef docs: add example of a server in golang for deployment (#33092)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+3.sha-a1d7b6b.with-local-changes
+ * @license Angular v9.0.0-next.13+5.sha-2f3812a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19258,7 +19258,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-next.13+3.sha-a1d7b6b.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-next.13+5.sha-2f3812a.with-local-changes');
 
     /**
      * @license
