518fd35dbd7b41b071ba75b6bfed04677916a204
Andrew Kushnir <akushnir@google.com>
2019-10-25 17:15:59 +0000

    26a6db208c docs(docs-infra): document running docs examples in ivy mode (#33399)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+2.sha-e0428fe.with-local-changes
+ * @license Angular v8.2.12+3.sha-26a6db2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18648,7 +18648,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('8.2.12+2.sha-e0428fe.with-local-changes');
+var VERSION$1 = new Version('8.2.12+3.sha-26a6db2.with-local-changes');
 
 /**
  * @license
