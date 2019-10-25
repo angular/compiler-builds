91db20e012f28be2cb5096e39f775394d87d4fd4
Andrew Kushnir <akushnir@google.com>
2019-10-25 23:02:10 +0000

    7a280b1a21 test(ivy): clean up class_binding perf tests for readability (#33413)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+59.sha-5607ad8.with-local-changes
+ * @license Angular v9.0.0-next.14+1.sha-7a280b1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17935,7 +17935,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.13+59.sha-5607ad8.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.14+1.sha-7a280b1.with-local-changes');
 
 /**
  * @license
