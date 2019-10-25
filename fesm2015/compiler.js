1adbb6eb738b581d58aed935e50e1d7306eeef11
Andrew Kushnir <akushnir@google.com>
2019-10-25 16:41:07 +0000

    aa36b55bd9 build: address review comments in .bazelrc (#33367)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+42.sha-b381497.with-local-changes
+ * @license Angular v9.0.0-next.13+47.sha-aa36b55.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17990,7 +17990,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.13+42.sha-b381497.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.13+47.sha-aa36b55.with-local-changes');
 
 /**
  * @license
