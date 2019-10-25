3d10e8f405fc88164182014b670ecc120dd8b386
Andrew Kushnir <akushnir@google.com>
2019-10-25 00:59:06 +0000

    dcdb433b7d perf(ivy): apply [style]/[class] bindings directly to style/className (#33336)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+34.sha-ee4fc12.with-local-changes
+ * @license Angular v9.0.0-next.13+35.sha-dcdb433.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -14001,7 +14001,10 @@ class StylingBuilder {
     }
     _buildMapBasedInstruction(valueConverter, isClassBased, stylingInput) {
         // each styling binding value is stored in the LView
-        let totalBindingSlotsRequired = 1;
+        // map-based bindings allocate two slots: one for the
+        // previous binding value and another for the previous
+        // className or style attribute value.
+        let totalBindingSlotsRequired = 2;
         // these values must be outside of the update block so that they can
         // be evaluated (the AST visit call) during creation time so that any
         // pipes can be picked up in time before the template is built
@@ -17983,7 +17986,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.13+34.sha-ee4fc12.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.13+35.sha-dcdb433.with-local-changes');
 
 /**
  * @license
