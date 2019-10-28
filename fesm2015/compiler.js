fe85f207b4a3f4cc8d2da8387edbb92daa485e13
Andrew Kushnir <akushnir@google.com>
2019-10-28 17:28:48 +0000

    41979d6a27 fix(ivy): i18n - update `localize-translate` to accept target-locales (#33381)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+2.sha-e23bc49.with-local-changes
+ * @license Angular v9.0.0-next.14+4.sha-41979d6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17935,7 +17935,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-next.14+2.sha-e23bc49.with-local-changes');
+const VERSION$1 = new Version('9.0.0-next.14+4.sha-41979d6.with-local-changes');
 
 /**
  * @license
