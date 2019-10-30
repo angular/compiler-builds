f11f770a8115abea51e1254f33b2ac869c69213b
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:50:44 +0000

    f197191a5f refactor(core): better error message for undecorated-classes-with-di migration (#33315)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+34.sha-fba72f0.with-local-changes
+ * @license Angular v9.0.0-next.14+36.sha-f197191.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19024,7 +19024,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-next.14+34.sha-fba72f0.with-local-changes');
+var VERSION$1 = new Version('9.0.0-next.14+36.sha-f197191.with-local-changes');
 
 /**
  * @license
