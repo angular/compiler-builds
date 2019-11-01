e89a3b1af575859d967b39e823af3f0e8892a4a0
atscott <atscott01@gmail.com>
2019-11-01 18:00:47 +0000

    9ebac71521 fix(language-service): Should not crash if expr ends unexpectedly (#33524)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
+ * @license Angular v9.0.0-rc.0+20.sha-9ebac71.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17935,7 +17935,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.0+20.sha-9ebac71.with-local-changes');
 
 /**
  * @license
