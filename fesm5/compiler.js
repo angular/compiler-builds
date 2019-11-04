430fce1396f197646535df0d8d148ebe81d0555a
atscott <atscott01@gmail.com>
2019-11-04 20:27:24 +0000

    4ff43e1324 refactor(ivy): remove the getHostNative utility function (#33554)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+35.sha-66725b7.with-local-changes
+ * @license Angular v9.0.0-rc.0+36.sha-4ff43e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19032,7 +19032,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.0+35.sha-66725b7.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.0+36.sha-4ff43e1.with-local-changes');
 
 /**
  * @license
