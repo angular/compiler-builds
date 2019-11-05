5fc4625c3b7bb6a89888ed4475c1bd0aa2045f97
atscott <atscott01@gmail.com>
2019-11-05 01:12:41 +0000

    1b08f7c919 docs: Adding Doug Parker to contributors list. (#33516)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+40.sha-8de3fcf.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-1b08f7c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17939,7 +17939,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.0+40.sha-8de3fcf.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.0+42.sha-1b08f7c.with-local-changes');
 
 /**
  * @license
