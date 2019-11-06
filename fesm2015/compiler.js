377de5926ddc93c6ab20303f67bc6abec6fb2526
atscott <atscott01@gmail.com>
2019-11-06 18:23:13 +0000

    e89c2dd8d0 perf(ivy): add ngIf-like directive to the ng_template benchmark (#33595)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+67.sha-3297a76.with-local-changes
+ * @license Angular v9.0.0-rc.0+68.sha-e89c2dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17958,7 +17958,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.0+67.sha-3297a76.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.0+68.sha-e89c2dd.with-local-changes');
 
 /**
  * @license
