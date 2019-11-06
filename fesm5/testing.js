adb13fba56ca1444e9ef1ec7dce28f388fba11e7
atscott <atscott01@gmail.com>
2019-11-06 20:53:25 +0000

    41560b47c4 refactor(ivy): move all styling configurations into `TNodeFlags` (#33540)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+77.sha-2ed04ff.with-local-changes
+ * @license Angular v9.0.0-rc.0+69.sha-41560b4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -302,5 +302,5 @@ var MockPipeResolver = /** @class */ (function (_super) {
  * found in the LICENSE file at https://angular.io/license
  */
 
-export { MockDirectiveResolver, MockNgModuleResolver, MockPipeResolver, MockResourceLoader, MockSchemaRegistry };
+export { MockResourceLoader, MockSchemaRegistry, MockDirectiveResolver, MockNgModuleResolver, MockPipeResolver };
 //# sourceMappingURL=testing.js.map
