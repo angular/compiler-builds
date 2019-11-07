f7cb32e74226ed11391fa85a88c84b7304e91fff
Andrew Scott <atscott01@gmail.com>
2019-11-07 02:17:36 +0000

    7bccef516f fix(compiler-cli): Fix typo $implict (#33633)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+2.sha-3419b50.with-local-changes
+ * @license Angular v9.0.0-rc.1+4.sha-7bccef5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7182,7 +7182,7 @@
                 }
                 else {
                     this._reportError('Blank expressions are not allowed in interpolated strings', input, "at column " + this._findInterpolationErrorColumn(parts, i, interpolationConfig) + " in", location);
-                    expressions.push('$implict');
+                    expressions.push('$implicit');
                     offsets.push(offset);
                 }
             }
@@ -19230,7 +19230,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+2.sha-3419b50.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+4.sha-7bccef5.with-local-changes');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+2.sha-3419b50.with-local-changes
+ * @license Angular v9.0.0-rc.1+4.sha-7bccef5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -6445,7 +6445,7 @@ class Parser {
             }
             else {
                 this._reportError('Blank expressions are not allowed in interpolated strings', input, `at column ${this._findInterpolationErrorColumn(parts, i, interpolationConfig)} in`, location);
-                expressions.push('$implict');
+                expressions.push('$implicit');
                 offsets.push(offset);
             }
         }
@@ -17959,7 +17959,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+2.sha-3419b50.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+4.sha-7bccef5.with-local-changes');
 
 /**
  * @license
