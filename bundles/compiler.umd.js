2a2989344e2894281fb1412019bbfbdd8f5cae41
Andrew Scott <atscott01@gmail.com>
2019-11-07 02:14:46 +0000

    39550746f8 fix(ivy): better support for i18n attributes on <ng-container>s (#33599)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+1.sha-c0f69f3.with-local-changes
+ * @license Angular v9.0.0-rc.1+3.sha-3955074.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19230,7 +19230,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+1.sha-c0f69f3.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+3.sha-3955074.with-local-changes');
 
     /**
      * @license
