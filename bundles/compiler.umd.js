7ab3d57539fc3844fdba8813be60110b3d811c97
Kara Erickson <karakara@google.com>
2019-11-08 23:03:30 +0000

    b97577bd94 docs: grammar fix (#33170)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+32.sha-a40103d.with-local-changes
+ * @license Angular v9.0.0-rc.1+31.sha-b97577b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19232,7 +19232,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+32.sha-a40103d.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+31.sha-b97577b.with-local-changes');
 
     /**
      * @license
