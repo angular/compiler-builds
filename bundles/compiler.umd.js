8f7148df1363d5059ea4dde45d357094a19de2e4
Kara Erickson <karakara@google.com>
2019-11-13 20:38:11 +0000

    03b3a3881a build: set up all packages to publish via wombot proxy

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.13+4.sha-2c74996.with-local-changes
+ * @license Angular v8.2.13+5.sha-03b3a38.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18817,7 +18817,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('8.2.13+4.sha-2c74996.with-local-changes');
+    var VERSION$1 = new Version('8.2.13+5.sha-03b3a38.with-local-changes');
 
     /**
      * @license
