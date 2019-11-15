c51f2f6b599656d8d25a64c78d48ace348346b18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 00:20:25 +0000

    84524583e9 fix(ivy): extend assertion in `directiveInject` function to support IcuContainers (#33832)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+20.sha-a61fb76.with-local-changes
+ * @license Angular v9.0.0-rc.2+21.sha-8452458.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -17953,7 +17953,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.2+20.sha-a61fb76.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.2+21.sha-8452458.with-local-changes');
 
 /**
  * @license
