dfb714bc5459ec3cf5bf136da44f6cdda91e1afd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 18:50:45 +0000

    a959fae66e build: add ngJitMode to ng_rollup_bundle terser config (#33865)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+180.sha-bb290ce.with-local-changes
+ * @license Angular v9.0.0-rc.1+181.sha-a959fae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18023,7 +18023,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+180.sha-bb290ce.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+181.sha-a959fae.with-local-changes');
 
 /**
  * @license
