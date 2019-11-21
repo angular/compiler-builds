5f4aa014871beafe41c13f7182d43423905202be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 20:01:47 +0000

    250e6fdd39 fix(service-worker): throw when using the unsupported `versionedFiles` option in config (#33903)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+20.sha-c6695fa.with-local-changes
+ * @license Angular v9.0.0-rc.3+22.sha-250e6fd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19274,7 +19274,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.3+20.sha-c6695fa.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.3+22.sha-250e6fd.with-local-changes');
 
     /**
      * @license
