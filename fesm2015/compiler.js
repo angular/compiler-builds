702375408465d1efc36acf6fcaaff829debcca0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:15:40 +0000

    bf1bcd1e08 fix(ngcc): render localized strings when in ES5 format (#33857)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+209.sha-346e1c1.with-local-changes
+ * @license Angular v9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18044,7 +18044,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+209.sha-346e1c1.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes');
 
 /**
  * @license
