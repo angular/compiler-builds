571e53b2d9a24463de185c94ab14f721518dc762
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:18:26 +0000

    fd5f101bab docs: add CSRF to XSRF section of http guide (#32933)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+215.sha-2303dc4.with-local-changes
+ * @license Angular v9.0.0-rc.1+216.sha-fd5f101.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18044,7 +18044,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+215.sha-2303dc4.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+216.sha-fd5f101.with-local-changes');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+215.sha-2303dc4.with-local-changes
+ * @license Angular v9.0.0-rc.1+216.sha-fd5f101.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19139,7 +19139,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+215.sha-2303dc4.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+216.sha-fd5f101.with-local-changes');
 
 /**
  * @license
