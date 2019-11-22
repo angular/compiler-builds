885ed11aadf04dd18ee9ed8cfbba42c45578d30b
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:10:23 +0000

    de043a4bcb refactor(bazel): use getattr instead of hasattr in ng_module.bzl (#33765)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+232.sha-485ce64.with-local-changes
+ * @license Angular v9.0.0-rc.1+237.sha-de043a4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19139,7 +19139,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+232.sha-485ce64.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+237.sha-de043a4.with-local-changes');
 
 /**
  * @license
