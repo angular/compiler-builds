68057c21a8f5bcd6e581b202c459d865f2f899ff
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:05:09 +0000

    31e2bdffa8 perf(ivy): fix creation time micro-benchmarks (#34031)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
+ * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19317,7 +19317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+253.sha-d80eeb6.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+254.sha-31e2bdf.with-local-changes');
 
     /**
      * @license
