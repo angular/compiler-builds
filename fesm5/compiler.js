7c99365bde0357e84597ea9b1518ae8a9f20f886
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:56:28 +0000

    1217c6b7cc docs: replace class hero to interface wherever class properties not used (#34022)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+244.sha-5129bcc.with-local-changes
+ * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19139,7 +19139,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+244.sha-5129bcc.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+246.sha-1217c6b.with-local-changes');
 
 /**
  * @license
