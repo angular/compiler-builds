96fe4fc206830b1810458c7e16ee4252b316ca1f
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:55:24 +0000

    51a56bc4c6 docs: fix typos and highlight booleans to keep consistency (#34013)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
+ * @license Angular v9.0.0-rc.1+247.sha-51a56bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19317,7 +19317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+246.sha-1217c6b.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+247.sha-51a56bc.with-local-changes');
 
     /**
      * @license
