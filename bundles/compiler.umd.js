ff4e7e95bab605209eb18f4116a7a9c38fc9a352
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 22:08:47 +0000

    fadb2d9004 perf(ivy): do no work if moving a `viewRef` to the same position (#34052)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+268.sha-5de7960.with-local-changes
+ * @license Angular v9.0.0-rc.1+270.sha-fadb2d9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19317,7 +19317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+268.sha-5de7960.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+270.sha-fadb2d9.with-local-changes');
 
     /**
      * @license
