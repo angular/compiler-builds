8fed5723931eed97f77f589f9af7aebca2c5f0b5
Miško Hevery <misko@hevery.com>
2019-11-27 00:34:32 +0000

    49804fe017 fix(language-service): determine index types accessed using dot notation (#33884)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+286.sha-3f68377.with-local-changes
+ * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19317,7 +19317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
 
     /**
      * @license
