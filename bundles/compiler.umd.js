38fad03b636681328663abf4b77e7fc4ba8b47b0
Miško Hevery <misko@hevery.com>
2019-11-27 00:41:07 +0000

    005c4a1cc5 build: set default ts_devserver serving_path to /app_bundle.js to match g3 (#33996)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+282.sha-49804fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+288.sha-005c4a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19317,7 +19317,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+282.sha-49804fe.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+288.sha-005c4a1.with-local-changes');
 
     /**
      * @license
