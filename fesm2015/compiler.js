2342f5bfa9fc17ab566b9918488f85fc44252286
Miško Hevery <misko@hevery.com>
2019-12-02 19:50:17 +0000

    3408468121 build: remove fsevents from package.json (#34058)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+303.sha-168abc6.with-local-changes
+ * @license Angular v9.0.0-rc.1+308.sha-3408468.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18044,7 +18044,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+303.sha-168abc6.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+308.sha-3408468.with-local-changes');
 
 /**
  * @license
