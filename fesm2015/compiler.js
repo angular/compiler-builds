c46c566426ac9dd6e96565c08c8b157568e12565
Miško Hevery <misko@hevery.com>
2019-12-03 18:34:34 +0000

    dd944ef73f fix(language-service): Insert parentheses for method completion (#33860)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+333.sha-c60d756.with-local-changes
+ * @license Angular v9.0.0-rc.1+334.sha-dd944ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18048,7 +18048,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+333.sha-c60d756.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+334.sha-dd944ef.with-local-changes');
 
 /**
  * @license
