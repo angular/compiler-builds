9080b43f63645982832a191b590d68812d4b5a37
Misko Hevery <misko@hevery.com>
2019-12-04 07:20:52 +0000

    634887c0e7 test(ivy): update `ngI18nClosureMode` flag usage in tests (#34224)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes
+ * @license Angular v9.0.0-rc.1+370.sha-634887c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18307,7 +18307,7 @@
      *
      * ```
      * var I18N_1;
-     * if (ngI18nClosureMode) {
+     * if (typeof ngI18nClosureMode !== undefined && ngI18nClosureMode) {
      *     var MSG_EXTERNAL_XXX = goog.getMsg(
      *          "Some message with {$interpolation}!",
      *          { "interpolation": "\uFFFD0\uFFFD" }
@@ -18328,16 +18328,29 @@
      * post-processing).
      * @returns An array of statements that defined a given translation.
      */
-    function getTranslationDeclStmts(message, variable$1, closureVar, params, transformFn) {
+    function getTranslationDeclStmts(message, variable, closureVar, params, transformFn) {
         if (params === void 0) { params = {}; }
         var statements = [
-            declareI18nVariable(variable$1),
-            ifStmt(variable(NG_I18N_CLOSURE_MODE), createGoogleGetMsgStatements(variable$1, message, closureVar, i18nFormatPlaceholderNames(params, /* useCamelCase */ true)), createLocalizeStatements(variable$1, message, i18nFormatPlaceholderNames(params, /* useCamelCase */ false))),
+            declareI18nVariable(variable),
+            ifStmt(createClosureModeGuard(), createGoogleGetMsgStatements(variable, message, closureVar, i18nFormatPlaceholderNames(params, /* useCamelCase */ true)), createLocalizeStatements(variable, message, i18nFormatPlaceholderNames(params, /* useCamelCase */ false))),
         ];
         if (transformFn) {
-            statements.push(new ExpressionStatement(variable$1.set(transformFn(variable$1))));
+            statements.push(new ExpressionStatement(variable.set(transformFn(variable))));
         }
         return statements;
+    }
+    /**
+     * Create the expression that will be used to guard the closure mode block
+     * It is equivalent to:
+     *
+     * ```
+     * typeof ngI18nClosureMode !== undefined && ngI18nClosureMode
+     * ```
+     */
+    function createClosureModeGuard() {
+        return typeofExpr(variable(NG_I18N_CLOSURE_MODE))
+            .notIdentical(literal('undefined', STRING_TYPE))
+            .and(variable(NG_I18N_CLOSURE_MODE));
     }
 
     /**
@@ -19325,7 +19338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+365.sha-9b6a1b8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+370.sha-634887c.with-local-changes');
 
     /**
      * @license
