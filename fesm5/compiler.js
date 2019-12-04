579a326a6e7ff9713210c898b6a4a84933d6aa7b
Miško Hevery <misko@hevery.com>
2019-12-04 23:23:09 +0000

    1183075d16 Revert "fix(common): update closure locales to include directionality data (#34240)"

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5.with-local-changes
+ * @license Angular v9.0.0-rc.5+2.sha-1183075.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19117,7 +19117,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.5.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.5+2.sha-1183075.with-local-changes');
 
 /**
  * @license
