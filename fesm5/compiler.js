fe1fad93bb5e58187e792b009697f9a3b9f3a611
Miško Hevery <misko@hevery.com>
2019-12-04 00:31:02 +0000

    627814cc4a docs(compiler): fix typos and spelling (#34008)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+355.sha-8f102fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+356.sha-627814c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19141,7 +19141,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+355.sha-8f102fe.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+356.sha-627814c.with-local-changes');
 
 /**
  * @license
