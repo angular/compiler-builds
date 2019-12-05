41f4f60ccd32420423b16d42eea6542407cdf8af
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:41:58 +0000

    37bb90140c refactor(platform-browser): avoid mutable exports. (#34207)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+377.sha-542f236.with-local-changes
+ * @license Angular v9.0.0-rc.1+378.sha-37bb901.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19338,7 +19338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+377.sha-542f236.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+378.sha-37bb901.with-local-changes');
 
     /**
      * @license
