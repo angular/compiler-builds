7646222888a09a84a0d2fbee8cc7a59478382008
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:24:40 +0000

    de62d8ebdb docs: fix migrations formatting (#33267)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+385.sha-0341f32.with-local-changes
+ * @license Angular v9.0.0-rc.1+383.sha-de62d8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19338,7 +19338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+385.sha-0341f32.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+383.sha-de62d8e.with-local-changes');
 
     /**
      * @license
