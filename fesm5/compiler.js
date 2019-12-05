64e43fba51a23b040dfcab0e45e38af117eea3fd
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:22:27 +0000

    421dba0184 fix(docs-infra): make header full-width and panels full-width on mobile screens (#34188)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+382.sha-0213417.with-local-changes
+ * @license Angular v9.0.0-rc.1+384.sha-421dba0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19160,7 +19160,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+382.sha-0213417.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+384.sha-421dba0.with-local-changes');
 
 /**
  * @license
