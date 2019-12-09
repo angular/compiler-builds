d3df4afd5341ce2ca94993668ffd159e09085634
Andrew Kushnir <akushnir@google.com>
2019-12-09 18:55:56 +0000

    d3069dbec6 refactor(ivy): minor cleanup in the listener instruction (#34255)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes
+ * @license Angular v9.0.0-rc.1+404.sha-d3069db.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18098,7 +18098,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+404.sha-d3069db.with-local-changes');
 
 /**
  * @license
