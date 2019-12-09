c6f99604efdb3c9dbb41784f1fdc3f44e0047d8f
Andrew Kushnir <akushnir@google.com>
2019-12-09 18:56:01 +0000

    f98aeca146 docs(forms): fix a punctuation error (#34278) PR Close #34278

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes
+ * @license Angular v9.0.0-rc.1+403.sha-f98aeca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19194,7 +19194,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+402.sha-a1d0f1e.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+403.sha-f98aeca.with-local-changes');
 
 /**
  * @license
