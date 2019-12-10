df64a8ae034f343de578a675d765e6f55dcaad32
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:43:21 +0000

    2ecf9f3f2a refactor(docs-infra): give more descriptive variable name (#34284)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+415.sha-8683c03.with-local-changes
+ * @license Angular v9.0.0-rc.1+416.sha-2ecf9f3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19201,7 +19201,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+415.sha-8683c03.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+416.sha-2ecf9f3.with-local-changes');
 
 /**
  * @license
