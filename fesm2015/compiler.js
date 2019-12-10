8bb33ccd160c23fab260b685ac9f032d304513e9
Andrew Kushnir <akushnir@google.com>
2019-12-10 22:27:25 +0000

    74bbe3e413 refactor(ivy): avoid type coercion in `saveNameToExportMap` function (#34335)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
+ * @license Angular v9.0.0-rc.5+48.sha-74bbe3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18055,7 +18055,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.5+47.sha-46f9201.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.5+48.sha-74bbe3e.with-local-changes');
 
 /**
  * @license
