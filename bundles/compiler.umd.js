4bb3d02958deb899ad3a6c523e6d1b3e00176faa
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:13:22 +0000

    08ea38f197 fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
+ * @license Angular v9.0.0-rc.1+428.sha-08ea38f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19383,7 +19383,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+428.sha-08ea38f.with-local-changes');
 
     /**
      * @license
