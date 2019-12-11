3f97cee7eaa6eb949688e6966e9f6a35ab2080e3
Andrew Kushnir <akushnir@google.com>
2019-12-11 18:15:12 +0000

    d7c459a81a fix(bazel): improve performance of tsHost.writeFile() (#34331)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+58.sha-ad82ed8.with-local-changes
+ * @license Angular v9.0.0-rc.5+61.sha-d7c459a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18109,7 +18109,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.5+58.sha-ad82ed8.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.5+61.sha-d7c459a.with-local-changes');
 
 /**
  * @license
