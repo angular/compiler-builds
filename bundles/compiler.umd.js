12c346700af508d1e0d2815f4a1caeb6e482f6df
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:52:19 +0000

    0984fbc748 fix(compiler-cli): allow declaration-only template type check members (#34296)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+421.sha-c9df9af.with-local-changes
+ * @license Angular v9.0.0-rc.1+423.sha-0984fbc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19379,7 +19379,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+421.sha-c9df9af.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
 
     /**
      * @license
