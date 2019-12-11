88041102be2b922de823756783feb5627621a098
Kara Erickson <karakara@google.com>
2019-12-11 21:42:58 +0000

    81c75cf560 fix(ngcc): handle imports in dts files when processing UMD (#34356)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6.with-local-changes
+ * @license Angular v9.0.0-rc.6+8.sha-81c75cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19205,7 +19205,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.6.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.6+8.sha-81c75cf.with-local-changes');
 
 /**
  * @license
