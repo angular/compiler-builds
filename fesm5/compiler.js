5c83275921076ed48e7999582f9c91af1e097a9e
Andrew Kushnir <akushnir@google.com>
2019-12-11 00:52:04 +0000

    bbb9412a17 fix(compiler-cli): allow declaration-only template type check members (#34296)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+48.sha-74bbe3e.with-local-changes
+ * @license Angular v9.0.0-rc.5+51.sha-bbb9412.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19151,7 +19151,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.5+48.sha-74bbe3e.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.5+51.sha-bbb9412.with-local-changes');
 
 /**
  * @license
