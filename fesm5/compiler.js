57857aa2663319280538129dae2db530f1b4b2e4
Kara Erickson <karakara@google.com>
2019-12-12 22:36:38 +0000

    af95dddd7e perf(ivy): eagerly parse the template twice during analysis (#34334)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+457.sha-ff0a914.with-local-changes
+ * @license Angular v9.0.0-rc.1+458.sha-af95ddd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19239,7 +19239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+457.sha-ff0a914.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.1+458.sha-af95ddd.with-local-changes');
 
 /**
  * @license
