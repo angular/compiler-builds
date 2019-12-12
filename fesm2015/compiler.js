980b7f3763b633886faa199ed3c4c2d0832db120
Kara Erickson <karakara@google.com>
2019-12-12 21:41:33 +0000

    3255d2b197 test(core): fix schematics calls to run synchronously (#34364)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+448.sha-74edde0.with-local-changes
+ * @license Angular v9.0.0-rc.1+451.sha-3255d2b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18109,7 +18109,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+448.sha-74edde0.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+451.sha-3255d2b.with-local-changes');
 
 /**
  * @license
