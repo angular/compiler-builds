255562fa00e32d5c1c35907e586e80e633b53565
Kara Erickson <karakara@google.com>
2019-12-12 00:19:46 +0000

    de71f9152c Revert "build: update integration/bazel to rules_nodejs 0.42.1" (#34360)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+8.sha-81c75cf.with-local-changes
+ * @license Angular v9.0.0-rc.6+13.sha-de71f91.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18109,7 +18109,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.6+8.sha-81c75cf.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.6+13.sha-de71f91.with-local-changes');
 
 /**
  * @license
