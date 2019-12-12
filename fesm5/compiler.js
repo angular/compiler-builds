0cc0b78b5fdf870225f4a972641befaf6e404423
Kara Erickson <karakara@google.com>
2019-12-12 19:24:42 +0000

    25b210a269 style(common): remove unnecessary jsdoc type (#34369)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+15.sha-c26738d.with-local-changes
+ * @license Angular v9.0.0-rc.6+16.sha-25b210a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19205,7 +19205,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.6+15.sha-c26738d.with-local-changes');
+var VERSION$1 = new Version('9.0.0-rc.6+16.sha-25b210a.with-local-changes');
 
 /**
  * @license
