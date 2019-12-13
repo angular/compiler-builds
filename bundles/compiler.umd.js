eb2306d77dee79c3400a3c6411e9bbdeaed4407e
Kara Erickson <karakara@google.com>
2019-12-13 22:44:01 +0000

    f79110c637 fix(ivy): incorrect injectable name logged in warning message on IE (#34305)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+464.sha-17f7f06.with-local-changes
+ * @license Angular v9.0.0-rc.1+472.sha-f79110c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19417,7 +19417,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+464.sha-17f7f06.with-local-changes');
+    var VERSION$1 = new Version('9.0.0-rc.1+472.sha-f79110c.with-local-changes');
 
     /**
      * @license
