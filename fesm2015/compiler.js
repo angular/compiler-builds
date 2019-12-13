a429fab831c1668797d08959f81f477142cf3175
Kara Erickson <karakara@google.com>
2019-12-13 19:11:33 +0000

    ea57587b7c docs(ivy): document breaking changes for DebugElement classes and attributes (#34328)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+460.sha-0fba79c.with-local-changes
+ * @license Angular v9.0.0-rc.1+463.sha-ea57587
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18151,7 +18151,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+460.sha-0fba79c.with-local-changes');
+const VERSION$1 = new Version('9.0.0-rc.1+463.sha-ea57587');
 
 /**
  * @license
