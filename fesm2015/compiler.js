099b8ee9f9d036377cc6884b8cefbd68bbd17cec
Kara Erickson <karakara@google.com>
2019-12-16 16:05:41 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+476.sha-3bbd12d
+ * @license Angular v9.0.0-rc.1+478.sha-04ab036
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18151,7 +18151,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+476.sha-3bbd12d');
+const VERSION$1 = new Version('9.0.0-rc.1+478.sha-04ab036');
 
 /**
  * @license
