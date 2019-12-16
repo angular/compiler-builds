0e5c0f37754fd4fadbc2fcb768d8755b85e51335
Kara Erickson <karakara@google.com>
2019-12-16 16:09:03 +0000

    12444a8afc test(ngcc): cleanup entry-point bundle testcases (#34415)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+478.sha-04ab036
+ * @license Angular v9.0.0-rc.1+480.sha-12444a8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19239,7 +19239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+478.sha-04ab036');
+var VERSION$1 = new Version('9.0.0-rc.1+480.sha-12444a8');
 
 /**
  * @license
