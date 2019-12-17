918e7ae7c3510f82cde03e81e832bc950d18a726
Kara Erickson <karakara@google.com>
2019-12-17 21:54:51 +0000

    b637b9322e ci(docs-infra): fix failure in `aio_monitoring_stable` due to yarn version mismatch (#34451)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+490.sha-5cecd97
+ * @license Angular v9.0.0-rc.1+496.sha-b637b93
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19439,7 +19439,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+490.sha-5cecd97');
+    var VERSION$1 = new Version('9.0.0-rc.1+496.sha-b637b93');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+490.sha-5cecd97
+ * @license Angular v9.0.0-rc.1+496.sha-b637b93
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18173,7 +18173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+490.sha-5cecd97');
+const VERSION$1 = new Version('9.0.0-rc.1+496.sha-b637b93');
 
 /**
  * @license
