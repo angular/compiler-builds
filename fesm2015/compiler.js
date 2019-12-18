c0ecd89a0ca73ba1ea012ad83956bc66d04f6e78
Kara Erickson <karakara@google.com>
2019-12-18 17:36:20 +0000

    9d1175e2b2 fix(ivy): improve ExpressionChangedAfterChecked error (#34381)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+500.sha-a04f7c0
+ * @license Angular v9.0.0-rc.1+499.sha-9d1175e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18173,7 +18173,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+500.sha-a04f7c0');
+const VERSION$1 = new Version('9.0.0-rc.1+499.sha-9d1175e');
 
 /**
  * @license
