4c288812d631e17c18d4a64b907d8257d7c0c60c
Kara Erickson <karakara@google.com>
2019-12-18 18:42:31 +0000

    dfecca29da refactor: TypeScript 3.7 fixes. (#34372)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19261,7 +19261,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+499.sha-9d1175e');
+var VERSION$1 = new Version('9.0.0-rc.1+502.sha-dfecca2');
 
 /**
  * @license
