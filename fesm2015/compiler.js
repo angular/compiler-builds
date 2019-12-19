13a38d888271ddfd48d6e94f06455e5e45b60c6e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-19 19:57:02 +0000

    2dffe65cfd fix(language-service): completions after "let x of |" in ngFor (#34473)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+519.sha-260a061
+ * @license Angular v9.0.0-rc.1+520.sha-2dffe65
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18177,7 +18177,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+519.sha-260a061');
+const VERSION$1 = new Version('9.0.0-rc.1+520.sha-2dffe65');
 
 /**
  * @license
