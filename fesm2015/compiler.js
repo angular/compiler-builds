4bfd5955b9047f9a8707eb13ace2f1c766e6a75c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-20 22:59:51 +0000

    9b9116c79d feat(language-service): Append symbol type to hover tooltip (#34515)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+520.sha-2dffe65
+ * @license Angular v9.0.0-rc.1+521.sha-9b9116c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18177,7 +18177,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+520.sha-2dffe65');
+const VERSION$1 = new Version('9.0.0-rc.1+521.sha-9b9116c');
 
 /**
  * @license
