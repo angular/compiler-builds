43bca04df7341edceea0ac428a2ffc1eddff6082
George Kalpakas <kalpakas.g@gmail.com>
2019-12-29 10:16:08 +0000

    61c805fa1b build(docs-infra): bump handlebars from 4.1.2 to 4.5.3 in /aio

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-22ac0e9
+ * @license Angular v9.0.0-rc.1+522.sha-61c805f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18177,7 +18177,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+522.sha-22ac0e9');
+const VERSION$1 = new Version('9.0.0-rc.1+522.sha-61c805f');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+522.sha-22ac0e9
+ * @license Angular v9.0.0-rc.1+522.sha-61c805f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19265,7 +19265,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+522.sha-22ac0e9');
+var VERSION$1 = new Version('9.0.0-rc.1+522.sha-61c805f');
 
 /**
  * @license
