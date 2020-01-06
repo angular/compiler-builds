75f979c1c1a57f638e533a1db3f0bb4c55fa346b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:22:46 +0000

    8410278b6d docs: copy edit getting started step 4 (#32839)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+521.sha-9b9116c
+ * @license Angular v9.0.0-rc.1+522.sha-8410278
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18177,7 +18177,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+521.sha-9b9116c');
+const VERSION$1 = new Version('9.0.0-rc.1+522.sha-8410278');
 
 /**
  * @license
