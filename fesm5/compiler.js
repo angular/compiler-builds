9c11edd543b5c2d64adf147638962c6e66a7cb51
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:38:06 +0000

    1de49ba369 refactor(ivy): consistently translate types to `ts.TypeNode` (#34021)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+523.sha-19944c2
+ * @license Angular v9.0.0-rc.1+525.sha-1de49ba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19266,7 +19266,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+523.sha-19944c2');
+var VERSION$1 = new Version('9.0.0-rc.1+525.sha-1de49ba');
 
 /**
  * @license
