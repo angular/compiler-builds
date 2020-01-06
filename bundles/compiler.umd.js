95dce1cc38b57cee078e666f683b1e1003be3277
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:45:29 +0000

    eab7f9f101 docs: add a comma (#34426) PR Close #34426

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+528.sha-e14e43b
+ * @license Angular v9.0.0-rc.1+529.sha-eab7f9f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19444,7 +19444,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+528.sha-e14e43b');
+    var VERSION$1 = new Version('9.0.0-rc.1+529.sha-eab7f9f');
 
     /**
      * @license
