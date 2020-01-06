0b4eb48e484cb284446a5bc3310d31da6889129e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 20:29:51 +0000

    5b864ede13 fix(ivy): TestBed not unwrapping imports array function when overriding provider (#34629)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+530.sha-ba2fd31
+ * @license Angular v9.0.0-rc.1+533.sha-5b864ed
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19444,7 +19444,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+530.sha-ba2fd31');
+    var VERSION$1 = new Version('9.0.0-rc.1+533.sha-5b864ed');
 
     /**
      * @license
