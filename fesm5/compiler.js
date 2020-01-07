46e2960454a40fe161f4118ffed35568f1c1b883
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 18:50:23 +0000

    effb92dfae fix(ivy): skip field inheritance if InheritDefinitionFeature is present on parent def (#34244)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+533.sha-5b864ed
+ * @license Angular v9.0.0-rc.1+534.sha-effb92d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19266,7 +19266,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+533.sha-5b864ed');
+var VERSION$1 = new Version('9.0.0-rc.1+534.sha-effb92d');
 
 /**
  * @license
