bb14aaa428cfdd4bbfcded3d0817abb45612c323
atscott <atscott@google.com>
2020-01-09 23:34:06 +0000

    9c5510b28e docs(ivy): document breaking change with <select> and binding order (#34709)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+583.sha-c3f14bb
+ * @license Angular v9.0.0-rc.1+585.sha-9c5510b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+583.sha-c3f14bb');
+const VERSION$1 = new Version('9.0.0-rc.1+585.sha-9c5510b');
 
 /**
  * @license
