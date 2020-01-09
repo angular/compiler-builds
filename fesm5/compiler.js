ca13b76f5fb17201df6a036098f5665d2cdc8711
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 00:05:17 +0000

    8815ace418 fix(ngcc): insert definitions after statement (#34677)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+557.sha-02e4921
+ * @license Angular v9.0.0-rc.1+574.sha-8815ace
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+557.sha-02e4921');
+var VERSION$1 = new Version('9.0.0-rc.1+574.sha-8815ace');
 
 /**
  * @license
