25542040105cd6e7a3b666ee9b08daf7b5f85ad6
atscott <atscott@google.com>
2020-01-09 21:51:19 +0000

    0083443050 docs: add note on importing AppRoutingModule (#34710)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+574.sha-8815ace
+ * @license Angular v9.0.0-rc.1+575.sha-0083443
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+574.sha-8815ace');
+const VERSION$1 = new Version('9.0.0-rc.1+575.sha-0083443');
 
 /**
  * @license
