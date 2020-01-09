ac1ca5b1bdf4d2fad7a497e6436c442f0f1c3e9f
atscott <atscott@google.com>
2020-01-09 23:35:28 +0000

    f7a2ed7d66 docs(ivy): document breaking change with <select> and binding order (#34709)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+18.sha-f295240
+ * @license Angular v9.0.0-rc.8+29.sha-f7a2ed7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.8+18.sha-f295240');
+    var VERSION$1 = new Version('9.0.0-rc.8+29.sha-f7a2ed7');
 
     /**
      * @license
