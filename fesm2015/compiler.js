af8fea06c858f5aacd8b400f9d404697053af510
atscott <atscott@google.com>
2020-01-10 22:32:50 +0000

    c3490f7092 Revert "build: upgrade to rules_nodejs 1.0.0 (first stable release) (#34589)" (#34730)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+55.sha-a5cdad5
+ * @license Angular v9.0.0-rc.8+73.sha-c3490f7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('0.0.0');
+const VERSION$1 = new Version('9.0.0-rc.8+73.sha-c3490f7');
 
 /**
  * @license
