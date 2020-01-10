2ecf45aff520a61160e03b3262c05c15a60b9b58
atscott <atscott@google.com>
2020-01-10 00:35:09 +0000

    25eaff447a refactor(core): remove duplicate InjectableType declaration (#34679)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+585.sha-9c5510b
+ * @license Angular v9.0.0-rc.1+586.sha-25eaff4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+585.sha-9c5510b');
+const VERSION$1 = new Version('9.0.0-rc.1+586.sha-25eaff4');
 
 /**
  * @license
