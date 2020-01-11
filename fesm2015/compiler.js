9c6097d6dad61e5c519352953e5578c1be21f363
atscott <atscott@google.com>
2020-01-11 00:14:20 +0000

    6d534f10e6 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+631.sha-6d28a20
+ * @license Angular v9.0.0-rc.1+632.sha-6d534f1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+631.sha-6d28a20');
+const VERSION$1 = new Version('9.0.0-rc.1+632.sha-6d534f1');
 
 /**
  * @license
