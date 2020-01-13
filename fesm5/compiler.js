24728d3deca81be2e6d4fed5ca1e59a2c85946f5
atscott <atscott@google.com>
2020-01-13 18:24:42 +0000

    f1cdb8f15d fix(ivy): Prevent errors when querying DebugElement roots that were outside angular context (#34687)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+88.sha-ff02ddf
+ * @license Angular v9.0.0-rc.8+89.sha-f1cdb8f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.8+88.sha-ff02ddf');
+var VERSION$1 = new Version('9.0.0-rc.8+89.sha-f1cdb8f');
 
 /**
  * @license
