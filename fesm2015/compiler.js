5473eb9e04f0fa5bc723ddc3cb8af44a34a96cd2
atscott <atscott@google.com>
2020-01-13 15:44:10 +0000

    55037d1107 build: bump year (#34651)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v9.0.0-rc.1+632.sha-6d534f1
- * (c) 2010-2019 Google LLC. https://angular.io/
+ * @license Angular v9.0.0-rc.1+640.sha-55037d1
+ * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+632.sha-6d534f1');
+const VERSION$1 = new Version('9.0.0-rc.1+640.sha-55037d1');
 
 /**
  * @license
