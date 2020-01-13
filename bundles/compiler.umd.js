ef94653d4f496d001bc8fd02cb1bd1b03e6d6efb
atscott <atscott@google.com>
2020-01-13 23:25:41 +0000

    15b4173a76 refactor(language-service): create attr for missing attr, bound tmpl (#34743)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+650.sha-7d40185
+ * @license Angular v9.0.0-rc.1+651.sha-15b4173
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+650.sha-7d40185');
+    var VERSION$1 = new Version('9.0.0-rc.1+651.sha-15b4173');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+650.sha-7d40185
+ * @license Angular v9.0.0-rc.1+651.sha-15b4173
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+650.sha-7d40185');
+const VERSION$1 = new Version('9.0.0-rc.1+651.sha-15b4173');
 
 /**
  * @license
