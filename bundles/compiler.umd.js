67367c5df01255a269c808482c0370048f27c7eb
atscott <atscott@google.com>
2020-01-13 23:39:32 +0000

    7325053dfa refactor(language-service): adds Symbol#typeArguments and does cleanup (#34571)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+651.sha-15b4173
+ * @license Angular v9.0.0-rc.1+652.sha-7325053
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+651.sha-15b4173');
+    var VERSION$1 = new Version('9.0.0-rc.1+652.sha-7325053');
 
     /**
      * @license
