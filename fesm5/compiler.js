a222dbb7b9b2c10e9d821834baf7735f5364172e
atscott <atscott@google.com>
2020-01-13 15:44:42 +0000

    a0eb57fb81 docs: replace from class hero to interface with accordance with # 344022 (#34294) Fixes #21186

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v9.0.0-rc.8+76.sha-08c2581
- * (c) 2010-2019 Google LLC. https://angular.io/
+ * @license Angular v9.0.0-rc.8+85.sha-a0eb57f
+ * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.8+76.sha-08c2581');
+var VERSION$1 = new Version('9.0.0-rc.8+85.sha-a0eb57f');
 
 /**
  * @license
