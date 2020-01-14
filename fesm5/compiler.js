c5baa58e51e9789d49f5bc833abfac653aa0622c
atscott <atscott@google.com>
2020-01-14 18:06:34 +0000

    277681096d fix(ivy): properly bootstrap components with attribute selectors (#34450)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+657.sha-1a45387
+ * @license Angular v9.0.0-rc.1+658.sha-2776810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+657.sha-1a45387');
+var VERSION$1 = new Version('9.0.0-rc.1+658.sha-2776810');
 
 /**
  * @license
