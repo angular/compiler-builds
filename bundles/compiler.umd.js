4d7f973b333f4334d1f4132bcef909ac0345ef76
atscott <atscott@google.com>
2020-01-14 22:00:46 +0000

    84c659e246 refactor(language-service): Construct proper template AST from HTML ast (#34764)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+668.sha-ec147bd
+ * @license Angular v9.0.0-rc.1+670.sha-84c659e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+668.sha-ec147bd');
+    var VERSION$1 = new Version('9.0.0-rc.1+670.sha-84c659e');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+668.sha-ec147bd
+ * @license Angular v9.0.0-rc.1+670.sha-84c659e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+668.sha-ec147bd');
+var VERSION$1 = new Version('9.0.0-rc.1+670.sha-84c659e');
 
 /**
  * @license
