da39104f93c156fdab18cfd1eaec5ae616538cf5
atscott <atscott@google.com>
2020-01-14 23:13:48 +0000

    9ceee07d83 fix(forms): accept number length in length validators (#32057)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+671.sha-d863526
+ * @license Angular v9.0.0-rc.1+672.sha-9ceee07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+671.sha-d863526');
+    var VERSION$1 = new Version('9.0.0-rc.1+672.sha-9ceee07');
 
     /**
      * @license
