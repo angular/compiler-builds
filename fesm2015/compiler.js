f0a69605751df9ed3e471ad349c82a1704ac6782
atscott <atscott@google.com>
2020-01-14 17:50:42 +0000

    1a453872c8 fix(language-service): apply suggested change. (#34564)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+652.sha-7325053
+ * @license Angular v9.0.0-rc.1+657.sha-1a45387
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+652.sha-7325053');
+const VERSION$1 = new Version('9.0.0-rc.1+657.sha-1a45387');
 
 /**
  * @license
