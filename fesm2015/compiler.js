278c2da17b7610d28fc83680f701c885de0daf53
atscott <atscott@google.com>
2020-01-14 18:50:36 +0000

    ec147bd839 docs: remove old contributors (#34486)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+659.sha-280aae6
+ * @license Angular v9.0.0-rc.1+668.sha-ec147bd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+659.sha-280aae6');
+const VERSION$1 = new Version('9.0.0-rc.1+668.sha-ec147bd');
 
 /**
  * @license
