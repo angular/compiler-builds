bdebe6a3aa1166cf67a9e05c0edb87a98471323a
atscott <atscott@google.com>
2020-01-14 22:03:54 +0000

    23cbfa791c docs: expand doc for app_initializer token (#34760)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+670.sha-84c659e
+ * @license Angular v9.0.0-rc.1+669.sha-23cbfa7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+670.sha-84c659e');
+const VERSION$1 = new Version('9.0.0-rc.1+669.sha-23cbfa7');
 
 /**
  * @license
