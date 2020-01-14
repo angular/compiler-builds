bb829b2308cf3c511a965dab937445dcdbb8a6cd
atscott <atscott@google.com>
2020-01-14 18:27:48 +0000

    280aae6fc5 docs: edit injectable decorator doc (#34218)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+658.sha-2776810
+ * @license Angular v9.0.0-rc.1+659.sha-280aae6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+658.sha-2776810');
+    var VERSION$1 = new Version('9.0.0-rc.1+659.sha-280aae6');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+658.sha-2776810
+ * @license Angular v9.0.0-rc.1+659.sha-280aae6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+658.sha-2776810');
+var VERSION$1 = new Version('9.0.0-rc.1+659.sha-280aae6');
 
 /**
  * @license
