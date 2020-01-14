abab32e7dc74a5e5ddde457c26e0085fd315852f
atscott <atscott@google.com>
2020-01-14 23:47:57 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+const VERSION$1 = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19250,7 +19250,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+var VERSION$1 = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
 /**
  * @license
