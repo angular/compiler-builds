da79cb4ea425b2ab540134a75b2261764d5d3d3a
GitHub <noreply@github.com>
2020-01-15 21:35:07 +0000

    1fd4d4f0d4 fixup! docs: fix public api tool name

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-966cb35
+ * @license Angular v9.0.0-rc.1+693.sha-1fd4d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+692.sha-966cb35');
+const VERSION$1 = new Version('9.0.0-rc.1+693.sha-1fd4d4f');
 
 /**
  * @license
