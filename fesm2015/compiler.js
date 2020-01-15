30adf2405a6fb8a1483aa9c912f03e5774326503
Matias Niemelä <matias@yearofmoo.com>
2020-01-15 20:56:10 +0000

    436e7388c3 build: update to rules_nodejs 1.1.0 (#34736)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
+ * @license Angular v9.0.0-rc.1+710.sha-436e738
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18162,7 +18162,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+692.sha-3d5bcd5');
+const VERSION$1 = new Version('0.0.0');
 
 /**
  * @license
