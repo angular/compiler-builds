38cd26582de8308b8da3c82d11c38a819b59545e
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:18:19 +0000

    92c411f86d docs: update payload size limits for angular.io application (#34913)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
+ * @license Angular v9.0.0-rc.1+755.sha-92c411f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18195,7 +18195,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+750.sha-e1fc44f');
+const VERSION$1 = new Version('9.0.0-rc.1+755.sha-92c411f');
 
 /**
  * @license
