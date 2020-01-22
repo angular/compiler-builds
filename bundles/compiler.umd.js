6020aa98a86de9e04671cb467d6aec60c18e074a
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:17:16 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
+ * @license Angular v9.0.0-rc.1+747.sha-61ad50d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19460,7 +19460,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+746.sha-e7cf37d');
+    var VERSION$1 = new Version('9.0.0-rc.1+747.sha-61ad50d');
 
     /**
      * @license
