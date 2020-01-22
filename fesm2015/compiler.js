a8b686247b5066fe8aa51a776cd861ddd7a5df58
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:26:11 +0000

    34c30ae0aa build(docs-infra): always specify a landing file for StackBlitz projects (#34553)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+9.sha-20dc436
+ * @license Angular v9.0.0-rc.10+14.sha-34c30ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18195,7 +18195,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.10+9.sha-20dc436');
+const VERSION$1 = new Version('9.0.0-rc.10+14.sha-34c30ae');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+9.sha-20dc436
+ * @license Angular v9.0.0-rc.10+14.sha-34c30ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.10+9.sha-20dc436');
+var VERSION$1 = new Version('9.0.0-rc.10+14.sha-34c30ae');
 
 /**
  * @license
