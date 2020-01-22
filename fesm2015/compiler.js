840951f61727b4cd29f7cdef495dd4dcb1a7ad0d
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:18:50 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
+ * @license Angular v9.0.0-rc.1+747.sha-61ad50d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18186,7 +18186,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+746.sha-e7cf37d');
+const VERSION$1 = new Version('9.0.0-rc.1+747.sha-61ad50d');
 
 /**
  * @license
