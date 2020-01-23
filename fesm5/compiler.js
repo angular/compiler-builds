9af8c072aa436a7f6af5f7f3df3b46a0d4783207
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:27:10 +0000

    48f8ca5483 build: upgrade to bazel 2.0.0 (#34877)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+761.sha-32b72f3
+ * @license Angular v9.0.0-rc.1+762.sha-48f8ca5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+761.sha-32b72f3');
+var VERSION$1 = new Version('9.0.0-rc.1+762.sha-48f8ca5');
 
 /**
  * @license
