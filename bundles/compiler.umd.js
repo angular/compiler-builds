d9485b6db6e022c067a702ade20ce0c27cb0961f
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:06:39 +0000

    0c8d085666 fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+767.sha-6b468f9
+ * @license Angular v9.0.0-rc.1+773.sha-0c8d085
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19469,7 +19469,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+767.sha-6b468f9');
+    var VERSION$1 = new Version('9.0.0-rc.1+773.sha-0c8d085');
 
     /**
      * @license
