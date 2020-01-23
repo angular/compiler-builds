bccaac594e09487421965e6160a7f8778a70f605
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:04:32 +0000

    e69d02c69f fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+22.sha-3999d7a
+ * @license Angular v9.0.0-rc.10+34.sha-e69d02c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.10+22.sha-3999d7a');
+var VERSION$1 = new Version('9.0.0-rc.10+34.sha-e69d02c');
 
 /**
  * @license
