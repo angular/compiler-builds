82cd8f88c6b68b68f9803b965220ef5a27a83eae
Andrew Kushnir <akushnir@google.com>
2020-01-23 00:12:18 +0000

    32b72f39f0 fix(ivy): ensure eventListeners added outside angular context are not called... (#34514)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+760.sha-d15cf60
+ * @license Angular v9.0.0-rc.1+761.sha-32b72f3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18195,7 +18195,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+760.sha-d15cf60');
+const VERSION$1 = new Version('9.0.0-rc.1+761.sha-32b72f3');
 
 /**
  * @license
