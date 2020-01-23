012fef7dec79842aae82c32f6843a0c84bc059b6
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:35:08 +0000

    68025ce09a test(language-service): recognize inputs/ouputs declared in decorator (#34875)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19469,7 +19469,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+    var VERSION$1 = new Version('9.0.0-rc.1+764.sha-68025ce');
 
     /**
      * @license
