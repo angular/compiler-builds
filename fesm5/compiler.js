b627cae7452a02b34db54019da15c4f4c742a61e
Andrew Kushnir <akushnir@google.com>
2020-01-23 20:46:20 +0000

    4ba478267e feat(language-service): specific suggestions for template context diags (#34751)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+776.sha-eb4b7d2
+ * @license Angular v9.0.0-rc.1+777.sha-4ba4782
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+776.sha-eb4b7d2');
+var VERSION$1 = new Version('9.0.0-rc.1+777.sha-4ba4782');
 
 /**
  * @license
