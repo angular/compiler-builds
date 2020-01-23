33bc9a6ba9084e9c56fec2718d28afffdca20cec
Andrew Kushnir <akushnir@google.com>
2020-01-23 22:25:32 +0000

    c80392bcd2 fix(ngcc): do not collect private declarations from external packages (#34811)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+42.sha-43e2fd2
+ * @license Angular v9.0.0-rc.10+45.sha-c80392b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19469,7 +19469,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.10+42.sha-43e2fd2');
+    var VERSION$1 = new Version('9.0.0-rc.10+45.sha-c80392b');
 
     /**
      * @license
