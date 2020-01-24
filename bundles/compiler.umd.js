6e04e093d79f455a13b7b339754c789a94784942
Andrew Kushnir <akushnir@google.com>
2020-01-24 19:54:46 +0000

    65354fb4ac ci(docs-infra): use the correct version of yarn in `aio_monitoring_stable` (#34954)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+51.sha-bca432c
+ * @license Angular v9.0.0-rc.10+52.sha-65354fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19469,7 +19469,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.10+51.sha-bca432c');
+    var VERSION$1 = new Version('9.0.0-rc.10+52.sha-65354fb');
 
     /**
      * @license
