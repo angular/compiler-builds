512e0e35438c5cf01bee7631e6b0276857a5c8ae
Andrew Kushnir <akushnir@google.com>
2020-01-24 17:24:33 +0000

    31e9dda2c8 perf(ivy): add more styling use-cases to benchmarks (#34923)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+789.sha-0223aa6
+ * @license Angular v9.0.0-rc.1+790.sha-31e9dda
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19291,7 +19291,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+789.sha-0223aa6');
+var VERSION$1 = new Version('9.0.0-rc.1+790.sha-31e9dda');
 
 /**
  * @license
