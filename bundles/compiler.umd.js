310e99fedcb1cbc2b5fb977d0d31672597552e54
Miško Hevery <misko@hevery.com>
2020-01-24 20:51:36 +0000

    9bd9590767 refactor(ivy): change styling to use programmatic API on updates (#34804)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+804.sha-0eaf874
+ * @license Angular v9.0.0-rc.1+806.sha-9bd9590
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+804.sha-0eaf874');
+    var VERSION$1 = new Version('9.0.0-rc.1+806.sha-9bd9590');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+804.sha-0eaf874
+ * @license Angular v9.0.0-rc.1+806.sha-9bd9590
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18239,7 +18239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+804.sha-0eaf874');
+const VERSION$1 = new Version('9.0.0-rc.1+806.sha-9bd9590');
 
 /**
  * @license
