ef94dfdcbf85fb97bcb87340b7691f7651eceb1d
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:17:22 +0000

    b50ed5c22c fix(language-service): Make metadata in Declaration non-optional (#34936)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+787.sha-7659f2e
+ * @license Angular v9.0.0-rc.1+788.sha-b50ed5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19469,7 +19469,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+787.sha-7659f2e');
+    var VERSION$1 = new Version('9.0.0-rc.1+788.sha-b50ed5c');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+787.sha-7659f2e
+ * @license Angular v9.0.0-rc.1+788.sha-b50ed5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18195,7 +18195,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+787.sha-7659f2e');
+const VERSION$1 = new Version('9.0.0-rc.1+788.sha-b50ed5c');
 
 /**
  * @license
