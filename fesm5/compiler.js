150de36496c06e5f7ac883aa5f81526d5941e11e
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:06:08 +0000

    8926f764a5 docs: release notes for the v9.0.0-rc.11 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+806.sha-9bd9590
+ * @license Angular v9.0.0-rc.1+807.sha-8926f76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19334,7 +19334,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+806.sha-9bd9590');
+var VERSION$1 = new Version('9.0.0-rc.1+807.sha-8926f76');
 
 /**
  * @license
