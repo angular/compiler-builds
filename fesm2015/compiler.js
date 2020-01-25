25592f39f255b5f066ad68a354941e5d509b33bf
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:13:43 +0000

    3414ef276e refactor(language-service): Avoid leaking host outside of LanguageService (#34941)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+807.sha-8926f76
+ * @license Angular v9.0.0-rc.1+808.sha-3414ef2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18239,7 +18239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+807.sha-8926f76');
+const VERSION$1 = new Version('9.0.0-rc.1+808.sha-3414ef2');
 
 /**
  * @license
