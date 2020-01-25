54a9dcfd9bfdfd99e694ecc86e718beceb98d993
Igor Minar <igor@angular.io>
2020-01-25 06:47:36 +0000

    bdd3865314 build(docs-infra): update CLI to 9.0.0-rc.10

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
+ * @license Angular v9.0.0-rc.11+5.sha-bdd3865
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.11+4.sha-8fca9b6');
+    var VERSION$1 = new Version('9.0.0-rc.11+5.sha-bdd3865');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
+ * @license Angular v9.0.0-rc.11+5.sha-bdd3865
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18239,7 +18239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.11+4.sha-8fca9b6');
+const VERSION$1 = new Version('9.0.0-rc.11+5.sha-bdd3865');
 
 /**
  * @license
