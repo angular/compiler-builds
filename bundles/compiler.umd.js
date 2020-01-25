c23783ba9251f58552b693242f069bf83792ce2d
Igor Minar <igor@angular.io>
2020-01-25 06:12:42 +0000

    be8d06c6ea build(docs-infra): update CLI to 9.0.0-rc.10

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+808.sha-3414ef2
+ * @license Angular v9.0.0-rc.1+809.sha-be8d06c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+808.sha-3414ef2');
+    var VERSION$1 = new Version('9.0.0-rc.1+809.sha-be8d06c');
 
     /**
      * @license
