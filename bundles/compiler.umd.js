ad0337a5ad19b9de8870230316d87d0d24dd4616
Igor Minar <igor@angular.io>
2020-01-26 23:14:32 +0000

    4a6093d002 test: add 'localize' as an allowed commit message scope (#34974)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+5.sha-bdd3865
+ * @license Angular v9.0.0-rc.11+10.sha-4a6093d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.11+5.sha-bdd3865');
+    var VERSION$1 = new Version('9.0.0-rc.11+10.sha-4a6093d');
 
     /**
      * @license
