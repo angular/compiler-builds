ee6007218e7cbca4100450b76fa245a1718752db
Igor Minar <igor@angular.io>
2020-01-26 23:18:59 +0000

    3d4b1431ec test: add 'localize' as an allowed commit message scope (#34974)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+809.sha-be8d06c
+ * @license Angular v9.0.0-rc.1+814.sha-3d4b143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19334,7 +19334,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+809.sha-be8d06c');
+var VERSION$1 = new Version('9.0.0-rc.1+814.sha-3d4b143');
 
 /**
  * @license
