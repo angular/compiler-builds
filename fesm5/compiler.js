d6ee5447b58e609b6b01a9a9db1f455308603777
Andrew Kushnir <akushnir@google.com>
2020-01-27 21:00:13 +0000

    7069a83727 fix(localize): re-enable filename in code-frame error messages (#34994)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+827.sha-6e5cfd2
+ * @license Angular v9.0.0-rc.1+828.sha-7069a83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19334,7 +19334,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+827.sha-6e5cfd2');
+var VERSION$1 = new Version('9.0.0-rc.1+828.sha-7069a83');
 
 /**
  * @license
