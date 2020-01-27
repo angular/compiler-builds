cc637f130294668a12d9a529034ced3d5a2d8071
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:13:10 +0000

    549ab2ecc3 build(docs-infra): add words to ignore in snippets (#34915)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+823.sha-09869af
+ * @license Angular v9.0.0-rc.1+824.sha-549ab2e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+823.sha-09869af');
+    var VERSION$1 = new Version('9.0.0-rc.1+824.sha-549ab2e');
 
     /**
      * @license
