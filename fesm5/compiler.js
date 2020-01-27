0986657ca318f0f6554493df399fca576ddbec8e
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:59:57 +0000

    b3bd6ca925 build: update API Golden files after node 12 update (#34955)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+815.sha-c349cb2
+ * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19334,7 +19334,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+815.sha-c349cb2');
+var VERSION$1 = new Version('9.0.0-rc.1+819.sha-b3bd6ca');
 
 /**
  * @license
