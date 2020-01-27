efb5f2bf0b553782fff9de86ced78894e9d8307a
Andrew Kushnir <akushnir@google.com>
2020-01-27 20:44:23 +0000

    c7c7b20684 fix(localize): re-enable filename in code-frame error messages (#34994)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+23.sha-c0e73e0
+ * @license Angular v9.0.0-rc.11+24.sha-c7c7b20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19512,7 +19512,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.11+23.sha-c0e73e0');
+    var VERSION$1 = new Version('9.0.0-rc.11+24.sha-c7c7b20');
 
     /**
      * @license
