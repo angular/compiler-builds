ab853348cdd8936995ae0e57da0767e3b98e815c
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:26:10 +0000

    6e5cfd2cd2 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+824.sha-549ab2e
+ * @license Angular v9.0.0-rc.1+827.sha-6e5cfd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18239,7 +18239,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+824.sha-549ab2e');
+const VERSION$1 = new Version('9.0.0-rc.1+827.sha-6e5cfd2');
 
 /**
  * @license
