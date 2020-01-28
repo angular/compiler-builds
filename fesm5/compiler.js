d2197254b10a95e28b1455daf7ad89c8ae241a0e
Andrew Kushnir <akushnir@google.com>
2020-01-28 21:25:58 +0000

    a5a598104f docs: change p to pointer for better understanding (#34927)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+836.sha-1cee9b3
+ * @license Angular v9.0.0-rc.1+837.sha-a5a5981
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+836.sha-1cee9b3');
+var VERSION$1 = new Version('9.0.0-rc.1+837.sha-a5a5981');
 
 /**
  * @license
