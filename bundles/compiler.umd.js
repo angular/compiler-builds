548c7c10dfe326d9e447faa6baa65254011b3495
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:57:24 +0000

    d15d6b7a63 ci: fix pullapprove_conditions (#35011)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+834.sha-296dc06
+ * @license Angular v9.0.0-rc.1+835.sha-d15d6b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+834.sha-296dc06');
+    var VERSION$1 = new Version('9.0.0-rc.1+835.sha-d15d6b7');
 
     /**
      * @license
