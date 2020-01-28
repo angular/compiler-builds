f58b7ef8d2c169e3b66ae7aeda01fae054e523d5
Andrew Kushnir <akushnir@google.com>
2020-01-28 17:29:43 +0000

    e7c74cbd69 feat(language-service): completions for output $event properties in (#34570)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+830.sha-90b303f
+ * @license Angular v9.0.0-rc.1+832.sha-e7c74cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+830.sha-90b303f');
+var VERSION$1 = new Version('9.0.0-rc.1+832.sha-e7c74cb');
 
 /**
  * @license
