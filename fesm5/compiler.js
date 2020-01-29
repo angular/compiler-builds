72ea7743affc8b94cd14d3cff219e512cb58be4e
Andrew Kushnir <akushnir@google.com>
2020-01-29 23:36:22 +0000

    0390d20d60 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+58.sha-9b7d703
+ * @license Angular v9.0.0-rc.11+59.sha-0390d20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.11+58.sha-9b7d703');
+var VERSION$1 = new Version('9.0.0-rc.11+59.sha-0390d20');
 
 /**
  * @license
