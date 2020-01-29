bafd0b87891244dc71e966f46f3837257efb9bec
Andrew Kushnir <akushnir@google.com>
2020-01-29 20:43:03 +0000

    519e9e1ae8 ci: minor PullApprove fixes/improvements (#35015)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+850.sha-8fd2a97
+ * @license Angular v9.0.0-rc.1+855.sha-519e9e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+850.sha-8fd2a97');
+var VERSION$1 = new Version('9.0.0-rc.1+855.sha-519e9e1');
 
 /**
  * @license
