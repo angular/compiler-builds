52f497acd31831015b2212cb0d48a29a4e8c2f8a
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:35:27 +0000

    95383ee6a9 ci: don't run benchmark_tests on remote executors (#34996)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+858.sha-d5205a0
+ * @license Angular v9.0.0-rc.1+861.sha-95383ee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+858.sha-d5205a0');
+const VERSION$1 = new Version('9.0.0-rc.1+861.sha-95383ee');
 
 /**
  * @license
