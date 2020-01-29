6d8513d1cead8943e51d7c1aab5d20dfb1042f48
Andrew Kushnir <akushnir@google.com>
2020-01-29 17:48:40 +0000

    d43187f7ef ci(docs-infra): ignore node version in `aio_monitoring_stable` CI job (for real) (#35033)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+848.sha-fb852f7
+ * @license Angular v9.0.0-rc.1+849.sha-d43187f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+848.sha-fb852f7');
+const VERSION$1 = new Version('9.0.0-rc.1+849.sha-d43187f');
 
 /**
  * @license
