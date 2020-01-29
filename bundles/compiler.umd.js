8083f5f38bf8048769d3a111b052832c0699ac2e
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:29:02 +0000

    7e0a031543 docs: remove named node version (#35014)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+    var VERSION$1 = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+53.sha-eaa4a5a
+ * @license Angular v9.0.0-rc.11+54.sha-7e0a031
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.11+53.sha-eaa4a5a');
+const VERSION$1 = new Version('9.0.0-rc.11+54.sha-7e0a031');
 
 /**
  * @license
