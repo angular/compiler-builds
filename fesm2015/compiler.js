4e40e529807eda1b33c04ab895faefe1a740efbb
Andrew Kushnir <akushnir@google.com>
2020-01-29 22:36:32 +0000

    d5205a01cb build: update to @angular/cli@9.0.0-rc.11 (#35042)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+860.sha-7ca7437
+ * @license Angular v9.0.0-rc.1+858.sha-d5205a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+860.sha-7ca7437');
+const VERSION$1 = new Version('9.0.0-rc.1+858.sha-d5205a0');
 
 /**
  * @license
