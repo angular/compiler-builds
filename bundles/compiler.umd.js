6de16833e6d10eeabe8a5198b6037add01835d83
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:30:38 +0000

    a892054448 docs: remove named node version (#35014)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+855.sha-519e9e1
+ * @license Angular v9.0.0-rc.1+857.sha-a892054
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+855.sha-519e9e1');
+    var VERSION$1 = new Version('9.0.0-rc.1+857.sha-a892054');
 
     /**
      * @license
