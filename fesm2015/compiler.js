a126d46b8e2fe6ea5d788407ee4b8a3ab0aed84f
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:25:10 +0000

    23c0147f25 fix(ivy): ensure `DebugNode`/`DebugElement` are tree-shakeable in Ivy (#35003)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+35.sha-fd03709
+ * @license Angular v9.0.0-rc.11+40.sha-23c0147
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.11+35.sha-fd03709');
+const VERSION$1 = new Version('9.0.0-rc.11+40.sha-23c0147');
 
 /**
  * @license
