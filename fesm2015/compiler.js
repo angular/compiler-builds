295667c2d02b6c4f506846984bc61fb032d60401
Andrew Kushnir <akushnir@google.com>
2020-01-29 19:14:57 +0000

    e554f2d3ed docs: ng-conf 2020 date change (#35019)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+46.sha-2d377b0
+ * @license Angular v9.0.0-rc.11+47.sha-e554f2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.11+46.sha-2d377b0');
+const VERSION$1 = new Version('9.0.0-rc.11+47.sha-e554f2d');
 
 /**
  * @license
