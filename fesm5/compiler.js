817560a30813266afeb0002bae3f7f91f645dca3
Misko Hevery <misko@hevery.com>
2020-01-31 19:52:47 +0000

    1799f18264 feat(docs-infra): change navigation in resources page (#34756)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.12+1.sha-3bdf116
+ * @license Angular v9.0.0-rc.12+9.sha-1799f18
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.12+1.sha-3bdf116');
+var VERSION$1 = new Version('9.0.0-rc.12+9.sha-1799f18');
 
 /**
  * @license
