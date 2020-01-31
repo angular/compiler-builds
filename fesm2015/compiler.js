8f3f59cf2b91c17746761814d366793b8ce11481
Misko Hevery <misko@hevery.com>
2020-01-31 22:55:53 +0000

    6d11a81994 fix(compiler-cli): add `sass` as a valid css preprocessor extension (#35052)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+887.sha-58b29f1
+ * @license Angular v9.0.0-rc.1+888.sha-6d11a81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+887.sha-58b29f1');
+const VERSION$1 = new Version('9.0.0-rc.1+888.sha-6d11a81');
 
 /**
  * @license
