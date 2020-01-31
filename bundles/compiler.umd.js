f755f3dbd49e02377795191dfcbb807a65d89487
Misko Hevery <misko@hevery.com>
2020-01-31 19:34:02 +0000

    68f51e0adc ci: update pullapprove.yml to cover all files in the repo (#35072)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+871.sha-a25cb2d
+ * @license Angular v9.0.0-rc.1+873.sha-68f51e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+871.sha-a25cb2d');
+    var VERSION$1 = new Version('9.0.0-rc.1+873.sha-68f51e0');
 
     /**
      * @license
