49e5bcad8194c15005b18be36190dd802bfc3c40
Miško Hevery <misko@hevery.com>
2020-02-04 19:07:36 +0000

    1a23c79835 docs: changes AoT to AOT for consistency (#35112)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+2.sha-a320dc9
+ * @license Angular v9.0.0-rc.14+12.sha-1a23c79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.14+2.sha-a320dc9');
+    var VERSION$1 = new Version('9.0.0-rc.14+12.sha-1a23c79');
 
     /**
      * @license
