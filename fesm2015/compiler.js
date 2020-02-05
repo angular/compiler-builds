21a0650daedbf7ae8f24b788813b8b501cf820b3
Miško Hevery <misko@hevery.com>
2020-02-05 00:20:08 +0000

    491654d882 fix(docs-infra): fix CSS issues on home page and search results (#35098)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
+ * @license Angular v9.0.0-rc.1+925.sha-491654d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+919.sha-7fb66ec');
+const VERSION$1 = new Version('9.0.0-rc.1+925.sha-491654d');
 
 /**
  * @license
