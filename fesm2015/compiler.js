bb6a283f5cf5248d3ca9a09bcd696baa96a53519
Kara Erickson <karakara@google.com>
2020-02-06 22:51:01 +0000

    bcb539f6de docs: remove the release schedule from docs (#34474)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+7.sha-f4b9d66
+ * @license Angular v9.0.0+9.sha-bcb539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0+7.sha-f4b9d66');
+const VERSION$1 = new Version('9.0.0+9.sha-bcb539f');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+7.sha-f4b9d66
+ * @license Angular v9.0.0+9.sha-bcb539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0+7.sha-f4b9d66');
+var VERSION$1 = new Version('9.0.0+9.sha-bcb539f');
 
 /**
  * @license
