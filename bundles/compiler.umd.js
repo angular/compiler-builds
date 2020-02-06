b73d776e70508fcfc786b2de1ce514d1633ac197
Kara Erickson <karakara@google.com>
2020-02-06 22:51:25 +0000

    532ccb647c docs: remove the release schedule from docs (#34474)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+940.sha-ba0cdd3
+ * @license Angular v9.0.0-rc.1+941.sha-532ccb6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+940.sha-ba0cdd3');
+    var VERSION$1 = new Version('9.0.0-rc.1+941.sha-532ccb6');
 
     /**
      * @license
