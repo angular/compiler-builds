0b23d212085b630a47e25edef7d3a7a0f99712bc
Misko Hevery <misko@hevery.com>
2020-02-06 23:18:17 +0000

    60b887090c docs: consilidate release notes for the v8.0.0 release (#35197)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+9.sha-bcb539f
+ * @license Angular v9.0.0+10.sha-60b8870
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0+9.sha-bcb539f');
+const VERSION$1 = new Version('9.0.0+10.sha-60b8870');
 
 /**
  * @license
