c4ed84e91618ef87a9df7cdb5b80243a28edf058
Misko Hevery <misko@hevery.com>
2020-02-06 23:56:17 +0000

    3c69442dbd feat(compiler-cli): implement NgTscPlugin on top of the NgCompiler API (#34792)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+942.sha-49dd7b6
+ * @license Angular v9.0.0-rc.1+944.sha-3c69442
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+942.sha-49dd7b6');
+const VERSION$1 = new Version('9.0.0-rc.1+944.sha-3c69442');
 
 /**
  * @license
