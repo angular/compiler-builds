5f657ad2fa7fc13625aac4bae4f4cb82efbce010
Misko Hevery <misko@hevery.com>
2020-02-06 17:54:19 +0000

    7526ef77b7 docs(ivy): add docs for styling priority order (#35066)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+930.sha-4fc2678
+ * @license Angular v9.0.0-rc.1+933.sha-7526ef7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+930.sha-4fc2678');
+    var VERSION$1 = new Version('9.0.0-rc.1+933.sha-7526ef7');
 
     /**
      * @license
