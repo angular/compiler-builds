1d2749b3fe175bb29c537bb91a0afd61633b9864
Misko Hevery <misko@hevery.com>
2020-02-06 23:59:03 +0000

    6fb538ed1d fix(bazel): update ibazel to 0.11.1 (#35158)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+944.sha-3c69442
+ * @license Angular v9.0.0-rc.1+948.sha-6fb538e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+944.sha-3c69442');
+var VERSION$1 = new Version('9.0.0-rc.1+948.sha-6fb538e');
 
 /**
  * @license
