49b66b2404e78e41581d19d8a26a47299efedc39
Misko Hevery <misko@hevery.com>
2020-02-06 02:40:23 +0000

    4fc2678438 docs(ivy): document breaking change with host attribute priority (#35175)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+929.sha-c73d718
+ * @license Angular v9.0.0-rc.1+930.sha-4fc2678
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+929.sha-c73d718');
+var VERSION$1 = new Version('9.0.0-rc.1+930.sha-4fc2678');
 
 /**
  * @license
