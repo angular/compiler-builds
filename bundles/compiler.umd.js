de150227b09ba56eb0f70acf294557c5ff270777
Misko Hevery <misko@hevery.com>
2020-02-07 18:22:46 +0000

    0cbdd54fd0 fix(ivy): ensure module imports are instantiated before the module being declared (#35172)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+958.sha-79742a3
+ * @license Angular v9.0.0-rc.1+959.sha-0cbdd54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19507,7 +19507,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+958.sha-79742a3');
+    var VERSION$1 = new Version('9.0.0-rc.1+959.sha-0cbdd54');
 
     /**
      * @license
