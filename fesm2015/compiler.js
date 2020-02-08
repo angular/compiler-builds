4c1a17e89306e4277ea84ee40767c7b606e7821d
Kara Erickson <karakara@google.com>
2020-02-08 01:43:17 +0000

    ae0253f34a fix(ivy): set namespace for host elements of dynamically created components (#35136)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+972.sha-d5d9971
+ * @license Angular v9.0.0-rc.1+973.sha-ae0253f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18234,7 +18234,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+972.sha-d5d9971');
+const VERSION$1 = new Version('9.0.0-rc.1+973.sha-ae0253f');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+972.sha-d5d9971
+ * @license Angular v9.0.0-rc.1+973.sha-ae0253f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19329,7 +19329,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+972.sha-d5d9971');
+var VERSION$1 = new Version('9.0.0-rc.1+973.sha-ae0253f');
 
 /**
  * @license
