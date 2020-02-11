c75aef5b4317ce68f527b5d683c913d746022d26
Kara Erickson <karakara@google.com>
2020-02-11 20:01:06 +0000

    332937ef24 feat: make mocha a zone module. (#34719)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+983.sha-81241af
+ * @license Angular v9.0.0-rc.1+984.sha-332937e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19528,7 +19528,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.0-rc.1+983.sha-81241af');
+    var VERSION$1 = new Version('9.0.0-rc.1+984.sha-332937e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+983.sha-81241af
+ * @license Angular v9.0.0-rc.1+984.sha-332937e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+983.sha-81241af');
+const VERSION$1 = new Version('9.0.0-rc.1+984.sha-332937e');
 
 /**
  * @license
