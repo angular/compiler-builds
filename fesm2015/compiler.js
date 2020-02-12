39485258790471f4b8f0db13fa6a585211557ecc
Kara Erickson <karakara@google.com>
2020-02-12 19:21:14 +0000

    4aeb6c4e78 docs: release notes for the v9.0.1 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+990.sha-eef047b
+ * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0-rc.1+990.sha-eef047b');
+const VERSION$1 = new Version('9.0.0-rc.1+991.sha-4aeb6c4');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+990.sha-eef047b
+ * @license Angular v9.0.0-rc.1+991.sha-4aeb6c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.0-rc.1+990.sha-eef047b');
+var VERSION$1 = new Version('9.0.0-rc.1+991.sha-4aeb6c4');
 
 /**
  * @license
