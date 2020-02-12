6a792016bce8f83247cb6e7afd53227936ddff14
Kara Erickson <karakara@google.com>
2020-02-12 01:24:09 +0000

    554c2cbd5c fix(forms): change Array.reduce usage to Array.forEach (#35349)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+47.sha-94d002b
+ * @license Angular v9.0.0+53.sha-554c2cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.0+47.sha-94d002b');
+const VERSION$1 = new Version('9.0.0+53.sha-554c2cb');
 
 /**
  * @license
