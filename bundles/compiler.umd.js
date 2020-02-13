737f49949500effe3b8f2a2d5ac8a02bb1211337
Kara Erickson <karakara@google.com>
2020-02-13 01:37:33 +0000

    d6bc63fa38 fix(ivy): error if directive with synthetic property binding is on same node as directive that injects ViewContainerRef (#35343)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+5.sha-a6aa35e
+ * @license Angular v9.1.0-next.0+7.sha-d6bc63f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19528,7 +19528,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+5.sha-a6aa35e');
+    var VERSION$1 = new Version('9.1.0-next.0+7.sha-d6bc63f');
 
     /**
      * @license
