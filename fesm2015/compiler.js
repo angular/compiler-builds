e87b777c6f72e5fdba483d5fa103f66927d75857
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-13 21:54:10 +0000

    db693f482f docs: Fix minor typos and coding styles (#35325)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+16.sha-84c9c6e
+ * @license Angular v9.0.1+17.sha-db693f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.1+16.sha-84c9c6e');
+const VERSION$1 = new Version('9.0.1+17.sha-db693f4');
 
 /**
  * @license
