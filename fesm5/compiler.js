59f10a1d6f028c2243a7a801b2d974e68eedb7e2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:42:12 +0000

    2ca7984d55 fix(core): remove support for `Map`/`Set` in `[class]`/`[style]` bindings (#35392)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+21.sha-b9b512f
+ * @license Angular v9.1.0-next.0+23.sha-2ca7984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.0+21.sha-b9b512f');
+var VERSION$1 = new Version('9.1.0-next.0+23.sha-2ca7984');
 
 /**
  * @license
