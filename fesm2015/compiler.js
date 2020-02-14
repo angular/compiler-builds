d5e20b3a6d72e02af23f01e63a77f34c6f6311f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 00:57:15 +0000

    27cd01f606 build: update to bazel_toolchains 2.1.0 (#35430)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+17.sha-9ca1faf
+ * @license Angular v9.1.0-next.0+20.sha-27cd01f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.0+17.sha-9ca1faf');
+const VERSION$1 = new Version('9.1.0-next.0+20.sha-27cd01f');
 
 /**
  * @license
