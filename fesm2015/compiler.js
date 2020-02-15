90a020330343dd63d560f048f7a598373bcb6256
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-15 00:05:59 +0000

    d05629be3b build: enable network for docker on remote executors (#35432)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+27.sha-939e2f7
+ * @license Angular v9.1.0-next.0+26.sha-d05629b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.0+27.sha-939e2f7');
+const VERSION$1 = new Version('9.1.0-next.0+26.sha-d05629b');
 
 /**
  * @license
