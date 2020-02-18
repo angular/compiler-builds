4d35be38803bd208bd38b4fdb8f25e08550eb17b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:18:58 +0000

    2f18e8565e style(docs-infra): properly format files (#35318)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19528,7 +19528,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+34.sha-c013dd4');
+    var VERSION$1 = new Version('9.1.0-next.0+36.sha-2f18e85');
 
     /**
      * @license
