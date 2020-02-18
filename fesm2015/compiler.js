5c33cbd46070b7236d9e6e9c075771f0541f00a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:11:37 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+26.sha-d05629b
+ * @license Angular v9.1.0-next.0+29.sha-d7c4f40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.0+26.sha-d05629b');
+const VERSION$1 = new Version('9.1.0-next.0+29.sha-d7c4f40');
 
 /**
  * @license
