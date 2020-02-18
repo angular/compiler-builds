70ecd9b35454197483a106cca6e98633a21194f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:09:52 +0000

    7b56daffe6 fix(docs-infra): add fallback to web fonts so downloading does not block rendering (#35352)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+33.sha-7b56daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.0+29.sha-d7c4f40');
+const VERSION$1 = new Version('9.1.0-next.0+33.sha-7b56daf');
 
 /**
  * @license
