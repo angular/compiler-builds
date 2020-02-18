9363cbba49e68b11e4f8e834dcb81f18e01840c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:17:31 +0000

    c013dd4ca6 fix(core): better handing of ICUs outside of i18n blocks (#35347)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+32.sha-1939676
+ * @license Angular v9.1.0-next.0+34.sha-c013dd4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.0+32.sha-1939676');
+var VERSION$1 = new Version('9.1.0-next.0+34.sha-c013dd4');
 
 /**
  * @license
