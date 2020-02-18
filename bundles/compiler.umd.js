d851ea99c6267ca9bff4f261434c0391b05d5cfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:09:46 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+26.sha-d05629b
+ * @license Angular v9.1.0-next.0+29.sha-d7c4f40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19528,7 +19528,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+26.sha-d05629b');
+    var VERSION$1 = new Version('9.1.0-next.0+29.sha-d7c4f40');
 
     /**
      * @license
