150617b302b4abd7ff83dde225e3f9ce029bfe8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:23:51 +0000

    a756161dc2 fix(core): make subclass inherit developer-defined data (#35105)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+44.sha-90692f0
+ * @license Angular v9.1.0-next.1+2.sha-a756161
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.0+44.sha-90692f0');
+var VERSION$1 = new Version('9.1.0-next.1+2.sha-a756161');
 
 /**
  * @license
