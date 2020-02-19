3d967d5261623bd6c8fa508b3e338e14b6992c29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 20:38:07 +0000

    90692f0f2e docs: release notes for the v9.0.2 release

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+43.sha-d0d36a5
+ * @license Angular v9.1.0-next.0+44.sha-90692f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.0+43.sha-d0d36a5');
+var VERSION$1 = new Version('9.1.0-next.0+44.sha-90692f0');
 
 /**
  * @license
