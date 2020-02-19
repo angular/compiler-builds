37e656dfab2935ea642d2dcb3381123b20382edf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 01:45:05 +0000

    3f4e02b8c7 fix(ivy): queries should match elements inside ng-container with the descendants: false option (#35384)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+38.sha-5fbfe69
+ * @license Angular v9.1.0-next.0+39.sha-3f4e02b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19528,7 +19528,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.0+38.sha-5fbfe69');
+    var VERSION$1 = new Version('9.1.0-next.0+39.sha-3f4e02b');
 
     /**
      * @license
