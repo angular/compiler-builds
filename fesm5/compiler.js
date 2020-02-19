1ed6770668124a1ab7c9130e45392a5c2ce8bc82
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:23:42 +0000

    3a97972e58 fix(docs-infra): improve focus styles in topnav and footer (#33255)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2
+ * @license Angular v9.0.2+3.sha-3a97972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.2');
+var VERSION$1 = new Version('9.0.2+3.sha-3a97972');
 
 /**
  * @license
