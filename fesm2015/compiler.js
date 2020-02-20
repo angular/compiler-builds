002aeb8003a8fe6d20237214f870da1eac2eaf89
Miško Hevery <misko@hevery.com>
2020-02-20 19:14:18 +0000

    1b72fc10eb docs: edit displaying-data page to introduce section (#35507)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+1.sha-8e12707
+ * @license Angular v9.1.0-next.1+5.sha-1b72fc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18255,7 +18255,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+1.sha-8e12707');
+const VERSION$1 = new Version('9.1.0-next.1+5.sha-1b72fc1');
 
 /**
  * @license
