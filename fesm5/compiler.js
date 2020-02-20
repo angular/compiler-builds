725d49d803c24fae80570408a5ba64b83c7eb656
Miško Hevery <misko@hevery.com>
2020-02-20 21:03:19 +0000

    646655d09a fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+4.sha-4c693ea
+ * @license Angular v9.1.0-next.1+11.sha-646655d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19350,7 +19350,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+4.sha-4c693ea');
+var VERSION$1 = new Version('9.1.0-next.1+11.sha-646655d');
 
 /**
  * @license
