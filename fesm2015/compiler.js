71794787474fc792d21c3adb462c3555d0d965d4
Miško Hevery <misko@hevery.com>
2020-02-21 22:49:09 +0000

    e084835fb1 Revert "feat: support passive event options by defining global variables in zone.js config file (#34503)"

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+31.sha-e17bde9
+ * @license Angular v9.1.0-next.1+32.sha-e084835
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+31.sha-e17bde9');
+const VERSION$1 = new Version('9.1.0-next.1+32.sha-e084835');
 
 /**
  * @license
