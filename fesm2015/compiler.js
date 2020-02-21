0d16b034bffaef7b2d675eaf32651734dbf2e06a
Miško Hevery <misko@hevery.com>
2020-02-21 21:06:10 +0000

    bb09cd0e41 fix(ivy): error in AOT when pipe inherits constructor from injectable that uses DI (#35468)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+23.sha-a491f7e
+ * @license Angular v9.0.2+27.sha-bb09cd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.2+23.sha-a491f7e');
+const VERSION$1 = new Version('9.0.2+27.sha-bb09cd0');
 
 /**
  * @license
