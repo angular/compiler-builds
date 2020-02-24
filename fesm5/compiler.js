648acce0f1413348c818e58a2ec0197c1803d67f
Miško Hevery <misko@hevery.com>
2020-02-24 17:34:38 +0000

    3893799183 docs: add amazon s3 builder package (#34783)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+44.sha-53f059e
+ * @license Angular v9.1.0-next.1+46.sha-3893799
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+44.sha-53f059e');
+var VERSION$1 = new Version('9.1.0-next.1+46.sha-3893799');
 
 /**
  * @license
