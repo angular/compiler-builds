5d99ac44c33483b5e01def67e147f45b2b464d4b
Miško Hevery <misko@hevery.com>
2020-02-25 21:38:47 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+61.sha-1e20b2c');
+    var VERSION$1 = new Version('9.1.0-next.1+59.sha-8e354da');
 
     /**
      * @license
