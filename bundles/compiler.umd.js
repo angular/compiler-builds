02fad124948d75f4374f45604668c46174091f30
Miško Hevery <misko@hevery.com>
2020-02-25 01:51:26 +0000

    737ec52e9d docs: add more relevant statistic for page load (#35649)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+48.sha-c839c05
+ * @license Angular v9.1.0-next.1+47.sha-737ec52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+48.sha-c839c05');
+    var VERSION$1 = new Version('9.1.0-next.1+47.sha-737ec52');
 
     /**
      * @license
