b20613d0e6fcd6112fff42717705fdb7b1bc0816
Miško Hevery <misko@hevery.com>
2020-02-25 01:46:26 +0000

    c839c05620 fix(router): removed unused ApplicationRef dependency (#35642)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+46.sha-3893799
+ * @license Angular v9.1.0-next.1+48.sha-c839c05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+46.sha-3893799');
+var VERSION$1 = new Version('9.1.0-next.1+48.sha-c839c05');
 
 /**
  * @license
