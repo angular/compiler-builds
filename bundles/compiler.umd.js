d2ab65afb873e51385fc663bc8851ba3b81d21c3
Miško Hevery <misko@hevery.com>
2020-02-25 21:47:42 +0000

    d6904882d0 fix(ivy): error when accessing NgModuleRef.componentFactoryResolver in constructor (#35637)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+47.sha-8cac5fe
+ * @license Angular v9.0.2+54.sha-d690488
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.2+47.sha-8cac5fe');
+    var VERSION$1 = new Version('9.0.2+54.sha-d690488');
 
     /**
      * @license
