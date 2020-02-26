012b27a64e48538144df8420b563d221f77d605c
Miško Hevery <misko@hevery.com>
2020-02-26 21:22:04 +0000

    a7ed74f573 docs(core): correct insertBefore's description (#34547)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+68.sha-2a8dd47
+ * @license Angular v9.1.0-next.1+66.sha-a7ed74f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+68.sha-2a8dd47');
+var VERSION$1 = new Version('9.1.0-next.1+66.sha-a7ed74f');
 
 /**
  * @license
