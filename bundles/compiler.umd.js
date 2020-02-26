8bc5b205b33751e1ab9eb3a02f69b8174007fefc
Miško Hevery <misko@hevery.com>
2020-02-26 21:30:06 +0000

    df816c9c80 feat(ngcc): implement source-map flattening (#35132)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+66.sha-a7ed74f');
+    var VERSION$1 = new Version('9.1.0-next.1+69.sha-df816c9');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+66.sha-a7ed74f
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+66.sha-a7ed74f');
+const VERSION$1 = new Version('9.1.0-next.1+69.sha-df816c9');
 
 /**
  * @license
