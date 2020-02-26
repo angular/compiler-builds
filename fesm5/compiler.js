48528a4c1e39652f54ad0e9f34b897f6e3e3c9ab
Miško Hevery <misko@hevery.com>
2020-02-26 21:28:27 +0000

    df816c9c80 feat(ngcc): implement source-map flattening (#35132)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+67.sha-d419884
+ * @license Angular v9.1.0-next.1+69.sha-df816c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+67.sha-d419884');
+var VERSION$1 = new Version('9.1.0-next.1+69.sha-df816c9');
 
 /**
  * @license
