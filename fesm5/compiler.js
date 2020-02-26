ec54c6aa56aa1a141043b3bd8a9e710a38f2fe70
Miško Hevery <misko@hevery.com>
2020-02-26 20:59:51 +0000

    bde4cd7982 docs: add documentation of NgZone with zone.js (#34295)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+62.sha-835618c
+ * @license Angular v9.1.0-next.1+63.sha-bde4cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+62.sha-835618c');
+var VERSION$1 = new Version('9.1.0-next.1+63.sha-bde4cd7');
 
 /**
  * @license
