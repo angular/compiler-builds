c67782bad7c12f0d7dd30b4f5f6146bcea1b93e5
Miško Hevery <misko@hevery.com>
2020-02-26 21:01:51 +0000

    6f68eae9dd docs: more precise docs for template statement syntax (#34348)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+63.sha-bde4cd7
+ * @license Angular v9.1.0-next.1+64.sha-6f68eae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+63.sha-bde4cd7');
+const VERSION$1 = new Version('9.1.0-next.1+64.sha-6f68eae');
 
 /**
  * @license
