789ea80114149fa1f4179493b3cc251a8863c06e
Miško Hevery <misko@hevery.com>
2020-02-26 21:21:13 +0000

    2a8dd4758c test(core): check dependency in extended child (#34767)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+65.sha-8fbb966
+ * @license Angular v9.1.0-next.1+68.sha-2a8dd47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+65.sha-8fbb966');
+const VERSION$1 = new Version('9.1.0-next.1+68.sha-2a8dd47');
 
 /**
  * @license
