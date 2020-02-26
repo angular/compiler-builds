62267db347803d83e00ca5de43e7ddeedee44ca3
Miško Hevery <misko@hevery.com>
2020-02-26 21:03:57 +0000

    6f68eae9dd docs: more precise docs for template statement syntax (#34348)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+63.sha-bde4cd7
+ * @license Angular v9.1.0-next.1+64.sha-6f68eae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+63.sha-bde4cd7');
+    var VERSION$1 = new Version('9.1.0-next.1+64.sha-6f68eae');
 
     /**
      * @license
