48dcdf79a8d88b4257997e8e0e853e33e2a61303
Miško Hevery <misko@hevery.com>
2020-02-26 21:14:05 +0000

    8fbb966ca7 docs(core): describe your change... (#34513)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+64.sha-6f68eae
+ * @license Angular v9.1.0-next.1+65.sha-8fbb966
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.1+64.sha-6f68eae');
+var VERSION$1 = new Version('9.1.0-next.1+65.sha-8fbb966');
 
 /**
  * @license
