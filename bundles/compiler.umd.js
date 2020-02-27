6cc4c7c019e7afc18c12b1f1aec95ff126441158
atscott <atscott@google.com>
2020-02-27 22:26:53 +0000

    7b13977c3a fix(core): use proper configuration to compile Injectable in JIT (#35706)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+11.sha-8fed1fe
+ * @license Angular v9.1.0-next.2+12.sha-7b13977
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+11.sha-8fed1fe');
+    var VERSION$1 = new Version('9.1.0-next.2+12.sha-7b13977');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+11.sha-8fed1fe
+ * @license Angular v9.1.0-next.2+12.sha-7b13977
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+11.sha-8fed1fe');
+const VERSION$1 = new Version('9.1.0-next.2+12.sha-7b13977');
 
 /**
  * @license
