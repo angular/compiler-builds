eb0e11c4917d2685b422eb4a03bbb044b1bfc285
atscott <atscott@google.com>
2020-02-27 19:08:28 +0000

    0b97d07ad6 test(docs-infra): fix tests with new `topics` property (#35539)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+87.sha-d3c0d92
+ * @license Angular v9.1.0-next.2+2.sha-0b97d07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.1+87.sha-d3c0d92');
+const VERSION$1 = new Version('9.1.0-next.2+2.sha-0b97d07');
 
 /**
  * @license
