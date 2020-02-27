668fc6ac5ad634ecc0ad8094b48fd14519738ab9
Misko Hevery <misko@hevery.com>
2020-02-27 05:16:21 +0000

    c78df781f7 release: cut the v9.0.3 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+74.sha-dc800b2
+ * @license Angular v9.0.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.2+74.sha-dc800b2');
+    var VERSION$1 = new Version('9.0.3');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+74.sha-dc800b2
+ * @license Angular v9.0.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.2+74.sha-dc800b2');
+const VERSION$1 = new Version('9.0.3');
 
 /**
  * @license
