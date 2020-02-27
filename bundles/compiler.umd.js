9de76eaa5aa3f2eb61ce10d1420a89d16becd71f
Misko Hevery <misko@hevery.com>
2020-02-27 06:30:37 +0000

    d3c0d92d28 release: cut the v9.1.0-next.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+85.sha-91934eb
+ * @license Angular v9.1.0-next.1+87.sha-d3c0d92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.1+85.sha-91934eb');
+    var VERSION$1 = new Version('9.1.0-next.1+87.sha-d3c0d92');
 
     /**
      * @license
