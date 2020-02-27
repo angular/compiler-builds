2b935362eac351826b582ba3ba98fd708e310a66
atscott <atscott@google.com>
2020-02-27 22:24:07 +0000

    8fed1fe792 perf(core): adding micro benchmark for host bindings (#35705)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+10.sha-bc7b589
+ * @license Angular v9.1.0-next.2+11.sha-8fed1fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19585,7 +19585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+10.sha-bc7b589');
+    var VERSION$1 = new Version('9.1.0-next.2+11.sha-8fed1fe');
 
     /**
      * @license
