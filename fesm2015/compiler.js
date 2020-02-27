ed11a7790e21cc8cf1b922a487ddf15edbe49c7e
atscott <atscott@google.com>
2020-02-27 22:17:36 +0000

    bc7b589611 docs: release notes for the v9.0.4 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+9.sha-5d8f7da
+ * @license Angular v9.1.0-next.2+10.sha-bc7b589
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+9.sha-5d8f7da');
+const VERSION$1 = new Version('9.1.0-next.2+10.sha-bc7b589');
 
 /**
  * @license
