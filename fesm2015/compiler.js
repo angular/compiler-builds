a26f61b6e435d2670977af93d36a964030af5f48
atscott <atscott@google.com>
2020-02-27 22:29:41 +0000

    b7519e5cfa build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+12.sha-7b13977
+ * @license Angular v9.1.0-next.2+13.sha-b7519e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18312,7 +18312,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+12.sha-7b13977');
+const VERSION$1 = new Version('9.1.0-next.2+13.sha-b7519e5');
 
 /**
  * @license
