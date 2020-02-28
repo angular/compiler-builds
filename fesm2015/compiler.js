3dd40ffdc0d2e396693b6e68ba3a0432fce65d95
atscott <atscott@google.com>
2020-02-28 20:49:49 +0000

    ef75875ca3 fix(core): handle `<ng-template>` with local refs in i18n blocks (#35758)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+23.sha-4003538
+ * @license Angular v9.1.0-next.2+24.sha-ef75875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18224,7 +18224,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+23.sha-4003538');
+const VERSION$1 = new Version('9.1.0-next.2+24.sha-ef75875');
 
 /**
  * @license
