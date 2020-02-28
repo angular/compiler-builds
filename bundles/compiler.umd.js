d32f58ed402d29ca15bd3154cb682ba7050d68ac
atscott <atscott@google.com>
2020-02-28 16:01:25 +0000

    40039d8068 fix(ivy): narrow `NgIf` context variables in template type checker (#35125)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+20.sha-208ef7b
+ * @license Angular v9.1.0-next.2+21.sha-40039d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19504,7 +19504,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+20.sha-208ef7b');
+    var VERSION$1 = new Version('9.1.0-next.2+21.sha-40039d8');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+20.sha-208ef7b
+ * @license Angular v9.1.0-next.2+21.sha-40039d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18224,7 +18224,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+20.sha-208ef7b');
+const VERSION$1 = new Version('9.1.0-next.2+21.sha-40039d8');
 
 /**
  * @license
