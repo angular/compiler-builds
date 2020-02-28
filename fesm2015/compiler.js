09c21b4c4d4395420d8f17e3b5656e31cdf0a05e
Joey Perrott <josephperrott.github@gmail.com>
2020-02-28 20:32:20 +0000

    3b32bc7b41 ci: move public-api goldens to goldens directory

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+22.sha-7bfcc35
+ * @license Angular v9.1.0-next.2+22.sha-3b32bc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18224,7 +18224,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+22.sha-7bfcc35');
+const VERSION$1 = new Version('9.1.0-next.2+22.sha-3b32bc7');
 
 /**
  * @license
