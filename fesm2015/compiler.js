e07abada3935037e26521aa686c9c2f8f6c671ff
atscott <atscott@google.com>
2020-03-02 21:55:54 +0000

    ef2721b903 build: fix flakiness in integration/bazel by disabling symlinked_node_modules (#35804)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+40.sha-8eb4a9d
+ * @license Angular v9.1.0-next.2+39.sha-ef2721b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18227,7 +18227,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+40.sha-8eb4a9d');
+const VERSION$1 = new Version('9.1.0-next.2+39.sha-ef2721b');
 
 /**
  * @license
