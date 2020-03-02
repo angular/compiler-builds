a92d9fb9ab983ac4a1e4bdca84f3f61d8a91bfca
atscott <atscott@google.com>
2020-03-02 21:55:12 +0000

    54288401f2 docs: fix docs and associated code snippets for enabling more macro tasks in `fakeAsync()` (#35778)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+25.sha-710fca2
+ * @license Angular v9.0.4+27.sha-5428840
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19508,7 +19508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.4+25.sha-710fca2');
+    var VERSION$1 = new Version('9.0.4+27.sha-5428840');
 
     /**
      * @license
