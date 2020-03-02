616b7a1d1e40e7f369bb0f367d0bedf4cc70413d
atscott <atscott@google.com>
2020-03-02 19:38:24 +0000

    5652fb1018 fix(core): allow null / undefined values in query results (#35796)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+36.sha-44e47da
+ * @license Angular v9.1.0-next.2+37.sha-5652fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19330,7 +19330,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.2+36.sha-44e47da');
+var VERSION$1 = new Version('9.1.0-next.2+37.sha-5652fb1');
 
 /**
  * @license
