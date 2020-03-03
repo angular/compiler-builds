2369cb71c422d1e7aa78aa2df2169fc1d887df25
atscott <atscott@google.com>
2020-03-03 22:23:03 +0000

    218e82ebb4 fix(core): log error instead of warning for unknown properties and elements (#35798)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+38.sha-12e52a7
+ * @license Angular v9.0.4+44.sha-218e82e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19330,7 +19330,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.4+38.sha-12e52a7');
+var VERSION$1 = new Version('9.0.4+44.sha-218e82e');
 
 /**
  * @license
