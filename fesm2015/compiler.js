f1583b87161b7d7fab4d2bc19b6e475ec7abfa4f
atscott <atscott@google.com>
2020-03-03 17:21:46 +0000

    bf42807aa6 docs(common): switch HTTP guide examples to `TestBed.inject()` (#35777)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+46.sha-c195d22
+ * @license Angular v9.1.0-next.2+47.sha-bf42807
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18227,7 +18227,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+46.sha-c195d22');
+const VERSION$1 = new Version('9.1.0-next.2+47.sha-bf42807');
 
 /**
  * @license
