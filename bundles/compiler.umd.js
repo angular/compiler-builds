13d4b2d715eea37e2aae7063a61edbcef5a26da1
atscott <atscott@google.com>
2020-03-03 22:02:19 +0000

    17cf04ebea fix(animations): allow computeStyle to work on elements created in Node (#35810)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+55.sha-c7d0567
+ * @license Angular v9.1.0-next.2+56.sha-17cf04e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19508,7 +19508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+55.sha-c7d0567');
+    var VERSION$1 = new Version('9.1.0-next.2+56.sha-17cf04e');
 
     /**
      * @license
