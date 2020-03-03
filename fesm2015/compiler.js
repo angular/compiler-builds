19ae184ea7641c12d4632a280dacc45d202a4e4c
atscott <atscott@google.com>
2020-03-03 18:06:33 +0000

    c7d0567d37 Revert "docs: add comment markers for internal goog.define replacement (#32795)" (#35830)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+54.sha-5615928
+ * @license Angular v9.1.0-next.2+55.sha-c7d0567
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18227,7 +18227,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+54.sha-5615928');
+const VERSION$1 = new Version('9.1.0-next.2+55.sha-c7d0567');
 
 /**
  * @license
