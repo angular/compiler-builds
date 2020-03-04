200e6cc3d3e74ea1343a2f4867cbfc9d31679d29
atscott <atscott@google.com>
2020-03-04 20:12:48 +0000

    b57be3bb41 docs: release notes for the v9.1.0-next.3 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+72.sha-dcf28bf
+ * @license Angular v9.1.0-next.2+73.sha-b57be3b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19508,7 +19508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+72.sha-dcf28bf');
+    var VERSION$1 = new Version('9.1.0-next.2+73.sha-b57be3b');
 
     /**
      * @license
