89a747587f4a90ad297cdf8dc95e3108ce508726
atscott <atscott@google.com>
2020-03-04 17:03:25 +0000

    2e728f7fff docs: remove `ivy` and mention `ve` label in docs (#35809)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+61.sha-d543b13
+ * @license Angular v9.1.0-next.2+62.sha-2e728f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19330,7 +19330,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.2+61.sha-d543b13');
+var VERSION$1 = new Version('9.1.0-next.2+62.sha-2e728f7');
 
 /**
  * @license
