cb43f4c98fb9a063a0bbf5f5c4cf894a36b1bd6a
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:22:01 +0000

    e0a35e13d5 perf(ngcc): reduce directory traversing (#35756)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+75.sha-a1d0396
+ * @license Angular v9.1.0-next.2+77.sha-e0a35e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19508,7 +19508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+75.sha-a1d0396');
+    var VERSION$1 = new Version('9.1.0-next.2+77.sha-e0a35e1');
 
     /**
      * @license
