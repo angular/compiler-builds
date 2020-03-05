67080bed6751ea89917a6c4ad36122a379c92966
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 23:06:09 +0000

    98a9daf4f4 Revert "fix(router): state data missing in routerLink (#33203)" (#35882)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+79.sha-716d50a
+ * @license Angular v9.1.0-next.2+80.sha-98a9daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+79.sha-716d50a');
+const VERSION$1 = new Version('9.1.0-next.2+80.sha-98a9daf');
 
 /**
  * @license
