bd8531e96709c81025968a10d480efd2dd361411
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:24:47 +0000

    8ef29b65ff fix: fix flaky test cases of passive events (#35679)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+77.sha-e0a35e1
+ * @license Angular v9.1.0-next.2+76.sha-8ef29b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19508,7 +19508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+77.sha-e0a35e1');
+    var VERSION$1 = new Version('9.1.0-next.2+76.sha-8ef29b6');
 
     /**
      * @license
