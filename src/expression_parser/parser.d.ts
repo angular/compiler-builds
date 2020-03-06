9246e435799f63c95f871b71a941a94a632abf04
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:04:44 +0000

    59078c444c docs: move ngIndia 2020 to past events on events page (#35901)

--- src/expression_parser/parser.d.ts
+++ src/expression_parser/parser.d.ts
@@ -89,7 +89,7 @@ export declare class _ParseAST {
     span(start: number): ParseSpan;
     sourceSpan(start: number): AbsoluteSourceSpan;
     advance(): void;
-    consumeOptionalCharacter(code: number): boolean;
+    optionalCharacter(code: number): boolean;
     peekKeywordLet(): boolean;
     peekKeywordAs(): boolean;
     expectCharacter(code: number): void;
