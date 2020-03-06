9e67078a83bb7ec4e11d14842cf7138b64c7c893
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:05:46 +0000

    4e1d780554 fix(language-service): resolve the real path for symlink (#35895)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+89.sha-59078c4
+ * @license Angular v9.1.0-next.2+88.sha-4e1d780
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19466,7 +19466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.2+89.sha-59078c4');
+var VERSION$1 = new Version('9.1.0-next.2+88.sha-4e1d780');
 
 /**
  * @license
