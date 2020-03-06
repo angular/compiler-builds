cf4b8b29929710e4e8a9fecefd033eabb516684a
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:02:25 +0000

    a1b90619ba test(zone.js): sets rollup_bundle to use the --silent flag in karma tests (#35881)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+87.sha-44f9fa6
+ * @license Angular v9.1.0-next.2+86.sha-a1b9061
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+87.sha-44f9fa6');
+const VERSION$1 = new Version('9.1.0-next.2+86.sha-a1b9061');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+87.sha-44f9fa6
+ * @license Angular v9.1.0-next.2+86.sha-a1b9061
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19466,7 +19466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.2+87.sha-44f9fa6');
+var VERSION$1 = new Version('9.1.0-next.2+86.sha-a1b9061');
 
 /**
  * @license
