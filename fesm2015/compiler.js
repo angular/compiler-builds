797df87d5fdd1bd524989944edf6aca4ec29f8b0
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:56:48 +0000

    55b3f97be0 fix(zone.js): `tickOptions`'s `processNewMacroTasksSynchronously` should default to true when flag omitted (#35814)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+95.sha-86a45ff
+ * @license Angular v9.1.0-next.2+99.sha-55b3f97
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+95.sha-86a45ff');
+const VERSION$1 = new Version('9.1.0-next.2+99.sha-55b3f97');
 
 /**
  * @license
