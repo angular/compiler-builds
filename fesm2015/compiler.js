0876823f83c0811fcc810d2c19b648399b88cbb1
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:17:33 +0000

    84c2458ea7 refactor: cleanup zone.js test instruction (#34860)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+93.sha-c06f9a1
+ * @license Angular v9.1.0-next.2+94.sha-84c2458
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+93.sha-c06f9a1');
+const VERSION$1 = new Version('9.1.0-next.2+94.sha-84c2458');
 
 /**
  * @license
