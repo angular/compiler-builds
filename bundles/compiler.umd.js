7bf27df51dc9012ad1298434b7f5304ccccd43a6
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:59:12 +0000

    82a6fc5ef9 build(docs-infra): use local version of Zone.js when testing against local packages (#35858)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+99.sha-55b3f97
+ * @license Angular v9.1.0-next.2+96.sha-82a6fc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+99.sha-55b3f97');
+    var VERSION$1 = new Version('9.1.0-next.2+96.sha-82a6fc5');
 
     /**
      * @license
