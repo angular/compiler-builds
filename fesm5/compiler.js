77b3ab712366fe3ad811bf212e5dc2db6958906a
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 00:17:10 +0000

    2cccf59415 build: create dev-infra-private npm package (#35862)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+5.sha-2a9b254
+ * @license Angular v9.0.5+6.sha-2cccf59
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19466,7 +19466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.5+5.sha-2a9b254');
+var VERSION$1 = new Version('9.0.5+6.sha-2cccf59');
 
 /**
  * @license
