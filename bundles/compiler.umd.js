d3acb12f912a80bafa435b42c4e8b5fd2ded30d7
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:08:49 +0000

    d912d4f34c docs: markup mini fix (#35883) PR Close #35883

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+90.sha-bef14cf
+ * @license Angular v9.1.0-next.2+92.sha-d912d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+90.sha-bef14cf');
+    var VERSION$1 = new Version('9.1.0-next.2+92.sha-d912d4f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+90.sha-bef14cf
+ * @license Angular v9.1.0-next.2+92.sha-d912d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+90.sha-bef14cf');
+const VERSION$1 = new Version('9.1.0-next.2+92.sha-d912d4f');
 
 /**
  * @license
