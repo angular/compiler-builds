c23d182d52fdc57a6e9d4eca4c7bcf0f8f6b0b54
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:39:31 +0000

    0e2a577b42 fix(localize): improve matching and parsing of XTB translation files (#35793)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+100.sha-99ce015
+ * @license Angular v9.1.0-next.2+105.sha-0e2a577
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+100.sha-99ce015');
+const VERSION$1 = new Version('9.1.0-next.2+105.sha-0e2a577');
 
 /**
  * @license
