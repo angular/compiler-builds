4f80dd78333062b74c288578912e38fef1cbb822
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 18:47:19 +0000

    fc4c3c3eb5 fix(localize): merge translation from all  XLIFF `<file>` elements (#35936)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+111.sha-9ff9a07
+ * @license Angular v9.1.0-next.2+110.sha-fc4c3c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+111.sha-9ff9a07');
+    var VERSION$1 = new Version('9.1.0-next.2+110.sha-fc4c3c3');
 
     /**
      * @license
