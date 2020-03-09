437dc6e026e1941290ab557be7bb4072dec67dad
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 17:33:32 +0000

    9ff9a072e6 feat(bazel): transform generated shims (in Ivy) with tsickle (#35848)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+108.sha-2b8edbb
+ * @license Angular v9.1.0-next.2+111.sha-9ff9a07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+108.sha-2b8edbb');
+const VERSION$1 = new Version('9.1.0-next.2+111.sha-9ff9a07');
 
 /**
  * @license
