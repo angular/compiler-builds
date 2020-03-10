1e5c90597afb52ea23ba50e4a5c511a279360dcc
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:58:33 +0000

    e4b1e6c622 build: Add pullapprove verification tool to dev-infra-private package (#35911)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+119.sha-64d6f13
+ * @license Angular v9.1.0-next.2+127.sha-e4b1e6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+119.sha-64d6f13');
+    var VERSION$1 = new Version('9.1.0-next.2+127.sha-e4b1e6c');
 
     /**
      * @license
