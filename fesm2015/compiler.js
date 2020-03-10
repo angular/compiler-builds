b90e364cd41f8d0895555cc2db52c6d70b57feff
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 22:03:22 +0000

    ce728dd323 build: adding a script to compare commits in master and stable branches (#35130)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+43.sha-20dd0ac
+ * @license Angular v9.0.5+44.sha-ce728dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.5+43.sha-20dd0ac');
+const VERSION$1 = new Version('9.0.5+44.sha-ce728dd');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+43.sha-20dd0ac
+ * @license Angular v9.0.5+44.sha-ce728dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19466,7 +19466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.0.5+43.sha-20dd0ac');
+var VERSION$1 = new Version('9.0.5+44.sha-ce728dd');
 
 /**
  * @license
