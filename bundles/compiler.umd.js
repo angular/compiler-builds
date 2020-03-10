b6da436216192440aaeb9d0139960e52904e6454
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:59:28 +0000

    65a6848ed7 build: creates a script to get contributor stats from across the angular org (#35834)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+123.sha-9368053
+ * @license Angular v9.1.0-next.2+126.sha-65a6848
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.2+123.sha-9368053');
+    var VERSION$1 = new Version('9.1.0-next.2+126.sha-65a6848');
 
     /**
      * @license
