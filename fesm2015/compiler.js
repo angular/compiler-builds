e5d7487a445aefe897185b3af16c6a4751f6c7a8
Matias Niemelä <matias@yearofmoo.com>
2020-03-10 18:59:46 +0000

    0bf6e58db2 fix(compiler): process `imports` first and `declarations` second while calculating scopes (#35850)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+126.sha-65a6848
+ * @license Angular v9.1.0-next.2+125.sha-0bf6e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18363,7 +18363,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+126.sha-65a6848');
+const VERSION$1 = new Version('9.1.0-next.2+125.sha-0bf6e58');
 
 /**
  * @license
