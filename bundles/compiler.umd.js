c5e168786067dd16afe7cca4385b9887139a022b
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:07:29 +0000

    e74738db8c build: fix `CHANGELOG.md` date for `v9.0.6`

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4
+ * @license Angular v9.1.0-next.4+2.sha-e74738d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19642,7 +19642,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4');
+    var VERSION$1 = new Version('9.1.0-next.4+2.sha-e74738d');
 
     /**
      * @license
