870e458f436519162320d89915b6581e42c7d46b
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:07:36 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+34.sha-99ea5d7
+ * @license Angular v9.1.0-next.4+35.sha-2b6028b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19510,7 +19510,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.4+34.sha-99ea5d7');
+var VERSION$1 = new Version('9.1.0-next.4+35.sha-2b6028b');
 
 /**
  * @license
