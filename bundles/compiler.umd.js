26f6f51bacef821292452d02726fef5ec1244d60
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 18:52:00 +0000

    42375c4b3a ci: prevent api golden tests on windows CI (#36025)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+5.sha-15f8afa
+ * @license Angular v9.1.0-next.4+8.sha-42375c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19642,7 +19642,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+5.sha-15f8afa');
+    var VERSION$1 = new Version('9.1.0-next.4+8.sha-42375c4');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+5.sha-15f8afa
+ * @license Angular v9.1.0-next.4+8.sha-42375c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+5.sha-15f8afa');
+const VERSION$1 = new Version('9.1.0-next.4+8.sha-42375c4');
 
 /**
  * @license
