e1a8618e39f03fe707f1e687d40e7203bf52b191
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 18:19:14 +0000

    3112b1b00a ci: prevent api golden tests on windows CI (#36025)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+5.sha-8bf26c6
+ * @license Angular v9.0.6+6.sha-3112b1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.6+5.sha-8bf26c6');
+    var VERSION$1 = new Version('9.0.6+6.sha-3112b1b');
 
     /**
      * @license
