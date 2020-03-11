0f51022fba26e50e34a64eecd060cf5a4ab5abef
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:11:34 +0000

    5e3a898f10 fix(bazel): update typescript peer dependency range (#36013)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+13.sha-49478f5
+ * @license Angular v9.1.0-next.4+14.sha-5e3a898
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19642,7 +19642,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+13.sha-49478f5');
+    var VERSION$1 = new Version('9.1.0-next.4+14.sha-5e3a898');
 
     /**
      * @license
