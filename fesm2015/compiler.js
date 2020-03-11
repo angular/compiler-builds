a8a280f299bc3b772f31af12c637da6ff3c6734c
Andrew Kushnir <akushnir@google.com>
2020-03-11 22:30:19 +0000

    8ea61a19cd feat(ngcc): support invalidating the entry-point manifest (#35931)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+28.sha-5af0e75
+ * @license Angular v9.1.0-next.4+33.sha-8ea61a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18402,7 +18402,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+28.sha-5af0e75');
+const VERSION$1 = new Version('9.1.0-next.4+33.sha-8ea61a1');
 
 /**
  * @license
