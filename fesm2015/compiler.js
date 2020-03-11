6ab2af2e599659ae9a199f29543ab29518592b15
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:26:13 +0000

    146ef48a6e docs: change grammar in architecture guide (#35541)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+25.sha-32f099a
+ * @license Angular v9.1.0-next.4+19.sha-146ef48
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+25.sha-32f099a');
+const VERSION$1 = new Version('9.1.0-next.4+19.sha-146ef48');
 
 /**
  * @license
