e7f647c7c355fc4fa7c458e31ad924d04a9beb28
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:42:20 +0000

    15f8afa4bf ci: move public-api goldens to goldens directory (#35768)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+7.sha-13495c6
+ * @license Angular v9.1.0-next.4+5.sha-15f8afa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19642,7 +19642,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+7.sha-13495c6');
+    var VERSION$1 = new Version('9.1.0-next.4+5.sha-15f8afa');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+7.sha-13495c6
+ * @license Angular v9.1.0-next.4+5.sha-15f8afa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+7.sha-13495c6');
+const VERSION$1 = new Version('9.1.0-next.4+5.sha-15f8afa');
 
 /**
  * @license
