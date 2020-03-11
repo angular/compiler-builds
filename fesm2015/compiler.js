ee12fb3778c7fd04f21592598b5d8a2c8b495bc6
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:11:28 +0000

    49478f5e19 docs: Added ngVikings 2020 on angular.io events (#35129)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+17.sha-b99b80c
+ * @license Angular v9.1.0-next.4+13.sha-49478f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+17.sha-b99b80c');
+const VERSION$1 = new Version('9.1.0-next.4+13.sha-49478f5');
 
 /**
  * @license
