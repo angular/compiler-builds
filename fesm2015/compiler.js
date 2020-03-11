020edadb215872f383e0ef675682584af0bfa9f5
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 00:53:39 +0000

    5cb082ed53 release: cut the v9.1.0-next.4 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+129.sha-95c729f
+ * @license Angular v9.1.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.2+129.sha-95c729f');
+const VERSION$1 = new Version('9.1.0-next.4');
 
 /**
  * @license
