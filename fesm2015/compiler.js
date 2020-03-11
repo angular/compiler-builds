592c85b6f19b38ddafa9a24a764937aabf3248fd
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:16:23 +0000

    9a1240360b build: add more info to SauceLabs README (#35704)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+12.sha-10f1e94
+ * @license Angular v9.1.0-next.4+18.sha-9a12403
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18361,7 +18361,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+12.sha-10f1e94');
+const VERSION$1 = new Version('9.1.0-next.4+18.sha-9a12403');
 
 /**
  * @license
