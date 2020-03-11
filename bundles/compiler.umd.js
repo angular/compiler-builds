45593e00a5d0a0123a445a7060b6f36bccdfe584
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:33:34 +0000

    0d806c84e2 docs: add developer survey 2020 (#35655)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+35.sha-2b6028b
+ * @license Angular v9.1.0-next.4+36.sha-0d806c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19688,7 +19688,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+35.sha-2b6028b');
+    var VERSION$1 = new Version('9.1.0-next.4+36.sha-0d806c8');
 
     /**
      * @license
