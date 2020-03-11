39401882d6754bb219e30adccb4d403d3315b878
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:36:39 +0000

    19cfaf7f4c fix(core): don't re-invoke pure pipes that throw and arguments are the same (#35827)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+1.sha-3aa5ae9
+ * @license Angular v9.1.0-next.4+4.sha-19cfaf7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19642,7 +19642,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+1.sha-3aa5ae9');
+    var VERSION$1 = new Version('9.1.0-next.4+4.sha-19cfaf7');
 
     /**
      * @license
