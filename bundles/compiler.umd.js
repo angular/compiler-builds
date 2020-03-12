5082890c48f37d3d1536ff0d57fcf8e106b2b2b2
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:09:57 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19688,7 +19688,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.4+36.sha-0d806c8');
+    var VERSION$1 = new Version('9.1.0-next.4+37.sha-37a4839');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18402,7 +18402,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+36.sha-0d806c8');
+const VERSION$1 = new Version('9.1.0-next.4+37.sha-37a4839');
 
 /**
  * @license
