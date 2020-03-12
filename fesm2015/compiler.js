63eaa5cc17ed6cbc0923f5e176e98c4c0c7b9ee9
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:16:06 +0000

    372a56a0c9 revert: "ci: prevent api golden tests on windows CI (#36025)" (#36034)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+38.sha-43635a6
+ * @license Angular v9.1.0-next.4+40.sha-372a56a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18402,7 +18402,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+38.sha-43635a6');
+const VERSION$1 = new Version('9.1.0-next.4+40.sha-372a56a');
 
 /**
  * @license
