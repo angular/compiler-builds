51576071b10bca7c4521b5b34997ff3ee4c31dfa
Andrew Kushnir <akushnir@google.com>
2020-03-13 19:56:54 +0000

    a24eddbcfd test(core): Update transplanted views benchpress test to support VE (#36058)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+37.sha-9e698b7
+ * @license Angular v9.0.6+38.sha-a24eddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19644,7 +19644,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.6+37.sha-9e698b7');
+    var VERSION$1 = new Version('9.0.6+38.sha-a24eddb');
 
     /**
      * @license
