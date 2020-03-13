dc6fafaf332a0bd186d0af64d3bdc51911ae3912
Andrew Kushnir <akushnir@google.com>
2020-03-13 15:44:27 +0000

    6b4c5289b3 fix(docs-infra): remove `routerLink` from `top-bar` (#35951)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+50.sha-db74f20
+ * @license Angular v9.1.0-next.4+51.sha-6b4c528
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18402,7 +18402,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+50.sha-db74f20');
+const VERSION$1 = new Version('9.1.0-next.4+51.sha-6b4c528');
 
 /**
  * @license
