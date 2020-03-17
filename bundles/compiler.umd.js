47f430a8614771fedf8d6f6be25cc8611e1567f0
Andrew Kushnir <akushnir@google.com>
2020-03-17 20:58:13 +0000

    76d7188872 fix(ngcc): consistently delegate to TypeScript host for typing files (#36089)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+56.sha-3c19a5d
+ * @license Angular v9.0.6+58.sha-76d7188
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19654,7 +19654,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.0.6+56.sha-3c19a5d');
+    var VERSION$1 = new Version('9.0.6+58.sha-76d7188');
 
     /**
      * @license
