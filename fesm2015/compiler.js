ec847668744570e70856d79905e3c55413f91333
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:26 +0000

    c9f554cda7 fix(ngcc): do not crash on overlapping entry-points (#36083)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5
+ * @license Angular v9.1.0-next.5+6.sha-c9f554c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.5');
+const VERSION$1 = new Version('9.1.0-next.5+6.sha-c9f554c');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5
+ * @license Angular v9.1.0-next.5+6.sha-c9f554c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19555,7 +19555,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.5');
+var VERSION$1 = new Version('9.1.0-next.5+6.sha-c9f554c');
 
 /**
  * @license
