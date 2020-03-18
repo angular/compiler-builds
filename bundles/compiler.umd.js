a045b72511876d89e4bcdfd70e1f2c765d71ef1a
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:19:58 +0000

    c0143cb2ab build: ng_rollup_bundle internal rule should support ngcc (#36044)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+1.sha-d50881a
+ * @license Angular v9.1.0-next.5+7.sha-c0143cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19733,7 +19733,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-next.5+1.sha-d50881a');
+    var VERSION$1 = new Version('9.1.0-next.5+7.sha-c0143cb');
 
     /**
      * @license
