7ce9e6a616e3d0c6bcdf21c1a32ead58378bf05b
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:21:33 +0000

    67b9adfd1a docs(upgrade): remove outdated/non-applicable AOT section from upgrade guide (#36015)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+80.sha-9e70bcb
+ * @license Angular v9.1.0-next.4+101.sha-67b9adf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+80.sha-9e70bcb');
+const VERSION$1 = new Version('9.1.0-next.4+101.sha-67b9adf');
 
 /**
  * @license
