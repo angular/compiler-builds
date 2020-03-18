02a8ff240006fab0953723dc1583cbdb1ad465be
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:18:36 +0000

    6d4f437973 build: ng_rollup_bundle internal rule should support ngcc (#36044)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.7
+ * @license Angular v9.0.7+2.sha-6d4f437
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18373,7 +18373,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.0.7');
+const VERSION$1 = new Version('9.0.7+2.sha-6d4f437');
 
 /**
  * @license
