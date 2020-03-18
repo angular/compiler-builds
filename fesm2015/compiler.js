16914f29d2a720c01b714482b3d4a4eb0a4a77e2
Andrew Kushnir <akushnir@google.com>
2020-03-18 17:26:44 +0000

    1aa5783a45 docs: adding a note on extra text interpolations and HTML tags in translations to Ivy guide (#36029)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+101.sha-67b9adf
+ * @license Angular v9.1.0-next.4+102.sha-1aa5783
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.4+101.sha-67b9adf');
+const VERSION$1 = new Version('9.1.0-next.4+102.sha-1aa5783');
 
 /**
  * @license
