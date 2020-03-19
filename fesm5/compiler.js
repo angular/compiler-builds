d47988a1405c910b42e45699a74764459c964124
Andrew Kushnir <akushnir@google.com>
2020-03-19 16:28:05 +0000

    e342ffd855 fix(core): adhere to bootstrap options for JIT compiled components (#35534)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+9.sha-fb92f5d
+ * @license Angular v9.1.0-next.5+8.sha-e342ffd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19555,7 +19555,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.5+9.sha-fb92f5d');
+var VERSION$1 = new Version('9.1.0-next.5+8.sha-e342ffd');
 
 /**
  * @license
