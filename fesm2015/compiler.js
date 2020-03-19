57e0802bbc01153bbb3bd57fb5ef5abdb80e74e2
Andrew Kushnir <akushnir@google.com>
2020-03-19 20:25:49 +0000

    b4972fa165 release: cut the v9.1.0-rc.0 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+8.sha-e342ffd
+ * @license Angular v9.1.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18436,7 +18436,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-next.5+8.sha-e342ffd');
+const VERSION$1 = new Version('9.1.0-rc.0');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+8.sha-e342ffd
+ * @license Angular v9.1.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19555,7 +19555,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-next.5+8.sha-e342ffd');
+var VERSION$1 = new Version('9.1.0-rc.0');
 
 /**
  * @license
