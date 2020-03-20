97318ef0cbd9a3f6d8124f388072e6cd79d7feda
Misko Hevery <misko@hevery.com>
2020-03-20 21:17:13 +0000

    c21c46a8e8 fix(docs-infra): include correct dependencies in StackBlitz examples (#36071)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+8.sha-ae3eaf8
+ * @license Angular v9.1.0-rc.0+10.sha-c21c46a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18439,7 +18439,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+8.sha-ae3eaf8');
+const VERSION$1 = new Version('9.1.0-rc.0+10.sha-c21c46a');
 
 /**
  * @license
