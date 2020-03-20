dbe0ffdc33f92c8004054c2d64929d5da38e9e11
Misko Hevery <misko@hevery.com>
2020-03-20 17:48:47 +0000

    912692137a fix(docs-infra): fix image name in example (#36127)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+2.sha-df890d7
+ * @license Angular v9.1.0-rc.0+6.sha-9126921
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19557,7 +19557,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+2.sha-df890d7');
+var VERSION$1 = new Version('9.1.0-rc.0+6.sha-9126921');
 
 /**
  * @license
