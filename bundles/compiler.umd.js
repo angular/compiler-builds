3269e970e4a453179b1c57bb71b28a189d436942
Misko Hevery <misko@hevery.com>
2020-03-20 17:37:28 +0000

    47bfec4e46 feat(core): add `isPromise` generic (#34168)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0
+ * @license Angular v9.1.0-rc.0+1.sha-47bfec4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19733,7 +19733,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0');
+    var VERSION$1 = new Version('9.1.0-rc.0+1.sha-47bfec4');
 
     /**
      * @license
