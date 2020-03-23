318be2db36be35ec971ca6aeb944b8df22eafbf0
Misko Hevery <misko@hevery.com>
2020-03-23 18:57:45 +0000

    e81ad3a1bc docs: Add asterisk info in template syntax guide (#36176)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+13.sha-0ce8ad3
+ * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19736,7 +19736,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+13.sha-0ce8ad3');
+    var VERSION$1 = new Version('9.1.0-rc.0+16.sha-e81ad3a');
 
     /**
      * @license
