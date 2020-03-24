b00df649c1a1d754392075993fe04f8ae12882bd
Misko Hevery <misko@hevery.com>
2020-03-24 17:47:12 +0000

    55dac05cf2 docs: fix typo in testing component with dependencies (#36219)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+25.sha-4f97173
+ * @license Angular v9.1.0-rc.0+28.sha-55dac05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19736,7 +19736,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+25.sha-4f97173');
+    var VERSION$1 = new Version('9.1.0-rc.0+28.sha-55dac05');
 
     /**
      * @license
