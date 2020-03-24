8ae4638fd828ff4519b35f2f005e2d78ec3cabf4
Misko Hevery <misko@hevery.com>
2020-03-24 17:44:51 +0000

    4f9717331d test(common): Add test for NgForOfContext.count (#36046)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
+ * @license Angular v9.1.0-rc.0+25.sha-4f97173
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19736,7 +19736,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+27.sha-ae28d7c');
+    var VERSION$1 = new Version('9.1.0-rc.0+25.sha-4f97173');
 
     /**
      * @license
