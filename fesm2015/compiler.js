823e80ad386b8fcb81096e103cb7c9e8276ba5d0
Misko Hevery <misko@hevery.com>
2020-03-24 17:43:10 +0000

    4f9717331d test(common): Add test for NgForOfContext.count (#36046)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+27.sha-ae28d7c
+ * @license Angular v9.1.0-rc.0+25.sha-4f97173
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18439,7 +18439,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+27.sha-ae28d7c');
+const VERSION$1 = new Version('9.1.0-rc.0+25.sha-4f97173');
 
 /**
  * @license
