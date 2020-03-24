451f0ec34942aaf57ac524ae374e84bdd581d942
Misko Hevery <misko@hevery.com>
2020-03-24 17:36:37 +0000

    380de1e7b4 fix(ngcc): use path-mappings from tsconfig in dependency resolution (#36180)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+21.sha-4419907
+ * @license Angular v9.1.0-rc.0+26.sha-380de1e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19558,7 +19558,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+21.sha-4419907');
+var VERSION$1 = new Version('9.1.0-rc.0+26.sha-380de1e');
 
 /**
  * @license
