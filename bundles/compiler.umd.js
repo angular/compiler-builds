d514e505bf09f6c0502dcb9dac7e6b82ee2db617
Misko Hevery <misko@hevery.com>
2020-03-24 17:59:06 +0000

    ff4eb0cb63 docs(elements): correct typo in custom elements image (#36090) Fixes #36050

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+30.sha-9ba46d9
+ * @license Angular v9.1.0-rc.0+31.sha-ff4eb0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19736,7 +19736,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+30.sha-9ba46d9');
+    var VERSION$1 = new Version('9.1.0-rc.0+31.sha-ff4eb0c');
 
     /**
      * @license
