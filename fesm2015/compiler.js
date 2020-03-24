ed82a909279e693df93779d7a527c069436883e4
Misko Hevery <misko@hevery.com>
2020-03-24 17:38:53 +0000

    58f4254fba fix(dev-infra): use @angular/dev-infra-private package for pullapprove verification (#35996)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+26.sha-380de1e
+ * @license Angular v9.1.0-rc.0+22.sha-58f4254
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18439,7 +18439,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+26.sha-380de1e');
+const VERSION$1 = new Version('9.1.0-rc.0+22.sha-58f4254');
 
 /**
  * @license
