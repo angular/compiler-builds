da2575de6fdc4e6198730a801a64d27456723cd2
Misko Hevery <misko@hevery.com>
2020-03-25 19:12:51 +0000

    d37dad82f1 build: ensure that refs and shas for PRs only need to be requested once (#36207)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+36.sha-22710fc
+ * @license Angular v9.1.0-rc.0+37.sha-d37dad8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+36.sha-22710fc');
+var VERSION$1 = new Version('9.1.0-rc.0+37.sha-d37dad8');
 
 /**
  * @license
