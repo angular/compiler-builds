717139b088325c1c898bcb0bb7231dd69c0be72c
Misko Hevery <misko@hevery.com>
2020-03-25 21:42:58 +0000

    294c6297d7 docs: coalesce release notes for the v9.1.0 release (#36247)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+1.sha-58fc65d
+ * @license Angular v9.1.0+2.sha-294c629
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+1.sha-58fc65d');
+    var VERSION$1 = new Version('9.1.0+2.sha-294c629');
 
     /**
      * @license
