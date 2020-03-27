a60da5d37e87cb0775132aef715817b01d995518
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 21:35:56 +0000

    6ea232eb3c fix(ngcc): do not spawn more processes than intended in parallel mode (#36280)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+14.sha-0baab0f
+ * @license Angular v9.1.0+15.sha-6ea232e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18494,7 +18494,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0+14.sha-0baab0f');
+const VERSION$1 = new Version('9.1.0+15.sha-6ea232e');
 
 /**
  * @license
