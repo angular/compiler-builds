db8cd90500c1b174d1959b7885ed8302ba93c8af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-27 18:22:08 +0000

    bfa7b1a494 docs: correct a misleading sentence (#36155) (#36158)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+38.sha-fc3e5cb
+ * @license Angular v9.1.0-rc.0+46.sha-bfa7b1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+38.sha-fc3e5cb');
+    var VERSION$1 = new Version('9.1.0-rc.0+46.sha-bfa7b1a');
 
     /**
      * @license
