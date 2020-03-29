6bdc2b5f3483be31ca223229dcf98bc0ebdc1732
Igor Minar <igor@angular.io>
2020-03-29 06:43:37 +0000

    c94a33c525 refactor(dev-infra): fix lint warnings for Pullapprove-related scripts (#36287)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+54.sha-5cee709
+ * @license Angular v9.1.0-rc.0+55.sha-c94a33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18494,7 +18494,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+54.sha-5cee709');
+const VERSION$1 = new Version('9.1.0-rc.0+55.sha-c94a33c');
 
 /**
  * @license
