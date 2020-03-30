d3714b28b9b966383ade356feff1e33023cdc4ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 18:57:24 +0000

    5ac308060d refactor(ngcc): rename `workerCount` to `maxWorkerCount` (#36298)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+58.sha-c6dd900
+ * @license Angular v9.1.0-rc.0+57.sha-5ac3080
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18494,7 +18494,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+58.sha-c6dd900');
+const VERSION$1 = new Version('9.1.0-rc.0+57.sha-5ac3080');
 
 /**
  * @license
