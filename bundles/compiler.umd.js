1eacfe6cd902a70286acb3903249f8624b3a65d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:52:46 +0000

    2510e7dad6 docs: fix typo in Schematics guide (#36328)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+60.sha-5b6ced5
+ * @license Angular v9.1.0-rc.0+62.sha-2510e7d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+60.sha-5b6ced5');
+    var VERSION$1 = new Version('9.1.0-rc.0+62.sha-2510e7d');
 
     /**
      * @license
