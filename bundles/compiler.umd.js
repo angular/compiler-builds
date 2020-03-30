8d5cec55f3890b1e163d48ee25f59ab891e976f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:54:47 +0000

    0ab244f39e docs: fix typo in Schematics guide (#36328)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+20.sha-15cffa2
+ * @license Angular v9.1.0+22.sha-0ab244f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+20.sha-15cffa2');
+    var VERSION$1 = new Version('9.1.0+22.sha-0ab244f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+20.sha-15cffa2
+ * @license Angular v9.1.0+22.sha-0ab244f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18494,7 +18494,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0+20.sha-15cffa2');
+const VERSION$1 = new Version('9.1.0+22.sha-0ab244f');
 
 /**
  * @license
