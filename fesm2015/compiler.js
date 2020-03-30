c871da86a15f0c59596e809913c1e42fe212be1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:24:52 +0000

    5b6ced5599 build: update to rules_nodejs 1.5.0 (#36307)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+59.sha-a383116
+ * @license Angular v9.1.0-rc.0+60.sha-5b6ced5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18494,7 +18494,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+59.sha-a383116');
+const VERSION$1 = new Version('9.1.0-rc.0+60.sha-5b6ced5');
 
 /**
  * @license
