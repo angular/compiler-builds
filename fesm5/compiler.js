cdff5f2580fe0db6113efe022d8b669355f29905
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-31 18:27:19 +0000

    08348fc2e8 build(docs-infra): rename duplicate test name (#36348)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+65.sha-63fbc71
+ * @license Angular v9.1.0-rc.0+66.sha-08348fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+65.sha-63fbc71');
+var VERSION$1 = new Version('9.1.0-rc.0+66.sha-08348fc');
 
 /**
  * @license
