8db917d1a3e96454929e9d8421e2c2516d9d5619
Kara Erickson <karakara@google.com>
2020-04-01 20:35:19 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+67.sha-ec8bae1
+ * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+67.sha-ec8bae1');
+var VERSION$1 = new Version('9.1.0-rc.0+68.sha-4d1d0fa');
 
 /**
  * @license
