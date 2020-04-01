c16b8357acdb06291b2b0e6c1f2b9fbe42fa929d
Kara Erickson <karakara@google.com>
2020-04-01 22:44:49 +0000

    4d458db1b5 docs: update and edit web-worker page (#36383)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+84.sha-326240e
+ * @license Angular v9.1.0-rc.0+87.sha-4d458db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+84.sha-326240e');
+var VERSION$1 = new Version('9.1.0-rc.0+87.sha-4d458db');
 
 /**
  * @license
