b7e258429703c2ff68e62b0242765ee7131afa4f
Kara Erickson <karakara@google.com>
2020-04-01 18:32:23 +0000

    beb3f19dc0 docs: update end date of survey to middle of April (#36339)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+26.sha-e8f3c47
+ * @license Angular v9.1.0+27.sha-beb3f19
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+26.sha-e8f3c47');
+    var VERSION$1 = new Version('9.1.0+27.sha-beb3f19');
 
     /**
      * @license
