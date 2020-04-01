e32b075301cb3fe346ac91a133a7daacf74bcb4c
Kara Erickson <karakara@google.com>
2020-04-01 23:02:30 +0000

    e9de28111d build: enable service-worker tests on saucelabs (#36129)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+88.sha-a5eb0e5
+ * @license Angular v9.1.0-rc.0+90.sha-e9de281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+88.sha-a5eb0e5');
+var VERSION$1 = new Version('9.1.0-rc.0+90.sha-e9de281');
 
 /**
  * @license
