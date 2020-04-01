45733e9b15f70245429bb4330fb63cdac2969f41
Kara Erickson <karakara@google.com>
2020-04-01 22:59:24 +0000

    a5eb0e56b6 docs(zone.js): fix typos and align formatting (#35907)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+86.sha-24a9247
+ * @license Angular v9.1.0-rc.0+88.sha-a5eb0e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19791,7 +19791,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+86.sha-24a9247');
+    var VERSION$1 = new Version('9.1.0-rc.0+88.sha-a5eb0e5');
 
     /**
      * @license
