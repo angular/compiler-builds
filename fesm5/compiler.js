bc58749ca910d1fffc025db904e73c576d877d04
Kara Erickson <karakara@google.com>
2020-04-03 18:33:57 +0000

    776f991c1a build: update pullapprove config to better handle global approvals (#36384)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+56.sha-9f486c3
+ * @license Angular v9.1.0+67.sha-776f991
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19613,7 +19613,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0+56.sha-9f486c3');
+var VERSION$1 = new Version('9.1.0+67.sha-776f991');
 
 /**
  * @license
