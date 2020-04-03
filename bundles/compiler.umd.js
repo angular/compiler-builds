8e7dbaed92f02c2622f88c879c1b9acc6322a708
Kara Erickson <karakara@google.com>
2020-04-03 21:23:23 +0000

    c8bef1259c docs: adding Doguhan Uluca as GDE resource (#34789)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+109.sha-568e9df
+ * @license Angular v9.1.0-rc.0+110.sha-c8bef12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19808,7 +19808,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+109.sha-568e9df');
+    var VERSION$1 = new Version('9.1.0-rc.0+110.sha-c8bef12');
 
     /**
      * @license
