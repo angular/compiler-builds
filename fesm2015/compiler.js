dc8717bdbc82bb1aa18a4a0d6af6f718af00163a
Kara Erickson <karakara@google.com>
2020-04-06 16:58:01 +0000

    1b4df6484e docs: add missing command in `WebWorker` guide (#36397)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+115.sha-1140bbc
+ * @license Angular v9.1.0-rc.0+114.sha-1b4df64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18492,7 +18492,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.0-rc.0+115.sha-1140bbc');
+const VERSION$1 = new Version('9.1.0-rc.0+114.sha-1b4df64');
 
 /**
  * @license
