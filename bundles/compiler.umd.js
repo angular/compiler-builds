6a3b1d244566cd5a0ade3b6a46ae39792ddf4d6a
Kara Erickson <karakara@google.com>
2020-04-06 23:00:41 +0000

    93302b7fb8 build: update to latest version of husky (#36459)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+130.sha-416c786
+ * @license Angular v9.1.0-rc.0+129.sha-93302b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+130.sha-416c786');
+    var VERSION$1 = new Version('9.1.0-rc.0+129.sha-93302b7');
 
     /**
      * @license
