632109a16c596b325ed95ad426d1ecceb3fd4af4
Kara Erickson <karakara@google.com>
2020-04-06 22:56:34 +0000

    aeb6d0d0f7 fix(zone.js): should not try to patch fetch if it is not writable (#36311)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+81.sha-068e283
+ * @license Angular v9.1.0+88.sha-aeb6d0d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+81.sha-068e283');
+    var VERSION$1 = new Version('9.1.0+88.sha-aeb6d0d');
 
     /**
      * @license
