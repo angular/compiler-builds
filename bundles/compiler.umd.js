00dfcfdb51a13c2aa4742583a656c82e644a8d16
Kara Erickson <karakara@google.com>
2020-04-06 20:44:21 +0000

    d43c30688a fix(core): avoid migration error when non-existent symbol is imported (#36367)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+121.sha-ee70a18
+ * @license Angular v9.1.0-rc.0+122.sha-d43c306
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0-rc.0+121.sha-ee70a18');
+    var VERSION$1 = new Version('9.1.0-rc.0+122.sha-d43c306');
 
     /**
      * @license
