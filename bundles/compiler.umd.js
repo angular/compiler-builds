0adfe04049c45a8944ad96c1cb000a47646c9954
Kara Erickson <karakara@google.com>
2020-04-07 17:50:53 +0000

    af4269471d fix(core): ngOnDestroy on multi providers called with incorrect context (#35840)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+89.sha-f48a065
+ * @license Angular v9.1.0+95.sha-af42694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+89.sha-f48a065');
+    var VERSION$1 = new Version('9.1.0+95.sha-af42694');
 
     /**
      * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+89.sha-f48a065
+ * @license Angular v9.1.0+95.sha-af42694
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19611,7 +19611,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0+89.sha-f48a065');
+var VERSION$1 = new Version('9.1.0+95.sha-af42694');
 
 /**
  * @license
