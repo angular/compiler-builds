083eb054f80e81fc3ad25147ad4adbc8637785ac
Kara Erickson <karakara@google.com>
2020-04-07 17:09:19 +0000

    f48a065db0 style(forms): reformat of the `forms` package after clang update (#36466)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+88.sha-aeb6d0d
+ * @license Angular v9.1.0+89.sha-f48a065
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.0+88.sha-aeb6d0d');
+    var VERSION$1 = new Version('9.1.0+89.sha-f48a065');
 
     /**
      * @license
