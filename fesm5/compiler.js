124301800546010defe330cd251b3ec3a90092bf
Kara Erickson <karakara@google.com>
2020-04-07 18:53:13 +0000

    e0415dbf16 fix(router): state data missing in routerLink (#36462)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+137.sha-95fc3d4
+ * @license Angular v9.1.0-rc.0+138.sha-e0415db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19611,7 +19611,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.0-rc.0+137.sha-95fc3d4');
+var VERSION$1 = new Version('9.1.0-rc.0+138.sha-e0415db');
 
 /**
  * @license
