65c3b97f1f716a694462211463785628473f8735
atscott <atscott@google.com>
2020-04-08 21:54:20 +0000

    717df13207 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1
+ * @license Angular v10.0.0-next.1+1.sha-717df13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18492,7 +18492,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1');
+const VERSION$1 = new Version('10.0.0-next.1+1.sha-717df13');
 
 /**
  * @license
