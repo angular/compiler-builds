db7f78ff9a22b9b59586867e8ce702f40b255fbf
atscott <atscott@google.com>
2020-04-08 22:48:13 +0000

    5da621d3dd fix(language-service): remove circular dependency instance (#36463)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20350,7 +20350,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+11.sha-cbed582');
+    var VERSION$1 = new Version('9.1.1+12.sha-5da621d');
 
     /**
      * @license
