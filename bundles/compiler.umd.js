4abfe63f1693655a9a5019064c890cb27ec54d0d
Kara Erickson <karakara@google.com>
2020-04-08 19:32:27 +0000

    72053b0f27 build(bazel): fix runfiles resolve in karma-saucelabs.js after $location => $rootpath cleanup (#36511)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+1.sha-52ab939
+ * @license Angular v9.1.1+8.sha-72053b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+1.sha-52ab939');
+    var VERSION$1 = new Version('9.1.1+8.sha-72053b0');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+1.sha-52ab939
+ * @license Angular v9.1.1+8.sha-72053b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -18492,7 +18492,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.1+1.sha-52ab939');
+const VERSION$1 = new Version('9.1.1+8.sha-72053b0');
 
 /**
  * @license
