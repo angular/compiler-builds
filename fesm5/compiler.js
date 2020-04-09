b39f32238768b912d8110fd8c28e348853431238
atscott <atscott@google.com>
2020-04-09 17:51:10 +0000

    66724fd159 docs(forms): clarify the description of `minLength` and `maxLength` (#36297)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+17.sha-c8f9092
+ * @license Angular v9.1.1+19.sha-66724fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20148,7 +20148,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.1+17.sha-c8f9092');
+var VERSION$1 = new Version('9.1.1+19.sha-66724fd');
 
 /**
  * @license
