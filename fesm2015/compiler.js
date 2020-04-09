ff00d17d933e0d90c9ba3703acb61dc5def43659
atscott <atscott@google.com>
2020-04-09 18:49:54 +0000

    078b0be4dc fix(common): `locales/global/*.js` are not ES5 compliant (#36342)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+11.sha-99b93e5
+ * @license Angular v10.0.0-next.1+13.sha-078b0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+11.sha-99b93e5');
+const VERSION$1 = new Version('10.0.0-next.1+13.sha-078b0be');
 
 /**
  * @license
