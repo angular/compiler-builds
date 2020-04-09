4edaefb56464b37ea4c641efac9db1ba7ac9efd4
atscott <atscott@google.com>
2020-04-09 23:49:26 +0000

    fee316161d build: update `REQUIRED_BASE_SHA` in merge script to clang 1.4.0 upgrade commit (#36547)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+18.sha-2e4244d
+ * @license Angular v10.0.0-next.1+19.sha-fee3161
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20343,7 +20343,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.1+18.sha-2e4244d');
+    var VERSION$1 = new Version('10.0.0-next.1+19.sha-fee3161');
 
     /**
      * @license
