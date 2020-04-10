c079f23e48ff114d4d2176a6f22cd46e7d76aa3e
atscott <atscott@google.com>
2020-04-10 16:29:28 +0000

    6ab43d7335 fix(ngcc): correctly detect external files from nested `node_modules/` (#36559)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+19.sha-fee3161
+ * @license Angular v10.0.0-next.1+20.sha-6ab43d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+19.sha-fee3161');
+const VERSION$1 = new Version('10.0.0-next.1+20.sha-6ab43d7');
 
 /**
  * @license
