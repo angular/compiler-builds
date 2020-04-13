c19e2d4a5317f61250f32cdac1bf97bd9f3870cf
atscott <atscott@google.com>
2020-04-13 15:46:31 +0000

    2d16b4711e fix(docs-infra): fix About page button text being truncated on small screens (#36576)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+25.sha-d972821
+ * @license Angular v10.0.0-next.1+24.sha-2d16b47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+25.sha-d972821');
+const VERSION$1 = new Version('10.0.0-next.1+24.sha-2d16b47');
 
 /**
  * @license
