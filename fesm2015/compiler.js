44bce01a959545bc30cc6b787ecf0404edda9d8a
atscott <atscott@google.com>
2020-04-13 15:40:49 +0000

    5737e65700 docs(zone.js): fix typos in NgZone guide code example (#36597)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+23.sha-9181b73
+ * @license Angular v10.0.0-next.1+22.sha-5737e65
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+23.sha-9181b73');
+const VERSION$1 = new Version('10.0.0-next.1+22.sha-5737e65');
 
 /**
  * @license
