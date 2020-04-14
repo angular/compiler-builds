557a60006082de514afc4927704861e19df2a0f4
atscott <atscott@google.com>
2020-04-14 21:22:55 +0000

    c5e725111d build: fix circular deps failure (#36629)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+41.sha-9399386
+ * @license Angular v9.1.1+42.sha-c5e7251
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20343,7 +20343,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+41.sha-9399386');
+    var VERSION$1 = new Version('9.1.1+42.sha-c5e7251');
 
     /**
      * @license
