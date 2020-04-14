207791e9be2e9a421233187fdfaf3cd2a16f5f35
atscott <atscott@google.com>
2020-04-14 17:39:30 +0000

    5e80e7e216 refactor(language-service): clean up and exports and consolidate types (#36533)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+31.sha-d50cb30
+ * @license Angular v10.0.0-next.1+32.sha-5e80e7e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+31.sha-d50cb30');
+const VERSION$1 = new Version('10.0.0-next.1+32.sha-5e80e7e');
 
 /**
  * @license
