c91f0b274eb6908798ac72704b0ed53b6d9ee6d0
atscott <atscott@google.com>
2020-04-14 17:36:28 +0000

    29122c518b style: lint (#36580)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.1+27.sha-0cc53fb');
+const VERSION$1 = new Version('10.0.0-next.1+30.sha-29122c5');
 
 /**
  * @license
