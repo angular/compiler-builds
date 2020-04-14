b8140ca2a8cb5a99538927a80215d06325d6cabb
Joey Perrott <josephperrott@gmail.com>
2020-04-14 01:04:25 +0000

    870657ca93 build: reformat repo to new clang@1.4.0

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+35.sha-97c1f02
+ * @license Angular v9.1.1+35.sha-870657c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20148,7 +20148,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.1+35.sha-97c1f02');
+var VERSION$1 = new Version('9.1.1+35.sha-870657c');
 
 /**
  * @license
