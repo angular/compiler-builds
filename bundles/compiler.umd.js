fd1e846a676032dafd10fdee9d617b9ec6fddd50
atscott <atscott@google.com>
2020-04-14 00:55:11 +0000

    c8f2ca2349 docs: edit to setup-local (#36168)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+34.sha-fd76982
+ * @license Angular v9.1.1+36.sha-c8f2ca2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20343,7 +20343,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.1+34.sha-fd76982');
+    var VERSION$1 = new Version('9.1.1+36.sha-c8f2ca2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+34.sha-fd76982
+ * @license Angular v9.1.1+36.sha-c8f2ca2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.1+34.sha-fd76982');
+const VERSION$1 = new Version('9.1.1+36.sha-c8f2ca2');
 
 /**
  * @license
