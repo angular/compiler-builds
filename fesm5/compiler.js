56d68527ec5cc8da756f931700264b006809dd96
atscott <atscott@google.com>
2020-04-14 19:33:22 +0000

    26f49151e7 build: reformat repo to new clang@1.4.0 (#36628)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+36.sha-c8f2ca2
+ * @license Angular v9.1.1+40.sha-26f4915
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20148,7 +20148,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.1+36.sha-c8f2ca2');
+var VERSION$1 = new Version('9.1.1+40.sha-26f4915');
 
 /**
  * @license
