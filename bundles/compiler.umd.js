7b1462701bfa2d8bba7ef839d721d488bb20c95d
atscott <atscott@google.com>
2020-04-16 18:29:49 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+18.sha-305b059');
+    var VERSION$1 = new Version('10.0.0-next.2+19.sha-bfda7ca');
 
     /**
      * @license
