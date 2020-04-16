d196901f5c6e3bee2ad2f8e20ab1fd4570aa2ada
atscott <atscott@google.com>
2020-04-16 00:09:22 +0000

    793a001d7c release: cut the v9.1.2 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.1+46.sha-5c3774c');
+const VERSION$1 = new Version('9.1.2');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+46.sha-5c3774c
+ * @license Angular v9.1.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20148,7 +20148,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.1+46.sha-5c3774c');
+var VERSION$1 = new Version('9.1.2');
 
 /**
  * @license
