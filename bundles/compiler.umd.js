84c844c8e638d714cb96e0e243e9df17b4674d23
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:29:23 +0000

    f2fca3e243 docs: getting started guide use pipe before introduction (#36584)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+18.sha-1e4fb74
+ * @license Angular v9.1.2+29.sha-f2fca3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.2+18.sha-1e4fb74');
+    var VERSION$1 = new Version('9.1.2+29.sha-f2fca3e');
 
     /**
      * @license
