b80ba0d9efd6accb4779f9dce003bdafb2e482a5
atscott <atscott@google.com>
2020-04-16 17:03:17 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+2.sha-03ff380
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19065,7 +19065,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+2.sha-03ff380');
+const VERSION$1 = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @license
