8b7f9aafa2ecc862eb50556e87c87fb7b0782465
atscott <atscott@google.com>
2020-04-16 18:02:53 +0000

    1e4fb74ec8 docs: refactor routing doc (#35566)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+17.sha-797c306
+ * @license Angular v9.1.2+18.sha-1e4fb74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.2+17.sha-797c306');
+const VERSION$1 = new Version('9.1.2+18.sha-1e4fb74');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+17.sha-797c306
+ * @license Angular v9.1.2+18.sha-1e4fb74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.2+17.sha-797c306');
+var VERSION$1 = new Version('9.1.2+18.sha-1e4fb74');
 
 /**
  * @license
