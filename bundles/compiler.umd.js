7c546184a946c0c9739d87ed60c0518a0f838345
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:27:39 +0000

    8be0972836 fix(language-service): properly evaluate types in comparable expressions (#36529)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+27.sha-c332d4d
+ * @license Angular v10.0.0-next.2+29.sha-8be0972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+27.sha-c332d4d');
+    var VERSION$1 = new Version('10.0.0-next.2+29.sha-8be0972');
 
     /**
      * @license
