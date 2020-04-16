3b8426c11e320d11a03bdc49c6b25589e6a93bbf
atscott <atscott@google.com>
2020-04-16 17:05:10 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20343,7 +20343,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+1.sha-c6e5fc4');
+    var VERSION$1 = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
     /**
      * @license
