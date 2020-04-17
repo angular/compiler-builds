8c42e6bad65587b9f1a61416330d1c898a3b2657
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 22:37:42 +0000

    d7e9d8746a build: list feat commits in patch branch in relase review script (#36651)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+44.sha-3517599
+ * @license Angular v10.0.0-next.2+45.sha-d7e9d87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+44.sha-3517599');
+const VERSION$1 = new Version('10.0.0-next.2+45.sha-d7e9d87');
 
 /**
  * @license
