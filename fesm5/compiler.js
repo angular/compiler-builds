39c975dc2d70b53f08d9ff8bffcb8ecccbc152c4
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:56:53 +0000

    95f45e8070 refactor(compiler): remove unused CachedFileSystem (#36687)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+37.sha-fd7c39e
+ * @license Angular v9.1.2+41.sha-95f45e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.2+37.sha-fd7c39e');
+var VERSION$1 = new Version('9.1.2+41.sha-95f45e8');
 
 /**
  * @license
