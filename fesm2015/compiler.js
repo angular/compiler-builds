eee75e267d127d8754b803449c893b68f36e7142
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 17:00:13 +0000

    fd7c39e3cf fixup!: build(docs-infra): Ensures that only member docs are linked (#36316)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+30.sha-0fa5ac8
+ * @license Angular v9.1.2+37.sha-fd7c39e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.2+30.sha-0fa5ac8');
+const VERSION$1 = new Version('9.1.2+37.sha-fd7c39e');
 
 /**
  * @license
