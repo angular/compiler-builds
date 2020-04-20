7bd7965abce456a3c762c295d369800baa9a0a14
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 20:43:21 +0000

    254b9ea44c docs: place download section in accessibility to the top (#36561)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+46.sha-2a53f47
+ * @license Angular v9.1.2+47.sha-254b9ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.2+46.sha-2a53f47');
+const VERSION$1 = new Version('9.1.2+47.sha-254b9ea');
 
 /**
  * @license
