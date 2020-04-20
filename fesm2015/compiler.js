af391532d17f92823ff7abd351f9819bf29f679c
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:50:21 +0000

    4341743b4a fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+48.sha-b5e92b9
+ * @license Angular v10.0.0-next.2+49.sha-4341743
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+48.sha-b5e92b9');
+const VERSION$1 = new Version('10.0.0-next.2+49.sha-4341743');
 
 /**
  * @license
