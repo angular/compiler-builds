ca22cf277e25ff4530fbdc3e857e3aaa343f5cc7
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:48:29 +0000

    4341743b4a fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+48.sha-b5e92b9
+ * @license Angular v10.0.0-next.2+49.sha-4341743
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+48.sha-b5e92b9');
+    var VERSION$1 = new Version('10.0.0-next.2+49.sha-4341743');
 
     /**
      * @license
