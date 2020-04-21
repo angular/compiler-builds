9eac23ca42ac15eeca995fc6411f391c9ef8fa4b
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:18:00 +0000

    28995dba19 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+51.sha-51b0924
+ * @license Angular v10.0.0-next.2+54.sha-28995db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+51.sha-51b0924');
+const VERSION$1 = new Version('10.0.0-next.2+54.sha-28995db');
 
 /**
  * @license
