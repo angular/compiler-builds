daeb4b3c8e6b0022d47eb78cadf2def217a7dc38
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:51:00 +0000

    c0ed57db76 fix(core): do not use unbound attributes as inputs to structural directives (#36441)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+51.sha-0bd50e2
+ * @license Angular v9.1.2+52.sha-c0ed57d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.2+51.sha-0bd50e2');
+    var VERSION$1 = new Version('9.1.2+52.sha-c0ed57d');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+51.sha-0bd50e2
+ * @license Angular v9.1.2+52.sha-c0ed57d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.2+51.sha-0bd50e2');
+const VERSION$1 = new Version('9.1.2+52.sha-c0ed57d');
 
 /**
  * @license
