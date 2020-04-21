3d66d7980bbe4c9d87232e080d42bf9b270ec25c
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:10:27 +0000

    f186c32245 docs: place download section in architecture to the top (#36565)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+50.sha-458dc10
+ * @license Angular v10.0.0-next.2+53.sha-f186c32
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+50.sha-458dc10');
+    var VERSION$1 = new Version('10.0.0-next.2+53.sha-f186c32');
 
     /**
      * @license
