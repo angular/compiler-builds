fc9ac06682a06b3c94a8d15bbfea04e58afcd5c4
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 19:16:13 +0000

    e1071615c6 release: cut the v9.1.3 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.2+56.sha-8bd5374');
+const VERSION$1 = new Version('9.1.3');
 
 /**
  * @license
