1f47606a6bd062dcd3f539233ff9ad354d61e3e7
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 04:46:38 +0000

    5db8d93fe5 build: fix the compare master and patch script output (#36749)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+57.sha-535c249
+ * @license Angular v10.0.0-next.2+58.sha-5db8d93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+57.sha-535c249');
+const VERSION$1 = new Version('10.0.0-next.2+58.sha-5db8d93');
 
 /**
  * @license
