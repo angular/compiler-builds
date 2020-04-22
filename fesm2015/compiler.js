3f32e8697d2300d04be15fde488cd0acff848d39
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:33:13 +0000

    d40bcce84e build: add AndrewKushnir to the `fw-testing` group (#36744)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+2.sha-aa94cd5
+ * @license Angular v9.1.3+6.sha-d40bcce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.3+2.sha-aa94cd5');
+const VERSION$1 = new Version('9.1.3+6.sha-d40bcce');
 
 /**
  * @license
