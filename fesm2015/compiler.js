e6cee5fb743f0c3515c0afde967131f0def934b7
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:31:20 +0000

    8306da5ce1 release(benchpress): bump version of benchpress to 0.2.0 (#36457)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+2.sha-47f9867
+ * @license Angular v10.0.0-next.3+5.sha-8306da5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.3+2.sha-47f9867');
+const VERSION$1 = new Version('10.0.0-next.3+5.sha-8306da5');
 
 /**
  * @license
