15dac721237cea143bf9edc7dd387ba5b0948f1f
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:33:48 +0000

    1b3ee17888 build: move circular deps golden to a subfolder (#36630)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+3.sha-fc1b557
+ * @license Angular v10.0.0-next.3+6.sha-1b3ee17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+3.sha-fc1b557');
+    var VERSION$1 = new Version('10.0.0-next.3+6.sha-1b3ee17');
 
     /**
      * @license
