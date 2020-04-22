5138d5d2c45dd7a0fbba7f5d35d41b1d163e9f11
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:18:27 +0000

    942b986ef3 fix(core): properly identify modules affected by overrides in TestBed (#36649)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+55.sha-acf6075
+ * @license Angular v10.0.0-next.2+56.sha-942b986
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.2+55.sha-acf6075');
+    var VERSION$1 = new Version('10.0.0-next.2+56.sha-942b986');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+55.sha-acf6075
+ * @license Angular v10.0.0-next.2+56.sha-942b986
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.2+55.sha-acf6075');
+const VERSION$1 = new Version('10.0.0-next.2+56.sha-942b986');
 
 /**
  * @license
