b0d46e2a6c06de7c0c0bc76d9d2435cb8bb1b99d
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:39:01 +0000

    f898c9ab57 build: require a commit body in commit messages (#36632)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+9.sha-7743c43
+ * @license Angular v9.1.3+15.sha-f898c9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.3+9.sha-7743c43');
+const VERSION$1 = new Version('9.1.3+15.sha-f898c9a');
 
 /**
  * @license
