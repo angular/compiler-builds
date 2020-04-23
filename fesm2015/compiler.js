2462a99a3a05ae1640462b489d1507debc4b27b8
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:36:14 +0000

    f0ec4ae941 ci: fix pullapprove incorrectly skipping fw-compiler as owner (#36661)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+16.sha-e5d2853
+ * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.3+16.sha-e5d2853');
+const VERSION$1 = new Version('10.0.0-next.3+19.sha-f0ec4ae');
 
 /**
  * @license
