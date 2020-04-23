6bf7a94c64a64734a39459a0a4c5b268b1037975
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:36:15 +0000

    7743c43529 test(router): fix router test failing on IE (#36742)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.3+6.sha-d40bcce');
+const VERSION$1 = new Version('9.1.3+9.sha-7743c43');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+6.sha-d40bcce
+ * @license Angular v9.1.3+9.sha-7743c43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.3+6.sha-d40bcce');
+var VERSION$1 = new Version('9.1.3+9.sha-7743c43');
 
 /**
  * @license
