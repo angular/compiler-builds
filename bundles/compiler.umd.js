7187ad0e5f00ba452f8836cc36d9625dae7b256a
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:33:57 +0000

    152293d3cf test(router): fix router test failing on IE (#36742)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+14.sha-ceb61d1
+ * @license Angular v10.0.0-next.3+15.sha-152293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+14.sha-ceb61d1');
+    var VERSION$1 = new Version('10.0.0-next.3+15.sha-152293d');
 
     /**
      * @license
