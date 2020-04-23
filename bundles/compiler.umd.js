e841e10e7d522067fde2637549ef0828d8634dfd
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:40:27 +0000

    e37f2663c2 build: require a commit body in commit messages (#36632)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
+ * @license Angular v10.0.0-next.3+21.sha-e37f266
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+19.sha-f0ec4ae');
+    var VERSION$1 = new Version('10.0.0-next.3+21.sha-e37f266');
 
     /**
      * @license
