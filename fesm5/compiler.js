33a8b8b87a1c7126697fdfd64c1ade1332d0db65
Matias Niemelä <matias@yearofmoo.com>
2020-04-23 16:28:54 +0000

    113c75dd27 refactor(localize): remove unused code (#36745)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+7.sha-4275b34
+ * @license Angular v10.0.0-next.3+12.sha-113c75d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('10.0.0-next.3+7.sha-4275b34');
+var VERSION$1 = new Version('10.0.0-next.3+12.sha-113c75d');
 
 /**
  * @license
