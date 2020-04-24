e54b64eafa29ffb2dbab9d279f3543cf39d3ab80
Andrew Kushnir <akushnir@google.com>
2020-04-24 19:54:12 +0000

    2365bb89d7 build: migrate from gulp to ng-dev for running formatting (#36726)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+30.sha-ad8c4cd
+ * @license Angular v10.0.0-next.3+32.sha-2365bb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+30.sha-ad8c4cd');
+    var VERSION$1 = new Version('10.0.0-next.3+32.sha-2365bb8');
 
     /**
      * @license
