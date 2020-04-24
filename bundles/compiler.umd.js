8b1abe55f62f68490a6b1995d76976982c1905a4
Andrew Kushnir <akushnir@google.com>
2020-04-24 18:30:33 +0000

    cc71116ba6 revert: "feat(dev-infra): exposed new rule 'component_benchmark' via dev_infra (#36434)" (#36798)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+21.sha-b3fe901
+ * @license Angular v9.1.3+24.sha-cc71116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.3+21.sha-b3fe901');
+    var VERSION$1 = new Version('9.1.3+24.sha-cc71116');
 
     /**
      * @license
