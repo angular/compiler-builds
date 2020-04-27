599c2e312bb4b2722c56b3041089377df000f825
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:18:38 +0000

    f27deea003 fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+35.sha-00e6cb1
+ * @license Angular v10.0.0-next.3+36.sha-f27deea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('10.0.0-next.3+35.sha-00e6cb1');
+var VERSION$1 = new Version('10.0.0-next.3+36.sha-f27deea');
 
 /**
  * @license
