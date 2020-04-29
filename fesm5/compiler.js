2b9c11e09244a084b6d0e618663f4f07078bcd05
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:52:06 +0000

    c8c2272a9f fix(core): Refresh transplanted views at insertion point only (#35968)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4
+ * @license Angular v9.1.4+14.sha-c8c2272
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20165,7 +20165,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.4');
+var VERSION$1 = new Version('9.1.4+14.sha-c8c2272');
 
 /**
  * @license
