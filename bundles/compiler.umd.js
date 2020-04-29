adab682f88d1bd5d2c8b1789ad47b37acd49e73c
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:19:44 +0000

    c89421bfa8 docs: release notes for the v9.1.4 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+47.sha-cebd4fa
+ * @license Angular v10.0.0-next.3+48.sha-c89421b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.3+47.sha-cebd4fa');
+    var VERSION$1 = new Version('10.0.0-next.3+48.sha-c89421b');
 
     /**
      * @license
