f58e64f2246b2458264c8b038d45bdb590aa1680
Andrew Kushnir <akushnir@google.com>
2020-04-29 20:27:17 +0000

    cebd4fada7 build: use sh instead of exec for release scripts (#36862)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+46.sha-3d82aa7
+ * @license Angular v10.0.0-next.3+47.sha-cebd4fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.3+46.sha-3d82aa7');
+const VERSION$1 = new Version('10.0.0-next.3+47.sha-cebd4fa');
 
 /**
  * @license
