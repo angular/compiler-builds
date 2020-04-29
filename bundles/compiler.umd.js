c5f6f926fbce12e4ed6b153ab975f441716bb389
Andrew Kushnir <akushnir@google.com>
2020-04-29 15:57:37 +0000

    d7433316b0 fix(core): attempt to recover from user errors during creation (#36381)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+35.sha-c440165
+ * @license Angular v9.1.3+36.sha-d743331
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.3+35.sha-c440165');
+    var VERSION$1 = new Version('9.1.3+36.sha-d743331');
 
     /**
      * @license
