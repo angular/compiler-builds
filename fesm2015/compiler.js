8a866cc269cfcbdbf5db9f84c6402e0b86657029
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-30 19:05:13 +0000

    49be32c931 refactor(common): remove WrappedValue from AsyncPipe (#36633)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+14.sha-1786586
+ * @license Angular v10.0.0-next.4+15.sha-49be32c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.4+14.sha-1786586');
+const VERSION$1 = new Version('10.0.0-next.4+15.sha-49be32c');
 
 /**
  * @license
