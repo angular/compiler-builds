2ab2465e7b068db24cbbaaada9fff5a882ba9f74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:23:51 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+15.sha-49be32c');
+    var VERSION$1 = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
     /**
      * @license
