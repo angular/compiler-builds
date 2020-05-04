cde3d35faf4a5d762a6107f9a3d7f1f50d8298c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:15:58 +0000

    6046e86506 build: update setup-rbe.sh script to accept non google.com/angular.io (#36846)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+43.sha-42c58d9
+ * @license Angular v10.0.0-next.4+37.sha-6046e86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20185,7 +20185,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('10.0.0-next.4+43.sha-42c58d9');
+var VERSION$1 = new Version('10.0.0-next.4+37.sha-6046e86');
 
 /**
  * @license
