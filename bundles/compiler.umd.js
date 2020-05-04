18ce9ecce35c29c9857648b1c33dc752dd758320
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:21:32 +0000

    b3713a112f test(language-service): Add method to override inline template (#36890)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+37.sha-6046e86
+ * @license Angular v10.0.0-next.4+42.sha-b3713a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+37.sha-6046e86');
+    var VERSION$1 = new Version('10.0.0-next.4+42.sha-b3713a1');
 
     /**
      * @license
