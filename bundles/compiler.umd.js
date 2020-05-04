1e04b69ad985c1f153d2fe750a857d86e21cb670
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 19:59:47 +0000

    d9c4840a9c fix(router): cancel navigation when at least one resolver completes with no "next" emission (#24621)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+29.sha-e9300c9
+ * @license Angular v10.0.0-next.4+32.sha-d9c4840
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+29.sha-e9300c9');
+    var VERSION$1 = new Version('10.0.0-next.4+32.sha-d9c4840');
 
     /**
      * @license
