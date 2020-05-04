808fef3491d17371afebb93f1def03e35e72fbac
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:13:05 +0000

    db4c59dad9 fix(ngcc): support TS 3.9 wrapped ES2015 classes (#36884)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+38.sha-58ea040
+ * @license Angular v10.0.0-next.4+39.sha-db4c59d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.4+38.sha-58ea040');
+const VERSION$1 = new Version('10.0.0-next.4+39.sha-db4c59d');
 
 /**
  * @license
