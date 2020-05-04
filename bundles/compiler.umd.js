f815423019232557735656de37d4efaabdaa80d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:15:23 +0000

    42c58d9fe8 docs(dev-infra): update format commands described in DEVELOPER.md (#36910)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+39.sha-db4c59d
+ * @license Angular v10.0.0-next.4+43.sha-42c58d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.4+39.sha-db4c59d');
+    var VERSION$1 = new Version('10.0.0-next.4+43.sha-42c58d9');
 
     /**
      * @license
