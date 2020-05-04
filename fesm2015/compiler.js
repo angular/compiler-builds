5e6f02cc75c9b27df43850b3ac818a8fc4b0e4d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:04:53 +0000

    0f3831b105 build: create macro for transpiling javascript file to es5 (#36802)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+33.sha-9d9d46f
+ * @license Angular v10.0.0-next.4+34.sha-0f3831b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.4+33.sha-9d9d46f');
+const VERSION$1 = new Version('10.0.0-next.4+34.sha-0f3831b');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+33.sha-9d9d46f
+ * @license Angular v10.0.0-next.4+34.sha-0f3831b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20185,7 +20185,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('10.0.0-next.4+33.sha-9d9d46f');
+var VERSION$1 = new Version('10.0.0-next.4+34.sha-0f3831b');
 
 /**
  * @license
