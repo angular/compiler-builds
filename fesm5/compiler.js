758bf5a72886b97e7ae6255907d147c519fd1621
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:19:08 +0000

    306f46ce92 build(docs-infra): upgrade docs examples to latest Angular CLI and framework (#36145)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+9.sha-70b25a3
+ * @license Angular v10.0.0-next.5+7.sha-306f46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20185,7 +20185,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('10.0.0-next.5+9.sha-70b25a3');
+var VERSION$1 = new Version('10.0.0-next.5+7.sha-306f46c');
 
 /**
  * @license
