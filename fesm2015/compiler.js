8f7c572eaf634bd98a53360af0cb32725e7b816c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:26:25 +0000

    20cc3ab37e refactor(core): make generic mandatory for ModuleWithProviders (#36892)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+54.sha-420b9be
+ * @license Angular v10.0.0-next.5+55.sha-20cc3ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.5+54.sha-420b9be');
+const VERSION$1 = new Version('10.0.0-next.5+55.sha-20cc3ab');
 
 /**
  * @license
