84b3521f6a37a222f396cd023eea3690b9599d6b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 00:26:38 +0000

    fbd281c26e build: remove typescript 3.6 and 3.7 support (#36329)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+22.sha-35d61c1
+ * @license Angular v10.0.0-next.5+24.sha-fbd281c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.5+22.sha-35d61c1');
+const VERSION$1 = new Version('10.0.0-next.5+24.sha-fbd281c');
 
 /**
  * @license
