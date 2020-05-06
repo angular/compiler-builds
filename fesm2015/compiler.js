a3b4709300f4affca3242c0d16ad7f4af82e0cbd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 20:56:32 +0000

    fafa50d97f fix(ngcc): support ModuleWithProviders functions that delegate (#36948)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+43.sha-c9e0db5
+ * @license Angular v10.0.0-next.5+45.sha-fafa50d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.5+43.sha-c9e0db5');
+const VERSION$1 = new Version('10.0.0-next.5+45.sha-fafa50d');
 
 /**
  * @license
