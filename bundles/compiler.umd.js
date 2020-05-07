b8970de7ee03bbe4714a91a75e22a06f0df7139c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:44:23 +0000

    97d6d909e7 fix(forms): number input fires valueChanges twice. (#36087)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+71.sha-f7815cf
+ * @license Angular v10.0.0-next.5+72.sha-97d6d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+71.sha-f7815cf');
+    var VERSION$1 = new Version('10.0.0-next.5+72.sha-97d6d90');
 
     /**
      * @license
