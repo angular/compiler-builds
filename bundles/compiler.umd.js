34385022ae83d227a2345d3f5bf058c553c465fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:53:23 +0000

    dcf1dcb757 test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36968)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+57.sha-9d13ee0
+ * @license Angular v9.1.4+67.sha-dcf1dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20360,7 +20360,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.4+57.sha-9d13ee0');
+    var VERSION$1 = new Version('9.1.4+67.sha-dcf1dcb');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.4+57.sha-9d13ee0
+ * @license Angular v9.1.4+67.sha-dcf1dcb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19082,7 +19082,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.4+57.sha-9d13ee0');
+const VERSION$1 = new Version('9.1.4+67.sha-dcf1dcb');
 
 /**
  * @license
