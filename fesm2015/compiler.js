ada94038c610ab58316590fffb8fdf933dab8a76
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:42:25 +0000

    97d6d909e7 fix(forms): number input fires valueChanges twice. (#36087)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+71.sha-f7815cf
+ * @license Angular v10.0.0-next.5+72.sha-97d6d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.5+71.sha-f7815cf');
+const VERSION$1 = new Version('10.0.0-next.5+72.sha-97d6d90');
 
 /**
  * @license
