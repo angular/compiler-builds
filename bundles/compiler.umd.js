e005df6354a39f35508f840dd0997dcbc63ebc77
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:08:39 +0000

    42d1091d6a fix(compiler-cli): don't try to tag non-ts files as shims (#36987)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.5+73.sha-d0280a0');
+    var VERSION$1 = new Version('10.0.0-next.5+74.sha-42d1091');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+73.sha-d0280a0
+ * @license Angular v10.0.0-next.5+74.sha-42d1091
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.5+73.sha-d0280a0');
+const VERSION$1 = new Version('10.0.0-next.5+74.sha-42d1091');
 
 /**
  * @license
