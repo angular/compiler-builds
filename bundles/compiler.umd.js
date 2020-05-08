74d3b9a07401dbf44a90dc679aacc5e03d6845c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:03:25 +0000

    14be55c9fa docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+7.sha-39e066a
+ * @license Angular v10.0.0-next.6+5.sha-14be55c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20380,7 +20380,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+7.sha-39e066a');
+    var VERSION$1 = new Version('10.0.0-next.6+5.sha-14be55c');
 
     /**
      * @license
