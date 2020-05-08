bb21ea580a0c81ccab98646accc49a384ce900a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:06:05 +0000

    178e750f16 docs: Fix typo in navigation.json (#36955)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+5.sha-14be55c
+ * @license Angular v10.0.0-next.6+6.sha-178e750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19102,7 +19102,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.6+5.sha-14be55c');
+const VERSION$1 = new Version('10.0.0-next.6+6.sha-178e750');
 
 /**
  * @license
