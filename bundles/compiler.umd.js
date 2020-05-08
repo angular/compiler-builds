5fa530b387cbf007d2af784dccac762dbf3394c1
Misko Hevery <misko@hevery.com>
2020-05-08 22:08:20 +0000

    ed1b4a8f19 docs: add migration guides to sidenav in updating to Version 9 (#34979)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+16.sha-eb34aa5
+ * @license Angular v10.0.0-next.6+18.sha-ed1b4a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20416,7 +20416,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+16.sha-eb34aa5');
+    var VERSION$1 = new Version('10.0.0-next.6+18.sha-ed1b4a8');
 
     /**
      * @license
