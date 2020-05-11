b45585bb9735d1251a631013f62fb0aff9bf1acf
Misko Hevery <misko@hevery.com>
2020-05-11 18:28:24 +0000

    40025f55ad refactor(language-service): move TS utils to separate file (#36984)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+23.sha-286fbf4
+ * @license Angular v10.0.0-next.6+22.sha-40025f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.6+23.sha-286fbf4');
+const VERSION$1 = new Version('10.0.0-next.6+22.sha-40025f5');
 
 /**
  * @license
