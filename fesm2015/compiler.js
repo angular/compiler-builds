bc8c3a5257eb42e534ef3f3a7d9300326d0bb2bb
Misko Hevery <misko@hevery.com>
2020-05-11 18:20:26 +0000

    7eb1a58b26 docs: refactor form validation doc to be task-oriented (#36374)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+20.sha-28f3c1c
+ * @license Angular v10.0.0-next.6+21.sha-7eb1a58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.6+20.sha-28f3c1c');
+const VERSION$1 = new Version('10.0.0-next.6+21.sha-7eb1a58');
 
 /**
  * @license
