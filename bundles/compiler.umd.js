f0119ae39d7d7a71b626d053775f1f9155eeabc1
Misko Hevery <misko@hevery.com>
2020-05-12 22:00:34 +0000

    7addc9f1a9 docs: add doc for template type guards to stuctural-directives.md (#34549)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
+ * @license Angular v10.0.0-next.6+30.sha-7addc9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20415,7 +20415,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.6+29.sha-cb6ddfc');
+    var VERSION$1 = new Version('10.0.0-next.6+30.sha-7addc9f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
+ * @license Angular v10.0.0-next.6+30.sha-7addc9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.6+29.sha-cb6ddfc');
+const VERSION$1 = new Version('10.0.0-next.6+30.sha-7addc9f');
 
 /**
  * @license
