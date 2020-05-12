949970c238e301ad1f906d73d13e536295129ab3
Misko Hevery <misko@hevery.com>
2020-05-12 20:38:42 +0000

    cb6ddfc215 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#37074)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+26.sha-198fc6f
+ * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.6+26.sha-198fc6f');
+const VERSION$1 = new Version('10.0.0-next.6+29.sha-cb6ddfc');
 
 /**
  * @license
