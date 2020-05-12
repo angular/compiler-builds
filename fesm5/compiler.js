e6e435f741097a24bb601cdc3cc74fdaaf0b95f8
Misko Hevery <misko@hevery.com>
2020-05-12 18:10:58 +0000

    468aeb454a docs: minor typo fix in the bazel documentation (#37043)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+11.sha-efe1713
+ * @license Angular v9.1.6+15.sha-468aeb4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20200,7 +20200,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.6+11.sha-efe1713');
+var VERSION$1 = new Version('9.1.6+15.sha-468aeb4');
 
 /**
  * @license
