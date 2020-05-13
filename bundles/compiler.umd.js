405806fc527075ce9f1d56aaa94ebe00febc7a52
Misko Hevery <misko@hevery.com>
2020-05-13 23:21:33 +0000

    45f4a47286 refactor(core): remove style sanitization code for `[style]`/`[style.prop]` bindings (#36965)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+8.sha-14c0ec9
+ * @license Angular v10.0.0-next.7+7.sha-45f4a47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+8.sha-14c0ec9');
+    var VERSION$1 = new Version('10.0.0-next.7+7.sha-45f4a47');
 
     /**
      * @license
