96a67af9612056422d2a525728b1b71f59bde0eb
Misko Hevery <misko@hevery.com>
2020-05-13 23:14:10 +0000

    019835c721 docs: minor typos (#36772)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+1.sha-255c2b2
+ * @license Angular v10.0.0-next.7+2.sha-019835c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+1.sha-255c2b2');
+const VERSION$1 = new Version('10.0.0-next.7+2.sha-019835c');
 
 /**
  * @license
