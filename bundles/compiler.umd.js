42541b34c71728e1b18d45d9f31c090fb6bff379
Misko Hevery <misko@hevery.com>
2020-05-13 23:12:16 +0000

    019835c721 docs: minor typos (#36772)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+1.sha-255c2b2
+ * @license Angular v10.0.0-next.7+2.sha-019835c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20415,7 +20415,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+1.sha-255c2b2');
+    var VERSION$1 = new Version('10.0.0-next.7+2.sha-019835c');
 
     /**
      * @license
