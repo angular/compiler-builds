d5a03b311fd09a9491650366cf22d16ebda7511e
Misko Hevery <misko@hevery.com>
2020-05-13 23:26:45 +0000

    9edea0bb75 feat(platform-server): use absolute URLs from Location for HTTP (#37071)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+10.sha-ce39755
+ * @license Angular v10.0.0-next.7+11.sha-9edea0bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+10.sha-ce39755');
+const VERSION$1 = new Version('10.0.0-next.7+11.sha-9edea0bb');
 
 /**
  * @license
