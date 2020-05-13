35380f1d068a7310d169b2250feb6bc291b57199
Misko Hevery <misko@hevery.com>
2020-05-13 23:14:54 +0000

    141fcb95a4 refactor(localize): use the `FileSystem` from ngtsc (#36843)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+2.sha-019835c
+ * @license Angular v10.0.0-next.7+6.sha-141fcb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19126,7 +19126,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+2.sha-019835c');
+const VERSION$1 = new Version('10.0.0-next.7+6.sha-141fcb9');
 
 /**
  * @license
