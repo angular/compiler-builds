0caa3689eaffcf508bb6779e8e51652312e4afe9
Kara Erickson <karakara@google.com>
2020-05-14 20:25:18 +0000

    7acd33007d fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+48.sha-8a56c99
+ * @license Angular v10.0.0-next.7+49.sha-7acd330
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+48.sha-8a56c99');
+const VERSION$1 = new Version('10.0.0-next.7+49.sha-7acd330');
 
 /**
  * @license
