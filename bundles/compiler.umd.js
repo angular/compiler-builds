fb4e06d4d309cacce90205cf17642582877d93b0
Kara Erickson <karakara@google.com>
2020-05-14 22:45:57 +0000

    7a30153aa1 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+49.sha-7acd330
+ * @license Angular v10.0.0-next.7+50.sha-7a30153
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+49.sha-7acd330');
+    var VERSION$1 = new Version('10.0.0-next.7+50.sha-7a30153');
 
     /**
      * @license
