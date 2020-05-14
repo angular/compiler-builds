37809619ac1bfd6181796c6fb647fe1609d29a15
Kara Erickson <karakara@google.com>
2020-05-14 18:22:21 +0000

    b2a3325258 docs: remove the note about @angular/http from the deprecations guide (#37070)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+43.sha-f16ca1c
+ * @license Angular v10.0.0-next.7+45.sha-b2a3325
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+43.sha-f16ca1c');
+    var VERSION$1 = new Version('10.0.0-next.7+45.sha-b2a3325');
 
     /**
      * @license
