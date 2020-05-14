a9f949b87a70a3fd7261b50cf32fb02926cad14b
Kara Erickson <karakara@google.com>
2020-05-14 19:22:00 +0000

    3813f54bec docs(router): filter the event to subscribe (#37027)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+33.sha-ef9da2f
+ * @license Angular v9.1.6+35.sha-3813f54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20200,7 +20200,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.6+33.sha-ef9da2f');
+var VERSION$1 = new Version('9.1.6+35.sha-3813f54');
 
 /**
  * @license
