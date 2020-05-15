349b4790c9b434f4bd47e3152cf931939520273a
Kara Erickson <karakara@google.com>
2020-05-15 18:49:33 +0000

    3d7c85b2aa feat(dev-infra): create tool to determine conflicts created by a PR (#37051)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+62.sha-55c2433
+ * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+62.sha-55c2433');
+    var VERSION$1 = new Version('10.0.0-next.7+63.sha-3d7c85b2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+62.sha-55c2433
+ * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+62.sha-55c2433');
+const VERSION$1 = new Version('10.0.0-next.7+63.sha-3d7c85b2');
 
 /**
  * @license
