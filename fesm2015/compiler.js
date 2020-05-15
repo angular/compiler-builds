6b3820bea06ea9e32cd2ccdab13f68dfa8c1cb59
Kara Erickson <karakara@google.com>
2020-05-15 17:45:39 +0000

    55c2433171 docs(changelog): remove duplicate features and bug fixes (#37101)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+61.sha-03afa36
+ * @license Angular v10.0.0-next.7+62.sha-55c2433
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.7+61.sha-03afa36');
+const VERSION$1 = new Version('10.0.0-next.7+62.sha-55c2433');
 
 /**
  * @license
