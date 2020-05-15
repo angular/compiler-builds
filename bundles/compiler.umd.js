6e6f487a60b9a4cd946a03556f8ba09fc2cbbc3b
Kara Erickson <karakara@google.com>
2020-05-15 02:39:32 +0000

    d808333047 ci: increase AIO payload size limit (#37124)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+37.sha-4de546e
+ * @license Angular v9.1.6+41.sha-d808333
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20395,7 +20395,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.6+37.sha-4de546e');
+    var VERSION$1 = new Version('9.1.6+41.sha-d808333');
 
     /**
      * @license
