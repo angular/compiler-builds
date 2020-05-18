9be13c47a1156f3dd493d45228ab9eb4f8f81084
Kara Erickson <karakara@google.com>
2020-05-18 17:01:31 +0000

    1754beb59c release: cut the v10.0.0-next.8 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+70.sha-844208f
+ * @license Angular v10.0.0-next.8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.7+70.sha-844208f');
+    var VERSION$1 = new Version('10.0.0-next.8');
 
     /**
      * @license
