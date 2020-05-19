e4bdde66b562fd530bbf09d92419b2790ef40136
Kara Erickson <karakara@google.com>
2020-05-19 17:41:20 +0000

    deac994517 refactor(docs-infra): remove `rev` property from `Resource` interface and data (#37181)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+24.sha-b754ab2
+ * @license Angular v10.0.0-next.8+25.sha-deac994
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.8+24.sha-b754ab2');
+    var VERSION$1 = new Version('10.0.0-next.8+25.sha-deac994');
 
     /**
      * @license
