9402ae347d479a38a06fb9eb98527798701160aa
Kara Erickson <karakara@google.com>
2020-05-19 00:18:26 +0000

    3a4e1b8f86 docs(router): corrected minor typo (#37166)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+15.sha-c2c14c2
+ * @license Angular v10.0.0-next.8+23.sha-3a4e1b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20385,7 +20385,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.8+15.sha-c2c14c2');
+    var VERSION$1 = new Version('10.0.0-next.8+23.sha-3a4e1b8');
 
     /**
      * @license
