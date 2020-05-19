fb26bba4501ffa320b6f67191822be9c2e218fca
Kara Erickson <karakara@google.com>
2020-05-19 22:25:05 +0000

    4e96cdc23f build: Add deprecation notice to merge-pr script to nudge to new tooling (#37204)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+25.sha-deac994
+ * @license Angular v10.0.0-next.8+28.sha-4e96cdc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.8+25.sha-deac994');
+const VERSION$1 = new Version('10.0.0-next.8+28.sha-4e96cdc');
 
 /**
  * @license
