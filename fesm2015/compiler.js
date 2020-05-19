a50cb9386293c9851d14eb268a04e74bebcf12fa
Kara Erickson <karakara@google.com>
2020-05-19 22:26:20 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+28.sha-4e96cdc
+ * @license Angular v10.0.0-next.8+27.sha-772c5b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.8+28.sha-4e96cdc');
+const VERSION$1 = new Version('10.0.0-next.8+27.sha-772c5b8');
 
 /**
  * @license
