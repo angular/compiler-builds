70007accf30a0f58b25a4e36e124a30760f8b2e3
Kara Erickson <karakara@google.com>
2020-05-20 21:35:29 +0000

    267f844811 docs: update docs for microbenchmarks (#37140)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+20.sha-59e7203
+ * @license Angular v10.0.0-next.9+21.sha-267f844
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.9+20.sha-59e7203');
+const VERSION$1 = new Version('10.0.0-next.9+21.sha-267f844');
 
 /**
  * @license
