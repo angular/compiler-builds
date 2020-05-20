2923849eed61d425e286d2e422aa8a6741f9020f
Kara Erickson <karakara@google.com>
2020-05-20 23:17:50 +0000

    b797913d10 release: cut the v9.1.9 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+19.sha-1465372
+ * @license Angular v9.1.9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19106,7 +19106,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.8+19.sha-1465372');
+const VERSION$1 = new Version('9.1.9');
 
 /**
  * @license
