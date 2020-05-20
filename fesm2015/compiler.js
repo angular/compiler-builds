8a9c95da7eed508a5719f4e54ba6825ee8e33139
Kara Erickson <karakara@google.com>
2020-05-20 23:06:12 +0000

    6ca3fb19fd docs: release notes for the v${releaseVersion} release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+21.sha-267f844
+ * @license Angular v10.0.0-next.9+22.sha-6ca3fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-next.9+21.sha-267f844');
+const VERSION$1 = new Version('10.0.0-next.9+22.sha-6ca3fb1');
 
 /**
  * @license
