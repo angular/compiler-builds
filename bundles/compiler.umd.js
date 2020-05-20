16fdcbea58bf0cf6c8938410749720da0c0c4f30
Kara Erickson <karakara@google.com>
2020-05-20 20:58:54 +0000

    3eee53603c docs: update docs for microbenchmarks (#37140)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+7.sha-19313f7
+ * @license Angular v9.1.8+12.sha-3eee536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20395,7 +20395,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.8+7.sha-19313f7');
+    var VERSION$1 = new Version('9.1.8+12.sha-3eee536');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+7.sha-19313f7
+ * @license Angular v9.1.8+12.sha-3eee536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19106,7 +19106,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.8+7.sha-19313f7');
+const VERSION$1 = new Version('9.1.8+12.sha-3eee536');
 
 /**
  * @license
