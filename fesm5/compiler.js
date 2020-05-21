088801395bdd46301a44f7708d18eefe1cf7a1f5
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 22:02:53 +0000

    0b96908225 feat(dev-infra): migrate release tool to use new logging system (#37232)

--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+3.sha-711e4d4
+ * @license Angular v9.1.9+13.sha-0b96908
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20200,7 +20200,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.9+3.sha-711e4d4');
+var VERSION$1 = new Version('9.1.9+13.sha-0b96908');
 
 /**
  * @license
