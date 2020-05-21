4f53c0252cf28fe659e76137bad51d9243f55c47
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:40:58 +0000

    87b1aeac0f build(docs-infra): upgrade cli command docs sources to 89dbc2caa (#37243)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
+ * @license Angular v10.0.0-rc.0+4.sha-87b1aea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+2.sha-cdb5d07');
+    var VERSION$1 = new Version('10.0.0-rc.0+4.sha-87b1aea');
 
     /**
      * @license
