7b7914ace3137aeefc63b7b7ecbe22478245cd5e
Kara Erickson <karakara@google.com>
2020-05-21 16:43:17 +0000

    a1001f2ea0 fix(core): disable tsickle pass when producing APF packages (#37221)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+22.sha-6ca3fb1
+ * @license Angular v10.0.0-next.9+23.sha-a1001f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-next.9+22.sha-6ca3fb1');
+    var VERSION$1 = new Version('10.0.0-next.9+23.sha-a1001f2');
 
     /**
      * @license
