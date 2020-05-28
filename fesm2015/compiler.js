01e64f629f067eabe082780d93042bfe537217f0
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 23:04:24 +0000

    270da1f69f build(docs-infra): upgrade cli command docs sources to 14af4e07c (#37310)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+9.sha-3642707
+ * @license Angular v10.0.0-rc.0+11.sha-270da1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+9.sha-3642707');
+const VERSION$1 = new Version('10.0.0-rc.0+11.sha-270da1f');
 
 /**
  * @license
