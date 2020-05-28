f393457d7f2554955065bd807c27e44d1c34b237
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 23:09:24 +0000

    df082307b9 docs: fix typo in committer.md (#37171)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+21.sha-f001aef
+ * @license Angular v10.0.0-rc.0+23.sha-df08230
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+21.sha-f001aef');
+const VERSION$1 = new Version('10.0.0-rc.0+23.sha-df08230');
 
 /**
  * @license
