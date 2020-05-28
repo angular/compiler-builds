33ddeffdc1c9d1cfc514facd394ad5d1fbf12c62
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:35:23 +0000

    f001aef2dc build: update requiredBaseCommit for patch branch merges (#37316)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+22.sha-98d8200
+ * @license Angular v10.0.0-rc.0+21.sha-f001aef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+22.sha-98d8200');
+const VERSION$1 = new Version('10.0.0-rc.0+21.sha-f001aef');
 
 /**
  * @license
