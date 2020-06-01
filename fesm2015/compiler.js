1d919b399ba5dbc85ba25d188e7195c8c1b1485e
Matias Niemelä <matias@yearofmoo.com>
2020-06-01 21:45:57 +0000

    c3651cec0b docs(core): fix typo in decorators.ts relating to the use of Object.defineProperty. (#37369)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+38.sha-3b52f70
+ * @license Angular v10.0.0-rc.0+39.sha-c3651ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+38.sha-3b52f70');
+const VERSION$1 = new Version('10.0.0-rc.0+39.sha-c3651ce');
 
 /**
  * @license
