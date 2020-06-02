5efa2600286e4bce56e69c901f3e0cffda7c3b8b
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:48:35 +0000

    561c0f81a0 perf(ngcc): allow immediately reporting a stale lock file (#37250)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+44.sha-1b55da1
+ * @license Angular v10.0.0-rc.0+46.sha-561c0f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+44.sha-1b55da1');
+const VERSION$1 = new Version('10.0.0-rc.0+46.sha-561c0f8');
 
 /**
  * @license
