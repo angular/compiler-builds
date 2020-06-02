91f96a7916b8ecca5580101530689038e855cd8d
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:50:49 +0000

    561c0f81a0 perf(ngcc): allow immediately reporting a stale lock file (#37250)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+44.sha-1b55da1
+ * @license Angular v10.0.0-rc.0+46.sha-561c0f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+44.sha-1b55da1');
+    var VERSION$1 = new Version('10.0.0-rc.0+46.sha-561c0f8');
 
     /**
      * @license
