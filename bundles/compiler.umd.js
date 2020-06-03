915ce34e6f3e19944faab48e6774b4699b5cc826
atscott <atscott@google.com>
2020-06-03 20:47:26 +0000

    965a688c97 fix(compiler-cli): use ModuleWithProviders type if static eval fails (#37126)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
+ * @license Angular v10.0.0-rc.0+56.sha-965a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
+    var VERSION$1 = new Version('10.0.0-rc.0+56.sha-965a688');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
+ * @license Angular v10.0.0-rc.0+56.sha-965a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
+const VERSION$1 = new Version('10.0.0-rc.0+56.sha-965a688');
 
 /**
  * @license
