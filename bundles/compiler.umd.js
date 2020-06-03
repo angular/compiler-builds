0cd7a523b6888eddbf7959f90bc971c5a3b212c4
atscott <atscott@google.com>
2020-06-03 20:51:37 +0000

    e648a0c4ca refactor(compiler-cli): extract NgCompilerAdapter interface (#37118)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+56.sha-965a688
+ * @license Angular v10.0.0-rc.0+57.sha-e648a0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+56.sha-965a688');
+    var VERSION$1 = new Version('10.0.0-rc.0+57.sha-e648a0c');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+56.sha-965a688
+ * @license Angular v10.0.0-rc.0+57.sha-e648a0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+56.sha-965a688');
+const VERSION$1 = new Version('10.0.0-rc.0+57.sha-e648a0c');
 
 /**
  * @license
