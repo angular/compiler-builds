5123b25cfc8aca20bb47253aade4eb23bd700165
atscott <atscott@google.com>
2020-06-03 19:46:32 +0000

    382aa50c54 docs: refactor template-driven forms doc as a tutorial (#36732)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+48.sha-044a199
+ * @license Angular v10.0.0-rc.0+49.sha-382aa50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+48.sha-044a199');
+const VERSION$1 = new Version('10.0.0-rc.0+49.sha-382aa50');
 
 /**
  * @license
