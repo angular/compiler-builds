97cecfe12b186f819010f673f996b3ca76071f5c
atscott <atscott@google.com>
2020-06-03 20:40:16 +0000

    ab3f4c9fe1 fix(core): infinite loop if injectable using inheritance has a custom decorator (#37022)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
+ * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+54.sha-40f3bb5');
+const VERSION$1 = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
 
 /**
  * @license
