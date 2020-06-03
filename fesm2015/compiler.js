be5de9e716494dfe5ee0250ef07c3fbba87e1643
atscott <atscott@google.com>
2020-06-03 20:40:13 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
+ * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+50.sha-06a8ad2');
+const VERSION$1 = new Version('10.0.0-rc.0+54.sha-40f3bb5');
 
 /**
  * @license
