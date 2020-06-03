70f7c6e52c34216f4416fffa61789e71343ae7c3
atscott <atscott@google.com>
2020-06-03 19:55:33 +0000

    c7c0c1f626 refactor(forms): use a type guard to get rid of casts (#32541)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
+ * @license Angular v10.0.0-rc.2+14.sha-c7c0c1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+12.sha-2d86dbb');
+    var VERSION$1 = new Version('10.0.0-rc.2+14.sha-c7c0c1f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
+ * @license Angular v10.0.0-rc.2+14.sha-c7c0c1f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+12.sha-2d86dbb');
+const VERSION$1 = new Version('10.0.0-rc.2+14.sha-c7c0c1f');
 
 /**
  * @license
