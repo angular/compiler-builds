beec49389074e75c20ef757f1ef50a7f17cbcabd
atscott <atscott@google.com>
2020-06-03 22:49:44 +0000

    e35269dd87 docs: update file header to be correct (#37425)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+21.sha-60a03b7
+ * @license Angular v10.0.0-rc.2+22.sha-e35269d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+21.sha-60a03b7');
+    var VERSION$1 = new Version('10.0.0-rc.2+22.sha-e35269d');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+21.sha-60a03b7
+ * @license Angular v10.0.0-rc.2+22.sha-e35269d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+21.sha-60a03b7');
+const VERSION$1 = new Version('10.0.0-rc.2+22.sha-e35269d');
 
 /**
  * @license
