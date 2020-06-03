e50d5aee94fd67cbb92088aef21f79b87c848e57
atscott <atscott@google.com>
2020-06-03 18:41:38 +0000

    2d86dbb090 docs: update aio in support for #BlackLivesMatter (#37409)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+11.sha-91767ff
+ * @license Angular v10.0.0-rc.2+12.sha-2d86dbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+11.sha-91767ff');
+    var VERSION$1 = new Version('10.0.0-rc.2+12.sha-2d86dbb');
 
     /**
      * @license
