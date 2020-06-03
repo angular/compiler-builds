3c9d510b2d4ae3962d20974e58cabab336636d69
atscott <atscott@google.com>
2020-06-03 19:56:50 +0000

    06a8ad28a7 refactor(forms): use a type guard to get rid of casts (#32541)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+49.sha-382aa50
+ * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+49.sha-382aa50');
+const VERSION$1 = new Version('10.0.0-rc.0+50.sha-06a8ad2');
 
 /**
  * @license
