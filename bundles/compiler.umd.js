1673605af22c42a754b60a58ca77379375b62e69
atscott <atscott@google.com>
2020-06-03 23:12:47 +0000

    34827559b9 docs: update the stackblitz in the GitHub Issue template (#37219)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+58.sha-65c3888
+ * @license Angular v10.0.0-rc.0+59.sha-3482755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+58.sha-65c3888');
+    var VERSION$1 = new Version('10.0.0-rc.0+59.sha-3482755');
 
     /**
      * @license
