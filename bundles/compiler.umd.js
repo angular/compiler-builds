581f035d6986ea4dbb3d4b9347f53c8d3991346f
atscott <atscott@google.com>
2020-06-04 20:02:55 +0000

    d713e33cc4 style(dev-infra): correct tslint failures in dev-infra directory (#37233)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
+ * @license Angular v10.0.0-rc.2+48.sha-d713e33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+45.sha-9ec25ea');
+    var VERSION$1 = new Version('10.0.0-rc.2+48.sha-d713e33');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
+ * @license Angular v10.0.0-rc.2+48.sha-d713e33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+45.sha-9ec25ea');
+const VERSION$1 = new Version('10.0.0-rc.2+48.sha-d713e33');
 
 /**
  * @license
