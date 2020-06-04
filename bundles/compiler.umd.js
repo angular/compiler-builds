d95efc2e4bb08bb0c6f71eade4bcab746d37753e
atscott <atscott@google.com>
2020-06-04 23:22:32 +0000

    eb6ba9ac80 docs: fix various typos (#37443)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+73.sha-4e7a454
+ * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+73.sha-4e7a454');
+    var VERSION$1 = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+73.sha-4e7a454
+ * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+73.sha-4e7a454');
+const VERSION$1 = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
 
 /**
  * @license
