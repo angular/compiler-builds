2a9c8b5e2bb8db2b9cb2ae64eaffd0205acde957
atscott <atscott@google.com>
2020-06-04 16:36:58 +0000

    6e7bd939f6 perf(ngcc): cache parsed tsconfig between runs (#37417)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+60.sha-c4f4675
+ * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+60.sha-c4f4675');
+const VERSION$1 = new Version('10.0.0-rc.0+62.sha-6e7bd93');
 
 /**
  * @license
