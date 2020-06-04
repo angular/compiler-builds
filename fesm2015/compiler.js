4f2a22caf4fefa889b5746e4429044ac6cdfee9e
atscott <atscott@google.com>
2020-06-04 17:02:01 +0000

    c025357fb8 feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+67.sha-57411c8
+ * @license Angular v10.0.0-rc.0+68.sha-c025357
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+67.sha-57411c8');
+const VERSION$1 = new Version('10.0.0-rc.0+68.sha-c025357');
 
 /**
  * @license
