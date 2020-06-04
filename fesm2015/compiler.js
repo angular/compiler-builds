f644e1a9623cf17d22e9a7a75b484aed2de9ea74
atscott <atscott@google.com>
2020-06-04 16:42:45 +0000

    c61decf7e5 docs: add blank line before header (#37391)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+61.sha-2021ad1
+ * @license Angular v10.0.0-rc.0+63.sha-c61decf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+61.sha-2021ad1');
+const VERSION$1 = new Version('10.0.0-rc.0+63.sha-c61decf');
 
 /**
  * @license
