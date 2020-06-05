a2b4a0047a00e334a747bd9480f29999cc361d79
George Kalpakas <kalpakas.g@gmail.com>
2020-06-05 22:28:14 +0000

    f0f319b1d6 ci: temporarily disable Android 10 browser unit tests on Saucelabs (#37399)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+14.sha-6855396
+ * @license Angular v9.1.9+17.sha-f0f319b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19106,7 +19106,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.9+14.sha-6855396');
+const VERSION$1 = new Version('9.1.9+17.sha-f0f319b');
 
 /**
  * @license
