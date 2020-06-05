5c2fd953417d13688f99056d790e9437273568fa
atscott <atscott@google.com>
2020-06-05 16:41:55 +0000

    2cb3b66640 fix(ngcc): find decorated constructor params on IIFE wrapped classes (#37436)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+50.sha-fff424a
+ * @license Angular v10.0.0-rc.2+53.sha-2cb3b66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+50.sha-fff424a');
+    var VERSION$1 = new Version('10.0.0-rc.2+53.sha-2cb3b66');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+50.sha-fff424a
+ * @license Angular v10.0.0-rc.2+53.sha-2cb3b66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+50.sha-fff424a');
+const VERSION$1 = new Version('10.0.0-rc.2+53.sha-2cb3b66');
 
 /**
  * @license
