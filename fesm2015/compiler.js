3140645972c494b9a47b50ad0e10f62fe5492ba3
atscott <atscott@google.com>
2020-06-05 18:28:57 +0000

    17996bf719 fix(dev-infra): properly determine oauth scopes for git client token (#37462)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+79.sha-85b6c94
+ * @license Angular v10.0.0-rc.0+82.sha-17996bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+79.sha-85b6c94');
+const VERSION$1 = new Version('10.0.0-rc.0+82.sha-17996bf');
 
 /**
  * @license
