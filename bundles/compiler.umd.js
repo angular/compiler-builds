8828b71388e247dde0fd96d9134fa3279c79e995
atscott <atscott@google.com>
2020-06-05 00:07:30 +0000

    3569fdf451 fix(common): prevent duplicate URL change notifications (#37404)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
+ * @license Angular v10.0.0-rc.0+75.sha-3569fdf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
+    var VERSION$1 = new Version('10.0.0-rc.0+75.sha-3569fdf');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
+ * @license Angular v10.0.0-rc.0+75.sha-3569fdf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
+const VERSION$1 = new Version('10.0.0-rc.0+75.sha-3569fdf');
 
 /**
  * @license
