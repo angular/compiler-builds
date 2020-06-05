1fdbc1d8439368ece656f0331cbebb732fb91810
atscott <atscott@google.com>
2020-06-05 16:41:44 +0000

    d4c0962c7b refactor(dev-infra): use the `exec()` helper from `utils/shelljs` whenever possible (#37444)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+76.sha-e31208b
+ * @license Angular v10.0.0-rc.0+77.sha-d4c0962
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+76.sha-e31208b');
+    var VERSION$1 = new Version('10.0.0-rc.0+77.sha-d4c0962');
 
     /**
      * @license
