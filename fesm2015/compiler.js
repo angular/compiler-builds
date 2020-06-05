c28efee907b7f3c910080d743c8ce07ce38d68a7
atscott <atscott@google.com>
2020-06-05 17:06:38 +0000

    53e1fb3554 refactor(dev-infra): move GitClient to common util (#37318)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+53.sha-2cb3b66
+ * @license Angular v10.0.0-rc.2+54.sha-53e1fb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+53.sha-2cb3b66');
+const VERSION$1 = new Version('10.0.0-rc.2+54.sha-53e1fb3');
 
 /**
  * @license
