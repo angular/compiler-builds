8c04a8060d3d94a05a68f8600d3cb25813c676a4
atscott <atscott@google.com>
2020-06-06 02:46:43 +0000

    aaa20093b2 ci: special case tooling-cli-shared-api review group (#37467)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+82.sha-17996bf
+ * @license Angular v10.0.0-rc.0+84.sha-aaa2009
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+82.sha-17996bf');
+    var VERSION$1 = new Version('10.0.0-rc.0+84.sha-aaa2009');
 
     /**
      * @license
