901ffe311181127fcf7337f09828607427bb2ff1
atscott <atscott@google.com>
2020-06-06 02:48:13 +0000

    75294e7dad ci: special case tooling-cli-shared-api review group (#37467)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+57.sha-2349143
+ * @license Angular v10.0.0-rc.2+59.sha-75294e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+57.sha-2349143');
+    var VERSION$1 = new Version('10.0.0-rc.2+59.sha-75294e7');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+57.sha-2349143
+ * @license Angular v10.0.0-rc.2+59.sha-75294e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.2+57.sha-2349143');
+const VERSION$1 = new Version('10.0.0-rc.2+59.sha-75294e7');
 
 /**
  * @license
