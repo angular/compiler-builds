55e3b7b9eb8378e5dc72d2d123f219d3bc41ad88
atscott <atscott@google.com>
2020-06-08 22:01:46 +0000

    1c22dff714 docs(docs-infra): fix small typo (#37258)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+69.sha-8d1d6e8
+ * @license Angular v10.0.0-rc.2+70.sha-1c22dff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.2+69.sha-8d1d6e8');
+    var VERSION$1 = new Version('10.0.0-rc.2+70.sha-1c22dff');
 
     /**
      * @license
