5b37d43ff8c319ed38122a1bd2336943c555495a
atscott <atscott@google.com>
2020-06-08 16:49:58 +0000

    5e64c2b1df style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+86.sha-d887ba8
+ * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+86.sha-d887ba8');
+const VERSION$1 = new Version('10.0.0-rc.0+89.sha-5e64c2b');
 
 /**
  * @license
