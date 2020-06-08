280b8ca48687732d0449c31fc00c149e053fc47c
atscott <atscott@google.com>
2020-06-08 17:30:38 +0000

    ed3c549063 ci: remove IgorMinar from reviewers list for pullapprove fallback group (#36456)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+90.sha-3a11617
+ * @license Angular v10.0.0-rc.0+91.sha-ed3c549
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+90.sha-3a11617');
+    var VERSION$1 = new Version('10.0.0-rc.0+91.sha-ed3c549');
 
     /**
      * @license
