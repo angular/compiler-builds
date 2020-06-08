832702717f837403c9546e1853217f9f39a06af7
atscott <atscott@google.com>
2020-06-08 17:48:46 +0000

    650974e1b3 test(language-service): Remove all markers from test project (#37475)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+91.sha-ed3c549
+ * @license Angular v10.0.0-rc.0+92.sha-650974e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+91.sha-ed3c549');
+    var VERSION$1 = new Version('10.0.0-rc.0+92.sha-650974e');
 
     /**
      * @license
