4e1b3947d81c959aa180fa111b472d9fa01992fa
Pete Bacon Darwin <pete@bacondarwin.com>
2020-06-08 18:10:20 +0000

    1aae94a421 perf(ngcc): cache parsed tsconfig between runs

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+17.sha-f0f319b
+ * @license Angular v9.1.9+18.sha-1aae94a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19106,7 +19106,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('9.1.9+17.sha-f0f319b');
+const VERSION$1 = new Version('9.1.9+18.sha-1aae94a');
 
 /**
  * @license
--- fesm5/compiler.js
+++ fesm5/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+17.sha-f0f319b
+ * @license Angular v9.1.9+18.sha-1aae94a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20200,7 +20200,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION$1 = new Version('9.1.9+17.sha-f0f319b');
+var VERSION$1 = new Version('9.1.9+18.sha-1aae94a');
 
 /**
  * @license
