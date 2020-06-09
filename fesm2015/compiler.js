da72c515d6eec9514d96586b90c5c8d33b36395c
atscott <atscott@google.com>
2020-06-09 22:10:46 +0000

    6114cd2bd4 perf(core): avoid pulling in jit-specific code in aot bundles (#37372) (#37514)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+12.sha-d494f7b
+ * @license Angular v10.0.0-rc.3+13.sha-6114cd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.3+12.sha-d494f7b');
+const VERSION$1 = new Version('10.0.0-rc.3+13.sha-6114cd2');
 
 /**
  * @license
