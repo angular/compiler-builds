c09d51b0cd4695e528ea76a7fd5616d0743f644b
atscott <atscott@google.com>
2020-06-09 00:33:43 +0000

    8d817daf78 fix(router): Fix relative link generation from empty path components (#37446)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+97.sha-87fdd23
+ * @license Angular v10.0.0-rc.0+99.sha-8d817da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+97.sha-87fdd23');
+    var VERSION$1 = new Version('10.0.0-rc.0+99.sha-8d817da');
 
     /**
      * @license
