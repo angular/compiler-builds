c30bae1e6399ac565ab0e1b12d2485f40b2d7e46
atscott <atscott@google.com>
2020-06-09 15:49:16 +0000

    7301e70ddd fix(platform-server): correctly handle absolute relative URLs (#37341)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+101.sha-55979fe
+ * @license Angular v10.0.0-rc.0+104.sha-7301e70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+101.sha-55979fe');
+    var VERSION$1 = new Version('10.0.0-rc.0+104.sha-7301e70');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+101.sha-55979fe
+ * @license Angular v10.0.0-rc.0+104.sha-7301e70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+101.sha-55979fe');
+const VERSION$1 = new Version('10.0.0-rc.0+104.sha-7301e70');
 
 /**
  * @license
