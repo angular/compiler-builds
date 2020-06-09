290ca1ce5aec04cd6385de68eed830c2e1d8e46f
atscott <atscott@google.com>
2020-06-09 20:54:23 +0000

    d494f7bd5e docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
+ * @license Angular v10.0.0-rc.3+12.sha-d494f7b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.3+11.sha-ec6a7ab');
+    var VERSION$1 = new Version('10.0.0-rc.3+12.sha-d494f7b');
 
     /**
      * @license
