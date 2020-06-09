cf2868228267bfa01d4fe84213c0ee046e23ff64
atscott <atscott@google.com>
2020-06-09 18:42:49 +0000

    ec6a7ab721 docs: wrong links in lifecycle hooks api documentaion (#36557)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
+ * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.3+10.sha-ad6d2b4');
+    var VERSION$1 = new Version('10.0.0-rc.3+11.sha-ec6a7ab');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
+ * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.3+10.sha-ad6d2b4');
+const VERSION$1 = new Version('10.0.0-rc.3+11.sha-ec6a7ab');
 
 /**
  * @license
