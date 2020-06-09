480185c05472e3c58a4d7770a078c9b60cd83033
atscott <atscott@google.com>
2020-06-09 00:44:59 +0000

    55979fe0ae feat(language-service): TS references from template items (#37437)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+100.sha-6280cf9
+ * @license Angular v10.0.0-rc.0+101.sha-55979fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+100.sha-6280cf9');
+const VERSION$1 = new Version('10.0.0-rc.0+101.sha-55979fe');
 
 /**
  * @license
