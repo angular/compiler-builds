f6acb5eb7f253b35aa741269ac278333440d140d
atscott <atscott@google.com>
2020-06-09 15:50:08 +0000

    7edb026619 fix(common): prevent duplicate URL change notifications (#37459)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+107.sha-a331370
+ * @license Angular v10.0.0-rc.0+103.sha-7edb026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+107.sha-a331370');
+    var VERSION$1 = new Version('10.0.0-rc.0+103.sha-7edb026');
 
     /**
      * @license
