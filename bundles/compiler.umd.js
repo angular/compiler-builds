ec038a417d992a8d0c9f25c35138c7e9afa1c0b4
atscott <atscott@google.com>
2020-06-10 20:21:28 +0000

    20346ff192 build: remove ngcontainer Docker (#36421)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+123.sha-bd9c1e6
+ * @license Angular v10.0.0-rc.0+124.sha-20346ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+123.sha-bd9c1e6');
+    var VERSION$1 = new Version('10.0.0-rc.0+124.sha-20346ff');
 
     /**
      * @license
