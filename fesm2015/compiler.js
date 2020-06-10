732c7c45fc02bfdf300f8944b9c3d744bda7e389
atscott <atscott@google.com>
2020-06-10 19:11:18 +0000

    d76a872efb docs: add Mike Huang as a GDE (#36472)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+117.sha-021dd33
+ * @license Angular v10.0.0-rc.0+122.sha-d76a872
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+117.sha-021dd33');
+const VERSION$1 = new Version('10.0.0-rc.0+122.sha-d76a872');
 
 /**
  * @license
