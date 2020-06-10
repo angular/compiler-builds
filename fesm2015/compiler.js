410a9a66763acf3def9b79c2c55360872bd1deb1
atscott <atscott01@gmail.com>
2020-06-10 17:51:19 +0000

    7b005bb9cf Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+114.sha-401ef71
+ * @license Angular v10.0.0-rc.0+115.sha-7b005bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+114.sha-401ef71');
+const VERSION$1 = new Version('10.0.0-rc.0+115.sha-7b005bb');
 
 /**
  * @license
