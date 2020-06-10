14e39fcbe5c6b1e91ae5473f1b746d2625ec3686
atscott <atscott@google.com>
2020-06-10 19:24:13 +0000

    231095fe8a build: add commit message scope for migration changes (#36390)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+16.sha-742f3d6
+ * @license Angular v10.0.0-rc.4+5.sha-231095f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.3+16.sha-742f3d6');
+    var VERSION$1 = new Version('10.0.0-rc.4+5.sha-231095f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+16.sha-742f3d6
+ * @license Angular v10.0.0-rc.4+5.sha-231095f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.3+16.sha-742f3d6');
+const VERSION$1 = new Version('10.0.0-rc.4+5.sha-231095f');
 
 /**
  * @license
