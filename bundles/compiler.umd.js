a28ec7db9cc5e252b9423c8f07083fe1de51507d
Misko Hevery <misko@hevery.com>
2020-06-11 18:40:36 +0000

    8c682c52b1 fix(ngcc): use annotateForClosureCompiler option (#36652)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+129.sha-e5b09cc
+ * @license Angular v10.0.0-rc.0+130.sha-8c682c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+129.sha-e5b09cc');
+    var VERSION$1 = new Version('10.0.0-rc.0+130.sha-8c682c5');
 
     /**
      * @license
