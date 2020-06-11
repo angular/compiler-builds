efece4af852c6f0aff0604a210809d1c5c35dc10
Misko Hevery <misko@hevery.com>
2020-06-11 20:53:22 +0000

    97dc85ba5e feat(core): support injection token as predicate in queries (#37506)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+134.sha-a937889
+ * @license Angular v10.0.0-rc.0+135.sha-97dc85b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+134.sha-a937889');
+    var VERSION$1 = new Version('10.0.0-rc.0+135.sha-97dc85b');
 
     /**
      * @license
