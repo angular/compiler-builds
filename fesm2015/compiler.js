d2b0e8a1bcdfc0073ea61ffa561c45fab2d934f8
Misko Hevery <misko@hevery.com>
2020-06-11 18:33:26 +0000

    b199ef6bfc feat(zone.js): move all zone optional bundles to plugins folders (#36540)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+126.sha-1fce6d6
+ * @license Angular v10.0.0-rc.0+128.sha-b199ef6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19105,7 +19105,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+126.sha-1fce6d6');
+const VERSION$1 = new Version('10.0.0-rc.0+128.sha-b199ef6');
 
 /**
  * @license
