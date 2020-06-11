af8d674769785e7b81c7cb6b546876fb047267ff
Misko Hevery <misko@hevery.com>
2020-06-11 18:25:42 +0000

    39f973430a docs: remove out of date GDEs (#36467)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+124.sha-20346ff
+ * @license Angular v10.0.0-rc.0+125.sha-39f9734
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+124.sha-20346ff');
+    var VERSION$1 = new Version('10.0.0-rc.0+125.sha-39f9734');
 
     /**
      * @license
