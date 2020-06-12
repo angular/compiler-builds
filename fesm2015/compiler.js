db45be52acc8efb785263fca7773aad1c9ff5e74
Misko Hevery <misko@hevery.com>
2020-06-12 22:38:00 +0000

    1091ddbb8e build(bazel): check for renderer flag in `ng_module()` (#37529)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+168.sha-62e4acc
+ * @license Angular v10.0.0-rc.0+167.sha-1091ddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+168.sha-62e4acc');
+const VERSION$1 = new Version('10.0.0-rc.0+167.sha-1091ddb');
 
 /**
  * @license
