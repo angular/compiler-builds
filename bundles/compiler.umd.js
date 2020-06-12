c95f9c0c48faa28f9d2214510f2fe1eee4678978
crisbeto <crisbeto@abv.bg>
2020-06-12 23:03:43 +0000

    6c1ab479ee fix(core): infinite loop if injectable using inheritance has a custom decorator

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11
+ * @license Angular v9.1.11+1.sha-6c1ab47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20395,7 +20395,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('9.1.11');
+    var VERSION$1 = new Version('9.1.11+1.sha-6c1ab47');
 
     /**
      * @license
