f178af2fd6266dd11cb5ecce9f40187f8836a423
Misko Hevery <misko@hevery.com>
2020-06-12 16:13:39 +0000

    8dcf618411 docs: add redirects for cli generated config files (#37533)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+158.sha-aba33de
+ * @license Angular v10.0.0-rc.0+161.sha-8dcf618
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+158.sha-aba33de');
+    var VERSION$1 = new Version('10.0.0-rc.0+161.sha-8dcf618');
 
     /**
      * @license
