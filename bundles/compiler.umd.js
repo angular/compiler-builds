fe94122c71cd01027aa30642e557df21727e46b3
Misko Hevery <misko@hevery.com>
2020-06-12 22:35:17 +0000

    1091ddbb8e build(bazel): check for renderer flag in `ng_module()` (#37529)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+168.sha-62e4acc
+ * @license Angular v10.0.0-rc.0+167.sha-1091ddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+168.sha-62e4acc');
+    var VERSION$1 = new Version('10.0.0-rc.0+167.sha-1091ddb');
 
     /**
      * @license
