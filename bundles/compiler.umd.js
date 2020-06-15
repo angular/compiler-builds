32cd00b0b646c525a6b59e1fde28a8c9c3884c06
Misko Hevery <misko@hevery.com>
2020-06-15 17:12:24 +0000

    adc9d5cdcb docs: describe how to configure CommonJS modules (#37331)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+173.sha-d330cd8
+ * @license Angular v10.0.0-rc.0+174.sha-adc9d5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+173.sha-d330cd8');
+    var VERSION$1 = new Version('10.0.0-rc.0+174.sha-adc9d5c');
 
     /**
      * @license
