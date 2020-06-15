c671364b065c2e7f10e1d46288af464af8c21615
Misko Hevery <misko@hevery.com>
2020-06-15 17:44:16 +0000

    cc49a91de7 Revert "feat(bazel): expose explicit mapping from closure to devmode files (#36262)"

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+178.sha-300c2fe
+ * @license Angular v10.0.0-rc.0+179.sha-cc49a91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+178.sha-300c2fe');
+const VERSION$1 = new Version('10.0.0-rc.0+179.sha-cc49a91');
 
 /**
  * @license
