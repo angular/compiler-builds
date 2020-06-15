1f340aa004695ca97a585f5751d6c9864c56b1ba
Misko Hevery <misko@hevery.com>
2020-06-15 21:53:32 +0000

    87a679b210 build: import in-memory-web-api project (#37182)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+185.sha-2a145f2
+ * @license Angular v10.0.0-rc.0+182.sha-87a679b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+185.sha-2a145f2');
+    var VERSION$1 = new Version('10.0.0-rc.0+182.sha-87a679b');
 
     /**
      * @license
