15f7eee6de53b8b0690dc0c308d9175a1d66d8d4
Misko Hevery <misko@hevery.com>
2020-06-15 21:47:57 +0000

    a7359d494a docs: release notes for the v10.0.0-rc.6 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+180.sha-66e6b93
+ * @license Angular v10.0.0-rc.0+181.sha-a7359d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+180.sha-66e6b93');
+    var VERSION$1 = new Version('10.0.0-rc.0+181.sha-a7359d4');
 
     /**
      * @license
