93cde9d345d50d5c33f95c2068ec5ef8b0486fa8
Misko Hevery <misko@hevery.com>
2020-06-15 21:45:20 +0000

    a7359d494a docs: release notes for the v10.0.0-rc.6 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+180.sha-66e6b93
+ * @license Angular v10.0.0-rc.0+181.sha-a7359d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+180.sha-66e6b93');
+const VERSION$1 = new Version('10.0.0-rc.0+181.sha-a7359d4');
 
 /**
  * @license
