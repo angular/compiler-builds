f2a1d4773fdf82149f0ac63a94a885a1e593d90c
Misko Hevery <misko@hevery.com>
2020-06-15 17:14:53 +0000

    a7faa6bb65 docs(service-worker): minor fixes/improvements in the SW Communication guide (#37555)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+174.sha-adc9d5c
+ * @license Angular v10.0.0-rc.0+177.sha-a7faa6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+174.sha-adc9d5c');
+const VERSION$1 = new Version('10.0.0-rc.0+177.sha-a7faa6b');
 
 /**
  * @license
