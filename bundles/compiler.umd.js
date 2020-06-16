d5b89d88a7719ea27a87cbe25f1590d7be9b9805
Misko Hevery <misko@hevery.com>
2020-06-16 19:24:44 +0000

    26b0f3dc96 docs: add side effect `package.json` in app structure (#37521)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+4.sha-97bb88f
+ * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.6+4.sha-97bb88f');
+    var VERSION$1 = new Version('10.0.0-rc.6+7.sha-26b0f3d');
 
     /**
      * @license
