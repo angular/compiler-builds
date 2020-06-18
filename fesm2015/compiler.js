9e9a5376a658de9773ae4fe76063ee08b9528558
Misko Hevery <misko@hevery.com>
2020-06-18 23:21:19 +0000

    71f008f906 fix(router): remove parenthesis for primary outlet segment after removing auxiliary outlet segment (#24656) (#37163)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+188.sha-7521834
+ * @license Angular v10.0.0-rc.0+193.sha-71f008f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+188.sha-7521834');
+const VERSION$1 = new Version('10.0.0-rc.0+193.sha-71f008f');
 
 /**
  * @license
