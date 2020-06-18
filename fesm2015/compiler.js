0e3fade75f0053d1a983c79018afe0abea1d74dc
Misko Hevery <misko@hevery.com>
2020-06-18 23:26:53 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+193.sha-71f008f
+ * @license Angular v10.0.0-rc.0+198.sha-bd7f440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+193.sha-71f008f');
+const VERSION$1 = new Version('10.0.0-rc.0+198.sha-bd7f440');
 
 /**
  * @license
