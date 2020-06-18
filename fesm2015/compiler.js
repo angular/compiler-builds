280f9e49d4f0008cfaed3be928110ebe1863bfd4
Misko Hevery <misko@hevery.com>
2020-06-18 23:29:22 +0000

    1d844b9bd8 docs(core): correct type for opts.read (#37626)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+195.sha-53e95233
+ * @license Angular v10.0.0-rc.0+196.sha-1d844b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+195.sha-53e95233');
+const VERSION$1 = new Version('10.0.0-rc.0+196.sha-1d844b9');
 
 /**
  * @license
