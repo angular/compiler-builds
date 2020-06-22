ef78276463e9bd0efb5443fef96bd520cd5427d6
Misko Hevery <misko@hevery.com>
2020-06-22 18:25:26 +0000

    c8c0063499 ci: disable saucelabs tests on Firefox ESR while investigating failures (#37647)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+208.sha-fd65958
+ * @license Angular v10.0.0-rc.0+212.sha-c8c0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+208.sha-fd65958');
+    var VERSION$1 = new Version('10.0.0-rc.0+212.sha-c8c0063');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+208.sha-fd65958
+ * @license Angular v10.0.0-rc.0+212.sha-c8c0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+208.sha-fd65958');
+const VERSION$1 = new Version('10.0.0-rc.0+212.sha-c8c0063');
 
 /**
  * @license
