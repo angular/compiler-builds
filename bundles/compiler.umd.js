1534921808ca92cb91c0f70deb230c4256546c8a
Misko Hevery <misko@hevery.com>
2020-06-23 23:57:23 +0000

    c95802f26f docs: add lightweight token page to library section of docs (#36144)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+223.sha-71c0363
+ * @license Angular v10.0.0-rc.0+224.sha-c95802f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+223.sha-71c0363');
+    var VERSION$1 = new Version('10.0.0-rc.0+224.sha-c95802f');
 
     /**
      * @license
