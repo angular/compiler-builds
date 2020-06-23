b4fdebbeaad6fae1c4e9a90742fe6e5c1d8a5284
Misko Hevery <misko@hevery.com>
2020-06-23 20:36:25 +0000

    71c0363bb5 docs: add tslib update migration docs (#37402)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+222.sha-e03adaf
+ * @license Angular v10.0.0-rc.0+223.sha-71c0363
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+222.sha-e03adaf');
+const VERSION$1 = new Version('10.0.0-rc.0+223.sha-71c0363');
 
 /**
  * @license
