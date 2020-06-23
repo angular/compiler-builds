9c0059bb0f90896fa1bd8540fc1ebe6e8b415a33
Misko Hevery <misko@hevery.com>
2020-06-23 20:34:15 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+17.sha-82c8b44
+ * @license Angular v10.0.0-rc.6+20.sha-36a00a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.6+17.sha-82c8b44');
+const VERSION$1 = new Version('10.0.0-rc.6+20.sha-36a00a2');
 
 /**
  * @license
