ead5be2cc47a2d72c944daf457ad69232d59e160
Misko Hevery <misko@hevery.com>
2020-06-23 20:09:53 +0000

    e03adaf9b0 docs: add module/target compiler option migration docs (#37429)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+221.sha-38dfbc7
+ * @license Angular v10.0.0-rc.0+222.sha-e03adaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+221.sha-38dfbc7');
+    var VERSION$1 = new Version('10.0.0-rc.0+222.sha-e03adaf');
 
     /**
      * @license
