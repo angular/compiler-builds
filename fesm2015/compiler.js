e378b828c6bd4d493d7ca80d0b1cd2b5dd45ddf9
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:01:55 +0000

    f3ec37ebbf release(benchpress): bump version to v0.2.1 and update `@angular/core` peer dependency (#37676)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+232.sha-29490cc
+ * @license Angular v10.0.0-rc.0+234.sha-f3ec37e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+232.sha-29490cc');
+const VERSION$1 = new Version('10.0.0-rc.0+234.sha-f3ec37e');
 
 /**
  * @license
