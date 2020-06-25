40f33af4bb1e9cf97e61f491c91d43894576307b
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:44:39 +0000

    a9a4edebe2 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+20.sha-64f2ffa
+ * @license Angular v10.0.0+21.sha-a9a4ede
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0+20.sha-64f2ffa');
+    var VERSION$1 = new Version('10.0.0+21.sha-a9a4ede');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+20.sha-64f2ffa
+ * @license Angular v10.0.0+21.sha-a9a4ede
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0+20.sha-64f2ffa');
+const VERSION$1 = new Version('10.0.0+21.sha-a9a4ede');
 
 /**
  * @license
