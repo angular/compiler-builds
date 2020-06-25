74085f7ef2503218c0a2dcce87b70ff0d8709253
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:37:02 +0000

    a5d5f67be7 fix(http): avoid abort a request when fetch operation is completed (#37367)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+4.sha-0d2cdf6
+ * @license Angular v10.0.0+15.sha-a5d5f67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0+4.sha-0d2cdf6');
+    var VERSION$1 = new Version('10.0.0+15.sha-a5d5f67');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+4.sha-0d2cdf6
+ * @license Angular v10.0.0+15.sha-a5d5f67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0+4.sha-0d2cdf6');
+const VERSION$1 = new Version('10.0.0+15.sha-a5d5f67');
 
 /**
  * @license
