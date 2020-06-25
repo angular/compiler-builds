6a30acc697297138a0379295318bed701043641a
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:17:20 +0000

    5218916a7e feat(dev-infra): add a way to pass assets down to a benchmark application (#37695)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+236.sha-4967997
+ * @license Angular v10.0.0-rc.0+238.sha-5218916
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+236.sha-4967997');
+    var VERSION$1 = new Version('10.0.0-rc.0+238.sha-5218916');
 
     /**
      * @license
