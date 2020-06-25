46e4f3bd50dae273d34ac94075a6671b7bddc395
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:23:28 +0000

    616543ded0 fix(router): add null support for RouterLink directive (#32616)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+240.sha-2170ea2
+ * @license Angular v10.0.0-rc.0+241.sha-616543d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+240.sha-2170ea2');
+    var VERSION$1 = new Version('10.0.0-rc.0+241.sha-616543d');
 
     /**
      * @license
