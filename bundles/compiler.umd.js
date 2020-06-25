e1a5ef12b9bc4d7df3fc46a51789dd7ed9a95ae2
Misko Hevery <misko@hevery.com>
2020-06-25 03:17:15 +0000

    f5bbfd7b8c docs(changelog): fix v10 announcement url (#37722)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+226.sha-41ce524
+ * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+226.sha-41ce524');
+    var VERSION$1 = new Version('10.0.0-rc.0+227.sha-f5bbfd7');
 
     /**
      * @license
