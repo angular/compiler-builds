df0c20d7e87a587a559cb18b92d515945d5cf9c4
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:00:34 +0000

    29490cce30 build: set up caretaker note label in merge tooling (#37675)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+230.sha-a0595f4
+ * @license Angular v10.0.0-rc.0+232.sha-29490cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+230.sha-a0595f4');
+const VERSION$1 = new Version('10.0.0-rc.0+232.sha-29490cc');
 
 /**
  * @license
