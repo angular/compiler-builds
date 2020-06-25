ea3faa0596a1cc74cbf6dc6355fd29e8da53981e
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:12:54 +0000

    2cbe53a9ba docs: Uses correct component in the `MessageService` (#37666)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+15.sha-a5d5f67
+ * @license Angular v10.0.0+17.sha-2cbe53a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0+15.sha-a5d5f67');
+    var VERSION$1 = new Version('10.0.0+17.sha-2cbe53a');
 
     /**
      * @license
