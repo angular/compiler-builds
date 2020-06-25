fe69caf4a4280ed57bd7500bf5b59b43f51e287d
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:01:49 +0000

    8924ec1474 test(docs-infra): add end to end tests for api reference (#37612)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+231.sha-bedc045
+ * @license Angular v10.0.0-rc.0+229.sha-8924ec1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+231.sha-bedc045');
+const VERSION$1 = new Version('10.0.0-rc.0+229.sha-8924ec1');
 
 /**
  * @license
