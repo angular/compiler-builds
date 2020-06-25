91a2ed6762ec4a2e446a16645ed11e3410c05a80
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:19:24 +0000

    2170ea270a fix(router): fix error when calling ParamMap.get function (#31599)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+239.sha-80d0067
+ * @license Angular v10.0.0-rc.0+240.sha-2170ea2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+239.sha-80d0067');
+    var VERSION$1 = new Version('10.0.0-rc.0+240.sha-2170ea2');
 
     /**
      * @license
