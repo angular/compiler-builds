fd36236eac739f23592c621089ea9066d36c13e5
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:36:49 +0000

    5103d908c8 perf(compiler-cli): fix regressions in incremental program reuse (#37641)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+255.sha-9318e23
+ * @license Angular v10.0.0-rc.0+256.sha-5103d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+255.sha-9318e23');
+    var VERSION$1 = new Version('10.0.0-rc.0+256.sha-5103d90');
 
     /**
      * @license
