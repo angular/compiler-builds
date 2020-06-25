3589c53faf625c96377bf098d6cc199e1b6d5f1d
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:29:43 +0000

    77b62a52c0 fix(forms): handle form groups/arrays own pending async validation (#22575)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+241.sha-616543d
+ * @license Angular v10.0.0-rc.0+242.sha-77b62a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20261,7 +20261,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+241.sha-616543d');
+    var VERSION$1 = new Version('10.0.0-rc.0+242.sha-77b62a5');
 
     /**
      * @license
