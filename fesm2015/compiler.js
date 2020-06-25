b6e079483e5aebe0a67b78582f8c9ff406669842
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:34:51 +0000

    e36d5b201a fix(forms): correct usage of `selectedOptions` (#37620)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+245.sha-8a74508
+ * @license Angular v10.0.0-rc.0+243.sha-e36d5b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+245.sha-8a74508');
+const VERSION$1 = new Version('10.0.0-rc.0+243.sha-e36d5b2');
 
 /**
  * @license
