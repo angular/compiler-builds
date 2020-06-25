0426e0701d00ac3d3047ee8061fb5889d7762995
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:44:46 +0000

    0e3e9cd382 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+262.sha-9118f49
+ * @license Angular v10.0.0-rc.0+263.sha-0e3e9cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+262.sha-9118f49');
+    var VERSION$1 = new Version('10.0.0-rc.0+263.sha-0e3e9cd');
 
     /**
      * @license
