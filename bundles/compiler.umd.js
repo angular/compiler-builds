2e9d1eb89b74c63deae80ec0e9b4a6c0b1409691
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:31:36 +0000

    e0eeb4afcb refactor(compiler-cli): Remove any cast for CompilerHost (#37079)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+277.sha-83fe963
+ * @license Angular v10.0.0-rc.0+276.sha-e0eeb4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+277.sha-83fe963');
+    var VERSION$1 = new Version('10.0.0-rc.0+276.sha-e0eeb4a');
 
     /**
      * @license
