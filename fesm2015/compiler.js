6d6da7ed8a02a6fd5e6809edfa61da3f819dabd5
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:37:01 +0000

    acf3cff9ee ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+276.sha-e0eeb4a
+ * @license Angular v10.0.0-rc.0+281.sha-acf3cff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+276.sha-e0eeb4a');
+const VERSION$1 = new Version('10.0.0-rc.0+281.sha-acf3cff');
 
 /**
  * @license
