53209f750da531b00221d139c9936e3264180be0
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:21:08 +0000

    f954ab6f10 docs: add note about the month being zero-based in the Date constructor (#37770)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+272.sha-3ee6665
+ * @license Angular v10.0.0-rc.0+273.sha-f954ab6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+272.sha-3ee6665');
+const VERSION$1 = new Version('10.0.0-rc.0+273.sha-f954ab6');
 
 /**
  * @license
