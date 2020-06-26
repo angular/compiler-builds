8c1dd91516472ec802e79f0a4de849be7bd4208b
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:27:35 +0000

    9c668e091f docs(elements): fixed command that adds the package @angular/elements (#37681)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+265.sha-4c7f32f
+ * @license Angular v10.0.0-rc.0+268.sha-9c668e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+265.sha-4c7f32f');
+const VERSION$1 = new Version('10.0.0-rc.0+268.sha-9c668e0');
 
 /**
  * @license
