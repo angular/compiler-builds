2f5e5edda1bdb7b44dd3f318c3fca32cb2fde6c3
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:35:05 +0000

    67cfc4c9bc build: add wombot proxy for publish config for @angular/benchpress (#37752)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+21.sha-a9a4ede
+ * @license Angular v10.0.0+25.sha-67cfc4c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0+21.sha-a9a4ede');
+    var VERSION$1 = new Version('10.0.0+25.sha-67cfc4c');
 
     /**
      * @license
