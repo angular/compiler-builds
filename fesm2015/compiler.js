0c3e0acc789f25ccd8458d5bcc0da9c13e53e4ad
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:17:52 +0000

    ae5257cda6 fix(language-service): incorrect autocomplete results on unknown symbol (#37518)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+282.sha-8fd8143
+ * @license Angular v10.0.0-rc.0+283.sha-ae5257c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+282.sha-8fd8143');
+const VERSION$1 = new Version('10.0.0-rc.0+283.sha-ae5257c');
 
 /**
  * @license
