4df96f3667ecd1e06e806c86b72ee164345b7dc7
Andrew Kushnir <akushnir@google.com>
2020-06-26 20:56:32 +0000

    8fd8143ab8 docs: release notes for the v10.0.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+278.sha-0879d2e
+ * @license Angular v10.0.0-rc.0+282.sha-8fd8143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+278.sha-0879d2e');
+    var VERSION$1 = new Version('10.0.0-rc.0+282.sha-8fd8143');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+278.sha-0879d2e
+ * @license Angular v10.0.0-rc.0+282.sha-8fd8143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+278.sha-0879d2e');
+const VERSION$1 = new Version('10.0.0-rc.0+282.sha-8fd8143');
 
 /**
  * @license
