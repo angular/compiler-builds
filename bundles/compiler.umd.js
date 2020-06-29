7afc42e6554abbbf8d1e3738fa42e80dc44753db
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:23:57 +0000

    873330b82c docs: correct the spelling mistake in observables error handling code (#36437)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+300.sha-435a28e
+ * @license Angular v10.0.0-rc.0+302.sha-873330b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+300.sha-435a28e');
+    var VERSION$1 = new Version('10.0.0-rc.0+302.sha-873330b');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+300.sha-435a28e
+ * @license Angular v10.0.0-rc.0+302.sha-873330b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+300.sha-435a28e');
+const VERSION$1 = new Version('10.0.0-rc.0+302.sha-873330b');
 
 /**
  * @license
