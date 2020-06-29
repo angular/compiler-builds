666298144105b203c0e108a8d85003114a263e6e
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:35:15 +0000

    ee1526029f docs: add Amadou Sall to GDE page (#36509)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+292.sha-eba9dd3
+ * @license Angular v10.0.0-rc.0+293.sha-ee15260
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+292.sha-eba9dd3');
+    var VERSION$1 = new Version('10.0.0-rc.0+293.sha-ee15260');
 
     /**
      * @license
