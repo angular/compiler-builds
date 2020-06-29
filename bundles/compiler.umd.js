9209798cb25d25745d007e32e08464a959e5f922
Andrew Kushnir <akushnir@google.com>
2020-06-29 23:58:17 +0000

    57a518a36d perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.1+20.sha-29b8318');
+    var VERSION$1 = new Version('10.0.1+21.sha-57a518a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.1+20.sha-29b8318');
+const VERSION$1 = new Version('10.0.1+21.sha-57a518a');
 
 /**
  * @license
