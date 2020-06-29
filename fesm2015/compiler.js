0b472df29a8dcd5b1afc29168bf00851e5d70fa7
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:26:46 +0000

    a177b1b7b1 docs: change definition of providedIn any (#35292)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+303.sha-e84539f
+ * @license Angular v10.0.0-rc.0+301.sha-a177b1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+303.sha-e84539f');
+const VERSION$1 = new Version('10.0.0-rc.0+301.sha-a177b1b');
 
 /**
  * @license
