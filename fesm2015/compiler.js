68274f61d3aee1267e6b3f4502faa44b5f11a937
Andrew Kushnir <akushnir@google.com>
2020-06-29 20:14:21 +0000

    435a28e937 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+298.sha-a87951a
+ * @license Angular v10.0.0-rc.0+300.sha-435a28e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+298.sha-a87951a');
+const VERSION$1 = new Version('10.0.0-rc.0+300.sha-435a28e');
 
 /**
  * @license
