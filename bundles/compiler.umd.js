3a7a389cbaeabfbdb58fed9115d1452cdb7e0cc1
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:00:26 +0000

    89e16ed6a5 fix(elements): fire custom element output events during component initialization (#37570)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+4.sha-78460c1
+ * @license Angular v10.0.1+10.sha-89e16ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.1+4.sha-78460c1');
+    var VERSION$1 = new Version('10.0.1+10.sha-89e16ed');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+4.sha-78460c1
+ * @license Angular v10.0.1+10.sha-89e16ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.1+4.sha-78460c1');
+const VERSION$1 = new Version('10.0.1+10.sha-89e16ed');
 
 /**
  * @license
