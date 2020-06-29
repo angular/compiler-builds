85dadc09a66ef8d05628fb75c43f668facaf69cb
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:26:18 +0000

    e84539f809 build(docs-infra): update to latest dgeni-packages (#37793)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+302.sha-873330b
+ * @license Angular v10.0.0-rc.0+303.sha-e84539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20268,7 +20268,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+302.sha-873330b');
+    var VERSION$1 = new Version('10.0.0-rc.0+303.sha-e84539f');
 
     /**
      * @license
