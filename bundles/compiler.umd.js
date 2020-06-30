d92aa3c6f873f5718da72b5982f13354fb7fdc4c
Andrew Kushnir <akushnir@google.com>
2020-06-30 18:10:22 +0000

    f9f2ba6faf docs: add Sonu Kapoor to the collaborator list (#37777)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+21.sha-57a518a
+ * @license Angular v10.0.1+23.sha-f9f2ba6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20399,7 +20399,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.1+21.sha-57a518a');
+    var VERSION$1 = new Version('10.0.1+23.sha-f9f2ba6');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+21.sha-57a518a
+ * @license Angular v10.0.1+23.sha-f9f2ba6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19096,7 +19096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.1+21.sha-57a518a');
+const VERSION$1 = new Version('10.0.1+23.sha-f9f2ba6');
 
 /**
  * @license
