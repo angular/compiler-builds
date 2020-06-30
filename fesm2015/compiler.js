3dca8ad76fe6816270dd5bafde864041d76c31a2
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:22:25 +0000

    6fc80690ae docs: release notes for the v10.0.2 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+306.sha-f6d2705
+ * @license Angular v10.0.0-rc.0+307.sha-6fc8069
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+306.sha-f6d2705');
+const VERSION$1 = new Version('10.0.0-rc.0+307.sha-6fc8069');
 
 /**
  * @license
