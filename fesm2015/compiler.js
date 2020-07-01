beacb01b56d1374f5ffe70219f9b3f4741794ea5
Andrew Kushnir <akushnir@google.com>
2020-07-01 16:48:23 +0000

    1d5ca55aab docs(changelog): remove empty sections (#36264)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+309.sha-9206a26
+ * @license Angular v10.0.0-rc.0+311.sha-1d5ca55
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+309.sha-9206a26');
+const VERSION$1 = new Version('10.0.0-rc.0+311.sha-1d5ca55');
 
 /**
  * @license
