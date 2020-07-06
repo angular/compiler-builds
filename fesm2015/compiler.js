c2413bb86e45c8b8064bf64d9c89eacf69a6f814
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:03:09 +0000

    a5c30733cb docs: mention for depreciation for `Testbed.get()` (#37815)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+313.sha-a5ffca0
+ * @license Angular v10.0.0-rc.0+315.sha-a5c3073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+313.sha-a5ffca0');
+const VERSION$1 = new Version('10.0.0-rc.0+315.sha-a5c3073');
 
 /**
  * @license
