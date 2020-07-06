b0f1e8dae35da82ab747726ce6e4c0a389ecb2e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:17:48 +0000

    1801d0c650 fix(router): properly compare array queryParams for equality (#37709) (#37860)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+314.sha-d7dd295
+ * @license Angular v10.0.0-rc.0+316.sha-1801d0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+314.sha-d7dd295');
+const VERSION$1 = new Version('10.0.0-rc.0+316.sha-1801d0c');
 
 /**
  * @license
