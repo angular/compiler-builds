7917d68e5b5a0eb366215f4f3163826a4fcf92a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:42:36 +0000

    f296fea112 docs: minor fixes to docs related to updating to v10 (#37897)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.2+9.sha-9d54b3a
+ * @license Angular v10.0.2+11.sha-f296fea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.2+9.sha-9d54b3a');
+const VERSION$1 = new Version('10.0.2+11.sha-f296fea');
 
 /**
  * @license
