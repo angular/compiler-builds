ba7048ea2eadb7de31cc283c35df91b7100f1425
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:29:02 +0000

    82a2207f4a build: filter out duplicate cherry-picked commits in changelog (#37956)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+325.sha-289a96b
+ * @license Angular v10.0.0-rc.0+326.sha-82a2207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20275,7 +20275,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.0-rc.0+325.sha-289a96b');
+    var VERSION$1 = new Version('10.0.0-rc.0+326.sha-82a2207');
 
     /**
      * @license
