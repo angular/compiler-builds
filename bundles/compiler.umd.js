62dfc76937e1110e9dfa9c83f9e49d8b5f837370
atscott <atscott@google.com>
2020-07-08 23:31:25 +0000

    bbe3543c69 refactor(core): remove duplicated WrappedValue class (#37940)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+10.sha-e50c7dc
+ * @license Angular v10.1.0-next.0+4.sha-bbe3543
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20275,7 +20275,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.0+10.sha-e50c7dc');
+    var VERSION$1 = new Version('10.1.0-next.0+4.sha-bbe3543');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+10.sha-e50c7dc
+ * @license Angular v10.1.0-next.0+4.sha-bbe3543
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+10.sha-e50c7dc');
+const VERSION$1 = new Version('10.1.0-next.0+4.sha-bbe3543');
 
 /**
  * @license
