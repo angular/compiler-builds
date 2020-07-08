c9e33d50b14df5e71e07c0a3590569df014e160e
atscott <atscott@google.com>
2020-07-08 23:01:16 +0000

    879b2273c1 ci: decrease the minBodyLength commit message limit to 20 chars (#37949)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3
+ * @license Angular v10.0.3+1.sha-879b227
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.3');
+const VERSION$1 = new Version('10.0.3+1.sha-879b227');
 
 /**
  * @license
