71fe948616b9035e7461f5264a0f19d20295dc5d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:32:09 +0000

    6eb868b63a build(compiler-cli): fix bazel deps rules for ngtsc testing packages (#37977)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+326.sha-82a2207
+ * @license Angular v10.0.0-rc.0+327.sha-6eb868b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.0-rc.0+326.sha-82a2207');
+const VERSION$1 = new Version('10.0.0-rc.0+327.sha-6eb868b');
 
 /**
  * @license
