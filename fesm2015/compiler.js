3a266191343f0da85fc6e6db580ecb8a52ac1e19
atscott <atscott01@gmail.com>
2020-07-09 23:00:29 +0000

    1550663b9e fix(bazel): ng_module rule does not expose flat module information in Ivy (#36971)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+18.sha-c40cbec
+ * @license Angular v10.1.0-next.0+19.sha-1550663
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+18.sha-c40cbec');
+const VERSION$1 = new Version('10.1.0-next.0+19.sha-1550663');
 
 /**
  * @license
