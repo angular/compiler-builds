978bfd5baecf07aea53f38af0f38d4aa60b70dde
atscott <atscott@google.com>
2020-07-09 16:43:01 +0000

    b98314e306 docs: add Formly library to the list of resources (#37257)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+9.sha-6b731a0
+ * @license Angular v10.1.0-next.0+11.sha-b98314e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+9.sha-6b731a0');
+const VERSION$1 = new Version('10.1.0-next.0+11.sha-b98314e');
 
 /**
  * @license
