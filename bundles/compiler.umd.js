e439028839a5019960053b6a67cf93f14b431283
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:33:48 +0000

    95669df4c3 docs: remove Markus Padourek from angular collaborators (#37962)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+37.sha-36dd817
+ * @license Angular v10.1.0-next.0+34.sha-95669df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20273,7 +20273,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.0+37.sha-36dd817');
+    var VERSION$1 = new Version('10.1.0-next.0+34.sha-95669df');
 
     /**
      * @license
