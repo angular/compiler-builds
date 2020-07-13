5ed7d9da4740261664c38df717ddc45c81148058
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:54:17 +0000

    9cf78d5701 docs(core): Fixed typo in Type JSdoc (#37930)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+45.sha-53e4ff7
+ * @license Angular v10.0.3+52.sha-9cf78d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.3+45.sha-53e4ff7');
+const VERSION$1 = new Version('10.0.3+52.sha-9cf78d5');
 
 /**
  * @license
