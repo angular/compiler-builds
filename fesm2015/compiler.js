e2f79aa690e696b143a04d65783b40a4500a48b7
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:31:12 +0000

    95669df4c3 docs: remove Markus Padourek from angular collaborators (#37962)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+37.sha-36dd817
+ * @license Angular v10.1.0-next.0+34.sha-95669df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+37.sha-36dd817');
+const VERSION$1 = new Version('10.1.0-next.0+34.sha-95669df');
 
 /**
  * @license
