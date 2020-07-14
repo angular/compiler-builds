56d8474dc6948400f665a8fcdf9d85dd6988937f
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-14 18:40:09 +0000

    e52ac490c8 docs(core): fixes minor typo in initNgDevMode function docs

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-2dd22b7
+ * @license Angular v10.1.0-next.0+42.sha-e52ac49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+42.sha-2dd22b7');
+const VERSION$1 = new Version('10.1.0-next.0+42.sha-e52ac49');
 
 /**
  * @license
