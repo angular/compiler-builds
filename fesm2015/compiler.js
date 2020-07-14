1ad27d8a36de42948282bfa456e0e1401deeb5e2
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:44:47 +0000

    f01b0337d2 docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+43.sha-4ec3026
+ * @license Angular v10.1.0-next.0+44.sha-f01b033
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+43.sha-4ec3026');
+const VERSION$1 = new Version('10.1.0-next.0+44.sha-f01b033');
 
 /**
  * @license
