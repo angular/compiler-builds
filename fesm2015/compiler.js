d509d5ff207e9ba47bb8ecbb63542fd3cdfce3f1
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:36:10 +0000

    4ec3026a9a docs(router): fix typo in https://angular.io/guide/router#activated-route (#38034)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-9021d3f
+ * @license Angular v10.1.0-next.0+43.sha-4ec3026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.0+42.sha-9021d3f');
+const VERSION$1 = new Version('10.1.0-next.0+43.sha-4ec3026');
 
 /**
  * @license
