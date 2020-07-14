52557eac176b24924499434b89d022aacaf846ab
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:49:03 +0000

    e3b801053a fix(ngcc): report a warning if ngcc tries to use a solution-style tsconfig (#38003)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+58.sha-396033d
+ * @license Angular v10.0.3+60.sha-e3b8010
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19103,7 +19103,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.3+58.sha-396033d');
+const VERSION$1 = new Version('10.0.3+60.sha-e3b8010');
 
 /**
  * @license
