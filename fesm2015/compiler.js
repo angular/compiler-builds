44f835a0df737be677167e8ad5decd432e75b81f
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:11:26 +0000

    406f801b70 build: remove obsolete Bazel function ctx.new_file (#38080)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+8.sha-c8e77d8
+ * @license Angular v10.1.0-next.1+9.sha-406f801
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19110,7 +19110,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+8.sha-c8e77d8');
+const VERSION$1 = new Version('10.1.0-next.1+9.sha-406f801');
 
 /**
  * @license
