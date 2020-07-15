652ff2a05da5d95448fce3da6b5e7356b54db7a4
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:06:08 +0000

    545f1dbac9 feat(dev-infra): add bazel firefox browser with RBE compatibility (#38029)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+2.sha-45d79ef
+ * @license Angular v10.1.0-next.1+4.sha-545f1db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20273,7 +20273,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.1+2.sha-45d79ef');
+    var VERSION$1 = new Version('10.1.0-next.1+4.sha-545f1db');
 
     /**
      * @license
