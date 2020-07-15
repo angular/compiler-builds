8e646a9a57bbb883f73663f3565c47fafd906400
Andrew Kushnir <akushnir@google.com>
2020-07-15 23:47:41 +0000

    737506e79c fix(core): Allow modification of lifecycle hooks any time before bootstrap (#35464)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+13.sha-469aba0
+ * @license Angular v10.1.0-next.1+14.sha-737506e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19111,7 +19111,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+13.sha-469aba0');
+const VERSION$1 = new Version('10.1.0-next.1+14.sha-737506e');
 
 /**
  * @license
