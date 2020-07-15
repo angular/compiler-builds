6410a62941b23f115794b8cffb97399583ea42e9
Andrew Kushnir <akushnir@google.com>
2020-07-15 21:13:37 +0000

    469aba0140 docs: add Emma Twersky to DevRel Contributor page (#38084)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20274,7 +20274,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.1+12.sha-d8c47b9');
+    var VERSION$1 = new Version('10.1.0-next.1+13.sha-469aba0');
 
     /**
      * @license
