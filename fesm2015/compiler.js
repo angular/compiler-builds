d9fbeebc1bcea15718a1df511d4f8c18084ccbb1
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:25:11 +0000

    99960a98d2 docs: update router api documentation (#37980)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+15.sha-9ac3383
+ * @license Angular v10.0.4+16.sha-99960a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19104,7 +19104,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.4+15.sha-9ac3383');
+const VERSION$1 = new Version('10.0.4+16.sha-99960a9');
 
 /**
  * @license
