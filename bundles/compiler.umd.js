cbe2dd13add4d0e1efc56792e070b48b6443fb16
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:25:18 +0000

    c1539b16ae docs: add Ajit Singh to the collaborators (#37792)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+14.sha-737506e
+ * @license Angular v10.1.0-next.1+15.sha-c1539b1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20274,7 +20274,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.1+14.sha-737506e');
+    var VERSION$1 = new Version('10.1.0-next.1+15.sha-c1539b1');
 
     /**
      * @license
