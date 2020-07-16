b3f26708a86eea64b93c96ec8ab45208795bbd97
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:30:25 +0000

    9ac3383d01 build(language-service): remove typescript from ivy bundle (#38088)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+12.sha-6e32972
+ * @license Angular v10.0.4+15.sha-9ac3383
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.4+12.sha-6e32972');
+    var VERSION$1 = new Version('10.0.4+15.sha-9ac3383');
 
     /**
      * @license
