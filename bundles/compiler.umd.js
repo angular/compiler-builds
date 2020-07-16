b074b59471abfb524c93532a51d0ca091e900f79
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:31:45 +0000

    019a696a6a Revert "build: remove obsolete Bazel function ctx.new_file (#38080)" (#38101)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+15.sha-c1539b1
+ * @license Angular v10.1.0-next.1+19.sha-019a696
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20274,7 +20274,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.1+15.sha-c1539b1');
+    var VERSION$1 = new Version('10.1.0-next.1+19.sha-019a696');
 
     /**
      * @license
