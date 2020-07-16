230305c4446b3e1bbde11426d96570de73c8dce8
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:36:34 +0000

    e0b35bfc18 ci: add more owners for some categories (#37994)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+17.sha-bd71f10
+ * @license Angular v10.1.0-next.1+16.sha-e0b35bf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19111,7 +19111,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+17.sha-bd71f10');
+const VERSION$1 = new Version('10.1.0-next.1+16.sha-e0b35bf');
 
 /**
  * @license
