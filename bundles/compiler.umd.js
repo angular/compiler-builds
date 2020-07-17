4e66d3089363cf61ee42fcdc015fc3677b0df744
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:10:22 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+24.sha-58f6079
+ * @license Angular v10.1.0-next.1+25.sha-fd45f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20274,7 +20274,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.1+24.sha-58f6079');
+    var VERSION$1 = new Version('10.1.0-next.1+25.sha-fd45f74');
 
     /**
      * @license
