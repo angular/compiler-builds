23009d2c24b0ff998e8a45232f37f45800efc2ac
Andrew Kushnir <akushnir@google.com>
2020-07-17 00:04:43 +0000

    e9cb6fbe87 build(language-service): add script to build package locally (#38103)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+16.sha-99960a9
+ * @license Angular v10.0.4+17.sha-e9cb6fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19104,7 +19104,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.4+16.sha-99960a9');
+const VERSION$1 = new Version('10.0.4+17.sha-e9cb6fb');
 
 /**
  * @license
