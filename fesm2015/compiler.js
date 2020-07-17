7dd6b5c6ea530fd55cf716733ab0c50abc937d9b
Andrew Kushnir <akushnir@google.com>
2020-07-17 21:03:25 +0000

    095cfd961d build: Ignore .history for the xyz.local-history VSCode extension (#38121)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+27.sha-38a7021
+ * @license Angular v10.1.0-next.1+28.sha-095cfd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19111,7 +19111,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+27.sha-38a7021');
+const VERSION$1 = new Version('10.1.0-next.1+28.sha-095cfd9');
 
 /**
  * @license
