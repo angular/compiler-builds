eff15b21f1a2784217c98565f92266986967326f
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:07:57 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+24.sha-58f6079
+ * @license Angular v10.1.0-next.1+25.sha-fd45f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19111,7 +19111,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+24.sha-58f6079');
+const VERSION$1 = new Version('10.1.0-next.1+25.sha-fd45f74');
 
 /**
  * @license
