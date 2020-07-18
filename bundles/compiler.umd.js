416f77e4ee785a9f5bb10eb78b0b1073222b5575
Andrew Kushnir <akushnir@google.com>
2020-07-18 00:19:03 +0000

    8e305e7099 docs: correct flag default values in `--strict` (#37982)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+22.sha-481df83
+ * @license Angular v10.0.4+23.sha-8e305e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20267,7 +20267,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.4+22.sha-481df83');
+    var VERSION$1 = new Version('10.0.4+23.sha-8e305e7');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+22.sha-481df83
+ * @license Angular v10.0.4+23.sha-8e305e7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19104,7 +19104,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.4+22.sha-481df83');
+const VERSION$1 = new Version('10.0.4+23.sha-8e305e7');
 
 /**
  * @license
