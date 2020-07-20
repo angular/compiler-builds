e54168fdb736345c038170cc0d3b38de16406c8d
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:45:00 +0000

    9cb318f5a2 docs: separate template syntax into multiple docs (#36954)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+27.sha-e80278c
+ * @license Angular v10.0.4+31.sha-9cb318f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.4+27.sha-e80278c');
+    var VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+27.sha-e80278c
+ * @license Angular v10.0.4+31.sha-9cb318f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.4+27.sha-e80278c');
+const VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
 
 /**
  * @license
