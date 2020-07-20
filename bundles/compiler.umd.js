91eb5f706cbeb8fbc620a8cc0979f225f666e2a7
Andrew Kushnir <akushnir@google.com>
2020-07-20 20:58:52 +0000

    d6d3984524 docs(common): fix selector field in NgIfAs example component (#35854)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+31.sha-9cb318f
+ * @license Angular v10.0.4+33.sha-d6d3984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
+    var VERSION$1 = new Version('10.0.4+33.sha-d6d3984');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+31.sha-9cb318f
+ * @license Angular v10.0.4+33.sha-d6d3984
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.4+31.sha-9cb318f');
+const VERSION$1 = new Version('10.0.4+33.sha-d6d3984');
 
 /**
  * @license
