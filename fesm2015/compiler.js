86e62b8143ca5a1f48f952bcba73a80fd5c8100d
Andrew Kushnir <akushnir@google.com>
2020-07-20 21:36:38 +0000

    4a6dcd006b docs(router): fix typos (#38132)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+40.sha-3373453
+ * @license Angular v10.1.0-next.1+39.sha-4a6dcd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+40.sha-3373453');
+const VERSION$1 = new Version('10.1.0-next.1+39.sha-4a6dcd0');
 
 /**
  * @license
