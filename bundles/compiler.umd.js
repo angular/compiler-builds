2fc61685cd348e60c82d83234865ec9ef8078518
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:31:58 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+34.sha-3797861
+ * @license Angular v10.0.4+35.sha-4664acc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.4+34.sha-3797861');
+    var VERSION$1 = new Version('10.0.4+35.sha-4664acc');
 
     /**
      * @license
