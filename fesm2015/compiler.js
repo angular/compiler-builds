4894aeb59732ccd7cedb5ec6f6def9cdb686eae7
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:58:49 +0000

    a9910f5991 docs: fix DOCUMENT import path (#38158) (#38159)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+43.sha-ad7046b
+ * @license Angular v10.1.0-next.1+44.sha-a9910f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.1+43.sha-ad7046b');
+const VERSION$1 = new Version('10.1.0-next.1+44.sha-a9910f5');
 
 /**
  * @license
