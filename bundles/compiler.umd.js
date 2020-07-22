410df965ae583af2b326e60a937bba7f6c9a14b0
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:36:05 +0000

    5e742d29d0 docs: fix typo from singular to plural spelling (#36586)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2
+ * @license Angular v10.1.0-next.2+1.sha-5e742d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20236,7 +20236,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2');
+    var VERSION$1 = new Version('10.1.0-next.2+1.sha-5e742d2');
 
     /**
      * @license
