2a6abb32036ff630b3d7404de29a2ea983846062
Misko Hevery <misko@hevery.com>
2020-07-23 04:01:27 +0000

    8df888dfb4 fix(elements): run strategy methods in correct zone (#37814)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+9.sha-778ad37
+ * @license Angular v10.1.0-next.2+10.sha-8df888d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20236,7 +20236,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+9.sha-778ad37');
+    var VERSION$1 = new Version('10.1.0-next.2+10.sha-8df888d');
 
     /**
      * @license
