e031a82ea9200fc3ed627d6d82becfe6d726b3ba
Misko Hevery <misko@hevery.com>
2020-07-23 03:50:31 +0000

    778ad3776a docs: create coding standards doc (#37700)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20236,7 +20236,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+2.sha-d88711c');
+    var VERSION$1 = new Version('10.1.0-next.2+9.sha-778ad37');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+2.sha-d88711c');
+const VERSION$1 = new Version('10.1.0-next.2+9.sha-778ad37');
 
 /**
  * @license
