292b4f69e3f26397f68b88fa93c614e1d38602a6
Misko Hevery <misko@hevery.com>
2020-07-23 03:58:49 +0000

    8df888dfb4 fix(elements): run strategy methods in correct zone (#37814)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+9.sha-778ad37
+ * @license Angular v10.1.0-next.2+10.sha-8df888d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+9.sha-778ad37');
+const VERSION$1 = new Version('10.1.0-next.2+10.sha-8df888d');
 
 /**
  * @license
