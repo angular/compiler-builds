8aa19386785da119cb449ddcaf16ec117ea5adb5
Misko Hevery <misko@hevery.com>
2020-07-23 18:22:25 +0000

    8bb8f98c28 docs: remove duplicate `https://` (#38199)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+12.sha-3c1866b
+ * @license Angular v10.1.0-next.2+13.sha-8bb8f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+12.sha-3c1866b');
+const VERSION$1 = new Version('10.1.0-next.2+13.sha-8bb8f98');
 
 /**
  * @license
