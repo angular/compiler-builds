0b992f6ad73def03e222c48fa6a43afd47b3a369
Misko Hevery <misko@hevery.com>
2020-07-23 18:25:03 +0000

    7b2e2f5d91 build(forms): create sample forms app (#38044)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+14.sha-51aa4aa
+ * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20236,7 +20236,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+14.sha-51aa4aa');
+    var VERSION$1 = new Version('10.1.0-next.2+16.sha-7b2e2f5');
 
     /**
      * @license
