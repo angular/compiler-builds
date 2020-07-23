8a0600cf9b9932b0ca9109f7d3faaad477b5df86
Misko Hevery <misko@hevery.com>
2020-07-23 18:27:43 +0000

    7b2e2f5d91 build(forms): create sample forms app (#38044)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+14.sha-51aa4aa
+ * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+14.sha-51aa4aa');
+const VERSION$1 = new Version('10.1.0-next.2+16.sha-7b2e2f5');
 
 /**
  * @license
