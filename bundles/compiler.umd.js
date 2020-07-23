504c932c65dc1327934939a14134fad61d0a8379
Misko Hevery <misko@hevery.com>
2020-07-23 04:05:15 +0000

    062b8d90af refactor(forms): refactor common validators used in unit tests (#38020)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+10.sha-8df888d
+ * @license Angular v10.1.0-next.2+11.sha-062b8d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20236,7 +20236,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+10.sha-8df888d');
+    var VERSION$1 = new Version('10.1.0-next.2+11.sha-062b8d9');
 
     /**
      * @license
