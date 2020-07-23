52406965dcfe71e7a33d6bbd8ac25cc6d64ca39c
Misko Hevery <misko@hevery.com>
2020-07-23 04:11:03 +0000

    a50a688aaf docs: update api reference for router outlet directive (#38166)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+7.sha-6ec7297
+ * @license Angular v10.0.5+8.sha-a50a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.5+7.sha-6ec7297');
+    var VERSION$1 = new Version('10.0.5+8.sha-a50a688');
 
     /**
      * @license
