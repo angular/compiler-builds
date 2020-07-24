1d818aeb2c6a8e59158da72ac812ad5c7da1ad67
Misko Hevery <misko@hevery.com>
2020-07-24 22:41:13 +0000

    a71f114ba4 fix(zone.js): clearTimeout/clearInterval should call on object global (#37858)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+24.sha-253337d
+ * @license Angular v10.1.0-next.2+25.sha-a71f114
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+24.sha-253337d');
+const VERSION$1 = new Version('10.1.0-next.2+25.sha-a71f114');
 
 /**
  * @license
