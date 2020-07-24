8dd5759134854345e17c00ea6ca4aed5a8b415dc
Misko Hevery <misko@hevery.com>
2020-07-24 15:38:40 +0000

    b8af10902f docs: fixed that class attribute is not closed (#38219)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20229,7 +20229,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.5+15.sha-f411c9e');
+    var VERSION$1 = new Version('10.0.5+16.sha-b8af109');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.5+15.sha-f411c9e');
+const VERSION$1 = new Version('10.0.5+16.sha-b8af109');
 
 /**
  * @license
