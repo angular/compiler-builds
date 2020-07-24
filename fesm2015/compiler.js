488975349e0181f9f0240295b68c1995676ad27d
Misko Hevery <misko@hevery.com>
2020-07-24 23:08:07 +0000

    c4e6f585c5 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+19.sha-7467fd3
+ * @license Angular v10.0.5+20.sha-c4e6f58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19066,7 +19066,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.5+19.sha-7467fd3');
+const VERSION$1 = new Version('10.0.5+20.sha-c4e6f58');
 
 /**
  * @license
