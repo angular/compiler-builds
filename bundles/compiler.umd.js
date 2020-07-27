550e4425be1ebfa8e3c1156ceb441e44e5fe2c5b
Misko Hevery <misko@hevery.com>
2020-07-27 19:36:23 +0000

    776067cd43 fix(zone.js): zone patch rxjs should return null _unsubscribe correctly. (#37091)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+30.sha-89a7ff3
+ * @license Angular v10.0.5+32.sha-776067c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20240,7 +20240,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.5+30.sha-89a7ff3');
+    var VERSION$1 = new Version('10.0.5+32.sha-776067c');
 
     /**
      * @license
