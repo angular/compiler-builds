e7d8f52f61ecbfb7ddf851d5cd2b8b3effe6c716
Misko Hevery <misko@hevery.com>
2020-07-27 17:12:34 +0000

    4dfc3fe6a2 refactor(router): extract Router config utils to a separate file (#38229)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+32.sha-1296003
+ * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+32.sha-1296003');
+const VERSION$1 = new Version('10.1.0-next.2+31.sha-4dfc3fe');
 
 /**
  * @license
