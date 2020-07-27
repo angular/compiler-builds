b5a9facff43dd21a3f0c6ab052711bbf89c0abce
Misko Hevery <misko@hevery.com>
2020-07-27 17:13:20 +0000

    1296003445 docs: add ng-add save option (#38198)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+30.sha-67e3ecc
+ * @license Angular v10.1.0-next.2+32.sha-1296003
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19073,7 +19073,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+30.sha-67e3ecc');
+const VERSION$1 = new Version('10.1.0-next.2+32.sha-1296003');
 
 /**
  * @license
