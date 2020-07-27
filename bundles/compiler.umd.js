6604b5ca4f432a2ef469bda1b86264fc939e5d73
Misko Hevery <misko@hevery.com>
2020-07-27 17:47:19 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+35.sha-8e5969b
+ * @license Angular v10.1.0-next.2+36.sha-f74cfad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20247,7 +20247,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+35.sha-8e5969b');
+    var VERSION$1 = new Version('10.1.0-next.2+36.sha-f74cfad');
 
     /**
      * @license
