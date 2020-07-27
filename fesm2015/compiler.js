7814b1b538f2d6038f771fef89e2bf12ad62d568
Misko Hevery <misko@hevery.com>
2020-07-27 17:44:43 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+35.sha-8e5969b
+ * @license Angular v10.1.0-next.2+36.sha-f74cfad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19084,7 +19084,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+35.sha-8e5969b');
+const VERSION$1 = new Version('10.1.0-next.2+36.sha-f74cfad');
 
 /**
  * @license
