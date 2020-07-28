b1ebc72a29a4bd428b659475a7039b3d72637fca
Misko Hevery <misko@hevery.com>
2020-07-28 19:58:04 +0000

    2a45b932e2 build: fix broken build (#38274)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+45.sha-65cc0c8
+ * @license Angular v10.1.0-next.2+58.sha-2a45b93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19084,7 +19084,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.2+45.sha-65cc0c8');
+const VERSION$1 = new Version('10.1.0-next.2+58.sha-2a45b93');
 
 /**
  * @license
