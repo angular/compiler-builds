59837dbda58376551b48f21f7e6d9ac11b9b8678
Misko Hevery <misko@hevery.com>
2020-07-28 23:02:46 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20247,7 +20247,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.2+59.sha-28c3e2d');
+    var VERSION$1 = new Version('10.1.0-next.3');
 
     /**
      * @license
