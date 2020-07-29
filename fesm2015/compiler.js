0b17eaa510748683a3992a6c97b2bd889619f984
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 21:00:14 +0000

    8effc83b92 docs(router): clarify how base href is used to construct targets (#38123)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+15.sha-887c350
+ * @license Angular v10.1.0-next.3+17.sha-8effc83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19114,7 +19114,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.3+15.sha-887c350');
+const VERSION$1 = new Version('10.1.0-next.3+17.sha-8effc83');
 
 /**
  * @license
