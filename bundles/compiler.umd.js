e2b5da2490008088190e5d7a524a2eec7c8c2fd2
Misko Hevery <misko@hevery.com>
2020-07-29 17:55:07 +0000

    9c8bc4a239 fix(common): narrow `NgIf` context variables in template type checker (#36627)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+1.sha-cf4da82
+ * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20247,7 +20247,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.3+1.sha-cf4da82');
+    var VERSION$1 = new Version('10.1.0-next.3+2.sha-9c8bc4a');
 
     /**
      * @license
