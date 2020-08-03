dbf4ea2e4bd92e74dc2041fd1016f2d271d2a71e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:20:47 +0000

    8fbf40bf40 feat(core): update reference and doc to change `async` to `waitAsync`. (#37583)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+40.sha-87baa06
+ * @license Angular v10.1.0-next.3+43.sha-8fbf40b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.3+40.sha-87baa06');
+    var VERSION$1 = new Version('10.1.0-next.3+43.sha-8fbf40b');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+40.sha-87baa06
+ * @license Angular v10.1.0-next.3+43.sha-8fbf40b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.3+40.sha-87baa06');
+const VERSION$1 = new Version('10.1.0-next.3+43.sha-8fbf40b');
 
 /**
  * @license
