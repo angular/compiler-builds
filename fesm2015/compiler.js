da5dfa79bdde885cc437ba18a548c22523cdf35d
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:11:08 +0000

    585ef2bdd7 docs: fix typo in glossary (#38318)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4
+ * @license Angular v10.1.0-next.4+1.sha-585ef2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4');
+const VERSION$1 = new Version('10.1.0-next.4+1.sha-585ef2b');
 
 /**
  * @license
