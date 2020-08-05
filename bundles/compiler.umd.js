c64ae8880509a8d89ac77c48bfb945c4bc1c5fcf
Andrew Kushnir <akushnir@google.com>
2020-08-05 23:38:08 +0000

    bb88c9fa3d fix(common): ensure scrollRestoration is writable (#30630)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+7.sha-1609815
+ * @license Angular v10.1.0-next.4+9.sha-bb88c9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.4+7.sha-1609815');
+    var VERSION$1 = new Version('10.1.0-next.4+9.sha-bb88c9f');
 
     /**
      * @license
