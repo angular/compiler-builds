7535103a41d74c0e18d3790dd779c236f0268709
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:24:02 +0000

    763023472b fix(router): prevent calling unsubscribe on undefined subscription in RouterPreloader (#38344)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+4.sha-f0766a4
+ * @license Angular v10.1.0-next.4+6.sha-7630234
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.4+4.sha-f0766a4');
+    var VERSION$1 = new Version('10.1.0-next.4+6.sha-7630234');
 
     /**
      * @license
