e0a030378f3d241d7b5a67d1c5cb1477569a412c
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:24:22 +0000

    ba175be41f fix(compiler-cli): match wrapHost parameter types within plugin interface (#38004)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+6.sha-7630234
+ * @license Angular v10.1.0-next.4+5.sha-ba175be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+6.sha-7630234');
+const VERSION$1 = new Version('10.1.0-next.4+5.sha-ba175be');
 
 /**
  * @license
