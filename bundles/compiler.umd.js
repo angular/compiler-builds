8d12e1449e7d6ddd6a109dd18503b8670152b13f
Andrew Kushnir <akushnir@google.com>
2020-08-06 23:50:38 +0000

    deb290bc2c refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154) (#38370)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+17.sha-f5d5bac
+ * @license Angular v10.0.8+19.sha-deb290b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+17.sha-f5d5bac');
+    var VERSION$1 = new Version('10.0.8+19.sha-deb290b');
 
     /**
      * @license
