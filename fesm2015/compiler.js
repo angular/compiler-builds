632d432a44c6b9442c8ce524d5c5374141620939
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:46:27 +0000

    26be5b4994 refactor(core): extract `icuSwitchCase`, `icuUpdateCase`, `removeNestedIcu` (#38154)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+17.sha-18cd1a9
+ * @license Angular v10.1.0-next.4+19.sha-26be5b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+17.sha-18cd1a9');
+const VERSION$1 = new Version('10.1.0-next.4+19.sha-26be5b4');
 
 /**
  * @license
