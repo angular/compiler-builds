3b27d32fd1ad2062c13dcc931a2b05842be3f3a0
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:27:12 +0000

    016a41b14d fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+10.sha-58f4b3a
+ * @license Angular v10.0.8+14.sha-016a41b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+10.sha-58f4b3a');
+const VERSION$1 = new Version('10.0.8+14.sha-016a41b');
 
 /**
  * @license
