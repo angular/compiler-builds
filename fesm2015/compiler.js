8b834011eb070bee2e17650a27b3f76bd42522ac
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:30:04 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+9.sha-bb88c9f');
+const VERSION$1 = new Version('10.1.0-next.4+10.sha-0c24903');
 
 /**
  * @license
