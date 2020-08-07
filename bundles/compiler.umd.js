578e155f77c2fba1c5372ab2c58b927525524b82
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:37:00 +0000

    354e66efad refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#30143)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+23.sha-702958e
+ * @license Angular v10.1.0-next.4+24.sha-354e66e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.4+23.sha-702958e');
+    var VERSION$1 = new Version('10.1.0-next.4+24.sha-354e66e');
 
     /**
      * @license
