b746a5985cb948a4cef664043b71a95d1b90923f
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:21:13 +0000

    ef7b70a375 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+19.sha-deb290b
+ * @license Angular v10.0.8+20.sha-ef7b70a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+19.sha-deb290b');
+const VERSION$1 = new Version('10.0.8+20.sha-ef7b70a');
 
 /**
  * @license
