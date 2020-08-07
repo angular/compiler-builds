eb58c65df03177ab567f79c78d08a9f8e3b1c299
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:31:34 +0000

    47fbfb37bf refactor(forms): get rid of duplicate functions (#38371) (#38380)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+22.sha-4107abb
+ * @license Angular v10.0.8+23.sha-47fbfb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+22.sha-4107abb');
+    var VERSION$1 = new Version('10.0.8+23.sha-47fbfb3');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+22.sha-4107abb
+ * @license Angular v10.0.8+23.sha-47fbfb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+22.sha-4107abb');
+const VERSION$1 = new Version('10.0.8+23.sha-47fbfb3');
 
 /**
  * @license
