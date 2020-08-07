d50f84cdb0a7e7780c9dc5cac4dd4d5b7f3d3541
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:28:55 +0000

    42a649d80f docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+23.sha-47fbfb3
+ * @license Angular v10.0.8+24.sha-42a649d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+23.sha-47fbfb3');
+    var VERSION$1 = new Version('10.0.8+24.sha-42a649d');
 
     /**
      * @license
