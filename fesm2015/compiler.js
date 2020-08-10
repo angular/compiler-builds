516048e14b79a0141cbfa8327f5d98ec30694337
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:44:15 +0000

    8f708b561c fix(router): defer loading of wildcard module until needed (#38348)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+30.sha-e34c33c
+ * @license Angular v10.1.0-next.4+31.sha-8f708b5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+30.sha-e34c33c');
+const VERSION$1 = new Version('10.1.0-next.4+31.sha-8f708b5');
 
 /**
  * @license
