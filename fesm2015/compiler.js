1ab9ad3e5556d314e27a5da376395368bb94e31d
Andrew Kushnir <akushnir@google.com>
2020-08-10 17:18:30 +0000

    6ff28ac944 docs: Remove redundant sentence from Router (#38398) PR Close #38398

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+26.sha-445ac15
+ * @license Angular v10.1.0-next.4+28.sha-6ff28ac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+26.sha-445ac15');
+const VERSION$1 = new Version('10.1.0-next.4+28.sha-6ff28ac');
 
 /**
  * @license
