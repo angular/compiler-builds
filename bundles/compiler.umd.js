c38a400d2517a7f5ad7f9332b347b0f0ddd9b4bd
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:54:26 +0000

    32109dc414 fix(core): queries not matching string injection tokens (#38321)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+28.sha-7f11149
+ * @license Angular v10.0.8+31.sha-32109dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+28.sha-7f11149');
+    var VERSION$1 = new Version('10.0.8+31.sha-32109dc');
 
     /**
      * @license
