e048c08f593ca846fc69ce52d0769e9d7aa270d7
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:03:35 +0000

    ee5123f5dc fix(core): Store the currently selected ICU in `LView` (#38345)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+26.sha-3dfaf56
+ * @license Angular v10.0.8+27.sha-ee5123f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+26.sha-3dfaf56');
+const VERSION$1 = new Version('10.0.8+27.sha-ee5123f');
 
 /**
  * @license
