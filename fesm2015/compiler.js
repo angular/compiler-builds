36b083ec9bdc36a287e8f570006febef78fb820e
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:33:55 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+27.sha-ee5123f
+ * @license Angular v10.0.8+28.sha-7f11149
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+27.sha-ee5123f');
+const VERSION$1 = new Version('10.0.8+28.sha-7f11149');
 
 /**
  * @license
