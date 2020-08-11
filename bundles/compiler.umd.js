32b0e20f5a8c104f2510480f0f684be609356eb7
Andrew Kushnir <akushnir@google.com>
2020-08-11 20:26:36 +0000

    ca2b4bcd4b fix(compiler-cli): avoid creating value expressions for symbols from type-only imports (#38415)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+36.sha-f6cfc92');
+    var VERSION$1 = new Version('10.0.8+37.sha-ca2b4bc');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+36.sha-f6cfc92');
+const VERSION$1 = new Version('10.0.8+37.sha-ca2b4bc');
 
 /**
  * @license
