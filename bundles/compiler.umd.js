04c876305d96f32a907ad60c4326906fc8f658f1
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:51:29 +0000

    02aca135bd fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+31.sha-32109dc
+ * @license Angular v10.0.8+32.sha-02aca13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.8+31.sha-32109dc');
+    var VERSION$1 = new Version('10.0.8+32.sha-02aca13');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+31.sha-32109dc
+ * @license Angular v10.0.8+32.sha-02aca13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19120,7 +19120,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.8+31.sha-32109dc');
+const VERSION$1 = new Version('10.0.8+32.sha-02aca13');
 
 /**
  * @license
