78a4f22f1e9526979ab4815d622ddbf1a74cc1a9
Andrew Kushnir <akushnir@google.com>
2020-08-11 16:03:52 +0000

    df76a2048b fix(router): restore 'history.state' object for navigations coming from Angular router (#28108) (#28176)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+35.sha-3d15616
+ * @license Angular v10.1.0-next.4+36.sha-df76a20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.4+35.sha-3d15616');
+    var VERSION$1 = new Version('10.1.0-next.4+36.sha-df76a20');
 
     /**
      * @license
