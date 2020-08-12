35e63c58cf74764bc9547542d7acdfe1233ce098
Andrew Kushnir <akushnir@google.com>
2020-08-12 15:45:33 +0000

    4d17418569 docs: delete one superfluous sentence (#38339)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+44.sha-a6292fa
+ * @license Angular v10.1.0-next.4+47.sha-4d17418
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.4+44.sha-a6292fa');
+const VERSION$1 = new Version('10.1.0-next.4+47.sha-4d17418');
 
 /**
  * @license
