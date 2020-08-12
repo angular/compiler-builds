7efc384042c941378c05c64e152e0c2e5fc59e66
Andrew Scott <atscott01@gmail.com>
2020-08-12 19:00:57 +0000

    5f2e475abf docs: remove unused Input decorator (#38306)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+2.sha-aa3520e
+ * @license Angular v10.1.0-next.5+3.sha-5f2e475
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+2.sha-aa3520e');
+const VERSION$1 = new Version('10.1.0-next.5+3.sha-5f2e475');
 
 /**
  * @license
