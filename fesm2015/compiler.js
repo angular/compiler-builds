b0efc91d4b701fc3e417041d76b43e28289b6a03
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:01:16 +0000

    d5f819ebc1 style(compiler-cli): remove unused constant (#38441)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+16.sha-1388c17
+ * @license Angular v10.1.0-next.5+17.sha-d5f819e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+16.sha-1388c17');
+const VERSION$1 = new Version('10.1.0-next.5+17.sha-d5f819e');
 
 /**
  * @license
