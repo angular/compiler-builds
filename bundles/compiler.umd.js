0577e1615a0199af1d9c8e008d0a1fcc7dd5bd9c
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:59:24 +0000

    cb83b8a887 fix(core): error if CSS custom property in host binding has number in name (#38432)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+8.sha-8bb726e
+ * @license Angular v10.0.9+9.sha-cb83b8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.9+8.sha-8bb726e');
+    var VERSION$1 = new Version('10.0.9+9.sha-cb83b8a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+8.sha-8bb726e
+ * @license Angular v10.0.9+9.sha-cb83b8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.9+8.sha-8bb726e');
+const VERSION$1 = new Version('10.0.9+9.sha-cb83b8a');
 
 /**
  * @license
