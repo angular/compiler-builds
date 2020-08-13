b04d8df18e8056efc03f802cee5e7e08785e36d6
Andrew Scott <atscott01@gmail.com>
2020-08-13 23:32:22 +0000

    ca798804b2 fix(router): export DefaultRouteReuseStrategy to Router public_api (#31575)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+18.sha-b071495
+ * @license Angular v10.1.0-next.5+19.sha-ca79880
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20298,7 +20298,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.5+18.sha-b071495');
+    var VERSION$1 = new Version('10.1.0-next.5+19.sha-ca79880');
 
     /**
      * @license
