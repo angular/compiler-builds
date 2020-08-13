6d061badb3e2c783c71196d595894812a65bb22c
Andrew Scott <atscott01@gmail.com>
2020-08-13 18:55:44 +0000

    b769771d60 refactor(router): Add annotations to correct Router documentation (#38448)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+10.sha-a80f654
+ * @license Angular v10.1.0-next.5+11.sha-b769771
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+10.sha-a80f654');
+const VERSION$1 = new Version('10.1.0-next.5+11.sha-b769771');
 
 /**
  * @license
