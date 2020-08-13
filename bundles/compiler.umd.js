9ea7f52f6f7b09d04c0de0e02991b32b65c711f3
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:01:09 +0000

    d5e09f4d62 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+14.sha-692e34d
+ * @license Angular v10.0.9+16.sha-d5e09f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.9+14.sha-692e34d');
+    var VERSION$1 = new Version('10.0.9+16.sha-d5e09f4');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+14.sha-692e34d
+ * @license Angular v10.0.9+16.sha-d5e09f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.9+14.sha-692e34d');
+const VERSION$1 = new Version('10.0.9+16.sha-d5e09f4');
 
 /**
  * @license
