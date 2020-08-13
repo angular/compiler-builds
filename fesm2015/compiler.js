1bd7dc19e35b604abe03ed139614ca05bb709e65
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:57:31 +0000

    a80f654af9 fix(core): error if CSS custom property in host binding has number in name (#38432)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+9.sha-aa847cb
+ * @license Angular v10.1.0-next.5+10.sha-a80f654
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+9.sha-aa847cb');
+const VERSION$1 = new Version('10.1.0-next.5+10.sha-a80f654');
 
 /**
  * @license
