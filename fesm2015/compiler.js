9e4385fe248bf16f7b8c84276dc05f148161c50c
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:08:38 +0000

    945751e2e8 ci: disable closure size tracking test (#38449)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+11.sha-b769771
+ * @license Angular v10.1.0-next.5+12.sha-945751e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+11.sha-b769771');
+const VERSION$1 = new Version('10.1.0-next.5+12.sha-945751e');
 
 /**
  * @license
