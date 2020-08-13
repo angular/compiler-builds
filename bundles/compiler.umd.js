cfca549cc985ae66412d6779f46a4f4c7c07e7ce
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:32:28 +0000

    8763d8201c build: update ng-dev config file for new commit message configuration (#38430)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+6.sha-773f790
+ * @license Angular v10.1.0-next.5+8.sha-8763d82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20298,7 +20298,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.5+6.sha-773f790');
+    var VERSION$1 = new Version('10.1.0-next.5+8.sha-8763d82');
 
     /**
      * @license
