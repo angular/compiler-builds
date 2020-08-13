6302e26ce943ba279b36613304624eb0a877bc8a
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:05:58 +0000

    aa847cb014 build: run browsers tests on chromium locally (#38435)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+8.sha-8763d82
+ * @license Angular v10.1.0-next.5+9.sha-aa847cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20298,7 +20298,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.5+8.sha-8763d82');
+    var VERSION$1 = new Version('10.1.0-next.5+9.sha-aa847cb');
 
     /**
      * @license
