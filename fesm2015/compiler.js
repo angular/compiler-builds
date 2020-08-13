520a4c082618131778fb0690dfa790a24ce039a5
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:35:10 +0000

    8763d8201c build: update ng-dev config file for new commit message configuration (#38430)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+6.sha-773f790
+ * @license Angular v10.1.0-next.5+8.sha-8763d82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19134,7 +19134,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.5+6.sha-773f790');
+const VERSION$1 = new Version('10.1.0-next.5+8.sha-8763d82');
 
 /**
  * @license
