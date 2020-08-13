05a5d36353ed03abf66ae26f5f854396379ba32b
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:31:44 +0000

    773f7908c0 feat(dev-infra): update to latest benchpress version (#38440)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+5.sha-f4ced74
+ * @license Angular v10.1.0-next.5+6.sha-773f790
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20298,7 +20298,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.5+5.sha-f4ced74');
+    var VERSION$1 = new Version('10.1.0-next.5+6.sha-773f790');
 
     /**
      * @license
