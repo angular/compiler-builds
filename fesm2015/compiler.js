c5427aa365d9f1b46c0ef0bace2502194cab5c17
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:05:31 +0000

    b32126c335 fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+16.sha-d5e09f4
+ * @license Angular v10.0.9+17.sha-b32126c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.9+16.sha-d5e09f4');
+const VERSION$1 = new Version('10.0.9+17.sha-b32126c');
 
 /**
  * @license
