e5a7a51f15a7ed252b76461b8ea34e4715a3667c
Andrew Scott <atscott@google.com>
2020-08-17 18:50:57 +0000

    cfe424e875 refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+26.sha-3b9c802
+ * @license Angular v10.1.0-next.5+27.sha-cfe424e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20316,7 +20316,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.5+26.sha-3b9c802');
+    var VERSION$1 = new Version('10.1.0-next.5+27.sha-cfe424e');
 
     /**
      * @license
