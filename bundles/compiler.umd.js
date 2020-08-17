18bda4f70faaa68722ea99672d3fabc65065117e
atscott <atscott01@gmail.com>
2020-08-17 20:40:42 +0000

    3e80f0e526 release: cut the v10.0.10 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+21.sha-f3dd6c2
+ * @license Angular v10.0.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.9+21.sha-f3dd6c2');
+    var VERSION$1 = new Version('10.0.10');
 
     /**
      * @license
