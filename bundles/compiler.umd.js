ec62fdbbc3eaad4f66fe72d51d3d92bee53a82e5
Andrew Scott <atscott@google.com>
2020-08-17 20:14:21 +0000

    f3dd6c224c fix(ngcc): detect synthesized delegate constructors for downleveled ES2015 classes (#38500)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+18.sha-84d1ba7
+ * @license Angular v10.0.9+21.sha-f3dd6c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.9+18.sha-84d1ba7');
+    var VERSION$1 = new Version('10.0.9+21.sha-f3dd6c2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+18.sha-84d1ba7
+ * @license Angular v10.0.9+21.sha-f3dd6c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.9+18.sha-84d1ba7');
+const VERSION$1 = new Version('10.0.9+21.sha-f3dd6c2');
 
 /**
  * @license
