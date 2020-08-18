b5ad506acb6c8565bda3a3fd936e70e300c7476d
Andrew Scott <atscott@google.com>
2020-08-18 17:43:40 +0000

    dbfb50e9f4 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19152,7 +19152,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.6+4.sha-723a9ff');
+const VERSION$1 = new Version('10.1.0-next.6+6.sha-dbfb50e');
 
 /**
  * @license
