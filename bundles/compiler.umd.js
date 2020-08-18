8e1d563fd9637d32d9c731388c2f8620028f097b
Andrew Scott <atscott@google.com>
2020-08-18 19:18:04 +0000

    aaa1d8e2fe release: cut the zone.js-0.11.0 release (#38473)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+6.sha-dbfb50e
+ * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20316,7 +20316,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.6+6.sha-dbfb50e');
+    var VERSION$1 = new Version('10.1.0-next.6+7.sha-aaa1d8e');
 
     /**
      * @license
