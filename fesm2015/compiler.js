6022eb5c1e5166615559a25758c1e0fca58da63d
Andrew Scott <atscott@google.com>
2020-08-18 23:47:48 +0000

    dcc3f6d74d feat(dev-infra): tooling to check out pending PR (#38474)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.10+6.sha-0af9533
+ * @license Angular v10.0.10+7.sha-dcc3f6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.10+6.sha-0af9533');
+const VERSION$1 = new Version('10.0.10+7.sha-dcc3f6d');
 
 /**
  * @license
