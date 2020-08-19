d9d12a22dde1d6f2a60ce77d4c6b0db9185adeff
Andrew Scott <atscott@google.com>
2020-08-19 15:52:32 +0000

    55fd725e74 docs: Fix typo in the inputs and outputs guide (#38524)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+9.sha-f77fd5e
+ * @license Angular v10.1.0-next.6+10.sha-55fd725
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19152,7 +19152,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.6+9.sha-f77fd5e');
+const VERSION$1 = new Version('10.1.0-next.6+10.sha-55fd725');
 
 /**
  * @license
