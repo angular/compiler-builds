d948dc05608578d8413654b8094b2083010d7422
Misko Hevery <misko@hevery.com>
2020-08-19 21:42:06 +0000

    552853648c fix(core): remove closing body tag from inert DOM builder (#38454)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11
+ * @license Angular v10.0.11+5.sha-5528536
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.11');
+const VERSION$1 = new Version('10.0.11+5.sha-5528536');
 
 /**
  * @license
