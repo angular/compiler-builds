fee5a3d0b6718136da53487067bf03d717232844
Misko Hevery <misko@hevery.com>
2020-08-20 04:25:49 +0000

    87bbf69ce8 Revert "fix(core): remove closing body tag from inert DOM builder (#38454)"

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+5.sha-5528536
+ * @license Angular v10.0.11+6.sha-87bbf69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.11+5.sha-5528536');
+    var VERSION$1 = new Version('10.0.11+6.sha-87bbf69');
 
     /**
      * @license
