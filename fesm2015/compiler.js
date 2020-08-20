03498ccf2224e62b2d152d4ada83d9214a6660c3
Misko Hevery <misko@hevery.com>
2020-08-20 20:28:50 +0000

    7f2a3e4b6b docs: apply code styling in template reference variables guide (#38522)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+10.sha-1775f35
+ * @license Angular v10.0.11+11.sha-7f2a3e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.11+10.sha-1775f35');
+const VERSION$1 = new Version('10.0.11+11.sha-7f2a3e4');
 
 /**
  * @license
