5e10d60fc25bd45a36edbea91068542c6a893db8
Misko Hevery <misko@hevery.com>
2020-08-21 19:42:12 +0000

    2a643e1ab6 docs: change function name from async -> waitForAsync (#38548)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+14.sha-956b25a
+ * @license Angular v10.1.0-next.7+16.sha-2a643e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20316,7 +20316,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.7+14.sha-956b25a');
+    var VERSION$1 = new Version('10.1.0-next.7+16.sha-2a643e1');
 
     /**
      * @license
