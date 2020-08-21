cadda87af1ab9261a0bf7fc86c1df3707f00a2ac
Misko Hevery <misko@hevery.com>
2020-08-21 19:47:41 +0000

    814b43647d fix(compiler-cli): adding references to const enums in runtime code (#38542)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+11.sha-7f2a3e4
+ * @license Angular v10.0.11+13.sha-814b436
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19127,7 +19127,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.11+11.sha-7f2a3e4');
+const VERSION$1 = new Version('10.0.11+13.sha-814b436');
 
 /**
  * @license
