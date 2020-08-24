37aa8008f644ed32e0462acbbf880edd6e83f970
Misko Hevery <misko@hevery.com>
2020-08-24 16:54:39 +0000

    b48cc6ead5 feat(language-service): introduce hybrid visitor to locate AST node (#38540)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+22.sha-201a546
+ * @license Angular v10.1.0-next.7+19.sha-b48cc6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20459,7 +20459,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-next.7+22.sha-201a546');
+    var VERSION$1 = new Version('10.1.0-next.7+19.sha-b48cc6e');
 
     /**
      * @license
