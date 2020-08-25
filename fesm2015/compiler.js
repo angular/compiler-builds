5a94ac1cd8b4c57ee9ec22c1f6d037b5bcaebc08
Misko Hevery <misko@hevery.com>
2020-08-25 22:38:42 +0000

    f0af387f6c fix(localize): ensure required XLIFF parameters are serialized (#38575)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+12.sha-841dfa6
+ * @license Angular v10.1.0-next.8+11.sha-f0af387
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-next.8+12.sha-841dfa6');
+const VERSION$1 = new Version('10.1.0-next.8+11.sha-f0af387');
 
 /**
  * @license
