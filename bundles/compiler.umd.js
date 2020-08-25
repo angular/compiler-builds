e50dfe83ff274c21f5a6d55d4c8fa98fc4d22c2a
Misko Hevery <misko@hevery.com>
2020-08-25 17:16:38 +0000

    d06636848e docs: add correct path for karma.conf.js file (#38571)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12
+ * @license Angular v10.0.12+3.sha-d066368
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.12');
+    var VERSION$1 = new Version('10.0.12+3.sha-d066368');
 
     /**
      * @license
