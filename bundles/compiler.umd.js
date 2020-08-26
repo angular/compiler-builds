f7f5ca80122c5a77c87e066d6ec827971db366f8
Misko Hevery <misko@hevery.com>
2020-08-26 19:14:28 +0000

    7d8dce11c0 release: cut the v10.0.14 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12+4.sha-9168919
+ * @license Angular v10.0.14
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.12+4.sha-9168919');
+    var VERSION$1 = new Version('10.0.14');
 
     /**
      * @license
