0a19efa58b6f8c32df0c81212745dd40d4359cbd
Joey Perrott <josephperrott@gmail.com>
2020-08-27 16:26:54 +0000

    164cd274a4 test(docs-infra): add commands to run the unit tests (#34537)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+2.sha-4aee008
+ * @license Angular v10.1.0-rc.0+5.sha-164cd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+2.sha-4aee008');
+const VERSION$1 = new Version('10.1.0-rc.0+5.sha-164cd27');
 
 /**
  * @license
