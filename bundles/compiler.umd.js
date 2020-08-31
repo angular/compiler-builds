8f0c438fa348a2b6affe61f179af02ade40eb20c
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:57:36 +0000

    9a649809ca refactor(forms): remove extra space in error message (#38637)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+19.sha-4e8592e
+ * @license Angular v10.0.14+21.sha-9a64980
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20291,7 +20291,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.0.14+19.sha-4e8592e');
+    var VERSION$1 = new Version('10.0.14+21.sha-9a64980');
 
     /**
      * @license
