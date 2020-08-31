10fbc2b7408baf750447ae629bce7e1cb512f8c1
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:05:48 +0000

    827ba05914 docs: remove first person and space in CircleCI in the testing guide. (#38631)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
+ * @license Angular v10.1.0-rc.0+19.sha-827ba05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+17.sha-5d5caf2');
+const VERSION$1 = new Version('10.1.0-rc.0+19.sha-827ba05');
 
 /**
  * @license
