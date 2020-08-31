4e15c37525c922e16aec62cd21acdaf24cb4f1f2
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:09:08 +0000

    6b1a505566 feat(dev-infra): write outputs of command runs to ng-dev log file (#38599)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+6.sha-dd82f2f
+ * @license Angular v10.1.0-rc.0+12.sha-6b1a505
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+6.sha-dd82f2f');
+const VERSION$1 = new Version('10.1.0-rc.0+12.sha-6b1a505');
 
 /**
  * @license
