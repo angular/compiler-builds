de441d5ffd1403953dc9ec32274d88f77192babd
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:56:22 +0000

    5e4aeaa348 refactor(dev-infra): use a mixin to require a `github-token` for an ng-dev command (#38630)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-cbbf8b5
+ * @license Angular v10.1.0-rc.0+16.sha-5e4aeaa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+15.sha-cbbf8b5');
+const VERSION$1 = new Version('10.1.0-rc.0+16.sha-5e4aeaa');
 
 /**
  * @license
