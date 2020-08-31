473a89dd12f01ef54ae4df38dd24bcd6c4a73a94
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:06:12 +0000

    52c7a4bfc6 docs: ng generate module command doc change (#38480)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+18.sha-b2857b4
+ * @license Angular v10.1.0-rc.0+20.sha-52c7a4b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20459,7 +20459,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.0-rc.0+18.sha-b2857b4');
+    var VERSION$1 = new Version('10.1.0-rc.0+20.sha-52c7a4b');
 
     /**
      * @license
