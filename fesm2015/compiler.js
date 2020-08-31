5e3082235aecd061ced9046221089f72b47042f2
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:52:18 +0000

    0a7a5e3aff docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
+ * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19286,7 +19286,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+22.sha-ebc0e46');
+const VERSION$1 = new Version('10.1.0-rc.0+24.sha-0a7a5e3');
 
 /**
  * @license
