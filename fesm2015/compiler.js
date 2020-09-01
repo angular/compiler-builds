4f15a053dc0fc3ba58053bf1dad506847f298acd
Joey Perrott <josephperrott@gmail.com>
2020-09-01 00:32:08 +0000

    626ac45482 docs: Restructure table of contents to provide a more streamlined experience (#38658)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+24.sha-a938180
+ * @license Angular v10.0.14+25.sha-626ac45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19146,7 +19146,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.0.14+24.sha-a938180');
+const VERSION$1 = new Version('10.0.14+25.sha-626ac45');
 
 /**
  * @license
