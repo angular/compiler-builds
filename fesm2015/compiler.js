7ea23fa711fb89e248ca3bbd21c2459204341179
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:36:09 +0000

    e6ee7c2aeb release: cut the v11.0.0-next.0 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+37.sha-59c234c
+ * @license Angular v11.0.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19305,7 +19305,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0-rc.0+37.sha-59c234c');
+const VERSION$1 = new Version('11.0.0-next.0');
 
 /**
  * @license
