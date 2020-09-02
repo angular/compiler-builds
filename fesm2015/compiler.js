5946f77d2bad963a7a280d41040ea3be492b9b7f
atscott <atscott01@gmail.com>
2020-09-02 23:00:35 +0000

    6aac499ee7 docs: Restructure table of contents to provide a more streamlined experience (#38689)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+2.sha-32f33f0
+ * @license Angular v10.1.0+3.sha-6aac499
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19305,7 +19305,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0+2.sha-32f33f0');
+const VERSION$1 = new Version('10.1.0+3.sha-6aac499');
 
 /**
  * @license
