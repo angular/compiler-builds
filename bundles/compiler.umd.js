2c5aedca21a96420a92ae86837023b3bf9693c74
atscott <atscott01@gmail.com>
2020-09-03 19:21:23 +0000

    1c7e5cef3e docs: add dayjs date adapter to resources page (#38031)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+8.sha-44bb85a
+ * @license Angular v11.0.0-next.0+10.sha-1c7e5ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20483,7 +20483,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.0+8.sha-44bb85a');
+    var VERSION$1 = new Version('11.0.0-next.0+10.sha-1c7e5ce');
 
     /**
      * @license
