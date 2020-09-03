6039c02e41ea24a6e1373a247205b46aad5b8f3f
atscott <atscott01@gmail.com>
2020-09-03 17:06:47 +0000

    2cb3d58b42 docs(dev-infra): fix typo in comment (arguements --> arguments) (#38653)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+7.sha-50f4d8a
+ * @license Angular v11.0.0-next.0+9.sha-2cb3d58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19304,7 +19304,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.0+7.sha-50f4d8a');
+const VERSION$1 = new Version('11.0.0-next.0+9.sha-2cb3d58');
 
 /**
  * @license
