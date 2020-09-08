7f972d4e15e5edb06b9e17b9b9aceb8ef5002fae
atscott <atscott01@gmail.com>
2020-09-08 17:29:29 +0000

    a29f9a9fb3 fix(dev-infra): change logging of commit message restoration to debug (#38704)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+17.sha-3e57ca1
+ * @license Angular v11.0.0-next.0+23.sha-a29f9a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20478,7 +20478,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.0+17.sha-3e57ca1');
+    var VERSION$1 = new Version('11.0.0-next.0+23.sha-a29f9a9');
 
     /**
      * @license
