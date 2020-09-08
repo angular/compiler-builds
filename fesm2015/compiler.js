5bd8aa7b38184b69b00e072d011370c6001f0a31
atscott <atscott01@gmail.com>
2020-09-08 23:00:00 +0000

    e156e29edd perf(compiler-cli): optimize computation of type-check scope information (#38749)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+34.sha-190dca0
+ * @license Angular v10.1.0+36.sha-e156e29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19331,7 +19331,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.0+34.sha-190dca0');
+const VERSION$1 = new Version('10.1.0+36.sha-e156e29');
 
 /**
  * @license
