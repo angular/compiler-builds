69499b02d84a95be6c9870e4cb7e5288fa1c8c37
atscott <atscott01@gmail.com>
2020-09-08 17:26:44 +0000

    a29f9a9fb3 fix(dev-infra): change logging of commit message restoration to debug (#38704)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+17.sha-3e57ca1
+ * @license Angular v11.0.0-next.0+23.sha-a29f9a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19299,7 +19299,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.0+17.sha-3e57ca1');
+const VERSION$1 = new Version('11.0.0-next.0+23.sha-a29f9a9');
 
 /**
  * @license
