d0ff46c6ff986821ccc766b1016e7317b11c50f3
atscott <atscott01@gmail.com>
2020-09-08 17:31:06 +0000

    9f28f82598 docs: add space between icon and text in issue template (#38712)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+16.sha-c2d017d
+ * @license Angular v11.0.0-next.0+22.sha-9f28f82
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19299,7 +19299,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.0+16.sha-c2d017d');
+const VERSION$1 = new Version('11.0.0-next.0+22.sha-9f28f82');
 
 /**
  * @license
