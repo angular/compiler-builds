926ac9f343e666f3b663dcc52c6fe6c207162a6d
Andrew Scott <atscott@google.com>
2020-09-08 16:35:53 +0000

    73001b42fe docs: remove reverted bug fix from 10.1 change log (#38718)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+12.sha-c90eb54
+ * @license Angular v11.0.0-next.0+13.sha-73001b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20478,7 +20478,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.0+12.sha-c90eb54');
+    var VERSION$1 = new Version('11.0.0-next.0+13.sha-73001b4');
 
     /**
      * @license
