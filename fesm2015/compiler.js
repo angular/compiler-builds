ed13d95315fae5fda60a37f8b17bf51917a0203a
atscott <atscott01@gmail.com>
2020-09-09 23:44:46 +0000

    c6ebb77cec test(language-service): [ivy] remove all markers from test (#38777)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+2.sha-f942118
+ * @license Angular v11.0.0-next.1+1.sha-c6ebb77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19351,7 +19351,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+2.sha-f942118');
+const VERSION$1 = new Version('11.0.0-next.1+1.sha-c6ebb77');
 
 /**
  * @license
