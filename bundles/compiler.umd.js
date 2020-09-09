fd0e958d0734a396adfc459bcd254dc84b4e6fa8
atscott <atscott01@gmail.com>
2020-09-09 16:32:25 +0000

    ded075a79c docs: add missing space (#38759)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+51.sha-6d9bfb8
+ * @license Angular v11.0.0-next.0+46.sha-ded075a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20534,7 +20534,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.0+51.sha-6d9bfb8');
+    var VERSION$1 = new Version('11.0.0-next.0+46.sha-ded075a');
 
     /**
      * @license
