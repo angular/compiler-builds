1a2a2e6525ef148ad726c4721595f643cffa86b5
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:22:20 +0000

    26f28200bf fix(common): do not round up fractions of a millisecond in `DatePipe` (#38009)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+12.sha-ce1efc1
+ * @license Angular v11.0.0-next.1+13.sha-26f2820
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19351,7 +19351,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+12.sha-ce1efc1');
+const VERSION$1 = new Version('11.0.0-next.1+13.sha-26f2820');
 
 /**
  * @license
