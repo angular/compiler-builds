4f4fa647f046a480ebca18906105f5b5d843175f
Andrew Kushnir <akushnir@google.com>
2020-09-10 17:02:36 +0000

    9078187378 docs: add note about only one label target being allowed to be applied to a PR (#38793)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+9.sha-edb7f90
+ * @license Angular v10.1.1+11.sha-9078187
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19331,7 +19331,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.1+9.sha-edb7f90');
+const VERSION$1 = new Version('10.1.1+11.sha-9078187');
 
 /**
  * @license
