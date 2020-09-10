c3d6d685906f916202a503f297a635193c4f6b16
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:57:33 +0000

    6acea54f62 fix(common): mark locale data arrays as readonly (#30397)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+22.sha-2d52c80
+ * @license Angular v11.0.0-next.1+23.sha-6acea54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+22.sha-2d52c80');
+const VERSION$1 = new Version('11.0.0-next.1+23.sha-6acea54');
 
 /**
  * @license
