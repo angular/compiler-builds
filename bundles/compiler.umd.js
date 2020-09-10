52bc4e1f6300f4a16a8d1f22e58c5595ea32f143
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:17:23 +0000

    ce1efc1af2 fix(localize): ensure that `formatOptions` is optional (#38787)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+11.sha-d141516
+ * @license Angular v11.0.0-next.1+12.sha-ce1efc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20534,7 +20534,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.1+11.sha-d141516');
+    var VERSION$1 = new Version('11.0.0-next.1+12.sha-ce1efc1');
 
     /**
      * @license
