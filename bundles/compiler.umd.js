78bb612d67c529b52ad123e6dc7c88ef0271b438
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:44:48 +0000

    57c442f930 build(router): update symbols for routing app (#38817)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+27.sha-15207e3
+ * @license Angular v11.0.0-next.1+29.sha-57c442f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20536,7 +20536,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.1+27.sha-15207e3');
+    var VERSION$1 = new Version('11.0.0-next.1+29.sha-57c442f');
 
     /**
      * @license
