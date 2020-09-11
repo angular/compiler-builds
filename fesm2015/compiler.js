983f3415c420f4debd219ecd03fbfb06ef90a4f3
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:21:34 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19331,7 +19331,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.1+18.sha-b05d79d');
+const VERSION$1 = new Version('10.1.1+19.sha-aa43cbf');
 
 /**
  * @license
