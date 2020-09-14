6651ff84550852ee448617f2c6030c873aef8fc7
Andrew Kushnir <akushnir@google.com>
2020-09-14 20:29:25 +0000

    ebede67433 perf(compiler-cli): optimize computation of type-check scope information (#38843)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+20.sha-c62d1cb
+ * @license Angular v10.1.1+22.sha-ebede67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20508,7 +20508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.1+20.sha-c62d1cb');
+    var VERSION$1 = new Version('10.1.1+22.sha-ebede67');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+20.sha-c62d1cb
+ * @license Angular v10.1.1+22.sha-ebede67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19331,7 +19331,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.1+20.sha-c62d1cb');
+const VERSION$1 = new Version('10.1.1+22.sha-ebede67');
 
 /**
  * @license
