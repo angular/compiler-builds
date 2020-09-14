0acf32d503bb0ec4d0c8e7fa3e4c5c69f93ce6bd
Andrew Kushnir <akushnir@google.com>
2020-09-14 19:16:40 +0000

    297c060ae7 perf(compiler-cli): optimize computation of type-check scope information (#38539)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+32.sha-e162da0
+ * @license Angular v11.0.0-next.1+34.sha-297c060
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20536,7 +20536,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.1+32.sha-e162da0');
+    var VERSION$1 = new Version('11.0.0-next.1+34.sha-297c060');
 
     /**
      * @license
