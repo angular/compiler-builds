e3c0e3abf393968cca56f673a6f7932078186c2a
Andrew Kushnir <akushnir@google.com>
2020-09-14 16:02:25 +0000

    c62d1cb80a refactor(dev-infra): do not validate config file multiple times (#38808)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+19.sha-aa43cbf
+ * @license Angular v10.1.1+20.sha-c62d1cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20508,7 +20508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.1+19.sha-aa43cbf');
+    var VERSION$1 = new Version('10.1.1+20.sha-c62d1cb');
 
     /**
      * @license
