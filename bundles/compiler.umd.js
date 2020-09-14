27c8f6f0b325ad45c1c1c02abfbb54884565efc3
Andrew Kushnir <akushnir@google.com>
2020-09-14 15:55:43 +0000

    e162da0753 refactor(dev-infra): do not validate config file multiple times (#38808)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+30.sha-6768fe9
+ * @license Angular v11.0.0-next.1+32.sha-e162da0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20536,7 +20536,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.1+30.sha-6768fe9');
+    var VERSION$1 = new Version('11.0.0-next.1+32.sha-e162da0');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+30.sha-6768fe9
+ * @license Angular v11.0.0-next.1+32.sha-e162da0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+30.sha-6768fe9');
+const VERSION$1 = new Version('11.0.0-next.1+32.sha-e162da0');
 
 /**
  * @license
