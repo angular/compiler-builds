2b90b91dd667c7f20164fde7d21790355ed69da1
Andrew Kushnir <akushnir@google.com>
2020-09-15 00:14:54 +0000

    dc4f85888e build: create temporary script for symbol extractor tests (#38819)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+24.sha-26deef2
+ * @license Angular v10.1.1+26.sha-dc4f858
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19331,7 +19331,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.1+24.sha-26deef2');
+const VERSION$1 = new Version('10.1.1+26.sha-dc4f858');
 
 /**
  * @license
