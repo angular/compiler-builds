ed0668db90ca6ae041c622d56dfb696cc07a0a9d
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:07:38 +0000

    e00535a2a4 fix(dev-infra): remove ANSI escape codes from log file outputs (#38792)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+38.sha-593bd59
+ * @license Angular v11.0.0-next.1+39.sha-e00535a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+38.sha-593bd59');
+const VERSION$1 = new Version('11.0.0-next.1+39.sha-e00535a');
 
 /**
  * @license
