840808aa231d38344c66904d7d4966f382d2eec1
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:11:51 +0000

    b9ca6d20cc feat(dev-infra): include CI status check in the caretaker check (#38779)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+39.sha-e00535a
+ * @license Angular v11.0.0-next.1+40.sha-b9ca6d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.1+39.sha-e00535a');
+const VERSION$1 = new Version('11.0.0-next.1+40.sha-b9ca6d2');
 
 /**
  * @license
