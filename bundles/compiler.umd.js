12ec1756991987715e39d2f9e788ac5519fbe80b
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:30:00 +0000

    3d94919800 refactor(dev-infra): refactor commit-message files (#38845)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+32.sha-af3b401
+ * @license Angular v10.1.1+33.sha-3d94919
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20508,7 +20508,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.1+32.sha-af3b401');
+    var VERSION$1 = new Version('10.1.1+33.sha-3d94919');
 
     /**
      * @license
