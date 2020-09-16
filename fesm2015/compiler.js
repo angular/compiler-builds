ba3976ff9abd452194d1a5437a8d4b036dbdae2a
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:42:04 +0000

    e62a918542 docs(forms): fix grammar in first sentence of reset function docs (#38872)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+7.sha-7849fdd
+ * @license Angular v11.0.0-next.2+9.sha-e62a918
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.2+7.sha-7849fdd');
+const VERSION$1 = new Version('11.0.0-next.2+9.sha-e62a918');
 
 /**
  * @license
