5bf7507cfd1a3d146403ba078800e1e47de99e80
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:54:02 +0000

    129107191c refactor(compiler): always return a mutable clone from `Scope#resolve` (#38857)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+13.sha-dd8d8c8
+ * @license Angular v11.0.0-next.2+12.sha-1291071
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.2+13.sha-dd8d8c8');
+const VERSION$1 = new Version('11.0.0-next.2+12.sha-1291071');
 
 /**
  * @license
