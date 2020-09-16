44d5e972e4f86cea969ae9a363f6b1ef967b5aa5
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:36:53 +0000

    44074499dc test(docs-infra): improve typeahead example and add unit test (#34190)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2
+ * @license Angular v11.0.0-next.2+2.sha-4407449
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.2');
+const VERSION$1 = new Version('11.0.0-next.2+2.sha-4407449');
 
 /**
  * @license
