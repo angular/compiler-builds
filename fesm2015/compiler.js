cf6873481317db57420490a55bb3f3e11b977de7
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:48:21 +0000

    722699fb0c fix(dev-infra): handle no caretaker config being defined (#38862)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+8.sha-49ee90b
+ * @license Angular v11.0.0-next.2+11.sha-722699f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19353,7 +19353,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.2+8.sha-49ee90b');
+const VERSION$1 = new Version('11.0.0-next.2+11.sha-722699f');
 
 /**
  * @license
