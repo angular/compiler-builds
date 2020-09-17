6e47f08827a62396e1ddc8ed73d991eb758e87b7
Misko Hevery <misko@hevery.com>
2020-09-17 16:57:37 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20536,7 +20536,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+12.sha-1291071');
+    var VERSION$1 = new Version('11.0.0-next.2+15.sha-a33d630');
 
     /**
      * @license
