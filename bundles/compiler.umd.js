b205a0a78c20304cc1e116fe66088d60794cc112
Misko Hevery <misko@hevery.com>
2020-09-19 00:52:05 +0000

    d5ddb9f340 docs: Grammar fixes (#38900)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+30.sha-a91f0f6
+ * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20526,7 +20526,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+30.sha-a91f0f6');
+    var VERSION$1 = new Version('11.0.0-next.2+29.sha-d5ddb9f');
 
     /**
      * @license
