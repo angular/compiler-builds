0ebd62d9b08eb34030a78f1632454be98a7c8f0a
Misko Hevery <misko@hevery.com>
2020-09-21 23:55:56 +0000

    145ab3d7e0 docs: Update titles to getting started topics (#38887)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+48.sha-3082f73
+ * @license Angular v11.0.0-next.2+49.sha-145ab3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20542,7 +20542,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+48.sha-3082f73');
+    var VERSION$1 = new Version('11.0.0-next.2+49.sha-145ab3d');
 
     /**
      * @license
