631c30aa4b1a0df40d0d3e1424a9c6cfee8ca608
Misko Hevery <misko@hevery.com>
2020-09-21 23:54:16 +0000

    3082f7378b test(compiler-cli): make typescript_ast_factory_spec tests resilient to line-endings (#38925)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+47.sha-297b123
+ * @license Angular v11.0.0-next.2+48.sha-3082f73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20542,7 +20542,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+47.sha-297b123');
+    var VERSION$1 = new Version('11.0.0-next.2+48.sha-3082f73');
 
     /**
      * @license
