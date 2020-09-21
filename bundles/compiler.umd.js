3e9aaa8055abfdecefedfd3bdaffaf32572b3d43
Misko Hevery <misko@hevery.com>
2020-09-21 19:54:38 +0000

    0c0c54d615 refactor(compiler): simplify visitor logic for attributes (#38899)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+33.sha-dfbfabc
+ * @license Angular v11.0.0-next.2+34.sha-0c0c54d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20526,7 +20526,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+33.sha-dfbfabc');
+    var VERSION$1 = new Version('11.0.0-next.2+34.sha-0c0c54d');
 
     /**
      * @license
