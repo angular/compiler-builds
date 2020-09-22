605a29c25a9a4f34c80bd5527adfdb31f9ae006e
Misko Hevery <misko@hevery.com>
2020-09-22 22:37:03 +0000

    75610505c6 refactor(zone.js): remove usages of blacklist related to UNPATCHED_EVENTS (#38930)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+54.sha-b168252
+ * @license Angular v11.0.0-next.2+53.sha-7561050
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20583,7 +20583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.2+54.sha-b168252');
+    var VERSION$1 = new Version('11.0.0-next.2+53.sha-7561050');
 
     /**
      * @license
