baed315d884c00f8da9b11f7661cd57478e3998d
Misko Hevery <misko@hevery.com>
2020-09-22 22:35:31 +0000

    b1682526dd refactor(dev-infra): simplify runBenchmark (#38941)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+52.sha-ba3f4c2
+ * @license Angular v11.0.0-next.2+54.sha-b168252
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.2+52.sha-ba3f4c2');
+const VERSION$1 = new Version('11.0.0-next.2+54.sha-b168252');
 
 /**
  * @license
