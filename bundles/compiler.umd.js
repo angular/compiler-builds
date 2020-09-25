109a56e7423b478284992c0ef0be6017c20a20f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:53:13 +0000

    cf9176eb46 docs(docs-infra): remove usage of whitelist in aio-builds-setup docs (#38964)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+18.sha-2f6777d
+ * @license Angular v11.0.0-next.3+19.sha-cf9176e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20583,7 +20583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.3+18.sha-2f6777d');
+    var VERSION$1 = new Version('11.0.0-next.3+19.sha-cf9176e');
 
     /**
      * @license
