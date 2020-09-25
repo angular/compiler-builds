09a74546ac3ba389fd2ad899072cdf3ebee06f7e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:58:42 +0000

    323be39297 fix(language-service): hybrid visitor returns parent node of BoundAttribute (#38995)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+20.sha-239968d
+ * @license Angular v11.0.0-next.3+22.sha-323be39
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+20.sha-239968d');
+const VERSION$1 = new Version('11.0.0-next.3+22.sha-323be39');
 
 /**
  * @license
