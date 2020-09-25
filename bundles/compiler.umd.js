4b5b6dda10c43f103a30804b5711d39679821738
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:32 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20583,7 +20583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.3+17.sha-4ca1c73');
+    var VERSION$1 = new Version('11.0.0-next.3+14.sha-e790c85');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+17.sha-4ca1c73');
+const VERSION$1 = new Version('11.0.0-next.3+14.sha-e790c85');
 
 /**
  * @license
