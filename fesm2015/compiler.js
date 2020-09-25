b7b88d1644d44744b8190aaae3f6783d929ab92a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 15:04:34 +0000

    e4f4d18e7e refactor(router): Adjust type of parameter in navigateByUrl and createUrlTree to be more accurate (#38227)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+10.sha-837889f
+ * @license Angular v11.0.0-next.3+11.sha-e4f4d18
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19407,7 +19407,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+10.sha-837889f');
+const VERSION$1 = new Version('11.0.0-next.3+11.sha-e4f4d18');
 
 /**
  * @license
