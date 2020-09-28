3830e7f8eb16a2c9540604c25e5bbbcf2a2d0b6a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:56:13 +0000

    3ba97ab391 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+65.sha-c5e1789
+ * @license Angular v11.0.0-next.3+77.sha-3ba97ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+65.sha-c5e1789');
+const VERSION$1 = new Version('11.0.0-next.3+77.sha-3ba97ab');
 
 /**
  * @license
