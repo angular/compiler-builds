3a2fe05e99c2ad2f572b618d1c64776a627ac3cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:51:41 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+63.sha-4a9f452');
+const VERSION$1 = new Version('11.0.0-next.3+69.sha-4fd635a');
 
 /**
  * @license
