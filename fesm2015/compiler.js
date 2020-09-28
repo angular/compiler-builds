cf7db9966ff0a08c3a3ec3ea81cf251edd7ebf5f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:49:39 +0000

    914d7099ee build: remove usage of blacklist in benchmark tooling (#38926)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+64.sha-cb7164a
+ * @license Angular v11.0.0-next.3+62.sha-914d709
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+64.sha-cb7164a');
+const VERSION$1 = new Version('11.0.0-next.3+62.sha-914d709');
 
 /**
  * @license
