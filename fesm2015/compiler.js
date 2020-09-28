6369c0dea8aa8b9c860c2892e758765f51a68102
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:54:14 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+77.sha-3ba97ab
+ * @license Angular v11.0.0-next.3+72.sha-06525cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+77.sha-3ba97ab');
+const VERSION$1 = new Version('11.0.0-next.3+72.sha-06525cf');
 
 /**
  * @license
