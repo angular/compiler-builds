0b3e21f22e920b90778fb756554990f44876c173
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:48:24 +0000

    cb7164a236 refactor(zone.js): rename BlacklistedStackFrames to InternalZoneJsStackFrames (#38978)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+46.sha-f96dcc5
+ * @license Angular v11.0.0-next.3+64.sha-cb7164a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20585,7 +20585,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.3+46.sha-f96dcc5');
+    var VERSION$1 = new Version('11.0.0-next.3+64.sha-cb7164a');
 
     /**
      * @license
