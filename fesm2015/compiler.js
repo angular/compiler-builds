bd9d9a0e2f4c2dabfc76e010c7ce1d2abe78d7d0
Misko Hevery <misko@hevery.com>
2020-09-29 15:04:20 +0000

    69302adc02 refactor(core): Create `NodeInjectorOffset` type which better describes NodeInjector (#38707)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+49.sha-875fcfe
+ * @license Angular v10.1.3+60.sha-69302ad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19328,7 +19328,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.3+49.sha-875fcfe');
+const VERSION$1 = new Version('10.1.3+60.sha-69302ad');
 
 /**
  * @license
