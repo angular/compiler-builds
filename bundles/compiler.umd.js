c4f489ff0f04b7406b7f61a2ca63333abd06c8d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:58:55 +0000

    ddc9e8e47a refactor(compiler): refactor template symbol builder (#39047)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+92.sha-827245f
+ * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20586,7 +20586,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.3+92.sha-827245f');
+    var VERSION$1 = new Version('11.0.0-next.3+95.sha-ddc9e8e');
 
     /**
      * @license
