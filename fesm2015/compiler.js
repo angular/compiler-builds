28e965270cf94d5fc942bd26ae2f094ed57b6e42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:02:38 +0000

    31e42f0947 refactor(language-service): create getTypeDefinitionAtPosition stubs (#39050)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
+ * @license Angular v11.0.0-next.3+96.sha-31e42f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19410,7 +19410,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+95.sha-ddc9e8e');
+const VERSION$1 = new Version('11.0.0-next.3+96.sha-31e42f0');
 
 /**
  * @license
