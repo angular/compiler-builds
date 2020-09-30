372101c0182280dc2eb145918ec77cf187d08830
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 17:30:15 +0000

    a8134dcfd4 release: cut the v10.1.4 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+72.sha-a1bf0de
+ * @license Angular v10.1.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20498,7 +20498,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.3+72.sha-a1bf0de');
+    var VERSION$1 = new Version('10.1.4');
 
     /**
      * @license
