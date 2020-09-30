f3023aaeaa34daf9dc63d9d12ff31e29bb1b730f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:56:15 +0000

    2aeaedc519 docs(docs-infra): replace a comma with a period at the end of a sentence (#39034)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+87.sha-8f11b51
+ * @license Angular v11.0.0-next.3+89.sha-2aeaedc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+87.sha-8f11b51');
+const VERSION$1 = new Version('11.0.0-next.3+89.sha-2aeaedc');
 
 /**
  * @license
