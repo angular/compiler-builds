587e1c7343d3896a7691f1bff07c90179df9b2b2
Joey Perrott <josephperrott@gmail.com>
2020-09-30 22:24:27 +0000

    ea1968317e fix(router): update getRouteGuards to check if the context outlet is activated (#39049)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+10.sha-5b15e5e
+ * @license Angular v11.0.0-next.4+11.sha-ea19683
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19410,7 +19410,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+10.sha-5b15e5e');
+const VERSION$1 = new Version('11.0.0-next.4+11.sha-ea19683');
 
 /**
  * @license
