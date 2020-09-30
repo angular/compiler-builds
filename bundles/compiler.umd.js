60d726bddc2144e36e4c04a78ee048012b2a8f3c
Joey Perrott <josephperrott@gmail.com>
2020-09-30 20:55:34 +0000

    5b15e5eeae ci: remove errant comma from angular robot config (#39066)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+8.sha-79620f5
+ * @license Angular v11.0.0-next.4+10.sha-5b15e5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20586,7 +20586,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.4+8.sha-79620f5');
+    var VERSION$1 = new Version('11.0.0-next.4+10.sha-5b15e5e');
 
     /**
      * @license
