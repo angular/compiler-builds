dd3ba7d491b7f6535520aa39ca5ee72a033db77c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:00:10 +0000

    31e42f0947 refactor(language-service): create getTypeDefinitionAtPosition stubs (#39050)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+95.sha-ddc9e8e
+ * @license Angular v11.0.0-next.3+96.sha-31e42f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20586,7 +20586,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.3+95.sha-ddc9e8e');
+    var VERSION$1 = new Version('11.0.0-next.3+96.sha-31e42f0');
 
     /**
      * @license
