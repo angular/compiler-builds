af235d9c4aab755800097ad837f8f2eb4f32e5cc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:39:54 +0000

    a6971ba89a release: cut the zone.js-0.11.2 release (#38911)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+76.sha-e2997ed
+ * @license Angular v11.0.0-next.3+80.sha-a6971ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+76.sha-e2997ed');
+const VERSION$1 = new Version('11.0.0-next.3+80.sha-a6971ba');
 
 /**
  * @license
