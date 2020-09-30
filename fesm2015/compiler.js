718e68d2cd40a56d4fb07e8dafb1320291802425
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:56:35 +0000

    8b7acc4f8f refactor(compiler): Binding parser sets binding span as source span in Ivy (#39036)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+93.sha-e8084ff
+ * @license Angular v11.0.0-next.3+91.sha-8b7acc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19410,7 +19410,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.3+93.sha-e8084ff');
+const VERSION$1 = new Version('11.0.0-next.3+91.sha-8b7acc4');
 
 /**
  * @license
