2674d460d4471e9344a13516d435e08bd8ac04e3
Joey Perrott <josephperrott@gmail.com>
2020-10-01 15:54:02 +0000

    bb11fd9058 ci: change required labels for issue triage (#38932)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+5.sha-5d584b7
+ * @license Angular v10.1.4+7.sha-bb11fd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20498,7 +20498,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.4+5.sha-5d584b7');
+    var VERSION$1 = new Version('10.1.4+7.sha-bb11fd9');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+5.sha-5d584b7
+ * @license Angular v10.1.4+7.sha-bb11fd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19328,7 +19328,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.4+5.sha-5d584b7');
+const VERSION$1 = new Version('10.1.4+7.sha-bb11fd9');
 
 /**
  * @license
