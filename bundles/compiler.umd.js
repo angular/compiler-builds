46418a5aa5df3682169313b8a9776781c0a82aa3
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:56:38 +0000

    50e83e2566 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20498,7 +20498,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.4+7.sha-bb11fd9');
+    var VERSION$1 = new Version('10.1.4+14.sha-50e83e2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19328,7 +19328,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.4+7.sha-bb11fd9');
+const VERSION$1 = new Version('10.1.4+14.sha-50e83e2');
 
 /**
  * @license
