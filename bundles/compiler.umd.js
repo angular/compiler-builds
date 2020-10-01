03efc79505cc605191f55328c8854781b283e55d
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:52:24 +0000

    21213e1531 fix(dev-infra): run git fetch in quiet mode (#39068)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+12.sha-fb12c61
+ * @license Angular v11.0.0-next.4+13.sha-21213e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20586,7 +20586,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.4+12.sha-fb12c61');
+    var VERSION$1 = new Version('11.0.0-next.4+13.sha-21213e1');
 
     /**
      * @license
