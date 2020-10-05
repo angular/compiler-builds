44577827474950fbe8a7299b57e58c81ac9db092
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:30:53 +0000

    4c30f5135b docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+22.sha-6791cd7
+ * @license Angular v10.1.4+23.sha-4c30f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20580,7 +20580,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.4+22.sha-6791cd7');
+    var VERSION$1 = new Version('10.1.4+23.sha-4c30f51');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+22.sha-6791cd7
+ * @license Angular v10.1.4+23.sha-4c30f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.4+22.sha-6791cd7');
+const VERSION$1 = new Version('10.1.4+23.sha-4c30f51');
 
 /**
  * @license
