255f7219f35dba762e05b68f2247ce5071562ca3
Joey Perrott <josephperrott@gmail.com>
2020-10-05 17:45:35 +0000

    f3150223da feat(dev-infra): show CI status of all active release trains (#39067)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+38.sha-f4f1bcc
+ * @license Angular v11.0.0-next.4+39.sha-f315022
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+38.sha-f4f1bcc');
+const VERSION$1 = new Version('11.0.0-next.4+39.sha-f315022');
 
 /**
  * @license
