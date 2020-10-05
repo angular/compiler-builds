af653d496eb0eec6698643840f4ee22ea6450c7e
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:50:04 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+43.sha-82d54fe
+ * @license Angular v11.0.0-next.4+44.sha-bc69182
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+43.sha-82d54fe');
+const VERSION$1 = new Version('11.0.0-next.4+44.sha-bc69182');
 
 /**
  * @license
