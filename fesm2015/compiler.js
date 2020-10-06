c2abd7a5341c526fde246cd676d6d137ba81edb1
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:00:23 +0000

    d8c05347c3 fix(router): properly assign ExtraOptions to Router in RouterTestingModule (#39096)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+47.sha-faa81dc
+ * @license Angular v11.0.0-next.4+48.sha-d8c0534
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+47.sha-faa81dc');
+const VERSION$1 = new Version('11.0.0-next.4+48.sha-d8c0534');
 
 /**
  * @license
