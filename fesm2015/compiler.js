ca79117b2cccc7fc44e9feb1db103542372eb2e5
Joey Perrott <josephperrott@gmail.com>
2020-10-06 20:47:43 +0000

    3975dd90a6 feat(language-service): Add getDefinitionAndBoundSpan (go to definition) (#39101)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+56.sha-05672ab
+ * @license Angular v11.0.0-next.4+57.sha-3975dd9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+56.sha-05672ab');
+const VERSION$1 = new Version('11.0.0-next.4+57.sha-3975dd9');
 
 /**
  * @license
