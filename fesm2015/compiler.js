bdd19e90f5a85a834090d0a91b37159bb33bd984
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:31:06 +0000

    ee03408c44 ci: use larger resource classes for bazel builds (#39124)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+48.sha-d8c0534
+ * @license Angular v11.0.0-next.4+49.sha-ee03408
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+48.sha-d8c0534');
+const VERSION$1 = new Version('11.0.0-next.4+49.sha-ee03408');
 
 /**
  * @license
