e14650c9c61535044488c38e725cfe1d5397a7bf
Joey Perrott <josephperrott@gmail.com>
2020-10-06 18:59:14 +0000

    70e13dc31e fix(language-service): [Ivy] hybrid visitor should not locate let keyword (#39061)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+54.sha-246de9a
+ * @license Angular v11.0.0-next.4+55.sha-70e13dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+54.sha-246de9a');
+const VERSION$1 = new Version('11.0.0-next.4+55.sha-70e13dc');
 
 /**
  * @license
