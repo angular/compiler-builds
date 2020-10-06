0e5a8b5dd5125ac5d1684fc51d28d96b1bd59e60
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:02:33 +0000

    07a66fc4c2 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
+ * @license Angular v11.0.0-next.4+53.sha-07a66fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.4+52.sha-6e0a8eb');
+const VERSION$1 = new Version('11.0.0-next.4+53.sha-07a66fc');
 
 /**
  * @license
