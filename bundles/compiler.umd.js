5d04934e95953af73ad060b6321e60ffa6767a2a
atscott <atscott01@gmail.com>
2020-10-07 23:32:13 +0000

    eb34037cd7 Revert "refactor(compiler-cli): wrap RHS of property write in parens (#39143)" (#39171)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+36.sha-cc13b37
+ * @license Angular v10.1.5+2.sha-eb34037
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20580,7 +20580,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.4+36.sha-cc13b37');
+    var VERSION$1 = new Version('10.1.5+2.sha-eb34037');
 
     /**
      * @license
