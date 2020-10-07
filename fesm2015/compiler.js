fc78416db361dc8f01b86cbcbfa23bd3920cd6fe
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:57:25 +0000

    cc13b37446 build(docs-infra): render optional decorator options with a ? (#39167)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+33.sha-9f132d0
+ * @license Angular v10.1.4+36.sha-cc13b37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.4+33.sha-9f132d0');
+const VERSION$1 = new Version('10.1.4+36.sha-cc13b37');
 
 /**
  * @license
