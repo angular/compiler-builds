ffea197a9fa73336499ecdf9157581cbfa190627
Joey Perrott <josephperrott@gmail.com>
2020-10-07 20:56:17 +0000

    5fa012efb8 docs: change heading to a sub-heading (#39131)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+59.sha-f2fca6d
+ * @license Angular v11.0.0-next.4+61.sha-5fa012e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20668,7 +20668,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.4+59.sha-f2fca6d');
+    var VERSION$1 = new Version('11.0.0-next.4+61.sha-5fa012e');
 
     /**
      * @license
