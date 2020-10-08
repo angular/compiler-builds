3a679f5752f8f57732cf847c1b3dea608b79bc16
atscott <atscott01@gmail.com>
2020-10-08 16:58:40 +0000

    dace4aacb6 docs: fix for HttpRequest expansion panel (#39166)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+5.sha-63624a2
+ * @license Angular v11.0.0-next.5+6.sha-dace4aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+5.sha-63624a2');
+const VERSION$1 = new Version('11.0.0-next.5+6.sha-dace4aa');
 
 /**
  * @license
