4092270234ce623a80aa7a7316ba6418ae557cbc
atscott <atscott01@gmail.com>
2020-10-08 17:02:55 +0000

    b417dd7b9c docs: fix for HttpRequest expansion panel (#39166)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+4.sha-8cf16b4
+ * @license Angular v10.1.5+5.sha-b417dd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20580,7 +20580,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.5+4.sha-8cf16b4');
+    var VERSION$1 = new Version('10.1.5+5.sha-b417dd7');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+4.sha-8cf16b4
+ * @license Angular v10.1.5+5.sha-b417dd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.5+4.sha-8cf16b4');
+const VERSION$1 = new Version('10.1.5+5.sha-b417dd7');
 
 /**
  * @license
