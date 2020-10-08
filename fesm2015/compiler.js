0f7268ec1b9bbf13fb801b6e6b5e33e3cfbea8aa
atscott <atscott01@gmail.com>
2020-10-08 19:21:22 +0000

    0a16e60afa fix(compiler-cli): type checking of expressions within ICUs (#39072)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+13.sha-4a1c12c
+ * @license Angular v11.0.0-next.5+11.sha-0a16e60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -794,7 +794,7 @@ const createDirective = makeMetadataFactory('Directive', (dir = {}) => dir);
 var ViewEncapsulation;
 (function (ViewEncapsulation) {
     ViewEncapsulation[ViewEncapsulation["Emulated"] = 0] = "Emulated";
-    // Historically the 1 value was for `Native` encapsulation which has been removed as of v11.
+    ViewEncapsulation[ViewEncapsulation["Native"] = 1] = "Native";
     ViewEncapsulation[ViewEncapsulation["None"] = 2] = "None";
     ViewEncapsulation[ViewEncapsulation["ShadowDom"] = 3] = "ShadowDom";
 })(ViewEncapsulation || (ViewEncapsulation = {}));
@@ -19537,7 +19537,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+13.sha-4a1c12c');
+const VERSION$1 = new Version('11.0.0-next.5+11.sha-0a16e60');
 
 /**
  * @license
