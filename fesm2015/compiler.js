3fe7b467f0ec4af12b2583c7fc818092e3d665fc
Joey Perrott <josephperrott@gmail.com>
2020-10-08 18:03:30 +0000

    635543e6f6 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+3.sha-b635f25
+ * @license Angular v11.0.0-next.5+7.sha-635543e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19491,7 +19491,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+3.sha-b635f25');
+const VERSION$1 = new Version('11.0.0-next.5+7.sha-635543e');
 
 /**
  * @license
