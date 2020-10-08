1838afc24c94ead2ad78d58b659a1278246db03f
atscott <atscott01@gmail.com>
2020-10-08 19:22:21 +0000

    7628c36f49 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#39182)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+5.sha-b417dd7
+ * @license Angular v10.1.5+6.sha-7628c36
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20580,7 +20580,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.5+5.sha-b417dd7');
+    var VERSION$1 = new Version('10.1.5+6.sha-7628c36');
 
     /**
      * @license
