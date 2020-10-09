9bbd8b95f3590a167dc6b9911b559487c4db43d7
atscott <atscott01@gmail.com>
2020-10-09 15:21:50 +0000

    8913aee527 build: bump Chromium to next stable version: 84.0.4147 (#39179)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+6.sha-7628c36
+ * @license Angular v10.1.5+7.sha-8913aee
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19409,7 +19409,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.5+6.sha-7628c36');
+const VERSION$1 = new Version('10.1.5+7.sha-8913aee');
 
 /**
  * @license
