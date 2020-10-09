9929a620fb5749264d253212651d1a62a8c8a1f4
atscott <atscott01@gmail.com>
2020-10-09 17:02:21 +0000

    42be9047d8 feat(dev-infra): Add github links to caretaker checks (#39185)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+8.sha-52a0c6b
+ * @license Angular v10.1.5+9.sha-42be904
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20596,7 +20596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.5+8.sha-52a0c6b');
+    var VERSION$1 = new Version('10.1.5+9.sha-42be904');
 
     /**
      * @license
