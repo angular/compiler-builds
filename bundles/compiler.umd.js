b5bd03e493ade69bd911a5722f20dcc7236a7e6d
atscott <atscott01@gmail.com>
2020-10-09 17:25:45 +0000

    bf717b1a31 fix(elements): detect matchesSelector prototype without IIFE (#37799)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+16.sha-b2342d4
+ * @license Angular v11.0.0-next.5+18.sha-bf717b1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20730,7 +20730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.5+16.sha-b2342d4');
+    var VERSION$1 = new Version('11.0.0-next.5+18.sha-bf717b1');
 
     /**
      * @license
