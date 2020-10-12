d592e5f25d28070bb8ea9ba1c39894a90f6b0003
atscott <atscott01@gmail.com>
2020-10-12 15:32:04 +0000

    a84976fdfc feat(language-service): Add getTypeDefinitionAtPosition (go to type definition) (#39145)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+17.sha-9f3388e
+ * @license Angular v11.0.0-next.5+19.sha-a84976f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20730,7 +20730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.5+17.sha-9f3388e');
+    var VERSION$1 = new Version('11.0.0-next.5+19.sha-a84976f');
 
     /**
      * @license
