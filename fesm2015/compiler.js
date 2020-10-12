fe45ec4a1e7c2cc8361cacf103861b6d3371b825
atscott <atscott01@gmail.com>
2020-10-12 20:03:17 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+17.sha-1efc75c
+ * @license Angular v10.1.5+18.sha-3487d5a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19425,7 +19425,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.5+17.sha-1efc75c');
+const VERSION$1 = new Version('10.1.5+18.sha-3487d5a');
 
 /**
  * @license
