a17a870211a321cbeb55a05376de804ec40004a6
atscott <atscott01@gmail.com>
2020-10-12 16:15:57 +0000

    952710b43b refactor(compiler-cli): ensure `isNamed....()` helpers check name is identity (#38959)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+22.sha-e930b0c
+ * @license Angular v11.0.0-next.5+40.sha-952710b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+22.sha-e930b0c');
+const VERSION$1 = new Version('11.0.0-next.5+40.sha-952710b');
 
 /**
  * @license
