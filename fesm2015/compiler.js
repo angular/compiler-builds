05e19436d52c81fb6f990d46a02fc88a21ff04a3
atscott <atscott01@gmail.com>
2020-10-13 22:23:28 +0000

    584f37cfca updated sajee's profile (#39019)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+53.sha-b642f0b
+ * @license Angular v11.0.0-next.5+55.sha-584f37c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+53.sha-b642f0b');
+const VERSION$1 = new Version('11.0.0-next.5+55.sha-584f37c');
 
 /**
  * @license
