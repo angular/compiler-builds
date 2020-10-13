905ad277f46e6112a920ad9647b4e9ad291f2a66
atscott <atscott01@gmail.com>
2020-10-13 17:20:29 +0000

    5ce71e0fbc feat(core): add automated migration to replace async with waitForAsync (#39212)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+48.sha-437e563
+ * @license Angular v11.0.0-next.5+49.sha-5ce71e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20730,7 +20730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.5+48.sha-437e563');
+    var VERSION$1 = new Version('11.0.0-next.5+49.sha-5ce71e0');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+48.sha-437e563
+ * @license Angular v11.0.0-next.5+49.sha-5ce71e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+48.sha-437e563');
+const VERSION$1 = new Version('11.0.0-next.5+49.sha-5ce71e0');
 
 /**
  * @license
