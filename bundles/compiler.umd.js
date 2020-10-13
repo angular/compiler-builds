1d79e7b1658eb001a63e74823a401c666af4d0fb
atscott <atscott01@gmail.com>
2020-10-13 22:21:08 +0000

    584f37cfca updated sajee's profile (#39019)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+53.sha-b642f0b
+ * @license Angular v11.0.0-next.5+55.sha-584f37c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20730,7 +20730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.5+53.sha-b642f0b');
+    var VERSION$1 = new Version('11.0.0-next.5+55.sha-584f37c');
 
     /**
      * @license
