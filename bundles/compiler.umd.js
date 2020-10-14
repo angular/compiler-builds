cc6f68d60e49b057deefecb7401ec58cadd00851
atscott <atscott01@gmail.com>
2020-10-14 22:03:18 +0000

    822b838fbc fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39267)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+5.sha-a83693d
+ * @license Angular v11.0.0-next.6+10.sha-822b838
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20730,7 +20730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+5.sha-a83693d');
+    var VERSION$1 = new Version('11.0.0-next.6+10.sha-822b838');
 
     /**
      * @license
