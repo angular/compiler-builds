ae2adf7acdd2e8b8e12e0f0780291f7cc28012e1
atscott <atscott01@gmail.com>
2020-10-14 20:22:07 +0000

    e08d02145e fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39272)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+23.sha-df09415
+ * @license Angular v10.1.5+42.sha-e08d021
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19425,7 +19425,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.5+23.sha-df09415');
+const VERSION$1 = new Version('10.1.5+42.sha-e08d021');
 
 /**
  * @license
