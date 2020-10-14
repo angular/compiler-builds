3efc114569d7ba0ccd16aeadbebb9d794e3f9b34
atscott <atscott01@gmail.com>
2020-10-14 21:03:56 +0000

    5db7b4c354 release: cut the v10.1.6 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+42.sha-e08d021
+ * @license Angular v10.1.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19425,7 +19425,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.5+42.sha-e08d021');
+const VERSION$1 = new Version('10.1.6');
 
 /**
  * @license
