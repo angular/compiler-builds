9525aa84fcc2a85d55b0f4fc5bca726fa1d02174
atscott <atscott01@gmail.com>
2020-10-14 16:58:35 +0000

    898be92f70 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+62.sha-8fd25d9
+ * @license Angular v11.0.0-next.5+68.sha-898be92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19553,7 +19553,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.5+62.sha-8fd25d9');
+const VERSION$1 = new Version('11.0.0-next.5+68.sha-898be92');
 
 /**
  * @license
