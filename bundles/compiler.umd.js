6a51403a15f7336981daa73c94681a9de44565c9
atscott <atscott01@gmail.com>
2020-10-14 17:47:49 +0000

    df0941554b docs: add Discord as an official communication channel (#39149)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+22.sha-5221df8
+ * @license Angular v10.1.5+23.sha-df09415
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20596,7 +20596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.5+22.sha-5221df8');
+    var VERSION$1 = new Version('10.1.5+23.sha-df09415');
 
     /**
      * @license
