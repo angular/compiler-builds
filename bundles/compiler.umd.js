682b22f1a38059d1d919abdb88878c4681b0f713
Andrew Kushnir <akushnir@google.com>
2020-10-15 21:39:50 +0000

    6acddb9518 refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+15.sha-14f15ec
+ * @license Angular v11.0.0-next.6+22.sha-6acddb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20746,7 +20746,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+15.sha-14f15ec');
+    var VERSION$1 = new Version('11.0.0-next.6+22.sha-6acddb9');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+15.sha-14f15ec
+ * @license Angular v11.0.0-next.6+22.sha-6acddb9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+15.sha-14f15ec');
+const VERSION$1 = new Version('11.0.0-next.6+22.sha-6acddb9');
 
 /**
  * @license
