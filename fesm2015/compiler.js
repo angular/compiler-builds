244cd470bef8ff9f56681ae2842b0adbdfe6bd57
Andrew Kushnir <akushnir@google.com>
2020-10-15 21:41:16 +0000

    7669827436 docs: fix typo at console.log (#39252)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+23.sha-f9f3c54
+ * @license Angular v11.0.0-next.6+22.sha-7669827
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+23.sha-f9f3c54');
+const VERSION$1 = new Version('11.0.0-next.6+22.sha-7669827');
 
 /**
  * @license
