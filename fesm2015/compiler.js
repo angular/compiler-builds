5c22deade69b800fba0f6e35388b54c521381489
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:43:01 +0000

    14f15ecd38 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+14.sha-56ab6d5
+ * @license Angular v11.0.0-next.6+15.sha-14f15ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+14.sha-56ab6d5');
+const VERSION$1 = new Version('11.0.0-next.6+15.sha-14f15ec');
 
 /**
  * @license
