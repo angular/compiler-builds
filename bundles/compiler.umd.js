c835c16972cc7caf8d34f0f029b258a56f425848
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:57:05 +0000

    563fb6cdbe feat(language-service): Implement go to definition for style and template urls (#39202)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20827,7 +20827,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+41.sha-1b21350');
+    var VERSION$1 = new Version('11.0.0-next.6+39.sha-563fb6c');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+41.sha-1b21350
+ * @license Angular v11.0.0-next.6+39.sha-563fb6c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+41.sha-1b21350');
+const VERSION$1 = new Version('11.0.0-next.6+39.sha-563fb6c');
 
 /**
  * @license
