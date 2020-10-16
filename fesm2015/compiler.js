b4ff1902f2da54e4ed05989fbcf2242070e9187d
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:41:07 +0000

    fdbc61111d build(docs-infra): add a tool to create new examples (#39283)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+24.sha-20b801e
+ * @license Angular v11.0.0-next.6+26.sha-fdbc611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+24.sha-20b801e');
+const VERSION$1 = new Version('11.0.0-next.6+26.sha-fdbc611');
 
 /**
  * @license
