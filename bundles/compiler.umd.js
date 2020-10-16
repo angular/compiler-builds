f949a82c29c74aff20dd3d7dec5cc2eba320ebf3
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:29:41 +0000

    79d67dc1f4 refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-b5ec5a7
+ * @license Angular v11.0.0-next.6+43.sha-79d67dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20827,7 +20827,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+42.sha-b5ec5a7');
+    var VERSION$1 = new Version('11.0.0-next.6+43.sha-79d67dc');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-b5ec5a7
+ * @license Angular v11.0.0-next.6+43.sha-79d67dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+42.sha-b5ec5a7');
+const VERSION$1 = new Version('11.0.0-next.6+43.sha-79d67dc');
 
 /**
  * @license
