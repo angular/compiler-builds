0280a02c0f4b1fe0fa60a5034e389707276e4190
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:47:45 +0000

    a3812c6cbd refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+25.sha-96f59f6
+ * @license Angular v11.0.0-next.6+26.sha-a3812c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+25.sha-96f59f6');
+const VERSION$1 = new Version('11.0.0-next.6+26.sha-a3812c6');
 
 /**
  * @license
