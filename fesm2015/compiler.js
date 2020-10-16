f112aeb9c73504d66459ab084442f00a86047553
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:27:54 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+34.sha-34b74ce
+ * @license Angular v11.0.0-next.6+36.sha-4dc8d83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+34.sha-34b74ce');
+const VERSION$1 = new Version('11.0.0-next.6+36.sha-4dc8d83');
 
 /**
  * @license
