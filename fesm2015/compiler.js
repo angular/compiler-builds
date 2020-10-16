a385052abb954edb5077bbc4564afc0932cf71f9
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:41:31 +0000

    a38293d213 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+35.sha-492e236
+ * @license Angular v11.0.0-next.6+37.sha-a38293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+35.sha-492e236');
+const VERSION$1 = new Version('11.0.0-next.6+37.sha-a38293d');
 
 /**
  * @license
