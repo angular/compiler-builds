2db4f50c58de10cadcdd51f0c94843637ba9ebc6
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:27:24 +0000

    d8e313bb6d docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+18.sha-4a5f17c
+ * @license Angular v10.1.6+20.sha-d8e313b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20596,7 +20596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.6+18.sha-4a5f17c');
+    var VERSION$1 = new Version('10.1.6+20.sha-d8e313b');
 
     /**
      * @license
