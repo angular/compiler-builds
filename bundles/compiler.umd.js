8291b941488e8f6c80da0138ba4312de4b8f4dc3
Andrew Kushnir <akushnir@google.com>
2020-10-16 15:43:12 +0000

    81aa119739 fix(core): convert legacy-sanitized values to Trusted Types (#39218)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+29.sha-765fa33
+ * @license Angular v11.0.0-next.6+33.sha-81aa119
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20827,7 +20827,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+29.sha-765fa33');
+    var VERSION$1 = new Version('11.0.0-next.6+33.sha-81aa119');
 
     /**
      * @license
