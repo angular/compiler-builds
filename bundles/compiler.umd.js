7002b10877a253c7c5acdfac9ef35fe22a80cc69
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:47:12 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+24.sha-38db976
+ * @license Angular v10.1.6+25.sha-7827d3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20596,7 +20596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.6+24.sha-38db976');
+    var VERSION$1 = new Version('10.1.6+25.sha-7827d3e');
 
     /**
      * @license
