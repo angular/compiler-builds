4c6b0c9893bf3eba06afbd42f122c315903b72ba
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:54:21 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+26.sha-32a8e72
+ * @license Angular v10.1.6+27.sha-02b8750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20596,7 +20596,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.1.6+26.sha-32a8e72');
+    var VERSION$1 = new Version('10.1.6+27.sha-02b8750');
 
     /**
      * @license
