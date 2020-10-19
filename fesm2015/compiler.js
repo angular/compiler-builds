ba67fe27626fdce1af2cdf5e769366ef2db6583a
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:07:08 +0000

    7e742aea7c refactor(compiler-cli): linker - add Babel plugin, FileLinker and initial PartialLinkers (#39116)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+49.sha-b304bd0
+ * @license Angular v11.0.0-next.6+50.sha-7e742ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+49.sha-b304bd0');
+const VERSION$1 = new Version('11.0.0-next.6+50.sha-7e742ae');
 
 /**
  * @license
