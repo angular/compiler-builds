e811d3e10cc7a19ab911127c5f429a3bd33353a7
Andrew Kushnir <akushnir@google.com>
2020-10-19 17:51:21 +0000

    0268b72d2c Revert "test(language-service): Make project service a singleton (#39308)" (#39322)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+47.sha-a93b7fd
+ * @license Angular v11.0.0-next.6+48.sha-0268b72
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+47.sha-a93b7fd');
+const VERSION$1 = new Version('11.0.0-next.6+48.sha-0268b72');
 
 /**
  * @license
