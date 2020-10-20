ae71a7762791c2a16de116745bd4f87c38de0ff5
Andrew Kushnir <akushnir@google.com>
2020-10-20 00:01:52 +0000

    5167eead45 docs: add Jessica Janiuk to contributors.json (#39253)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+42.sha-736e064
+ * @license Angular v11.0.0-next.6+43.sha-5167eea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+42.sha-736e064');
+const VERSION$1 = new Version('11.0.0-next.6+43.sha-5167eea');
 
 /**
  * @license
