88719a56d90e094feef40009722b665ab03f80f5
Andrew Kushnir <akushnir@google.com>
2020-10-20 18:21:21 +0000

    595062e415 docs: Add Component Overview topic to angular.io (#39186)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-5167eea
+ * @license Angular v11.0.0-next.6+46.sha-595062e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20746,7 +20746,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+43.sha-5167eea');
+    var VERSION$1 = new Version('11.0.0-next.6+46.sha-595062e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+43.sha-5167eea
+ * @license Angular v11.0.0-next.6+46.sha-595062e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+43.sha-5167eea');
+const VERSION$1 = new Version('11.0.0-next.6+46.sha-595062e');
 
 /**
  * @license
