f0fff5d69ffc8fd9a3bfa5e6fac3d0feafe94e47
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:52:59 +0000

    ac53101fc6 build: upgrade karma to version 4.4.0 (#39180)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+49.sha-161b278
+ * @license Angular v11.0.0-next.6+50.sha-ac53101
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20746,7 +20746,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+49.sha-161b278');
+    var VERSION$1 = new Version('11.0.0-next.6+50.sha-ac53101');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+49.sha-161b278
+ * @license Angular v11.0.0-next.6+50.sha-ac53101
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+49.sha-161b278');
+const VERSION$1 = new Version('11.0.0-next.6+50.sha-ac53101');
 
 /**
  * @license
