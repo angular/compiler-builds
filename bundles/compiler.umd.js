b958b7c9a1bf6530d3ef4d98fac7e9e5dd19d33b
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:01:30 +0000

    b4e8399144 fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-d8c9f42
+ * @license Angular v11.0.0-next.6+55.sha-b4e8399
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20746,7 +20746,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+54.sha-d8c9f42');
+    var VERSION$1 = new Version('11.0.0-next.6+55.sha-b4e8399');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+54.sha-d8c9f42
+ * @license Angular v11.0.0-next.6+55.sha-b4e8399
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19569,7 +19569,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+54.sha-d8c9f42');
+const VERSION$1 = new Version('11.0.0-next.6+55.sha-b4e8399');
 
 /**
  * @license
