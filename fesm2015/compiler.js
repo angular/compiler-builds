ef0158151b03502675ddf3d3ee1493199a937712
Andrew Kushnir <akushnir@google.com>
2020-10-21 16:24:55 +0000

    7ca17a6249 build(docs-infra): upgrade cli command docs sources to cba6d86ca (#39360)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+29.sha-823200c
+ * @license Angular v10.1.6+32.sha-7ca17a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19425,7 +19425,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.1.6+29.sha-823200c');
+const VERSION$1 = new Version('10.1.6+32.sha-7ca17a6');
 
 /**
  * @license
