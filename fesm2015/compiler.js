39eeaca4ca57d587e411d3741870018e5165bfa8
Andrew Kushnir <akushnir@google.com>
2020-10-21 15:53:42 +0000

    ef2d07fdd1 docs: remove `style` commit type from CONTRIBUTING.md (#39357)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+61.sha-cfac8e0
+ * @license Angular v11.0.0-next.6+64.sha-ef2d07f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19655,7 +19655,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+61.sha-cfac8e0');
+const VERSION$1 = new Version('11.0.0-next.6+64.sha-ef2d07f');
 
 /**
  * @license
