0ea72329956613c6fdfce26a499ec718babd2365
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:27:38 +0000

    7768aeb62f fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+65.sha-d33eaa6
+ * @license Angular v11.0.0-next.6+66.sha-7768aeb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20827,7 +20827,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+65.sha-d33eaa6');
+    var VERSION$1 = new Version('11.0.0-next.6+66.sha-7768aeb');
 
     /**
      * @license
