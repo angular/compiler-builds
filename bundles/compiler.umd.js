6d313f87518535ce2ae5baf4e3f6998cb84e6717
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-22 01:57:19 +0000

    08f3d62391 refactor(core): clean up circular dependencies (#39233)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+69.sha-eb4c05d
+ * @license Angular v11.0.0-next.6+81.sha-08f3d62
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20827,7 +20827,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+69.sha-eb4c05d');
+    var VERSION$1 = new Version('11.0.0-next.6+81.sha-08f3d62');
 
     /**
      * @license
