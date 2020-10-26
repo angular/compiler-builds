498f76c36f567798519911cf80f97e527bb58663
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:13:59 +0000

    3fbf32534c docs: fix typo in component overview (#39425)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+29.sha-6669571
+ * @license Angular v11.0.0-rc.0+35.sha-3fbf325
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20749,7 +20749,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-rc.0+29.sha-6669571');
+    var VERSION$1 = new Version('11.0.0-rc.0+35.sha-3fbf325');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+29.sha-6669571
+ * @license Angular v11.0.0-rc.0+35.sha-3fbf325
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19572,7 +19572,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-rc.0+29.sha-6669571');
+const VERSION$1 = new Version('11.0.0-rc.0+35.sha-3fbf325');
 
 /**
  * @license
