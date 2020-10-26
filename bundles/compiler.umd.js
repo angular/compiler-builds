6957730d79aeec7dc0df633030d3966d7785fd1f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 17:40:24 +0000

    90c0772743 fix(zone.js): remove global declaration (#37861)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+95.sha-b989ba2
+ * @license Angular v11.0.0-next.6+99.sha-90c0772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20837,7 +20837,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+95.sha-b989ba2');
+    var VERSION$1 = new Version('11.0.0-next.6+99.sha-90c0772');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+95.sha-b989ba2
+ * @license Angular v11.0.0-next.6+99.sha-90c0772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19664,7 +19664,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+95.sha-b989ba2');
+const VERSION$1 = new Version('11.0.0-next.6+99.sha-90c0772');
 
 /**
  * @license
