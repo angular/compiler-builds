84678958135611e96ff08a1601d72867f97d7d50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:11:05 +0000

    43edc1b34c release: cut the v10.2.1 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+40.sha-1fbe217
+ * @license Angular v10.2.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.0+40.sha-1fbe217');
+const VERSION$1 = new Version('10.2.1');
 
 /**
  * @license
