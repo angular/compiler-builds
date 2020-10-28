786f36d8ec2fdfe5fabca0ff169d68c724ed5b9b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:22:18 +0000

    1508fd290b docs: update Learning Angular book edition (#39400)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+134.sha-7c45cbc
+ * @license Angular v11.0.0-next.6+137.sha-1508fd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+134.sha-7c45cbc');
+    var VERSION$1 = new Version('11.0.0-next.6+137.sha-1508fd2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+134.sha-7c45cbc
+ * @license Angular v11.0.0-next.6+137.sha-1508fd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19663,7 +19663,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+134.sha-7c45cbc');
+const VERSION$1 = new Version('11.0.0-next.6+137.sha-1508fd2');
 
 /**
  * @license
