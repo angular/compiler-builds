1be25c3d345eac0027a2dee98691a9c61ebee145
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:22:38 +0000

    20c26cd49b build: add PullApprove: disable as a caretaker note label (#39430)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+137.sha-1508fd2
+ * @license Angular v11.0.0-next.6+140.sha-20c26cd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+137.sha-1508fd2');
+    var VERSION$1 = new Version('11.0.0-next.6+140.sha-20c26cd');
 
     /**
      * @license
