cec79f1f228c76bf9708fe76686c701ebd4c6efc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 21:45:37 +0000

    bdec818b3a docs: release notes for the v11.0.0-rc.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+146.sha-e42d3ec');
+    var VERSION$1 = new Version('11.0.0-next.6+147.sha-bdec818');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+146.sha-e42d3ec
+ * @license Angular v11.0.0-next.6+147.sha-bdec818
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19663,7 +19663,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+146.sha-e42d3ec');
+const VERSION$1 = new Version('11.0.0-next.6+147.sha-bdec818');
 
 /**
  * @license
