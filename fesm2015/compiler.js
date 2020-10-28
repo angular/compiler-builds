e33f7b67d50272593a4f80ed7aec51c676ef7ac4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 17:09:16 +0000

    0723331b2a refactor(forms): move common validators-related logic to the `AbstractControlDirective` class (#38280)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+130.sha-e649f1d
+ * @license Angular v11.0.0-next.6+131.sha-0723331
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19663,7 +19663,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+130.sha-e649f1d');
+const VERSION$1 = new Version('11.0.0-next.6+131.sha-0723331');
 
 /**
  * @license
