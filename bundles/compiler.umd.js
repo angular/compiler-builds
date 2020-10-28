2522407ac08bb89fc4183df953ea93e89df057a2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:15:26 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+132.sha-e6ca3d3
+ * @license Angular v11.0.0-next.6+133.sha-b9358c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+132.sha-e6ca3d3');
+    var VERSION$1 = new Version('11.0.0-next.6+133.sha-b9358c2');
 
     /**
      * @license
