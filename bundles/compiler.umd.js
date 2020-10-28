f54a098cf778cae2cd7dbeeb9a49cacc0982bca3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:26:05 +0000

    371fb9a955 refactor(compiler-cli): Track external component resources in ResourceRegistry (#39373)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+140.sha-20c26cd
+ * @license Angular v11.0.0-next.6+136.sha-371fb9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+140.sha-20c26cd');
+    var VERSION$1 = new Version('11.0.0-next.6+136.sha-371fb9a');
 
     /**
      * @license
