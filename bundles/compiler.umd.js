603d70c2db0daf267f898a18b03c984f313cc68b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:17:27 +0000

    7c45cbc523 docs: fix links to Material examples (#39093)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+133.sha-b9358c2
+ * @license Angular v11.0.0-next.6+134.sha-7c45cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20858,7 +20858,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+133.sha-b9358c2');
+    var VERSION$1 = new Version('11.0.0-next.6+134.sha-7c45cbc');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+133.sha-b9358c2
+ * @license Angular v11.0.0-next.6+134.sha-7c45cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19663,7 +19663,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+133.sha-b9358c2');
+const VERSION$1 = new Version('11.0.0-next.6+134.sha-7c45cbc');
 
 /**
  * @license
