fec7f3f1d165112dfd2a6f108d4fed97b3eb3170
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:35:17 +0000

    ddc070e778 docs(core): add jessicajaniuk to pullapprove list (#39473)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.0+60.sha-366be11
+ * @license Angular v11.0.0-rc.0+70.sha-ddc070e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20770,7 +20770,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-rc.0+60.sha-366be11');
+    var VERSION$1 = new Version('11.0.0-rc.0+70.sha-ddc070e');
 
     /**
      * @license
