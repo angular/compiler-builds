f07aa60cc3cf4a799d65748bf5aa2599c3a0719f
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:51:39 +0000

    90acb9171e docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+10.sha-fb96c3d
+ * @license Angular v10.2.1+12.sha-90acb91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+10.sha-fb96c3d');
+    var VERSION$1 = new Version('10.2.1+12.sha-90acb91');
 
     /**
      * @license
