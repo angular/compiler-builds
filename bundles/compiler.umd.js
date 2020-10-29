6bc383b15e096f4d7ee9851c9b4e44e77be82489
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:17:38 +0000

    e45da28ed5 build: update to CLI 11.0.0-rc.1 (#39432)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+10.sha-ec416d0
+ * @license Angular v11.0.0-rc.1+12.sha-e45da28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20770,7 +20770,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-rc.1+10.sha-ec416d0');
+    var VERSION$1 = new Version('11.0.0-rc.1+12.sha-e45da28');
 
     /**
      * @license
