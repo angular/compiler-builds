1e9cb013a196d48a3553a202b272fb70be73fa1a
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:16:56 +0000

    cff61b2abe fix(dev-infra): update generated ng-dev compiled output (#39474)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+4.sha-9205fce
+ * @license Angular v10.2.1+8.sha-cff61b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+4.sha-9205fce');
+    var VERSION$1 = new Version('10.2.1+8.sha-cff61b2');
 
     /**
      * @license
