805861ee3189c0a7cba5ae2aeffabb8fdee433d3
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:48:21 +0000

    fb96c3d624 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+9.sha-f9d3588
+ * @license Angular v10.2.1+10.sha-fb96c3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+9.sha-f9d3588');
+    var VERSION$1 = new Version('10.2.1+10.sha-fb96c3d');
 
     /**
      * @license
