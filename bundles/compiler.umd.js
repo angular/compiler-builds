cd241a77b60da64a8c4b40e84ffa69495fa21b8c
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:44:03 +0000

    f9d35884a3 docs(forms): update ngModel documentation (#39481)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+8.sha-cff61b2
+ * @license Angular v10.2.1+9.sha-f9d3588
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+8.sha-cff61b2');
+    var VERSION$1 = new Version('10.2.1+9.sha-f9d3588');
 
     /**
      * @license
