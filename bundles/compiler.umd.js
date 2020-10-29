fe119ee7c302382aaa524be1dddaeafd7f6ad7a6
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:37:47 +0000

    4a8d5ae970 fix(core): markDirty() should only mark flags when really scheduling tick. (#39316)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+162.sha-170af07
+ * @license Angular v11.0.0-next.6+163.sha-4a8d5ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20863,7 +20863,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+162.sha-170af07');
+    var VERSION$1 = new Version('11.0.0-next.6+163.sha-4a8d5ae');
 
     /**
      * @license
