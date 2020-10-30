ac4300cedf5fa8539d48a88c137b9d166c7eb565
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:49:21 +0000

    4e68254514 fix(common): change the week-numbering year format from `r` -> `Y` (#39495)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+167.sha-beb9356
+ * @license Angular v11.0.0-next.6+168.sha-4e68254
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20871,7 +20871,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+167.sha-beb9356');
+    var VERSION$1 = new Version('11.0.0-next.6+168.sha-4e68254');
 
     /**
      * @license
