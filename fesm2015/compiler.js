64deb4180cc9e09d8f771e1f19d81dcb97ee29df
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:44:34 +0000

    85d5242630 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+12.sha-90acb91
+ * @license Angular v10.2.1+14.sha-85d5242
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.1+12.sha-90acb91');
+const VERSION$1 = new Version('10.2.1+14.sha-85d5242');
 
 /**
  * @license
