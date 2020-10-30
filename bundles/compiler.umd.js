7be7aade62cb1313ebabeaf5aa7ab1a19165c4ab
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:48:58 +0000

    feda78e1c8 fix(common): change the week-numbering year format from `r` -> `Y` (#39495)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+17.sha-feda78e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20760,7 +20760,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-rc.1+16.sha-3091534');
+    var VERSION$1 = new Version('11.0.0-rc.1+17.sha-feda78e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.1+16.sha-3091534
+ * @license Angular v11.0.0-rc.1+17.sha-feda78e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19561,7 +19561,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-rc.1+16.sha-3091534');
+const VERSION$1 = new Version('11.0.0-rc.1+17.sha-feda78e');
 
 /**
  * @license
