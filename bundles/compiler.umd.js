e23df1207c67fa0c51a71eaed9ca7e3055bf27f8
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:25:52 +0000

    71d0063285 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+14.sha-85d5242
+ * @license Angular v10.2.1+15.sha-71d0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+14.sha-85d5242');
+    var VERSION$1 = new Version('10.2.1+15.sha-71d0063');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+14.sha-85d5242
+ * @license Angular v10.2.1+15.sha-71d0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19428,7 +19428,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.1+14.sha-85d5242');
+const VERSION$1 = new Version('10.2.1+15.sha-71d0063');
 
 /**
  * @license
