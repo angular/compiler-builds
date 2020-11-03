d51a523a25293ead430cbce4f78a032931abfde3
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:04:23 +0000

    be64f98076 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+189.sha-3cab823
+ * @license Angular v11.0.0-next.6+191.sha-be64f98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19723,7 +19723,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+189.sha-3cab823');
+const VERSION$1 = new Version('11.0.0-next.6+191.sha-be64f98');
 
 /**
  * @license
