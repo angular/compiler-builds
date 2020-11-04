f12f0eef4b426c57190e9fe8d98a8d8246ecc54c
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:13:49 +0000

    08e077c0e0 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+33.sha-8929355
+ * @license Angular v10.2.2+1.sha-08e077c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20599,7 +20599,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.1+33.sha-8929355');
+    var VERSION$1 = new Version('10.2.2+1.sha-08e077c');
 
     /**
      * @license
