50f0f14adc003f26df4a1855d4e3a9933a0a2fcd
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:06:24 +0000

    3701376894 docs: release notes for the v10.2.2 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+205.sha-27ae060
+ * @license Angular v11.0.0-next.6+206.sha-3701376
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19722,7 +19722,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+205.sha-27ae060');
+const VERSION$1 = new Version('11.0.0-next.6+206.sha-3701376');
 
 /**
  * @license
