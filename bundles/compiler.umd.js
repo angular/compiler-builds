0ee9c2b9efa2f2f7e98c1ad91482b4f433fbb56f
Misko Hevery <misko@hevery.com>
2020-11-05 19:48:22 +0000

    27358eb60f feat(zone.js): monkey patches queueMicrotask() (#38904)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+209.sha-6138bc2
+ * @license Angular v11.0.0-next.6+210.sha-27358eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20932,7 +20932,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+209.sha-6138bc2');
+    var VERSION$1 = new Version('11.0.0-next.6+210.sha-27358eb');
 
     /**
      * @license
