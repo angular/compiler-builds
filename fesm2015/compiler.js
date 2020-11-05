cca6c77270fabe8ccde83cca0e8938ceb48af22e
Misko Hevery <misko@hevery.com>
2020-11-05 16:22:37 +0000

    6138bc2774 build(docs-infra): upgrade cli command docs sources to 0defb1136 (#39575)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+207.sha-8c40578
+ * @license Angular v11.0.0-next.6+209.sha-6138bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19722,7 +19722,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+207.sha-8c40578');
+const VERSION$1 = new Version('11.0.0-next.6+209.sha-6138bc2');
 
 /**
  * @license
