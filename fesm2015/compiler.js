0b4bdfa0c0bad72c6551808ba6f1c32122486a0d
Misko Hevery <misko@hevery.com>
2020-11-06 17:52:28 +0000

    b9b9178458 fix(compiler-cli): do not drop non-Angular decorators when downleveling (#39577)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+216.sha-3a1d36c
+ * @license Angular v11.0.0-next.6+217.sha-b9b9178
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19752,7 +19752,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+216.sha-3a1d36c');
+const VERSION$1 = new Version('11.0.0-next.6+217.sha-b9b9178');
 
 /**
  * @license
