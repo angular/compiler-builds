276b9b45174c4525260b54f1b33e052e69470bdf
Misko Hevery <misko@hevery.com>
2020-11-06 23:52:43 +0000

    cf88ea0bf3 fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+225.sha-0929099
+ * @license Angular v11.0.0-next.6+226.sha-cf88ea0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19752,7 +19752,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+225.sha-0929099');
+const VERSION$1 = new Version('11.0.0-next.6+226.sha-cf88ea0');
 
 /**
  * @license
