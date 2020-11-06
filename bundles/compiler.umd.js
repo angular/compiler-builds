db32f6c28f54102a897e58122b2d194e7e204f5c
Misko Hevery <misko@hevery.com>
2020-11-06 17:55:58 +0000

    290ea57a93 fix(core): Access injected parent values using SelfSkip (#39464)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+217.sha-b9b9178
+ * @license Angular v11.0.0-next.6+218.sha-290ea57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20963,7 +20963,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+217.sha-b9b9178');
+    var VERSION$1 = new Version('11.0.0-next.6+218.sha-290ea57');
 
     /**
      * @license
