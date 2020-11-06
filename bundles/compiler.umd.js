0769c86a82ec73da666c8c34d44d32ee1295c733
Misko Hevery <misko@hevery.com>
2020-11-06 23:51:38 +0000

    861e4fa627 fix(compiler-cli): avoid duplicate diagnostics about unknown pipes (#39517)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+7.sha-1c6cf8a
+ * @license Angular v10.2.2+8.sha-861e4fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20632,7 +20632,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.2+7.sha-1c6cf8a');
+    var VERSION$1 = new Version('10.2.2+8.sha-861e4fa');
 
     /**
      * @license
