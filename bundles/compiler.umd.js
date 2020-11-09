9a476cc06773237672a70b78874939223873cf7f
Misko Hevery <misko@hevery.com>
2020-11-09 21:30:08 +0000

    a1b319e25d docs: release notes for the v10.2.3 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+230.sha-f405aa5
+ * @license Angular v11.0.0-next.6+231.sha-a1b319e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20963,7 +20963,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+230.sha-f405aa5');
+    var VERSION$1 = new Version('11.0.0-next.6+231.sha-a1b319e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+230.sha-f405aa5
+ * @license Angular v11.0.0-next.6+231.sha-a1b319e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19752,7 +19752,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+230.sha-f405aa5');
+const VERSION$1 = new Version('11.0.0-next.6+231.sha-a1b319e');
 
 /**
  * @license
