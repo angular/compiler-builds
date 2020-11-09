153794d00c06bb18cc810f9f340f05577835d6cf
Misko Hevery <misko@hevery.com>
2020-11-09 16:02:15 +0000

    ae71b76089 ci: log commands output when deploying angular.io to Firebase (#39596)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+8.sha-861e4fa
+ * @license Angular v10.2.2+10.sha-ae71b76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20632,7 +20632,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.2+8.sha-861e4fa');
+    var VERSION$1 = new Version('10.2.2+10.sha-ae71b76');
 
     /**
      * @license
