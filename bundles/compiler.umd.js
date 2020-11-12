06bda2bb760aac706e8c5b009b19be9de37d94c9
atscott <atscott01@gmail.com>
2020-11-12 22:15:58 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+240.sha-b015d3e
+ * @license Angular v11.0.0-next.6+241.sha-a872362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20963,7 +20963,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+240.sha-b015d3e');
+    var VERSION$1 = new Version('11.0.0-next.6+241.sha-a872362');
 
     /**
      * @license
