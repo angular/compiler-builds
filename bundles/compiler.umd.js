a2d1a9d47fecadf094031830721c691752f55233
Misko Hevery <misko@hevery.com>
2020-11-12 00:08:04 +0000

    92900d561d docs: update the roadmap with our priorities at Q4 (#39635)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.3
+ * @license Angular v11.0.0+1.sha-92900d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20853,7 +20853,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-rc.3');
+    var VERSION$1 = new Version('11.0.0+1.sha-92900d5');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-rc.3
+ * @license Angular v11.0.0+1.sha-92900d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19638,7 +19638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-rc.3');
+const VERSION$1 = new Version('11.0.0+1.sha-92900d5');
 
 /**
  * @license
