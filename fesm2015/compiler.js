ddbab675a177e4c71a0a563052a5c0bdfb8f3150
Misko Hevery <misko@hevery.com>
2020-11-12 00:02:31 +0000

    8a03e3ee48 release: cut the v11.0.0 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+232.sha-5205db5
+ * @license Angular v11.0.0-next.6+233.sha-8a03e3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19752,7 +19752,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+232.sha-5205db5');
+const VERSION$1 = new Version('11.0.0-next.6+233.sha-8a03e3e');
 
 /**
  * @license
