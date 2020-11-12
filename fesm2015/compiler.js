8af80737b0b8282967db7d4f5c47361ca9924781
atscott <atscott01@gmail.com>
2020-11-12 22:19:44 +0000

    7ce825011b docs: archive di-navtree doc (#39543)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+242.sha-f8956ad
+ * @license Angular v11.0.0-next.6+243.sha-7ce8250
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19752,7 +19752,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+242.sha-f8956ad');
+const VERSION$1 = new Version('11.0.0-next.6+243.sha-7ce8250');
 
 /**
  * @license
