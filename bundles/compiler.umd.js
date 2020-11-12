0dcf0dda1e53a7eea7fe616c99c4f44407bdd045
atscott <atscott01@gmail.com>
2020-11-12 20:02:33 +0000

    20db90a41d fix(core): not inserting ViewContainerRef nodes when inside root of a component (#39599)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+6.sha-050cea9
+ * @license Angular v11.0.0+7.sha-20db90a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20853,7 +20853,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0+6.sha-050cea9');
+    var VERSION$1 = new Version('11.0.0+7.sha-20db90a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+6.sha-050cea9
+ * @license Angular v11.0.0+7.sha-20db90a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19638,7 +19638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+6.sha-050cea9');
+const VERSION$1 = new Version('11.0.0+7.sha-20db90a');
 
 /**
  * @license
