7d37435da428d128d23b00826401989d1abcd6c6
Misko Hevery <misko@hevery.com>
2020-11-12 00:08:22 +0000

    6d1347363e docs: update the roadmap with our priorities at Q4 (#39635)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+233.sha-8a03e3e
+ * @license Angular v11.0.0-next.6+234.sha-6d13473
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20963,7 +20963,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+233.sha-8a03e3e');
+    var VERSION$1 = new Version('11.0.0-next.6+234.sha-6d13473');
 
     /**
      * @license
