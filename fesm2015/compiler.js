e874386b916301ef66bc0aeed833d4681a5a134a
atscott <atscott01@gmail.com>
2020-11-12 17:38:35 +0000

    978f0810d9 fix(core): handle !important in style property value (#39603)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+1.sha-92900d5
+ * @license Angular v11.0.0+5.sha-978f081
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19638,7 +19638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+1.sha-92900d5');
+const VERSION$1 = new Version('11.0.0+5.sha-978f081');
 
 /**
  * @license
