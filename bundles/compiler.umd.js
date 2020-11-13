9b6d050dca23e5ed2c36c7ed5f6855a75a8dd305
atscott <atscott01@gmail.com>
2020-11-13 20:21:59 +0000

    24e13e0ed6 refactor(core): document setComponentScope to provide context (#39662)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+259.sha-25d6fca
+ * @license Angular v11.0.0-next.6+261.sha-24e13e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20987,7 +20987,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+259.sha-25d6fca');
+    var VERSION$1 = new Version('11.0.0-next.6+261.sha-24e13e0');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+259.sha-25d6fca
+ * @license Angular v11.0.0-next.6+261.sha-24e13e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19776,7 +19776,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+259.sha-25d6fca');
+const VERSION$1 = new Version('11.0.0-next.6+261.sha-24e13e0');
 
 /**
  * @license
