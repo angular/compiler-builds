7d0f8f2a993b5d91d96c06186bc89deb74702ace
atscott <atscott01@gmail.com>
2020-11-13 19:55:58 +0000

    f1d8717a88 docs: add doc with list of example apps currently in aio (#39295)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+14.sha-06de803
+ * @license Angular v11.0.0+17.sha-f1d8717
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19638,7 +19638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+14.sha-06de803');
+const VERSION$1 = new Version('11.0.0+17.sha-f1d8717');
 
 /**
  * @license
