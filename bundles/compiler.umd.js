8ec9767ff3af993d84ec084cabfcb071739e1ea9
atscott <atscott01@gmail.com>
2020-11-13 22:01:08 +0000

    7f3314a1e8 ci: update angular-robot config for L2 triage of PRs (#39655)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+19.sha-83a6ab1
+ * @license Angular v11.0.0+20.sha-7f3314a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20853,7 +20853,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0+19.sha-83a6ab1');
+    var VERSION$1 = new Version('11.0.0+20.sha-7f3314a');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+19.sha-83a6ab1
+ * @license Angular v11.0.0+20.sha-7f3314a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19638,7 +19638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+19.sha-83a6ab1');
+const VERSION$1 = new Version('11.0.0+20.sha-7f3314a');
 
 /**
  * @license
