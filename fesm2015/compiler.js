87c5a9f75abe20b83c18185729de47ac309bca49
atscott <atscott01@gmail.com>
2020-11-13 19:48:42 +0000

    eea5d6b38f test(compiler-cli): convert element related compliance tests (#39617)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+258.sha-3f66a84
+ * @license Angular v11.0.0-next.6+257.sha-eea5d6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19776,7 +19776,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+258.sha-3f66a84');
+const VERSION$1 = new Version('11.0.0-next.6+257.sha-eea5d6b');
 
 /**
  * @license
