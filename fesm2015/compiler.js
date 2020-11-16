2f1f02f6385d4b8124c970b01a5d599b90223199
atscott <atscott01@gmail.com>
2020-11-16 17:43:11 +0000

    b0c4ecf563 docs: improve commit message format documentation (#39705)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+263.sha-5e92d64
+ * @license Angular v11.0.0-next.6+265.sha-b0c4ecf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19776,7 +19776,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+263.sha-5e92d64');
+const VERSION$1 = new Version('11.0.0-next.6+265.sha-b0c4ecf');
 
 /**
  * @license
