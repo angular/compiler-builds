9a641ea1d181217251c4b274fbc9b19ceb0ba695
atscott <atscott01@gmail.com>
2020-11-16 17:25:06 +0000

    5e92d649f2 feat(core): add shouldCoalesceRunChangeDetection option to coalesce change detections in the same event loop. (#39422)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+262.sha-d68cac6
+ * @license Angular v11.0.0-next.6+263.sha-5e92d64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19776,7 +19776,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+262.sha-d68cac6');
+const VERSION$1 = new Version('11.0.0-next.6+263.sha-5e92d64');
 
 /**
  * @license
