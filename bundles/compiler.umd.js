6796722e32969b0bcfe25bae67dd041d07093073
atscott <atscott01@gmail.com>
2020-11-16 19:41:32 +0000

    9e87fc88ff refactor(core): Remove circular dependency on `ApplicationRef` (#39710)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+24.sha-e05cfdd
+ * @license Angular v11.0.0+35.sha-9e87fc8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20853,7 +20853,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0+24.sha-e05cfdd');
+    var VERSION$1 = new Version('11.0.0+35.sha-9e87fc8');
 
     /**
      * @license
