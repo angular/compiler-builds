92f1cb11695d64a651121cd837433cb7fd61187e
atscott <atscott01@gmail.com>
2020-11-16 17:40:36 +0000

    b0c4ecf563 docs: improve commit message format documentation (#39705)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+263.sha-5e92d64
+ * @license Angular v11.0.0-next.6+265.sha-b0c4ecf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20987,7 +20987,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+263.sha-5e92d64');
+    var VERSION$1 = new Version('11.0.0-next.6+265.sha-b0c4ecf');
 
     /**
      * @license
