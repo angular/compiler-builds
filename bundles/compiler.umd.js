128edc16583c60820498b16468421981652e01b8
atscott <atscott01@gmail.com>
2020-11-17 20:19:36 +0000

    806d7aa22c docs: add documentation for ngNonBindable (#36560)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+283.sha-209a18c
+ * @license Angular v11.0.0-next.6+284.sha-806d7aa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21158,7 +21158,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+283.sha-209a18c');
+    var VERSION$1 = new Version('11.0.0-next.6+284.sha-806d7aa');
 
     /**
      * @license
