faee444edde79d89c0ef227ce6a2fb1029bfc321
atscott <atscott01@gmail.com>
2020-11-17 20:19:39 +0000

    209a18c624 docs: remove bazel from deprecations doc (#39540)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+282.sha-d281ea8
+ * @license Angular v11.0.0-next.6+283.sha-209a18c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+282.sha-d281ea8');
+const VERSION$1 = new Version('11.0.0-next.6+283.sha-209a18c');
 
 /**
  * @license
