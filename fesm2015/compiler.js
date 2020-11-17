cfd2b2ef081fd361edf344bba5f0fa465a7e3e54
atscott <atscott01@gmail.com>
2020-11-17 21:34:35 +0000

    49410f8e93 docs: add spanish lang (#39502)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+288.sha-b6893d2
+ * @license Angular v11.0.0-next.6+290.sha-49410f8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+288.sha-b6893d2');
+const VERSION$1 = new Version('11.0.0-next.6+290.sha-49410f8');
 
 /**
  * @license
