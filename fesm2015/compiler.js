829f77fb27b3d0d8092ad74f5999e78d6fca14e3
atscott <atscott01@gmail.com>
2020-11-17 23:11:29 +0000

    d39c4bbe37 refactor(language-service): language_service_adapter -> adapters (#39619)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+289.sha-3926665
+ * @license Angular v11.0.0-next.6+292.sha-d39c4bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+289.sha-3926665');
+const VERSION$1 = new Version('11.0.0-next.6+292.sha-d39c4bb');
 
 /**
  * @license
