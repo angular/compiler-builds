f359a523eaf593aefa205e5ce109a5661c1457b9
atscott <atscott01@gmail.com>
2020-11-18 18:46:15 +0000

    7261e393f1 refactor(dev-infra): extract the parsing of a range of commits into a util function (#39726)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+306.sha-d20cab1
+ * @license Angular v11.0.0-next.6+300.sha-7261e39
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+306.sha-d20cab1');
+const VERSION$1 = new Version('11.0.0-next.6+300.sha-7261e39');
 
 /**
  * @license
