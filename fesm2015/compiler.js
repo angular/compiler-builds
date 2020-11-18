1c663ec0014b516601482b192f67c905e1805179
atscott <atscott01@gmail.com>
2020-11-18 17:46:00 +0000

    3b2e5be6cb refactor(core): clean up circular dependencies (#39722)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+296.sha-7d68f92
+ * @license Angular v11.0.0-next.6+299.sha-3b2e5be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+296.sha-7d68f92');
+const VERSION$1 = new Version('11.0.0-next.6+299.sha-3b2e5be');
 
 /**
  * @license
