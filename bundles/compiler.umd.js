01e088c669eff38cc095e513cef2689ead01cf72
atscott <atscott01@gmail.com>
2020-11-18 20:34:45 +0000

    3273856d33 docs: fixes on paragraph (#39664)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+3.sha-cc509d6
+ * @license Angular v11.1.0-next.0+4.sha-3273856
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21158,7 +21158,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+3.sha-cc509d6');
+    var VERSION$1 = new Version('11.1.0-next.0+4.sha-3273856');
 
     /**
      * @license
