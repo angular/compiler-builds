4177308ae9bf32b1335c541d1be17ffff14fe038
atscott <atscott01@gmail.com>
2020-11-18 19:41:14 +0000

    cc509d609f build: update to husky@5 (#39739)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+1.sha-3a344d4
+ * @license Angular v11.1.0-next.0+3.sha-cc509d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21158,7 +21158,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+1.sha-3a344d4');
+    var VERSION$1 = new Version('11.1.0-next.0+3.sha-cc509d6');
 
     /**
      * @license
