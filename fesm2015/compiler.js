19c165bbfd218d1a79a6b35f546754bc03be9954
atscott <atscott01@gmail.com>
2020-11-18 19:34:46 +0000

    3a344d4e0e test(language-service): add renamed, deleted test file (#39742)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0
+ * @license Angular v11.1.0-next.0+1.sha-3a344d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0');
+const VERSION$1 = new Version('11.1.0-next.0+1.sha-3a344d4');
 
 /**
  * @license
