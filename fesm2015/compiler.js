fe074ffc167a620ae64913e84870a04894b0adbf
atscott <atscott01@gmail.com>
2020-11-18 19:05:54 +0000

    153b42b01e release: cut the v11.0.1 release

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+53.sha-163fdc3
+ * @license Angular v11.0.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+53.sha-163fdc3');
+const VERSION$1 = new Version('11.0.1');
 
 /**
  * @license
