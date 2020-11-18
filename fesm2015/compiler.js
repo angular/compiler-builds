660b90c96b0a53529781f24ac1266cf096b9e583
atscott <atscott01@gmail.com>
2020-11-18 17:34:35 +0000

    0ab833f5c2 docs: meetup url to https (#33713)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+293.sha-00291ae
+ * @license Angular v11.0.0-next.6+294.sha-0ab833f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0-next.6+293.sha-00291ae');
+const VERSION$1 = new Version('11.0.0-next.6+294.sha-0ab833f');
 
 /**
  * @license
