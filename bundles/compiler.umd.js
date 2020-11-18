2052190a54c5d425cd2d18f86cbba01a5eb0a2b7
atscott <atscott01@gmail.com>
2020-11-18 17:32:31 +0000

    0ab833f5c2 docs: meetup url to https (#33713)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+293.sha-00291ae
+ * @license Angular v11.0.0-next.6+294.sha-0ab833f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21158,7 +21158,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.0-next.6+293.sha-00291ae');
+    var VERSION$1 = new Version('11.0.0-next.6+294.sha-0ab833f');
 
     /**
      * @license
