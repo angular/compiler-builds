a6d349e79b875f02fa18dab2e0aa48176055c43d
atscott <atscott01@gmail.com>
2020-11-18 18:45:08 +0000

    163fdc35f6 docs: add links to corresponding commits in v11.0.0 breaking changes in `CHANGELOG.md` (#39704)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+46.sha-5d8b5eb
+ * @license Angular v11.0.0+53.sha-163fdc3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.0+46.sha-5d8b5eb');
+const VERSION$1 = new Version('11.0.0+53.sha-163fdc3');
 
 /**
  * @license
