9213fe315fbc0e0f5f324d868053573dec82ca79
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:48:19 +0000

    1951342124 docs(zone.js): update zone.js readme for the bundle format (#39508)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+14.sha-5b45fab
+ * @license Angular v11.1.0-next.0+15.sha-1951342
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19944,7 +19944,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+14.sha-5b45fab');
+const VERSION$1 = new Version('11.1.0-next.0+15.sha-1951342');
 
 /**
  * @license
