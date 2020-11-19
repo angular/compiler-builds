b02cd28621f8bac3c34bd8e1d03c44affcaa3f4c
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:06:55 +0000

    33639db911 release: cut the v11.0.2 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+17.sha-2fe2104
+ * @license Angular v11.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21024,7 +21024,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.1+17.sha-2fe2104');
+    var VERSION$1 = new Version('11.0.2');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.1+17.sha-2fe2104
+ * @license Angular v11.0.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19806,7 +19806,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.1+17.sha-2fe2104');
+const VERSION$1 = new Version('11.0.2');
 
 /**
  * @license
