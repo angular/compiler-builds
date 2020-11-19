a927607227aa6635cffb367aa9646faa3d258063
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:45:59 +0000

    be998e830b refactor(core): move `injectAttributeImpl` to avoid cycles (#37085)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+31.sha-a965589
+ * @license Angular v11.1.0-next.0+33.sha-be998e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21157,7 +21157,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+31.sha-a965589');
+    var VERSION$1 = new Version('11.1.0-next.0+33.sha-be998e8');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+31.sha-a965589
+ * @license Angular v11.1.0-next.0+33.sha-be998e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19943,7 +19943,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+31.sha-a965589');
+const VERSION$1 = new Version('11.1.0-next.0+33.sha-be998e8');
 
 /**
  * @license
