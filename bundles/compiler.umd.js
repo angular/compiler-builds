b577f559f530305981b6549944f378e4cbbe7368
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:25:40 +0000

    d001ecc5a0 docs: fix confusing description for strictTemplates (#39745)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+12.sha-f8c6230
+ * @license Angular v11.1.0-next.0+13.sha-d001ecc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21158,7 +21158,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+12.sha-f8c6230');
+    var VERSION$1 = new Version('11.1.0-next.0+13.sha-d001ecc');
 
     /**
      * @license
