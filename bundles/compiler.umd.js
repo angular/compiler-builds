192643cdf3b03d5a4ba999332a80f39826a64fcd
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:02:39 +0000

    1cba56e11f refactor(core): remove unused fakeAsyncFallback and asyncFallback (#37879)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+38.sha-66d863f
+ * @license Angular v11.1.0-next.0+39.sha-1cba56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21157,7 +21157,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+38.sha-66d863f');
+    var VERSION$1 = new Version('11.1.0-next.0+39.sha-1cba56e');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+38.sha-66d863f
+ * @license Angular v11.1.0-next.0+39.sha-1cba56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19943,7 +19943,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+38.sha-66d863f');
+const VERSION$1 = new Version('11.1.0-next.0+39.sha-1cba56e');
 
 /**
  * @license
