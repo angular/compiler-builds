10c8615383fe482442808ad5c4005cf339069199
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:48:45 +0000

    9aaa86967d build(language-service): include sources for Ivy LS (#39748)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+45.sha-156f9f3
+ * @license Angular v11.1.0-next.0+44.sha-9aaa869
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20009,7 +20009,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+45.sha-156f9f3');
+const VERSION$1 = new Version('11.1.0-next.0+44.sha-9aaa869');
 
 /**
  * @license
