5e0fb22ba0a67e2d1c66a6175c06b019fc0f1486
Andrew Kushnir <akushnir@google.com>
2020-11-23 17:42:17 +0000

    7c9b84f528 docs(http): Add mention to default value 'OK' for statusText. #23334 (#23494)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+36.sha-e32fe1a
+ * @license Angular v11.0.2+39.sha-7c9b84f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21089,7 +21089,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.2+36.sha-e32fe1a');
+    var VERSION$1 = new Version('11.0.2+39.sha-7c9b84f');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+36.sha-e32fe1a
+ * @license Angular v11.0.2+39.sha-7c9b84f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19871,7 +19871,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+36.sha-e32fe1a');
+const VERSION$1 = new Version('11.0.2+39.sha-7c9b84f');
 
 /**
  * @license
