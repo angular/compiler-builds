b8952a369860ac042f252aeb31e32464ebe20080
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:11:11 +0000

    39a47c21f9 docs(core): update links to schema.json of @schematics/angular (#39827)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+71.sha-a1b6ad0
+ * @license Angular v11.1.0-next.0+72.sha-39a47c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21306,7 +21306,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+71.sha-a1b6ad0');
+    var VERSION$1 = new Version('11.1.0-next.0+72.sha-39a47c2');
 
     /**
      * @license
