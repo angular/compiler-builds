a078962acc9e741624bd57eb1ad9ed3f8e63c67f
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:31:41 +0000

    75b53690f6 docs: explain foreign function/value constraints in Ivy compatibility examples (#36092)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+49.sha-9cb12ca
+ * @license Angular v11.0.2+51.sha-75b5369
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21089,7 +21089,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.2+49.sha-9cb12ca');
+    var VERSION$1 = new Version('11.0.2+51.sha-75b5369');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+49.sha-9cb12ca
+ * @license Angular v11.0.2+51.sha-75b5369
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19871,7 +19871,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+49.sha-9cb12ca');
+const VERSION$1 = new Version('11.0.2+51.sha-75b5369');
 
 /**
  * @license
