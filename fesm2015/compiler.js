64845f03f85fb8fa69f4fc2d708ce05dce5f7830
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:06:48 +0000

    b5c0f9d7d1 feat(platform-browser): add doubletap HammerJS support (#26362)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+66.sha-88fbc06
+ * @license Angular v11.1.0-next.0+69.sha-b5c0f9d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20096,7 +20096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+66.sha-88fbc06');
+const VERSION$1 = new Version('11.1.0-next.0+69.sha-b5c0f9d');
 
 /**
  * @license
