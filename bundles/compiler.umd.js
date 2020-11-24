04e32043a4abc992df44474784a44e984ef96861
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:10:48 +0000

    8634611e37 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+43.sha-7bf73d7
+ * @license Angular v11.0.2+48.sha-8634611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21089,7 +21089,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.2+43.sha-7bf73d7');
+    var VERSION$1 = new Version('11.0.2+48.sha-8634611');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+43.sha-7bf73d7
+ * @license Angular v11.0.2+48.sha-8634611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19871,7 +19871,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+43.sha-7bf73d7');
+const VERSION$1 = new Version('11.0.2+48.sha-8634611');
 
 /**
  * @license
