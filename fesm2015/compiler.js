1bb092b5c64498f97fbade13e8ba11fa58e3511d
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:05:50 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+64.sha-6046419
+ * @license Angular v11.1.0-next.0+67.sha-2c275f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20096,7 +20096,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+64.sha-6046419');
+const VERSION$1 = new Version('11.1.0-next.0+67.sha-2c275f6');
 
 /**
  * @license
