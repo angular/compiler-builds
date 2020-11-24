36bca5128f15c641f92d132e9af89098b34d03da
Andrew Kushnir <akushnir@google.com>
2020-11-24 22:45:19 +0000

    8758006acc refactor(language-service): create getReferencesAtPosition stubs (#39829)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+83.sha-3a6e7b5
+ * @license Angular v11.1.0-next.0+84.sha-8758006
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20124,7 +20124,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+83.sha-3a6e7b5');
+const VERSION$1 = new Version('11.1.0-next.0+84.sha-8758006');
 
 /**
  * @license
