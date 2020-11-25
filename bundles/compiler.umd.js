fe4d93ef3913ecf404a5d2c13568c34f03ee5efd
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:30:36 +0000

    bf12f51b49 build(docs-infra): upgrade cli command docs sources to 70150e3f0 (#39839)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+92.sha-bfa197f
+ * @license Angular v11.1.0-next.0+93.sha-bf12f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21335,7 +21335,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+92.sha-bfa197f');
+    var VERSION$1 = new Version('11.1.0-next.0+93.sha-bf12f51');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+92.sha-bfa197f
+ * @license Angular v11.1.0-next.0+93.sha-bf12f51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+92.sha-bfa197f');
+const VERSION$1 = new Version('11.1.0-next.0+93.sha-bf12f51');
 
 /**
  * @license
