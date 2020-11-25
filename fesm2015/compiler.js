afe938656d51275598994149f2095a0cdc4a3c18
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 22:47:43 +0000

    c43267b912 build: update peerDependencies of zone.js to 0.10~0.11 (#39809)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+101.sha-ba6945b
+ * @license Angular v11.1.0-next.0+102.sha-c43267b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+101.sha-ba6945b');
+const VERSION$1 = new Version('11.1.0-next.0+102.sha-c43267b');
 
 /**
  * @license
