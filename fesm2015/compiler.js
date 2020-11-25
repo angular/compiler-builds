8a3e6029e4ddd8da241d8ff3a47a6d543f722fd0
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 20:54:46 +0000

    4cfa2098d0 docs(docs-infra): display angulararchitects.io in trainings (#37139)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+59.sha-2bfa8ee
+ * @license Angular v11.0.2+61.sha-4cfa209
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19872,7 +19872,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+59.sha-2bfa8ee');
+const VERSION$1 = new Version('11.0.2+61.sha-4cfa209');
 
 /**
  * @license
