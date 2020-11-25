414238c0cd9e8e22364acd208a3dccf9ded35469
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:57:25 +0000

    01c1bfdbdd fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+61.sha-4cfa209
+ * @license Angular v11.0.2+63.sha-01c1bfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19872,7 +19872,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+61.sha-4cfa209');
+const VERSION$1 = new Version('11.0.2+63.sha-01c1bfd');
 
 /**
  * @license
