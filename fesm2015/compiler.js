4a65c57316355e5454829fa5b8e565e70d5ba234
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:35:16 +0000

    dd3ed3cb2b docs: fix builder section of generated angular.json file for libs (#39888)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
+const VERSION$1 = new Version('11.1.0-next.0+114.sha-dd3ed3c');
 
 /**
  * @license
