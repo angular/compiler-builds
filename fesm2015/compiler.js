df449309bf082fb3839fffe12e6858e66b5c0058
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:32:12 +0000

    71a5314335 test(compiler-cli): migrate providers compliance tests (#39878)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+112.sha-1539c64
+ * @license Angular v11.1.0-next.0+113.sha-71a5314
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+112.sha-1539c64');
+const VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
 
 /**
  * @license
