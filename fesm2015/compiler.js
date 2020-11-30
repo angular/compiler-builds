fbe7662065f265dfb56ecc9a191bc2ce94625cbc
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:31:31 +0000

    93d689ffa8 docs: edit dependency-injection.md (#39255)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+104.sha-68d4a74
+ * @license Angular v11.1.0-next.0+105.sha-93d689f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+104.sha-68d4a74');
+const VERSION$1 = new Version('11.1.0-next.0+105.sha-93d689f');
 
 /**
  * @license
