024a38f2eedab1f3708aa4296ef37d5eac31eb17
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:32:47 +0000

    dd3ed3cb2b docs: fix builder section of generated angular.json file for libs (#39888)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+113.sha-71a5314
+ * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21335,7 +21335,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.0+113.sha-71a5314');
+    var VERSION$1 = new Version('11.1.0-next.0+114.sha-dd3ed3c');
 
     /**
      * @license
