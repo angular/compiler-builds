d8d13e24f3722f29d32810889b6f20fc8a467635
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 23:26:59 +0000

    1de59d2818 docs: edit step 2 of getting started start-routing.md (#39593)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+114.sha-dd3ed3c
+ * @license Angular v11.1.0-next.0+115.sha-1de59d2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+114.sha-dd3ed3c');
+const VERSION$1 = new Version('11.1.0-next.0+115.sha-1de59d2');
 
 /**
  * @license
