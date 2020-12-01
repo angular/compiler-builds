00fd49df57ac9a489ec40824fde3de3dd7ebd2ab
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 19:30:45 +0000

    425e3297f8 build(docs-infra): upgrade cli command docs sources to 4f756ce2f (#39907)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+72.sha-75e22ab
+ * @license Angular v11.0.2+74.sha-425e329
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19872,7 +19872,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.2+72.sha-75e22ab');
+const VERSION$1 = new Version('11.0.2+74.sha-425e329');
 
 /**
  * @license
