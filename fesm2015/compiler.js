1540a3faeb18e825ea4bd99579fa5c71b3643342
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 00:46:55 +0000

    11cd37fae3 fix(core): not invoking object's toString when rendering to the DOM (#39843)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+115.sha-1de59d2
+ * @license Angular v11.1.0-next.0+116.sha-11cd37f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.0+115.sha-1de59d2');
+const VERSION$1 = new Version('11.1.0-next.0+116.sha-11cd37f');
 
 /**
  * @license
