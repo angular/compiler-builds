43c437969b741452ae411a90dad36438e0e0936a
Misko Hevery <misko@hevery.com>
2020-12-02 23:24:23 +0000

    8d613c1d42 feat(compiler): allow trailing comma in array literal (#22277)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+22.sha-744f46c
+ * @license Angular v11.1.0-next.1+25.sha-8d613c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15140,11 +15140,14 @@ class _ParseAST {
     }
     parseExpressionList(terminator) {
         const result = [];
-        if (!this.next.isCharacter(terminator)) {
-            do {
+        do {
+            if (!this.next.isCharacter(terminator)) {
                 result.push(this.parsePipe());
-            } while (this.consumeOptionalCharacter($COMMA));
-        }
+            }
+            else {
+                break;
+            }
+        } while (this.consumeOptionalCharacter($COMMA));
         return result;
     }
     parseLiteralMap() {
@@ -20125,7 +20128,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+22.sha-744f46c');
+const VERSION$1 = new Version('11.1.0-next.1+25.sha-8d613c1');
 
 /**
  * @license
