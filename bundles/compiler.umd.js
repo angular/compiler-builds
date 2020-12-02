4a04064c5db83ca4d01acc5ed731779a5cb00420
Misko Hevery <misko@hevery.com>
2020-12-02 21:17:00 +0000

    75fc89384d refactor(compiler-cli): expose TTC method to determine if file is tracked shim (#39768)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+6.sha-c923aaf
+ * @license Angular v11.1.0-next.1+18.sha-75fc893
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21335,7 +21335,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+6.sha-c923aaf');
+    var VERSION$1 = new Version('11.1.0-next.1+18.sha-75fc893');
 
     /**
      * @license
