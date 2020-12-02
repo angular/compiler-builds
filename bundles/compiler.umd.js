c05b18bcafa4c4dce10a4a7f03c060c4c44dad8b
Joey Perrott <josephperrott@gmail.com>
2020-12-02 18:36:34 +0000

    7001ef6429 release: cut the v11.0.3 release (#39931)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+77.sha-3d19645
+ * @license Angular v11.0.2+78.sha-7001ef6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21090,7 +21090,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.2+77.sha-3d19645');
+    var VERSION$1 = new Version('11.0.2+78.sha-7001ef6');
 
     /**
      * @license
