588d478538e70b62103a61f7958a97b142f20c16
Misko Hevery <misko@hevery.com>
2020-12-02 23:19:07 +0000

    744f46c37d refactor(compiler-cli): migrate view compiler directive tests (#39929)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+20.sha-a55e01b
+ * @license Angular v11.1.0-next.1+22.sha-744f46c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+20.sha-a55e01b');
+const VERSION$1 = new Version('11.1.0-next.1+22.sha-744f46c');
 
 /**
  * @license
