3f4e8ed111ab42e849d63604c74027094f5c1007
Misko Hevery <misko@hevery.com>
2020-12-02 21:26:19 +0000

    a55e01b89c test(core): verify `onDestroy` callbacks are invoked when ComponentRef is destroyed (#39876)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+12.sha-eba185e
+ * @license Angular v11.1.0-next.1+20.sha-a55e01b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20125,7 +20125,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+12.sha-eba185e');
+const VERSION$1 = new Version('11.1.0-next.1+20.sha-a55e01b');
 
 /**
  * @license
