3a1e8db9a4d2eac800b6a01a74946d55d1960859
Misko Hevery <misko@hevery.com>
2020-12-03 19:08:04 +0000

    e84ea8d6a7 docs: add alvarocamillont to GDE resources (#39138)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+26.sha-2b84882
+ * @license Angular v11.1.0-next.1+27.sha-e84ea8d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21338,7 +21338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+26.sha-2b84882');
+    var VERSION$1 = new Version('11.1.0-next.1+27.sha-e84ea8d');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+26.sha-2b84882
+ * @license Angular v11.1.0-next.1+27.sha-e84ea8d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20128,7 +20128,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+26.sha-2b84882');
+const VERSION$1 = new Version('11.1.0-next.1+27.sha-e84ea8d');
 
 /**
  * @license
