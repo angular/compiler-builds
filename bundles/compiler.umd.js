19ab89a486c746fc7185fa2456f45810487163c6
Misko Hevery <misko@hevery.com>
2020-12-03 22:11:53 +0000

    65651b094d docs: remove support for v8 LTS (#39915)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+34.sha-f01c713
+ * @license Angular v11.1.0-next.1+38.sha-65651b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21338,7 +21338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+34.sha-f01c713');
+    var VERSION$1 = new Version('11.1.0-next.1+38.sha-65651b0');
 
     /**
      * @license
