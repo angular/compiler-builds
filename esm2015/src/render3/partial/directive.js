8470559eb3800227150041014d653e2d43d586c2
Misko Hevery <misko@hevery.com>
2020-12-07 18:17:16 +0000

    775394c809 docs: use double colon for pseudo-elements (#40004)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+52.sha-6d19c16'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+53.sha-775394c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
