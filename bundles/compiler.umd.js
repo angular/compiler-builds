4fcb390cfafd37c5b517973a019026cf505589da
Misko Hevery <misko@hevery.com>
2020-12-07 18:17:19 +0000

    6d629713f4 docs: use double colon for pseudo-elements (#40004)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+26.sha-1e3534f
+ * @license Angular v11.0.3+39.sha-6d62971
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21093,7 +21093,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.3+26.sha-1e3534f');
+    var VERSION$1 = new Version('11.0.3+39.sha-6d62971');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+26.sha-1e3534f
+ * @license Angular v11.0.3+39.sha-6d62971
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19875,7 +19875,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.3+26.sha-1e3534f');
+const VERSION$1 = new Version('11.0.3+39.sha-6d62971');
 
 /**
  * @license
