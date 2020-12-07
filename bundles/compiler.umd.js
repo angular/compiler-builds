01e67f67e5bd50529484aae258217add87e476a2
Misko Hevery <misko@hevery.com>
2020-12-07 21:43:44 +0000

    e92d8a8e8f test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+54.sha-5fc4508
+ * @license Angular v11.1.0-next.1+56.sha-e92d8a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21338,7 +21338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+54.sha-5fc4508');
+    var VERSION$1 = new Version('11.1.0-next.1+56.sha-e92d8a8');
 
     /**
      * @license
@@ -31236,7 +31236,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+54.sha-5fc4508'));
+        definitionMap.set('version', literal('11.1.0-next.1+56.sha-e92d8a8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
