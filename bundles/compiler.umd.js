0964e38fd1d81e2d98685a6efcdc374b1fed0dcd
Misko Hevery <misko@hevery.com>
2020-12-07 18:15:51 +0000

    5fc45082ca fix(core): Support extending differs from root `NgModule` (#39981)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+53.sha-775394c
+ * @license Angular v11.1.0-next.1+54.sha-5fc4508
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21338,7 +21338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+53.sha-775394c');
+    var VERSION$1 = new Version('11.1.0-next.1+54.sha-5fc4508');
 
     /**
      * @license
@@ -31236,7 +31236,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+53.sha-775394c'));
+        definitionMap.set('version', literal('11.1.0-next.1+54.sha-5fc4508'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
