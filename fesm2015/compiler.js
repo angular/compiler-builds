053847ccbcd5d6e6fb61ecaffc76681913e9ba57
Misko Hevery <misko@hevery.com>
2020-12-07 18:07:44 +0000

    a8aeb74714 test(upgrade): run tests against AngularJS v1.8.x as well (#39972)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+49.sha-7954c8d
+ * @license Angular v11.1.0-next.1+50.sha-a8aeb74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20128,7 +20128,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+49.sha-7954c8d');
+const VERSION$1 = new Version('11.1.0-next.1+50.sha-a8aeb74');
 
 /**
  * @license
@@ -29650,7 +29650,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.1+49.sha-7954c8d'));
+    definitionMap.set('version', literal('11.1.0-next.1+50.sha-a8aeb74'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+49.sha-7954c8d'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+50.sha-a8aeb74'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
