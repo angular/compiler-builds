5f855859fd022e4b55c7a22a0e681ae776cc0c44
Misko Hevery <misko@hevery.com>
2020-12-07 21:44:44 +0000

    6bb6dfd616 test(compiler-cli): Add test for checkTypeOfDomReferences = false (#39539)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+39.sha-6d62971
+ * @license Angular v11.0.3+41.sha-6bb6dfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21093,7 +21093,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.3+39.sha-6d62971');
+    var VERSION$1 = new Version('11.0.3+41.sha-6bb6dfd');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+39.sha-6d62971
+ * @license Angular v11.0.3+41.sha-6bb6dfd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19875,7 +19875,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.3+39.sha-6d62971');
+const VERSION$1 = new Version('11.0.3+41.sha-6bb6dfd');
 
 /**
  * @license
