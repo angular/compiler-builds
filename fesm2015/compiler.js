45852d752f3b3943a8cce49b93aac5b78a1e8ef3
Misko Hevery <misko@hevery.com>
2020-12-07 18:14:49 +0000

    775394c809 docs: use double colon for pseudo-elements (#40004)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+52.sha-6d19c16
+ * @license Angular v11.1.0-next.1+53.sha-775394c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20128,7 +20128,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+52.sha-6d19c16');
+const VERSION$1 = new Version('11.1.0-next.1+53.sha-775394c');
 
 /**
  * @license
@@ -29650,7 +29650,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.1+52.sha-6d19c16'));
+    definitionMap.set('version', literal('11.1.0-next.1+53.sha-775394c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+52.sha-6d19c16'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+53.sha-775394c'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
