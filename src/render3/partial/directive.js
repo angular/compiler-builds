cd9224d69d7fca176374f71f4187cd1a8ba246b3
Misko Hevery <misko@hevery.com>
2020-12-07 21:48:58 +0000

    7901ebafa0 build(docs-infra): upgrade cli command docs sources to 2eb97bf5b (#39943)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+56.sha-e92d8a8'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+55.sha-7901eba'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
