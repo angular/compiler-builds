8d04337e16a1437d1a729e302aca65ded58ab133
Misko Hevery <misko@hevery.com>
2020-12-07 18:12:23 +0000

    d9356f2842 feat(dev-infra): close pull requests on merge which do not target master branch (#39979)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+50.sha-a8aeb74
+ * @license Angular v11.1.0-next.1+51.sha-d9356f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21338,7 +21338,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+50.sha-a8aeb74');
+    var VERSION$1 = new Version('11.1.0-next.1+51.sha-d9356f2');
 
     /**
      * @license
@@ -31236,7 +31236,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+50.sha-a8aeb74'));
+        definitionMap.set('version', literal('11.1.0-next.1+51.sha-d9356f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+50.sha-a8aeb74'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+51.sha-d9356f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
