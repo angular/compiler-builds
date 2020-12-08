baff8499dc5bb3338dc4b8d4b7dace498deb9c5f
Misko Hevery <misko@hevery.com>
2020-12-08 17:18:48 +0000

    e8d2348e21 docs:  how to create a custom Attribute decorator in a child component and use it from a parent component (#38721)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+66.sha-f408af3'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+65.sha-e8d2348'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
