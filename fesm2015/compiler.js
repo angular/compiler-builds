405a0e3534a12aa2f9a4c2aea3acbe37f027cd2d
Misko Hevery <misko@hevery.com>
2020-12-08 20:36:27 +0000

    65e93ea7b3 ci: add atscott to language-service approvers (#40027)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+56.sha-19309b5
+ * @license Angular v11.0.3+63.sha-65e93ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19963,7 +19963,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.3+56.sha-19309b5');
+const VERSION$1 = new Version('11.0.3+63.sha-65e93ea');
 
 /**
  * @license
