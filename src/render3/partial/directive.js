5a52062f2a60e36ff7cc749be65a5a4f497b2e43
Misko Hevery <misko@hevery.com>
2020-12-08 17:16:25 +0000

    25e3dc594d refactor(dev-infra): separate retrieving targeted branches from printing them (#39897)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+66.sha-f408af3'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+68.sha-25e3dc5'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
