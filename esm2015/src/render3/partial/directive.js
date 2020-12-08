496e2736659932a88c1f0aa27ca4a6416ea4163b
Misko Hevery <misko@hevery.com>
2020-12-08 19:15:47 +0000

    a694838c41 refactor(compiler-cli): TemplateTypeChecker with checkTypeOfAttributes=false should still work (#39537)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+76.sha-5a2a907'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+73.sha-a694838'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
