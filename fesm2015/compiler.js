744e4cb2496f447fb5a27cfa64d1a2295b7b7668
Misko Hevery <misko@hevery.com>
2020-12-08 20:36:11 +0000

    55c21fadc8 ci: add atscott to language-service approvers (#40027)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+86.sha-74e42cf
+ * @license Angular v11.1.0-next.1+89.sha-55c21fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20221,7 +20221,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+86.sha-74e42cf');
+const VERSION$1 = new Version('11.1.0-next.1+89.sha-55c21fa');
 
 /**
  * @license
@@ -29751,7 +29751,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.1+86.sha-74e42cf'));
+    definitionMap.set('version', literal('11.1.0-next.1+89.sha-55c21fa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
