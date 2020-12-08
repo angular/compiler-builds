c5bb6517193c861c97cb2dec3c4cddc966514710
Misko Hevery <misko@hevery.com>
2020-12-08 17:37:42 +0000

    86fdc77ddf docs: replace image with one containing correct text (#40022)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+71.sha-2ffd739'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+72.sha-86fdc77'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.1+71.sha-2ffd739'));
+        definitionMap.set('version', o.literal('11.1.0-next.1+72.sha-86fdc77'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
