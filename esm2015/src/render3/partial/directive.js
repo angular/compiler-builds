c8016a99ddfacf07490b389555fb5f3a992d5fbd
Misko Hevery <misko@hevery.com>
2020-12-08 00:50:12 +0000

    b8714c384f test(compiler-cli): add source-map compliance tests for inline-templates (#39939)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+55.sha-7901eba'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+62.sha-b8714c3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
