d86d4bc074d7762acd3d0fd100df902845276785
Misko Hevery <misko@hevery.com>
2020-12-08 17:19:00 +0000

    25e3dc594d refactor(dev-infra): separate retrieving targeted branches from printing them (#39897)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+65.sha-e8d2348'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+68.sha-25e3dc5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
