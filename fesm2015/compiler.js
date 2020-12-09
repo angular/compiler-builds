cb3e3adb252a0479f9b459c4f0fa4674bc3c81e5
Misko Hevery <misko@hevery.com>
2020-12-09 03:39:20 +0000

    cfb094dbaf test(compiler-cli): temporary disable source mapping tests which have errors (#40033)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+92.sha-2f8a420
+ * @license Angular v11.1.0-next.1+95.sha-cfb094d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20221,7 +20221,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+92.sha-2f8a420');
+const VERSION$1 = new Version('11.1.0-next.1+95.sha-cfb094d');
 
 /**
  * @license
@@ -29751,7 +29751,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.1+92.sha-2f8a420'));
+    definitionMap.set('version', literal('11.1.0-next.1+95.sha-cfb094d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
