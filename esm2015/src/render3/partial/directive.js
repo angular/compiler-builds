f2362dd1c80b9a6d08f315a643f7633d502accea
Misko Hevery <misko@hevery.com>
2020-12-09 17:29:19 +0000

    13dfef14dc docs: remove cursor: hand (#39918)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+100.sha-e39f923'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+99.sha-13dfef1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
