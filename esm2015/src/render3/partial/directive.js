c91492a8184e4a6ac734ba5bc45d32978f30359d
Misko Hevery <misko@hevery.com>
2020-12-09 17:21:51 +0000

    1c193af65f docs: add angular-prerender to resources.json (#39387)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.1+96.sha-bb3f207'));
+    definitionMap.set('version', o.literal('11.1.0-next.1+97.sha-1c193af'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
