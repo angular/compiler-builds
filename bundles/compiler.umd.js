460eb6333f77a7470fcd1fa323949bf1e27f0c02
Misko Hevery <misko@hevery.com>
2020-12-09 17:36:48 +0000

    42d478b16a docs(common): use table layout for Pre-defined format options (#40036)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+101.sha-13126d2
+ * @license Angular v11.1.0-next.1+102.sha-42d478b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21438,7 +21438,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+101.sha-13126d2');
+    var VERSION$1 = new Version('11.1.0-next.1+102.sha-42d478b');
 
     /**
      * @license
@@ -31344,7 +31344,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+101.sha-13126d2'));
+        definitionMap.set('version', literal('11.1.0-next.1+102.sha-42d478b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
