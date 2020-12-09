ad5e37551b7777d155bbde0c2b98b46561a437e1
Misko Hevery <misko@hevery.com>
2020-12-09 00:49:18 +0000

    5a765f0be9 fix(animations): implement getPosition in browser animation builder (#39983)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+63.sha-65e93ea
+ * @license Angular v11.0.3+66.sha-5a765f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21188,7 +21188,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.3+63.sha-65e93ea');
+    var VERSION$1 = new Version('11.0.3+66.sha-5a765f0');
 
     /**
      * @license
