0ca93987b2d6dea375dd964114c428c784d6e59b
Misko Hevery <misko@hevery.com>
2020-12-09 17:21:14 +0000

    bb3f207f44 docs: add reason to use Angular in README (#36927)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+95.sha-cfb094d
+ * @license Angular v11.1.0-next.1+96.sha-bb3f207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21438,7 +21438,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+95.sha-cfb094d');
+    var VERSION$1 = new Version('11.1.0-next.1+96.sha-bb3f207');
 
     /**
      * @license
@@ -31344,7 +31344,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+95.sha-cfb094d'));
+        definitionMap.set('version', literal('11.1.0-next.1+96.sha-bb3f207'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
