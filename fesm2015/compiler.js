5181a2e94a40f0feba85f07f01e3705769ee6770
Misko Hevery <misko@hevery.com>
2020-12-09 17:23:16 +0000

    fc323d08d0 docs: fix typos in the Style Precedence guide (#39560)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+97.sha-1c193af
+ * @license Angular v11.1.0-next.1+98.sha-fc323d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20221,7 +20221,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.1+97.sha-1c193af');
+const VERSION$1 = new Version('11.1.0-next.1+98.sha-fc323d0');
 
 /**
  * @license
@@ -29751,7 +29751,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.1+97.sha-1c193af'));
+    definitionMap.set('version', literal('11.1.0-next.1+98.sha-fc323d0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
