c8214ac8f944ac8f904ef4c096c57d132df667e5
Misko Hevery <misko@hevery.com>
2020-12-09 17:31:57 +0000

    13dfef14dc docs: remove cursor: hand (#39918)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+100.sha-e39f923
+ * @license Angular v11.1.0-next.1+99.sha-13dfef1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21438,7 +21438,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+100.sha-e39f923');
+    var VERSION$1 = new Version('11.1.0-next.1+99.sha-13dfef1');
 
     /**
      * @license
@@ -31344,7 +31344,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+100.sha-e39f923'));
+        definitionMap.set('version', literal('11.1.0-next.1+99.sha-13dfef1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
