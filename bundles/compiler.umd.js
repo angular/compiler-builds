e6676be6df2348261d62351be2609c5bc2612019
Misko Hevery <misko@hevery.com>
2020-12-09 17:25:27 +0000

    e39f923ff9 ci: update pullapprove config to add comp labels (#39962)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+98.sha-fc323d0
+ * @license Angular v11.1.0-next.1+100.sha-e39f923
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21438,7 +21438,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.1+98.sha-fc323d0');
+    var VERSION$1 = new Version('11.1.0-next.1+100.sha-e39f923');
 
     /**
      * @license
@@ -31344,7 +31344,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.1+98.sha-fc323d0'));
+        definitionMap.set('version', literal('11.1.0-next.1+100.sha-e39f923'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
