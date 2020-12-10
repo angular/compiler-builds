e632bb85ee394a7a4962768f2f6bb55d310afad5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:56:18 +0000

    e90a7f7ff1 docs: remove unused CSS rules (#40069)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+3.sha-f5aab2b
+ * @license Angular v11.0.4+15.sha-e90a7f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19901,7 +19901,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.4+3.sha-f5aab2b');
+const VERSION$1 = new Version('11.0.4+15.sha-e90a7f7');
 
 /**
  * @license
