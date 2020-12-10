e654e0f4d985a2479bb85af4e4db6eeb8b565034
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:56:25 +0000

    b08fd0c18c docs: remove unused CSS rules (#40069)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+23.sha-91fb830'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+24.sha-b08fd0c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
