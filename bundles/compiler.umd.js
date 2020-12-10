431e47b33939c29c245448015bbdb837e4ac67c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:01:02 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+9.sha-1f73af7
+ * @license Angular v11.1.0-next.2+10.sha-76e3de2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21473,7 +21473,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.2+9.sha-1f73af7');
+    var VERSION$1 = new Version('11.1.0-next.2+10.sha-76e3de2');
 
     /**
      * @license
@@ -31379,7 +31379,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.2+9.sha-1f73af7'));
+        definitionMap.set('version', literal('11.1.0-next.2+10.sha-76e3de2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
