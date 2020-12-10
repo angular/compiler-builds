664b4b81fa9351f8f79f69670f426250f1dce214
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:56:10 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+21.sha-79a42d9
+ * @license Angular v11.1.0-next.2+23.sha-91fb830
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21473,7 +21473,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.2+21.sha-79a42d9');
+    var VERSION$1 = new Version('11.1.0-next.2+23.sha-91fb830');
 
     /**
      * @license
@@ -31379,7 +31379,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.2+21.sha-79a42d9'));
+        definitionMap.set('version', literal('11.1.0-next.2+23.sha-91fb830'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+21.sha-79a42d9'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+23.sha-91fb830'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
