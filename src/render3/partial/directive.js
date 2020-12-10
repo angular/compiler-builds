3f655bd51d49870add53ddf5154632189212e161
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:12:09 +0000

    0817758b6a feat(docs-infra): add no upcoming events message (#40055)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.2+16.sha-973f797'));
+        definitionMap.set('version', o.literal('11.1.0-next.2+17.sha-0817758'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
