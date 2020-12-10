94881790931bff85046f9c97a4631732b9acc94a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:53:34 +0000

    91fb830c86 docs: remove prefixed border-radius related properties (#40066)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+22.sha-fea9505'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+23.sha-91fb830'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.2+22.sha-fea9505'));
+        definitionMap.set('version', o.literal('11.1.0-next.2+23.sha-91fb830'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
