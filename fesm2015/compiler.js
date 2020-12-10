1505180677872f55546ee919d33750e5c81b9e0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:09:39 +0000

    0817758b6a feat(docs-infra): add no upcoming events message (#40055)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+16.sha-973f797
+ * @license Angular v11.1.0-next.2+17.sha-0817758
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20256,7 +20256,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.2+16.sha-973f797');
+const VERSION$1 = new Version('11.1.0-next.2+17.sha-0817758');
 
 /**
  * @license
@@ -29786,7 +29786,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.2+16.sha-973f797'));
+    definitionMap.set('version', literal('11.1.0-next.2+17.sha-0817758'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
