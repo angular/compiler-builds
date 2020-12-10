b378ecf8bec6fc688e778217d80692e6e4f6a3ab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:03:27 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+9.sha-1f73af7'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+10.sha-76e3de2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
