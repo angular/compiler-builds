016b539771916066fb86b149a21281abc0f4649a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:02:51 +0000

    4f4f318d28 docs: remove prefixed animation related CSS properties (#40084)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.2+30.sha-35a1975'));
+        definitionMap.set('version', o.literal('11.1.0-next.2+35.sha-4f4f318'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
