5db9f0b97da8e08b5680e72e12420954799daaf0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:00:14 +0000

    4f4f318d28 docs: remove prefixed animation related CSS properties (#40084)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+30.sha-35a1975'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+35.sha-4f4f318'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
