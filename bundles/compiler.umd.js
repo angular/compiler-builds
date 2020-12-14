88be521fadc5aacf09d54a642abbf4afbf1bbd46
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:59:40 +0000

    35a1975697 build: update to use npm_package.pack rule (#39636)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+34.sha-1e05109
+ * @license Angular v11.1.0-next.2+30.sha-35a1975
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21473,7 +21473,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.2+34.sha-1e05109');
+    var VERSION$1 = new Version('11.1.0-next.2+30.sha-35a1975');
 
     /**
      * @license
@@ -31379,7 +31379,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.2+34.sha-1e05109'));
+        definitionMap.set('version', literal('11.1.0-next.2+30.sha-35a1975'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.2+34.sha-1e05109'));
+        definitionMap.set('version', o.literal('11.1.0-next.2+30.sha-35a1975'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
