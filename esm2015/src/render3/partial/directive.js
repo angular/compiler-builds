8e651473473294384e7dc52b507b825405292f58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 21:57:09 +0000

    dcb3d176a6 docs: update optimization and source map section (#40037)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+47.sha-973bb40'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+50.sha-dcb3d17'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
