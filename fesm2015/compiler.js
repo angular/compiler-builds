36f2d8c2f00079a780c561fdbeff5dc9bf6a2c23
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:45:03 +0000

    70b4816cd5 fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+55.sha-47d9b6d
+ * @license Angular v11.1.0-next.2+56.sha-70b4816
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.2+55.sha-47d9b6d');
+const VERSION$1 = new Version('11.1.0-next.2+56.sha-70b4816');
 
 /**
  * @license
@@ -29814,7 +29814,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.2+55.sha-47d9b6d'));
+    definitionMap.set('version', literal('11.1.0-next.2+56.sha-70b4816'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
