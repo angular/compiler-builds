3166f92871149a2a8f515df5e8bd23023fb0487b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 18:01:47 +0000

    47d9b6d72d fix(core): fix possible XSS attack in development through SSR. (#40136)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+54.sha-2e7727c'));
+    definitionMap.set('version', o.literal('11.1.0-next.2+55.sha-47d9b6d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+54.sha-2e7727c
+ * @license Angular v11.1.0-next.2+55.sha-47d9b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.2+54.sha-2e7727c');
+const VERSION$1 = new Version('11.1.0-next.2+55.sha-47d9b6d');
 
 /**
  * @license
@@ -29814,7 +29814,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.2+54.sha-2e7727c'));
+    definitionMap.set('version', literal('11.1.0-next.2+55.sha-47d9b6d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
