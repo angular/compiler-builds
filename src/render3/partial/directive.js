b805d578c3b8921e7991866018b1c904a580fbd7
Joey Perrott <josephperrott@gmail.com>
2020-12-17 19:44:46 +0000

    effc079831 build: use "outFiles" in .vscode/launch.json to speed up debugging sessions (#39848)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+3.sha-356a256'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+4.sha-effc079'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
