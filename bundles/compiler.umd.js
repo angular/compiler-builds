4db0cc0653a57514664c9e8e2fa65a35ba40dee8
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:05:10 +0000

    c53bae839d ci: fail CI jobs when rebasing on master fails (#40161)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+1.sha-ee6b8a7
+ * @license Angular v11.1.0-next.3+2.sha-c53bae8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21501,7 +21501,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+1.sha-ee6b8a7');
+    var VERSION$1 = new Version('11.1.0-next.3+2.sha-c53bae8');
 
     /**
      * @license
@@ -31407,7 +31407,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+1.sha-ee6b8a7'));
+        definitionMap.set('version', literal('11.1.0-next.3+2.sha-c53bae8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
