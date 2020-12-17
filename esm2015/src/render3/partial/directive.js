0663d9529856b206cb15d942332fcad4ca6fcd88
Joey Perrott <josephperrott@gmail.com>
2020-12-17 17:56:42 +0000

    ee6b8a7afb Revert "build: update bazel rules_nodejs to version 2.3.1 (#39636)" (#40174)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.2+57.sha-0fc8466'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+1.sha-ee6b8a7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
