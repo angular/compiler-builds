2c868022e5ecb04f93f651e31415ba654c43d052
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:28:58 +0000

    2075cf79a3 docs: use an explicit product id in the getting started example (#34934)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+2.sha-ab34211
+ * @license Angular v11.0.5+5.sha-2075cf7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19901,7 +19901,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.5+2.sha-ab34211');
+const VERSION$1 = new Version('11.0.5+5.sha-2075cf7');
 
 /**
  * @license
