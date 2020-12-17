cfec265c0a77f00e6943019bd8742889d4ed7518
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:15:50 +0000

    475468cc8f refactor(core): remove custom globalThis (#40123)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+4.sha-effc079
+ * @license Angular v11.1.0-next.3+5.sha-475468c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.3+4.sha-effc079');
+const VERSION$1 = new Version('11.1.0-next.3+5.sha-475468c');
 
 /**
  * @license
@@ -29814,7 +29814,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.3+4.sha-effc079'));
+    definitionMap.set('version', literal('11.1.0-next.3+5.sha-475468c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
