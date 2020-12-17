00d947b8adeb50e465632b948c30ded942653e5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-17 00:05:33 +0000

    0fc8466f1b release: cut the v11.1.0-next.3 release (#40166)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+56.sha-70b4816
+ * @license Angular v11.1.0-next.2+57.sha-0fc8466
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20284,7 +20284,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.2+56.sha-70b4816');
+const VERSION$1 = new Version('11.1.0-next.2+57.sha-0fc8466');
 
 /**
  * @license
@@ -29814,7 +29814,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.2+56.sha-70b4816'));
+    definitionMap.set('version', literal('11.1.0-next.2+57.sha-0fc8466'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
