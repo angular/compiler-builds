86578019decd49eb403a4f7b8fe0fe92eb92b51a
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:35:31 +0000

    5831d2b685 ci: correctly rebase PRs for branches contain a slash (/) (#40184)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+6.sha-362f45c'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+8.sha-5831d2b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
