165071c9c2b5bdf32ec1ca27b312aed2297d3793
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:43:09 +0000

    eae07e10aa docs: fix styles in binding guides (#40182)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+9.sha-f7642ce
+ * @license Angular v11.1.0-next.3+10.sha-eae07e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21501,7 +21501,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+9.sha-f7642ce');
+    var VERSION$1 = new Version('11.1.0-next.3+10.sha-eae07e1');
 
     /**
      * @license
@@ -31407,7 +31407,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+9.sha-f7642ce'));
+        definitionMap.set('version', literal('11.1.0-next.3+10.sha-eae07e1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
