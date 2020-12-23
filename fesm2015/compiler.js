af40dc152449a43a7cc113c84c3f38bd20de04d6
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:28:46 +0000

    c22d00547a docs(router): fix typo (#40244)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+31.sha-9186f1f
+ * @license Angular v11.1.0-next.3+32.sha-c22d005
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20354,7 +20354,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.3+31.sha-9186f1f');
+const VERSION$1 = new Version('11.1.0-next.3+32.sha-c22d005');
 
 /**
  * @license
@@ -29884,7 +29884,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.3+31.sha-9186f1f'));
+    definitionMap.set('version', literal('11.1.0-next.3+32.sha-c22d005'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
