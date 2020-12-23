34484bd1f20993823ca5e503a5ee9869350927d7
Joey Perrott <josephperrott@gmail.com>
2020-12-23 21:31:09 +0000

    d7665fdc96 build(docs-infra): upgrade cli command docs sources to 4cefc7dc1 (#40250)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+32.sha-c22d005
+ * @license Angular v11.1.0-next.3+33.sha-d7665fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21583,7 +21583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+32.sha-c22d005');
+    var VERSION$1 = new Version('11.1.0-next.3+33.sha-d7665fd');
 
     /**
      * @license
@@ -31489,7 +31489,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+32.sha-c22d005'));
+        definitionMap.set('version', literal('11.1.0-next.3+33.sha-d7665fd'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+32.sha-c22d005'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+33.sha-d7665fd'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
