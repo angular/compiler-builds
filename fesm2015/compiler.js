0dafa853215e4f078e194db88824c10d09c023c1
Joey Perrott <josephperrott@gmail.com>
2020-12-23 17:00:43 +0000

    9a149d4c49 docs: use kebab-case for CLI commands (#40240)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+23.sha-ef13e83
+ * @license Angular v11.0.5+24.sha-9a149d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19901,7 +19901,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.5+23.sha-ef13e83');
+const VERSION$1 = new Version('11.0.5+24.sha-9a149d4');
 
 /**
  * @license
