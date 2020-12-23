bea6d71029bed5151cf77be0a1584092caa39200
Joey Perrott <josephperrott@gmail.com>
2020-12-23 17:00:34 +0000

    53b64fb659 docs: use kebab-case for CLI commands (#40240)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+27.sha-e4fbab9
+ * @license Angular v11.1.0-next.3+29.sha-53b64fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21502,7 +21502,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+27.sha-e4fbab9');
+    var VERSION$1 = new Version('11.1.0-next.3+29.sha-53b64fb');
 
     /**
      * @license
@@ -31408,7 +31408,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+27.sha-e4fbab9'));
+        definitionMap.set('version', literal('11.1.0-next.3+29.sha-53b64fb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
