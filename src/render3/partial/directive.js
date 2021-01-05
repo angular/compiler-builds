5c88f6d07c7aa7b2db56b1fd132977467988ea21
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:11:30 +0000

    4eac7e6436 fix(router): Router should focus element after scrolling (#40241)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+34.sha-9248ee2'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+37.sha-4eac7e6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
