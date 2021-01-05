cd3beb1ca26c74c0a918a8bdd7fd36997957f623
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:22:44 +0000

    6a9d7e5969 refactor(core): express trusted constants with tagged template literals (#40082)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+53.sha-8cdfd77'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+54.sha-6a9d7e5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
