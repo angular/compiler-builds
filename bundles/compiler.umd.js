020e688b26a4fd1e5e1329c65954df8c39df5529
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:55:46 +0000

    b473bc226c ci: remove autolabeling from pullapprove config (#40314)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+43.sha-a384961
+ * @license Angular v11.1.0-next.3+44.sha-b473bc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21583,7 +21583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+43.sha-a384961');
+    var VERSION$1 = new Version('11.1.0-next.3+44.sha-b473bc2');
 
     /**
      * @license
@@ -31489,7 +31489,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+43.sha-a384961'));
+        definitionMap.set('version', literal('11.1.0-next.3+44.sha-b473bc2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
