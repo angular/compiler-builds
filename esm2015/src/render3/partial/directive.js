8ed2de864e557cbc39eb7e1b6ce5126b5cc25b82
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:21:21 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+52.sha-c1d7805'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+53.sha-8cdfd77'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+52.sha-c1d7805'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+53.sha-8cdfd77'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
