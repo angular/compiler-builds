55971bdb4c4c7d9bb23bb692aaf4b04d82d79430
Joey Perrott <josephperrott@gmail.com>
2021-01-05 19:42:54 +0000

    a3849611b7 fix(forms): clean up connection between FormControl/FormGroup and corresponding directive instances (#39235)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+42.sha-3735633'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+43.sha-a384961'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
