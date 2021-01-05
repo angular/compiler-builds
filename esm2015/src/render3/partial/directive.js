06ed8c4bc4710ced6074af8ada153baaf7adb126
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:27:07 +0000

    605775398f docs(router): Clarify 'parent' for params inheritance (#40304)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+54.sha-6a9d7e5'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+51.sha-6057753'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+54.sha-6a9d7e5'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+51.sha-6057753'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
