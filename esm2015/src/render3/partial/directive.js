43aa693cd703689de72b7e8e727bf081d546c144
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:17:02 +0000

    c1d7805acc docs(core): Fix spelling (#40296) PR Close #40296

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+50.sha-e43f7e2'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+52.sha-c1d7805'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
