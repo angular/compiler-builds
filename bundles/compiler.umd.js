c966120da4be849eb27dd4ebeeba58c5477e1bf1
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:23:52 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+52.sha-c1d7805
+ * @license Angular v11.1.0-next.3+53.sha-8cdfd77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21583,7 +21583,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+52.sha-c1d7805');
+    var VERSION$1 = new Version('11.1.0-next.3+53.sha-8cdfd77');
 
     /**
      * @license
@@ -31489,7 +31489,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+52.sha-c1d7805'));
+        definitionMap.set('version', literal('11.1.0-next.3+53.sha-8cdfd77'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
