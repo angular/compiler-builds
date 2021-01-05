5f83588b930d3690db480a0cac090ace8d5d78ea
Joey Perrott <josephperrott@gmail.com>
2021-01-05 21:09:19 +0000

    e43f7e26fe fix(router): apply redirects should match named outlets with empty path parents (#40029)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+44.sha-b473bc2'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+50.sha-e43f7e2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
