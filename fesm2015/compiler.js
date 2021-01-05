b1adcce837f98b32044fbddd1090997cd460e177
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:41:55 +0000

    3735633bb0 fix(core): take @Host into account while processing `useFactory` arguments (#40122)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+40.sha-805b4f9
+ * @license Angular v11.1.0-next.3+42.sha-3735633
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20354,7 +20354,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.3+40.sha-805b4f9');
+const VERSION$1 = new Version('11.1.0-next.3+42.sha-3735633');
 
 /**
  * @license
@@ -29884,7 +29884,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.3+40.sha-805b4f9'));
+    definitionMap.set('version', literal('11.1.0-next.3+42.sha-3735633'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
