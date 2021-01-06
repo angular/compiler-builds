71b16fb1ef3ed917ecea3754a5e26daa4aea3eea
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:56:22 +0000

    c130812afe docs: fix `fullTemplateTypeCheck` compiler option description (#40253)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+62.sha-7eadf2e'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+61.sha-c130812'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
