bf4071790b725af603f51154165dcfa4501583dc
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:38:24 +0000

    7c167629b8 fix(compiler-cli): ngcc - remove outdated link (#40285)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+59.sha-f846c09'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+60.sha-7c16762'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+59.sha-f846c09'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+60.sha-7c16762'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
