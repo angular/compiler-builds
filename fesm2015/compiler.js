cddef7e587f3bb3d4ad9089e89d1d212a2023f88
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:20:15 +0000

    0264f76e94 fix(language-service): LSParseConfigHost.resolve should not concat abs paths (#40242)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+68.sha-335d6c8
+ * @license Angular v11.1.0-next.3+70.sha-0264f76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20422,7 +20422,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.3+68.sha-335d6c8');
+const VERSION$1 = new Version('11.1.0-next.3+70.sha-0264f76');
 
 /**
  * @license
@@ -29952,7 +29952,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.3+68.sha-335d6c8'));
+    definitionMap.set('version', literal('11.1.0-next.3+70.sha-0264f76'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+68.sha-335d6c8'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+70.sha-0264f76'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
