ae265d52066310f690cb999e9c970d387bf64fd5
Joey Perrott <josephperrott@gmail.com>
2021-01-06 18:57:58 +0000

    8dbd2204c3 fix(dev-infra): fix `yarn symbol-extractor` command (#40163)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+65.sha-fdec650
+ * @license Angular v11.1.0-next.3+66.sha-8dbd220
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21687,7 +21687,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+65.sha-fdec650');
+    var VERSION$1 = new Version('11.1.0-next.3+66.sha-8dbd220');
 
     /**
      * @license
@@ -31593,7 +31593,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+65.sha-fdec650'));
+        definitionMap.set('version', literal('11.1.0-next.3+66.sha-8dbd220'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+65.sha-fdec650'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+66.sha-8dbd220'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
