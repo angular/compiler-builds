75b262aa3700b8fe978461d8f59148c8e3c558d9
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:57:04 +0000

    183fb7e7b9 fix(language-service): return all typecheck files via getExternalFiles (#40162)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+70.sha-0264f76'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+71.sha-183fb7e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
