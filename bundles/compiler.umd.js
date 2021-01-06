ea4cfe2880f0c2cb0d303b04014031c609eb6c4b
atscott <atscott01@gmail.com>
2021-01-06 22:22:01 +0000

    1e151f9fe4 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+3.sha-989b4a9
+ * @license Angular v11.1.0-next.4+7.sha-1e151f9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21701,7 +21701,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+3.sha-989b4a9');
+    var VERSION$1 = new Version('11.1.0-next.4+7.sha-1e151f9');
 
     /**
      * @license
@@ -31607,7 +31607,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+3.sha-989b4a9'));
+        definitionMap.set('version', literal('11.1.0-next.4+7.sha-1e151f9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
