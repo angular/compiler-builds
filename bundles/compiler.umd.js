fe3dfd5744659b618d14b9cd37c74577d2078702
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:58:59 +0000

    c130812afe docs: fix `fullTemplateTypeCheck` compiler option description (#40253)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+62.sha-7eadf2e
+ * @license Angular v11.1.0-next.3+61.sha-c130812
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21622,7 +21622,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+62.sha-7eadf2e');
+    var VERSION$1 = new Version('11.1.0-next.3+61.sha-c130812');
 
     /**
      * @license
@@ -31528,7 +31528,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+62.sha-7eadf2e'));
+        definitionMap.set('version', literal('11.1.0-next.3+61.sha-c130812'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
