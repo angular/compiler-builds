c92c7a48df81df1feeed7623952001b0b8678fa6
Joey Perrott <josephperrott@gmail.com>
2021-01-06 18:59:48 +0000

    8ebac24b48 fix(core): ensure sanitizer works if DOMParser return null body (#40107)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.3+66.sha-8dbd220'));
+    definitionMap.set('version', o.literal('11.1.0-next.3+67.sha-8ebac24'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
