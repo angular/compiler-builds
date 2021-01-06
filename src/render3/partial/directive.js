629356ccc66e146a1828fc0ee2bf5086a8bb2b32
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:35:09 +0000

    f846c093b1 docs: specify fallback generic font family (#40254)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+57.sha-d5a7a26'));
+        definitionMap.set('version', o.literal('11.1.0-next.3+59.sha-f846c09'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
