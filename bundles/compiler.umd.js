f5a0ed5308564b49fe9b55e2c0a22749a21072f3
GitHub <noreply@github.com>
2021-01-06 21:45:47 +0000

    bff48a0885 release: cut the v11.1.0-next.4 release (#40330)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.3+72.sha-c81d5bf
+ * @license Angular v11.1.0-next.3+73.sha-bff48a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21701,7 +21701,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.3+72.sha-c81d5bf');
+    var VERSION$1 = new Version('11.1.0-next.3+73.sha-bff48a0');
 
     /**
      * @license
@@ -31607,7 +31607,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.3+72.sha-c81d5bf'));
+        definitionMap.set('version', literal('11.1.0-next.3+73.sha-bff48a0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
