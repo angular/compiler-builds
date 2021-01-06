1e806edf913b7d6c2ee76813c560447d3b803724
atscott <atscott01@gmail.com>
2021-01-06 22:12:20 +0000

    13020f904f fix(router): correctly deactivate children with componentless parent (#40196)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.3+73.sha-bff48a0'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+1.sha-13020f9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
