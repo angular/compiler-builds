4c2a085ca231ff576fd3dca818274b55669c79a5
atscott <atscott01@gmail.com>
2021-01-07 21:29:56 +0000

    9be9e466b1 docs: remove duplicated 'the' (#40333)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+13.sha-aea265b
+ * @license Angular v11.1.0-next.4+12.sha-9be9e46
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20422,7 +20422,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+13.sha-aea265b');
+const VERSION$1 = new Version('11.1.0-next.4+12.sha-9be9e46');
 
 /**
  * @license
@@ -29952,7 +29952,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+13.sha-aea265b'));
+    definitionMap.set('version', literal('11.1.0-next.4+12.sha-9be9e46'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
