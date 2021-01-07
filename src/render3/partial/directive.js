84a57346757414f07acc18237ed5a650501396d4
atscott <atscott01@gmail.com>
2021-01-07 21:54:21 +0000

    da6c739bb6 test(core): update test expectation to account for IE11 anonymous function name (#40342)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+30.sha-15b15be'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+31.sha-da6c739'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
