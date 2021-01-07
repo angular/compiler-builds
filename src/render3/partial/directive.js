687e35ee399e404bc25a7ba2a43250fde0bf3275
atscott <atscott01@gmail.com>
2021-01-07 21:32:39 +0000

    9be9e466b1 docs: remove duplicated 'the' (#40333)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+13.sha-aea265b'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+12.sha-9be9e46'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
