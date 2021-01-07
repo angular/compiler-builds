884e1bfeda4c59f1eb588db1966a1e6055a1f059
atscott <atscott01@gmail.com>
2021-01-07 21:32:01 +0000

    335c1ab37f docs: add Craig Spence to contributor list (#40335)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+10.sha-1437f4c
+ * @license Angular v11.1.0-next.4+11.sha-335c1ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20422,7 +20422,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+10.sha-1437f4c');
+const VERSION$1 = new Version('11.1.0-next.4+11.sha-335c1ab');
 
 /**
  * @license
@@ -29952,7 +29952,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+10.sha-1437f4c'));
+    definitionMap.set('version', literal('11.1.0-next.4+11.sha-335c1ab'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
