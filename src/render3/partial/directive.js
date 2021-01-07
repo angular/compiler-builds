641a964997d2fd0147c6617bdba1d2ce51135951
atscott <atscott01@gmail.com>
2021-01-07 21:29:43 +0000

    aea265bfc5 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+9.sha-482a4c6'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+13.sha-aea265b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
