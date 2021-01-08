ffcc28a2ba7c548a10ec0d61a88d65cecc544f47
Andrew Scott <atscott@google.com>
2021-01-08 18:07:51 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+35.sha-e27b920
+ * @license Angular v11.1.0-next.4+37.sha-7a819ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21710,7 +21710,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+35.sha-e27b920');
+    var VERSION$1 = new Version('11.1.0-next.4+37.sha-7a819ca');
 
     /**
      * @license
@@ -31616,7 +31616,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+35.sha-e27b920'));
+        definitionMap.set('version', literal('11.1.0-next.4+37.sha-7a819ca'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
