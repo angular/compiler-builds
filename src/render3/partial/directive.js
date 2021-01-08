47757c8281e619e7b50d58bfddf4deb5f27c1613
Andrew Scott <atscott@google.com>
2021-01-08 17:58:22 +0000

    e27b920ac3 refactor(compiler-cli): split up `NodeJSFileSystem` class (#40281)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+22.sha-1045465'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+35.sha-e27b920'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
