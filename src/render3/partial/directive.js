6001c792ba2cf6bd9af3d28b59649231294bdca9
Andrew Scott <atscott@google.com>
2021-01-08 18:10:34 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+35.sha-e27b920'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+37.sha-7a819ca'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
