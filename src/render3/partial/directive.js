11866ee6ae6deb4051ae35f5063cc39ff292f553
Andrew Scott <atscott@google.com>
2021-01-08 18:19:17 +0000

    e05ac395fc docs: add @christianliebel to list of GDEs (#39788)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+39.sha-4f73820'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+43.sha-e05ac39'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
