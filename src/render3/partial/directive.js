ea7a298d3d4c5bf0dd04d2d51b895680b33690c9
atscott <atscott01@gmail.com>
2021-01-11 19:21:47 +0000

    4bb067d738 docs: update JSDOC comment for the ArbTranslationSerializer (#40354)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+64.sha-f8aad11'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+58.sha-4bb067d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
