a17be6bf1a6466449e894eaeaf81c31b7e8e29e4
atscott <atscott01@gmail.com>
2021-01-11 18:24:46 +0000

    6e57443acd docs(router): fix minor grammatical issue (#40380)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+51.sha-34d2988'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+54.sha-6e57443'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
