26b6ef83eeec60cc007e4874db03ac0c1ea02e05
atscott <atscott01@gmail.com>
2021-01-11 18:21:54 +0000

    34d2988d14 docs: Fix compiler option name reference (#40362)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+53.sha-3d7e207
+ * @license Angular v11.1.0-next.4+51.sha-34d2988
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21710,7 +21710,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+53.sha-3d7e207');
+    var VERSION$1 = new Version('11.1.0-next.4+51.sha-34d2988');
 
     /**
      * @license
@@ -31616,7 +31616,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+53.sha-3d7e207'));
+        definitionMap.set('version', literal('11.1.0-next.4+51.sha-34d2988'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
