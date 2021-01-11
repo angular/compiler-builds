08fb904248521165acc70682da94367bcd0ea1f7
atscott <atscott01@gmail.com>
2021-01-11 18:22:14 +0000

    6e57443acd docs(router): fix minor grammatical issue (#40380)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+51.sha-34d2988'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+54.sha-6e57443'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
