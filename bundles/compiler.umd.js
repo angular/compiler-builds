7c9764b2ea72e3365ab10766eec7722efc11273a
atscott <atscott01@gmail.com>
2021-01-11 23:58:36 +0000

    9105005192 refactor(router): refactor and simplify router RxJS chains (#40290)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+68.sha-4a7a649
+ * @license Angular v11.1.0-next.4+69.sha-9105005
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21710,7 +21710,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+68.sha-4a7a649');
+    var VERSION$1 = new Version('11.1.0-next.4+69.sha-9105005');
 
     /**
      * @license
@@ -31616,7 +31616,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+68.sha-4a7a649'));
+        definitionMap.set('version', literal('11.1.0-next.4+69.sha-9105005'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
