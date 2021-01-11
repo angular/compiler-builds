c1b9e2ed3d7aae1c23e0099493efc89acbb0edd3
atscott <atscott01@gmail.com>
2021-01-11 18:28:45 +0000

    3acbec8532 docs: remove invalid style binding example (#40334)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+54.sha-6e57443
+ * @license Angular v11.1.0-next.4+52.sha-3acbec8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21710,7 +21710,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+54.sha-6e57443');
+    var VERSION$1 = new Version('11.1.0-next.4+52.sha-3acbec8');
 
     /**
      * @license
@@ -31616,7 +31616,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+54.sha-6e57443'));
+        definitionMap.set('version', literal('11.1.0-next.4+52.sha-3acbec8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
