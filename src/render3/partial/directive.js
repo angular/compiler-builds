624676ae3d38a5ea83f9c7d39504447bd23a00a4
atscott <atscott01@gmail.com>
2021-01-11 19:17:18 +0000

    27d0e54705 fix(compiler-cli): prevent stack overflow in decorator transform for large number of files (#40374)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+52.sha-3acbec8'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+57.sha-27d0e54'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
