178253633299b0480221e66eee45978eb527b511
atscott <atscott01@gmail.com>
2021-01-11 18:22:54 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+50.sha-1e46b73
+ * @license Angular v11.1.0-next.4+53.sha-3d7e207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21710,7 +21710,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+50.sha-1e46b73');
+    var VERSION$1 = new Version('11.1.0-next.4+53.sha-3d7e207');
 
     /**
      * @license
@@ -31616,7 +31616,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+50.sha-1e46b73'));
+        definitionMap.set('version', literal('11.1.0-next.4+53.sha-3d7e207'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+50.sha-1e46b73'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+53.sha-3d7e207'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
