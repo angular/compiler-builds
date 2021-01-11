13d0885ad0f5c50459e3ea1b432caca11136cdd3
atscott <atscott01@gmail.com>
2021-01-11 23:56:01 +0000

    9105005192 refactor(router): refactor and simplify router RxJS chains (#40290)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+68.sha-4a7a649'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+69.sha-9105005'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
