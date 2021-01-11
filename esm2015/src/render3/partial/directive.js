7dcb562325bccbf8da4d785666967aaf95fbd761
atscott <atscott01@gmail.com>
2021-01-11 22:39:10 +0000

    4a7a649852 docs(router): clarify the description of legacy relativeLinkResolution (#40384)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+66.sha-7695468'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+68.sha-4a7a649'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
