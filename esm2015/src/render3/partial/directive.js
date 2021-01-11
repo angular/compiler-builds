f9878ba1964caea58c3271830e90b12c7984d035
atscott <atscott01@gmail.com>
2021-01-11 18:20:17 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+50.sha-1e46b73'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+53.sha-3d7e207'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
