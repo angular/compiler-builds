c1b1382176399754ba54d267186c03307cadfd5b
atscott <atscott01@gmail.com>
2021-01-11 18:26:18 +0000

    3acbec8532 docs: remove invalid style binding example (#40334)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+54.sha-6e57443'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+52.sha-3acbec8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
