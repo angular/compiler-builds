ca49fef706f75a351974a417dd6206df0593abae
GitHub <noreply@github.com>
2021-01-13 19:42:18 +0000

    391e0ebf73 docs: release notes for the v11.0.9 release (#40415)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+82.sha-4db89f4
+ * @license Angular v11.1.0-next.4+83.sha-391e0eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20442,7 +20442,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+82.sha-4db89f4');
+const VERSION$1 = new Version('11.1.0-next.4+83.sha-391e0eb');
 
 /**
  * @license
@@ -29974,7 +29974,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+82.sha-4db89f4'));
+    definitionMap.set('version', literal('11.1.0-next.4+83.sha-391e0eb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
