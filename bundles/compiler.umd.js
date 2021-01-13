6eef72d20c4fb54f2ec0a411429dfa3ed8b3a157
atscott <atscott01@gmail.com>
2021-01-13 22:31:22 +0000

    25788106e5 ci: add mgechev to docs-marketing approvers (#40396)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+86.sha-25892c1
+ * @license Angular v11.1.0-next.4+91.sha-2578810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21721,7 +21721,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+86.sha-25892c1');
+    var VERSION$1 = new Version('11.1.0-next.4+91.sha-2578810');
 
     /**
      * @license
@@ -31629,7 +31629,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+86.sha-25892c1'));
+        definitionMap.set('version', literal('11.1.0-next.4+91.sha-2578810'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+86.sha-25892c1'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+91.sha-2578810'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
