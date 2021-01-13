e131b3be433b4e87dd6fda8960f275307a875d02
GitHub <noreply@github.com>
2021-01-13 19:39:42 +0000

    391e0ebf73 docs: release notes for the v11.0.9 release (#40415)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+82.sha-4db89f4'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+83.sha-391e0eb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
