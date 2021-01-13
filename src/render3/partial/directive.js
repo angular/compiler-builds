e37803f77a73a8d6eee4b5b51e4924eb30bebe54
GitHub <noreply@github.com>
2021-01-13 21:47:06 +0000

    47b3b08118 Update next branch to reflect new release-train "v11.2.0-next.0". (#40418)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+83.sha-391e0eb'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+84.sha-47b3b08'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
