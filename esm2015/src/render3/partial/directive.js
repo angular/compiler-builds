b844006b2733a41a9f311dd0ee14eecf41b4640d
atscott <atscott01@gmail.com>
2021-01-13 22:33:57 +0000

    25788106e5 ci: add mgechev to docs-marketing approvers (#40396)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+86.sha-25892c1'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+91.sha-2578810'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+86.sha-25892c1
+ * @license Angular v11.1.0-next.4+91.sha-2578810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20442,7 +20442,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+86.sha-25892c1');
+const VERSION$1 = new Version('11.1.0-next.4+91.sha-2578810');
 
 /**
  * @license
@@ -29974,7 +29974,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+86.sha-25892c1'));
+    definitionMap.set('version', literal('11.1.0-next.4+91.sha-2578810'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
