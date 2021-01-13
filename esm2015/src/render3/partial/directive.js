09a207fc08f9c8085c092273ba914309fe06860c
atscott <atscott01@gmail.com>
2021-01-13 22:31:51 +0000

    25892c1bcf docs: add links to TypeScript (#40410)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+85.sha-e6ac542'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+86.sha-25892c1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
