2bd45709b742fcfa2a71f572e69ce670d0c015c6
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:58:28 +0000

    b971bc6f70 perf(ngcc): do not copy files that have been processed (#40429)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+100.sha-e4df942'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+103.sha-b971bc6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
