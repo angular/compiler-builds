3559e67421a6a2798d6f401824d2b9269ebd41a6
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:59:36 +0000

    9b0b2dd688 docs: Fix typo in Trusted Types documentation (#40433)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+103.sha-b971bc6'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+101.sha-9b0b2dd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
