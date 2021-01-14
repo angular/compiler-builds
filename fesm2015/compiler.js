a18340ccf4a39ca740ae8cb43e97467f21311daf
Andrew Scott <atscott@google.com>
2021-01-14 17:45:06 +0000

    f3a96a50b6 build(docs-infra): upgrade cli command docs sources to f67800842 (#40426)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+9.sha-5aa264f
+ * @license Angular v11.0.9+11.sha-f3a96a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19926,7 +19926,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.9+9.sha-5aa264f');
+const VERSION$1 = new Version('11.0.9+11.sha-f3a96a5');
 
 /**
  * @license
