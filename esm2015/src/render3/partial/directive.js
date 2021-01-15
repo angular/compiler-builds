ffec7ef3494e0d8ef1db8dbb522f0423783d1d24
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:13:02 +0000

    61792cc6f5 refactor(compiler): remove unused files in css_parser/ and corresponding spec tests (#37463)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+110.sha-96690ed'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+113.sha-61792cc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+110.sha-96690ed'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+113.sha-61792cc'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
