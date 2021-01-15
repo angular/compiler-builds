8c54e869090e65267b2e026bc1e2ed16ccf828fe
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:16:49 +0000

    e7ed209338 refactor(compiler): remove unused files in css_parser/ and corresponding spec tests (#37463)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+17.sha-2f7e315
+ * @license Angular v11.0.9+22.sha-e7ed209
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21183,7 +21183,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.0.9+17.sha-2f7e315');
+    var VERSION$1 = new Version('11.0.9+22.sha-e7ed209');
 
     /**
      * @license
