1b0cea26b058f7b5468497030cb3b05518050fca
Andrew Kushnir <akushnir@google.com>
2021-01-15 00:49:35 +0000

    9a5ac47331 feat(language-service): initial implementation for `findRenameLocations` (#40140)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+106.sha-d516113'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+107.sha-9a5ac47'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
