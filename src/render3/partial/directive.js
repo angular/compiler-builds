f0c50957940ebc2f5034912733abf84c16669024
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:39:15 +0000

    96690ed3a4 feat(docs-infra): add shortcuts for the angular.io PWA (#40393)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+111.sha-b37a9eb'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+110.sha-96690ed'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
