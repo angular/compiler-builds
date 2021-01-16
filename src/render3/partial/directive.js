909aadab57dadec1a5731cba07bf5a9ac317785d
Andrew Kushnir <akushnir@google.com>
2021-01-16 01:11:41 +0000

    4e8198d60f feat(language-service): Implement `getRenameInfo` (#40439)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+114.sha-40e0bfd'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+115.sha-4e8198d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
