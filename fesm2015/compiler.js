7648e1b5369d9ec8ac2e9217c61633c9727224f5
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:47:44 +0000

    2047d632d8 docs: add Angular Builders to community resources (#40475)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+28.sha-524415e
+ * @license Angular v11.1.0-rc.0+32.sha-2047d63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-rc.0+28.sha-524415e');
+const VERSION$1 = new Version('11.1.0-rc.0+32.sha-2047d63');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-rc.0+28.sha-524415e'));
+    definitionMap.set('version', literal('11.1.0-rc.0+32.sha-2047d63'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
