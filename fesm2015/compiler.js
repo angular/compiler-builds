5b358b9f6ed446250c783ab0017588536f19efa0
Andrew Kushnir <akushnir@google.com>
2021-01-19 20:07:50 +0000

    1d1304c70c fix(forms): error if control is removed as a result of another one being reset (#40462)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+123.sha-afabb83
+ * @license Angular v11.1.0-next.4+124.sha-1d1304c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+123.sha-afabb83');
+const VERSION$1 = new Version('11.1.0-next.4+124.sha-1d1304c');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+123.sha-afabb83'));
+    definitionMap.set('version', literal('11.1.0-next.4+124.sha-1d1304c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
