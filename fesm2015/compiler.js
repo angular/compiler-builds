1fb55b310254bb76036b1558672735ffa8b9970c
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:44:16 +0000

    96e6f20ce2 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+25.sha-724e44a
+ * @license Angular v11.0.9+26.sha-96e6f20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19926,7 +19926,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.0.9+25.sha-724e44a');
+const VERSION$1 = new Version('11.0.9+26.sha-96e6f20');
 
 /**
  * @license
