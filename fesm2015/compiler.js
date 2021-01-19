9a6d54c68847336e24b9650bb691ae4cb141638a
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:43:30 +0000

    5e95153d44 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+125.sha-3e97a1e
+ * @license Angular v11.1.0-next.4+126.sha-5e95153
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+125.sha-3e97a1e');
+const VERSION$1 = new Version('11.1.0-next.4+126.sha-5e95153');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+125.sha-3e97a1e'));
+    definitionMap.set('version', literal('11.1.0-next.4+126.sha-5e95153'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
