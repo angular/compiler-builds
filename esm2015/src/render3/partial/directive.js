6810a8ac270940d4ae2257c4e35690c18b7911e9
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:48:48 +0000

    2c95f57266 feat(docs-infra): use Record type instead of key-value (#40463)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+119.sha-b636406'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+120.sha-2c95f57'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+119.sha-b636406'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+120.sha-2c95f57'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
