ccafaeebe4fcad9ad0f81d514ded12d565ff20fb
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:41:53 +0000

    f49447df20 fix(core): fix fakeAsync() error messages (#40442)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+115.sha-4e8198d
+ * @license Angular v11.1.0-next.4+116.sha-f49447d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21745,7 +21745,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+115.sha-4e8198d');
+    var VERSION$1 = new Version('11.1.0-next.4+116.sha-f49447d');
 
     /**
      * @license
@@ -31657,7 +31657,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+115.sha-4e8198d'));
+        definitionMap.set('version', literal('11.1.0-next.4+116.sha-f49447d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+115.sha-4e8198d'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+116.sha-f49447d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
