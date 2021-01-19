92fe208272b353622a8aed72838b0c1e5804269d
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:47:46 +0000

    0568c7387f docs: add Angular Builders to community resources (#40475)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+120.sha-2c95f57'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+121.sha-0568c73'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
