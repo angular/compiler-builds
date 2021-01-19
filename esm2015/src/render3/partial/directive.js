efc76397702096455f265ced625112f801abf007
Andrew Kushnir <akushnir@google.com>
2021-01-19 18:40:57 +0000

    afabb83696 refactor(core): add links to top compiler errors (#40326)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+121.sha-0568c73'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+123.sha-afabb83'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+121.sha-0568c73'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+123.sha-afabb83'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
