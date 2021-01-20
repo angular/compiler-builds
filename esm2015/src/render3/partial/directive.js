c4b9e11e410baa4684f2d36fc8c43f81495ec9d2
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:25:16 +0000

    59680c2644 release: cut the v11.1.0 release

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-rc.0+39.sha-34092a5'));
+    definitionMap.set('version', o.literal('11.1.0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
