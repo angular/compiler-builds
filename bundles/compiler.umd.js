317abd31cdbd49e94d954ecd6478e8eb7f586b1b
Andrew Kushnir <akushnir@google.com>
2021-01-20 23:35:54 +0000

    0b46f814d0 docs: release notes for the v11.1.0 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+129.sha-3cfb4b4
+ * @license Angular v11.1.0-next.4+130.sha-0b46f81
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21745,7 +21745,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+129.sha-3cfb4b4');
+    var VERSION$1 = new Version('11.1.0-next.4+130.sha-0b46f81');
 
     /**
      * @license
@@ -31657,7 +31657,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+129.sha-3cfb4b4'));
+        definitionMap.set('version', literal('11.1.0-next.4+130.sha-0b46f81'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
