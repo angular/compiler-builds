9d3c607393d3800d18825389d09d57e7a1647099
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:32:28 +0000

    34092a5052 docs: add embedded videos to error guides (#40453)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-rc.0+37.sha-2731a4b'));
+        definitionMap.set('version', o.literal('11.1.0-rc.0+39.sha-34092a5'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
