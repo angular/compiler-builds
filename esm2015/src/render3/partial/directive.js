2b72a7b157f2b6ea2e936a7bb61d677bd6a76c23
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:07:57 +0000

    3f92be4ec3 docs: remove old content from CLI configuration guide (#38243)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+144.sha-0e95460'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+146.sha-3f92be4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
