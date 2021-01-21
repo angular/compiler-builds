ccce6a56e6d9d2191cac164e640b80337a5fe67f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:50:11 +0000

    315b272f9f docs: add missing type parameter to testing example (#40479)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+130.sha-0b46f81'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+137.sha-315b272'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
