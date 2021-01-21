84a85edbaf39354a2b75ca7ca34751e00a4495b4
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:27:57 +0000

    2b2a847ad7 refactor(migrations): remove rxjs usage within static queries migration (#38657)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+153.sha-dc06873'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+151.sha-2b2a847'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
