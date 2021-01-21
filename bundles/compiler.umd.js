431002932f6f96e359db81b74d607932bbcb00ea
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:29:22 +0000

    dc06873c72 fix(compiler-cli): handle pseudo cycles in inline source-maps (#40435)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+152.sha-d482f5c
+ * @license Angular v11.1.0-next.4+153.sha-dc06873
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21745,7 +21745,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+152.sha-d482f5c');
+    var VERSION$1 = new Version('11.1.0-next.4+153.sha-dc06873');
 
     /**
      * @license
@@ -31657,7 +31657,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+152.sha-d482f5c'));
+        definitionMap.set('version', literal('11.1.0-next.4+153.sha-dc06873'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
