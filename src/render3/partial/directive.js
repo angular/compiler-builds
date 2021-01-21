c48462cc5388c83d98d7df72b94837fedd5c71e4
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:21:32 +0000

    c9033d6193 build: set the repository information for ts-api-guardian (#40512)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+148.sha-ac253d0'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+149.sha-c9033d6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
