083956733d29fa2e460219528246e59ec8ce6edb
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 01:28:43 +0000

    69385f7df4 test(core): verify that token IDs that exceed the bloom filter size are handled correctly (#40489)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+140.sha-5bbb5ea'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+142.sha-69385f7'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
