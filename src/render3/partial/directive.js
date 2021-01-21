2e0fab64a36c5820d77b4f5bb69d987f74c62a68
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:10:29 +0000

    3f92be4ec3 docs: remove old content from CLI configuration guide (#38243)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+144.sha-0e95460'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+146.sha-3f92be4'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
