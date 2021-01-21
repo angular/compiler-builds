8d4447cfff7f459b4f196e51b8ba45caea30330e
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:23:51 +0000

    575a2d162c feat(common): implement `appendAll()` method on `HttpParams` (#20930)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+149.sha-c9033d6'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+150.sha-575a2d1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
