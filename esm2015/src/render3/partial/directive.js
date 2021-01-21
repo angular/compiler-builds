e7cbf87e2febdd2b7ceab0cd8f199e16bef8ff47
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:21:17 +0000

    575a2d162c feat(common): implement `appendAll()` method on `HttpParams` (#20930)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+149.sha-c9033d6'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+150.sha-575a2d1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
