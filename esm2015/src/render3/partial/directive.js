25221eb4996e936bf4385ba2df4ecd2ff3be83aa
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:52:37 +0000

    3cf4e3c7c5 fix(router): always stringify matrix parameters (#25095)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+137.sha-315b272'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+139.sha-3cf4e3c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
