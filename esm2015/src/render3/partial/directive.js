f8048a1c454f2417418bc630554e2cb843c1e04c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:45:11 +0000

    07b7af332f fix(compiler): support multiple `:host-context()` selectors (#40494)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+151.sha-2b2a847'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+154.sha-07b7af3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+151.sha-2b2a847'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+154.sha-07b7af3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
