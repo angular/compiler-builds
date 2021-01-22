0664a21700c41e164b89f20d4700dbf31a3f356e
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 21:55:09 +0000

    c5599ccd3e docs: fix NG0201 error description (#40500)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+156.sha-e25413c
+ * @license Angular v11.1.0-next.4+157.sha-c5599cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20514,7 +20514,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+156.sha-e25413c');
+const VERSION$1 = new Version('11.1.0-next.4+157.sha-c5599cc');
 
 /**
  * @license
@@ -30050,7 +30050,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+156.sha-e25413c'));
+    definitionMap.set('version', literal('11.1.0-next.4+157.sha-c5599cc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
