c7626e56cb4f78347dccf99e93a4e311eb4db0f5
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:46:38 +0000

    1e4b51e9f7 fix(core): improve injector debug information in `ngDevMode` (#40476)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+154.sha-07b7af3
+ * @license Angular v11.1.0-next.4+155.sha-1e4b51e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21800,7 +21800,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+154.sha-07b7af3');
+    var VERSION$1 = new Version('11.1.0-next.4+155.sha-1e4b51e');
 
     /**
      * @license
@@ -31712,7 +31712,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+154.sha-07b7af3'));
+        definitionMap.set('version', literal('11.1.0-next.4+155.sha-1e4b51e'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
