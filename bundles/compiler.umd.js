b55deca9caba1ddcc6a72634bb8e184de0dee623
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:52:54 +0000

    e25413c6da build: update to @bazel/bazelisk@1.7.3 (#40519)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+155.sha-1e4b51e
+ * @license Angular v11.1.0-next.4+156.sha-e25413c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21800,7 +21800,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+155.sha-1e4b51e');
+    var VERSION$1 = new Version('11.1.0-next.4+156.sha-e25413c');
 
     /**
      * @license
@@ -31712,7 +31712,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+155.sha-1e4b51e'));
+        definitionMap.set('version', literal('11.1.0-next.4+156.sha-e25413c'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+155.sha-1e4b51e
+ * @license Angular v11.1.0-next.4+156.sha-e25413c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20514,7 +20514,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+155.sha-1e4b51e');
+const VERSION$1 = new Version('11.1.0-next.4+156.sha-e25413c');
 
 /**
  * @license
@@ -30050,7 +30050,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+155.sha-1e4b51e'));
+    definitionMap.set('version', literal('11.1.0-next.4+156.sha-e25413c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
