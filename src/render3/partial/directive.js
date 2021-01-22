0f76f44939216d38a2f2cb114dc506747f01b77c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 21:57:28 +0000

    c5599ccd3e docs: fix NG0201 error description (#40500)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+156.sha-e25413c'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+157.sha-c5599cc'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
