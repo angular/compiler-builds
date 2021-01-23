6ed1e320442a9da82f074432e7ee5446e4473edc
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:03:30 +0000

    c64a56fbcc Revert "fix(core): fix possible XSS attack in development through SSR (#40525)" (#40533)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+158.sha-bb3b315
+ * @license Angular v11.1.0-next.4+159.sha-c64a56f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20514,7 +20514,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+158.sha-bb3b315');
+const VERSION$1 = new Version('11.1.0-next.4+159.sha-c64a56f');
 
 /**
  * @license
@@ -30050,7 +30050,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+158.sha-bb3b315'));
+    definitionMap.set('version', literal('11.1.0-next.4+159.sha-c64a56f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
