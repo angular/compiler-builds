12b1c82e667b520c50a134ca746157febff72512
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:25:09 +0000

    56612982c7 fix(localize): include meaning in generated ARB files (#40546)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0+30.sha-37710b9'));
+    definitionMap.set('version', o.literal('11.1.0+36.sha-5661298'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0+30.sha-37710b9'));
+        definitionMap.set('version', o.literal('11.1.0+36.sha-5661298'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
