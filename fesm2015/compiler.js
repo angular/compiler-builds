a2cc61c02df4259502112c386a8d95dc808bad08
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 23:23:38 +0000

    b630b09c7e fix(compiler-cli): use `Map` rather than `object` for map of partial linkers (#40563)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+174.sha-ecae75f
+ * @license Angular v11.1.0-next.4+176.sha-b630b09
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+174.sha-ecae75f');
+const VERSION$1 = new Version('11.1.0-next.4+176.sha-b630b09');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+174.sha-ecae75f'));
+    definitionMap.set('version', literal('11.1.0-next.4+176.sha-b630b09'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+174.sha-ecae75f'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+176.sha-b630b09'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
