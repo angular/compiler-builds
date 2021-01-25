f1a55c50331c286d76f104bd001f3cfe5128800c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 22:44:57 +0000

    ecae75f477 feat(language-service): Add diagnostics to suggest turning on strict mode (#40423)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+173.sha-c9fe455'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+174.sha-ecae75f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
