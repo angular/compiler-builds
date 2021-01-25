d2f4e99461d7848fac956f5ac547ff3d7a92e25f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 23:24:16 +0000

    33e0f2bd56 fix(compiler-cli): use `Map` rather than `object` for map of partial linkers (#40563)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+39.sha-2fab148
+ * @license Angular v11.1.0+41.sha-33e0f2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21745,7 +21745,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0+39.sha-2fab148');
+    var VERSION$1 = new Version('11.1.0+41.sha-33e0f2b');
 
     /**
      * @license
@@ -31657,7 +31657,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0+39.sha-2fab148'));
+        definitionMap.set('version', literal('11.1.0+41.sha-33e0f2b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0+39.sha-2fab148'));
+        definitionMap.set('version', o.literal('11.1.0+41.sha-33e0f2b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
