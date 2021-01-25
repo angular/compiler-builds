e244fd2fb59892f99f49d2b0d12fbbfd7cb6edb7
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 20:10:57 +0000

    c9fe455fa2 fix(forms): allow `patchValue()` method of `FormGroup` and `FormArray` classes to skip `null` values (#40534)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+171.sha-8b9f6b5'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+173.sha-c9fe455'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
