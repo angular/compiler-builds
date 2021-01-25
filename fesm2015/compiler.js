5ce91122c7361fbcd65dd39416a5b6422a486ee3
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:40:12 +0000

    21bc16d4d8 fix(service-worker): handle error with console.error (#40236)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+160.sha-fc64fa8
+ * @license Angular v11.1.0-next.4+164.sha-21bc16d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+160.sha-fc64fa8');
+const VERSION$1 = new Version('11.1.0-next.4+164.sha-21bc16d');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+160.sha-fc64fa8'));
+    definitionMap.set('version', literal('11.1.0-next.4+164.sha-21bc16d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
