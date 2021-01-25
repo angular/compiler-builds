de863f49afa68566c735453a0b4fc99e2002526b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:36:32 +0000

    37710b9be3 fix(service-worker): handle error with console.error (#40236)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0+26.sha-dfe4200'));
+        definitionMap.set('version', o.literal('11.1.0+30.sha-37710b9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
