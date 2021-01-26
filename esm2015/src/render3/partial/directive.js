688f9e05d76bcb5ef515a9c26768a59ff449df86
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 00:52:38 +0000

    956f75f068 docs: fix the docs because the interface does not exist (#40308)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+176.sha-b630b09'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+177.sha-956f75f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
