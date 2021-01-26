e661369ddc0c9bf5e9e85b3c02f09b4e7601d938
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 19:27:32 +0000

    ffad040a96 build: update to yarn v1.22.10 (#40562)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+179.sha-6bf99e0
+ * @license Angular v11.1.0-next.4+181.sha-ffad040
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+179.sha-6bf99e0');
+const VERSION$1 = new Version('11.1.0-next.4+181.sha-ffad040');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+179.sha-6bf99e0'));
+    definitionMap.set('version', literal('11.1.0-next.4+181.sha-ffad040'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
