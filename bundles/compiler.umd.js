b0fe1b608ea8ec653aa486ec37582a96c5ce9c63
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:56:32 +0000

    97ec6e4849 fix(core): fix possible XSS attack in development through SSR (#40525)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+43.sha-a87e900
+ * @license Angular v11.1.0+45.sha-97ec6e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21745,7 +21745,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0+43.sha-a87e900');
+    var VERSION$1 = new Version('11.1.0+45.sha-97ec6e4');
 
     /**
      * @license
@@ -31657,7 +31657,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0+43.sha-a87e900'));
+        definitionMap.set('version', literal('11.1.0+45.sha-97ec6e4'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0+43.sha-a87e900'));
+        definitionMap.set('version', o.literal('11.1.0+45.sha-97ec6e4'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
