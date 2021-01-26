5d22b30af86148d24fbf4522c876412bc6e365f6
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:04:34 +0000

    39b3cbbdf5 build: update .bazelversion (#40579)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+181.sha-ffad040
+ * @license Angular v11.1.0-next.4+182.sha-39b3cbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+181.sha-ffad040');
+const VERSION$1 = new Version('11.1.0-next.4+182.sha-39b3cbb');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+181.sha-ffad040'));
+    definitionMap.set('version', literal('11.1.0-next.4+182.sha-39b3cbb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
