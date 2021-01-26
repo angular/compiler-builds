3e5df3707d7687c94ec652fdb8f1451428c3caf1
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 20:07:04 +0000

    39b3cbbdf5 build: update .bazelversion (#40579)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+181.sha-ffad040'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+182.sha-39b3cbb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
