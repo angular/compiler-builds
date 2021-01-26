1a95d633e6e0433e6174749d8afa3031e0e4cf1f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:57:26 +0000

    6bf99e0eda fix(core): fix possible XSS attack in development through SSR (#40525)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+178.sha-365ac5e'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+179.sha-6bf99e0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
