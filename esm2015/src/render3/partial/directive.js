82f4578508a04eca4c4034b1c80d11c32b8f8eba
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:31:27 +0000

    cffb00ec11 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+182.sha-39b3cbb'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+183.sha-cffb00e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
