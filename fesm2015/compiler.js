60ec43b6a438bafe9e1fe6ff57d169137c1905a0
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 17:49:42 +0000

    365ac5e68e docs: fixed issue with missing code sample by rearranging import to proper docregion (#40565)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+177.sha-956f75f
+ * @license Angular v11.1.0-next.4+178.sha-365ac5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+177.sha-956f75f');
+const VERSION$1 = new Version('11.1.0-next.4+178.sha-365ac5e');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+177.sha-956f75f'));
+    definitionMap.set('version', literal('11.1.0-next.4+178.sha-365ac5e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
