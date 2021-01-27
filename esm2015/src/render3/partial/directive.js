2226e360273d27ab4e9c66ccdaa2f1e507e4ec17
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 00:07:31 +0000

    a8269264bf fix(core): make DefaultIterableDiffer keep the order of duplicates (#23941)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+183.sha-cffb00e'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+184.sha-a826926'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
