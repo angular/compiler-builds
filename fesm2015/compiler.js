36390cf33daf6871ce726d14869b363c5c015ed3
GitHub <noreply@github.com>
2021-01-27 21:24:34 +0000

    8772fbd4bb release: bump the version to 12.0.0-next.0 (#40604)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+195.sha-e75a4f2
+ * @license Angular v11.1.0-next.4+196.sha-8772fbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20500,7 +20500,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+195.sha-e75a4f2');
+const VERSION$1 = new Version('11.1.0-next.4+196.sha-8772fbd');
 
 /**
  * @license
@@ -30036,7 +30036,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+195.sha-e75a4f2'));
+    definitionMap.set('version', literal('11.1.0-next.4+196.sha-8772fbd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
