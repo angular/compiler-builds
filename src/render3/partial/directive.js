e5125a7f271b468cfff2f6542be2ffc4c67110af
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 18:48:33 +0000

    f1cf875872 docs(elements): add notice about choosing a tag name in Elements guide (#40510)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+184.sha-a826926'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+186.sha-f1cf875'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
