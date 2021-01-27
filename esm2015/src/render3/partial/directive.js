f05a46d380832a08c2a870553835495fa7662053
GitHub <noreply@github.com>
2021-01-27 20:21:08 +0000

    e75a4f261e docs: release notes for the v11.1.1 release (#40599)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+194.sha-be979c9'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+195.sha-e75a4f2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+194.sha-be979c9'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+195.sha-e75a4f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
