39526c60371a5140b96304a95bd0315df0e0534b
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 00:09:59 +0000

    a8269264bf fix(core): make DefaultIterableDiffer keep the order of duplicates (#23941)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+183.sha-cffb00e
+ * @license Angular v11.1.0-next.4+184.sha-a826926
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+183.sha-cffb00e');
+const VERSION$1 = new Version('11.1.0-next.4+184.sha-a826926');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+183.sha-cffb00e'));
+    definitionMap.set('version', literal('11.1.0-next.4+184.sha-a826926'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+183.sha-cffb00e'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+184.sha-a826926'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
