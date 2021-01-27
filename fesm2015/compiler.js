8b1594456ac493e798f8f88a1cb01f3594c8f59d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:10:39 +0000

    e43cba5c82 fix(language-service): implement realpath to resolve symlinks (#40593)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+190.sha-d703d21
+ * @license Angular v11.1.0-next.4+189.sha-e43cba5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20466,7 +20466,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+190.sha-d703d21');
+const VERSION$1 = new Version('11.1.0-next.4+189.sha-e43cba5');
 
 /**
  * @license
@@ -30002,7 +30002,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+190.sha-d703d21'));
+    definitionMap.set('version', literal('11.1.0-next.4+189.sha-e43cba5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+190.sha-d703d21'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+189.sha-e43cba5'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
