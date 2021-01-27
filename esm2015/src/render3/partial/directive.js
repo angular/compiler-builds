6a1686f9623a4c38d96aa1d18a4631bae9e578e3
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 21:30:49 +0000

    d4bb4a0cf1 docs: update release notes for the 11.1.1 release (#40602)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+196.sha-8772fbd'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+197.sha-d4bb4a0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+196.sha-8772fbd'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+197.sha-d4bb4a0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
