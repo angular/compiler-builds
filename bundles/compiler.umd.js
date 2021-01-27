ae67c3f5df7aca1368a5efa8dc0b48780839bd21
GitHub <noreply@github.com>
2021-01-27 20:04:18 +0000

    63275ae82c release: cut the v11.1.1 release (#40598)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+59.sha-156103c
+ * @license Angular v11.1.0+60.sha-63275ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21779,7 +21779,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0+59.sha-156103c');
+    var VERSION$1 = new Version('11.1.0+60.sha-63275ae');
 
     /**
      * @license
@@ -31691,7 +31691,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0+59.sha-156103c'));
+        definitionMap.set('version', literal('11.1.0+60.sha-63275ae'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0+59.sha-156103c'));
+        definitionMap.set('version', o.literal('11.1.0+60.sha-63275ae'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
