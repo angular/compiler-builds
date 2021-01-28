752c139078c55f68096e7db793410d82d2f83c1f
Misko Hevery <misko@hevery.com>
2021-01-28 23:10:03 +0000

    88f8ddd3d3 Revert "fix(core): remove duplicated EMPTY_ARRAY constant (#40587)"

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+197.sha-d4bb4a0'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+208.sha-88f8ddd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+197.sha-d4bb4a0'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+208.sha-88f8ddd'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
