24d37917aafe1758e55ba10b65197b67e79e93e8
Misko Hevery <misko@hevery.com>
2021-01-29 19:33:04 +0000

    ca0f6e6eed fix(common): parse `YYYY` strings as UTC dates (#40629)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+213.sha-b6cd38f
+ * @license Angular v11.1.0-next.4+214.sha-ca0f6e6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+213.sha-b6cd38f');
+    var VERSION$1 = new Version('11.1.0-next.4+214.sha-ca0f6e6');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+213.sha-b6cd38f'));
+        definitionMap.set('version', literal('11.1.0-next.4+214.sha-ca0f6e6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
