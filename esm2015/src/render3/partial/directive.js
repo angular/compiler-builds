19606e74b7611260bca494a853e854a2727f3442
Misko Hevery <misko@hevery.com>
2021-01-29 00:18:32 +0000

    2ba62e80fd ci: add pete to the fw-http group (#40619)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+209.sha-22f9e45'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+211.sha-2ba62e8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
