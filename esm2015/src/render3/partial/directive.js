0aff2259a124f68c22d48b4e411cc6f3be841cfa
Misko Hevery <misko@hevery.com>
2021-01-29 00:24:23 +0000

    4c5f18dfda fix(common): parse `YYYY-MM` strings as UTC dates (#40620)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.2.0-next.0+11.sha-552009e'));
+    definitionMap.set('version', o.literal('11.2.0-next.0+15.sha-4c5f18d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.0-next.0+11.sha-552009e'));
+        definitionMap.set('version', o.literal('11.2.0-next.0+15.sha-4c5f18d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
