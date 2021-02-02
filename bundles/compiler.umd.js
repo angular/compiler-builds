02f8d4991acef38e88652d6402712dbdeebe21c5
Misko Hevery <misko@hevery.com>
2021-02-02 20:23:08 +0000

    20654c95d8 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+17.sha-00eeebf4
+ * @license Angular v11.1.1+29.sha-20654c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.1+17.sha-00eeebf4');
+    var VERSION$1 = new Version('11.1.1+29.sha-20654c9');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.1+17.sha-00eeebf4'));
+        definitionMap.set('version', literal('11.1.1+29.sha-20654c9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.1+17.sha-00eeebf4'));
+    definitionMap.set('version', o.literal('11.1.1+29.sha-20654c9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
