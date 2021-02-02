3dc1cdd3761b82554fb6894dd85159743e3aaf2a
Misko Hevery <misko@hevery.com>
2021-02-02 20:23:58 +0000

    cc8af03eec docs: fix typo in lazy-loading guide (#40668)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+216.sha-bd0d191
+ * @license Angular v11.1.0-next.4+218.sha-cc8af03
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+216.sha-bd0d191');
+    var VERSION$1 = new Version('11.1.0-next.4+218.sha-cc8af03');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+216.sha-bd0d191'));
+        definitionMap.set('version', literal('11.1.0-next.4+218.sha-cc8af03'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
