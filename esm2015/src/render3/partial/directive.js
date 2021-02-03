1e50cd406f1c00448bbc42fe00701add35421da6
Misko Hevery <misko@hevery.com>
2021-02-03 18:26:08 +0000

    950875c1ba refactor(language-service): pull out interfaces on package toplevel (#40621)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+231.sha-a3b0864'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+237.sha-950875c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+231.sha-a3b0864
+ * @license Angular v11.1.0-next.4+237.sha-950875c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+231.sha-a3b0864');
+const VERSION$1 = new Version('11.1.0-next.4+237.sha-950875c');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+231.sha-a3b0864'));
+    definitionMap.set('version', literal('11.1.0-next.4+237.sha-950875c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
