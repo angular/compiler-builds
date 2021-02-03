a136e5fc7d0df4fd31bd8a74ca153f357690427f
Misko Hevery <misko@hevery.com>
2021-02-03 23:19:08 +0000

    6ac05c05e9 release: cut the v11.2.0-next.1 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0-next.0+37.sha-7c38a4a
+ * @license Angular v11.2.0-next.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.2.0-next.0+37.sha-7c38a4a');
+    var VERSION$1 = new Version('11.2.0-next.1');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.0-next.0+37.sha-7c38a4a'));
+        definitionMap.set('version', literal('11.2.0-next.1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
