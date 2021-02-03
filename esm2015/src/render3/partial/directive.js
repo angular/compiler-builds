0e435cc98667e2edc8ea073cd0fca29ac091962e
Misko Hevery <misko@hevery.com>
2021-02-03 22:38:55 +0000

    7ae7484862 docs: release notes for the v11.1.2 release

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+237.sha-950875c'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+238.sha-7ae7484'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+237.sha-950875c'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+238.sha-7ae7484'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
