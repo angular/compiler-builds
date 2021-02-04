9d9c7988d63b57908110361c8b7b9aec6bf87891
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:24:28 +0000

    0fc5a1690a docs: add Angular Global Summit to event pages (#40697)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.2
+ * @license Angular v11.1.2+4.sha-0fc5a16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.2');
+    var VERSION$1 = new Version('11.1.2+4.sha-0fc5a16');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.2'));
+        definitionMap.set('version', literal('11.1.2+4.sha-0fc5a16'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
