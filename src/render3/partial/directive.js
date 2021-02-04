1e01e2ae8b49e2adcb0efb56ba03059d09bf73ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:24:48 +0000

    d067dc0cb9 docs: add Angular Global Summit to event pages (#40697)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+241.sha-5324a62'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+245.sha-d067dc0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
