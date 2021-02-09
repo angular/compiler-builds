58848d0e11e187cecd60c77413fb839982b6424d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 18:56:08 +0000

    348d365666 docs(service-worker): copyedit getting started guide (#40543)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+246.sha-8fb83ea'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+247.sha-348d365'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
