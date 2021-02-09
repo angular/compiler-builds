78e61e923f659a0c46ddcca190e68ed456a8a80c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:06:27 +0000

    2a851c0924 docs: edit example to fetch hero before sending message (#40419)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+310.sha-820d95b
+ * @license Angular v11.1.0-next.4+311.sha-2a851c0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+310.sha-820d95b');
+    var VERSION$1 = new Version('11.1.0-next.4+311.sha-2a851c0');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+310.sha-820d95b'));
+        definitionMap.set('version', literal('11.1.0-next.4+311.sha-2a851c0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
