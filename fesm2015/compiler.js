ef4527be3977a98908c089f42ff13ba55206b652
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:12:49 +0000

    4ce44eac33 fix(animations): properly track listeners for a removed element (#40712)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+284.sha-089a634
+ * @license Angular v11.1.0-next.4+302.sha-4ce44ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+284.sha-089a634');
+const VERSION$1 = new Version('11.1.0-next.4+302.sha-4ce44ea');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+284.sha-089a634'));
+    definitionMap.set('version', literal('11.1.0-next.4+302.sha-4ce44ea'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+284.sha-089a634'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+302.sha-4ce44ea'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
