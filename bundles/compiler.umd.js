efcca3b764cf83628804f9b1fda256668bdbfc98
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:17:54 +0000

    4e4fcacb6d fix(docs-infra): correctly handle entry-points with no public exports (#40737)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+304.sha-e3213f6
+ * @license Angular v11.1.0-next.4+305.sha-4e4fcac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+304.sha-e3213f6');
+    var VERSION$1 = new Version('11.1.0-next.4+305.sha-4e4fcac');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+304.sha-e3213f6'));
+        definitionMap.set('version', literal('11.1.0-next.4+305.sha-4e4fcac'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+304.sha-e3213f6'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+305.sha-4e4fcac'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
