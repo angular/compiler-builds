7e4b05414439b96ffde1f5c6ae6131d717bde852
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:10:28 +0000

    4ce44eac33 fix(animations): properly track listeners for a removed element (#40712)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+284.sha-089a634
+ * @license Angular v11.1.0-next.4+302.sha-4ce44ea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21782,7 +21782,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.1.0-next.4+284.sha-089a634');
+    var VERSION$1 = new Version('11.1.0-next.4+302.sha-4ce44ea');
 
     /**
      * @license
@@ -31694,7 +31694,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.1.0-next.4+284.sha-089a634'));
+        definitionMap.set('version', literal('11.1.0-next.4+302.sha-4ce44ea'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
