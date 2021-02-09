51ada6971b89cb5a738efeb81697e807469c093a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:08:09 +0000

    61a0b6de6d fix(http): complete the request on timeout (#39807)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+311.sha-2a851c0
+ * @license Angular v11.1.0-next.4+309.sha-61a0b6d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+311.sha-2a851c0');
+const VERSION$1 = new Version('11.1.0-next.4+309.sha-61a0b6d');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+311.sha-2a851c0'));
+    definitionMap.set('version', literal('11.1.0-next.4+309.sha-61a0b6d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
