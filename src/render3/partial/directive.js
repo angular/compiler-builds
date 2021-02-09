6d4890b14b7d9e44286aeca123f5e8d546646a9e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 22:14:33 +0000

    ce44a3d1dc fix(localize): support downleveled inlined helper localize calls (#40754)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+307.sha-46ab859'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+306.sha-ce44a3d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
