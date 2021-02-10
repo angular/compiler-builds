c7365d8404bf149137d7b0a7264f3c67a7de621f
GitHub <noreply@github.com>
2021-02-10 23:49:26 +0000

    9b4263bf70 release: cut the v12.0.0-next.0 release (#40793)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+325.sha-06ee07b'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+326.sha-9b4263b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+325.sha-06ee07b'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+326.sha-9b4263b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
