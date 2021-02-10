ca41192f2d7265b22ab93e6a85a3d47bc791f3dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:25:21 +0000

    378da71f27 fix(compiler-cli): don't crash when we can't resolve a resource (#40660)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+312.sha-bc5a694'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+313.sha-378da71'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
