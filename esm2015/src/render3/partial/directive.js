12bbca488f8f333448cdf8c3cb06dff25e689908
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:44:31 +0000

    fface518f1 docs: add Angular + NgRx book by the asian BPB Publications (#39680)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+320.sha-bbf61fc'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+324.sha-fface51'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+320.sha-bbf61fc'));
+        definitionMap.set('version', o.literal('11.1.0-next.4+324.sha-fface51'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
