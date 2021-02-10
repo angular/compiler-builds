a925476e3718c1f826bc74ea9fe45ad8ef8fec25
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:45:34 +0000

    7a8821c9aa docs: add link to component-less routes description. (#40741)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+324.sha-fface51'));
+    definitionMap.set('version', o.literal('11.1.0-next.4+321.sha-7a8821c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
