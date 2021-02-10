effca1126a221d2c853e05c302338d2e976a5d89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:35:46 +0000

    03f0b157c1 fix(common): date is not correctly formatted when year is between 0 and 99 (#40448)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+313.sha-378da71
+ * @license Angular v11.1.0-next.4+316.sha-03f0b15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.1.0-next.4+313.sha-378da71');
+const VERSION$1 = new Version('11.1.0-next.4+316.sha-03f0b15');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.1.0-next.4+313.sha-378da71'));
+    definitionMap.set('version', literal('11.1.0-next.4+316.sha-03f0b15'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
