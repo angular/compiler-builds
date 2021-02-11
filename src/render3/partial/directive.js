61c91d7c9f727d451024425a604978724ec2940c
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:33:17 +0000

    c22ae5d9b9 fix(dev-infra): update npm package subsitutions to reflect no longer having an imported workspace (#40785)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.0+7.sha-2d7bc1d'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+8.sha-c22ae5d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
