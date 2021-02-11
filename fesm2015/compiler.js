eba92380468ece77c40389fd0bb928ea63caa1e7
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:00:16 +0000

    e27787a583 build: remove jpm dependency (#40807)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+12.sha-fcf00de
+ * @license Angular v12.0.0-next.0+13.sha-e27787a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.0+12.sha-fcf00de');
+const VERSION$1 = new Version('12.0.0-next.0+13.sha-e27787a');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.0+12.sha-fcf00de'));
+    definitionMap.set('version', literal('12.0.0-next.0+13.sha-e27787a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
