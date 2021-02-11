a1403395bd4c172ac528d2e6e2439ec7833000c0
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:19:56 +0000

    7ca244a38b docs(core): add missing dot at the end of the sentence (#40800)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+4.sha-68da05d
+ * @license Angular v12.0.0-next.0+6.sha-7ca244a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.0+4.sha-68da05d');
+const VERSION$1 = new Version('12.0.0-next.0+6.sha-7ca244a');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.0+4.sha-68da05d'));
+    definitionMap.set('version', literal('12.0.0-next.0+6.sha-7ca244a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
