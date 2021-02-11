022181ce1f146f1d61007751cda527842ad6828f
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:26:37 +0000

    6f017a4d60 docs: fix typo in workspace config docs (#40784)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+1.sha-06671e8
+ * @license Angular v12.0.0-next.0+2.sha-6f017a4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20503,7 +20503,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.0+1.sha-06671e8');
+const VERSION$1 = new Version('12.0.0-next.0+2.sha-6f017a4');
 
 /**
  * @license
@@ -30039,7 +30039,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.0+1.sha-06671e8'));
+    definitionMap.set('version', literal('12.0.0-next.0+2.sha-6f017a4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
