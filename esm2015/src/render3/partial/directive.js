cd9ef6875da861fdc6ee1884ff5e5021647cfcc1
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:24:11 +0000

    06671e85f9 ci: Remove language-service from testing group (#40787)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.1.0-next.4+326.sha-9b4263b'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+1.sha-06671e8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.1.0-next.4+326.sha-9b4263b'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+1.sha-06671e8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
