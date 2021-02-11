9290a663780db50df0b7f8a193d65b3ee4a9575c
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:02:04 +0000

    e27787a583 build: remove jpm dependency (#40807)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.0+12.sha-fcf00de'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+13.sha-e27787a'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
