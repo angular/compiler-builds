040adb15865b017ab0c33033c89c551b8092f259
Joey Perrott <josephperrott@gmail.com>
2021-02-12 21:50:05 +0000

    44b8b0ffe1 release: cut the zone.js-0.11.4 release (#40775)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.0+28.sha-b4c8143'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+30.sha-44b8b0f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
