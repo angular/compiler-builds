19ad65abcfed49089f5fbb399286988e92d7ae76
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:19:51 +0000

    6425a6d543 docs: typo fix (#40804) PR Close #40804

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+25.sha-9cdf654'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+26.sha-6425a6d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
