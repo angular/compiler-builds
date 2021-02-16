e28e2d620a4c45c6467440f7bbe9b8eeef19d485
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:24:33 +0000

    12f7101d92 docs(forms): fix link for browser compatibility of using selects in IE (#40855)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.0+67.sha-75db0c7'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+69.sha-12f7101'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
