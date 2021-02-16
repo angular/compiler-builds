ddbf7f13486055d65322480a5badaca8b7792b7f
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:23:26 +0000

    75db0c7445 docs: edit builtin-directives doc (#39816)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+66.sha-95ad452'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+67.sha-75db0c7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+66.sha-95ad452'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+67.sha-75db0c7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
