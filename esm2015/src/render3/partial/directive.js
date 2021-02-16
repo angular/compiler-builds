0984ee81622b61332cd2adfb5a8ca4f97b25ed61
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:23:34 +0000

    7fa881919b docs: Add documentation about the CLI failing (#35385)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+42.sha-19114dc'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+44.sha-7fa8819'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
