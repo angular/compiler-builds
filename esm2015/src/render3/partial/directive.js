dc99ae1eb0ab9b321321dbd55ac698100dd1916c
Joey Perrott <josephperrott@gmail.com>
2021-02-16 15:59:35 +0000

    5f0c219883 docs: fix schematics' package name for `ng generate` sub-commands (#40846)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+36.sha-72a00dc'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+38.sha-5f0c219'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
