0cc5be8a0eb3152e313c1e0d0ec1c85eb74715ec
Zach Arend <zachzach@google.com>
2021-02-17 21:42:13 +0000

    1de999b89d docs: fix minor grammar mistakes in What is Angular topic (#40892)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+2.sha-b7a2d0d'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+4.sha-1de999b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
