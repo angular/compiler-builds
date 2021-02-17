c6b040053983ecc3b9117d23e36e86ed9f831d08
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:23:32 +0000

    322951af49 refactor(compiler-cli): error on cyclic imports in partial compilation (#40782)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+75.sha-0807d13'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+73.sha-322951a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+75.sha-0807d13
+ * @license Angular v12.0.0-next.0+73.sha-322951a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20638,7 +20638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.0+75.sha-0807d13');
+const VERSION$1 = new Version('12.0.0-next.0+73.sha-322951a');
 
 /**
  * @license
@@ -30174,7 +30174,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.0+75.sha-0807d13'));
+    definitionMap.set('version', literal('12.0.0-next.0+73.sha-322951a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30395,7 +30395,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.0+75.sha-0807d13'));
+    definitionMap.set('version', literal('12.0.0-next.0+73.sha-322951a'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
