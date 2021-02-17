5f56b66959cff1af6542babb61889fbc8d20520a
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:20:12 +0000

    29a99e806e docs: fix grammar in angular-compiler-options file (#40879)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+61.sha-53865c4
+ * @license Angular v11.2.0+68.sha-29a99e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21924,7 +21924,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.2.0+61.sha-53865c4');
+    var VERSION$1 = new Version('11.2.0+68.sha-29a99e8');
 
     /**
      * @license
@@ -31836,7 +31836,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.0+61.sha-53865c4'));
+        definitionMap.set('version', literal('11.2.0+68.sha-29a99e8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32061,7 +32061,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.0+61.sha-53865c4'));
+        definitionMap.set('version', literal('11.2.0+68.sha-29a99e8'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+61.sha-53865c4
+ * @license Angular v11.2.0+68.sha-29a99e8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20638,7 +20638,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('11.2.0+61.sha-53865c4');
+const VERSION$1 = new Version('11.2.0+68.sha-29a99e8');
 
 /**
  * @license
@@ -30174,7 +30174,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.2.0+61.sha-53865c4'));
+    definitionMap.set('version', literal('11.2.0+68.sha-29a99e8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30395,7 +30395,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('11.2.0+61.sha-53865c4'));
+    definitionMap.set('version', literal('11.2.0+68.sha-29a99e8'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
