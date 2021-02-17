9cf31f11c27c852ff2b6995eb3e3b0b6d6d86250
GitHub <noreply@github.com>
2021-02-17 19:05:15 +0000

    a9936c22ba docs: release notes for the v11.2.1 release (#40888)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.0+73.sha-322951a'));
+        definitionMap.set('version', o.literal('12.0.0-next.0+77.sha-a9936c2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
