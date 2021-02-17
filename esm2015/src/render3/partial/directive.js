7bbfbc63a5b7bd597f751d15a562af438b81a3bb
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:19:21 +0000

    9b26f462d0 docs: fix grammar in angular-compiler-options file (#40879)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.0+74.sha-13d82ce'));
+    definitionMap.set('version', o.literal('12.0.0-next.0+76.sha-9b26f46'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
