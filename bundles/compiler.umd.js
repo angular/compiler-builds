3b21bbbe34086d086c38f1672ed284b941711ebe
Zach Arend <zachzach@google.com>
2021-02-18 01:24:00 +0000

    2375b85fe0 docs: remove mention of unused `Intl` polyfill (#40883)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+6.sha-f57cd19
+ * @license Angular v12.0.0-next.1+8.sha-2375b85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21950,7 +21950,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.1+6.sha-f57cd19');
+    var VERSION$1 = new Version('12.0.0-next.1+8.sha-2375b85');
 
     /**
      * @license
@@ -31862,7 +31862,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+6.sha-f57cd19'));
+        definitionMap.set('version', literal('12.0.0-next.1+8.sha-2375b85'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32087,7 +32087,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+6.sha-f57cd19'));
+        definitionMap.set('version', literal('12.0.0-next.1+8.sha-2375b85'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
