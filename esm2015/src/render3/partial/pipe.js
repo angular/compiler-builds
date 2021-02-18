a8a70506972e0bb7971c1ee37921d29985c8d1b3
Zach Arend <zachzach@google.com>
2021-02-18 01:26:10 +0000

    65baac6055 docs: update attribute-binding.md to correctly describe [attr.binding] rules (#40878)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+8.sha-2375b85'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+9.sha-65baac6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+8.sha-2375b85'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+9.sha-65baac6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
