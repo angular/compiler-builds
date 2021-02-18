9506de358c2d1b0f6400d2345c902657b4e7f9da
Zach Arend <zachzach@google.com>
2021-02-18 01:25:27 +0000

    2375b85fe0 docs: remove mention of unused `Intl` polyfill (#40883)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+6.sha-f57cd19'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+8.sha-2375b85'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+6.sha-f57cd19'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+8.sha-2375b85'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
