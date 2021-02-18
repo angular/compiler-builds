62c931e38e6582a8f19589602e430ca52afce4e3
Zach Arend <zachzach@google.com>
2021-02-18 01:27:44 +0000

    542ba1fc00 refactor(compiler): add process title to Angular node binaries (#40648)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+9.sha-65baac6'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+10.sha-542ba1f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
