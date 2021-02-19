c09b4943e46a1a5d82d48ac9cc05fc9ed6622881
atscott <atscott01@gmail.com>
2021-02-19 17:39:21 +0000

    a82fddf1ce feat(router): Allow for custom router outlet implementations (#40827)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+32.sha-09e1e19'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+16.sha-a82fddf'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
