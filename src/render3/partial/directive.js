9a8782b3b8bfa6752513ca0b798e5aa3b7a7e575
atscott <atscott01@gmail.com>
2021-02-19 17:36:03 +0000

    09e1e1935a refactor(docs-infra): convert Sass mixin from camelCase to kebab-case to follow Sass conventions (#40881)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+14.sha-b4301c3'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+32.sha-09e1e19'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
