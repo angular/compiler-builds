5e8e8b4b57c3b46151787df763b1794ecaafc4f0
atscott <atscott01@gmail.com>
2021-02-19 17:37:50 +0000

    a82fddf1ce feat(router): Allow for custom router outlet implementations (#40827)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+32.sha-09e1e19'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+16.sha-a82fddf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
