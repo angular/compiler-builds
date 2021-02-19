c528cbe883d41c3e95771ab5d14d4e35e4de6111
atscott <atscott01@gmail.com>
2021-02-19 17:25:33 +0000

    aed2782c4a docs: the token should be associated with LibHeaderComponent (#40922)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+11.sha-30d5e25'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+12.sha-aed2782'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+11.sha-30d5e25'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+12.sha-aed2782'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
