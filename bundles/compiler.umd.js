132607a6fcf9866be3fc66cddb30693b0cdc8298
Andrew Scott <atscott@google.com>
2021-02-19 16:33:32 +0000

    30d5e25a28 docs: edit attribute-directives.md, move best practice to styleguide (#39849)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+10.sha-542ba1f
+ * @license Angular v12.0.0-next.1+11.sha-30d5e25
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21950,7 +21950,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.1+10.sha-542ba1f');
+    var VERSION$1 = new Version('12.0.0-next.1+11.sha-30d5e25');
 
     /**
      * @license
@@ -31862,7 +31862,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+10.sha-542ba1f'));
+        definitionMap.set('version', literal('12.0.0-next.1+11.sha-30d5e25'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32087,7 +32087,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+10.sha-542ba1f'));
+        definitionMap.set('version', literal('12.0.0-next.1+11.sha-30d5e25'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
