5c7c886e0ec4dc2870e8237fcc86e7fb58963a03
Andrew Scott <atscott@google.com>
2021-02-22 21:37:35 +0000

    f31a6015a0 perf(language-service): short-circuit LS operations (#40946)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+42.sha-b84f719'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+43.sha-f31a601'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
