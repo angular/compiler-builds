65b98eb89c02acbd341ae9c6df5d040a23c7e4a2
atscott <atscott01@gmail.com>
2021-02-23 19:08:08 +0000

    edd26a2b79 docs: improve accessibility of toh-pt6 (#40805)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+52.sha-4d1299b'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+51.sha-edd26a2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+52.sha-4d1299b'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+51.sha-edd26a2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
