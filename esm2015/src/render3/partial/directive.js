d87c21fa2b9911aba0e9b0d54548a85e2a4a1dbf
atscott <atscott01@gmail.com>
2021-02-23 16:35:13 +0000

    d3705b3284 fix(common): avoid mutating context object in NgTemplateOutlet (#40360)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+45.sha-ddf7970'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+47.sha-d3705b3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
