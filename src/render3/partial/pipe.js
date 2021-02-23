ca33f2a7d4acf60fd43266dd1b724145b0e3686a
atscott <atscott01@gmail.com>
2021-02-23 19:02:39 +0000

    596dfb88a6 test(bazel): use bazel runfiles helper for resolving file names (#40955)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+47.sha-d3705b3'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+50.sha-596dfb8'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
