fff2f880681dffeef9cdec8f4ecdc659955fb7ba
atscott <atscott01@gmail.com>
2021-02-23 19:06:58 +0000

    bd6f377db2 docs: improve accessibility of tohpt4 (#40622)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+50.sha-596dfb8'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+53.sha-bd6f377'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+50.sha-596dfb8'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+53.sha-bd6f377'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
