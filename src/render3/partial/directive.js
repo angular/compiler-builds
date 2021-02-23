23ac868aaddec3b6b3bfcb0cadc95a8fe0d23bfb
atscott <atscott01@gmail.com>
2021-02-23 19:11:46 +0000

    1f7921be33 docs: improve accessibility of tohpt3 (#40617)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+55.sha-1f91672'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+54.sha-1f7921b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
