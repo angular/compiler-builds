75c6253e4f503b0213e0bc1b2af89dab3e5819aa
atscott <atscott01@gmail.com>
2021-02-23 19:14:21 +0000

    7101267923 refactor(language-service): add type guard for NgLanguageService (#40954)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+56.sha-06d3267'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+58.sha-7101267'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
