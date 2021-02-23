a87d1a0835bf9ec651cfbfed5d76c976c3184d7a
atscott <atscott01@gmail.com>
2021-02-23 19:10:17 +0000

    1f91672d50 docs: improve accessibility of toh2 example (#40575)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+51.sha-edd26a2'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+55.sha-1f91672'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
