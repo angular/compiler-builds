f2f59955bc1eb12d2032b99178cc420dbd215a2e
atscott <atscott01@gmail.com>
2021-02-23 16:33:41 +0000

    d3705b3284 fix(common): avoid mutating context object in NgTemplateOutlet (#40360)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+45.sha-ddf7970
+ * @license Angular v12.0.0-next.1+47.sha-d3705b3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20664,7 +20664,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.1+45.sha-ddf7970');
+const VERSION$1 = new Version('12.0.0-next.1+47.sha-d3705b3');
 
 /**
  * @license
@@ -30200,7 +30200,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.1+45.sha-ddf7970'));
+    definitionMap.set('version', literal('12.0.0-next.1+47.sha-d3705b3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30421,7 +30421,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.1+45.sha-ddf7970'));
+    definitionMap.set('version', literal('12.0.0-next.1+47.sha-d3705b3'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+45.sha-ddf7970'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+47.sha-d3705b3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
