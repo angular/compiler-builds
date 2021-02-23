d3cadfd44a9db05d3199b1e5a793f5fb12fa1ae4
atscott <atscott01@gmail.com>
2021-02-23 19:05:45 +0000

    c6e2234fa6 docs: improve accessibility for toh-pt1 example (#40560)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.2+1.sha-09b92d7'));
+        definitionMap.set('version', o.literal('11.2.2+10.sha-c6e2234'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
