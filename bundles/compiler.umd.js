98f8c026889745be5721a311a283942c5cb1fd7f
Zach Arend <zachzach@google.com>
2021-02-24 23:03:18 +0000

    51a79772b2 test(localize): ensure tests pass on Windows (#40952)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+1.sha-8d13f63
+ * @license Angular v12.0.0-next.2+3.sha-51a7977
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21950,7 +21950,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.2+1.sha-8d13f63');
+    var VERSION$1 = new Version('12.0.0-next.2+3.sha-51a7977');
 
     /**
      * @license
@@ -31862,7 +31862,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.2+1.sha-8d13f63'));
+        definitionMap.set('version', literal('12.0.0-next.2+3.sha-51a7977'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32087,7 +32087,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.2+1.sha-8d13f63'));
+        definitionMap.set('version', literal('12.0.0-next.2+3.sha-51a7977'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
