646396718fa2ec84f9d1b71a37914b783ab54d31
atscott <atscott01@gmail.com>
2021-02-24 17:47:36 +0000

    d1535a1a77 test(language-service): remove circular dependency (#40966)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+64.sha-f93eae7'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+74.sha-d1535a1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
