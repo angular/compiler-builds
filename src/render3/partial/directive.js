51cdce6c79add0f2623d9c826abe62070fbbd64b
Andrew Scott <atscott@google.com>
2021-02-24 16:23:42 +0000

    35514347df build: update to rules_nodejs@3.2.1 (#40972)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.2+13.sha-db28ed2'));
+        definitionMap.set('version', o.literal('11.2.2+15.sha-3551434'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
