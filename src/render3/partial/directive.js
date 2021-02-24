92f9184e8fa1f66b09ff223c5ca60eda58a4778b
Zach Arend <zachzach@google.com>
2021-02-24 23:50:46 +0000

    6c05c80f19 feat(router): Add more find-tuned control in `routerLinkActiveOptions` (#40303)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+6.sha-29d8a0a'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+7.sha-6c05c80'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
