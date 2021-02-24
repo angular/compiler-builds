47233b31f6afc07ba35c2e47492a993c1bc857fa
Andrew Scott <atscott@google.com>
2021-02-24 16:17:46 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+59.sha-011a527
+ * @license Angular v12.0.0-next.1+60.sha-03de2f2
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21950,7 +21950,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.1+59.sha-011a527');
+    var VERSION$1 = new Version('12.0.0-next.1+60.sha-03de2f2');
 
     /**
      * @license
@@ -31862,7 +31862,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+59.sha-011a527'));
+        definitionMap.set('version', literal('12.0.0-next.1+60.sha-03de2f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32087,7 +32087,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+59.sha-011a527'));
+        definitionMap.set('version', literal('12.0.0-next.1+60.sha-03de2f2'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+59.sha-011a527'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+60.sha-03de2f2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
