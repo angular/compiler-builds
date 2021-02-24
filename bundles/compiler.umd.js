37869be7be252ffeb8a69444ea405f40f7b1b86d
GitHub <noreply@github.com>
2021-02-24 18:12:39 +0000

    be2ddf34e8 docs: copy release notes from 11.2.3 (#40982)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.1+74.sha-d1535a1
+ * @license Angular v12.0.0-next.1+75.sha-be2ddf3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21950,7 +21950,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.1+74.sha-d1535a1');
+    var VERSION$1 = new Version('12.0.0-next.1+75.sha-be2ddf3');
 
     /**
      * @license
@@ -31862,7 +31862,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+74.sha-d1535a1'));
+        definitionMap.set('version', literal('12.0.0-next.1+75.sha-be2ddf3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32087,7 +32087,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.1+74.sha-d1535a1'));
+        definitionMap.set('version', literal('12.0.0-next.1+75.sha-be2ddf3'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
