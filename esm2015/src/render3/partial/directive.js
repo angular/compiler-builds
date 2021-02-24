e91bc5a4842c9959b51d8138c5cacd6aa79bf685
Andrew Scott <atscott@google.com>
2021-02-24 16:21:18 +0000

    aaf9b31fb4 feat(core): drop support for zone.js 0.10.x (#40823)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+60.sha-03de2f2'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+63.sha-aaf9b31'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+60.sha-03de2f2'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+63.sha-aaf9b31'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
