e2ac2160ab0fe0b771f5ddf37dc355896435cf9c
atscott <atscott01@gmail.com>
2021-02-24 17:13:01 +0000

    3fe759eb6c test(language-service): remove circular dependency (#40966)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.2+15.sha-3551434'));
+        definitionMap.set('version', o.literal('11.2.2+25.sha-3fe759e'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
