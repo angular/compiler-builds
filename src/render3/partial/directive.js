3297d335e4e60e70e22d6798cfc700080e8fc4fe
Zach Arend <zachzach@google.com>
2021-02-24 23:31:23 +0000

    29d8a0ab09 feat(animations): add support for disabling animations through BrowserAnimationsModule.withConfig (#40731)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+5.sha-3c24136'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+6.sha-29d8a0a'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
