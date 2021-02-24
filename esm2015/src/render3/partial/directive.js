6eede18c02c301468bba0f5b2a68322ce9194ea4
Andrew Scott <atscott@google.com>
2021-02-24 16:16:11 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.1+59.sha-011a527'));
+    definitionMap.set('version', o.literal('12.0.0-next.1+60.sha-03de2f2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.1+59.sha-011a527'));
+        definitionMap.set('version', o.literal('12.0.0-next.1+60.sha-03de2f2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
