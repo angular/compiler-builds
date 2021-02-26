6fc6bfced352952d0f226ace57bd04a3d2235e6d
Zach Arend <zachzach@google.com>
2021-02-26 16:44:09 +0000

    6206d99b86 docs(forms): add array example to get method (#41008)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+9.sha-953e98e
+ * @license Angular v12.0.0-next.2+8.sha-6206d99
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20665,7 +20665,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.2+9.sha-953e98e');
+const VERSION$1 = new Version('12.0.0-next.2+8.sha-6206d99');
 
 /**
  * @license
@@ -30201,7 +30201,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+9.sha-953e98e'));
+    definitionMap.set('version', literal('12.0.0-next.2+8.sha-6206d99'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30422,7 +30422,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+9.sha-953e98e'));
+    definitionMap.set('version', literal('12.0.0-next.2+8.sha-6206d99'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+9.sha-953e98e'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+8.sha-6206d99'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
