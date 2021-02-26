e9330486500a4b69ec7d0ceb5e53af44bf94a3fe
Zach Arend <zachzach@google.com>
2021-02-26 20:58:33 +0000

    91cdc11aa0 fix(common): allow number or boolean as http params (#40663)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+13.sha-383d226'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+14.sha-91cdc11'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
