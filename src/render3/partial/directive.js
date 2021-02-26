5f16a7d48cd218e55ba0a88fdcbcf46200cc87d1
Zach Arend <zachzach@google.com>
2021-02-26 23:32:09 +0000

    fb82558d78 docs: Change lexical in security guide (#41016)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+14.sha-91cdc11'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+15.sha-fb82558'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
