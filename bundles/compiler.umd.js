8c301a213be7d8384a84f38286873095430a5117
Zach Arend <zachzach@google.com>
2021-02-26 16:51:53 +0000

    383d226118 fix(docs-infra): add links to glossary for terms on the "Press kit" page (#40945)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+10.sha-284af73
+ * @license Angular v12.0.0-next.2+13.sha-383d226
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21951,7 +21951,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.2+10.sha-284af73');
+    var VERSION$1 = new Version('12.0.0-next.2+13.sha-383d226');
 
     /**
      * @license
@@ -31863,7 +31863,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.2+10.sha-284af73'));
+        definitionMap.set('version', literal('12.0.0-next.2+13.sha-383d226'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32088,7 +32088,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.2+10.sha-284af73'));
+        definitionMap.set('version', literal('12.0.0-next.2+13.sha-383d226'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
