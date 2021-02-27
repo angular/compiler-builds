85b5128360c2a3ed03aa63f5904a76eed0fa23eb
Zach Arend <zachzach@google.com>
2021-02-27 00:12:27 +0000

    be8893fd1d docs: add youtube to social icons (#40987)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+33.sha-bebdeba'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+34.sha-be8893f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
