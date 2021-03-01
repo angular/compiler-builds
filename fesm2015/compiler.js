fa3c82ce256885a1f8bc4d72ca912f070606a05a
Zach Arend <zachzach@google.com>
2021-03-01 23:50:27 +0000

    356d9a0537 docs: improve contrast of colors and body padding in boilerplate CSS (#40890)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+39.sha-54b0889
+ * @license Angular v12.0.0-next.2+40.sha-356d9a0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20660,7 +20660,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.2+39.sha-54b0889');
+const VERSION$1 = new Version('12.0.0-next.2+40.sha-356d9a0');
 
 /**
  * @license
@@ -30196,7 +30196,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+39.sha-54b0889'));
+    definitionMap.set('version', literal('12.0.0-next.2+40.sha-356d9a0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30417,7 +30417,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+39.sha-54b0889'));
+    definitionMap.set('version', literal('12.0.0-next.2+40.sha-356d9a0'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+39.sha-54b0889'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+40.sha-356d9a0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
