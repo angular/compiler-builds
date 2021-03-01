4f934709d4f7a5ea2d7b577fc7d7959b5466a68f
Zach Arend <zachzach@google.com>
2021-03-01 23:49:14 +0000

    54b088967a refactor(compiler): remove unreachable code (#40984)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+38.sha-a31da48'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+39.sha-54b0889'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
