29db257b654037ac6ab55d6aee2f15f9e9f3fb92
Zach Arend <zachzach@google.com>
2021-03-01 23:47:19 +0000

    a31da48507 fix(animations): cleanup DOM elements when the root view is removed (#41001)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+37.sha-8110cf0'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+38.sha-a31da48'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
