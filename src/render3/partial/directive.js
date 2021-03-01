985e1bb3b282dfa4cdd42c1ea39a329a1ce162d8
Zach Arend <zachzach@google.com>
2021-03-01 17:20:00 +0000

    e12d9dec64 fix(core): remove duplicated EMPTY_ARRAY constant (#40991)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+34.sha-be8893f'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+35.sha-e12d9de'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
