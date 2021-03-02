af9904151b47db75f6b1979148c3e996e6ef08fb
Zach Arend <zachzach@google.com>
2021-03-02 00:05:16 +0000

    3370ade9a4 refactor(platform-browser): cast response of JSON.parse (#41020)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+41.sha-6c783c7'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+43.sha-3370ade'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
