9397e422f4e8b727c83fb00021f60e893142d65b
Zach Arend <zachzach@google.com>
2021-03-03 21:59:17 +0000

    bb6e5e2dd0 fix(language-service): don't show external template diagnostics in ts files (#41070)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+57.sha-0847a03'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+58.sha-bb6e5e2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
