0b232161542ea08043fdcb77653a48b76ab2bb9f
Zach Arend <zachzach@google.com>
2021-03-03 18:01:15 +0000

    10aa5641dd fix(language-service): only provide template results on reference requests (#41041)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+46.sha-cb16035'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+47.sha-10aa564'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
