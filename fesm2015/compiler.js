62f7f1c1c20fd719d801de302bc4517aaf02e737
Zach Arend <zachzach@google.com>
2021-03-03 18:25:11 +0000

    7765b648e7 docs: update Angular Global Summit on event pages (#41050)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.2+54.sha-d105994
+ * @license Angular v12.0.0-next.2+55.sha-7765b64
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20661,7 +20661,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.2+54.sha-d105994');
+const VERSION$1 = new Version('12.0.0-next.2+55.sha-7765b64');
 
 /**
  * @license
@@ -30197,7 +30197,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+54.sha-d105994'));
+    definitionMap.set('version', literal('12.0.0-next.2+55.sha-7765b64'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30418,7 +30418,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.2+54.sha-d105994'));
+    definitionMap.set('version', literal('12.0.0-next.2+55.sha-7765b64'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
