fd698ede39ad97d91e0afa67ac93d524038fa61e
Zach Arend <zachzach@google.com>
2021-03-03 18:06:35 +0000

    21f0deeaa6 build(docs-infra): update Angular framework, Material and CLI to latest methods (#40994)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+47.sha-10aa564'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
