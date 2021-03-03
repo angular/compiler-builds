892bd14ae85c259a1202a770195b9a41d7aa9b0f
Zach Arend <zachzach@google.com>
2021-03-03 23:18:55 +0000

    4606a6097b docs: release notes for the v11.2.4 release

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+58.sha-bb6e5e2'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+59.sha-4606a60'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
