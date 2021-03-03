537a4ea2c76dfbaf446888403cef7231d3886a4a
Zach Arend <zachzach@google.com>
2021-03-03 18:21:16 +0000

    97b88f3631 fix(compiler): allow binding to autocomplete property on select and textarea elements (#40928)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+52.sha-736b1f9'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+53.sha-97b88f3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
