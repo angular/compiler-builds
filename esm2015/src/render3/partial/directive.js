43331872071d1eb165e454cc2fad67ce1989f6ae
Zach Arend <zachzach@google.com>
2021-03-03 18:05:11 +0000

    21f0deeaa6 build(docs-infra): update Angular framework, Material and CLI to latest methods (#40994)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+47.sha-10aa564'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+47.sha-10aa564'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
