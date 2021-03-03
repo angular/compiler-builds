d0f603968773ada0d46ad4538ad00a2d00e989bb
Zach Arend <zachzach@google.com>
2021-03-03 18:08:43 +0000

    e9e7c33f3c fix(language-service): Add plugin option to force strictTemplates (#41062)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
+    definitionMap.set('version', o.literal('12.0.0-next.2+50.sha-e9e7c33'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
