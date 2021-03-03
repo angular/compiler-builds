13dcb10a3b6402c9844bc098d93a2ac8dea04640
Zach Arend <zachzach@google.com>
2021-03-03 18:07:16 +0000

    e9e7c33f3c fix(language-service): Add plugin option to force strictTemplates (#41062)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+50.sha-e9e7c33'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+49.sha-21f0dee'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+50.sha-e9e7c33'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
