a00746addb71ce610bbf72ab4ace669187cb13b5
Zach Arend <zachzach@google.com>
2021-03-03 23:17:25 +0000

    4606a6097b docs: release notes for the v11.2.4 release

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+58.sha-bb6e5e2'));
+        definitionMap.set('version', o.literal('12.0.0-next.2+59.sha-4606a60'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
