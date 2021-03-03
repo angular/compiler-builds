3b1d72e675c6b8c40f70322679dddf8271f94bc0
Zach Arend <zachzach@google.com>
2021-03-03 23:45:37 +0000

    a10fe450d9 release: cut the v12.0.0-next.3 release

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.2+59.sha-4606a60'));
+    definitionMap.set('version', o.literal('12.0.0-next.3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
