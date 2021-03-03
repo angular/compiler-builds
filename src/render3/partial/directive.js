f3e0e00a8681fc81d93c068770ed63bd40ea31ca
Zach Arend <zachzach@google.com>
2021-03-03 23:44:11 +0000

    a10fe450d9 release: cut the v12.0.0-next.3 release

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.2+59.sha-4606a60'));
+        definitionMap.set('version', o.literal('12.0.0-next.3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
