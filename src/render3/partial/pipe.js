04b4d4419cba12bf9a9cb1232d15f3b24654635d
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:19:24 +0000

    d9acaa8547 docs: remove Codelyzer recommendation from style guide (#40992)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+3.sha-ad40fca'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+4.sha-d9acaa8'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
