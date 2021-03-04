eeae1700392bd97cc10839e68eb180147af036bf
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:16:35 +0000

    9ae0faa00a docs(router): fix typo for the equivalent `exactMatchOptions` value (#41075)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+2.sha-9ae0faa'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
