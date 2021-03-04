29a40f5c70207a53c7a1cbbb010b31bfa830b877
Andrew Kushnir <akushnir@google.com>
2021-03-04 23:09:13 +0000

    45216ccc0d fix(language-service): Only provide dom completions for inline templates (#41078)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+13.sha-38524c4'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+14.sha-45216cc'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
