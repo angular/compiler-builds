951fc08497ebadbb9621c34f3f01875c1f90bb86
Andrew Kushnir <akushnir@google.com>
2021-03-04 21:29:27 +0000

    38524c4d29 fix(common): cleanup location change listeners when the root view is removed (#40867)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+12.sha-ca721c2'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+13.sha-38524c4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
