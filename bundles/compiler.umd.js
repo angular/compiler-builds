bb4a76849b7ef0f913be3f159cacd093101810b7
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:17:52 +0000

    9ae0faa00a docs(router): fix typo for the equivalent `exactMatchOptions` value (#41075)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3
+ * @license Angular v12.0.0-next.3+2.sha-9ae0faa
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21948,7 +21948,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.3');
+    var VERSION$1 = new Version('12.0.0-next.3+2.sha-9ae0faa');
 
     /**
      * @license
@@ -31860,7 +31860,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3'));
+        definitionMap.set('version', literal('12.0.0-next.3+2.sha-9ae0faa'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -32085,7 +32085,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3'));
+        definitionMap.set('version', literal('12.0.0-next.3+2.sha-9ae0faa'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
