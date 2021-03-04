b5234e0a2ae8159cece7d0d9d142ccb24c6fbe3e
Andrew Kushnir <akushnir@google.com>
2021-03-04 20:21:25 +0000

    ca721c2972 feat(core): more precise type for `APP_INITIALIZER` token (#40986)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+11.sha-bf158e7'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+12.sha-ca721c2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
