ff276313c1bbeb7e336e20bb7255a200f7c2f001
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:03:31 +0000

    4637df5551 test(forms): add integration test app to keep track of payload size (#41045)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+15.sha-fbc9df1
+ * @license Angular v12.0.0-next.3+16.sha-4637df5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21770,7 +21770,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.3+15.sha-fbc9df1');
+    var VERSION$1 = new Version('12.0.0-next.3+16.sha-4637df5');
 
     /**
      * @license
@@ -31614,7 +31614,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+15.sha-fbc9df1'));
+        definitionMap.set('version', literal('12.0.0-next.3+16.sha-4637df5'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31839,7 +31839,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+15.sha-fbc9df1'));
+        definitionMap.set('version', literal('12.0.0-next.3+16.sha-4637df5'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
