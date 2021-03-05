dd0d97f5a4bb743f81080a1127874cd8c6f2eae7
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:04:45 +0000

    4637df5551 test(forms): add integration test app to keep track of payload size (#41045)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+15.sha-fbc9df1'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+16.sha-4637df5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
