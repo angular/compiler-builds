4c36592c191fdff93af93cc43200829cc53dde10
Andrew Kushnir <akushnir@google.com>
2021-03-05 00:25:34 +0000

    fbc9df181e feat(compiler-cli): support producing Closure-specific PURE annotations (#41021)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+14.sha-45216cc'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+15.sha-fbc9df1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
