e7a5ee6173b4eb5666334fe88e930f695177868b
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:08:31 +0000

    44bb78dbbd docs: update repo link to auto scroll to readme section (#41105)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+18.sha-c676ec1'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+21.sha-44bb78d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
