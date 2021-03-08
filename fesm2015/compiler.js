c397256c4dcdbae5b1de8b6ae4dc7e789f5a3494
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:19:17 +0000

    fed6a7ce7d perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild (#40947)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+27.sha-32dd3c5
+ * @license Angular v12.0.0-next.3+29.sha-fed6a7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20480,7 +20480,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('12.0.0-next.3+27.sha-32dd3c5');
+const VERSION$1 = new Version('12.0.0-next.3+29.sha-fed6a7c');
 
 /**
  * @license
@@ -29952,7 +29952,7 @@ function compileDeclareDirectiveFromMetadata(meta) {
  */
 function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.3+27.sha-32dd3c5'));
+    definitionMap.set('version', literal('12.0.0-next.3+29.sha-fed6a7c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
@@ -30173,7 +30173,7 @@ function compileDeclarePipeFromMetadata(meta) {
  */
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', literal('12.0.0-next.3+27.sha-32dd3c5'));
+    definitionMap.set('version', literal('12.0.0-next.3+29.sha-fed6a7c'));
     definitionMap.set('ngImport', importExpr(Identifiers$1.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
