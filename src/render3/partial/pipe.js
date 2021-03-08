6e32af7c18078e31de5da25bdb96eb7754cd4e45
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:19:49 +0000

    c1a93fcf32 docs: remove augury resource (#41107)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+29.sha-fed6a7c'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+31.sha-c1a93fc'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
