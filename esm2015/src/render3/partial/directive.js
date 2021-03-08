34a66e0dcc26f354e567a4ae998a14e1873c2559
Andrew Kushnir <akushnir@google.com>
2021-03-08 23:17:14 +0000

    c5ead386b8 docs: remove augury resource from docs, partially addresses #41030 (#41120)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.2.4+30.sha-35935b4'));
+    definitionMap.set('version', o.literal('11.2.4+31.sha-c5ead38'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
