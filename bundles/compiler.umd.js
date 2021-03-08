76f3d3d2ff0179dc376e90b2c7d652465116b58f
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:12:01 +0000

    7854c4ddbe test(docs-infra): run a11y tests against `/tutorial` and update a11y scores (#41103)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+25.sha-681c341
+ * @license Angular v12.0.0-next.3+23.sha-7854c4d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21770,7 +21770,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.3+25.sha-681c341');
+    var VERSION$1 = new Version('12.0.0-next.3+23.sha-7854c4d');
 
     /**
      * @license
@@ -31614,7 +31614,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+25.sha-681c341'));
+        definitionMap.set('version', literal('12.0.0-next.3+23.sha-7854c4d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31839,7 +31839,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+25.sha-681c341'));
+        definitionMap.set('version', literal('12.0.0-next.3+23.sha-7854c4d'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
