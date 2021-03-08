c7e71a97bdcdfeb3b7b30fad441cbe5639abd576
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:51:26 +0000

    153e3a8960 test(dev-infra): fix test order failure in tests for the release command (#41023)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+36.sha-03d47d5'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+37.sha-153e3a8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+36.sha-03d47d5'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+37.sha-153e3a8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
