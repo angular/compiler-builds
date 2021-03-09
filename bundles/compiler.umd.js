cb0f6f8640e782d0f586e2aeb9c58918233eaccc
Andrew Kushnir <akushnir@google.com>
2021-03-09 18:59:58 +0000

    b50d88073e perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.3+59.sha-a730315
+ * @license Angular v12.0.0-next.3+61.sha-b50d880
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21759,7 +21759,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.3+59.sha-a730315');
+    var VERSION$1 = new Version('12.0.0-next.3+61.sha-b50d880');
 
     /**
      * @license
@@ -31604,7 +31604,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+59.sha-a730315'));
+        definitionMap.set('version', literal('12.0.0-next.3+61.sha-b50d880'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31829,7 +31829,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.3+59.sha-a730315'));
+        definitionMap.set('version', literal('12.0.0-next.3+61.sha-b50d880'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
