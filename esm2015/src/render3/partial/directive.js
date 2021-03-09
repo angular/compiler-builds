9b6d9772a3e509c02290eb0f2572eb031f9e85d6
Andrew Kushnir <akushnir@google.com>
2021-03-09 00:24:35 +0000

    fa3689f432 test(forms): split Forms example app into Reactive and Template-driven ones (#41108)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+42.sha-2ebe2bc'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+43.sha-fa3689f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
