b75fd296258a0cc3cbd4b3e231642cbe0e8f2b19
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:22:36 +0000

    a730315fb2 docs: use double quotes in Tour of Heroes (pt1) (#41134)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+60.sha-e29a29c'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+59.sha-a730315'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
