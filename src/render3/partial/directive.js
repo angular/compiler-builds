9dc4256191a2fac7fd61e0ae677b2dc9edaa9c14
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:18:47 +0000

    f281310d39 fix(docs-infra): print info to help debugging SW cache issue (#41106)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+57.sha-7a27cd2'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+58.sha-f281310'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
