bd92f9653054cb1d6af9cde557c2f99cb6154ad1
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:01:18 +0000

    b50d88073e perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+59.sha-a730315'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+61.sha-b50d880'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
