fd2ab6d70f778891d78b11cd018be55df2a941ae
Andrew Kushnir <akushnir@google.com>
2021-03-10 18:21:50 +0000

    ab15e61656 docs: release notes for the v11.2.5 release

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.3+62.sha-41b8f98'));
+        definitionMap.set('version', o.literal('12.0.0-next.3+63.sha-ab15e61'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
