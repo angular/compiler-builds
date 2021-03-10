c8b888cec049f185482e873ea4c8c7887c5778c2
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:41:37 +0000

    4c79b8a644 docs: Remove of unnecessary Input (#41150)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+2.sha-ba81e8c'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+3.sha-4c79b8a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+2.sha-ba81e8c'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+3.sha-4c79b8a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
