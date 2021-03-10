651bf4109bc0dd4086e652ee24ba814a3683cbeb
GitHub <noreply@github.com>
2021-03-10 18:24:27 +0000

    1fb0dbb300 release: cut the v12.0.0-next.4 release (#41156)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.3+63.sha-ab15e61'));
+    definitionMap.set('version', o.literal('12.0.0-next.3+64.sha-1fb0dbb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
