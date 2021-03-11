288f0c031a5164fb6443cbd606bf1fda4fe1193c
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:45:30 +0000

    79e21b966e fix(docs-infra): fix angular.io on IE 11 (#41162)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+4.sha-3c66b10
+ * @license Angular v12.0.0-next.4+6.sha-79e21b9
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21759,7 +21759,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.4+4.sha-3c66b10');
+    var VERSION$1 = new Version('12.0.0-next.4+6.sha-79e21b9');
 
     /**
      * @license
@@ -31604,7 +31604,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+4.sha-3c66b10'));
+        definitionMap.set('version', literal('12.0.0-next.4+6.sha-79e21b9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31829,7 +31829,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+4.sha-3c66b10'));
+        definitionMap.set('version', literal('12.0.0-next.4+6.sha-79e21b9'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+4.sha-3c66b10'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+6.sha-79e21b9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
