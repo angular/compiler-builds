b6882deff6c97fd7136d8d58589833d92afa9e61
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:49:36 +0000

    69afeb3808 fix(localize): trim extracted `equiv-text` values (#41180)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+9.sha-94e8f5d'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+10.sha-69afeb3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
