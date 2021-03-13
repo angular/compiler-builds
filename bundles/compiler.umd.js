398f0de81ad0017e216ae630f65d5f4f723520ea
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-13 00:14:43 +0000

    937e90cd16 perf(forms): make built-in ControlValueAccessors more tree-shakable (#41146)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+21.sha-8a9fe49
+ * @license Angular v12.0.0-next.4+22.sha-937e90c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21758,7 +21758,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.4+21.sha-8a9fe49');
+    var VERSION$1 = new Version('12.0.0-next.4+22.sha-937e90c');
 
     /**
      * @license
@@ -31603,7 +31603,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+21.sha-8a9fe49'));
+        definitionMap.set('version', literal('12.0.0-next.4+22.sha-937e90c'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31831,7 +31831,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+21.sha-8a9fe49'));
+        definitionMap.set('version', literal('12.0.0-next.4+22.sha-937e90c'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
