1fb5287508c405a5e1638cb597358deb36f218e9
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:38:11 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+24.sha-1644d64'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+25.sha-3bd1992'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
