d4f7495f64b5b29cf7a2994bd4c2137b053ca3f3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 17:52:59 +0000

    9ad57e6cb3 docs: document `help wanted` and `good first issue` labels (#41195)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+22.sha-937e90c'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+23.sha-9ad57e6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
