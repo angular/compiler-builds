11382dcb7b20abc32d6bbc351bbbcdd8198ed526
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 19:35:36 +0000

    cc75e1d17b docs: add contributors guide to aio (#41061)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+25.sha-3bd1992
+ * @license Angular v12.0.0-next.4+26.sha-cc75e1d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21758,7 +21758,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('12.0.0-next.4+25.sha-3bd1992');
+    var VERSION$1 = new Version('12.0.0-next.4+26.sha-cc75e1d');
 
     /**
      * @license
@@ -31603,7 +31603,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+25.sha-3bd1992'));
+        definitionMap.set('version', literal('12.0.0-next.4+26.sha-cc75e1d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31831,7 +31831,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('12.0.0-next.4+25.sha-3bd1992'));
+        definitionMap.set('version', literal('12.0.0-next.4+26.sha-cc75e1d'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
