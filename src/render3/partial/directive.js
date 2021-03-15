a073411dbe420210140af989f76ae4c64c6c0743
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:07:44 +0000

    1644d64398 feat(compiler-cli): introduce HttpContext request context (#25751)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+23.sha-9ad57e6'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+24.sha-1644d64'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
