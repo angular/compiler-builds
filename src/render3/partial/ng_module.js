8224777d73dfa4515a6bbbd743b0f19c3f902fee
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:52:44 +0000

    7d6db009e9 docs: update docs to reflect production mode by default (#41173)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -31,7 +31,7 @@
     exports.compileDeclareNgModuleFromMetadata = compileDeclareNgModuleFromMetadata;
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+34.sha-1a9f526'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+35.sha-7d6db00'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
