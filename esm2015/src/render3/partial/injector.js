21ce58c9a513c40fd655f1e9fee6b128c43e9c6c
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:51:26 +0000

    7d6db009e9 docs: update docs to reflect production mode by default (#41173)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+34.sha-1a9f526'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+35.sha-7d6db00'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
