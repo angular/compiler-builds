a679f34d91da3d49194e7eabf2a84fec03de783d
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:56:15 +0000

    e88a9c6350 perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -18,7 +18,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 }
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+35.sha-7d6db00'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+38.sha-e88a9c6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
