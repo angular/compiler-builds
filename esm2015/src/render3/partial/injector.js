8d5104ce146c3260143bb49b65cb47cdcbe91349
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 18:24:44 +0000

    6bdad5ef6c build(docs-infra): upgrade cli command docs sources to 46538eea1 (#41227)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.4+38.sha-e88a9c6'));
+    definitionMap.set('version', o.literal('12.0.0-next.4+40.sha-6bdad5e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+38.sha-e88a9c6'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+40.sha-6bdad5e'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
