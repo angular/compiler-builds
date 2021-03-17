ab0d6fead0ac2776b02955bbbdd79f4f0ac8e964
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:29:32 +0000

    98de4cb1bf docs: add deprecation note next to IE 11 in browser-support page (#41147)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.4+44.sha-0f0f34f'));
+        definitionMap.set('version', o.literal('12.0.0-next.4+47.sha-98de4cb'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
