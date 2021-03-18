802e34e7033ae9b2402c64c5c2d53bf8af62de19
Misko Hevery <misko@hevery.com>
2021-03-18 17:36:28 +0000

    ac9a250fc4 docs: update link to schematics schema (#41258)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+6.sha-ac9c428'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+7.sha-ac9a250'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
