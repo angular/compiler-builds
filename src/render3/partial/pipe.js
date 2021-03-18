ca9dcd604943e4269f97d0ceccb9d77a8571aa10
Misko Hevery <misko@hevery.com>
2021-03-18 17:43:33 +0000

    655507012b build(docs-infra): upgrade cli command docs sources to b6effecae (#41259)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+5.sha-e239941'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+8.sha-6555070'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
