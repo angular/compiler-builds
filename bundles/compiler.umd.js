34f5ccd5e00f20b8bc21b1c67a8ec1b34daf92ab
Misko Hevery <misko@hevery.com>
2021-03-18 17:44:02 +0000

    ba8da742e3 fix(core): fix possible XSS attack in development through SSR (#40525)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4
+ * @license Angular v10.2.4+1.sha-ba8da74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20632,7 +20632,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.4');
+    var VERSION$1 = new Version('10.2.4+1.sha-ba8da74');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4
+ * @license Angular v10.2.4+1.sha-ba8da74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19460,7 +19460,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.4');
+const VERSION$1 = new Version('10.2.4+1.sha-ba8da74');
 
 /**
  * @license
