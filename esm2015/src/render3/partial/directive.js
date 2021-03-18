894510cd2e63bb1d59e2afff649c0e4275607cff
Misko Hevery <misko@hevery.com>
2021-03-18 17:42:22 +0000

    655507012b build(docs-infra): upgrade cli command docs sources to b6effecae (#41259)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+5.sha-e239941'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+8.sha-6555070'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+5.sha-e239941'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+8.sha-6555070'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
