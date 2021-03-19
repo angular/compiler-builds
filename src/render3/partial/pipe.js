fe0d401d840bde224366917949b3e63118216fa6
Misko Hevery <misko@hevery.com>
2021-03-19 20:11:45 +0000

    fbdb19c263 docs(localize): expose documentation for the `@angular/localize` package (#40317)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+8.sha-6555070'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+9.sha-fbdb19c'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
