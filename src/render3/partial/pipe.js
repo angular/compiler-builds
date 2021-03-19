2e34dce97d0e1f901fe9c7c60bd8b26d7b471eb9
Misko Hevery <misko@hevery.com>
2021-03-19 20:22:43 +0000

    5eb7f3491f fix(dev-infra): remove commit message wizard and builder (#41280)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+12.sha-aa039a1'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+15.sha-5eb7f34'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
