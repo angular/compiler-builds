b2a2b401233a958e71d8baa184c3ff5c36b85732
Misko Hevery <misko@hevery.com>
2021-03-19 20:13:58 +0000

    2ccb579e06 feat(docs-infra): add profile picture size check (#41253)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+9.sha-fbdb19c'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+10.sha-2ccb579'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+9.sha-fbdb19c'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+10.sha-2ccb579'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
