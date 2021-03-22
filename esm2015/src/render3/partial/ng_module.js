9049d44127959ae902680e80f8ae0e11cee684a2
Misko Hevery <misko@hevery.com>
2021-03-22 20:06:33 +0000

    b5551609fe fix(router): fragment can be null (#37336)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -18,7 +18,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 }
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+23.sha-e7b1d43'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+24.sha-b555160'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
