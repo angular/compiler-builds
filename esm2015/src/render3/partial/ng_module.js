7bb18bd71c75019f56d56d8dffccc0a31cf458b9
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:59 +0000

    efe02d8b04 docs: update contributors.json (#41284)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -18,7 +18,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 }
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+16.sha-ed5bfe8'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+19.sha-efe02d8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
