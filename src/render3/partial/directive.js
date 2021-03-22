8355c21eed4bb4d1db96bb60aaf6e0912229ece7
Misko Hevery <misko@hevery.com>
2021-03-22 16:15:27 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+15.sha-5eb7f34'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+18.sha-5bc0a47'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
