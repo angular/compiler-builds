792a27c412a68baa46de2ae51fc4e7192446446f
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:30 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+18.sha-5bc0a47'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+16.sha-ed5bfe8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
