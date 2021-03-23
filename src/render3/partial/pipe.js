5f428f6499bf2967be84bf5970f4ff631a527c97
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:23 +0000

    31dc483206 docs: add alisa duncan to GDE resources (#41295)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+25.sha-c49b280'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+26.sha-31dc483'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
