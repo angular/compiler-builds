ae337d6d5202bf359b90de067950efd83e292aa4
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:32:59 +0000

    0516fbb180 refactor(dev-infra): use conventional-commits-parser for commit parsing (#41286)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -18,7 +18,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 }
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+36.sha-eba1289'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+37.sha-0516fbb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -31,7 +31,7 @@
     exports.compileDeclareNgModuleFromMetadata = compileDeclareNgModuleFromMetadata;
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+36.sha-eba1289'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+37.sha-0516fbb'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
