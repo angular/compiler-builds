908e11b693d0c85f63853525a7ef4a3f741a1da1
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:43:22 +0000

    65f7d5380d docs: add link to fork repo how-to (#41266)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -31,7 +31,7 @@
     exports.compileDeclareNgModuleFromMetadata = compileDeclareNgModuleFromMetadata;
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+31.sha-190fa07'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+33.sha-65f7d53'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
