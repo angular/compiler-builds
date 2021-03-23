553c1bbde0a8ba485efb6c6a743a17ad8d0c45b0
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:47:14 +0000

    274dc15452 fix(core): detect synthesized constructors that have been downleveled using TS 4.2 (#41305)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+33.sha-65f7d53'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+35.sha-274dc15'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
