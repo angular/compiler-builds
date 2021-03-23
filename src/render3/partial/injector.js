a8d21b899b94d939313809d608f94f9bfa9ec915
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:38:28 +0000

    190fa07b9a feat(router): add migration for ActivatedRouteSnapshot.fragment (#41092)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -30,7 +30,7 @@
     exports.compileDeclareInjectorFromMetadata = compileDeclareInjectorFromMetadata;
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+32.sha-6a6d332'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+31.sha-190fa07'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
