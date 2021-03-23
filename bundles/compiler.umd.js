cd6ce0b9aff11712d1b5cae9d80f69be7e1db824
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:27:47 +0000

    a68f6b0ca6 docs: format browser-support tables for GitHub markdown (#41327)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.6+24.sha-9cb1175
+ * @license Angular v11.2.6+25.sha-a68f6b0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21730,7 +21730,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.2.6+24.sha-9cb1175');
+    var VERSION$1 = new Version('11.2.6+25.sha-a68f6b0');
 
     /**
      * @license
@@ -31574,7 +31574,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.6+24.sha-9cb1175'));
+        definitionMap.set('version', literal('11.2.6+25.sha-a68f6b0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31799,7 +31799,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.6+24.sha-9cb1175'));
+        definitionMap.set('version', literal('11.2.6+25.sha-a68f6b0'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.6+24.sha-9cb1175'));
+        definitionMap.set('version', o.literal('11.2.6+25.sha-a68f6b0'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
