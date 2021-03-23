5fcd018c3f352f902b9e09cfe12efb488922e9c4
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:16 +0000

    80f11a9b86 ci: check that there are no unused contributor images (#41290)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -18,7 +18,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 }
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+27.sha-e340d19'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+29.sha-80f11a9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+27.sha-e340d19'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+29.sha-80f11a9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
