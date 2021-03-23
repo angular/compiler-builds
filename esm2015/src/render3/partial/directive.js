3af1adfe8887a753b8b8d06886ddd6664c7b418c
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:11:32 +0000

    80f11a9b86 ci: check that there are no unused contributor images (#41290)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.5+27.sha-e340d19'));
+    definitionMap.set('version', o.literal('12.0.0-next.5+29.sha-80f11a9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+27.sha-e340d19'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+29.sha-80f11a9'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
