522c35591ba2348a858452ba3135ab2875640aca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-24 21:04:14 +0000

    b61c009c54 build: emit performance JSON file for each ng_module() (#41125)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+1.sha-dd82c8e'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+3.sha-b61c009'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+1.sha-dd82c8e'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+3.sha-b61c009'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
