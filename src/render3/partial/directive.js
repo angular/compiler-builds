325acfa07fdbe0df53fcb9ebd094ccbfa843610d
Joey Perrott <josephperrott@gmail.com>
2021-03-24 16:44:54 +0000

    3177d6375e docs: update changelog for the v11.2.7 release

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.5+39.sha-81a0e71'));
+        definitionMap.set('version', o.literal('12.0.0-next.5+40.sha-3177d63'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
