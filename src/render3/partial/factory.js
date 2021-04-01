35430916cb936a8f264f93d468407bcad358447d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:38:58 +0000

    ed77bfea26 test(core): fix JIT factory declaration test for IE11 (#41416)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -23,7 +23,7 @@
     var util_1 = require("@angular/compiler/src/render3/view/util");
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+48.sha-c4af493'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+50.sha-ed77bfe'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', compileDependencies(meta.deps));
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+48.sha-c4af493'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+50.sha-ed77bfe'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
