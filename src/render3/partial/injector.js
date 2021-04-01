678b0ad0a24cf19b971e3becb038fecc04a89056
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:15:03 +0000

    c6d9a193d6 fix(dev-infra): Correctly place comments in dockerfile (#41374)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -30,7 +30,7 @@
     exports.compileDeclareInjectorFromMetadata = compileDeclareInjectorFromMetadata;
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+42.sha-ff9470b'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+43.sha-c6d9a19'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
