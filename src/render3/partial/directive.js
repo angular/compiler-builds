de10b29e396fd9998fd07769a9d4616125e4de5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:28:48 +0000

    c4af49335c docs(forms): fix the short description for `DefaultValueAccessor` (#41409)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+44.sha-a5a3752'));
+        definitionMap.set('version', o.literal('12.0.0-next.6+48.sha-c4af493'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
