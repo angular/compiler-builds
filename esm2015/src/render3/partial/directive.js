e4620ddb75ad622918309b92a09f8e16d2bf24ea
atscott <atscott01@gmail.com>
2021-04-02 23:07:22 +0000

    64b979e12b fix(dev-infra): exclude node_modules from circular dependency checks (#41407)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.7+5.sha-03a46af'));
+    definitionMap.set('version', o.literal('12.0.0-next.7+7.sha-64b979e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
