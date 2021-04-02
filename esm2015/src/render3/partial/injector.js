e3db3525ab3e0c9de5d4428dcba7b54f02ccb4c6
atscott <atscott01@gmail.com>
2021-04-02 17:44:55 +0000

    c5e9944c42 fix(dev-infra): allow npm like scopes as commit message scopes (#41430)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.6+50.sha-ed77bfe'));
+    definitionMap.set('version', o.literal('12.0.0-next.7+1.sha-c5e9944'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.6+50.sha-ed77bfe'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+1.sha-c5e9944'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
