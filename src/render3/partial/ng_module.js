3e74b46701fda33b1e29a317bc37fc610b0a2ba7
atscott <atscott01@gmail.com>
2021-04-05 20:02:54 +0000

    ee25034a77 build: update the recommended `Dockerfile` for VSCode remote development (#41396)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -31,7 +31,7 @@
     exports.compileDeclareNgModuleFromMetadata = compileDeclareNgModuleFromMetadata;
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+14.sha-2cef385'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+15.sha-ee25034'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
