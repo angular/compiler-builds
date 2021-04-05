79a708a50a333163e5b39652b28c4b74cff1b6ef
atscott <atscott01@gmail.com>
2021-04-05 15:37:27 +0000

    fcd190aa9c docs: fix grammar mistake in `glossary.md` ("can exported" --> "can be exported") (#41440)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -17,7 +17,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 }
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.7+9.sha-1de04b1'));
+    definitionMap.set('version', o.literal('12.0.0-next.7+10.sha-fcd190a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
