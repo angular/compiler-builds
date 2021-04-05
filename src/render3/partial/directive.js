72afc443c5aecb564afc0437d101590de5558425
atscott <atscott01@gmail.com>
2021-04-05 16:20:32 +0000

    2cef385e43 feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+11.sha-ab47f41'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+14.sha-2cef385'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -30,7 +30,7 @@
     exports.compileDeclareInjectorFromMetadata = compileDeclareInjectorFromMetadata;
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+11.sha-ab47f41'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+14.sha-2cef385'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
