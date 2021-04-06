67e21ed7c9db21a8dbd97101f7b19b8949501ebe
atscott <atscott01@gmail.com>
2021-04-06 17:27:55 +0000

    575f537473 docs(router): make getCurrentNavigation() jsdoc more explicit (#41417)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.7+21.sha-acebe92'));
+    definitionMap.set('version', o.literal('12.0.0-next.7+25.sha-575f537'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
