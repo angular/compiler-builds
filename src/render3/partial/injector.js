6d546559bb14b881177ef686789176b3e2e9993e
atscott <atscott01@gmail.com>
2021-04-07 17:09:24 +0000

    8f12f47492 fix(compiler-cli): Allow analysis to continue with invalid style url (#41403)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -30,7 +30,7 @@
     exports.compileDeclareInjectorFromMetadata = compileDeclareInjectorFromMetadata;
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+37.sha-f98f379'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+38.sha-8f12f47'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
