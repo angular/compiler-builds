fd2969b7a568fca303c82eef8e5a7c35e796e09f
atscott <atscott01@gmail.com>
2021-04-07 18:12:06 +0000

    ae6c195c87 docs: fix link URL in NG0100 error page (#41484)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+38.sha-8f12f47'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+39.sha-ae6c195'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
