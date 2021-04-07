b0bfd2b63b8961b99b9212c98807c4ee50f44fb1
atscott <atscott01@gmail.com>
2021-04-07 19:12:27 +0000

    7a49aa5136 build: add `dev-infra` and `docs-infra` as targetLabelExemptScopes (#41459)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+2.sha-18d4446'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+6.sha-7a49aa5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
