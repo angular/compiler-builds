40d720caa9d8a94a9e1dec3f2941fc0dfb7cb516
atscott <atscott01@gmail.com>
2021-04-07 17:02:05 +0000

    f98f379dd8 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+34.sha-dd66da1'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+37.sha-f98f379'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -37,7 +37,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+34.sha-dd66da1'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+37.sha-f98f379'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
