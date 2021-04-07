72eec09e84ee255a6025a514d4c3d7b8714cb9da
atscott <atscott01@gmail.com>
2021-04-07 18:10:49 +0000

    ae6c195c87 docs: fix link URL in NG0100 error page (#41484)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.7+38.sha-8f12f47'));
+    definitionMap.set('version', o.literal('12.0.0-next.7+39.sha-ae6c195'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.7+38.sha-8f12f47'));
+        definitionMap.set('version', o.literal('12.0.0-next.7+39.sha-ae6c195'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
