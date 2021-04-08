81494eb0f6e88e59af19d50796de70e873a6abbf
Zach Arend <zachzach@google.com>
2021-04-08 17:44:22 +0000

    44a7fae00f fix(router): handle new navigations from a NavigationEnd event (#41262)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -23,7 +23,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+16.sha-10a7c87'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+18.sha-44a7fae'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
