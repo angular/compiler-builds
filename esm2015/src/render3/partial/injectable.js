8c63ce61b3600ad0b6250aaa371b03448462505e
Zach Arend <zachzach@google.com>
2021-04-12 17:56:10 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -24,7 +24,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
  */
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+32.sha-aa0e54f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+35.sha-42e3a52'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
