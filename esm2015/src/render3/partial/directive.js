48016fb00d320672917891fc3580efbdc0547220
Zach Arend <zachzach@google.com>
2021-04-13 20:28:39 +0000

    78236bfdca fix(language-service): use script versions for incremental compilations (#41475)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+65.sha-71b8c9a'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+69.sha-78236bf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -20,7 +20,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
  */
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+65.sha-71b8c9a'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+69.sha-78236bf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
