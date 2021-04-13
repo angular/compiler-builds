faecda6c2aff54192b49675a26b3bad429238cad
Zach Arend <zachzach@google.com>
2021-04-13 04:28:27 +0000

    bfbdb8f84d refactor(compiler-cli): cleanup redundant storage of reuse `ts.Program` (#41289)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -12,7 +12,7 @@ import { DefinitionMap } from '../view/util';
 import { compileDependencies } from './util';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+49.sha-319da89'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+52.sha-bfbdb8f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
