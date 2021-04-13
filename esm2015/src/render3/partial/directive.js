fa059e835ffaef0f1041b95862afdd7b73f840a7
Zach Arend <zachzach@google.com>
2021-04-13 02:29:47 +0000

    1381301afe refactor(compiler-cli): track a dependency on a default import on `WrappedNodeExpr` (#41557)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+45.sha-528e7d7'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+48.sha-1381301'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -24,7 +24,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
  */
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+45.sha-528e7d7'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+48.sha-1381301'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
