685ed1c5cbe36d10146d55b795f81de98cc1cddf
Zach Arend <zachzach@google.com>
2021-04-14 23:21:06 +0000

    4329ccae2c docs: add rx-angular to resources.json (#39291)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+106.sha-dde81ba'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+108.sha-4329cca'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
