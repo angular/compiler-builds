396b2ff9dfdfd56ab7178e2920779565cb67c431
Zach Arend <zachzach@google.com>
2021-04-14 23:22:07 +0000

    b616f731b2 docs: add rx-angular to resources.json (#39291)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.10'));
+        definitionMap.set('version', o.literal('11.2.10+4.sha-b616f73'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
