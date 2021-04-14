39114161a9fe35d8c08fc4670c456ced4dceb79e
Zach Arend <zachzach@google.com>
2021-04-14 21:58:44 +0000

    74eed46477 release: cut the v10.2.5 release

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4+2.sha-f630f33
+ * @license Angular v10.2.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -20632,7 +20632,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('10.2.4+2.sha-f630f33');
+    var VERSION$1 = new Version('10.2.5');
 
     /**
      * @license
--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4+2.sha-f630f33
+ * @license Angular v10.2.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19460,7 +19460,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.4+2.sha-f630f33');
+const VERSION$1 = new Version('10.2.5');
 
 /**
  * @license
