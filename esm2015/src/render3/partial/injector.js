cc58f7530a96be3e8dea13a3ac7453b449c06ff2
Zach Arend <zachzach@google.com>
2021-04-14 16:46:15 +0000

    0bc539af29 Revert "fix(compiler-cli): autocomplete literal types in templates. (#41456)" (#41623)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -20,7 +20,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
  */
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('12.0.0-next.8+77.sha-de93a7a'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+78.sha-0bc539a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -37,7 +37,7 @@
      */
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('12.0.0-next.8+77.sha-de93a7a'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+78.sha-0bc539a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
