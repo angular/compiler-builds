c12cecbc83dbad208c4248d87cba1146e1ac925f
Zach Arend <zachzach@google.com>
2021-04-14 23:22:26 +0000

    4329ccae2c docs: add rx-angular to resources.json (#39291)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+106.sha-dde81ba'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+108.sha-4329cca'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
