5987bb8eaa01652407a3a74b64002cb5a4bbe5e3
Zach Arend <zachzach@google.com>
2021-04-14 23:19:35 +0000

    dde81ba0cd perf(compiler): reduce amount of generated code for safe accesses and nullish coalescing (#41563)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+107.sha-b649273'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+106.sha-dde81ba'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
