a0ee93091d93b78f5aa9e4430aaf4be0b1e96e89
Zach Arend <zachzach@google.com>
2021-04-14 23:12:09 +0000

    1043aa42ad build: update typescript to version 4.2.4 (#41618)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+101.sha-2af894d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+102.sha-1043aa4'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
