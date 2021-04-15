af69179ca4ea8e0553eedd499686de470aae97d6
Andrew Kushnir <akushnir@google.com>
2021-04-15 22:01:47 +0000

    6c43a949b8 docs: update copy in schematics.md (#41521)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.2.10+7.sha-3aa9235'));
+    definitionMap.set('version', o.literal('11.2.10+8.sha-6c43a94'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.10+7.sha-3aa9235'));
+        definitionMap.set('version', o.literal('11.2.10+8.sha-6c43a94'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
