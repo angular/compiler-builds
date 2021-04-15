3a7a9a2c3d65401e1669366bc61d2c2fc1fad66f
Andrew Kushnir <akushnir@google.com>
2021-04-15 21:56:02 +0000

    9d8f345c0c docs: update copy in schematics.md (#41521)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+117.sha-8d0511d'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+120.sha-9d8f345'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
