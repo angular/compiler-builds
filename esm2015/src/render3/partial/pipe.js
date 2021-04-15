26402516786f021ed8b21741218edf6c24b6a64f
Andrew Kushnir <akushnir@google.com>
2021-04-15 23:20:00 +0000

    391d08f6fb docs: edits to remove jargon and abbreviations (#41530)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+120.sha-9d8f345'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+121.sha-391d08f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
