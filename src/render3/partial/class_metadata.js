ac7dd1693e14fa58909057f3d04d1aa9dd2a7706
Zach Arend <zachzach@google.com>
2021-04-15 02:16:19 +0000

    0d55d4557f test(router): add Router regression test for hash location strategy (#40409)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+110.sha-5332a4a'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+109.sha-0d55d45'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
