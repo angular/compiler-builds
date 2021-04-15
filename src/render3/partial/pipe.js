c410cb632bdd84265aa6f260c1598a419c72b7d2
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:19:40 +0000

    a4ff071e3f fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.9+11.sha-9438d48'));
+        definitionMap.set('version', o.literal('12.0.0-next.9+14.sha-a4ff071'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
