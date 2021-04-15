b2b7c697769cb65e4558974112038d6f24899454
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:10:43 +0000

    8d0511d009 ci: disable dependency updates of Angular JS packages (#41642)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+115.sha-a99aa29'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+117.sha-8d0511d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
