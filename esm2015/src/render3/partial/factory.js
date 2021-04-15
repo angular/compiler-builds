675099cc8d1341e1a6ccd1e1c3fd3ab01a672aff
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:20:16 +0000

    6bceb709df fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+111.sha-dd4c3db'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+114.sha-6bceb70'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
