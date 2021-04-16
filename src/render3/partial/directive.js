7d0d1a22595bed47c435097b59c1b3a12099ec86
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:14:10 +0000

    5d73b33ba2 build: update angular packages (#41633)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+121.sha-391d08f'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+123.sha-5d73b33'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
