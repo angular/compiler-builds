930cc8b43fe1c96ed00338ccffb99c74e8b0cb11
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:19:46 +0000

    b23850eb1c ci: disable dependency updates of `angular-mocks` packages (#41650)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+125.sha-2bd4363'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+124.sha-b23850e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
