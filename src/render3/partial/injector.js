decd35dfe87fcdbea2b9c266c280569e9186be80
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:16:54 +0000

    fe5bf7f53f fix(compiler-cli): autocomplete literal types in templates. (#41456) (#41645)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+123.sha-5d73b33'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+126.sha-fe5bf7f'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
