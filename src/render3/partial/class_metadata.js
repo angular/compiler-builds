ef34582a2a566d7f4f4fc7381302e37a6848c7eb
Andrew Kushnir <akushnir@google.com>
2021-04-16 20:53:36 +0000

    e251ece18d feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.9+32.sha-ab257b3'));
+        definitionMap.set('version', o.literal('12.0.0-next.9+33.sha-e251ece'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
