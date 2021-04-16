7b97af8f6b0d716d4bb8a14d92e726155d1baa65
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:17:21 +0000

    2bd4363f2a build: update bazel packages (#41654)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+126.sha-fe5bf7f'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+125.sha-2bd4363'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
