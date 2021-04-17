79995d46d314f1c48bfc528984d203499ff8424a
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:46:37 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+136.sha-96175fd'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+138.sha-de25cda'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
