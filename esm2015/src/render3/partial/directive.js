713ce3a739c9b7a3984b3772c9f770899f88e805
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:14:35 +0000

    d0b1f99598 docs: fix broken links to CLI wiki in the "Component styles" guide (#41516)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+138.sha-de25cda'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+140.sha-d0b1f99'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
