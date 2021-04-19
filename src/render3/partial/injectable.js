3c21fc695c2716605ef80a6d737479316df2187c
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:34:52 +0000

    ef4cf878b1 build: update google-closure-compiler to version 20210406.0.0 (#41687)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+169.sha-5e34317'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+170.sha-ef4cf87'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
