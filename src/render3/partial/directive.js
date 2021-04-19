41d835831863eb2f3c26ef4f757c266fe922e2f0
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:34:52 +0000

    5e343173e8 docs: fix styling issues in example CSS (#41222)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+167.sha-d58747d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+169.sha-5e34317'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+167.sha-d58747d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+169.sha-5e34317'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
