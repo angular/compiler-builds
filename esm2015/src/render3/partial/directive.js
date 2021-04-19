3823df67076132af9e210534f1abfa5eec898274
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:36:14 +0000

    ef4cf878b1 build: update google-closure-compiler to version 20210406.0.0 (#41687)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+169.sha-5e34317'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+170.sha-ef4cf87'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
