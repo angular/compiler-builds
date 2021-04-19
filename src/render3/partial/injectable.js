a5a6693a08ef1d2c4c5f845d4fc8caa80594b8c0
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:15:05 +0000

    251bec159a docs: documentation of descendants property of @ContentChildren (#35927)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+177.sha-853016d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+178.sha-251bec1'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
