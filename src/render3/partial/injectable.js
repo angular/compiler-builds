75721848f181279735ad8bbf89114443a62ddc2a
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:16:06 +0000

    cf6c5c1d2c build: update dependency-graph to version 0.11.0 (#41665)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+140.sha-d0b1f99'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+142.sha-cf6c5c1'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
