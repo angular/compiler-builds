f9ad6e3adedb26c49843a396e9c1b4976f36311d
Andrew Kushnir <akushnir@google.com>
2021-04-19 22:43:03 +0000

    b333a1fe5c build: update firebase-tools to version 9.10.0 (#41686)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.9+75.sha-2114a13'));
+    definitionMap.set('version', o.literal('12.0.0-next.9+77.sha-b333a1f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
