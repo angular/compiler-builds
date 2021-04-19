db1aac6a88e6f8b71fb7ad6bb6ac4047bfbad4e7
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:05:54 +0000

    d58747de8a Revert "fix(zone.js): should continue to executue listeners when throw error (#41562)" (#41707)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+165.sha-e256d92'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+167.sha-d58747d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
