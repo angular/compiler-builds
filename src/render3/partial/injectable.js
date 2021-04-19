4ea3be46a8e0e3d4833e02f37c0ec86cc6de5efe
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:20:11 +0000

    75168a2a70 build: update graceful-fs to version 4.2.6 (#41668)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+145.sha-3738dbb'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+144.sha-75168a2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
