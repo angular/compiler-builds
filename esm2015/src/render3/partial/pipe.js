ef776d9adf7ed653d6cff7bd6af4a64302b9a71a
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:21:25 +0000

    13913231f1 test(docs-infra): change URL separator from `\t` to `-->` in `URLS_TO_REDIRECT.txt` (#41680)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+144.sha-75168a2'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+147.sha-1391323'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
