16de8298f0416ae245f2a27c4adad87f4352116a
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:39:42 +0000

    6cdb8fb0aa build: remove entities dependency from root package.json (#41705)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+171.sha-c617f1f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+172.sha-6cdb8fb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
