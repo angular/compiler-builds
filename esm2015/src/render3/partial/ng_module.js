52400278d9a92eab591a7ad2422f7ba4292db1b3
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:20:03 +0000

    3738dbba05 ci: disable automatic updates of rollup (#41672)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+142.sha-cf6c5c1'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+145.sha-3738dbb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
