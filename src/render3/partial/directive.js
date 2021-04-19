9840cf86b93ab2a3ec524c52d71bde02adce813e
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:41:34 +0000

    70341bcfed build(docs-infra): update docs examples to Angular v11.2.10 (#41689)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+172.sha-6cdb8fb'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+175.sha-70341bc'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+172.sha-6cdb8fb'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+175.sha-70341bc'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
