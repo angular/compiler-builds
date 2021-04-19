182d3c9e22002aa3efba83670ff85ea75f537850
Andrew Kushnir <akushnir@google.com>
2021-04-19 17:03:04 +0000

    8c831a257c test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -24,7 +24,7 @@ export function compileDeclarePipeFromMetadata(meta) {
  */
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.2.10+16.sha-095bb52'));
+    definitionMap.set('version', o.literal('11.2.10+29.sha-8c831a2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.10+16.sha-095bb52'));
+        definitionMap.set('version', o.literal('11.2.10+29.sha-8c831a2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
