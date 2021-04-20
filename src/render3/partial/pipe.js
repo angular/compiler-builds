018f7f3c9c634ce6b74011ba62d315c1e8d62e5b
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:20:52 +0000

    c0eabb2e70 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+187.sha-a2b671e'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+188.sha-c0eabb2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
