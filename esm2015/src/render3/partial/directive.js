f7899bdef723f64a3e0abc401af0f237a8597dfe
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:02:07 +0000

    c2011c0ba1 build: update semver to version 7.3.5 (#41715)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+182.sha-e238f2c'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+184.sha-c2011c0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
