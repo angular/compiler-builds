b4d282d2170c19175b2a4be9ec3dfcf13070e84e
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:06:16 +0000

    73824d5337 fix(compiler): not generating update instructions for ng-template inside alternate namespaces (#41669)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+185.sha-364ff96'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+186.sha-73824d5'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
