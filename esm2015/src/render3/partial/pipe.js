7a905d59fd90bbe32e016fcf393c564bb37129b4
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 15:58:37 +0000

    dfdd4a2d74 docs: ident the library shematics guide properly (#41755)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+198.sha-122c8ba'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+199.sha-dfdd4a2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
