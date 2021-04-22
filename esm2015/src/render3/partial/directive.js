8934bc0db22b42552d1daa32133c63a884f65331
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:50:46 +0000

    2acb8d1865 fix(dev-infra): verify python3 is available before release (#41753)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-rc.0+2.sha-0830885'));
+    definitionMap.set('version', o.literal('12.0.0-rc.0+6.sha-2acb8d1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
