3d5ae77bf586715ef6926c397f13119875c890c0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 16:03:14 +0000

    6ca6302cf1 build(docs-infra): upgrade cli command docs sources to 064c3356e (#41765)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+199.sha-dfdd4a2'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+200.sha-6ca6302'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
