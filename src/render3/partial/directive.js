72a184799706a34c1757e459a4b9fec38ee00bc0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 15:59:24 +0000

    122c8ba0e3 build(docs-infra): fix `StackblitzBuilder` after `jsdom` update (#41725)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+196.sha-a4a55f0'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+198.sha-122c8ba'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
