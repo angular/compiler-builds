bc4b30e81e39cd31faff209417d9b355429ef743
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:01:18 +0000

    c3a512a9e9 fix(compiler-cli): match string indexed partial declarations (#41747)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+200.sha-6ca6302'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+202.sha-c3a512a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
