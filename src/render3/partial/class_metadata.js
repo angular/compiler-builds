288bca9bd5b7bfd428c2db1b8e7af051145516f9
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:10:09 +0000

    de4fafb818 refactor(core): add single type for injector token (#41580)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+204.sha-64567d3'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+206.sha-de4fafb'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
