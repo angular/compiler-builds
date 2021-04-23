9af4b2cc211148fbe96f04ef8cae3582631d076a
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:35:00 +0000

    097fdffc91 build: use platform from shared dev-infra package for remote execution (#41767)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-rc.0+18.sha-87dc851'));
+        definitionMap.set('version', o.literal('12.0.0-rc.0+22.sha-097fdff'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
