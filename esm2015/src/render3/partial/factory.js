cc72a72d60ecd491d1e1a645ef7061b5512fd252
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:12:53 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+213.sha-9f88da2'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+214.sha-73105aa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
