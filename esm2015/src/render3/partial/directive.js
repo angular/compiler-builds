bd8b149d300dd8f2d086d818f44072526d6c4afd
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:11:32 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+213.sha-9f88da2'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+214.sha-73105aa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
