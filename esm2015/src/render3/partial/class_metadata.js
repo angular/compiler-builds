e6046e339feaff1390bd1f9a21d022fcf43c0d71
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:40:34 +0000

    af12d8deaa fix(core): update supported range of node versions to only include LTS versions (#41822)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+233.sha-e47ea29'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+251.sha-af12d8d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
