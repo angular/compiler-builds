72dfa21775cb632cf9855e7a9700d203672b36ee
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:43:07 +0000

    27ad7624d0 fix(docs-infra): fix external link icons positioning (#41794)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+217.sha-01e546f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+218.sha-27ad762'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
