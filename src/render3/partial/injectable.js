dac3722adda5be67ec916aea581b02163870a65e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:37:58 +0000

    01e546f116 docs: improve accessibility of architecture example (#41314)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+216.sha-3268847'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+217.sha-01e546f'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
