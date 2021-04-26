5b6008d04322b46900d991ae031afc1a3b0726e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:36:34 +0000

    01e546f116 docs: improve accessibility of architecture example (#41314)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+216.sha-3268847'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+217.sha-01e546f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
