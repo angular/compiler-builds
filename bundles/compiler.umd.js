da86c19c772e136433e554486bf2f1dbc34b3258
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 17:03:50 +0000

    21f1466d84 build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- bundles/compiler.umd.js
+++ bundles/compiler.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+10.sha-163c076
+ * @license Angular v11.2.11+15.sha-21f1466
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -21776,7 +21776,7 @@
      * Use of this source code is governed by an MIT-style license that can be
      * found in the LICENSE file at https://angular.io/license
      */
-    var VERSION$1 = new Version('11.2.11+10.sha-163c076');
+    var VERSION$1 = new Version('11.2.11+15.sha-21f1466');
 
     /**
      * @license
@@ -31634,7 +31634,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.11+10.sha-163c076'));
+        definitionMap.set('version', literal('11.2.11+15.sha-21f1466'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
@@ -31859,7 +31859,7 @@
      */
     function createPipeDefinitionMap(meta) {
         var definitionMap = new DefinitionMap();
-        definitionMap.set('version', literal('11.2.11+10.sha-163c076'));
+        definitionMap.set('version', literal('11.2.11+15.sha-21f1466'));
         definitionMap.set('ngImport', importExpr(Identifiers$1.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -25,7 +25,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
  */
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
-    definitionMap.set('version', o.literal('11.2.11+10.sha-163c076'));
+    definitionMap.set('version', o.literal('11.2.11+15.sha-21f1466'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
