35354310dfde58cb0de3c5b79ca67a855d552c93
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:00:46 +0000

    295889ed2e ci: prevent CI cache from growing indefinitely (#41814)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+226.sha-a938849'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+228.sha-295889e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
