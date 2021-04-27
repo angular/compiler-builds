a8558d3ff469528b5ddd905e4f6ee6beb3be7a47
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 19:43:07 +0000

    0140d04ced feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+257.sha-cd004d4'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+258.sha-0140d04'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
