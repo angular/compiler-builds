a56b699b54f37e989970b2de50f8f34bdb6026a6
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 21:22:48 +0000

    3b7d4ebbd6 fix(docs-infra): do not redirect disambiguated URLs (#41842)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+259.sha-d580b27'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+265.sha-3b7d4eb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
