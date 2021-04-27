1201b8e41077968de7f795217f214e5b3ff95e4c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:44:33 +0000

    d580b27195 ci: several updates to renovate configuration (#41834)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+258.sha-0140d04'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+259.sha-d580b27'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
