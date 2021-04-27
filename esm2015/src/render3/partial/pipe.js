4504f75f8a9926d3ff6eccfb6933efd400152d7e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:41:35 +0000

    b1ae31c712 fix(dev-infra): add github token requirement for discover-new-conflicts (#41825)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+255.sha-3783976'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+256.sha-b1ae31c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
