4681b8e524c3e6bada2c43e186016a67679fff2b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:45:55 +0000

    d580b27195 ci: several updates to renovate configuration (#41834)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+258.sha-0140d04'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+259.sha-d580b27'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
