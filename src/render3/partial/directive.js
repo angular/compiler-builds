8a21c1a127e07fb92c254c8002b0c8278e2bbf1f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:41:53 +0000

    1aed2f3234 fix(dev-infra): replace `chalk` require with namespace import (#41847)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+265.sha-3b7d4eb'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+268.sha-1aed2f3'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
