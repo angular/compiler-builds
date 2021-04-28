528384035ac9b95f405f6143abd3542249a9a28d
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 18:45:46 +0000

    f550742020 release: cut the v11.2.12 release

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.11+38.sha-336fe91'));
+        definitionMap.set('version', o.literal('11.2.12'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
