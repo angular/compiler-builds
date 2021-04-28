5c3f34f7eef9ed53d3d8983202ded26d8cbf7ac9
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:40:29 +0000

    1aed2f3234 fix(dev-infra): replace `chalk` require with namespace import (#41847)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+265.sha-3b7d4eb'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+268.sha-1aed2f3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
