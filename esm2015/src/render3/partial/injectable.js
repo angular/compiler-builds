7ddc4001fd8d15b7224b9c0201c77a3b033ff774
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:53:19 +0000

    c31d6ea24e docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+279.sha-3ccad85'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+281.sha-c31d6ea'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
