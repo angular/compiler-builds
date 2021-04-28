ab80a6700532765d0f3a404dfdf8b91311f9be84
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:44:49 +0000

    3ccad85b01 docs: add error page for NG6999 (#41708)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+278.sha-42a64f8'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+279.sha-3ccad85'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+278.sha-42a64f8'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+279.sha-3ccad85'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
