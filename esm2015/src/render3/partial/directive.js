a20dc09491fc9d03be6cda02ffc514232d8d59a5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:53:13 +0000

    45792f145c docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-rc.1+6.sha-c691b66'));
+    definitionMap.set('version', o.literal('12.0.0-rc.1+9.sha-45792f1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-rc.1+6.sha-c691b66'));
+        definitionMap.set('version', o.literal('12.0.0-rc.1+9.sha-45792f1'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
