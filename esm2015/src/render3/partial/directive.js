9a337d78dd04e0d6028e30cafc256983bb93e0f4
Misko Hevery <misko@hevery.com>
2021-04-29 17:25:40 +0000

    595a75cda4 build: update dependency karma to v6 (#41696)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+282.sha-55d9713'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+283.sha-595a75c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
