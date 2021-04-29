c52dfb7c6b98c42ecd2c9006a48e2deedba00d35
Misko Hevery <misko@hevery.com>
2021-04-29 21:07:42 +0000

    e53c80c7ca ci: changes to `packages/common/http` should not require `fw-common` approval (#41887)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+289.sha-ca0fcd6'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+290.sha-e53c80c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
