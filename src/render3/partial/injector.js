129c07f3e267bf6f64710c45ef3ad10c10609435
Misko Hevery <misko@hevery.com>
2021-04-29 17:27:06 +0000

    595a75cda4 build: update dependency karma to v6 (#41696)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+282.sha-55d9713'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+283.sha-595a75c'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
