a8294145687dd64493000b67d41b9f5dd4d1bae1
Misko Hevery <misko@hevery.com>
2021-04-30 21:33:29 +0000

    2dd96e08ae Revert "perf(core): avoid storing LView in __ngContext__ (#41358)" (#41901)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+294.sha-0dfd940'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+295.sha-2dd96e0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
