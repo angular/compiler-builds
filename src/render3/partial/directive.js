eb1214fbca178d04218cabf98cba5a42d65075d2
Misko Hevery <misko@hevery.com>
2021-04-30 21:54:42 +0000

    7e9ef4d257 docs: Update link to Testing Angular online book (#41906)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -38,7 +38,7 @@
      */
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
-        definitionMap.set('version', o.literal('11.2.12+9.sha-5bae16d'));
+        definitionMap.set('version', o.literal('11.2.12+12.sha-7e9ef4d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
