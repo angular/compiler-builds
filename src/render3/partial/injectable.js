0206707e793c7a44f2b02ea578a1701f3689fdfe
Misko Hevery <misko@hevery.com>
2021-04-30 21:47:01 +0000

    9b4b281c52 fix(core): handle multiple i18n attributes with expression bindings (#41882)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+298.sha-35450c7'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+301.sha-9b4b281'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
