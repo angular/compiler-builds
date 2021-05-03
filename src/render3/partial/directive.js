6900d4d1ed2ba44b773c056a1ecfb3e97ba34283
Misko Hevery <misko@hevery.com>
2021-05-03 21:50:54 +0000

    741c295129 docs: update file structure document (#41923)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+315.sha-f42796a'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+314.sha-741c295'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
