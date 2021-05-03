80ba268f94c71614b7f6791178d31ac6d1fe1d2c
Misko Hevery <misko@hevery.com>
2021-05-03 21:48:51 +0000

    3b589030a8 docs: Remove references to protractor from documentation (#41731)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+312.sha-253ed5d'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+313.sha-3b58903'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+312.sha-253ed5d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+313.sha-3b58903'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
