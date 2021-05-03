381522e737ceb1defd788b65d3b9f134a3e9dca5
Misko Hevery <misko@hevery.com>
2021-05-03 21:51:41 +0000

    b5b09dcca4 Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-rc.1+35.sha-d59330b'));
+    definitionMap.set('version', o.literal('12.0.0-rc.1+44.sha-b5b09dc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-rc.1+35.sha-d59330b'));
+        definitionMap.set('version', o.literal('12.0.0-rc.1+44.sha-b5b09dc'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
