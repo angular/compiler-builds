413d602aa78874569c9fbb6243f5c52cf91568fa
Misko Hevery <misko@hevery.com>
2021-05-03 21:47:34 +0000

    3b589030a8 docs: Remove references to protractor from documentation (#41731)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+312.sha-253ed5d'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+313.sha-3b58903'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
