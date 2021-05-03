d287735101f77c714f74bda6892da5940b99f88c
Misko Hevery <misko@hevery.com>
2021-05-03 21:49:24 +0000

    f42796a8f5 build: lock file maintenance (#41924)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+316.sha-3777cf5'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+315.sha-f42796a'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
