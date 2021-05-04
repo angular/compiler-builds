0cdaa0fb976750a35c3793ef5abd963f46c2b3c8
Misko Hevery <misko@hevery.com>
2021-05-04 15:54:59 +0000

    dac78d1ba1 build: update dependency yargs to v17 (#41920)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+320.sha-4dae97d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+321.sha-dac78d1'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
