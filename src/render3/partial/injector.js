09daefbc35f7f06d700e7d81d662ebdc689fa241
Misko Hevery <misko@hevery.com>
2021-05-04 15:35:12 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+314.sha-741c295'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+318.sha-b7db98f'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
