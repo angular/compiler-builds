dc4048a9a6a12551e37eb06447ad3cd9db8673c3
Misko Hevery <misko@hevery.com>
2021-05-04 15:36:36 +0000

    b7db98fd92 build: remove emojis from github issue templates (#41902)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+314.sha-741c295'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+318.sha-b7db98f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
