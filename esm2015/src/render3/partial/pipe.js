782c4e54f40e6a8f2c10b52e4c5b4d87ab4e4895
Misko Hevery <misko@hevery.com>
2021-05-05 04:40:23 +0000

    6b8baad940 fix(common): add body as an optional property on the options of HttpClient.delete (#19438) (#41723)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+331.sha-19b12d7'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+332.sha-6b8baad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
