93f0ca1e8b523b5d0ffdc0772fb63e0690de4139
Misko Hevery <misko@hevery.com>
2021-05-05 04:37:38 +0000

    19b12d7286 refactor(dev-infra): switch away from deprecated platform execution properties (#41941)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+329.sha-5947239'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+331.sha-19b12d7'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
