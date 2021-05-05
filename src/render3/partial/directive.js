b4890da2650b0ec80f06a255f4af919cb2c4069a
Misko Hevery <misko@hevery.com>
2021-05-05 04:34:12 +0000

    5947239f89 fix(dev-infra): do not set lts dist tag on packages from release-candidate train (#41946)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+330.sha-378bb04'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+329.sha-5947239'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
