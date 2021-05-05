1a11dc0eef0624f1f8232409ac1ac03852806d92
Misko Hevery <misko@hevery.com>
2021-05-05 21:14:29 +0000

    c472c5b872 docs: initial commit of content projection topic. (#41143)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+336.sha-530e45f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+339.sha-c472c5b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+336.sha-530e45f'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+339.sha-c472c5b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
