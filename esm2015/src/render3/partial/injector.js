21c7eda8cc96369d59baf370629e09f737cfe723
Misko Hevery <misko@hevery.com>
2021-05-05 23:19:49 +0000

    d7454a1685 docs: release notes for the v12.0.0-rc.2 release

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+339.sha-c472c5b'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+342.sha-d7454a1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
