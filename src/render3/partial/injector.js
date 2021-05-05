3f3413cf1f0879e43996519ee8024099b0f9e9e3
Misko Hevery <misko@hevery.com>
2021-05-05 23:17:40 +0000

    768d4ac94d release: cut the v12.0.0-rc.2 release

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-rc.1+64.sha-df2ff04'));
+        definitionMap.set('version', o.literal('12.0.0-rc.2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
