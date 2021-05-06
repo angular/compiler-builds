6ae85751714cd8ef84b46b37407a35c3facacac4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 13:52:39 +0000

    abcd4bbfaa fix(compiler): preserve @page rules in encapsulated styles (#41915)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+343.sha-efe8566'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+344.sha-abcd4bb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
