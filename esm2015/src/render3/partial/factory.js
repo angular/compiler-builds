4ca90fb7f290330e08a50a389407b1e94774eb2b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:01:41 +0000

    5c9bd237e3 build: update release configuration to use new release notes system (#41905)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+345.sha-35fbd4f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+350.sha-5c9bd23'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
