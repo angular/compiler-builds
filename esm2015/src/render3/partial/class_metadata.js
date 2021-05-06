b64ac2c6a67b1f7f5af8fc05b5092fcb6df7bf54
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:00:19 +0000

    5c9bd237e3 build: update release configuration to use new release notes system (#41905)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+345.sha-35fbd4f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+350.sha-5c9bd23'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
