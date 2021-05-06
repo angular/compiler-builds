e51ac49e590f60c23fb3c21c9a984ee3a090eebd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:09:37 +0000

    4d7294d9f7 feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+354.sha-640ec78'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+355.sha-4d7294d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
