560f31a7961476b8c2da8aaad56fc34c51c32505
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 20:06:26 +0000

    cd252b99fe fix(compiler-cli): use '' for the source map URL of indirect templates (#41973)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+372.sha-2843f15'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+373.sha-cd252b9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
