9c28ee33e2e7c97e1c9b658649564ed768b77596
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 18:28:33 +0000

    2843f15e8c fix(dev-infra): merge tool should ensure that token has `workflow` oauth scope (#41989)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+371.sha-3a48c07'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+372.sha-2843f15'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
