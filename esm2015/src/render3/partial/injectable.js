50ed67e6a0f076eeda23cf4c9600e929b3645f08
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 14:35:24 +0000

    cb418bd696 ci: introduce action implementing feature request prioritization process (#41975)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+365.sha-3927e25'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+367.sha-cb418bd'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
