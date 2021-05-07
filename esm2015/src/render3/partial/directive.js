4c37dd263802d827d92503a1900df5adf9a350cb
Andrew Scott <atscott@google.com>
2021-05-07 01:29:10 +0000

    e29eb8abfb docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+355.sha-4d7294d'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+364.sha-e29eb8a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
