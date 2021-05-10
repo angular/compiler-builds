1328e742f18d28523780da788a3a2e08a87924c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:55:56 +0000

    a529d4f4f8 fix(core): do not retain dynamically compiled components and modules (#42003)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+376.sha-23f6b76'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+386.sha-a529d4f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
