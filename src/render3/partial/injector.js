0d617fb0810365c421d55f9c9781fafefdbba42c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:57:10 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+387.sha-ddf2a6c'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+385.sha-089daea'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
