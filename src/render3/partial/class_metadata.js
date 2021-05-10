94d99b52672ab533db39246d49ea59383789dae6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:49:09 +0000

    d59f2b0d0c refactor(core): remove unnecessary cast (#41960)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+373.sha-cd252b9'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+380.sha-d59f2b0'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
