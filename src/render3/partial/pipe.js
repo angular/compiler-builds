59153c98b7b18ae07fc7025a1a8d64e5075ae37e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 22:17:19 +0000

    eac42e9889 docs: release notes for the v12.0.0-rc.3 release (#42032)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+394.sha-96624b7'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+395.sha-eac42e9'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
