90fe0891ddf2236f6478dcf05fff18755c52f98e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 16:22:27 +0000

    d2296bc265 test: refactor test to work with latest @types/jasmine (#41956)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+385.sha-089daea'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+389.sha-d2296bc'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
