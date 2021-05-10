8064220b9d9f6f723fa1c15797931c452802e03c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:47:54 +0000

    23f6b76d1a fix(docs-infra): update glossary to include the term template-input-variable (#41632)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+380.sha-d59f2b0'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+376.sha-23f6b76'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+380.sha-d59f2b0'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+376.sha-23f6b76'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
