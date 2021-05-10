1e5ea3289139b595e4beb1eb38f8bb3eab541ec0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:58:09 +0000

    ddf2a6c975 build: update node version to stop aio from breaking while generating docs (#42009)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+386.sha-a529d4f'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+387.sha-ddf2a6c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
