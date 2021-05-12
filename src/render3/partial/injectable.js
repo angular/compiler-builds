595ac1ebafe87fa7b375e910c948399e91c9b258
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 23:56:59 +0000

    d34ce6b76a build(docs-infra): update docs examples to Angular v12.0.0 (#42071)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+398.sha-65ee1c3'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+399.sha-d34ce6b'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
