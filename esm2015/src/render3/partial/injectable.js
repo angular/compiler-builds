a92a41e56d59d635f79f867ece462d4a0e8d681c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:29:30 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+396.sha-a74eb52'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+397.sha-dad205e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
