2fdd92b8de751aa4f1241a16676d645e10fef55d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:22:46 +0000

    a7ff02f014 build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#42040)

--- fesm2015/compiler.js
+++ fesm2015/compiler.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.5+1.sha-318e7b8
+ * @license Angular v10.2.5+2.sha-a7ff02f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19460,7 +19460,7 @@ function publishFacade(global) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION$1 = new Version('10.2.5+1.sha-318e7b8');
+const VERSION$1 = new Version('10.2.5+2.sha-a7ff02f');
 
 /**
  * @license
