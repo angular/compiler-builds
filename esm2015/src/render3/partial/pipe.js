d6588fd99225fbd47f19bff917134bf2ce1e5220
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 20:35:09 +0000

    fdb3d3076e build: update angular (#42035)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+403.sha-45083bc'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+409.sha-fdb3d30'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
