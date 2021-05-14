2cd7cc6658131c34391ae06c55f131035335c596
Zach Arend <zachzach@google.com>
2021-05-14 21:27:36 +0000

    6d87895ffd test(dev-infra): add testing for integration of ReleaseNotes in publishing tooling (#41967)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+414.sha-e89e3cb'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+415.sha-6d87895'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
