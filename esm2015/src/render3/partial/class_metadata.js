6a0a1cdbe5d1d6da26de2764f0084a8050827f24
Zach Arend <zachzach@google.com>
2021-05-14 21:26:08 +0000

    e89e3cb375 docs: uppercase API word in Ivy Compatibility file (#42089)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+413.sha-7fb8527'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+414.sha-e89e3cb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
