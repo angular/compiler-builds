9ff8ae1e1c7b4a2317a1966a7097c4a9e8cf6f22
Zach Arend <zachzach@google.com>
2021-05-14 21:24:48 +0000

    e89e3cb375 docs: uppercase API word in Ivy Compatibility file (#42089)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+413.sha-7fb8527'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+414.sha-e89e3cb'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
