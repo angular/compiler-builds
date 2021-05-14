93e46622042a9c14772940df31441dc31b11cd8b
Zach Arend <zachzach@google.com>
2021-05-14 15:13:12 +0000

    4bc5b4d93b fix(core): global listeners not being bound on non-node host elements (#42014)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+410.sha-2e7eb27'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+411.sha-4bc5b4d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
