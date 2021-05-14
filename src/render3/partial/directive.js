d337864ab6d0b611286d0a5990abaa0aea95e9d1
Zach Arend <zachzach@google.com>
2021-05-14 21:26:13 +0000

    6d87895ffd test(dev-infra): add testing for integration of ReleaseNotes in publishing tooling (#41967)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+414.sha-e89e3cb'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+415.sha-6d87895'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
