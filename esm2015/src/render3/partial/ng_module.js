7c2bd0754e345fe352ad7b1004689251e15c1f20
Zach Arend <zachzach@google.com>
2021-05-14 15:13:13 +0000

    3a46ad96ea fix(core): global listeners not being bound on non-node host elements (#42014)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0+11.sha-28ee986'));
+    definitionMap.set('version', o.literal('12.0.0+12.sha-3a46ad9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
