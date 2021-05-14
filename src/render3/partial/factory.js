ff77020dfd16d99d89871b1d948dde2ef7f0718a
Zach Arend <zachzach@google.com>
2021-05-14 19:32:47 +0000

    3aec55b626 ci: update to latest feature request triage bot commit (#42084)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+411.sha-4bc5b4d'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+412.sha-3aec55b'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
