d42e27279b47b2fe087b064538ff9a650b40e9e7
atscott <atscott01@gmail.com>
2021-05-17 18:15:34 +0000

    c32bea6ccf docs: add link to the blog post in the changelog (#42109) PR Close #42109

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+418.sha-fc1bc0e'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+420.sha-c32bea6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
