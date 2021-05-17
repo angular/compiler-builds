51663d4739b2f409d1c4e4c298233db72bb9ca3a
atscott <atscott01@gmail.com>
2021-05-17 18:09:29 +0000

    e86a1d3441 docs: make all examples compatible with strict mode (#41999)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+416.sha-5960d4c'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+417.sha-e86a1d3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
