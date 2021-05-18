4e38fa8226ecf063a5f47945d153a34bb0dc0ea6
atscott <atscott01@gmail.com>
2021-05-18 16:00:18 +0000

    fd4dbcb0c7 docs: decorate input properties with @Input (#42128) PR Close #42128

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+425.sha-2dfa010'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+423.sha-fd4dbcb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
