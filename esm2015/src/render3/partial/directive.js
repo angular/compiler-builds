f24137c027f78b0f073aa7126d9a43c9bea349dd
atscott <atscott01@gmail.com>
2021-05-18 15:56:13 +0000

    5885cf5ad8 build(docs-infra): fixed broken stackblitz examples (#42122)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.0-next.8+420.sha-c32bea6'));
+    definitionMap.set('version', o.literal('12.0.0-next.8+422.sha-5885cf5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
