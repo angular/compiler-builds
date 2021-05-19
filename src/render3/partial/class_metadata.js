1da74bf1f94037e2f80b3f9575f8005e6502d2cd
atscott <atscott01@gmail.com>
2021-05-19 16:45:18 +0000

    8d20bb9855 ci: update to latest feature request triage bot commit (#42157)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+439.sha-d0958e8'));
+        definitionMap.set('version', o.literal('12.0.0-next.8+442.sha-8d20bb9'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
