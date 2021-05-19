3b7fd74966aaaab727f8b153ab3eff480a91d962
atscott <atscott01@gmail.com>
2021-05-19 18:37:56 +0000

    ef676bf946 build: reformat yml files (#42136)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.0-next.8+445.sha-4259945'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+2.sha-ef676bf'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
