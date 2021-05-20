ed0236f88d7bd0c5bf10049804967f54bf96b263
Zach Arend <zachzach@google.com>
2021-05-20 16:29:13 +0000

    04ab5edf65 test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+2.sha-ef676bf'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+3.sha-04ab5ed'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
