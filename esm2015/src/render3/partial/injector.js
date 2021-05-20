aad27543442c8b5c950719fde5359b9afc11431d
Zach Arend <zachzach@google.com>
2021-05-20 17:44:15 +0000

    d6bc61ab5f build: update angular (#42090)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+6.sha-a01fe8d'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+8.sha-d6bc61a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
