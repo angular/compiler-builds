0c023133111905ebc58c07d5089524f3af56d252
Zach Arend <zachzach@google.com>
2021-05-20 17:15:26 +0000

    640578fe16 docs: embed the devtools video in the corresponding guide (#42161)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.1+3.sha-390d24b'));
+        definitionMap.set('version', o.literal('12.0.1+4.sha-640578f'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
