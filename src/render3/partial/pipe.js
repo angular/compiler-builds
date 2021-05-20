3332d68874c4010b055ffcc75191948ece0bace1
Zach Arend <zachzach@google.com>
2021-05-20 17:15:33 +0000

    a01fe8d9b4 ci: add Teri to global approvers for docs changes list (#42156)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+3.sha-04ab5ed'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+6.sha-a01fe8d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
