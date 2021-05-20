ad0d6f0f27dcb1aec6601fa5bc80909c8e3b5e28
Zach Arend <zachzach@google.com>
2021-05-20 17:16:55 +0000

    a01fe8d9b4 ci: add Teri to global approvers for docs changes list (#42156)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+3.sha-04ab5ed'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+6.sha-a01fe8d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
