52f062539c5071cff2e4445ea418c4c1397871f9
Zach Arend <zachzach@google.com>
2021-05-21 18:24:43 +0000

    62becc88f9 fix(docs-infra): fix example compilation issues (#42199)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+14.sha-8c158ba'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+15.sha-62becc8'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
