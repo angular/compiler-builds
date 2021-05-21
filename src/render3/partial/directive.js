00d74bb20b065bc20f9be3f61fad5d71f4806b03
Zach Arend <zachzach@google.com>
2021-05-21 18:26:37 +0000

    03825cfab6 docs: updated animation docs intro section and example (#42046)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+15.sha-62becc8'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+17.sha-03825cf'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
