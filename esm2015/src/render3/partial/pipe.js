4576a48e4026bed5d841c21b8cae5af381aa3d97
Zach Arend <zachzach@google.com>
2021-05-21 18:27:56 +0000

    03825cfab6 docs: updated animation docs intro section and example (#42046)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+15.sha-62becc8'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+17.sha-03825cf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
