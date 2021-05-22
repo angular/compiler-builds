0d89298d3e684abb3aa7fa9c7b8e8e63f9fc1239
Zach Arend <zachzach@google.com>
2021-05-22 08:28:36 +0000

    3293dd34ca docs: add missing `Product` interface to example (#42147)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+19.sha-4d56c10'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+21.sha-3293dd3'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
