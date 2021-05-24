304a6f4ac2cffc0d06237ef0adc7aa8c0bfe94c6
Zach Arend <zachzach@google.com>
2021-05-24 19:43:36 +0000

    c820066d98 docs: align code in `product-example.component.*.ts` files (#42155)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+25.sha-bf505ad'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+31.sha-c820066'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
