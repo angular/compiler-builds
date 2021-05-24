485226d5b37db8ac151bb6aa72abe160b8e51cee
Zach Arend <zachzach@google.com>
2021-05-24 23:02:49 +0000

    a39169851e docs: clarify pathMatch strategy prefix (#42287)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+37.sha-1040697'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+38.sha-a391698'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
