1d41a61217cda64a5f3d4251ab441c5f571e72ac
Zach Arend <zachzach@google.com>
2021-05-24 15:05:41 +0000

    c03280fd54 docs: update the intro image in the devtools guide (#42162)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+21.sha-3293dd3'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+23.sha-c03280f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
