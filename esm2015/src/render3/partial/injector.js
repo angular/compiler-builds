ec3484ca3dcbc794a337dd20a3fd96eab1f8287b
Zach Arend <zachzach@google.com>
2021-05-24 22:44:58 +0000

    ad12b31c27 docs: unsubscribe from observables (#42284)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+34.sha-47bb841'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+33.sha-ad12b31'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
