c20baff8ec473cdd4845dee0686d6dfdfbd5f194
Zach Arend <zachzach@google.com>
2021-05-24 17:27:14 +0000

    bf505ad707 docs(core): tighten up PipeTransform example types (#42240)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+24.sha-4ac55ca'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+25.sha-bf505ad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
