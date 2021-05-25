5260c3308813693843190022fa00be37e05efe38
Zach Arend <zachzach@google.com>
2021-05-25 18:14:44 +0000

    ef33806480 docs: add small clarity around navigationCancel (#42282)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+61.sha-2ff5bfe'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+62.sha-ef33806'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
