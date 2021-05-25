6a8b3ab3f62febe37f13584b9de0d15b8544e07a
Zach Arend <zachzach@google.com>
2021-05-25 22:31:53 +0000

    bdd3cca9ab docs: update wording for angular libraries mapping peer dependencies (#42322)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+70.sha-3d9062d'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+72.sha-bdd3cca'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
