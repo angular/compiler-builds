cc32743cc280a8c38c14da84db44119a2b35424c
Zach Arend <zachzach@google.com>
2021-05-25 22:37:59 +0000

    b7721aa630 docs: Edit correction to remove jargon in Release section (#42029)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+80.sha-6da0c97'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+84.sha-b7721aa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
