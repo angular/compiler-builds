019fc9156df88280039fd4795df8050c883e8afe
Zach Arend <zachzach@google.com>
2021-05-25 18:20:25 +0000

    f74fd64523 fix(docs-infra): increase the opacity of background-color for inline `code` blocks (#42297)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+62.sha-ef33806'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+65.sha-f74fd64'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
