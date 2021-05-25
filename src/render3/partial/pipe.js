413a8f52acaba6d7ff688bb9332b3da98cbd9c89
Zach Arend <zachzach@google.com>
2021-05-25 22:32:33 +0000

    9f50495f28 docs: update date pipe api reference to indicate it is a pure pipe (#42320)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+77.sha-4a2cc3e'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+73.sha-9f50495'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
