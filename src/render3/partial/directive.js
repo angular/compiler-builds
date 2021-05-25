ca93237d48b61e8cfaa43ed4607c68bb9d8da55c
Zach Arend <zachzach@google.com>
2021-05-25 22:39:15 +0000

    b7721aa630 docs: Edit correction to remove jargon in Release section (#42029)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+80.sha-6da0c97'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+84.sha-b7721aa'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
