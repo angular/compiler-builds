58782c46c3363b87bc544b352bc8d8de2644fe69
Zach Arend <zachzach@google.com>
2021-05-25 20:50:40 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+68.sha-d7768c6'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+69.sha-3a6af8e'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
