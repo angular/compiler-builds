04b29d3e7c63feb37c3cca110f04829ed07d1310
Zach Arend <zachzach@google.com>
2021-05-25 18:27:10 +0000

    d7768c61ad docs: edit copy for tick() in testing and api docs (#35697)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+67.sha-594e633'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+68.sha-d7768c6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
