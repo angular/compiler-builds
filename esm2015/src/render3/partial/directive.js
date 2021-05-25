01abce9802ef4352e04bb6f529973a72419f0d80
Zach Arend <zachzach@google.com>
2021-05-25 18:15:01 +0000

    d15a6a3a71 docs: update web-worker syntax (#42306)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.2+59.sha-1955dbb'));
+    definitionMap.set('version', o.literal('12.1.0-next.2+58.sha-d15a6a3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
