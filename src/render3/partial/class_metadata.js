0cc30dd21ec5173706224c99b1cdbe32891fe1fb
Zach Arend <zachzach@google.com>
2021-05-25 18:16:01 +0000

    2ff5bfe139 docs: fix documentation of guard execution order (#42288)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+60.sha-dc6bff1'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+61.sha-2ff5bfe'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
