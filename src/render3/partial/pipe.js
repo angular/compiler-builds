f2bce62601392ebabcd780dd72a6e6028ca3fbae
Zach Arend <zachzach@google.com>
2021-05-25 22:41:37 +0000

    8228170919 docs: document shallow equality emit for ActivatedRoute (#42280)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.2+82.sha-850028d'));
+        definitionMap.set('version', o.literal('12.1.0-next.2+85.sha-8228170'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
