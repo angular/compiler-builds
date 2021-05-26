3e24410cb3e94405fbbfc55e8b985044f8d57ee6
Zach Arend <zachzach@google.com>
2021-05-26 20:25:17 +0000

    38b0e71cb2 docs: move the schematics under Angular Tools menu section (#42332)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+5.sha-40bbcaa'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+6.sha-38b0e71'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
