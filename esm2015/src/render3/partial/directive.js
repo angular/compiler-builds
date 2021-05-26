44e475234ca5b85c0d12aa9e05a5be19025478c1
Zach Arend <zachzach@google.com>
2021-05-26 15:23:48 +0000

    54b23aef42 docs: document shallow equality emit for ActivatedRoute (#42280)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.1+66.sha-c01da21'));
+    definitionMap.set('version', o.literal('12.0.1+83.sha-54b23ae'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
