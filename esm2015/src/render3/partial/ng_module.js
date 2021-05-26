8ca24e2c8c7f1b33d4c8ec49546f11fb98a43f93
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:44:33 +0000

    516923e3dc docs: clarify when the `ngDoBootstrap` hook is invoked (#42362)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+16.sha-1572659'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+18.sha-516923e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
