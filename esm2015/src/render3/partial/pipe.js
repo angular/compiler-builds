b9421c3f721d1f5a7450b381b146039968e30f4f
Andrew Kushnir <akushnir@google.com>
2021-05-26 23:07:18 +0000

    75a33e5fa9 docs: remove lightbox gif from Use Directives to Watch the DOM section. (#42369) This gif is out of date and causing reader confusion.

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.2+21.sha-765854e'));
+    definitionMap.set('version', o.literal('12.0.2+23.sha-75a33e5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
