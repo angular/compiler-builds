a8a0fcc9b8db90ac274765c45283e2cf9d00cc4f
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:39:37 +0000

    4e9b61aaf9 docs: remove/update broken links in resources appearing on aio (#42232)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+13.sha-9193ea2'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+14.sha-4e9b61a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
