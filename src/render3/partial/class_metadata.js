5a2271816839ad7e4c96950316e1bd8edddf8087
Zach Arend <zachzach@google.com>
2021-05-26 20:36:03 +0000

    9193ea20e4 docs: clarify providedIn options (#42355)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+11.sha-a787f78'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+13.sha-9193ea2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
