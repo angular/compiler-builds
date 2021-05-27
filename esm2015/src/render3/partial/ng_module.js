78a30a648e9c10a67e57c4077ded5f08c9365404
Andrew Kushnir <akushnir@google.com>
2021-05-27 00:34:25 +0000

    7a6358b4a8 docs: add note to clarify that ngOnChanges is only called if inputs are bound (#42374)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+21.sha-74186c3'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+23.sha-7a6358b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
