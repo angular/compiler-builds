a42c74021b9b9f5f4749a64d0055f9ca1a295432
Andrew Kushnir <akushnir@google.com>
2021-05-27 18:41:28 +0000

    cdb8f556fb feat(docs-infra): implement dark mode (#41129)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+31.sha-e73fef4'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+38.sha-cdb8f55'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
