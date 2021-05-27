c70027900b7468d3a4fdcc0b9ef2455669f2cc3e
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:23:09 +0000

    fa0144fde6 docs: clarify the shortcomings of local variable technique for parent-child component interactions (#42331)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+38.sha-cdb8f55'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+39.sha-fa0144f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
