878a2644c802d55fade5a8503981540f67b7991f
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:21:47 +0000

    fa0144fde6 docs: clarify the shortcomings of local variable technique for parent-child component interactions (#42331)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+38.sha-cdb8f55'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+39.sha-fa0144f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
