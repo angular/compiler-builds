f00868cd61cdfa6757c04a61cf930eee9be25eef
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:54:29 +0000

    306f7ac543 docs: cherry pick release notes for 12.0.2 to main (#42390)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+29.sha-deb8930'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+30.sha-306f7ac'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
