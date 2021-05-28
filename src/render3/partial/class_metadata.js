18c6cf73e169eceab8a38ec37243ac791819b0e7
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:22:07 +0000

    62b5a6cb07 docs: add docs for DebugElement (#42352)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+46.sha-4cd2cc4'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+48.sha-62b5a6c'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
