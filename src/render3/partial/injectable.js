13185e94308e7d8b9a6c6b569df998792e9941d8
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:06:49 +0000

    4cd2cc439d docs: Edits to remove jargon in Reference section (#42033)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+45.sha-ee0e3fb'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+46.sha-4cd2cc4'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
