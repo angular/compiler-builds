61ad80f4386c888fcb77b22677b2656757d39579
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:04:51 +0000

    ee0e3fbf89 docs: edits to remove jargon (#42025)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+42.sha-635aaaa'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+45.sha-ee0e3fb'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
