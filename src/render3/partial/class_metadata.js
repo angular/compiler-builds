33858b5bde5e875f8eb073376597f2a539209aa7
Andrew Kushnir <akushnir@google.com>
2021-06-01 21:33:03 +0000

    6a2e3e2b2c docs: change Navigation from type to interface (#42281)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+63.sha-5babdb2'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+65.sha-6a2e3e2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
