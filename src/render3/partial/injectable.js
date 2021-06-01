31ee4ca9d218d542ceeeeb0df06f8e38a05ad293
Andrew Kushnir <akushnir@google.com>
2021-06-01 20:26:34 +0000

    5babdb2284 build: update angular (#42379)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+62.sha-e039075'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+63.sha-5babdb2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
