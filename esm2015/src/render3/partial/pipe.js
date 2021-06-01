f7d380232cdb5afe28453915ce497077d322b63c
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:39:31 +0000

    3e192bfa4d build: fix linting against jasmine focus functions (#42415)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+61.sha-44b737e'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+60.sha-3e192bf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
