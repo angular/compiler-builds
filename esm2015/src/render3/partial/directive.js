9cb48d4915c79d4fbe1aa144e4ad8e8738aee3b9
Andrew Kushnir <akushnir@google.com>
2021-06-01 21:31:39 +0000

    6a2e3e2b2c docs: change Navigation from type to interface (#42281)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.3+63.sha-5babdb2'));
+    definitionMap.set('version', o.literal('12.1.0-next.3+65.sha-6a2e3e2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
