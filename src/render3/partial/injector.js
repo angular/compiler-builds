e15f8b25f3527780ef28797ad55b35223d284c01
Andrew Kushnir <akushnir@google.com>
2021-06-01 20:27:59 +0000

    5babdb2284 build: update angular (#42379)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.3+62.sha-e039075'));
+        definitionMap.set('version', o.literal('12.1.0-next.3+63.sha-5babdb2'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
