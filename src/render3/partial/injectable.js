b1e6f48d3d3ee66823b4bf821f4b5f75ec3bf364
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:34:52 +0000

    f3f10140b5 build: lock file maintenance (#42264)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+2.sha-f8814d3'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+4.sha-f3f1014'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
