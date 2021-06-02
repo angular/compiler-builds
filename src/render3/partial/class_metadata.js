8c4e1453d612d6ce26275ae4e015dacd11f1c3a0
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:59:50 +0000

    3d1c905c91 refactor(docs-infra): clean up AIO Sass files (#42442)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+13.sha-0ac193c'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+14.sha-3d1c905'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
