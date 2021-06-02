1d1565efdad7a8a7ede33cd708af3dafec86e087
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:01:13 +0000

    3d1c905c91 refactor(docs-infra): clean up AIO Sass files (#42442)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+13.sha-0ac193c'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+14.sha-3d1c905'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
