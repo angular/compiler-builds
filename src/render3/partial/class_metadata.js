82f00fc76d9cec729b22613457b069bb6bdd5286
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 21:03:26 +0000

    a6dec05e82 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.3+22.sha-50c87e8'));
+        definitionMap.set('version', o.literal('12.0.3+26.sha-a6dec05'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
