fd460b367971199d3ab23db1410dde45f949c0e1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 20:35:49 +0000

    fa84d19e97 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+30.sha-69e5782'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+32.sha-fa84d19'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
