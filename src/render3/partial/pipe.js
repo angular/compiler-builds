8f56cd0dd38e859a4868a9276ecb28a50c2e5eff
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:58:40 +0000

    69e57827e2 refactor(compiler-cli): remove unused return type transform (#41996)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+28.sha-751cd83'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+30.sha-69e5782'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
