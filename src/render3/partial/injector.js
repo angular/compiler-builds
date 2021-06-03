ecd3c74b1766f09a533aa2a6e3003678af76036c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:27:47 +0000

    374fa2c26f fix(common): initialize currencyCode in currencyPipe (#40505)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+14.sha-3d1c905'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+23.sha-374fa2c'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
