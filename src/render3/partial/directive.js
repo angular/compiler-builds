e76ed6e06064d5b73e8bd62afe29b882556d14f1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 19:00:11 +0000

    69e57827e2 refactor(compiler-cli): remove unused return type transform (#41996)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+24.sha-bd1836b'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+30.sha-69e5782'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
