2294ac940d1cb22e976a2c27b4a7bffdf7c810ce
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 20:40:25 +0000

    47270d9e63 feat(forms): add `ng-submitted` class to forms that have been submitted. (#42132)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+32.sha-fa84d19'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+33.sha-47270d9'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
