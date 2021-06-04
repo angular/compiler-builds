1fbbc808373d6d2d68b53bf3c4433b56ad455e64
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:54:07 +0000

    4fc24addcb build: suppress errors in git hooks (#42484)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+43.sha-71e14a7'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+46.sha-4fc24ad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
