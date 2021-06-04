e8325662c19ac961131513269e7363138c185dc9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 17:55:27 +0000

    69296254da build: update dependency google-closure-compiler to v20210601 (#42475)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+40.sha-00b1444'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+41.sha-6929625'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
