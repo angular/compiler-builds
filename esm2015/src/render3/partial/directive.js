15c90e84d84fb64e7ea97d500e66d9c27e18306b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 18:37:19 +0000

    71e14a71f5 test: update compiler-cli compliance goldens due to TS 4.3 emit format regression (#42022)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+41.sha-6929625'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+43.sha-71e14a7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
