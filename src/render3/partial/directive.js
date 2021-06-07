55c7c17138143563a97b85f4086920cad1041ecd
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:10:28 +0000

    f4c55e464a docs: provide more info on the `NO_ERRORS_SCHEMA` schema (#42327)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+51.sha-cc904b5'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+53.sha-f4c55e4'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
