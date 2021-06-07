f1cacdc7b265e901200c3d50fb0b8433df9b40e2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 17:20:35 +0000

    cc904b5226 docs(core): clarify deprecation of `entryComponents` (#42248)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+50.sha-b061e5b'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+51.sha-cc904b5'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
