ce02631452552d502385064c08ae9e638ececcbe
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:10:42 +0000

    64c8027538 docs: clarify how type-only imports can be used to avoid NG3003 (#42491)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+53.sha-f4c55e4'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+54.sha-64c8027'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
