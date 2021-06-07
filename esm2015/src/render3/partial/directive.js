ae61f9dec34d7e66266b5a8b094b3262d08d2f84
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:13:56 +0000

    38c592ea0a build: remove unused no-strict tsconfig file (#42506)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+54.sha-64c8027'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+55.sha-38c592e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
