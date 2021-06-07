21f4e4b67c117e3ef74ca76435ae272bfe7125db
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:16:32 +0000

    f85a120b7b docs(router): fix code-example headers (#42507)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+55.sha-38c592e'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+56.sha-f85a120'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
