a88e33301dce7c56f56826a543af8b282895046a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 18:12:27 +0000

    38c592ea0a build: remove unused no-strict tsconfig file (#42506)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+54.sha-64c8027'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+55.sha-38c592e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
