c2cd661c5c78f55097b4611219f0601b2d72405b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:02:48 +0000

    c44ab4f6da fix(router): fix `serializeQueryParams` logic (#42481)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+72.sha-d10c38a'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+76.sha-c44ab4f'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
