d4433a3dfc6594fb23dd7ae4197f425ecc5c965e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:07:42 +0000

    4975a106dc docs(platform-server): added commands for universal and add prerendering info (#36005)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+85.sha-43b4a8c'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+87.sha-4975a10'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+85.sha-43b4a8c'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+87.sha-4975a10'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
