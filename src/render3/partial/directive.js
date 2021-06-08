529da4fe3342123b7b9cc378d5df7291968ca460
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:07:03 +0000

    43b4a8c07c docs: Edited to remove jargon in Reference Errors (#42186)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+86.sha-f3fde4b'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+85.sha-43b4a8c'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
