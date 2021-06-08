634434e9abdc654090d2b280737872364976f2d8
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 22:44:13 +0000

    230f0c41b7 build: lock file maintenance (#42499)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+90.sha-34ce635'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+91.sha-230f0c4'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
