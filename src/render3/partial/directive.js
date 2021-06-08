4a2c15aa3a39974950ed91aaf6bdd25a66a593ee
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:57:48 +0000

    645cad5614 docs(core): describe interactions between view-encapsulated components (#42397)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+71.sha-751f68d'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+73.sha-645cad5'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
