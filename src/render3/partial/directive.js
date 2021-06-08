0331fbd1ea05459a3d8fe34dd2d4c209ee08e745
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 22:45:42 +0000

    230f0c41b7 build: lock file maintenance (#42499)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+90.sha-34ce635'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+91.sha-230f0c4'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
