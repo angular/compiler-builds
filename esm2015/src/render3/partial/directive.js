57bd6d0096ce36203f5199d1bca73a048491f517
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:06:39 +0000

    8fe40026c1 docs: clarify documentation regarding passing multiple configurations to ng build (#41333)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+75.sha-171428e'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+79.sha-8fe4002'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
