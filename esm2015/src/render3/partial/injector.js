9ade9db9bbf6d74aa5b58bc37a52070050a36223
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 01:43:44 +0000

    f1d58efdb1 docs: Edits to remove jargon in Reference Observables, Dependency Injection (#42185)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+63.sha-b762820'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+64.sha-f1d58ef'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+63.sha-b762820'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+64.sha-f1d58ef'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
