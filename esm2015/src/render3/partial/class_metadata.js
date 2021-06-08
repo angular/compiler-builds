53dff3369fd953f206feed48e618ec44cedf7a8d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 21:21:14 +0000

    34ce635e3a feat(forms): undo revert and add ng-submitted class to forms that have been submitted. (#42132) (#42132)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+88.sha-2a28a1d'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+90.sha-34ce635'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
