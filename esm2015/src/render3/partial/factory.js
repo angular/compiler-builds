1186784ddabb9840099b0dd358dba8ff52a58422
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:02:05 +0000

    5c0541f351 docs: fix issue 24571 in ngOnChanges (#42517)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+82.sha-386550c'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+84.sha-5c0541f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
