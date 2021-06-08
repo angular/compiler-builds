af3b42ea6a147226cdb49ff4964d8c660a5bd6f5
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:25:55 +0000

    66f49c2497 Revert "refactor: remove checked-in locale files" (#42521)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+64.sha-f1d58ef'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+70.sha-66f49c2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+64.sha-f1d58ef'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+70.sha-66f49c2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
