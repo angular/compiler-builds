ff43a491cb81a61af57e23732cbde73704edef80
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:02:47 +0000

    386550cdf3 docs: improve accessibility of reactive-forms example (#41252)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.4+81.sha-0031c8c'));
+    definitionMap.set('version', o.literal('12.1.0-next.4+82.sha-386550c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
