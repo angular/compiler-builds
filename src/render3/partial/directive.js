a455050cee79fa2939449f1db2b094db0ae52f45
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:10:46 +0000

    3abf365fdc docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.3+63.sha-5f6492e'));
+        definitionMap.set('version', o.literal('12.0.3+64.sha-3abf365'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
