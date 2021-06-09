1b11bcd87cb6b9b664a5dbf270aa9b3cccbc79ec
GitHub <noreply@github.com>
2021-06-09 18:47:33 +0000

    28a4631b63 release: cut the v12.1.0-next.5 release (#42534)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+96.sha-7b5e2fe'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+97.sha-28a4631'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
