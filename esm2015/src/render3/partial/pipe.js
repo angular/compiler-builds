69d221e86aff83ccec947ce7dc070fcfd90e8331
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:28:44 +0000

    22bda2226b fix(compiler-cli): prevent prior compilations from being retained in watch builds (#42537)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+2.sha-3961b3c'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+3.sha-22bda22'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+2.sha-3961b3c'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+3.sha-22bda22'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
