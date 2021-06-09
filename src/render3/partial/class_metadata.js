f6f9c228e0e78c9e23623d7033d1911038627e65
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:14:29 +0000

    3de774e778 docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.4+92.sha-e1a80d4'));
+        definitionMap.set('version', o.literal('12.1.0-next.4+93.sha-3de774e'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
