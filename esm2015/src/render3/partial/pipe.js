3e6b333c697848994f1452f69ac6e2d2a44e84a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:27:42 +0000

    013d1cb010 refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.0.4+6.sha-5185795'));
+    definitionMap.set('version', o.literal('12.0.4+11.sha-013d1cb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
