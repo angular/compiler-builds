8ee9364d12749aef016ff33388921711ea9b09c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:20:49 +0000

    cddb3a88d3 docs: fix typo in "deploy multiple locales" section (#42541)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+7.sha-aef489f'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+9.sha-cddb3a8'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
