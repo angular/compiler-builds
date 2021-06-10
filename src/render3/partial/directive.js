f51b5572bc99410959234098a143e5e656bba3d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:17:13 +0000

    aef489f139 docs: add note for naming libraries (#42335)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+6.sha-e299683'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+7.sha-aef489f'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
