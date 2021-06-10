040b7321166cbb30d86a32c44f49d861787c18f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:42:28 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.0.4+4.sha-a75874c'));
+        definitionMap.set('version', o.literal('12.0.4+5.sha-e0158a1'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
