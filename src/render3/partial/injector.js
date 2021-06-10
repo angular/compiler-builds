ed1a794e13189e9e8cc7a4add7b8040627e1782f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 17:47:43 +0000

    e299683692 docs: improve accessibility of router example (#40914)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+5.sha-299f92c'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+6.sha-e299683'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
