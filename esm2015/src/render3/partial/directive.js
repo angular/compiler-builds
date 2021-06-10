8b122331a3d27bad1f44386ec3c363be8d5e4676
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:37 +0000

    efb440eb2f refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+11.sha-52e0987'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+12.sha-efb440e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
