7de3834f26ba3eed41ee41221b3dc47a58c00783
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-11 15:20:33 +0000

    0c13e2bf97 build(packaging): remove rehype (#42557)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+12.sha-efb440e'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+13.sha-0c13e2b'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
