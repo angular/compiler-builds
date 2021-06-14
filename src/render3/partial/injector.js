1baff99c5c90163f1011d1debb8ba553fe118b97
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:20:06 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+21.sha-e7c69b8'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+19.sha-56a0582'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
