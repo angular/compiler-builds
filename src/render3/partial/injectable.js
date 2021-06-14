52e9ad9bde61d54e0ad9ce2d31d1a267e0843826
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:31:31 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+30.sha-228beea'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+31.sha-4001e9d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
