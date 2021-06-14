fb32bd3c7f0ab56791e9b91dc5472c378b23c9fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:49 +0000

    e7c69b8551 docs: Grammatical error (#42571)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+20.sha-7180ec9'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+21.sha-e7c69b8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
