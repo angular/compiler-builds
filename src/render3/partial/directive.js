f5fa56ca95823cc909cc7755a54f0c4877f05d6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:39 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+21.sha-e7c69b8'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+19.sha-56a0582'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
