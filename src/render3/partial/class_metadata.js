6d13878faf22cc958da155991ab433c4d4a46a66
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:13:04 +0000

    b52e93543f docs: add Daniel Trevino to contributors file (#42573)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+32.sha-a66dd88'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+33.sha-b52e935'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
