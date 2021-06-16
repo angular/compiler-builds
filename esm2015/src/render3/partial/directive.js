7cf37301d41f752a8f0700638b09e64292795e5f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:53:58 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+49.sha-7961e2f'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+50.sha-0e33521'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
