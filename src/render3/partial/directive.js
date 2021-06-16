0e65a40bd61fd7422f689c0d99a2be732820904c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:53 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+40.sha-2d1347b'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+41.sha-62aca30'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
