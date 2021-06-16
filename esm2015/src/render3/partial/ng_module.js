bae32fe357161289e5301f86899acb51dc830515
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:24:59 +0000

    eb04684ed3 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+46.sha-7b85a4d'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+47.sha-eb04684'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
