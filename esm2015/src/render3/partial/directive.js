02913efda757e0840a73c729fa17f878d5ca7c2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:21:14 +0000

    f3a79878af docs(forms): correct sample code for FormArray.reset (#42477)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+41.sha-62aca30'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+42.sha-f3a7987'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
