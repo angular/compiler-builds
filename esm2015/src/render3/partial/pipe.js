f16328862e780322608d24cd64af50a9aa9b30f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:19:25 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+40.sha-2d1347b'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+41.sha-62aca30'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
