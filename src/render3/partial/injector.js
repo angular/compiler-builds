8bc6c065ef1a3540d23f4d5ea0bce38115326032
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:52:31 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+49.sha-7961e2f'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+50.sha-0e33521'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
