9420d8a9d61e57089d886b2d302fcda5cae10eb8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:10 +0000

    3b81528ddd fix(docs-infra): improve card layout for different browser font sizes (#42533)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+42.sha-f3a7987'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+43.sha-3b81528'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
