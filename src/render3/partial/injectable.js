778297f34d61baaf9a8aa24a281a5886ba9e02e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 17:11:28 +0000

    2d1347b2ce Revert "refactor: remove checked-in locale files (#42230)" (#42583)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.5+33.sha-b52e935'));
+        definitionMap.set('version', o.literal('12.1.0-next.5+40.sha-2d1347b'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
