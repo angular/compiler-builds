e0c29295ae5e3a7aa502e46b7b92f2dbf9e2dd4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:23:25 +0000

    eb04684ed3 fix(docs-infra): make anchor in list item inherit line-height (#42572)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.5+46.sha-7b85a4d'));
+    definitionMap.set('version', o.literal('12.1.0-next.5+47.sha-eb04684'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
