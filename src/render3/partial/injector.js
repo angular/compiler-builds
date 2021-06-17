7330849c9e844337dc9fcde23d13349ca5f8825b
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:27:45 +0000

    166e98a594 build: update all non-major dependencies (#42544)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+2.sha-0af9b89'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+3.sha-166e98a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
