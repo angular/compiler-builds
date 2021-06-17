5aac7cb760f5fe2733882605fe2fe30cce9f2f1a
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:27:29 +0000

    0af9b89b86 build: update angular to v12.0.5 (#42588)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.6+1.sha-873229f'));
+    definitionMap.set('version', o.literal('12.1.0-next.6+2.sha-0af9b89'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.6+1.sha-873229f'));
+    definitionMap.set('version', o.literal('12.1.0-next.6+2.sha-0af9b89'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
