fe381061a81d203c62225016f5f7573e1d70dcf2
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:34:27 +0000

    b037df28fd Revert "fix(docs-infra): do not redirect disambiguated URLs" (#42414)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+4.sha-07c1ddc'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+7.sha-b037df2'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
