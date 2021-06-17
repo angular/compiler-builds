e847c15903194b737f79ad612ff507bc3ca44ffd
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:29:03 +0000

    0af9b89b86 build: update angular to v12.0.5 (#42588)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+1.sha-873229f'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+2.sha-0af9b89'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
