d85ca790a1be7a173751b2aaeb3d67fcfff7fc33
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 23:21:38 +0000

    983c540191 docs: fix pipe params (#42593)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.6+7.sha-b037df2'));
+    definitionMap.set('version', o.literal('12.1.0-next.6+8.sha-983c540'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
