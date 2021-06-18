4cc10eb414ecd6005fd77f14f29ef3913133449c
Dylan Hunn <dylhunn@gmail.com>
2021-06-18 17:52:33 +0000

    44291889ee docs(docs-infra): document how to configure redirects (#42452)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+13.sha-0c105c3'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+23.sha-4429188'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
