4317e50c6df3fd549a66990d67a0641c6b79b91a
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:52:48 +0000

    8a67770687 docs(core): improve applicationref.bootstrap docs (#42407)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+38.sha-9498da1'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+39.sha-8a67770'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
