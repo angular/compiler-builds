bbd67ca8ae30cad9ef4dd08cd21edf997ba60e29
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:15:35 +0000

    2203217c40 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+58.sha-637ac00'));
+        definitionMap.set('version', o.literal('12.1.0-next.6+59.sha-2203217'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
