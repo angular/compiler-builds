ee1cb5ced0d3925468fa8efd9e37e07e8cc9c78c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 17:29:25 +0000

    4e46aef17b build: update dependency madge to v5 (#42629)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.6+55.sha-cc30dc0'));
+    definitionMap.set('version', o.literal('12.1.0-next.6+56.sha-4e46aef'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
