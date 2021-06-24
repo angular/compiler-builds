86d42c18033ba39dad438604ec1f386e28bbc166
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 16:39:28 +0000

    874de59d35 fix(compiler-cli): change default ngcc  hash algorithm to be FIPS compliant (#42582)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0-next.6+45.sha-8da6f66'));
+    definitionMap.set('version', o.literal('12.1.0-next.6+48.sha-874de59'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
