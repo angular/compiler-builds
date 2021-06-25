a14d8e624f6bc2a65059a4335f9001645a4edc9e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-25 17:20:19 +0000

    f6b828e292 fix(compiler-cli): add support for partially evaluating types (#41661)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.0-next.6+60.sha-d71d521'));
+        definitionMap.set('version', o.literal('12.1.0+2.sha-f6b828e'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
