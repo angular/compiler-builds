cad3e79d218a6b1257a65c7725c22febd4faa4be
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:53:53 +0000

    2a260dc249 docs: update Roadmap section header from "Done" to "Completed" (#42620)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+4.sha-56fb4eb'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+9.sha-2a260dc'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
