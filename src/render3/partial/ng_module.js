cb4eb06c3a2cc4d285414e04d3cf7d1fde2275dc
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:47:33 +0000

    b54e8aee37 docs: update fonts optimization options description (#42642)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+12.sha-c93df7d'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+13.sha-b54e8ae'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
