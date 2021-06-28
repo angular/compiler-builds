ba85599c598d865871669c8dfec95e1ee6c18450
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:52:19 +0000

    2a260dc249 docs: update Roadmap section header from "Done" to "Completed" (#42620)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.0+4.sha-56fb4eb'));
+    definitionMap.set('version', o.literal('12.2.0-next.0+9.sha-2a260dc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
