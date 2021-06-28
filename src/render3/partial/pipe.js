3e7f78cc77da248540fc55ab6882935955b15c78
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:52:45 +0000

    56fb4eb08d docs(compiler): interally typo fixed (#42676)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+3.sha-37a740c'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+4.sha-56fb4eb'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
