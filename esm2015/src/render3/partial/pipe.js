91057841fb6d95291eb99c4b2e14a657ecb453ea
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:54:20 +0000

    4bea630baa docs(core): add context jsdoc param to createEmbeddedView (#42675) PR Close #42675

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.0+9.sha-2a260dc'));
+    definitionMap.set('version', o.literal('12.2.0-next.0+10.sha-4bea630'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
