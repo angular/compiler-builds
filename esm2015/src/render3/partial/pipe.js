30eee5dc334323e1ee8d2de5a32d3f7ccaaad85d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:19:03 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.0+13.sha-b54e8ae'));
+    definitionMap.set('version', o.literal('12.2.0-next.0+12.sha-c93df7d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
