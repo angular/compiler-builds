9f5aad918a0ca2e57e9eee64a831a8cec1ca8f00
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 17:11:36 +0000

    1142b51462 fix(docs-infra): styling fix on error pages (#42627)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.0+10.sha-4bea630'));
+    definitionMap.set('version', o.literal('12.2.0-next.0+11.sha-1142b51'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
