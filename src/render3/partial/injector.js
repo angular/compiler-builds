b4d4b648c9a990701de2ccbefa76df77c2fcb74a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:38:46 +0000

    8528073f6c fix(dev-infra): remove bots from special thanks section (#42697)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+19.sha-f29fe5c'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+20.sha-8528073'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
