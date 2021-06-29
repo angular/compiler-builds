8401b16c79dd8501c29ee4cc9c4a81521ea136a8
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:53:00 +0000

    02742552f3 refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.0+15.sha-2c490d5'));
+    definitionMap.set('version', o.literal('12.2.0-next.0+17.sha-0274255'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
