8cd07049bd8bed7e44461cd33fb683d2164d7074
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:02:17 +0000

    0e20b05f7d docs: add last updated date to roadmap (#42615)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+23.sha-9f5cc7c'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+24.sha-0e20b05'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
