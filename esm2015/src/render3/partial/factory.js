b117f33536be644a8b888dc23a5bc34383d1b617
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 21:51:11 +0000

    41c6877c01 fix(core): error in TestBed if module is reset mid-compilation in ViewEngine (#42669)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0+23.sha-12e0cfa'));
+    definitionMap.set('version', o.literal('12.1.1+1.sha-41c6877'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
