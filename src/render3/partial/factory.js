e81603d8fe8b98dee71577213a21d66d9c0f6a24
GitHub <noreply@github.com>
2021-06-30 20:43:04 +0000

    24624e607d docs: release notes for the v12.1.1 release (#42722)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+27.sha-59fe159'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+28.sha-24624e6'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
