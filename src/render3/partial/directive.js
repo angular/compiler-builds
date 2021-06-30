27052c1a2602b2bc3e1d3c6be80375f5ea2a7abe
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:04:04 +0000

    59fe159b78 build: update API goldens to reflect new tool (#42688)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+24.sha-0e20b05'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+27.sha-59fe159'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
