793200e5ac1d5ecb380b6f1c2a2aa86b0ce42b5a
GitHub <noreply@github.com>
2021-06-30 20:33:34 +0000

    12e0cfab54 release: cut the v12.1.1 release (#42721)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.0+22.sha-aab41ca'));
+    definitionMap.set('version', o.literal('12.1.0+23.sha-12e0cfa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
