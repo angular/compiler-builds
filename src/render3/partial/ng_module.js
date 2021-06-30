112d230703ee9314d0b74e9477454d28ed55c1fa
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:54:08 +0000

    234b5edcc7 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.0+21.sha-f3965ff'));
+        definitionMap.set('version', o.literal('12.2.0-next.0+22.sha-234b5ed'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
