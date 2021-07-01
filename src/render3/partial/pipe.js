c3f910f99099f708fe00d0042118e11be6ca2d2a
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:53:12 +0000

    d997303cf4 build: update dependency husky to v7 (#42730)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+1.sha-0f23f73'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+2.sha-d997303'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
