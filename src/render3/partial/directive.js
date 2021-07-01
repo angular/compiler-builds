1f686c22b54e948a223ac2ffde83868de00199ad
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:51:42 +0000

    d997303cf4 build: update dependency husky to v7 (#42730)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+1.sha-0f23f73'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+2.sha-d997303'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
