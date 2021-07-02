a22ef1c2b991c69a85d305d16bf485b3ee40a51a
atscott <atscott01@gmail.com>
2021-07-02 17:10:57 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+7.sha-19bcdfb'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+9.sha-e81efe6'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
