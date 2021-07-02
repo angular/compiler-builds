547d4c283ae6358ef4122cf56549f86a496512b1
atscott <atscott01@gmail.com>
2021-07-02 17:29:39 +0000

    e99af184ab build: update API goldens after api-extractor update (#42737)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+9.sha-e81efe6'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+12.sha-e99af18'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
