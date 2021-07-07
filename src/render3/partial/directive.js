5248448c4fb4183f38053b3d7d6942c97dd95770
Andrew Scott <atscott@google.com>
2021-07-07 17:33:46 +0000

    e064f177a7 docs: add missing import to `CartService` tutorial instructions (#42701)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+17.sha-0c0c32d'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+18.sha-e064f17'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
