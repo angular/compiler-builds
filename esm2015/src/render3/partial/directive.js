2c38b48aaf210e7d80b195d25693e1961d7f3a4b
atscott <atscott01@gmail.com>
2021-07-07 22:39:15 +0000

    30c82cd177 fix(compiler-cli): inline type checking instructions no longer prevent incremental reuse (#42759)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+25.sha-95ba5b4'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+26.sha-30c82cd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
