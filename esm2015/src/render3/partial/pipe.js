d5beb6102627598f4e62e3687067c267d66b6902
Andrew Scott <atscott@google.com>
2021-07-07 20:56:58 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+18.sha-e064f17'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+19.sha-24ac378'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
