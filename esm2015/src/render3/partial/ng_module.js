3fedda8336c44c828f57fb052c9d950fe99b003d
Andrew Scott <atscott@google.com>
2021-07-07 20:55:25 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+18.sha-e064f17'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+19.sha-24ac378'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
