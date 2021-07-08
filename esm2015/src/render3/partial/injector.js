1528a946c5ec51a064d009ee1144701ed922444f
atscott <atscott01@gmail.com>
2021-07-08 22:11:21 +0000

    1f3747b583 ci: configure renovate to ignore `@microsoft/api-extractor` (#42797)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+43.sha-e7832a6'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+45.sha-1f3747b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
