9789301367dcb9b2c9057fb0bf68c7decd8f6b19
atscott <atscott01@gmail.com>
2021-07-08 17:50:02 +0000

    cb2ca9a66e fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+29.sha-ad08b17'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+39.sha-cb2ca9a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
