11300cc59c189ef6f1a07e19d13508e68fc6b6f5
atscott <atscott01@gmail.com>
2021-07-08 17:51:32 +0000

    cb2ca9a66e fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+29.sha-ad08b17'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+39.sha-cb2ca9a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
