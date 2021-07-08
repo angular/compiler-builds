7844f4c878c269cf942bbdf6f43fe260b2796821
atscott <atscott01@gmail.com>
2021-07-08 21:25:00 +0000

    e26bfc1131 refactor(core): remove no longer needed types workaround (#42798)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+28.sha-f592a12'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+40.sha-e26bfc1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
