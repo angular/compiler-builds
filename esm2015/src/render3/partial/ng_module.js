9bc748d30db10b16e815224489dc262075b5884a
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:02:13 +0000

    4e73b8820a build: no longer ship ecmascript module files within NPM packages (#42809)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+59.sha-ef15b97'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+61.sha-4e73b88'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
