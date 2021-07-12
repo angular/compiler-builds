ac7da98bb1a1738952c53bfb92a0a4bf8e39bed0
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:13:11 +0000

    4c482bf3f1 fix(compiler-cli): properly emit literal types when recreating type parameters in a different file (#42761)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+62.sha-40da386'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+63.sha-4c482bf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
