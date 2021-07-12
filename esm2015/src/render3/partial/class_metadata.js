2656f730805f5b6bf9b6a31f8bf06adbca3edc3f
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:00:42 +0000

    4e73b8820a build: no longer ship ecmascript module files within NPM packages (#42809)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+59.sha-ef15b97'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+61.sha-4e73b88'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
