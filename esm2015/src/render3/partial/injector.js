59e17489ef4a7c7c83a751fd6b3dd63a7fd97f17
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:45:07 +0000

    88b15d572f docs: clarify voting phase for feature requests (#42663)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+98.sha-c1c1cda'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+99.sha-88b15d5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
