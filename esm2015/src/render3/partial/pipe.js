65772bc963948cb9f61534c7fac5c2210c72ca99
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:11:33 +0000

    4c482bf3f1 fix(compiler-cli): properly emit literal types when recreating type parameters in a different file (#42761)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+62.sha-40da386'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+63.sha-4c482bf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
