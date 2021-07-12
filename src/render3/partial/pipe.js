2171c2b7071a205fff2abbabca0bd993f1c923b9
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:09:23 +0000

    2ed5a2db79 docs(core): fix `ViewChildren` code examples to avoid TS error (#42816)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.1+46.sha-1c31828'));
+        definitionMap.set('version', o.literal('12.1.1+55.sha-2ed5a2d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
