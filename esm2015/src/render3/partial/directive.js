d3641cfed1de6e6a06fcc8667f6056c23c5ef62e
Andrew Kushnir <akushnir@google.com>
2021-07-12 21:58:23 +0000

    e6593ad94a fix(dev-infra): transitive targets cannot be resolved by API extractor (#42828)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+90.sha-a524af1'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+93.sha-e6593ad'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
