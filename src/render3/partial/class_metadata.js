61fae9f0c98ca6904f12fa45f8220478cff2a246
Andrew Kushnir <akushnir@google.com>
2021-07-13 23:20:11 +0000

    cd2d82a91a fix(core): associate the NgModule scope for an overridden component (#42817)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+103.sha-51156f3'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+104.sha-cd2d82a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
