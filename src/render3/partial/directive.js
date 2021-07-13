86305678b480e801c989fcc3f38001e9edca0be5
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:54:46 +0000

    d654c7933a fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.1+92.sha-b101ad0'));
+        definitionMap.set('version', o.literal('12.1.1+93.sha-d654c79'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
