8065ffa5f87ba75e6fa59be6033d49da331d84c3
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:27:58 +0000

    970d7f7c6f docs: update routing-overview topic with link to custom route matches tutorial (#42837)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+101.sha-e42aa6c'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+102.sha-970d7f7'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+101.sha-e42aa6c'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+102.sha-970d7f7'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
