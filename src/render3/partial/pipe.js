01e6b827d57be5918b50259bf983f94e683093ef
Andrew Kushnir <akushnir@google.com>
2021-07-13 16:10:53 +0000

    81dce5c664 fix(compiler-cli): check split two way binding (#42601)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+99.sha-88b15d5'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+100.sha-81dce5c'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
