af122fb42ba45195c8f5d57f372d9abb8044b176
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:43:57 +0000

    f5baa55b81 docs: remove reference to an obsolete design doc (#42842)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+11.sha-4e97514'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+12.sha-f5baa55'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
