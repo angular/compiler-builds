c115f2b885024fb56c415f0df9a8917d404852a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:45:31 +0000

    f5baa55b81 docs: remove reference to an obsolete design doc (#42842)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+11.sha-4e97514'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+12.sha-f5baa55'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
