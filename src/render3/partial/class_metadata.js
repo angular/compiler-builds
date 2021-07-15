aba67e144f4870ca4dcc1e6dc90fd6211b78c312
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:45:22 +0000

    4e97514465 docs: remove references to future in topic (#42841)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+9.sha-ea93672'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+11.sha-4e97514'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
