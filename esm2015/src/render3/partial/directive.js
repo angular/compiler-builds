0543630f59771b4ccfa2c04e93f6e04f690c67a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:34:59 +0000

    07d7e6034f perf(compiler-cli): optimize cycle detection using a persistent cache (#41271)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+3.sha-437759b'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+4.sha-07d7e60'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
