c6e0ef1d660509a4e4272e5813e24941cd38fb9c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:54:53 +0000

    642362a9c7 build: add configuration for caretaker handoff (#42859)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+12.sha-f5baa55'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+15.sha-642362a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
