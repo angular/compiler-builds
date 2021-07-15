aa53459f307c3fec15d34a15780c358e1353d781
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:40:50 +0000

    670300e9ab build: lock file maintenance (#42824)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+5.sha-31593db'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+10.sha-670300e'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
