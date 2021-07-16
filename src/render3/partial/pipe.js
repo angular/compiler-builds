c465373a67a7850334c28cb6a577a03aa060b470
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 20:05:31 +0000

    3b2f607cda build: generate closure locale files using hard-coded list of locales (#42230)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+16.sha-3da7146'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+24.sha-3b2f607'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
