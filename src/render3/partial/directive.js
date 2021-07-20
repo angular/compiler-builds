194757b9bf164ab357ecfd60da38a8964b18ac6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:52:18 +0000

    4286b08b5d ci: exempt bazel scope from merge tool target label checks (#42869)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+32.sha-96d31d8'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+33.sha-4286b08'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
