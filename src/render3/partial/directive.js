554ccde440db2fefcf431c1ce6efd31628d0d30b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:56:47 +0000

    52dc65dd1b build(docs-infra): improve @usageNotes message (#42888)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+39.sha-307dac6'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+40.sha-52dc65d'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
