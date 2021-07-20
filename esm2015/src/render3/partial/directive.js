11a171b65d832e100352734b07a2a71275ec5d19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:52:51 +0000

    7271ad15ba ci: remove RBE workaround from `components-repo-unit-tests` job (#42875)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+36.sha-7eddd12'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+35.sha-7271ad1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
