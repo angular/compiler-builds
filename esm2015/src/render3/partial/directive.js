d0cded89d774b52712368c1594041131100028cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:58:12 +0000

    52dc65dd1b build(docs-infra): improve @usageNotes message (#42888)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+39.sha-307dac6'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+40.sha-52dc65d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
