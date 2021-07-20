ae7de6c0e62b0a0d9bb89ed449dc5549c36fdefd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:51:53 +0000

    282e86ad71 fix(dev-infra): use the version from package.json rather than tags (#42872)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+40.sha-52dc65d'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+42.sha-282e86a'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
