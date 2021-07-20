c24523170f9cbf4a353f43dced433d38de7da8f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:51:50 +0000

    7eddd12bf0 feat(dev-infra): add option to setup global types in API golden test (#42876)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+34.sha-30165f9'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+36.sha-7eddd12'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
