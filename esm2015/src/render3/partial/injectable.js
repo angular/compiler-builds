1434b24b1b76ba5418fbd1fcaadb059e909f74c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:52:00 +0000

    445fee4174 feat(dev-infra): update package version verification to allow for experimental version (#42898)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+42.sha-282e86a'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+41.sha-445fee4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
