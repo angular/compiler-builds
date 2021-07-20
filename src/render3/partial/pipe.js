4bc498777fae44ca8101152ab91339aefcaa567d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:54:42 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+35.sha-7271ad1'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+38.sha-722eb5d'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
