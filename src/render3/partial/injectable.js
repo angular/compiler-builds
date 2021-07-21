6e75e33e74e579d859c8e5d95571e775f10b7a41
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:55:02 +0000

    4d86ea6471 ci: no longer use deprecated `pullapprove_conditions` option (#42895)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+46.sha-23d6133'));
+        definitionMap.set('version', o.literal('12.2.0-next.3+3.sha-4d86ea6.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
