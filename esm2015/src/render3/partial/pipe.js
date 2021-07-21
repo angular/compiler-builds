ee619791e0c8ecbb18354159030efc58b2c1cb05
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:55:17 +0000

    992dc93ea3 refactor(dev-infra): remove usages and dependency on shelljs (#42911)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+3.sha-4d86ea6.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+2.sha-992dc93.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.3+3.sha-4d86ea6.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.3+2.sha-992dc93.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
