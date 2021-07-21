11211d0b98ab162433025fcb905f29ee12031150
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:02:20 +0000

    5fb23eccea perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+5.sha-a502279.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+6.sha-5fb23ec.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
