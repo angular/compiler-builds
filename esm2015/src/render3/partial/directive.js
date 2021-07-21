01b5e774f25ce2d7f7a0e75d832602e51d4b4171
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:00:49 +0000

    5fb23eccea perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+5.sha-a502279.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+6.sha-5fb23ec.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+5.sha-a502279.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+6.sha-5fb23ec.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
