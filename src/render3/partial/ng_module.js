7bdc1b99559e526e8358ba7fce670dd359b834e8
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:53:37 +0000

    4d86ea6471 ci: no longer use deprecated `pullapprove_conditions` option (#42895)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+46.sha-23d6133'));
+        definitionMap.set('version', o.literal('12.2.0-next.3+3.sha-4d86ea6.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
