b7b55428e086faff2b79ca9b2b9dbf6e8ea02972
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:46:51 +0000

    7e9673d14a ci: fix typo in docs-bug issue template (#42908)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.3+6.sha-5fb23ec.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.3+9.sha-7e9673d.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
