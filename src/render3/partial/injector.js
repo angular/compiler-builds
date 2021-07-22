f102a127331a9e386cfaae71b762c720ab1d45b4
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 18:23:10 +0000

    c20103c7ca refactor(dev-infra): move semver utils into the utils directory (#42917)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.3+8.sha-268ba5c.with-local-changes'));
+        definitionMap.set('version', o.literal('12.1.3+11.sha-c20103c.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
