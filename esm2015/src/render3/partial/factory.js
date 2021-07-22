9f32110967ebf18d3cb453c19e61a7443574e8a5
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:45:19 +0000

    7e9673d14a ci: fix typo in docs-bug issue template (#42908)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+6.sha-5fb23ec.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+9.sha-7e9673d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
