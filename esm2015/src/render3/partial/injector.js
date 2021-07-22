248d8d0a09c1d3282cc6d8aaf97c6b9d3d6b55a1
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 18:22:59 +0000

    e462743969 refactor(dev-infra): move semver utils into the utils directory (#42917)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.3+10.sha-1d9d026.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.3+13.sha-e462743.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
