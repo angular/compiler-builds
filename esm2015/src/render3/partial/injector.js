b0c67e1ce1d82fa21d5a36580ce4555b80b0f5a5
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:27:06 +0000

    a6c256fcd6 docs: Add import line to router event example for clarification (#42935)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+70.sha-ccbb913.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+72.sha-7313756.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
