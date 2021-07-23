7add2681e4460a1a7613833bdd301826982df569
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:30:25 +0000

    8fc08c5ab1 ci: update pullapprove config to properly handle global approvals (#42929)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+71.sha-9073633.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+73.sha-c74927d.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
