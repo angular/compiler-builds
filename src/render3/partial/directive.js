d04785623f1005781da0549c77f8f8f9239662bf
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 19:27:13 +0000

    1f6cc57812 build(docs-infra): upgrade cli command docs sources to a57976b98 (#42943)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+72.sha-7313756.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+71.sha-9073633.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
