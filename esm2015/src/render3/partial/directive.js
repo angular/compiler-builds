101a383d81f8cb4219cff8a80640c414f65633f5
Dylan Hunn <dylhunn@gmail.com>
2021-07-24 00:33:44 +0000

    05d996d803 docs: Edits to remove jargon (#42861)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+75.sha-48c9a0d.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+76.sha-8948c93.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
