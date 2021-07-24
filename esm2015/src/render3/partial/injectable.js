105e2208d95ea60b7169fde86af43f82e735e7d6
Dylan Hunn <dylhunn@gmail.com>
2021-07-24 00:31:35 +0000

    e8eb87ff58 docs: Edits to remove jargon (#42861)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.1+51.sha-d8d7784.with-local-changes'));
+    definitionMap.set('version', o.literal('12.1.1+52.sha-7b5405b.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.1.1+51.sha-d8d7784.with-local-changes'));
+        definitionMap.set('version', o.literal('12.1.1+52.sha-7b5405b.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
