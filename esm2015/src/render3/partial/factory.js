aff43868bbbd829d48a0ee7bc7bb146486c6b12b
Dylan Hunn <dylhunn@gmail.com>
2021-07-24 00:35:09 +0000

    05d996d803 docs: Edits to remove jargon (#42861)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+75.sha-48c9a0d.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+76.sha-8948c93.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
