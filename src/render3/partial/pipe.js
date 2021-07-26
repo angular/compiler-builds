5dd9331b141d5748c3c152a34344c20228914943
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:22:59 +0000

    72c03a0d4f build: remove ts-api-guardian from repository (#42735)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+78.sha-634ba9c.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+79.sha-ff87da3.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
