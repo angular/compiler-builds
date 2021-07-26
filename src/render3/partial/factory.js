c48b7c40eb1d854dd035a01c143ca8feed667e10
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:23:43 +0000

    dbae00195e feat(router): ability to provide custom route reuse strategy via DI for `RouterTestingModule` (#42434)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+79.sha-ff87da3.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+80.sha-22290af.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
