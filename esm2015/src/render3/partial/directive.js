30b7e2e68c65af552ba8a834733b5ba41c556cc3
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 20:44:32 +0000

    77ae4459d3 fix(compiler-cli): use correct module resolution context for absolute imports in .d.ts files (#42879)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.1+54.sha-7dad7df.with-local-changes'));
+    definitionMap.set('version', o.literal('12.1.1+55.sha-2ed5a2d.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.1.1+54.sha-7dad7df.with-local-changes'));
+    definitionMap.set('version', o.literal('12.1.1+55.sha-2ed5a2d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
