730bf704ffea30f90d2795c9ad7bc7ecee52d149
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 16:41:48 +0000

    b93c42eff3 docs: updated input-output page documentation content (#42964) PR Close #42964

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+82.sha-01e869a.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+83.sha-388496c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
