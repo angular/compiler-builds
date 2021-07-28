229ba3087f7e42584e52888e87cec5d33131fb82
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:11:21 +0000

    047994b048 feat(dev-infra): introduce release action for directly branching-off into RC (#42973)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+86.sha-5b5868d.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+87.sha-1dffa51.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
