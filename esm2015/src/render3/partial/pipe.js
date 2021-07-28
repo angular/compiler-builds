7cd702aba15b808888bec4ddd781f17cf1cdadfe
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 18:16:29 +0000

    a2c893309d build: update bazel to v4.0.0-beta.1 (#42967)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+87.sha-1dffa51.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+90.sha-a524af1.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
