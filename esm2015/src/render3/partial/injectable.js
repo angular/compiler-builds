69e18b81e88647d93c96a2d43d47402f0bfb0811
Joey Perrott <josephperrott@gmail.com>
2021-07-28 19:27:09 +0000

    8116895ece docs: release notes for the v12.2.0-rc.0 release

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+91.sha-9456eca.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+93.sha-e6593ad.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
