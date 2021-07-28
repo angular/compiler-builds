c69a3e4ba8947e1b4da767313ac329b56043f16f
Joey Perrott <josephperrott@gmail.com>
2021-07-28 19:28:41 +0000

    8116895ece docs: release notes for the v12.2.0-rc.0 release

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+91.sha-9456eca.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+93.sha-e6593ad.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
