b6fd0b6f671188dac33d469474107f78eddb125e
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:16:18 +0000

    8ce1ac603a docs: Add josmar-crwdstffng to contributors list. (#42972)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+94.sha-08444c6.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+95.sha-f0d857e.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
