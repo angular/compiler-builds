3a2940b2d7c9f7a26edaba0489ed8b0ad452bf04
Joey Perrott <josephperrott@gmail.com>
2021-07-28 19:06:54 +0000

    b0739c21da docs: release notes for the v12.1.4 release

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+90.sha-a524af1.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+91.sha-9456eca.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
