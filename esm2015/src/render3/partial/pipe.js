cf713faa9edadec5ce63a34cf1f8fd9dbb538dbc
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:49:53 +0000

    dd6e80aac5 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+95.sha-f0d857e.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+98.sha-c1c1cda.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.1+95.sha-f0d857e.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.1+98.sha-c1c1cda.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
