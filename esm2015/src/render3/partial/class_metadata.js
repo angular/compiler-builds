4aed0e5d0e932f3831b351cf0fd6fb4dc0e63b28
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:52:04 +0000

    0af354ce05 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.1+98.sha-c1c1cda.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.1+100.sha-81dce5c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
