1b8a41408b6bf1e785f5da086613375dcd5daf4e
atscott <atscott01@gmail.com>
2021-08-02 18:03:35 +0000

    5d8759b6a2 build(docs-infra): upgrade cli command docs sources to 129d45cfa (#43004)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+10.sha-670300e.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+11.sha-4e97514.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
