2b00b474c7e13a2d837203c8361a79598f1a47b5
Andrew Scott <atscott@google.com>
2021-08-02 21:15:27 +0000

    0e47334db8 refactor(core): update peerDependencies to allow rxjs7 (#42991)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+6.sha-ca1fde9.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+7.sha-c11bcb7.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
