9b24150eb254e548650a1f267515454997e90080
atscott <atscott01@gmail.com>
2021-08-02 22:56:14 +0000

    f62366fd16 docs: amend links in content-projection guide (#43009)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+12.sha-f5baa55.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+13.sha-7422821.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
