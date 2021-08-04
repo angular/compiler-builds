9de67d53963cc8a5028d7293b0b184085351799d
GitHub <noreply@github.com>
2021-08-04 17:27:05 +0000

    82c8393d37 docs: release notes for the v12.1.5 release (#43046)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+29.sha-5327926.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+30.sha-89509df.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
