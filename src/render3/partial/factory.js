802dc26b6ea99d91b231720a976a1338173887a0
GitHub <noreply@github.com>
2021-08-04 17:43:15 +0000

    cdc8c7cad0 release: cut the v12.2.0 release (#43047)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+24.sha-3b2f607.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+25.sha-5d7359e.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
