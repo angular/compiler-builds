8ddb49deedf185c5d6f4b8e6982a0c949dd0dbd8
GitHub <noreply@github.com>
2021-08-04 17:25:59 +0000

    82c8393d37 docs: release notes for the v12.1.5 release (#43046)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+29.sha-5327926.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+30.sha-89509df.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
