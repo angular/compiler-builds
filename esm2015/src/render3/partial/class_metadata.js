e6eef1f06893bf32a12e3a4a5e9813461fb21ff0
GitHub <noreply@github.com>
2021-08-04 19:03:45 +0000

    a99892e62f release: cut the v13.0.0-next.0 release (#43049)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+31.sha-fd92a3e.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+32.sha-96d31d8.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
