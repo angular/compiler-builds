40c539b3c4b4d96d8ec71ce79c35c9ce0394359f
Andrew Kushnir <akushnir@google.com>
2021-08-04 23:47:19 +0000

    e49fc96ed3 feat(forms): Make Form Statuses use stricter types. (#42952)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+33.sha-4286b08.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+34.sha-30165f9.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
