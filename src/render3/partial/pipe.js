77f670c83bcd4f7718d5e054f065e5651146de13
Andrew Kushnir <akushnir@google.com>
2021-08-04 23:48:20 +0000

    e49fc96ed3 feat(forms): Make Form Statuses use stricter types. (#42952)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.0-next.2+33.sha-4286b08.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.0-next.2+34.sha-30165f9.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
