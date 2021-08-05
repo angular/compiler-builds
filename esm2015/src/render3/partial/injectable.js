0151b2348f6d1264d88f318ba89bb9ad45500285
Andrew Kushnir <akushnir@google.com>
2021-08-05 22:52:20 +0000

    30c5b9c144 docs: fix interval type in the example (#43042)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+26.sha-f12c533.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0-next.2+27.sha-b41a2b3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
