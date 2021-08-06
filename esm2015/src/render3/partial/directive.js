2c66879effa1a2acb9cd1a9b05393b08b1ee32f9
Andrew Kushnir <akushnir@google.com>
2021-08-06 16:13:31 +0000

    d8690d867c fix(dev-infra): search since HEAD on the local branch for version tags when creating stamps (#43065)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+36.sha-7eddd12.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.0+5.sha-d8690d8.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
