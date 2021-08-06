8665d4511bd995ff49367e6762e9347e58ea9d3c
Andrew Kushnir <akushnir@google.com>
2021-08-06 16:14:32 +0000

    d8690d867c fix(dev-infra): search since HEAD on the local branch for version tags when creating stamps (#43065)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0-next.2+36.sha-7eddd12.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.0+5.sha-d8690d8.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
