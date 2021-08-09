ae05021e258969ffbd8c300d8cc789d933d4322e
Andrew Kushnir <akushnir@google.com>
2021-08-09 19:45:45 +0000

    6c3ad1f1a4 fix(docs-infra): correctly detect chunk load errors (#43088)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.0+10.sha-bbad423.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.0+11.sha-6c3ad1f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
