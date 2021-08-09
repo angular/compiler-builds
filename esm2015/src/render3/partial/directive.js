eadf9ce7442d3a138b585a57fb4b1befa4ee4dd3
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:00:46 +0000

    d93427b806 docs: fix cli link (#43085) PR Close #43085

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0+4.sha-4371ff9.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.0+7.sha-d93427b.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
