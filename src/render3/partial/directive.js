4c37313848d359d1c800ce99b86bd80c104f47c2
GitHub <noreply@github.com>
2021-08-10 19:05:17 +0000

    181adff213 Revert "build(dev-infra): exlude commit message filter from formatting (#43091)" (#43104)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.0+14.sha-7edb128.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.0+15.sha-181adff.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.0+14.sha-7edb128.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.0+15.sha-181adff.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
