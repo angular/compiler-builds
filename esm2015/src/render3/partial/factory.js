436457f9f30048955dc5eb4c49b5a80cbe39b486
Andrew Kushnir <akushnir@google.com>
2021-08-10 18:07:52 +0000

    7edb128221 build(dev-infra): exlude commit message filter from formatting (#43091)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.0+13.sha-cb988ae.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.0+14.sha-7edb128.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
