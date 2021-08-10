5dda45546475c133bd6a290b6b682aa125e4301e
Andrew Kushnir <akushnir@google.com>
2021-08-10 19:14:20 +0000

    002d959fe5 docs: remove the mention of Stylus (#43013)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.0+15.sha-181adff.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.0+16.sha-002d959.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
