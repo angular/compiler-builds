c82073382fc7026437efb970b08360333117596f
GitHub <noreply@github.com>
2021-08-10 19:06:21 +0000

    181adff213 Revert "build(dev-infra): exlude commit message filter from formatting (#43091)" (#43104)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.0+14.sha-7edb128.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.0+15.sha-181adff.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
