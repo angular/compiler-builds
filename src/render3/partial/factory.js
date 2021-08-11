195dbdcdf0e08cf586ad30ea99886cd36cc5441d
Andrew Kushnir <akushnir@google.com>
2021-08-11 17:58:37 +0000

    9fb79d38aa build(docs-infra): upgrade cli command docs sources to 81ba264f9 (#43114)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.0+28.sha-1f18222.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+1.sha-9fb79d3.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.0+28.sha-1f18222.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+1.sha-9fb79d3.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
