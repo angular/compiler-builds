207f67d45bf2aa24207398677268364f35247fcf
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:55:48 +0000

    c7d305b072 refactor(compiler): Remove compileUndecoratedClasses config file (#43043)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+3.sha-a2a9b91.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+4.sha-c7d305b.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
