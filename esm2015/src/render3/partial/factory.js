4012342bb65f57dcdd957eba288020f6ab6457e6
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:58:38 +0000

    7a67597d01 docs: fix description: "property decorator" (not parameter) (#43068) (#43128)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.0+25.sha-9136dbd.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.1+3.sha-7a67597.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
