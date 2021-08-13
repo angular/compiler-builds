eb42ef97cc6d8e2d41fe32a8965260f669761d00
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:17:54 +0000

    2a1aa5867d ci: add breaking changes auto labeling action (#43141)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+6.sha-470371f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+7.sha-2a1aa58.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
