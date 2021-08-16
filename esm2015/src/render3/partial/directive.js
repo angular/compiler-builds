e2c86fa8fcba43f00a289561573d31a7e52ad811
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 20:27:41 +0000

    564ad0622a build: uipdate to latest @angular/dev-infra-private-builds (#43179)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+20.sha-c91cecd.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+22.sha-564ad06.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
