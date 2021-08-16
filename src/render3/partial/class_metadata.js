401b9c7cab504179a78a7f5541192538b5e441cd
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:11:28 +0000

    30bf472597 docs: update Aristeidis bio in GDE list (#43164)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+12.sha-286b280.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+13.sha-30bf472.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
