4f16e0db4d9ef49589ec09bd50a8f147dc4f299d
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:01:30 +0000

    0c84f6fd52 refactor(compiler): fix lateron typo in i18n ExtractorHost (#43160)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+8.sha-b0b7ef2.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+9.sha-0c84f6f.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
