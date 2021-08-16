86a841f7b1434417a7e16414f27ecacfae199594
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:01:34 +0000

    971f8572d2 refactor(compiler-cli): remove extra 'diagnostic' from readResource comment (#43147)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+9.sha-0c84f6f.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+10.sha-971f857.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
