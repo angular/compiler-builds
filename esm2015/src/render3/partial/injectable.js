aa198574859c40e6fdbc2f52107c88f6ddb0a442
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:12:28 +0000

    30bf472597 docs: update Aristeidis bio in GDE list (#43164)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+12.sha-286b280.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+13.sha-30bf472.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
