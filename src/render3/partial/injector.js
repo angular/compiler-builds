296ad1ed29a04ef65e12ad642d40dae85ef5ee5e
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:08:59 +0000

    286b2807de fix(router): eagerly update internal state on browser-triggered navigations (#43102)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+11.sha-b3bb6ec.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+12.sha-286b280.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
