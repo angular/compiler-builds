9d91bbaa934506b66c714859aad008c3e80cee93
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 17:00:28 +0000

    0c84f6fd52 refactor(compiler): fix lateron typo in i18n ExtractorHost (#43160)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+8.sha-b0b7ef2.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+9.sha-0c84f6f.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
