aca4c8278307cfed5cbd077ae6b2dcb9813ae531
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:44:45 +0000

    ec4dcdfbd7 build: lock file maintenance (#43015)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+34.sha-7e71370.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+35.sha-ec4dcdf.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
