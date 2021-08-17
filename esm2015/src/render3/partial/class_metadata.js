1522019278c0e3054097af41e168a0c95d3b85b5
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:40:56 +0000

    3cb1f18f97 fix(animations): add pure annotations to static property initializers (#43064)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+30.sha-b9fd76e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+33.sha-3cb1f18.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
