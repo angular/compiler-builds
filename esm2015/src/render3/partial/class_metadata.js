1769d6a99940e86f74201f7bb2bc96bbfc3db3f5
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 00:42:52 +0000

    bb6555979d fix(animations): add pure annotations to static property initializers (#43064)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.1+19.sha-cd10a4c.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.1+26.sha-bb65559.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.1+19.sha-cd10a4c.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.1+26.sha-bb65559.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
