673cbf355c62d022585f28305321e9c0954abed6
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:45:50 +0000

    ec4dcdfbd7 build: lock file maintenance (#43015)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+34.sha-7e71370.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+35.sha-ec4dcdf.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
