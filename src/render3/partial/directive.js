af9c110ccdb8280d870b63cb64b36ea045244d44
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:43:22 +0000

    545972e5e9 docs(upgrade): add `@extensible` to the `UpgradeComponent` class (#42807)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.1+26.sha-bb65559.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.1+29.sha-545972e.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
