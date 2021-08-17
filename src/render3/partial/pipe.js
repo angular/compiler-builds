3fc89d07ff6193d260b9acd336f4a2e5b5982b3c
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:43:38 +0000

    7e71370fe6 perf(platform-browser): avoid intermediate arrays in server transition (#43145)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+31.sha-cf33849.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+34.sha-7e71370.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
