47a2efb356a7aceefc9df211c0c76d841229a3e9
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 18:12:37 +0000

    3e756f06ee docs: fix typo (#43190)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+42.sha-edde058.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+43.sha-3e756f0.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
