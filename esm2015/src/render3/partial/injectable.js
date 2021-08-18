c331c2d0672bfe4542a364b409b8fb5cde0f8dcd
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 03:22:39 +0000

    3db35b360a docs: update links and formatting (#43066)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+39.sha-d7de6d6.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.1+40.sha-3db35b3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
