2afe854b2cb9c40495848f529db4aec138a2a122
GitHub <noreply@github.com>
2021-08-18 21:57:14 +0000

    e3b6e26565 release: cut the v13.0.0-next.2 release (#43200)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.1+43.sha-3e756f0.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.1+44.sha-e3b6e26.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
