8610dc8104d52f03158114f1d1f6e8a2f8dfc0af
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 19:16:24 +0000

    c389052bab test(language-service): add extended diagnostics test (#43107)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.2+3.sha-bed121c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.2+8.sha-c389052.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.2+3.sha-bed121c.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.2+8.sha-c389052.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
