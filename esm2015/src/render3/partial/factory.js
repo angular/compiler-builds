1ed11dfd66077f03627342e0022c00bce9d823c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 18:39:08 +0000

    47cca522f8 refactor(docs-infra): use tslint in aio's tools-lint script (#43010)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.1+44.sha-e3b6e26.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.2+1.sha-47cca52.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
