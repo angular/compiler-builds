95ef73366214df7c8b1d0f8a6ed77394cba9d98b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-23 20:49:12 +0000

    6e924313c3 fix(service-worker): NPE if onActionClick is undefined (#43210)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.2+9.sha-ccb09b4.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.2+11.sha-6e92431.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
