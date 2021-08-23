4778116203caa24d93dbbdbbd0eb34a52eee8553
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-23 20:50:09 +0000

    d7b6416fcf refactor(common): fix emiting typos (#43221)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.2+7.sha-cb8e51f.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.2+9.sha-d7b6416.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.2+7.sha-cb8e51f.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.2+9.sha-d7b6416.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
