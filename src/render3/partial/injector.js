013a964b21d712b3b7cfe4e005dea45530d5f261
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 21:40:15 +0000

    b8183614f5 release: cut the v12.2.3 release (#43257)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.2+16.sha-b46ce7d.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.2+17.sha-b818361.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
