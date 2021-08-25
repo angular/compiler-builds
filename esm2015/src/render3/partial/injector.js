5af4f18c167ebcd093ffd570dfa792da44bcc668
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 21:55:36 +0000

    86feec6030 docs: release notes for the v12.2.3 release (#43258)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.2+20.sha-a39c689.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.2+21.sha-86feec6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
