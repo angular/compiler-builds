3b2f69350dc8af033009e2ea3b507518edbdf839
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:10:22 +0000

    b46ce7d42f build(docs-infra): upgrade cli command docs sources to 56c5962df (#43239)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.2+9.sha-d7b6416.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.2+16.sha-b46ce7d.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.2+9.sha-d7b6416.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.2+16.sha-b46ce7d.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
