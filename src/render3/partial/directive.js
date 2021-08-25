859467af2cad76d62a7a6dbbfc71a5cf011e0582
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:03:38 +0000

    a39c68940a build(docs-infra): upgrade cli command docs sources to 2621e1d39 (#43238)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.2+19.sha-073b20b.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.2+20.sha-a39c689.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
