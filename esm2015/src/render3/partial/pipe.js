9592684a98e4d7abbafd7199bad7c6674c375039
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:04:36 +0000

    a39c68940a build(docs-infra): upgrade cli command docs sources to 2621e1d39 (#43238)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.2+19.sha-073b20b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.2+20.sha-a39c689.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
