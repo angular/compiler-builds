3f5e86f7fdbfecc7ecbd9fcb9008bcd52ef16a9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-25 18:01:11 +0000

    00aad59047 test(language-service): add test for extended diagnostics (#43134)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.2+12.sha-27fdd52.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.2+18.sha-00aad59.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
