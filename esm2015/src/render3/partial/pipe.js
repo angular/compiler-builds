cd51d3003abf5beb1d88a424d35136255832bd65
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 23:55:47 +0000

    95cfd0b0e1 test(compiler-cli): add tests for `NullishCoalescingNotNullableCheck` (#43232)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.3+9.sha-c33ba8d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.3+16.sha-95cfd0b.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
