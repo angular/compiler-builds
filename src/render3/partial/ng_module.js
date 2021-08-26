496a47d58458ad49e1fc452405f1f81e110fd8f2
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:25:37 +0000

    c3968047b6 refactor(forms): make error messages easier to tree shake (#43223)

--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+5.sha-030ff0d.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+7.sha-c396804.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
