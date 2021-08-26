22e86993185c816fb0ad8a2e104a2937c75db7d6
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:28:09 +0000

    78690ee354 docs(router): Fix incorrect name for url match options (#43268)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.3+7.sha-c396804.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.3+8.sha-78690ee.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
