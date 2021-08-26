831e7d0101f5453e6d1d9d87d875c197ca241ae4
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 23:57:05 +0000

    692e8ab21e test(compiler-cli): add tests for `NullishCoalescingNotNullableCheck` (#43232)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.3+6.sha-30d959a.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.3+13.sha-692e8ab.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.3+6.sha-30d959a.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.3+13.sha-692e8ab.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
