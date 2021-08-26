663878a7f51e510adf3d085a78a7c59d4cda95ac
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:27:07 +0000

    78690ee354 docs(router): Fix incorrect name for url match options (#43268)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+7.sha-c396804.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+8.sha-78690ee.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
