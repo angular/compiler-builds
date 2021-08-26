98ad872ed58e5be47d3b3a8a7cfa3fd4c295f346
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 20:03:55 +0000

    c33ba8d4de build: update issue template to include core package (#43272)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+8.sha-78690ee.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+9.sha-c33ba8d.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
