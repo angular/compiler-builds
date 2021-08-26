4fc5574f7bf07ced532a739f708a968739145072
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 23:56:44 +0000

    95cfd0b0e1 test(compiler-cli): add tests for `NullishCoalescingNotNullableCheck` (#43232)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+9.sha-c33ba8d.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+16.sha-95cfd0b.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
