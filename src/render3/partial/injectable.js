a0fd2d7ca531bbf8533a0d89876f13bb90ef2615
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:05:59 +0000

    030ff0d7a1 test(ngcc): use @angular npm packages in integration test (#43234)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+3.sha-298d8a4.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+5.sha-030ff0d.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
