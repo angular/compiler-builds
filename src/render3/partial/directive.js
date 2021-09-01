eaa62de2cb7fa7faa3f2975153cf44febff20606
GitHub <noreply@github.com>
2021-09-01 19:02:52 +0000

    8fdda0b52a docs: release notes for the v12.2.4 release (#43310)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+20.sha-faf9f5a.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+21.sha-8fdda0b.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
