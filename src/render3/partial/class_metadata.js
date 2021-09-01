86ab3013c100b95a0b2f0b5b0a09d3df6a4b0706
GitHub <noreply@github.com>
2021-09-01 19:28:03 +0000

    478131c519 release: cut the v13.0.0-next.4 release (#43312)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.3+21.sha-8fdda0b.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.3+22.sha-478131c.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
