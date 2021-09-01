69572245f89c354bf56fb8b75fe1ddf4c92d7bdc
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:40:09 +0000

    a0bd6e90f9 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.3+17.sha-caecd18.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.4+1.sha-a0bd6e9.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.3+17.sha-caecd18.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.4+1.sha-a0bd6e9.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
