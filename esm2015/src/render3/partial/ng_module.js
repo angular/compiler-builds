8a76282d06e151bf15991790c7d70d624c8a4c48
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:05:47 +0000

    c7a93e632b docs: enhance spacings in dynamic component loader section examples (#43241)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+1.sha-95f3aec.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+10.sha-c7a93e6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
