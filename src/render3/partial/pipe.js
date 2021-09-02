b11e46efc6d1f6da672d3517d440c7410c4993ad
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:05:57 +0000

    3c863c5c4e build(docs-infra): update dgeni-packages to improve checkLinks messages (#43317)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.4+10.sha-c7a93e6.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.4+2.sha-3c863c5.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
