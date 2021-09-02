da4f61dd51629a391f55040744f4e1cc634a541a
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:04:56 +0000

    3c863c5c4e build(docs-infra): update dgeni-packages to improve checkLinks messages (#43317)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+10.sha-c7a93e6.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+2.sha-3c863c5.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
