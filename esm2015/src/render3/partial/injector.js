d4e7965ea69bd0b4c4e6001b8222099c0e9f4206
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:12:53 +0000

    c6a93001eb fix(common): synchronise location mock behavior with the navigators (#41730)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+11.sha-fc3b50e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+12.sha-c6a9300.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
