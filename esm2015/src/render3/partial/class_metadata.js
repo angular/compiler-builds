0d4f6888d97718a850cbbf559df6ebf833c92fcd
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:13:52 +0000

    c6a93001eb fix(common): synchronise location mock behavior with the navigators (#41730)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+11.sha-fc3b50e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+12.sha-c6a9300.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+11.sha-fc3b50e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+12.sha-c6a9300.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
