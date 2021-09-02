4d4009260b50ef4fa8c57d6fb9db59f87ea39b5b
Andrew Scott <atscott01@gmail.com>
2021-09-02 17:04:43 +0000

    c7a93e632b docs: enhance spacings in dynamic component loader section examples (#43241)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+1.sha-95f3aec.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+10.sha-c7a93e6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
