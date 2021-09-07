cd55bcf6add92803e656a25e5fc708e9a65c982f
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:03:42 +0000

    652a6d36d4 ci: avoid failures when yarn unlinks bazelisk in windows jobs (#43373)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.4+15.sha-5472c28.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.4+16.sha-652a6d3.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
