6fa9cad51f3956d7608ffac265dbf74992627123
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:02:39 +0000

    652a6d36d4 ci: avoid failures when yarn unlinks bazelisk in windows jobs (#43373)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+15.sha-5472c28.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+16.sha-652a6d3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
