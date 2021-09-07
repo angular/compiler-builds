0c82467aed3bf0cc49752376aaa395115e3ede38
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:05:37 +0000

    3b4f121caf docs(common): correct typos in jsdoc of http client (#43352)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.4+10.sha-aa1782c.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.4+12.sha-3b4f121.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
