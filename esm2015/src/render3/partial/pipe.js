0f75677c457088aa4db98f184d0d16e159ad191d
Andrew Scott <atscott01@gmail.com>
2021-09-07 21:45:12 +0000

    2e11be8c91 build: remove unused `@types/node-fetch` package (#43345)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+20.sha-359f26e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+22.sha-2e11be8.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
