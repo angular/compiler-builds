38b38e144149dc8abcf36132c4181c5dbc470670
Andrew Scott <atscott01@gmail.com>
2021-09-08 17:15:18 +0000

    9e039ca68b fix(router): Only trigger router navigation on `popstate` events from `Location` subscription (#43328)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.4+29.sha-93c9932.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+1.sha-9e039ca.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
