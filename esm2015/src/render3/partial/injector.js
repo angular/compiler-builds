2450db2570823af62dde1801b1e746710af7d477
GitHub <noreply@github.com>
2021-09-08 17:03:44 +0000

    9c7a0194d4 docs: release notes for the v12.2.5 release (#43393)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+27.sha-ed24c4a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.4+28.sha-9c7a019.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
