afe9b7a23415d43b0e86ff1dea0ac96e95fc77dc
Andrew Scott <atscott01@gmail.com>
2021-09-08 17:14:15 +0000

    9e039ca68b fix(router): Only trigger router navigation on `popstate` events from `Location` subscription (#43328)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.4+29.sha-93c9932.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+1.sha-9e039ca.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- src/render3/partial/ng_module.js
+++ src/render3/partial/ng_module.js
@@ -43,7 +43,7 @@
     function createNgModuleDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.4+29.sha-93c9932.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+1.sha-9e039ca.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // We only generate the keys in the metadata if the arrays contain values.
