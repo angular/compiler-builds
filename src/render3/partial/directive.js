f4261bfb5ca121f537d0c37b5b85374df571b5f6
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:47:24 +0000

    4761842440 docs: remove extraneous word in the Dependency injection in action guide (#43255)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+20.sha-2d93e9c.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+21.sha-4761842.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
