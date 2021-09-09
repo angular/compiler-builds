ebc20e726d8217538ce11cfbe0fb696dc28e96a0
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:36:50 +0000

    99f63dc13b fix(docs-infra): bump aio size limit (#42800)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+9.sha-f57f0ed.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+19.sha-99f63dc.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
