e99a10d7c8fd12eb9c9aaa2e13cfb44f80831bd7
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:37:14 +0000

    5fea50c070 docs: remove references to differential loading (#43060)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+1.sha-9e039ca.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+3.sha-5fea50c.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
