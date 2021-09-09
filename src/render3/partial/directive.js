b2bf1dd7b35bfd73199f622ab02f166a6b90e87d
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:36:09 +0000

    5fea50c070 docs: remove references to differential loading (#43060)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+1.sha-9e039ca.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+3.sha-5fea50c.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
