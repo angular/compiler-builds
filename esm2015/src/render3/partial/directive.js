846541b35cca31c65dea58ed4e563717fd36647c
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:55:31 +0000

    f57f0edd23 refactor(router): clean up unnecessary flag in `restoreHistory` function (#43382)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+8.sha-6c84c5f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+9.sha-f57f0ed.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
