7d1860e406410d87f15ae480b0b46848d7787c1f
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:40:28 +0000

    66f13dade5 build: update all non-major dependencies (#43274)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+4.sha-e064509.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+5.sha-66f13da.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
