b7b987b749f2e6412b11044fcaa82d4dd581bde5
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:49:46 +0000

    a0fe89919f docs: update the reviewed tag for what-is-angular topic (#43213)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+21.sha-4761842.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+22.sha-a0fe899.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
