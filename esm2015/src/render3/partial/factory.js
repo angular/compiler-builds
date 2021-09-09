d1ee8ea3ec9889d5827eefff076fd57bbce30ce1
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:43:01 +0000

    2658219f7a refactor(router): clean up unnecessary flag in `restoreHistory` function (#43382)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.4+20.sha-e726a63.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.5+4.sha-2658219.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
