0761aa82972d73bfcb0fde21482e3072f2bf0ef8
Joey Perrott <josephperrott@gmail.com>
2021-09-13 20:17:10 +0000

    de682ebb1f docs: add Dmytro Mezhenskyi to GDE resources (#43282)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+30.sha-6d8a711.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+31.sha-de682eb.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
