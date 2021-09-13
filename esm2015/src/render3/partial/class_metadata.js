0d352d8badd00039d00093e096bef54631808480
Andrew Kushnir <akushnir@google.com>
2021-09-13 21:54:31 +0000

    59353c6305 fix(service-worker): clear service worker cache in safety worker (#43324)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+31.sha-de682eb.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+32.sha-59353c6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
