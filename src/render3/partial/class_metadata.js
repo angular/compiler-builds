5cf7f845c2831ec6fc707e46f06ae6361a736618
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:30:22 +0000

    3ec9ee65b9 refactor(router): Remove TODO and slightly adjust eager browserUrlTree update (#43426)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+45.sha-71e4323.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+47.sha-3ec9ee6.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
