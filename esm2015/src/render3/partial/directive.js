5e13e2a3a24d4a5511d2e6901c045661343c2021
Andrew Kushnir <akushnir@google.com>
2021-09-14 15:55:53 +0000

    796da641f0 fix(router): Do not modify parts of URL excluded from with 'eager' updates (#43421)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+43.sha-e8e41ef.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+44.sha-796da64.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
