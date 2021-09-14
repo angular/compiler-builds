b176a9ea48a1cc71876b4cf3a022724cb28274e5
Andrew Kushnir <akushnir@google.com>
2021-09-14 18:30:58 +0000

    5163999fb9 test(router): Add tests to document expected behavior (#43449)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.5+47.sha-3ec9ee6.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.5+46.sha-5163999.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
