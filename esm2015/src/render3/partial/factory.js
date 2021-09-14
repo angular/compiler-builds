ca2724f6a6df69e48eb804d588c73f21bbf5d41b
Andrew Kushnir <akushnir@google.com>
2021-09-14 15:52:45 +0000

    e8e41efcea docs: add guidelines for localizing angular documentation (#43314)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+42.sha-bdefeb1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+43.sha-e8e41ef.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
