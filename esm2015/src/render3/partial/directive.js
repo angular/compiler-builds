bd7f5f149b09f3bdc3cdf885c1dc35d120a08d5a
GitHub <noreply@github.com>
2021-09-15 17:58:41 +0000

    5e9eb7abbd release: cut the v13.0.0-next.6 release (#43464)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.5+55.sha-bab963f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.5+56.sha-5e9eb7a.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
