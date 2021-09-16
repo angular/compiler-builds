23d500c014070b9d178643305d311d153d9abba7
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 16:18:46 +0000

    7efa47075c docs: remove http example dependency on 3rd party service (#43475)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.6+14.sha-e5d08c1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.6+19.sha-7efa470.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
