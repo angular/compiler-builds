002c49b9f7792776171e89bd85566c84d84d87ee
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 17:15:58 +0000

    2292a9cbd9 refactor(compiler): improve regular expression for stripping comments (#43456)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+19.sha-7efa470.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+20.sha-2292a9c.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+19.sha-7efa470.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+20.sha-2292a9c.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
