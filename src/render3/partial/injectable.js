953c931d5054520eaa635e43dec678d8ac635a8a
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:30:18 +0000

    33708f256d docs: Edits to remove jargon (#43017)

--- src/render3/partial/injectable.js
+++ src/render3/partial/injectable.js
@@ -46,7 +46,7 @@
     function createInjectableDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+34.sha-cabbff6.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+35.sha-33708f2.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         // Only generate providedIn property if it has a non-null value
