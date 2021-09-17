352f612c654b1286234972ecd99502d33ed98c95
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 19:06:19 +0000

    91183eb142 docs: reviewed tag added (#43492)

--- esm2015/src/render3/partial/class_metadata.js
+++ esm2015/src/render3/partial/class_metadata.js
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.6+36.sha-3d090ba.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.6+38.sha-91183eb.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
