bd5653ab3e538bcaaad100f5aca4202b397e9c2f
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:17:38 +0000

    cabbff6f92 docs: reviewed tag added (#43491)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+33.sha-928e1e2.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+34.sha-cabbff6.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
