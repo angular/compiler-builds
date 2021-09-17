22bf47e35d0a1e35001950cc7349313f680b8c37
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 01:52:21 +0000

    b040e78e0a docs: reviewed tag added (#43479)

--- src/render3/partial/factory.js
+++ src/render3/partial/factory.js
@@ -33,7 +33,7 @@
     function compileDeclareFactoryFunction(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+28.sha-7970082.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+29.sha-b040e78.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('deps', util_2.compileDependencies(meta.deps));
