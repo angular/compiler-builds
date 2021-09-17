6e7206bb70a01a5feea3ed843b8512d06cc4e850
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:55:09 +0000

    046b35f701 docs: review tag added (#43471)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+35.sha-33708f2.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+37.sha-046b35f.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
