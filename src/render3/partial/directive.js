0dfd886c6f8ff1b7ac7093aec010f4f9e2271c99
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 17:00:39 +0000

    312e421ff3 docs: Edits to remove jargon (#43045)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+30.sha-313ea92.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+31.sha-312e421.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
