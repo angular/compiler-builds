7c65f272bf2e6c0f2ea57cc8f09aa8164dc529f6
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 18:28:58 +0000

    4e30381afd docs: Edits to remove jargon (#43017)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.6+31.sha-5a888f1.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.6+34.sha-4e30381.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
