f2c5c758ffcce423325d315bb5963f894dbb9fa5
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-17 19:07:22 +0000

    91183eb142 docs: reviewed tag added (#43492)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.6+36.sha-3d090ba.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.6+38.sha-91183eb.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
