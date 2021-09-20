000b260c7fbbc118111b7fa14bc5b5833b74e80e
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 16:22:19 +0000

    020f0d3858 fix(docs-infra): improve aio contributors page responsiveness (#43355)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.6+37.sha-909c706.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.6+38.sha-020f0d3.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
