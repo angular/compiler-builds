831af33062c206510e2a425b79754bd9ca6a2b5e
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-20 23:10:17 +0000

    bba683bb68 docs: Edits to remove jargon (#42918)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.6+46.sha-6ba6bdf.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.6+47.sha-bba683b.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
