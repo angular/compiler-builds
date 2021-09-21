7f4a42a7369533dc4e61a5a9340c65a379aca7f9
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-21 16:32:33 +0000

    d1baf7cdfb fix(docs-infra): add a "skip to content" link for better accessibility (#43460)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.6+48.sha-7d6ba72.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.6+53.sha-d1baf7c.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
