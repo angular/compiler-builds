34d27e5fb59feecbbb9b798d9bb641880be00505
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:37:37 +0000

    6ff0eef35a docs(http): get rid of not needed space at the end of the sentence (#43528)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.6+59.sha-ce406e3.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.6+61.sha-6ff0eef.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
