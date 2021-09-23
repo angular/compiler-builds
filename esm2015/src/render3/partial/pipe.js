0e956768d77bd0e9585ab0f5489af55174fad274
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:05:45 +0000

    cc427d18c4 docs(router): fix typo in router event API documentation (#43555)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.7+8.sha-6fdfab3.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.7+10.sha-cc427d1.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
