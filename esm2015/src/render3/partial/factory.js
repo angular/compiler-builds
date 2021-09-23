3df4a226072d13c02b3dbcf801afa730b02b0e96
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-23 22:04:53 +0000

    2306a3250f docs(router): fix typo in router event API documentation (#43555)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.6+58.sha-dad213e.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.7+9.sha-2306a32.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
