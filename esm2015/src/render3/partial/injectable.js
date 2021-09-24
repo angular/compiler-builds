4c4c782e74f26afcd942208fd98b7ceb2118c381
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 17:57:37 +0000

    d9d8f950e9 feat(forms): allow disabling min/max validators dynamically (by setting the value to `null`) (#42978)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.7+11.sha-ea61ec2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.7+29.sha-d9d8f95.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
