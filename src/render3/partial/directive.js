0379b887d0c1cee8d45c13dcbe7eb23adfc81959
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 17:56:32 +0000

    d9d8f950e9 feat(forms): allow disabling min/max validators dynamically (by setting the value to `null`) (#42978)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.7+11.sha-ea61ec2.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.7+29.sha-d9d8f95.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
