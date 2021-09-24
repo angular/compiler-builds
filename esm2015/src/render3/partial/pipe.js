cd751e1968e9391230fe14259f652adbcf0ffa2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:14:13 +0000

    e131540f71 fix(service-worker): do not unassign clients from a broken version (#43518)

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.7+36.sha-67ff93d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.7+38.sha-e131540.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
