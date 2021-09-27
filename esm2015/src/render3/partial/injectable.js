50342f876310f6c25c1e38f1412f708b47c2d1d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 21:41:42 +0000

    988cca7ef5 fix(ngcc): do not fail for packages which correspond with `Object` members (#43589)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+8.sha-bdf5887.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+10.sha-988cca7.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
