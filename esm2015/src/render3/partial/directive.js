228172d9d638d51b3bb6789cdbb2adb92be0a26e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 17:48:12 +0000

    22eebbafbc release: cut the v13.0.0-next.8 release (#43613)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.7+39.sha-d02c8da.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.7+41.sha-22eebba.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
