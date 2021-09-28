2155540d6d86f09e8c5cc1bf788ad60288d3eeb0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 22:03:26 +0000

    4555d8a316 docs: fix broken link to measure performance (#43628)

--- esm2015/src/render3/partial/injector.js
+++ esm2015/src/render3/partial/injector.js
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+17.sha-cb0d8d2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+18.sha-4555d8a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
