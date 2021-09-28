d76bbc201fdc9f80d0c12de514267c388f714af7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:37:50 +0000

    8d2b6affcd fix(compiler-cli): correctly interpret token arrays in @Injectable `deps` (#43226)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+11.sha-de662da.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+13.sha-8d2b6af.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
