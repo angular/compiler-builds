b06de4cbc590c9e9ff87ebf4a1cbdc3a4e23cdf0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:38:02 +0000

    8d34f86edb docs: deprecate `PlatformRef.bootstrapModuleFactory` and factory-based signature of `ApplicationRef.bootstrap` (#43560)

--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.8+14.sha-4f3beff.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.8+15.sha-8d34f86.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
