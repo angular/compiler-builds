b4ae5610aa0e377b0ba01ee51b949f951544a8c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 16:09:42 +0000

    c008e0fa90 refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+18.sha-4555d8a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+21.sha-c008e0f.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
