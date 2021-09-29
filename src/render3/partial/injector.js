1ea24fe4d0391e06664fbd1b3b64890e17e5b63b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:19:46 +0000

    e0a72857cc fix(bazel): construct a manifest file even when warnings are emitted (#43582)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.8+23.sha-1027780.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.8+25.sha-e0a7285.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
