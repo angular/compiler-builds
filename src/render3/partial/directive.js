4dc450b0ed6214d2341725f380b2ba9e21a3fab1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 21:11:10 +0000

    7dccbdd27b feat(core): add support for Types in ViewContainerRef.createComponent (#43022)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.8+30.sha-ab3de40.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.8+31.sha-7dccbdd.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
