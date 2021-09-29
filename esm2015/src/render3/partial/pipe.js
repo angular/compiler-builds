5cbe2b07d70cbd9984ae67603fa6c2f370f1663b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:58:15 +0000

    ab3de40ba3 Revert "refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)"

--- esm2015/src/render3/partial/pipe.js
+++ esm2015/src/render3/partial/pipe.js
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+29.sha-09d325a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+30.sha-ab3de40.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
