9b6e552c2aa891ac1494b38bd31f942c8667bef2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 16:08:35 +0000

    c008e0fa90 refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)

--- esm2015/src/render3/partial/factory.js
+++ esm2015/src/render3/partial/factory.js
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+18.sha-4555d8a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+21.sha-c008e0f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
