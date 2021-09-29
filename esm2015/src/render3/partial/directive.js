478a29487bf1cef7787708ddd1f9b37a7802b608
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:20:58 +0000

    6a3492b751 docs: fix typo in observables file (#43630) PR Close #43630

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+26.sha-d158960.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+27.sha-6a3492b.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
