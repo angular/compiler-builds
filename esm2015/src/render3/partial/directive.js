3267ac3960596542b69a76ae2f35e44b11db4e2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:19:58 +0000

    d1589604a8 build(docs-infra): ensure all overloads are shown in interfaces (#43614)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.8+25.sha-e0a7285.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.8+26.sha-d158960.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
