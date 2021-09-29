9e8157a9ff11ba5ce4f2c289fb356b0f4c8ae0c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:35:29 +0000

    09d325a9e6 test(router): refactor tests to not use deprecated loadChildren (#43578)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.8+27.sha-6a3492b.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.8+29.sha-09d325a.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
