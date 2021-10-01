c8fd09b33b5bc64f5a2852635134bde403ec21bb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-01 16:07:26 +0000

    544cb43eed docs: fix missing step in get started example (#43646)

--- src/render3/partial/class_metadata.js
+++ src/render3/partial/class_metadata.js
@@ -31,7 +31,7 @@
     function compileDeclareClassMetadata(metadata) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('13.0.0-next.9+1.sha-6e7454d.with-local-changes'));
+        definitionMap.set('version', o.literal('13.0.0-next.9+3.sha-544cb43.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', metadata.type);
         definitionMap.set('decorators', metadata.decorators);
