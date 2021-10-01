5ac37e7409bea610ff422fe78888abb902f29b9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-01 16:16:12 +0000

    7aea5256de docs: update commit message guidelines to reflect current requirements (#43650)

--- esm2015/src/render3/partial/ng_module.js
+++ esm2015/src/render3/partial/ng_module.js
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.9+3.sha-544cb43.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.9+4.sha-7aea525.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
