a886a882424b00cd0369c00aa461c90333ecaa05
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 17:47:21 +0000

    f513b1773e refactor(router): add stub files for g3 patch of NgModuleFactoryLoader (#43660)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.10+2.sha-d04b550.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.10+3.sha-f513b17.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
