654f3d2ffcd2d9a2eb8e829c1db4d1e942927d28
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:11:55 +0000

    16f504b2cf docs: add balram chavan to GDE resources (#43658)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.10+4.sha-0d43148.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.10+6.sha-16f504b.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
