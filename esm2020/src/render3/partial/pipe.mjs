dcac136d8778e9849b4386173c71ef1eae657bd1
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:13:04 +0000

    2cc5cc9d1f build(docs-infra): upgrade cli command docs sources to afd86f0cd (#43678)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.10+6.sha-16f504b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.10+5.sha-2cc5cc9.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
