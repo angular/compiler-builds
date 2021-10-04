68a3756943f941f6a70b8f1654643f6239f86759
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 18:14:36 +0000

    e5c3017df3 docs: move angular-compiler-options tsconfig snippets to external file (#43545)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.10+7.sha-dbfb2d2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.10+8.sha-e5c3017.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
