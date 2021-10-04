392eb607004ac6d4b197fcb8f9f78ecbf1170dfb
Dylan Hunn <dylhunn@gmail.com>
2021-10-04 17:45:46 +0000

    d04b550f2e docs: Deprecate longhand binding prefixes (#43671)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.10+1.sha-b64f796.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.10+2.sha-d04b550.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
