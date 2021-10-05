d20f6fda5c9812f889c7238ce9dc3efcd240d7aa
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 17:06:55 +0000

    318cf9144e test(core): fix failing unit test (#43714)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.11+3.sha-bdc6ff4.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.11+6.sha-318cf91.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
