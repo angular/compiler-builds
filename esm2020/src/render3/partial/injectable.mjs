33892635d9f41fe5a009d7a4b38ce9ae33740c35
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 17:23:30 +0000

    23ac3f7cef ci: test AIO/docs examples against the latest RxJS 7.x version (v7.3.1) (#43683)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.12+5.sha-52587e5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.12+9.sha-23ac3f7.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
