11540da2811d9e4f11e100f8e94b60e0fd1efdd0
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 17:24:44 +0000

    841c5aebbf build(docs-infra): ensure `ngsw.json` generation is correct and deterministic (#43686)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.12+9.sha-23ac3f7.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.12+14.sha-841c5ae.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.12+9.sha-23ac3f7.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.12+14.sha-841c5ae.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
