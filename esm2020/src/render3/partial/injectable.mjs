444cb7a6f7f8ced2cfb8a9e90799fffa9057ab51
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 22:45:22 +0000

    04fec60a61 ci: ignore "remark*" packages from automatic updates (#43735)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.12+17.sha-5d7229e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.13+3.sha-04fec60.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
