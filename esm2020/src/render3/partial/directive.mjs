89226c3cf3366b4fb5eab2392221ad3db2cd94f8
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 23:16:08 +0000

    48aa0039ea refactor(compiler-cli): update peerDependencies to require rxjs ^7.4.0 (#43748)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.13+3.sha-04fec60.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.13+5.sha-48aa003.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
