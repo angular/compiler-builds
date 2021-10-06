36f0a658437f6f8b463cdf56193636dfae0b8b8c
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:41:40 +0000

    fa6aa1be55 build(docs-infra): ensure all overloads are shown in interfaces (#43734)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.12+3.sha-7a971ca.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.12+2.sha-fa6aa1b.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
