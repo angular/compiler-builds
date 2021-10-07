7c5c2c3ec37b7e72a123aca05fbab562d50381ef
Andrew Scott <atscott01@gmail.com>
2021-10-07 21:14:43 +0000

    5b1b2fd2f9 build: update dependency google-closure-compiler to v20211006 (#43754)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+2.sha-e0667ef.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+5.sha-5b1b2fd.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
