9083e18586aa5f9983f2d955fafc08d461fed186
Joey Perrott <josephperrott@gmail.com>
2021-10-08 00:08:20 +0000

    4eedf77fd0 docs: deprecate ViewEngine-based `renderModuleFactory` (#43757)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+5.sha-5b1b2fd.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+7.sha-4eedf77.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
