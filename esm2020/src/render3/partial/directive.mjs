60a6e7dfaf07a0f642eea6a873a5c19fdbaf6fe6
Andrew Scott <atscott01@gmail.com>
2021-10-08 20:34:58 +0000

    bbb91aae91 build: update to latest @angular/dev-infra-private (#43785)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+7.sha-4eedf77.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+8.sha-bbb91aa.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
