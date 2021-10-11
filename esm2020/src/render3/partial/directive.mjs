9fb0da3ba527f0b4ce87dc706c59b75546fa39f2
Andrew Scott <atscott01@gmail.com>
2021-10-11 16:59:40 +0000

    8b2971417a docs: add missing declaration of `CartComponent` in tutorial (#43774)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+17.sha-4c00e92.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+19.sha-8b29714.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
