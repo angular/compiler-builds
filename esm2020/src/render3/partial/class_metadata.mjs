e6c9d55be3d2a941b696d8cba70640ce2bb86841
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:06:15 +0000

    325c6e4e7a docs: update CHANGELOG.md readme to include entry separators (#43799)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+35.sha-8671055.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+37.sha-325c6e4.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
