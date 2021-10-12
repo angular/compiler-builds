d5b0f30667de4f52e76a7dd6418fad5b374d95c0
Andrew Scott <atscott01@gmail.com>
2021-10-12 18:00:50 +0000

    17f5299967 ci: use Node.js browsers image (#43810)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+23.sha-08caead.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+30.sha-17f5299.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
