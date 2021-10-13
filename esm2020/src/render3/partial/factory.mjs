de70d86d5dc2f201f6ba45c7ce0c1ab39efde33c
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:17:47 +0000

    6b85519a80 ci: remove rollup from renovate ignoreDeps (#43737)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+43.sha-b5da715.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+47.sha-6b85519.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
