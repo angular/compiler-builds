30a607b47675b109a10ba0df20d200e787d493f9
Andrew Scott <atscott01@gmail.com>
2021-10-13 19:06:00 +0000

    6113813300 docs: add missing split marker (#43830)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.15+42.sha-b5eccde.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.0+1.sha-6113813.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
