517279f2c7ab5d921d7fb951cef7c7396314c3c4
Andrew Kushnir <akushnir@google.com>
2021-10-14 17:18:30 +0000

    7ff6f7ff70 docs: add natalia venditto to GDE resources (#43825)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+50.sha-9591310.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+51.sha-7ff6f7f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
