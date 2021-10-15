f798765dbfa07e7c5b481204c9190d29af605c00
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:57:21 +0000

    e79e3cafec fix(router): fix Router's public API for canceledNavigationResolution (#43842)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+65.sha-9a6bd26.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+64.sha-e79e3ca.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
