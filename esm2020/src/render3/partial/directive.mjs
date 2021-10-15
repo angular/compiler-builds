59f89d794f8bd52b358700e24af83ada391a7216
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:53:12 +0000

    70c14ed66d build(docs-infra): add a box around CLI command line reference syntax (#43850)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.0+11.sha-426a3ec.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.0+14.sha-70c14ed.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
