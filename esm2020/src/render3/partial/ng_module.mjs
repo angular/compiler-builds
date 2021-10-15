8ea2576a3427c7cdc1c61841e04f3411fb389c0a
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:52:31 +0000

    9a6bd264ab build(docs-infra): add a box around CLI command line reference syntax (#43850)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+63.sha-e78c7cc.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+65.sha-9a6bd26.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
