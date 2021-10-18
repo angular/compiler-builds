1036ef55e5fbd57738e5c9f48012f9d0d1ddde49
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:40:30 +0000

    b489ae5ecd ci: remove browserstack configuration (#43881)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.0+25.sha-fc6519c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.0+27.sha-b489ae5.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
