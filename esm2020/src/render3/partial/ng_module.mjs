a0c160140a2e153ee97e5874acfeeeb259a06fad
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:41:39 +0000

    c78f1cd34e docs: move deprecations example snippets to external file (#43700)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+97.sha-b8f1f82.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+98.sha-c78f1cd.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
