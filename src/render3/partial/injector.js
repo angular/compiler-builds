775fe609f8be87b5d516e9070a46bad8d429313b
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 16:45:49 +0000

    cef01b804a ci: update `angular/dev-infra/github-actions` sha (#43904)

--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.11+1.sha-e09556f.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.11+3.sha-cef01b8.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
