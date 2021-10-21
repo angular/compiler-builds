36068b60fc3f3e3c6bc69941a6dac85ec15dd8cd
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 18:31:53 +0000

    72f6877cb0 docs: remove Ivy-specific guides (#43860)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.1+7.sha-adc68b1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.1+8.sha-72f6877.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
