6bded0128728f847d9e0788b505249146c6ad864
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 15:26:00 +0000

    22009f62df docs: fix typos in the workspace config guide (#43918)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.1+3.sha-0039b71.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.1+4.sha-22009f6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
