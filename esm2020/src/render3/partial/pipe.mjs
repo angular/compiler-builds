262aaf5f3cda6f6e618e4913b8c7c67e50d4fc96
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:15:04 +0000

    6e37c938e2 fix(ngcc): support alternate UMD layout when adding new imports (#43931)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+112.sha-786f02b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+113.sha-6e37c93.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
