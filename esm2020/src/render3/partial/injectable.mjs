905b9a82493ed0fe3ac035fe864cd07a3bcda1e9
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:24:13 +0000

    940eafa351 docs(localize): fix spelling of documentation link (#43941)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.1+18.sha-087fae5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.1+20.sha-940eafa.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
