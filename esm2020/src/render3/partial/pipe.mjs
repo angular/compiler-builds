909cb9319cc9a12d66fbf741b4738612c62a1966
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 19:48:33 +0000

    0b4350c6ef refactor(migrations): remove no longer accessible migrations (#43942)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+114.sha-bf1a2fc.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+115.sha-0b4350c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
