366ee6044355121e8d9f20c0b12315071f2cd36e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:12:12 +0000

    be564e6291 docs: deprecate `@angular-devkit/build-optimizer` (#43915)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+107.sha-b1c0286.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+111.sha-be564e6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
