31c36aa1f212eadb151a41a8c251923e81e49bbe
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 19:48:22 +0000

    28a40f3396 refactor(compiler-cli): extract error documentation base url into separate file (#43527)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+118.sha-a91fe6d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+120.sha-28a40f3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
