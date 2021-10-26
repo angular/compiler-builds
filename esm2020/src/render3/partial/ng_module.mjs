22847447b0c2c92af19f0f2b84cad5f5227d8e94
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 23:34:56 +0000

    0dbe1c695a docs: reviewed tag added (#43488)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.1+28.sha-081a4c2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.1+29.sha-0dbe1c6.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
