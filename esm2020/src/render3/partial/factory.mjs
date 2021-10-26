af77972e4c4269247025535ca4cf6da21c5fd883
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:42:19 +0000

    5c22dfe861 refactor(forms): update minlength/maxlength to inherit abstractValidator (#43835)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+123.sha-5d66b4a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+124.sha-5c22dfe.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
