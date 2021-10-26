83f5620cb362e8e96290c4d95f6237f3eaeede49
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 20:37:12 +0000

    5d66b4aee6 ci: update commit-message-based-labels job to be called labels (#43944)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+122.sha-143bb9f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+123.sha-5d66b4a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
