d6ed60eb4c576b75c0e118b12055ed0a69991ca4
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-26 22:33:57 +0000

    8fcfd87d99 ci: update commit-message-based-labels job to be called labels (#43944)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.11+12.sha-bf59401.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.11+14.sha-8fcfd87.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/pipe.js
+++ src/render3/partial/pipe.js
@@ -45,7 +45,7 @@
     function createPipeDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.11+12.sha-bf59401.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.11+14.sha-8fcfd87.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         // e.g. `type: MyPipe`
         definitionMap.set('type', meta.internalType);
