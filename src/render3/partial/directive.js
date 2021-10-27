fad0a4096cea8420a3c1d84dc74ec8067461c2fa
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:36:44 +0000

    515949e7e6 docs: review tag added (#43465)

--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -47,7 +47,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.11+14.sha-8fcfd87.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.11+20.sha-515949e.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
--- src/render3/partial/injector.js
+++ src/render3/partial/injector.js
@@ -42,7 +42,7 @@
     function createInjectorDefinitionMap(meta) {
         var definitionMap = new util_1.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.11+14.sha-8fcfd87.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.11+20.sha-515949e.with-local-changes'));
         definitionMap.set('ngImport', o.importExpr(r3_identifiers_1.Identifiers.core));
         definitionMap.set('type', meta.internalType);
         definitionMap.set('providers', meta.providers);
