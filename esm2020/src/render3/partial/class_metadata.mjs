9a21e852fb8537233498c505d0796b060af11b68
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-28 18:38:52 +0000

    65cb2c5565 fix(core): support `InjectFlags` argument in `NodeInjector.get()` (#41592)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+135.sha-a84983e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+139.sha-65cb2c5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
