6c17e1b6cf685119156de09791afad6773bfa750
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-28 18:53:30 +0000

    f225bfde80 docs(common): describe behavior when reference changes (#42863) PR Close #42863

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+1.sha-8d89b28.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
