4eaf30d70ad6ac86b4691464145c44cf184023c1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 22:56:22 +0000

    967283e8a1 docs: update dynamic component guide to use factory-less ViewContainerRef.createComponent (#43895)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -33,7 +33,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+163.sha-8d8cdc1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+164.sha-967283e.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
