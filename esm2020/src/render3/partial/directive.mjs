ff740d12b672aa19044d558585af8b921286575b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 21:42:09 +0000

    96b99c443b docs: update `creating libraries` guide (#43982)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+9.sha-3faef71.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+10.sha-96b99c4.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
