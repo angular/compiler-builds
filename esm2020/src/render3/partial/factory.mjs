e7f054de31691f37f7985a75dfcc4767c87a9ed0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:02:57 +0000

    a01e02d4e2 refactor(docs-infra): remove obsolete way of disabling `DocViewer` animations (#43977)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+164.sha-967283e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+171.sha-a01e02d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
