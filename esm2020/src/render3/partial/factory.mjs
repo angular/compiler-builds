c6a7ba1dee5356a4fa93666f86e9cd3fc04ca803
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 20:31:04 +0000

    3faef7180d refactor(docs-infra): align docs examples with default CLI TS config (#43880)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+5.sha-f225bfd.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+9.sha-3faef71.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
