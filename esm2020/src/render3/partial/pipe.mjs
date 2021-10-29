85230b513a1eb581fe8bf6216739bdc1586d79f1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:06:16 +0000

    6247efa81b test: remove `ng_elements_schematics` integration test (#43975)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+172.sha-a72333c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+169.sha-6247efa.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
