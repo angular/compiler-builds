1da69218bd5555cda972300b51140b3a9c495582
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-29 23:06:05 +0000

    a72333c714 refactor(core): add `ViewRef` to private exports (#43978)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+175.sha-408e4da.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+172.sha-a72333c.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
