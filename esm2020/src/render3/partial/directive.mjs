d60ba781200a5c81d246619e1f40d2d64a0be893
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-10-30 00:09:05 +0000

    3d1ac6a009 docs: update left-nav (#43964)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+169.sha-6247efa.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+185.sha-3d1ac6a.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
