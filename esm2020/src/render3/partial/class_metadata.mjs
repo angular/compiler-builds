955dac71dc2a851b990ba96e5e62ed865584a17c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 02:10:06 +0000

    d24cbcea70 release: cut the v13.0.0-rc.3 release (#44011)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+54.sha-4fd388b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+55.sha-d24cbce.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
