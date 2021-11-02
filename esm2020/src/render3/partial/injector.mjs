04a538c9948591146a544ea58db34d34f3a1abf7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 00:57:28 +0000

    74c497289f docs: update per feedback (#44009)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+52.sha-892ded5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+53.sha-74c4972.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
