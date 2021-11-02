c1a3dd40932a039052fac4146c8297a0ca317999
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-02 01:42:18 +0000

    4fd388b084 docs: initial commit of Angular Package Format documentation (#43987)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.2+53.sha-74c4972.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-rc.2+54.sha-4fd388b.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
