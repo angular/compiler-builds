590ea340d5fee23da1031b278cf74936b17f8b2d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 23:39:32 +0000

    b361242a13 build(docs-infra): target webcontainers for stackblitz "live-example" links (#44041)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-rc.3+3.sha-b6f2a55.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0+1.sha-b361242.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
