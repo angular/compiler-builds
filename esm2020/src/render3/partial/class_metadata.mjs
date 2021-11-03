f0425ce7355e78c98190d82cea64ace046374c29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 20:57:00 +0000

    1cbe864d12 docs: release notes for the v12.2.13 release (#44039)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0-next.14+198.sha-1a1dfab.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.0-next.14+199.sha-1cbe864.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
