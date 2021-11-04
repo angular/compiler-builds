ac47f691397db31cec35e86cd2c1185242154d12
Andrew Scott <atscott01@gmail.com>
2021-11-04 19:14:14 +0000

    ec7dd5d4c2 test: re-enable platform-server integration test (#44055)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+18.sha-7441747.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.0+19.sha-ec7dd5d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
