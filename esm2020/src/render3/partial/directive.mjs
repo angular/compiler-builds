601004a39759eb11f8bd7828fafd727f6b7458de
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:49:30 +0000

    64af4b9c1b docs: fix typo in the CHANGELOG (#44058)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+28.sha-3507279.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.0+29.sha-64af4b9.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
