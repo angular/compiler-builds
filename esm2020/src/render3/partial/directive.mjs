1602c311b1e2150cb6f23f0bbfa7b08dc7d35f44
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:33:45 +0000

    baab456e56 docs(animations): update developers on state of package (#44014)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -34,7 +34,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+42.sha-acddb7f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.0+46.sha-baab456.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
