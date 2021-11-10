e71eeda514608fe7beeee3bc013cd087feba4bd7
Andrew Scott <atscott01@gmail.com>
2021-11-10 19:46:39 +0000

    790dfe5e25 docs: update review tag to topic (#44008)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+74.sha-393efa5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.0+75.sha-790dfe5.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
