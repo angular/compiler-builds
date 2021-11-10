693cfef66d7c0af343964b28b44113bb0131d3ac
Andrew Scott <atscott01@gmail.com>
2021-11-10 20:57:53 +0000

    4809bbd83c build: update angular (#44074)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+75.sha-790dfe5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.0+76.sha-4809bbd.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
