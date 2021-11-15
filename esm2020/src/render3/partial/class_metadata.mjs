ea1cfafebab593125394b4de01038f1dfb013f3d
Andrew Kushnir <akushnir@google.com>
2021-11-15 19:36:03 +0000

    44890437ae build: update to yarn v1.22.17 (#44150)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.0+81.sha-f817e30.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.1+1.sha-4489043.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
