b06cb0afe8087d9df9ed31a7a42d01f3b4140ef2
Andrew Kushnir <akushnir@google.com>
2021-11-16 03:19:10 +0000

    7115e838fa ci: remove `cla: yes` from required labels (#44179)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.0+78.sha-ca0873f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.1+3.sha-7115e83.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
