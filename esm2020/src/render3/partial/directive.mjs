41ef764422b14d22755ded2e117895c899301ed1
Andrew Kushnir <akushnir@google.com>
2021-11-16 20:24:18 +0000

    5a24f96792 refactor(elements): remove non-existent schematic collection breaking with `ng add` (#44187)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.1+6.sha-00ae55c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.1+8.sha-5a24f96.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
