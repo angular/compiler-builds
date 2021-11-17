5494c4931f28d0497cf35908fdded4756f6bc55e
GitHub <noreply@github.com>
2021-11-17 18:06:29 +0000

    90a4ae1dd7 release: cut the v13.1.0-next.2 release (#44201)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.1+12.sha-6c618de.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.1+14.sha-90a4ae1.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
