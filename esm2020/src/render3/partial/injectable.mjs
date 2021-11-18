5c2aa2ba0a4b42107f90d5aaf0aa31ce0c6acbab
Andrew Kushnir <akushnir@google.com>
2021-11-18 18:38:18 +0000

    88c656487d docs: remove tslint references from file structure document (guide/file-structure) (#44202)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+10.sha-8330520.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+11.sha-88c6564.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
