6050e86f0fefa08469e04bf93fc9eb41f636374f
Andrew Kushnir <akushnir@google.com>
2021-11-18 18:41:00 +0000

    213105c542 ci: add dylhunn to review groups for forms. (#44204)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+11.sha-88c6564.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+12.sha-213105c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
