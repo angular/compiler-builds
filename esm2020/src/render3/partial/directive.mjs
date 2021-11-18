5050443abc87ff3a4ad98084c640955997ca2f36
Andrew Kushnir <akushnir@google.com>
2021-11-18 19:05:15 +0000

    16d026e499 ci: add dylhunn to review groups for forms. (#44204)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+5.sha-96fedd2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+10.sha-16d026e.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
