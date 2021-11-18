e849b2f5fababa0758da4875a12f5e61d0364f80
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-18 21:31:27 +0000

    427822e6c3 refactor(animations): improve some animations comments (#44203)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+10.sha-16d026e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+11.sha-427822e.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
