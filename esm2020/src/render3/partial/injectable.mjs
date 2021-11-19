9202f9785feb1fc453b95fffebb1568ebce47c76
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 22:45:18 +0000

    3289aae620 refactor(bazel): always add `strictTemplates` option to `tsconfig.json` (#43674)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+19.sha-6e351ed.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+21.sha-3289aae.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
