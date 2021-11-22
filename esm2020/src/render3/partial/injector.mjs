5ecc44a71908939461098943e4473c6fcc2da9e7
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-22 22:00:29 +0000

    d9c99f7d44 build: update dependency @angular-eslint/eslint-plugin-template to v13 (#44224)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+26.sha-6f5c0c1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+27.sha-d9c99f7.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
