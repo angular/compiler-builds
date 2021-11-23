79c4e6c9de30e54b2732dbc66ec4cf6cbc9ca65f
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 21:27:34 +0000

    8c71b9fc42 refactor: delete the View Engine runtime (#43884)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+36.sha-f44cb57.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+39.sha-8c71b9f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
