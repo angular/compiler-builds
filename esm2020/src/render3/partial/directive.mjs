c7c7130469b0fa92363196bf2926c523d0d9eaf9
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:44:21 +0000

    5119be4e7c refactor(ngcc): remove Ivy switch marker transform (#43891)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+38.sha-b9362f0.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+47.sha-5119be4.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
