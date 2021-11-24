6a25db716c490444c528ad194cbb31e245d69268
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:15:14 +0000

    83bbbd3784 docs: document `ng update` support guarantees (#44142)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+50.sha-b06e398.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+49.sha-83bbbd3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
