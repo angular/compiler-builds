5558e734e15e1f6235d05fc5d7126a2f4b7db9a6
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:20:38 +0000

    2bf131a8a0 fix(docs-infra): move aio footer inside mat-sidenav-container (#44236)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+40.sha-414d5fe.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+43.sha-2bf131a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
