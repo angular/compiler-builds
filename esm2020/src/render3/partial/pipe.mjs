be415b75dd5b02dc3d8fc1fb3843fcf508e2bfd3
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:20:27 +0000

    414d5fef7f ci: add jessicajaniuk to pullapprove groups (#44255)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+39.sha-8c71b9f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+40.sha-414d5fe.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
