8f8dccf544d77a7aacf49512fa27c7c5435d94e1
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 19:14:24 +0000

    b06e3981bc fix(core): add missing info about a component in the "pipe could not be found" error message (#44081)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+44.sha-6ae3858.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+50.sha-b06e398.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
