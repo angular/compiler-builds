f94b253a1739a0666f2421558bcf5fa96277707b
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:34 +0000

    ef140a76d9 fix(docs-infra): fix the main content layout (#44282)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+66.sha-708eb8c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+65.sha-ef140a7.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
