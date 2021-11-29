71c18d11a67939c016a08141d48e4d9c73e88943
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:39:42 +0000

    3aafa765ee fix(language-service): Correctly parse inputs and selectors with dollar signs (#44268)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+65.sha-ef140a7.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+67.sha-3aafa76.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
