cef2ae24c19ddb4688cb0db699efff21ddf2c7e5
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:29 +0000

    708eb8c8d6 docs: fix instructions for debugging a karma test (#44244)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+56.sha-47f1c0f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+66.sha-708eb8c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+56.sha-47f1c0f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+66.sha-708eb8c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
