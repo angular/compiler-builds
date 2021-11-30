ace14e29e477685fe745885911adb9adbd40d3a4
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:39:32 +0000

    960068899c build: fix lint errors in build-saucelabs legacy script (#44311)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+75.sha-d56e3f4.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+76.sha-9600688.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
