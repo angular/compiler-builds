9d2666c051baf58fb34a144b0dc32c76e6b39674
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:35:31 +0000

    96dfc7af7c refactor(core): make the error messages tree shakable (#44219)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+80.sha-f058109.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+82.sha-96dfc7a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
