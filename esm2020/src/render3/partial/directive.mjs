4d21dea17cab409b30a95bcfe78cc9a66a76720b
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 21:58:41 +0000

    98430d678e refactor(forms): minor refactoring of `SelectMultipleControlValueAccessor` (#44261)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+76.sha-53bdf11.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+78.sha-98430d6.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
