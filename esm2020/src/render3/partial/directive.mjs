cc5fb6544a2ae5a86ccd66649d28a65f29307faa
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 22:02:23 +0000

    3f6d8f38de refactor(forms): minor refactoring of `SelectMultipleControlValueAccessor` (#44261)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+85.sha-5124c06.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+86.sha-3f6d8f3.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+85.sha-5124c06.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+86.sha-3f6d8f3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
