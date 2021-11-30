c2b2eafc02ce92c3ee2ff92bfa4c231ed083c487
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:06:23 +0000

    95ed446009 refactor(docs-infra): clean up entryComponents usages (#44257)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.2+78.sha-5dff077.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.2+79.sha-95ed446.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
