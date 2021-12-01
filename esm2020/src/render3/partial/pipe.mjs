17e65c01c5afcfde64dc9ac6852895e7581afbc4
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 23:38:16 +0000

    078940150e fix(docs-infra): prevent automatic linking of 'json' (#44294)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+82.sha-257a30d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.3+1.sha-0789401.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
