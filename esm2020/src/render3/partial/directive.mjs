732ed8d5970b78e57659abb3669799a017bd8063
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 21:21:43 +0000

    257a30d7b3 release: cut the v13.0.3 release (#44332)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.2+81.sha-0c9d33c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.2+82.sha-257a30d.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
