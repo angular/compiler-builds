1fd7953ad08ccd5dfe22a2cb83fdd45d4ec1b5b6
Andrew Kushnir <akushnir@google.com>
2021-12-02 20:08:33 +0000

    51093690bf build(docs-infra): update `.browserslistrc` to align with new CLI apps (#44283)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+4.sha-1336297.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+5.sha-5109369.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
