afbe8a9ce50db42374ac5d9e0105cf8852fdca9d
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:42:34 +0000

    7fb4309cca perf(common): code size reduction of `ngFor` directive (#44315)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.3+3.sha-5624dc6.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.3+4.sha-7fb4309.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
