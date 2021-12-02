727e45d62bf9c1eac91f05093371ffdf90fc50d2
Andrew Kushnir <akushnir@google.com>
2021-12-02 20:11:14 +0000

    a8f96e8aaa build(docs-infra): update `.browserslistrc` to align with new CLI apps (#44352)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.3+4.sha-7fb4309.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.3+5.sha-a8f96e8.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
