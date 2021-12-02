8195e07d6ceb18097ad2496da26bad1768f5f9f9
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:11:00 +0000

    99d85cba8e ci: update pullapprove groups (#44340)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+2.sha-360867b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+3.sha-99d85cb.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
