98e11f40b5aef8d21500ad60529dae2186005aee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:05:57 +0000

    e8e21ac9d0 build: update dependency jsdom to v19 (#44362)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.0.3+5.sha-a8f96e8.with-local-changes'));
+    definitionMap.set('version', o.literal('13.0.3+7.sha-e8e21ac.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
