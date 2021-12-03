d352edfe6b7cc4c603fed2ba85ca90a337617795
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-03 17:04:45 +0000

    927bfb9e7b build: update dependency eslint to v8 (#43789)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+5.sha-5109369.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+6.sha-927bfb9.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
