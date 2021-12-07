6522e2e679369fd9f8772dd4d296f126bff173c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-07 13:52:09 +0000

    b6554d75cd fix(ngcc): correctly resolve UMD dependencies (#44382)

--- esm2015/src/render3/partial/directive.js
+++ esm2015/src/render3/partial/directive.js
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.13+10.sha-4cf1a81.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.14+1.sha-b6554d7.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
