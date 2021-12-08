787e69ab40aa0ed1588fb00d645fe85866962af0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:38:54 +0000

    80ab604fe0 docs(localize): fix typo in i18n-overview.md (#44389)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+10.sha-4126591.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+14.sha-80ab604.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
