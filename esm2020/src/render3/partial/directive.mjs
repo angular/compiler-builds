d1491a7acaa70730e2a240a449948714305bf831
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-09 17:30:27 +0000

    532d7d436f docs: release notes for the v13.1.0 release (#44419)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+24.sha-cab7a84.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+25.sha-532d7d4.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+24.sha-cab7a84.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0-next.3+25.sha-532d7d4.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
