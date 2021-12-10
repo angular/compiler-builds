78ebb77cf90db33b74e326f1650c2baf87440e18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:46:47 +0000

    944b6705f8 build: remove unused starlark file that is no longer used (#44430)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0-next.3+28.sha-a7c117a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+2.sha-944b670.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
