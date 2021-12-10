60cbfe4953e35e07d286d1359d6dc3c1c18c6e85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:55:22 +0000

    7b6ca4c5d2 build: update angular (#44273)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+3.sha-9249506.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+4.sha-7b6ca4c.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
