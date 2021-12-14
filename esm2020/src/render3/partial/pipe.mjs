e88667eb23e04f203bbcee882e92ac54463a5ead
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:56 +0000

    f394831ec4 refactor(animations): remove the unused enter and leave selectors (#44460)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+20.sha-d640fa7.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+19.sha-f394831.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
