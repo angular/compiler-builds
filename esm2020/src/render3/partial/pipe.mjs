554ee7c3f5fa10e1c83e489f7e0361ca3ddac495
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:16 +0000

    e4839939b0 test(ngcc): don't use the workspace typescript version to generate integration test fixtures (#44448)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+12.sha-12f7c85.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+14.sha-e483993.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
