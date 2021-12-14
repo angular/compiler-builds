f17ce56572539e24b6d02463323a519d90c9de57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:19 +0000

    d640fa7062 docs: Fixed typo 'test' => 'text' (#44464) PR Close #44464

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+18.sha-f56ac0f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+20.sha-d640fa7.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
