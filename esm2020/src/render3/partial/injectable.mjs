f0c3e9548ce91bddf6854f296275de180a95667e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:27 +0000

    ab2248e773 docs(docs-infra): change variable names in route guards examples (#44444)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+14.sha-e483993.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+13.sha-ab2248e.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
