b570f2dbb460008a1f7b52c3fdf6ae526316eb8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:27:43 +0000

    5da31d6d25 test(forms): Add more tests for `FormBuilder` method argument shapes. (#44452)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+15.sha-815d4ce.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+17.sha-5da31d6.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
