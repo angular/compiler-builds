107db36a9e6367d9487099fe84b2a553be90d995
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 17:43:23 +0000

    bce108ab49 fix(forms): `_reduceValue` arrow function now has correct types. (#44483)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.0+24.sha-240edcb.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.0+25.sha-bce108a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
