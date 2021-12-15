dfb8a4088ffade71201142b03702e1ec3e8f3619
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 18:37:52 +0000

    7c246d513b fix(docs-infra): update examples to Angular 13.1 (#44487)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.0+26.sha-9921b2c.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.0+27.sha-7c246d5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
