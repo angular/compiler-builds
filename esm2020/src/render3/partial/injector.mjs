1c2cddc92b067ed472b256bf9b2ce9f27afb5168
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 19:17:04 +0000

    2469b88af2 docs: fix typo (#44592) PR Close #44592

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.1+2.sha-e6beeab.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.1+3.sha-2469b88.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
