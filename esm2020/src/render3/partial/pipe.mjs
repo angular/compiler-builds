204a6469624ed88bb6e7c3ab4d52466d536089e6
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:32:39 +0000

    bfd94b39d3 docs: Replace OnInit with AfterViewInit  (#44485)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+16.sha-9d2401d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+24.sha-bfd94b3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
