4c813cae42b71579b6726f83bcdb85f6c3a14d0c
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:29:38 +0000

    67c81ccef0 refactor(docs-infra): improve aio-footer's layout (#44532)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+14.sha-f7aa937.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+15.sha-67c81cc.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
