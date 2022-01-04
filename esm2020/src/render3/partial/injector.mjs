2e6b83f4b3f0d6cd0e07ba4c0ef2aca89ecfc9af
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 20:32:43 +0000

    f0b4c709c9 docs: fix code sample indentation (#44517)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+24.sha-bfd94b3.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+26.sha-f0b4c70.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
