412c218698c9e409a34430643e3a62e0ef608352
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:09:19 +0000

    d4015f762f test: remove newlines to make a test more stable (#44616)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+31.sha-f2b858d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+30.sha-d4015f7.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
