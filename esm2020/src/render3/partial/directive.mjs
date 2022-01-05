f22ac8a332bd4c74873ac51fb9c2a43d0521b576
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:36:16 +0000

    98592b03bd docs(common): url link format updated (#44638)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.1+49.sha-0daa9bf.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.1+57.sha-98592b0.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
