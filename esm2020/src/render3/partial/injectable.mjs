b882f4db90e341758e30d2d07e5672babb1d084b
Andrew Scott <atscott01@gmail.com>
2022-01-05 19:00:33 +0000

    0daa9bfd26 refactor(forms): make validators code compatible with property renaming (#44500)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.1+48.sha-426b6f9.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.1+49.sha-0daa9bf.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
