992b4976645025925c4b76bf2e7d8486d84f9669
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:03 +0000

    6a55affacd docs(router): fix code block for IsActiveMatchOptions (#44635)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+50.sha-d5ced77.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+52.sha-6a55aff.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
