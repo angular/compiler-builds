293f22e6c464eaab4e79795bffc17f9500c68ead
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:22 +0000

    8ba43a1a1e fix(animations): retain triggers values for moved tracked list items (#44578)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+85.sha-13d5f64.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+86.sha-8ba43a1.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
