cc7002b56298d3b005a15839e8b798d3cb13ba6d
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:25:33 +0000

    21f8d12c3c release: cherry-pick the v12.2.15 release notes (#44619)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+77.sha-0cf5501.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+79.sha-21f8d12.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
