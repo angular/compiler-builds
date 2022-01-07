23205f3455f7fcd7da01a756bf7042368715cea4
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:27:45 +0000

    c57e22a21b refactor(docs-infra): remove IE11 code (#44570)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+81.sha-277d2af.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+83.sha-c57e22a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
