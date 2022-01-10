2143ff01b2b09bb9f1c754230337a77dd544103d
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:15:45 +0000

    ebf8446981 docs: Updated described version number  (#44365)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+90.sha-dff497f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+91.sha-ebf8446.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
