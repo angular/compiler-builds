0741dfc579e9ef1ab2b191baa57570eaf7592b67
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:37:41 +0000

    08049fa23f fix(compiler-cli): enable narrowing of using type guard methods (#44447)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.1+84.sha-c375e5d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.1+85.sha-08049fa.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
