dda5304d420174b12e6e39f3147a898092eac77f
Andrew Scott <atscott01@gmail.com>
2022-01-11 22:20:02 +0000

    de0ae1b36a build(docs-infra): upgrade cli command docs sources to 9ac3df5d1 (#44682)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+105.sha-92b23f4.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+106.sha-de0ae1b.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
