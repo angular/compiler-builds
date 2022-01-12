4728de3c3a2197d4c3af9352c5d7a53cecfce5b8
GitHub <noreply@github.com>
2022-01-12 19:11:47 +0000

    555a57d505 docs: release notes for the v13.1.2 release (#44699)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+107.sha-e98b8f5.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.1+108.sha-555a57d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
