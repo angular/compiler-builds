beef48a7d06af002dfbcbd67a1823e90154496a5
Andrew Scott <atscott01@gmail.com>
2022-01-12 20:56:36 +0000

    c888dfdea0 docs: add Decoded Frontend YT channel to resources list (#44278)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.1+109.sha-42ce3ac.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+1.sha-c888dfd.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
