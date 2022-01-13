9f656a0a72bd9bcd24db9f06d89751b2fdbb6a4d
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:20:11 +0000

    af0a152a2c fix(animations): apply setStyles to only rootTimelines (#44515)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.2+10.sha-44ff4fb.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.2+12.sha-af0a152.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
