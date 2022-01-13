6a887912b0abaf549fb79f4262c1432e3e6fd489
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:43:15 +0000

    2189588ff0 docs: remove unnecessary parenthesis in View Encapsulation file (#44702) PR Close #44702

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+2.sha-c7b210d.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+6.sha-2189588.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
