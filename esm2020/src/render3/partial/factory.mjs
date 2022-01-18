7b7d4db43bc024e078127ba53f398ebd463f32fa
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 18:47:42 +0000

    4307b82058 refactor(common): make the error messages tree shakable (#44663)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+16.sha-35345b6.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+18.sha-4307b82.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
