ca8ab4e23ab926b5df173ba2f578c4f0de2b3b33
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 17:40:41 +0000

    ccb73c0ac8 docs: add Ankit to GDE resources (#44659)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+15.sha-b66e479.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+17.sha-ccb73c0.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
