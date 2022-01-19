7d3859ffe647343c8e375c64caa74052623f3f6b
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 18:16:58 +0000

    fa835b5a29 feat(compiler-cli): enable extended diagnostics by default (#44712)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+32.sha-4e95a31.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+33.sha-fa835b5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
