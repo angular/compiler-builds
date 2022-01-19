6054eea2fc818c2e0e06d70b05d83e415bb8742a
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 17:57:50 +0000

    2b01e17505 docs: release notes for the v13.1.3 release (#44758)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+28.sha-34d2292.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+29.sha-2b01e17.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
