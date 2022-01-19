753b7cde3f04abb309c81f2f28250c48715e16db
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 00:08:25 +0000

    929788d9a8 refactor(animations): change errors type from any to string (#44726)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.2+19.sha-e9bd16a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.2+20.sha-929788d.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
