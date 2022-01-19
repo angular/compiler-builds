6c32285dd2c74de065dde622f2b477158aa47083
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 01:54:30 +0000

    bc03a2e3e3 refactor(core): make the error messages tree shakable (#44359) Long error messages can be tree-shaken in the production build and replaced with error codes.

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+24.sha-41b8ecc.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+25.sha-bc03a2e.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
