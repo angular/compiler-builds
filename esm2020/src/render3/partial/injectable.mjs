97b09e16b41e222a9e6e8f6090d5bb7fd6be33a2
Dylan Hunn <dylhunn@gmail.com>
2022-01-19 18:00:40 +0000

    4e95a316ce docs: deprecate unused config options from the `CompilerOptions` interface (#44749)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+29.sha-2b01e17.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+32.sha-4e95a31.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
