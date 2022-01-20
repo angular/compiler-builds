22e03454c698e3b44420af73c00c02296226dbb6
Andrew Kushnir <akushnir@google.com>
2022-01-20 22:19:09 +0000

    a9f5c87cc6 build(docs-infra): upgrade cli command docs sources to 0ee298908 (#44769)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.3+3.sha-2b6461e.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.3+4.sha-a9f5c87.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
