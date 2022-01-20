7c51088a4891a32ff4e3ed748a5905336d6a7e3b
Dylan Hunn <dylhunn@gmail.com>
2022-01-20 01:04:10 +0000

    d509a0d75f fix(docs-infra): fix date parsing in a flaky test (#44763)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.2+22.sha-7cb3b78.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.3+1.sha-d509a0d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
