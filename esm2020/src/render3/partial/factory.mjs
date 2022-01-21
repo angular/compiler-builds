038fc6155a888ee8fbddb3618cdbd07570484c73
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:29:59 +0000

    45503b43e7 refactor(common): unused return value from attemptFocus (#44457)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.3+7.sha-ef5486f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.3+9.sha-45503b4.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
