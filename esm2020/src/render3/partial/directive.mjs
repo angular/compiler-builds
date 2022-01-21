8634d0030cfa29038fbc8697b519f2373db505ed
Andrew Kushnir <akushnir@google.com>
2022-01-21 21:27:55 +0000

    80467c3aa5 refactor(common): unused return value from attemptFocus (#44457)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+46.sha-627e807.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+47.sha-80467c3.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
