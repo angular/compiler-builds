595a1b2dbe2ead8a345f01a197af66cd52dd3f66
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:38:45 +0000

    ef5486fab5 build: update all non-major dependencies (#44713)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.1.3+4.sha-a9f5c87.with-local-changes'));
+    definitionMap.set('version', o.literal('13.1.3+7.sha-ef5486f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
