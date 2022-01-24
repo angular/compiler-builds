ddda76c0f88d39b665b21a238a9f4dc5b8fd6f30
Andrew Kushnir <akushnir@google.com>
2022-01-24 20:39:34 +0000

    a534a78e67 Revert "feat(core): allow for injector to be specified when creating an embedded view (#44666)" (#44807)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+56.sha-215db7f.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+58.sha-a534a78.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
