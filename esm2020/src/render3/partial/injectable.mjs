a8cb3b2f5b613b24205ef1ee08318e0e89586731
Andrew Kushnir <akushnir@google.com>
2022-01-24 19:01:20 +0000

    215db7fbe6 fix(core): error if NgZone.isInAngularZone is called with a noop zone (#44800)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+55.sha-8363db4.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+56.sha-215db7f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
