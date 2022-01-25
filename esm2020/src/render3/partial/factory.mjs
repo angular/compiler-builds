485c9842470e349c9a0e83caa5b9dc5b7bbacaab
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:31:43 +0000

    1bce51c0ed fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+76.sha-333cbfe.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+77.sha-1bce51c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
