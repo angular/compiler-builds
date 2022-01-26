bc5b899133ca1d6ec67ce1efd140620d5483f08c
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:52:17 +0000

    dc0cac235f docs: cleanup CHANGELOG.md file (#44842)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-rc.1+27.sha-de1e35b.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+1.sha-dc0cac2.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
