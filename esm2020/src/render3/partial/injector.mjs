f443dd828261ae8cbf121cb8a62ef2d58a1fed3b
Andrew Kushnir <akushnir@google.com>
2022-01-26 21:10:14 +0000

    fcc6528159 test(animations): Add bundling size regression test for animations package (#44826)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+1.sha-dc0cac2.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+2.sha-fcc6528.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
