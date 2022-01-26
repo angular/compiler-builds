841956d481e0600f997c8d93abace73b81aca17c
Andrew Kushnir <akushnir@google.com>
2022-01-26 19:57:24 +0000

    2244c5c9be release: update version to 14.0.0-next.0 (#44837)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0-next.2+78.sha-894fa75.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0-next.2+79.sha-2244c5c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
