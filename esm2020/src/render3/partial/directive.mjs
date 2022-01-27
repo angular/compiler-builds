fbaa429d449886f5b156d0f135ef9dc689c213fc
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-27 22:11:00 +0000

    7b7d644286 refactor: fix type checking error with devtools state-serializer spec (#44735)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1028.sha-c12110f.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1027.sha-7b7d644.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
