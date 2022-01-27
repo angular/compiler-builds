9249674ed5d3477a16882c2c18793d4697b78e06
GitHub <noreply@github.com>
2022-01-27 00:28:31 +0000

    b2a081a19d release: cut the v12.2.16 release (#44847)

--- esm2015/src/render3/partial/injectable.js
+++ esm2015/src/render3/partial/injectable.js
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('12.2.15+1.sha-460befd.with-local-changes'));
+    definitionMap.set('version', o.literal('12.2.15+3.sha-b2a081a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
--- src/render3/partial/directive.js
+++ src/render3/partial/directive.js
@@ -48,7 +48,7 @@
     function createDirectiveDefinitionMap(meta) {
         var definitionMap = new util_2.DefinitionMap();
         definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-        definitionMap.set('version', o.literal('12.2.15+1.sha-460befd.with-local-changes'));
+        definitionMap.set('version', o.literal('12.2.15+3.sha-b2a081a.with-local-changes'));
         // e.g. `type: MyDirective`
         definitionMap.set('type', meta.internalType);
         // e.g. `selector: 'some-dir'`
