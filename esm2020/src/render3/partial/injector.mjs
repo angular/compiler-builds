7dd6e6bd42a5f90d68b126d434f3097d24059686
Andrew Kushnir <akushnir@google.com>
2022-01-27 20:00:13 +0000

    1312ac1d98 docs: enable the survey banner (#44861)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1022.sha-df50af2.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1023.sha-1312ac1.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
