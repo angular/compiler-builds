00290b04cb46d206bfbfb0a3ef448ce8dec79a24
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 17:52:59 +0000

    f398717502 docs: fix typo in update guide (#44853)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+13.sha-c9abf34.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+14.sha-f398717.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
