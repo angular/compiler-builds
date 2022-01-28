02d17945290537ca5c9754d031528c76e920c615
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 22:16:26 +0000

    deb9531a31 fix(forms): Update the typed forms migration schematic to find all files. (#44881)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1037.sha-9229511.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1038.sha-deb9531.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
