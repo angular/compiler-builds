727d6c78e3a16e828f8738e289b232bde99d8c27
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 22:15:25 +0000

    6b7fffcbeb fix(forms): Update the typed forms migration schematic to find all files. (#44881)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+18.sha-641f27a.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+19.sha-6b7fffc.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
