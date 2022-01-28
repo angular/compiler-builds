fb5a403d6280d436d2607424719e0b89f23d6b45
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 18:06:24 +0000

    ab63ed3448 test(docs-infra): fix type checking errors with new jasmine types (#44876)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+14.sha-f398717.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+17.sha-ab63ed3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
