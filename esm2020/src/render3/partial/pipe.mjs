ae64f26fc83f1575f433f3b11b790dfe56ceb9b2
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 17:30:59 +0000

    94c9e47dbe Revert "fix(docs-infra): content overflow in mobile (#44851)" (#44899)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+19.sha-6b7fffc.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+20.sha-94c9e47.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
