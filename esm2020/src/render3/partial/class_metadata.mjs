18ee04d49769f2d7db58cf837d8c396d9e2fbdc1
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:12:16 +0000

    e62f0103cf build(docs-infra): upgrade cli command docs sources to db5e7c82e (#44888)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1045.sha-3f0a2d3.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1046.sha-e62f010.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
