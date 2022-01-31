b2ab2ea08445656aab148f4d8c6b1a905e74fe33
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:48:51 +0000

    5e099b5e6c docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1061.sha-7155e9e.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1062.sha-5e099b5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
