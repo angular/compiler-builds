94e753cb5889d8d044b8a2f4c9dee01d9440a027
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 18:54:02 +0000

    47c02caa29 refactor(compiler-cli): Update where and how the indexed errors are exposed (#44884)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+20.sha-94c9e47.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+21.sha-47c02ca.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
