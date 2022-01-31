314812933a851489febbb8610c6196d7af2bfa97
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:22:56 +0000

    390a653b7b build: update dependency d3 to v7 (#44892)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1048.sha-dea30d4.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1049.sha-390a653.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
