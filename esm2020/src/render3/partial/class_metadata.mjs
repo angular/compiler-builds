52caa5ffe4abfa2596ae33223a0e730188ad8823
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:42:37 +0000

    53a4a03144 build: bump version for `@angular/benchpress` to v0.3.0 (#44893)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1057.sha-366e424.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1059.sha-53a4a03.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
