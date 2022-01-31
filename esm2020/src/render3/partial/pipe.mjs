e14c15928949776d118836f93700329417026433
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:44:53 +0000

    7a81481fb2 perf(animations): Remove generic objects in favor of Maps (#44482)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1052.sha-8d9eb99.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1054.sha-7a81481.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
