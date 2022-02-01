3c9d10e17e63ddbbc901181d169086e5d5071f24
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 21:44:38 +0000

    d41dedb85b fix(devtools): highlighter should only show the hovered component's name (#44863)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1079.sha-c89cf63.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1081.sha-d41dedb.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
