e1e782986d7e3ff57c4254a06877bfc3365b1476
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:33:03 +0000

    5d7ffd5c0c docs: correct the demo code of NgZone.run (#44733)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.0+1075.sha-e0a340e.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.0+1077.sha-5d7ffd5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
