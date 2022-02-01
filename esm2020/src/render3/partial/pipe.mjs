ec7e2ef2e9f0f4f00ba6b32c94b087ebd5943baf
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:34:27 +0000

    1cc1588c29 docs: fix incorrect title and tooltip in navigation (#44710)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.0+41.sha-2916ac9.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.0+48.sha-1cc1588.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
