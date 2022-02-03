74c664101723afbdfe9065460930064f849fef52
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:11:43 +0000

    1b216297f4 docs: update input-output-diagram.svg (#39900)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.1+13.sha-f092bad.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.1+15.sha-1b21629.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
