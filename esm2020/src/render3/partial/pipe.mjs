87c8072d7236081978cfcb53f7d165ef788a91a0
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:56:37 +0000

    791bd31424 feat(router): set stricter type for Route.title (#44939)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.1+12.sha-0072eb4.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.1+16.sha-791bd31.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
