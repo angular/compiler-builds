31ab301f5c3c061afe8ca03924689c8d53b56fbd
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 01:50:10 +0000

    077593f3bc docs: remove old commits from CHANGELOG.md (#44956)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.1+7.sha-85ba38a.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.1+8.sha-077593f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
