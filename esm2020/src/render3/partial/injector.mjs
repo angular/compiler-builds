c4f3e6960885f8d970c504834f4fda48c3fe0828
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:01:20 +0000

    3fae6637e7 perf(http): remove IE special status handling (#44354)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.1+19.sha-30d1c34.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.1+20.sha-3fae663.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
