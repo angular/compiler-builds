bc33a9a1da92493537de28517c4bfe4079e1617a
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:25 +0000

    459cbed33b refactor(dev-infra): convert nullish coalescing operator to plain javascript (#44968)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.1+30.sha-8dd3f82.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.1+31.sha-459cbed.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
