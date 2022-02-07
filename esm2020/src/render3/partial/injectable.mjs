ba9e5c7a2047e6ea5df5c80415d2faa482784c71
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:02:43 +0000

    638a2d56dc ci: group all scorecard action dependencies (#44998)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.1+38.sha-4708cd0.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.1+39.sha-638a2d5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
