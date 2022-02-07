389dfcdc309742b98c13a85f3e5672595a3de059
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 20:24:08 +0000

    3919ee37cf refactor(forms): update `PatternValidator` to inherit `AbstractValidatorDirective` (#44887)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.1+24.sha-37af6ab.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.1+25.sha-3919ee3.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
