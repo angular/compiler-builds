f60650b4630c50783a86eeb44c378a46782ac18b
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:50:50 +0000

    4d63b80163 refactor: CHANGELOG is poorly formatted for most recent version. (#45009)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.1+47.sha-a3de44c.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.2+1.sha-4d63b80.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
