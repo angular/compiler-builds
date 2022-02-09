22940e8520469da8cac0f274e38a56ba9fcb2ba6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:39:05 +0000

    b8f8e8cf63 build: update scorecard action dependencies to 7f9dca2 (#45026)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.2+17.sha-7d9413e.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.2+15.sha-b8f8e8c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
