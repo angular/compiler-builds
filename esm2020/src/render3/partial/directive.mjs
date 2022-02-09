46b3c47ee4e3ca81686933f86eb85934bfb55daa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:35:09 +0000

    d5565ccdb4 fix(zone.js): fix several test cases which trigger `done()` multiple times (#45025)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.2+12.sha-c88e87d.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.2+14.sha-d5565cc.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
