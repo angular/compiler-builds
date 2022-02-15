468d1c75317baf2248c130fa7b854f7e9a77cbd0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:47:59 +0000

    aeeb1b55d3 docs(localize): fix angular.json syntax error about i18n (#45048)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.2+22.sha-0050b01.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.2+24.sha-aeeb1b5.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
