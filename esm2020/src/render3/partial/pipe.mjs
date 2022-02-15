cd60acea81d0f63fbec6ecabfbc93b3840c1c50b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:45:49 +0000

    13172421f9 docs: fix grammatical error (#45092)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.2+34.sha-787eaa7.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.2+35.sha-1317242.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
