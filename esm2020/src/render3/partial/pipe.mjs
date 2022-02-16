64b70de9deb8cc714b5e9db0de47d4a67d8f6393
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:01:58 +0000

    1eae65ead7 build: update .pullapprove.yml (#45061)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.2+40.sha-80585f4.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.2+43.sha-1eae65e.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
