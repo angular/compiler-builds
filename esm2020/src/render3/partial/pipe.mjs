ec578703a34dbda4111aa58771c1e9d50689f778
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 22:12:44 +0000

    5957ff4163 build: update scorecard action dependencies to 040feef (#45135)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+11.sha-f28276c.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+12.sha-5957ff4.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
