952a3b1bfe4036b69be1ccfcf2ee7b0ef7baea7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:50:54 +0000

    be50401090 docs(animations): remove query call in animations example (#45126)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+4.sha-561bd75.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+6.sha-be50401.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
