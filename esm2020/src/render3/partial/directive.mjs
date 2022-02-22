5ccf65cd20a005be767955d22e5bc75574995d93
Andrew Scott <atscott01@gmail.com>
2022-02-22 21:04:35 +0000

    bd168fc0c3 build: update ossf/scorecard-action commit hash to 3662744 (#45162)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+14.sha-c0778b4.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+16.sha-bd168fc.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
