e9fc028ee962cfc7ef1cfd1915de990cf43b403d
Andrew Scott <atscott01@gmail.com>
2022-02-23 21:19:00 +0000

    2e7e46f1b6 ci: use renovate Github Action tag version pinning (#45178)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+23.sha-0c26c93.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+1.sha-2e7e46f.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
