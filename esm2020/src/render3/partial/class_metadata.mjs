82fe02b99a3945db4a9e039c75a78cb0eda5e655
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:19:23 +0000

    75f20d8a88 docs: fix test description to match code example (#45101)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+19.sha-d65706a.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+21.sha-75f20d8.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
