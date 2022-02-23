87932c84c03d824a584638b99be0cebab187cc46
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:14:56 +0000

    d65706a3b2 feat(zone.js): update electron patch to support electron/remote 14 (#45073)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+20.sha-f8dc660.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+19.sha-d65706a.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
