33ae344a1c40227367ce7897e9da3a46899549c6
Andrew Scott <atscott01@gmail.com>
2022-02-23 00:07:03 +0000

    4fa530757b fix(docs-infra): removed duplicated style in aio examples (#45157)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.3+15.sha-ff6be32.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.3+18.sha-4fa5307.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
