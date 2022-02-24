6e4b55056205f197eeabbf103a01d6f72e186244
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:01:07 +0000

    7fd416d060 fix(router): Fix type of Route.pathMatch to be more accurate (#45176)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+6.sha-909b21a.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+8.sha-7fd416d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
