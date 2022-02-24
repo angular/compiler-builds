a81e7251ec9d133b7c16d2ffa6987446de07d9dc
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 23:14:59 +0000

    24e39a71c2 docs: fix style guide example app included as routing (#45148)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+13.sha-0465451.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+14.sha-24e39a7.with-local-changes'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
