727d8559535c6e262053f01c6cc119dc4a6cf3ce
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 19:31:56 +0000

    b39db80f91 test(animations): add route animation delay to pages tests (#45144)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.4+6.sha-3b86dda.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.4+7.sha-b39db80.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
