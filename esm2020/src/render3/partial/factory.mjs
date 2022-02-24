c9eaaa401f0ef77041538954dc11bbedcd441593
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 01:11:49 +0000

    4f1870362e docs: remove activatedRouteData backward accesses in aio guides (#45140)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.3+14.sha-e914da1.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.4+3.sha-4f18703.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
