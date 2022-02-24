16acadd386358b40b634ef8e699f6d768e8a4b4d
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 23:14:14 +0000

    c6e64aa8ad docs: fix style guide example app included as routing (#45148)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.4+9.sha-7379d75.with-local-changes'));
+    definitionMap.set('version', o.literal('13.2.4+10.sha-c6e64aa.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
