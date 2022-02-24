9c206420012455066ab126027486d6e805a1ca89
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 23:11:56 +0000

    0465451e00 build: update all non-major dependencies (#45189)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+12.sha-0debf84.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+13.sha-0465451.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
