4d9437a411f8175fdcf974eabcac8d1759a990c2
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 22:27:07 +0000

    78a0ac02a7 ci: instruct renovate to not update/override merge ready PRs (#45204)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+23.sha-e56472d.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+24.sha-78a0ac0.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
