0b59ae9f0082277f98104dffb65715c2e0a7688e
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 00:09:19 +0000

    88f1168506 perf(core): only track LViews that are referenced in __ngContext__ (#45172)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+16.sha-0920104.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+17.sha-88f1168.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
