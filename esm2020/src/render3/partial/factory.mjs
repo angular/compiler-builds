258d2677a11237491c8105a77b831cac25ac6d49
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:30:10 +0000

    e56472d3c2 ci: fix stamping for builds performed in CI (#45147)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+22.sha-e01820c.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+23.sha-e56472d.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
