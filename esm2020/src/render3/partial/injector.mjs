a688e481dd9aa20559a8a3f54009ee15dfb60c4c
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 00:13:25 +0000

    aae07c338f build: lock file maintenance (#45071)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+17.sha-88f1168.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+18.sha-aae07c3.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
