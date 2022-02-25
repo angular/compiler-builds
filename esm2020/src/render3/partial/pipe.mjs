a93b2e3257c7eaa7df176b6969aea791a1f6ba7a
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:23:51 +0000

    e01820c45f build: update dependency @types/chrome to ^0.0.179 (#45201)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+21.sha-63e9cc7.with-local-changes'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+22.sha-e01820c.with-local-changes'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
