921c737bd372aa71c22ec3f5fb6b74f21fb6f051
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 19:47:41 +0000

    9366a3c5f3 docs: improve structure of http example (#44986)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+29.sha-4d494d2'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+30.sha-9366a3c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
