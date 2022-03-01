f5846daa97494e794aa0fd0a4f67b4ae5f47fa7c
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 01:21:09 +0000

    4b936a1014 Revert "feat(core): allow for injector to be specified when creating an embedded view (#45156)" (#45220)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+34.sha-b817f3c'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+35.sha-4b936a1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
