599c8bc21f80089a564554294c738b20a7dfa2ee
GitHub <noreply@github.com>
2022-03-02 18:44:13 +0000

    3b85a121ba docs: release notes for the v13.2.5 release (#45242)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+43.sha-69018c9'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+44.sha-3b85a12'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
