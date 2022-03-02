f6cc86a9e44c5fd58e3505d48c3f5e980ff01c36
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-02 16:57:17 +0000

    69018c9f42 feat(core): allow for injector to be specified when creating an embedded view (#45156)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.4+42.sha-05d50b8'));
+    definitionMap.set('version', o.literal('14.0.0-next.4+43.sha-69018c9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
