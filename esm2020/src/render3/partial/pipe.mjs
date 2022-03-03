5fe87d8a5561c21887007ec96c13c48aaefa7a70
Andrew Kushnir <akushnir@google.com>
2022-03-03 20:46:22 +0000

    4e1d825d83 docs: change link for devtools to angular repo (#45207) PR Close #45207

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.5+6.sha-e16dab8'));
+    definitionMap.set('version', o.literal('13.2.5+9.sha-4e1d825'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
