78396a2412bfe00072871b7ac078433ec0e75860
Andrew Kushnir <akushnir@google.com>
2022-03-04 23:40:39 +0000

    26ead25df6 refactor(router): Use helper function to throw NoMatch (#45244)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+8.sha-bc89598'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+9.sha-26ead25'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
