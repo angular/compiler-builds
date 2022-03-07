a1d0f20a7a79197ed207fd0b564242877d33e7bf
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:58:03 +0000

    a659d15aa0 docs: Change tag name to TD from TR in context of colspan usage (#45257) PR Close #45257

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+18.sha-68eda48'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+21.sha-a659d15'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
