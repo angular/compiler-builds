80ce5d396c1e83e52516f024042eb56c158c101b
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:52:59 +0000

    d87f66c02f test(compiler): fix deprecation warning (#45285)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+15.sha-886cfe8'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+16.sha-d87f66c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
