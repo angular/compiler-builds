e81f5e792b89661be19512f25f9a5400e9af8815
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:59:12 +0000

    ad9d98146e docs: Change tag name to TD from TR in context of colspan usage (#45257) PR Close #45257

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.5+16.sha-8390e12'));
+    definitionMap.set('version', o.literal('13.2.5+17.sha-ad9d981'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
