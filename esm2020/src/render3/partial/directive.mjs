fa721be15daae86b6a30111d6ed09e7bede085d1
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:52:07 +0000

    f58b8a94d4 build: update all non-major dependencies (#45250)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+28.sha-f25c8be'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+29.sha-f58b8a9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
