d72677aabf67af4296b4d4e688d08bd5ea11d7b3
Andrew Kushnir <akushnir@google.com>
2022-03-08 23:06:24 +0000

    be220fa683 fix(language-service): Prioritize Angular-specific completions over DOM completions (#45293)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+35.sha-dc6094a'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+36.sha-be220fa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
