cae793ee0d8d6e8780dd70194c4eb96460ddd299
Andrew Kushnir <akushnir@google.com>
2022-03-08 20:43:54 +0000

    6eaaefd22e feat(core): drop support for Node.js 12 (#45286)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+31.sha-6cec8aa'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+30.sha-6eaaefd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
