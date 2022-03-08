4ac270816bcf06dec5086c2a9d92523442960c55
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:29:54 +0000

    ba0efd1f80 refactor: replace deprecated `HTMLDocument` with `Document` (#45282)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+30.sha-6eaaefd'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+32.sha-ba0efd1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
