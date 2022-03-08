226ad4a1b22c0d2fab1b223b26a9dee9d25a962d
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:44:38 +0000

    9e56e400c5 fix(docs-infra): remove anchor tags from heritage docs (#45287)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.5+23.sha-2b7553d'));
+    definitionMap.set('version', o.literal('14.0.0-next.5+26.sha-9e56e40'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
