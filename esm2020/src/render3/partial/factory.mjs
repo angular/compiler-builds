4115dfdb9fbf91ce7a2052c27e481cd149d86ddc
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:30:56 +0000

    8227eb914e refactor(migrations): update `isReferenceToImport` to not use `valueDeclaration` (#45292)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.5+21.sha-23003f8'));
+    definitionMap.set('version', o.literal('13.2.5+22.sha-8227eb9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.5+21.sha-23003f8'));
+    definitionMap.set('version', o.literal('13.2.5+22.sha-8227eb9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
