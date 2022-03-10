a4b6771c6f021664bbc62ceccab98b2b1270667a
GitHub <noreply@github.com>
2022-03-10 19:32:57 +0000

    0f8e748b07 release: cut the v13.3.0-rc.0 release (#45321)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.6+3.sha-895c541'));
+    definitionMap.set('version', o.literal('13.2.6+4.sha-0f8e748'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
