6ed52401b33b3e2db32f1aea8881ce981a1b6c3d
Andrew Scott <atscott01@gmail.com>
2022-03-14 20:28:20 +0000

    bb9b33053c docs: fix small English typo (#45340)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.0-rc.0+4.sha-7eddf58'));
+    definitionMap.set('version', o.literal('13.3.0-rc.0+5.sha-bb9b330'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
