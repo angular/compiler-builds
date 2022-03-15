06d8b1cacb3641e32baed029e30fc67cabdd3863
Andrew Scott <atscott01@gmail.com>
2022-03-15 20:44:32 +0000

    7ff4c0bf39 fix(forms): improve error message for invalid value accessors (#45192)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.0-rc.0+5.sha-bb9b330'));
+    definitionMap.set('version', o.literal('13.3.0-rc.0+7.sha-7ff4c0b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
