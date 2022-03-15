d03cebf52e980ce333f7a04842b451d1a5ca94b9
Andrew Scott <atscott01@gmail.com>
2022-03-15 23:48:18 +0000

    9361f091cc docs: add ngx-gantt to "EXPLORE ANGULAR RESOURCES" page (#45351)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.2.6+7.sha-eac94f0'));
+    definitionMap.set('version', o.literal('13.2.6+8.sha-9361f09'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
