4113b58d05f02b7c316363a24ab8989d8792fbd1
Andrew Scott <atscott01@gmail.com>
2022-03-16 18:39:12 +0000

    3ad033c3fb build: update github/codeql-action action to v1.1.5 (#45358)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.6+18.sha-e47ba49'));
+    definitionMap.set('version', o.literal('14.0.0-next.6+19.sha-3ad033c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
