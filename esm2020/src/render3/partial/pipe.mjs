a36cb3da867c11b7d56b74f76dec91a4d8ee0f47
GitHub <noreply@github.com>
2022-03-16 19:45:12 +0000

    34c48a3f9a docs: release notes for the v13.3.0 release (#45366)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.6+20.sha-306fb5e'));
+    definitionMap.set('version', o.literal('14.0.0-next.6+21.sha-34c48a3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
