b014d4fb25f7d562d9b68861beea0d34c29719fa
Andrew Scott <atscott01@gmail.com>
2022-03-16 21:53:18 +0000

    9d5ad672b4 build: lock file maintenance (#45341)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.6+22.sha-2b6749f'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+1.sha-9d5ad67'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
