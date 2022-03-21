4150b8742141acd28316fdb0eabb1793bbdd696f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 21:58:54 +0000

    c26915c8cf refactor(core): Make the typed forms migration apply to all usages of the symbols. (#45311)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+5.sha-79d334b'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+6.sha-c26915c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
