651fb2d044541ff0352c40c10a4b6b9a92529a7d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 22:07:27 +0000

    aec9abd374 fix(docs-infra): don't wrap subcommands in code element (#45343)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+6.sha-c26915c'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+7.sha-aec9abd'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
