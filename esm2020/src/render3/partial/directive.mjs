0b54f6bcb173fe10d467c2ed7763ce09d9169990
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:15:51 +0000

    650715062d build(docs-infra): upgrade cli command docs sources to bff1908b4 (#45395)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+10.sha-b716a81'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+12.sha-6507150'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
