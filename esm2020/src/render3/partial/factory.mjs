2428637df3a05aadc33abed07c6ff7421f9d0395
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:12:55 +0000

    b481eab39d fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.0+6.sha-ee20410'));
+    definitionMap.set('version', o.literal('13.3.0+11.sha-b481eab'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
