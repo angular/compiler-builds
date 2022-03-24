0a0e170e5d0e0106b102e797238a5d087e3867b4
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:47:33 +0000

    dc0e39a767 docs: update for 2022 angular events (#45386)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+35.sha-4ddcf81'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+36.sha-dc0e39a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
