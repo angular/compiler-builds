fdba9f61e4c7dd3f84b95f5f1fb90e008fcbc5c5
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:46:08 +0000

    aa29fc7b5c docs: update the public roadmap (#45415)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.0+11.sha-b481eab'));
+    definitionMap.set('version', o.literal('13.3.0+12.sha-aa29fc7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
