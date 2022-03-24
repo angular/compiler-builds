3a7be036925d1e9a79958a25100a69ca099f0e88
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:48:56 +0000

    7a9c6f56dd build: update dependency send to ^0.18.0 (#45428)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+36.sha-dc0e39a'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+37.sha-7a9c6f5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
