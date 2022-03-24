986241d85e420c02d9e03bff3297c5242a4d1826
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:13:35 +0000

    c9d566ce4b feat(core): drop support for TypeScript 4.4 and 4.5 (#45394)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+27.sha-c7bcc1b'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+25.sha-c9d566c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
