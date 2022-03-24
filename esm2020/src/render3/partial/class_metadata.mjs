08bdafadec5c7bd683a94b61e2aa76b0509862e2
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:43:17 +0000

    638e86054c docs: small English improvement (#45375)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+32.sha-be161be'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+33.sha-638e860'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
