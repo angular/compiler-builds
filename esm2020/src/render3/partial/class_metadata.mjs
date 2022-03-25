a5d2074a17b4361146e7c8acb2ca1a9b4d9d318a
Dylan Hunn <dylhunn@gmail.com>
2022-03-25 02:13:27 +0000

    e2eaac34b0 fix(zone.js): read `Symbol.species` safely (#45369)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.7+37.sha-7a9c6f5'));
+    definitionMap.set('version', o.literal('14.0.0-next.7+38.sha-e2eaac3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
