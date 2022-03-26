995dbf7d585d01a5958c50fc73992b681c262229
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:09:12 +0000

    96c7cc933e docs: deprecated relativeLinkResolution in the Router (#45308)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+3.sha-3143494'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+4.sha-96c7cc9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
