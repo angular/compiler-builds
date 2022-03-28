d9f6474c590d86ab9d120fb5b09913922ec31d64
Dylan Hunn <dylhunn@gmail.com>
2022-03-28 16:42:26 +0000

    3d2a60e027 build: update dependency @bazel/ibazel to ^0.16.0 (#45259)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+7.sha-c996b47'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+8.sha-3d2a60e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+7.sha-c996b47'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+8.sha-3d2a60e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
