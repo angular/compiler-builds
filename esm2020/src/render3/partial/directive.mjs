ee9732056e276d79dd053d4f6a0724cfd9f9d81e
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:40:01 +0000

    6e4cb48bc5 docs: adjust contributing, developing and overview markdown files (#45464)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+13.sha-801d11d'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+14.sha-6e4cb48'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
