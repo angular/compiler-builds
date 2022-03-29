b569ea630c03e3743be048b78a61c32d75bf5d66
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 20:42:01 +0000

    e55e98dce5 build: update all non-major dependencies (#45374)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+28.sha-2142ffd'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+29.sha-e55e98d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
