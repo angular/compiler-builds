c508895d6d6c68d38fae8ccbe9dadacb08f55e6f
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:27:59 +0000

    82956dcbd3 Update aio/content/marketing/resources.json (#45076)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+35.sha-7d94923'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+37.sha-82956dc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
