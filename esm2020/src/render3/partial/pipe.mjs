5c172da5672b3740947c8c0d4048fea2e434bbc8
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 21:37:59 +0000

    9c9a52540c release: cut the v13.3.1 release (#45485)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.0+50.sha-7f53c0f'));
+    definitionMap.set('version', o.literal('13.3.0+51.sha-9c9a525'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
