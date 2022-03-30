02e5aabe3408a7ec2f1360ae34e65eb9af01dcad
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:29:25 +0000

    e1e440d65a docs: add Angular university website to resources list (#45437)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.9+37.sha-82956dc'));
+    definitionMap.set('version', o.literal('14.0.0-next.9+38.sha-e1e440d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
