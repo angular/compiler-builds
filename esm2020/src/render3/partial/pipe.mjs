136f13dbd28154a09007621f947d93bb93ac0d15
Andrew Kushnir <akushnir@google.com>
2022-04-01 20:50:28 +0000

    8b1a2f1048 docs: change roadmap updated date (#45500)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.10+3.sha-71e9068'));
+    definitionMap.set('version', o.literal('14.0.0-next.10+5.sha-8b1a2f1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
