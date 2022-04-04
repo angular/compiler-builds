84769390dacd3e66ab9ffacf75c7e47e0e620880
Andrew Kushnir <akushnir@google.com>
2022-04-04 22:11:04 +0000

    035ba83b16 build: update dependency entities to v4 (#45509)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.10+9.sha-5aeaedf'));
+    definitionMap.set('version', o.literal('14.0.0-next.10+11.sha-035ba83'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
