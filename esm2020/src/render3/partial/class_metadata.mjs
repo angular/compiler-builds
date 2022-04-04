59331eee73cd0814a7ea65f7552d6e8c28523989
Andrew Kushnir <akushnir@google.com>
2022-04-04 22:10:41 +0000

    fc86fe5dad build: update dependency entities to v4 (#45509)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.1+5.sha-9fcc1b7'));
+    definitionMap.set('version', o.literal('13.3.1+6.sha-fc86fe5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
