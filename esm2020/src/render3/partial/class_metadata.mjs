50c72fd1d65c2899509bbb5bf83a0598cc9f7979
GitHub <noreply@github.com>
2022-04-06 16:08:31 +0000

    fb5e16e018 release: cut the v13.3.2 release (#45548)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.1+9.sha-05121c0'));
+    definitionMap.set('version', o.literal('13.3.1+10.sha-fb5e16e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
