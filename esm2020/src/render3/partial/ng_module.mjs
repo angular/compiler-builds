77dfa9dd5d21c79c2c18242f1906a9e3a2bde83a
GitHub <noreply@github.com>
2022-04-06 16:14:29 +0000

    d4270d2423 docs: release notes for the v13.3.2 release (#45549)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.10+14.sha-1c11a57'));
+    definitionMap.set('version', o.literal('14.0.0-next.10+15.sha-d4270d2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
