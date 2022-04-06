6754cc3ced4a1d2657ad4f59d409255dfdbdd1c6
GitHub <noreply@github.com>
2022-04-06 16:16:31 +0000

    f1a2816ba9 release: cut the v14.0.0-next.11 release (#45550)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.10+15.sha-d4270d2'));
+    definitionMap.set('version', o.literal('14.0.0-next.10+16.sha-f1a2816'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
