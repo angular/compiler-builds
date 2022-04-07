178a7a2ef402e261d8132058da70f81816cefc4c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:19:02 +0000

    5adfe8ef24 docs(forms): remove the incorrect set value from previous commit (#45533)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+5.sha-f8f3ab3'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+4.sha-5adfe8e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
