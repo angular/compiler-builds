d91647da3e347bad337d16f8e0158f4584d9c6c9
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-08 19:53:24 +0000

    e0ac614121 docs: update test threshold \nupdate test threshold to get this content merged. (#45325)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+9.sha-7a37fe9'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+12.sha-e0ac614'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
