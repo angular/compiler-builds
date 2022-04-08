f5809efca366a5a3bd3268679702389e448c97b3
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-08 16:13:28 +0000

    1b2c67d9fd test(docs-infra): fix e2e test for `{@example}` dgeni tags (#45551) (#45563)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.2+4.sha-208d819'));
+    definitionMap.set('version', o.literal('13.3.2+6.sha-1b2c67d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
