ccb5450f8670f18ac192fb5a800ee44a2268fab3
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-08 19:24:40 +0000

    7a37fe9f28 Revert "build: update to jasmine 4.0 (#45558)" (#45566)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+8.sha-a248df0'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+9.sha-7a37fe9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
