2033938652ac4e4a6ce13235202d3d2b4b634fa3
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 16:39:02 +0000

    20fbb4c35c build: update github/codeql-action action to v2.1.8 (#45542)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+15.sha-a6bf2c5'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+16.sha-20fbb4c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
