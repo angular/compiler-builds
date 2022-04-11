8d42f5bfe20604e38fc4ad1585b399a32f28e7a9
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 21:22:43 +0000

    3fceba4bd1 refactor(animations): include pushUnrecognizedPropertiesWarning in  ngDevMode check (#45591)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+30.sha-0facba5'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+31.sha-3fceba4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
