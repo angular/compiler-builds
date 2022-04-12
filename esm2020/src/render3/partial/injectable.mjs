d9bc1101910381df975f05f0d9518c7935da02e8
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:44:22 +0000

    3c1695e399 docs: make tutorial instructions consistent (#45372)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+6.sha-820077d'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+9.sha-3c1695e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
