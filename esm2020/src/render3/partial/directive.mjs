10397b570d3d0256342146c567e7bd683cc91b54
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:19:19 +0000

    820077df76 build: update dependency glob to v8 (#45598)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+5.sha-83920af'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+6.sha-820077d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
