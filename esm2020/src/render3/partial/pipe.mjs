5f5e38301275db4073334b2892b372d34b19c29c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:44:29 +0000

    ee3cfc8815 docs: fix path typo (#45604) PR Close #45604

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+1.sha-79de026'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+3.sha-ee3cfc8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
