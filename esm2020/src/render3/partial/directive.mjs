95beff9aade64f023e611974b842d5dae05c589d
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:31:13 +0000

    79de026a20 docs: add powershell execution policies note (#45576)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+47.sha-6532328'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+1.sha-79de026'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+47.sha-6532328'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+1.sha-79de026'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
