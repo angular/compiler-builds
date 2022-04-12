2700e17c7d4cf0a5e66f882def1da25bfaaa1d8a
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:47:11 +0000

    83920af7cd docs: new Pipes custom data doc (#45505)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+3.sha-ee3cfc8'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+5.sha-83920af'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
