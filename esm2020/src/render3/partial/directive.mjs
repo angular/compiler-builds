7f14da20246c36266ce7844eb2cb49aa9eec33ba
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:47:36 +0000

    89ed8d8976 refactor(core): make ComponentFactoryResolver lazily instantiated in ApplicationRef (#45507)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+10.sha-f38c344'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+11.sha-89ed8d8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
