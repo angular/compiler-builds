ce0fd7358f27cbce668dcf60dada9bbcd08606a8
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:43 +0000

    222d8662a2 docs: fix `schematicCollections` option (#45602) PR Close #45602

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+35.sha-eb2f449'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+34.sha-222d866'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
