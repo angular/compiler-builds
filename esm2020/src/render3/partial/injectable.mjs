df5abdc264c9c4ee5bf9ab8d573896de93f4d893
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 00:26:44 +0000

    d11d1c0b70 Revert "refactor(router): Add warning for `relativeLinkResolution: 'legacy'` (#45523)" (#45594)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+31.sha-3fceba4'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+32.sha-d11d1c0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
