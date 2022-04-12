c0da8daf30b4b8fa8c39d31ba66b85408958ecde
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:06 +0000

    eb2f4492eb docs(docs-infra): add missing period in `aio/README.md` (#45584)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.11+33.sha-89d2991'));
+    definitionMap.set('version', o.literal('14.0.0-next.11+35.sha-eb2f449'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
