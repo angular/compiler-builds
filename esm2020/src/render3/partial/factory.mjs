8bed6fca097fb9155cc0b2d717f1707c962bc5c6
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 00:12:26 +0000

    5fe59706fd docs: fix grammar (#45455) PR Close #45455

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.2+19.sha-fc145d0'));
+    definitionMap.set('version', o.literal('13.3.2+23.sha-5fe5970'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
