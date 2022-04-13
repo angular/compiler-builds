7bf17748096a7436815eb32bcd9b071c461ca0da
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 00:27:48 +0000

    a6ab3f7beb docs: fix grammar (#45455) PR Close #45455

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+11.sha-89ed8d8'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+16.sha-a6ab3f7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
