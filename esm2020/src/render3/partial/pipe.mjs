4d0dfa9c1ac2cc206eb0d4184ddc142e149bcf16
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:37:14 +0000

    47e7a2850b refactor(router): Add warning for `relativeLinkResolution: 'legacy'` (#45523)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.12+18.sha-598b759'));
+    definitionMap.set('version', o.literal('14.0.0-next.12+19.sha-47e7a28'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
