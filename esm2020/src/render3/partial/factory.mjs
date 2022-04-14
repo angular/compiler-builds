38ad19676054ea07ddc98bd0043b3d2f0fd26ba4
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:18:57 +0000

    a1c741846d docs: move old changelog entries to a separate file (#45639)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.3+1.sha-ec115a3'));
+    definitionMap.set('version', o.literal('13.3.3+3.sha-a1c7418'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
