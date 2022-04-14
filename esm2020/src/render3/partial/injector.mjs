4d0e3ab31e60649f76c029a52db1f4012abaca5b
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:17:08 +0000

    94fba9b047 docs(core): fix typo in View Encapsulation code example (#45629)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+3.sha-f8a1ea0'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+5.sha-94fba9b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
