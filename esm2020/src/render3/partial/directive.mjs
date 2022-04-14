918f9e252a20d22302eaddbfd06b7ef1a379ee8d
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:18:52 +0000

    6444a02620 docs: move old changelog entries to a separate file (#45638)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+6.sha-2f08b80'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+7.sha-6444a02'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
