480006bb508608c428af6d5161857bd1ef3dc9b5
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:08:58 +0000

    c6e0e3f6d3 fix(core): improve multiple components match error (#45645)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+13.sha-2e97312'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+14.sha-c6e0e3f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
