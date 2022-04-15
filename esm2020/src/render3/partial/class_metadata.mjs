53736ee92f5d6462adc8c6e3d5147a92d551c044
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:55:32 +0000

    68357104c9 build: lock file maintenance (#45457)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+18.sha-0b64072'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+19.sha-6835710'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
