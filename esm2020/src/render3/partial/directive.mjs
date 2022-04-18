8166d400a299e2c47cb008721d87fdb453329f2d
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 17:15:11 +0000

    37b3ac7f69 fix(docs-infra): implement focus trap for aio search results (#45654)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.3+9.sha-989ba8f'));
+    definitionMap.set('version', o.literal('13.3.3+13.sha-37b3ac7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
