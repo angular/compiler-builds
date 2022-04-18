17e408abcd27336faf4080ff7946069a9a3549d2
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 23:22:45 +0000

    ea8256f43d refactor(router): Move config loader tracking to the RouterConfigLoader (#45656)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+30.sha-aa966fd'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+34.sha-ea8256f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
