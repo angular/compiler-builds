1618f9376669247af276984628e286c45c524a32
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:37:20 +0000

    ceffdeda69 docs(common): fix links to locales in i18n guides (#45661)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+37.sha-c6feb0a'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+38.sha-ceffded'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
