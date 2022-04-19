7d392886d916434857ad6652ae4f3d64f5c77908
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:32:46 +0000

    c6feb0a7ae build: don't assign jelbourn to most review categories (#45668)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+36.sha-f5299a3'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+37.sha-c6feb0a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
