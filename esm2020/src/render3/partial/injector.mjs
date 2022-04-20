68467830b7037508358d3d8034681e3e17b3d445
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:38:03 +0000

    4ad7c2e4df docs: Update aio/content/guide/setup-local.md (#45644)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+41.sha-0231a36'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+43.sha-4ad7c2e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
