62a825d1ca66abfebf4e28c6785a08c709fa4930
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 17:48:28 +0000

    ba4542889e ci: update payload size golden file (#45699)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+57.sha-788f587'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+59.sha-ba45428'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
