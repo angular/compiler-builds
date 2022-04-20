4997c66276967b0bb8b8a12c22dc845ecee9232a
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:36:31 +0000

    0231a363fe build: preparation for primary branch rename in the Angular repos (#45681)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.13+39.sha-5d15595'));
+    definitionMap.set('version', o.literal('14.0.0-next.13+41.sha-0231a36'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
