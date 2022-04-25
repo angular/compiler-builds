a73fb1151361cc43cfce7fa82f8a04e4a4301147
Andrew Scott <atscott@google.com>
2022-04-25 22:54:13 +0000

    7992132be6 test(core): dynamic component creation and standalone injectors hierarchy (#45726)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.14+22.sha-31d7c3b'));
+    definitionMap.set('version', o.literal('14.0.0-next.14+23.sha-7992132'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
