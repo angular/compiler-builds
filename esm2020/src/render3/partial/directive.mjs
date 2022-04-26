cbb3948dad7f7bdd723e0cd7f38a7e37900b990b
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:29:56 +0000

    3a645c6bfc build: update actions/checkout action to v3.0.2 (#45764)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.4+3.sha-63398e0'));
+    definitionMap.set('version', o.literal('13.3.4+4.sha-3a645c6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
