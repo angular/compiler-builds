bc78f9e9ccd15c315878d64b149909fc65d02b36
Andrew Scott <atscott01@gmail.com>
2022-04-27 16:22:54 +0000

    2b5c2d6938 refactor(core): create a standalone injector during applicationBootstrap (#45766)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.14+30.sha-f1cc4a6'));
+    definitionMap.set('version', o.literal('14.0.0-next.14+32.sha-2b5c2d6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
