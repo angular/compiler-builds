65ba71ca0bd2a5f7d277225c6b1efc20f92769dc
Andrew Scott <atscott01@gmail.com>
2022-04-27 17:44:04 +0000

    3d4548450a refactor(compiler-cli): support schemas in standalone components (#45752)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.14+41.sha-2eb39c0'));
+    definitionMap.set('version', o.literal('14.0.0-next.14+42.sha-3d45484'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
