64bdc9fe629afc067bd5c27848158fc07f59ab5b
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 16:57:24 +0000

    14c4979a51 build(docs-infra): upgrade cli command docs sources to f258be5be (#45795)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.15+8.sha-a097be9'));
+    definitionMap.set('version', o.literal('14.0.0-next.15+5.sha-14c4979'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
