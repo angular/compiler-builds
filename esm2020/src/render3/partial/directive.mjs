4384b96055286fa4415f647cdeb8e0a0174443e8
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 20:29:39 +0000

    f0b5e830a5 build(bazel): change ngc-wrapped to use new bazelOpts.devmode (#45804)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.15+sha-0eee114'));
+    definitionMap.set('version', o.literal('14.0.0-next.15+sha-f0b5e83'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
