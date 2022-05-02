fc5984d60780c024e725a3955ad7e4da39e30a69
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:52:53 +0000

    e7cd87d237 ci: mark pkozlowski as availabe for PR reviews (#45842)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.5+4.sha-e59ed8c'));
+    definitionMap.set('version', o.literal('13.3.5+5.sha-e7cd87d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
