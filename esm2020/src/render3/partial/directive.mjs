d666242181c18f03248c57f2a821d173f44a3cc3
Dylan Hunn <dylhunn@gmail.com>
2022-05-03 18:24:07 +0000

    8ba4ddcb69 refactor(compiler-cli): improve error for `imports` in non-standalone cmp (#45851)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.15+sha-9cf14ff'));
+    definitionMap.set('version', o.literal('14.0.0-next.15+sha-8ba4ddc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.15+sha-9cf14ff'));
+    definitionMap.set('version', o.literal('14.0.0-next.15+sha-8ba4ddc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
