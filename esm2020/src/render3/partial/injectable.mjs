68643b4f35973157b2e3f859dca9abda5ef9b4ff
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 20:30:26 +0000

    e79260e445 release: cut the v14.0.0-next.16 release (#45875)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.15+sha-c988487'));
+    definitionMap.set('version', o.literal('14.0.0-next.16+sha-e79260e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
