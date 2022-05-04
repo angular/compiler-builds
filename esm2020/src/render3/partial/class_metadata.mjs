e6b150c87ac675d54a620a82e035591a4bfae38f
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:38:50 +0000

    aef5c92b0b ci: invalidate cache for windows job to prune unused nested `node_modules` (#45872)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-ecae55d'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-aef5c92'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
