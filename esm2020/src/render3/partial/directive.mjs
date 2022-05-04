62e3276f8e40927d205e82bab248f83702ebf438
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:43:32 +0000

    b5834deaa0 build(docs-infra): upgrade cli command docs sources to b71844f36 (#45871)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-68a6a07'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-b5834de'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
