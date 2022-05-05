b7284a2d85ee0da50c2236fe9c3a2ab1b62a3815
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:50:34 +0000

    b7fca64828 build(devtools): Optimize prod build for Angular DevTools (#45886)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-a212fb4'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-b7fca64'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
