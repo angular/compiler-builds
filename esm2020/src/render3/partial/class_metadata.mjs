b25a03d612eaa8ca1130af197b8402e8361722f4
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:06:59 +0000

    c9899637b0 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.6+2.sha-719199c'));
+    definitionMap.set('version', o.literal('13.3.6+4.sha-c989963'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
