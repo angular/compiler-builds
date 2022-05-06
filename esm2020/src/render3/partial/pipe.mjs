1bc04fa2764a306b2970538c84e9d9a82e79e025
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:16:09 +0000

    3f3812e20a docs(docs-infra): improve clarity of toh-pt2 li>button code (#45858)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-1a233c2'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-3f3812e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
