d5c873caf5c957f96834d3a8ac56e7e9d35c6d80
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:15:05 +0000

    1a233c2c55 docs: update the Angular DevTools supported version in docs and README (#45902)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-0ffb19a'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-1a233c2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
