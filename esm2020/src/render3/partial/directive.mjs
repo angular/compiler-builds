497cc95fe716c332e99a1558a6cebc6ab5e005cd
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:41:36 +0000

    5056cfe839 docs(docs-infra): clarify toh-2 error message (#45878)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.6+8.sha-6644165'));
+    definitionMap.set('version', o.literal('13.3.6+12.sha-5056cfe'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
