58a35b0352031fd8593503e9a7b4bb6a4a357a56
Andrew Kushnir <akushnir@google.com>
2022-05-10 17:23:15 +0000

    5ca3bcf4f8 fix(language-service): Add resource files as roots to their associated projects (#45601)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.6+24.sha-66c400c'));
+    definitionMap.set('version', o.literal('13.3.6+25.sha-5ca3bcf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
