1481f6a0de1628376d443f7275e28eed87530794
Andrew Kushnir <akushnir@google.com>
2022-05-10 16:55:25 +0000

    74321a45a0 refactor(core): remove ManualOnPush change detection (#45943)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-43ba4ab'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-74321a4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
