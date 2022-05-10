a496b601b5adbf770e15f42b3b83d22b6737892b
Andrew Kushnir <akushnir@google.com>
2022-05-10 00:48:26 +0000

    5d13e58aed fix(forms): Allow NonNullableFormBuilder to be injected. (#45904)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-next.16+sha-4a173eb'));
+    definitionMap.set('version', o.literal('14.0.0-next.16+sha-5d13e58'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
