931ad5c3eaecf8f0fcd22c9ff05fb8d178767194
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-05-11 17:35:52 +0000

    72c11f33b5 release: cut the v13.3.7 release

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.6+26.sha-3626464'));
+    definitionMap.set('version', o.literal('13.3.6+27.sha-72c11f3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
