8e78d76b0aae13cce8a03717501fe720dfe48206
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 16:19:50 +0000

    b056f1b823 fix(docs-infra): do not detect a running job as failed (#45948)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-1c0d0f0'));
+    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-b056f1b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
