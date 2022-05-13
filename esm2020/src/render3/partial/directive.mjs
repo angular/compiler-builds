e2bb5d5edb6d6b0f93b34053a6eff776fa1e36d5
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 18:37:18 +0000

    9a31c22cdd docs: remove Angular 11 from support table (#45980) PR Close #45980

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.8+1.sha-dfc4852'));
+    definitionMap.set('version', o.literal('13.3.8+2.sha-9a31c22'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
