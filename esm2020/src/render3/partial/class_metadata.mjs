3367ad963119b9573333a09ec4dec9f989febfb9
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 22:22:18 +0000

    57adaded2a docs: add selin tunc to contributors (#45989)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.8+3.sha-c224ada'));
+    definitionMap.set('version', o.literal('13.3.8+4.sha-57adade'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
