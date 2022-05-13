5d80b52df535d21d1738ec605d13f7196d4071a4
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 22:22:16 +0000

    19018ca61e docs: add selin tunc to contributors (#45989)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-bcef46d'));
+    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-19018ca'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
