c11635620cd413ab47e50f3751450a0256ed9a9d
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 16:26:16 +0000

    dfc4852d6c build: manually unshallow the repo for builds publishing, use absolute paths (#45979)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.7+1.sha-e223649'));
+    definitionMap.set('version', o.literal('13.3.8+1.sha-dfc4852'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
