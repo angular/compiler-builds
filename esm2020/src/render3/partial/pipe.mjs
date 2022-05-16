55b799265ac00db4736f122492552489719ea1ad
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 21:29:45 +0000

    3c690bc1e9 build: use updated commit sha for github actions (#46010)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-018550e'));
+    definitionMap.set('version', o.literal('14.0.0-rc.0+sha-3c690bc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
