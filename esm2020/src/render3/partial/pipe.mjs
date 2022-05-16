ea36871624baf4d07cc3adf7b185e14c9ede25bf
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:55:18 +0000

    dba6a60861 fix(forms): Value and RawValue should be part of the public API. (#45978)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-4070049'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-dba6a60'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
