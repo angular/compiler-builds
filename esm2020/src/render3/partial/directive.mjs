017e437cd9e290d2027ad4a20692bee788e093e6
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 18:05:00 +0000

    d7de3c54cd docs: remove Narco from hero list in code, text, and images (#46008)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.8+7.sha-db15a08'));
+    definitionMap.set('version', o.literal('13.3.8+8.sha-d7de3c5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
