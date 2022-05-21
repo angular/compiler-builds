6b82a7c4aff7c0b86e6579497ca541d4167218e3
Joey Perrott <josephperrott@gmail.com>
2022-05-21 20:38:55 +0000

    1d02c09010 build: update saucelabs key (#46089)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.8+11.sha-f708479'));
+    definitionMap.set('version', o.literal('13.3.9+2.sha-1d02c09'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
