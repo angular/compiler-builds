7a0786613a4383ef4f0ac7067b71ede4349731b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 21:16:20 +0000

    123782bfb6 docs: move all bazel testing info to a single location (#46084)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.9+4.sha-f6c265d'));
+    definitionMap.set('version', o.literal('13.3.9+13.sha-123782b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     // e.g. `selector: 'some-dir'`
