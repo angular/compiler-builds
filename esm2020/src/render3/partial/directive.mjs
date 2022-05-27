561b21d3acb37e055f5c425baf14fc0757cce3af
Andrew Scott <atscott01@gmail.com>
2022-05-27 17:35:06 +0000

    5dee53b352 docs: create a dedicated page for the NG0203 error (#46166)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-d7c3581'));
+    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-5dee53b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
