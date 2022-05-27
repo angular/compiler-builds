1cc372e1c3df82d7e95dd40c854081babdb60ee2
Andrew Scott <atscott01@gmail.com>
2022-05-27 19:05:04 +0000

    ba9f30c9a6 fix(core): include component name into unknown element/property error message (#46160)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-5dee53b'));
+    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-ba9f30c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-5dee53b'));
+    definitionMap.set('version', o.literal('14.0.0-rc.2+sha-ba9f30c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
