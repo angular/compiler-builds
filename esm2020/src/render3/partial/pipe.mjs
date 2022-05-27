5736e093b829f2f85bd4c5696fef36fe4800aed7
Andrew Scott <atscott01@gmail.com>
2022-05-27 17:32:29 +0000

    f87aa2da8c build(docs-infra): upgrade cli command docs sources to 874ad2184 (#46157)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-a31d821'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-f87aa2d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
