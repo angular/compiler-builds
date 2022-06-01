6c1d854bb8c23618e36918f48b0fbf5eccbde77e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-01 23:27:10 +0000

    bf6b44be35 docs(core): mark the standalone APIs as Developer Preview (#46050)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.3+sha-536939e'));
+    definitionMap.set('version', o.literal('14.0.0-rc.3+sha-bf6b44b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
