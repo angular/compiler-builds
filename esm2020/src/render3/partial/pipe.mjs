a5a0ad9d02ba3e73d9b8466c7b5f5b2c663f24d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 21:05:27 +0000

    36983a469a build(docs-infra): upgrade cli command docs sources to 847de3e7b (#46209)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0+sha-127e794'));
+    definitionMap.set('version', o.literal('14.0.0+sha-36983a4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
