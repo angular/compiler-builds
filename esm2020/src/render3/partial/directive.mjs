701459893c8772bfa61d2881627c86c31d928a2e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 17:10:53 +0000

    6280a98ff1 release: cut the v14.0.0 release (#46223)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.0-rc.3+sha-bf6b44b'));
+    definitionMap.set('version', o.literal('14.0.0+sha-6280a98'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
