fc38e15e246d1b67c15cddecdb037760c03eb61d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:05:37 +0000

    a9e8a212f8 docs: mark version 13 as LTS in releases guide (#46268)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-c083f6d'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-a9e8a21'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
