a6ba12070a51436217700201528b66c26b5467a7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:04:59 +0000

    c083f6dcb2 build: lock file maintenance (#46270)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-d67cf92'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-c083f6d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
