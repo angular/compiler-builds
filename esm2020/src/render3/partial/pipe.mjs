7eafbe3a35754c80836fd794c127d31a9d799e86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-07 19:08:11 +0000

    41c6f7a079 build: update dev-infra shared code to latest revision (#46292)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('13.3.11+2.sha-94c2cfb'));
+    definitionMap.set('version', o.literal('13.3.11+sha-41c6f7a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
