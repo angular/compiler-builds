187ddb94939cfba2a312785c007ba8f067b5e69d
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-10 18:17:36 +0000

    71b6b9e48b docs: Add kevindaviladev to contributors (#46330)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-adb21a2'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-71b6b9e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
