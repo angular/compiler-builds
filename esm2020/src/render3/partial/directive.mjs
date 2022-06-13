5b0befe141ab0a64a7bf69bc95983315efed59a5
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:57:26 +0000

    d6880c92f9 build: update angular version of in-memory-web-api (#46344)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-b1a3dec'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-d6880c9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
