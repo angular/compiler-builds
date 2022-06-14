69c656afe16ac0742062ecb366653284feedf20e
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-14 16:17:11 +0000

    86303f73c9 docs(core): fix broken link <BLANK LINE> Dead link is updated with new, working, and updated link. (#46359) PR Close #46359

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-714183d'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-86303f7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
