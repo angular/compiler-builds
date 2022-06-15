559b5b0e6e6d652fbceacc5a5fdb629e3b725df0
GitHub <noreply@github.com>
2022-06-15 17:46:47 +0000

    0ab0baec15 docs: release notes for the v14.0.2 release (#46376)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-7a37646'));
+    definitionMap.set('version', o.literal('14.1.0-next.0+sha-0ab0bae'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
