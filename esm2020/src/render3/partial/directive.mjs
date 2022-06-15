08e903d7e0e9b152bbaac183d1c4c513ec7ff8a6
Andrew Kushnir <akushnir@google.com>
2022-06-15 21:24:01 +0000

    14b96287e9 refactor(common): align tree shakable error messages to new format (#46370) Align tree shakable error messages are simplified with the new format and errorMessage variables are removed.

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.2+sha-88e1655'));
+    definitionMap.set('version', o.literal('14.0.2+sha-14b9628'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
