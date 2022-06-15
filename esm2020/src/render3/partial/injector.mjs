fe4f530a64c87d21348af998368b51a35248695d
GitHub <noreply@github.com>
2022-06-15 18:31:12 +0000

    c2dab9d8cb release: cut the v14.1.0-next.1 release (#46377)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.0+sha-0ab0bae'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-c2dab9d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
