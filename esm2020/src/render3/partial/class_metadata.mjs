622af2391f4c543ff287acd6df839519f55bc658
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:06:28 +0000

    ffedcdec08 build: replace copied `spec_bundle` rule in devtools with dev-infra rule (#46437)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-e8a33e7'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-ffedcde'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
