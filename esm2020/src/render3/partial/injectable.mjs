9d3612a14a7b721f2ef37b83965ec5980bc6a323
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:12:54 +0000

    08d3db232c fix(platform-server): invalid style attribute being generated for null values (#46433)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-96c7074'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-08d3db2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
