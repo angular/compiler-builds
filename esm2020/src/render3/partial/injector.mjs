dbb2818784b8799f7243f968e562b5548e0187b7
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:37:13 +0000

    db5d2683c8 fix(docs-infra): slightly improve aio-top-menu css (#46429)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-3d8787c'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-db5d268'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
