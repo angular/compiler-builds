b6c82fb409237f9bab7efa8a6b443a8fafc1e956
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:04:59 +0000

    1aa27ebbad docs: fix base href haser tag (#46431) I add '>' end of header tag Fixes #46430

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-88f53dc'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-1aa27eb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
