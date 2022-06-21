11ed556387c09189fc6ba3074e851c2945b6b9c9
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:07:30 +0000

    96c70741ab docs: update ng-de date (#46446)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-0c9af71'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-96c7074'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
