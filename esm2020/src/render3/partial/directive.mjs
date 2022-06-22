4422167c356cf6702c2f79423259922318ec63a3
Andrew Kushnir <akushnir@google.com>
2022-06-22 22:11:25 +0000

    63396a1ea7 build: avoid use of `ts-node` wrapper script as it breaks child process forking (#46456)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-2d713f5'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-63396a1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
