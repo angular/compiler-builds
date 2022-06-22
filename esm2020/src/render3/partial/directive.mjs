8415a5faa636768e72a0dc0106855911753fec74
Andrew Kushnir <akushnir@google.com>
2022-06-22 16:32:11 +0000

    cce395a928 build: load ng-dev release package builder using CJS interop (#46454)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.1+sha-2512f65'));
+    definitionMap.set('version', o.literal('14.1.0-next.1+sha-cce395a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
