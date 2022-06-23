3be609892d33f05b1e3f6e48dee53b428c8dc56d
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 23:58:08 +0000

    1da3a051b5 refactor(core): make i18n error messages tree shakable (#46468) Make the error messages tree shakable from the production build to reduce the bundle size.

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-4eb1ca1'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-1da3a05'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
