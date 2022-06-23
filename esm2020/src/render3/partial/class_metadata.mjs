6301e09e765a8081467348ee80e809f9a5f9dd69
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:06:07 +0000

    a7db899ade build: speed up `ng-dev` config loading by disabling type-checking (#46476)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-8d48549'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-a7db899'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
