36f064533c9f9513d55025139dc5dbdaad3176e8
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:06:50 +0000

    b417370383 fix(animations): enable shadowElements to leave when their parent does (#46459)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-a7db899'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-b417370'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
