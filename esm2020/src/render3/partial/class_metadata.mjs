1a1c5666aa9b4b4708f9e2e5c9d9d733b0441f18
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:46:47 +0000

    0c66032608 docs(forms): add usage notes (#46472)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.3+sha-be43177'));
+    definitionMap.set('version', o.literal('14.0.3+sha-0c66032'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
