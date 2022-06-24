59f7f88c8e2155cb5102ccf5ae82a4e37a894edd
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 22:45:38 +0000

    599870f121 refactor(docs-infra): create external-link-with-icon mixin (#46482)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-1c39aac'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-599870f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
