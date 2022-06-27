b6431405040092471d4ebb81b4c130b3e0fb20da
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 18:15:26 +0000

    21cce3a82b docs: add how to pass array options to the CLI (#46505)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-09ceb4e'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-21cce3a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
