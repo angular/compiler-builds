46789d7cd1e69cf7ed1b1109321d5b3acba4d1d3
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 22:59:41 +0000

    de171d8dde test(core): replace hand-written instructions in change detection tests with TestBed (#46529)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-2e59f9f'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-de171d8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
