bc1dc7ec8f0a3a0fec1c565cbcebdeb233649adf
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 20:13:22 +0000

    ee39cf62bc fix(router): Ensure that new `RouterOutlet` instances work after old ones are destroyed (#46554)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-f6a096e'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-ee39cf6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
