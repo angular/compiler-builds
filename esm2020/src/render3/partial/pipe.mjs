9d90c2db623244cb7f2a04e0b1667ea052abef5e
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:27:36 +0000

    2758a30a34 build: update dependency @rollup/plugin-commonjs to v22 (#46556) PR Close #46556

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-a33d4ee'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-2758a30'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
