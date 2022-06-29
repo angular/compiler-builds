e43871c101c31c943dd313d1b61ac468536e9efb
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 08:13:26 +0000

    72976c7a28 test: update golden symbols (#46582)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.3+sha-2298fd1'));
+    definitionMap.set('version', o.literal('14.0.3+sha-72976c7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
