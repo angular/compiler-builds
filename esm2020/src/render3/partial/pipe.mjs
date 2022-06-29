687f9fcc6c41ed37a73cd33fbf6315e461b8e687
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:34:21 +0000

    d478e60c4f ci: enable renovate commit message body table to fix minimum commit message (#46611)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-47c65ac'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-d478e60'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
