1a2b72be45f5c28c800dc46bc1d5edb89e32230c
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:41:04 +0000

    9d30298250 build: update bazel setup to v5.5.1 (#46543) PR Close #46543

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.2+sha-19364cf'));
+    definitionMap.set('version', o.literal('14.1.0-next.2+sha-9d30298'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
