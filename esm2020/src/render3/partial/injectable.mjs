00294f8cfbf87f562b2142572cf0766bb9b27f09
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:52:10 +0000

    55609aa564 ci: fix pullapprove `fnmatch` patterns due to recent changes (#46589)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.3+sha-72976c7'));
+    definitionMap.set('version', o.literal('14.0.3+sha-55609aa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
