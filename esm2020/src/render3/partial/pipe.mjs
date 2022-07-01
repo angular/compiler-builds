0a65d97824fdb2d00182f369e75f85e2f42f8819
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:37:37 +0000

    ee2e4917e6 docs: Update runGuardsAndResolvers documentation (#46671)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.3+sha-a7a14df'));
+    definitionMap.set('version', o.literal('14.1.0-next.3+sha-ee2e491'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
