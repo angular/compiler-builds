afa16e1da0740b19aed187c43adc5af93a4fe48b
Andrew Scott <atscott01@gmail.com>
2022-07-01 20:43:56 +0000

    dd3e0968ec refactor(router): Remove custom error message for invalid guards (#46678)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.3+sha-915e82d'));
+    definitionMap.set('version', o.literal('14.1.0-next.3+sha-dd3e096'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
