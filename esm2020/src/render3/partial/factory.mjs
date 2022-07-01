d31a1b64cdd709e7956fd93520b8158dae80a0a2
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:41:06 +0000

    853b1c1bc9 docs: move section on subscribing to observable (#46655)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.4+sha-7fe81a9'));
+    definitionMap.set('version', o.literal('14.0.4+sha-853b1c1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
