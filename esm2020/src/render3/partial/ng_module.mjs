d856063d091503484f7d766c0861769be5f229f3
GitHub <noreply@github.com>
2022-07-06 17:00:31 +0000

    458d3469cb release: cut the v14.1.0-next.4 release (#46726)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.3+sha-6c1357d'));
+    definitionMap.set('version', o.literal('14.1.0-next.4+sha-458d346'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
