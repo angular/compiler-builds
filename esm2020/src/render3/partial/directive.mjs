7a232d532216bf17252ed151ae1f0270e9413bd7
GitHub <noreply@github.com>
2022-07-06 16:47:08 +0000

    054de6dc34 release: cut the v14.0.5 release (#46724)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.4+sha-aee02b7'));
+    definitionMap.set('version', o.literal('14.0.5+sha-054de6d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
