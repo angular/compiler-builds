ab714a5fbde69f0fa56a428a72698fa7e1411d98
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 23:08:48 +0000

    42b6d23806 build: update scorecard action dependencies (#46706)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.4+sha-392c567'));
+    definitionMap.set('version', o.literal('14.1.0-next.4+sha-42b6d23'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
