b6b1f939e1dfbdd2927fb60b6c58bbb2cf13a789
Andrew Scott <atscott01@gmail.com>
2022-07-06 14:49:19 +0000

    e96ece901c build: update dependency concurrently to v7 (#46698)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.3+sha-5fd3bb6'));
+    definitionMap.set('version', o.literal('14.1.0-next.3+sha-e96ece9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
