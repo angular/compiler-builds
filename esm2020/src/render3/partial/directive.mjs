6af9cd65caebbb39bc10dae299a1cc7480e3075c
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-08 22:17:40 +0000

    28bd6dd3d8 build: update eslint dependencies to v5.30.5 (#46705)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-50126af'));
+    definitionMap.set('version', o.literal('14.0.5+sha-28bd6dd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
