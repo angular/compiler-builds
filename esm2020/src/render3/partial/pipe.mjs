a7549edd9aeb264eeceae3d00fa42964aaade510
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 20:20:40 +0000

    d51e0f27d7 docs(core): add full example of directive with typed context (#46689)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-ec1ff66'));
+    definitionMap.set('version', o.literal('14.0.5+sha-d51e0f2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
