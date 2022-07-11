f61bc521813d14ca25689f425bb95fbc150f19e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 19:57:58 +0000

    ec1ff66e34 Revert "build: update dependency firebase-tools to v11" (#46777)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-9749199'));
+    definitionMap.set('version', o.literal('14.0.5+sha-ec1ff66'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
