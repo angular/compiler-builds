10ba851ca69d61beddbbb4a8e474c100ab99405a
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 21:13:35 +0000

    7b9269c5e4 build: account for integration test rule change (#46778)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.4+sha-19e6d9c'));
+    definitionMap.set('version', o.literal('14.1.0-next.4+sha-7b9269c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
