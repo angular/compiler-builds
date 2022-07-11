96f4df2ee16a1ebb52eddbd505e1d5b8409761e9
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 22:50:23 +0000

    023c218c43 docs: update topics to include feedback from PR review (#46647)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-9d2d92a'));
+    definitionMap.set('version', o.literal('14.0.5+sha-023c218'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
