ad232bc5b3260d5c1c8edb4e0b3c563e9bf4212a
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 18:06:49 +0000

    93c65e7b14 feat(compiler-cli): add extended diagnostic for non-nullable optional chains (#46686)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.4+sha-c0ca3fc'));
+    definitionMap.set('version', o.literal('14.1.0-next.4+sha-93c65e7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
