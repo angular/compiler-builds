83372b283ca85eb251d156831b824e3a869fb4e4
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 16:32:13 +0000

    785b098260 build: update cross-repo angular dependencies to 6af9f6c (#46791)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-98b41bf'));
+    definitionMap.set('version', o.literal('14.0.5+sha-785b098'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
