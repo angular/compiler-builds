e9fa8424e50427fd2dbb58f0a01f6bbdb95f1176
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 18:05:50 +0000

    c0ca3fc71a fix(forms): expose ControlConfig in public API (#46594)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-next.4+sha-a6d5fe2'));
+    definitionMap.set('version', o.literal('14.1.0-next.4+sha-c0ca3fc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
