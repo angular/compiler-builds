2846d604e6c616e361bdc14c5d047fd1cd05c10c
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 22:54:06 +0000

    862dd03764 refactor(animations): refactor non-animatable check to be timeline based (#46666)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-99697da'));
+    definitionMap.set('version', o.literal('14.0.5+sha-862dd03'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
