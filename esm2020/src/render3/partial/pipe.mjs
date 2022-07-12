a4d1441e9a4e747b059d46a6a08573d3ef868a56
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 20:01:23 +0000

    4a2c6e3693 test: update payload size goldens for AIO after analytics change (#46801)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.5+sha-4f469cb'));
+    definitionMap.set('version', o.literal('14.0.5+sha-4a2c6e3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
