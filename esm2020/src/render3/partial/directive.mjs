5bd23e135ee171a73926a23411c5fe4916a2fd06
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 23:11:18 +0000

    211c35358a docs: correct debugging task step (#46832)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-66e4122'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-211c353'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
