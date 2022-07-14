a0058e9e99be417a6ded068b369c753c84a0fc9c
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-14 21:26:55 +0000

    26ea97688c feat(router): Make router directives standalone (#46758)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-4a81fe7'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-26ea976'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
