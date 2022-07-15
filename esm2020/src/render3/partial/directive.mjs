aa7083c590be2d8d77f0922420d7c7e447dec074
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 16:17:09 +0000

    571d7debb5 build: update cross-repo angular dependencies to 84dd092 (#46848)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-rc.0+sha-5e09cd1'));
+    definitionMap.set('version', o.literal('14.1.0-rc.0+sha-571d7de'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
