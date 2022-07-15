64ed9bdb0cee78299a7bca68879bf48ed5fb5670
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 22:18:25 +0000

    089efa1ac9 refactor(forms): simplify group builder function (#46844)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-60d08b7'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-089efa1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
