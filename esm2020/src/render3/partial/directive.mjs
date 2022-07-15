a498923a4b46135b49a3f82d7a90f25a4c82e663
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-15 22:18:38 +0000

    031deb3b95 refactor(forms): simplify group builder function (#46844)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.6+sha-e76f9a1'));
+    definitionMap.set('version', o.literal('14.0.6+sha-031deb3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
