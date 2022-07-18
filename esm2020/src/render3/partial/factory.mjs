37f91b98fd159f7b12d923abb3171ea24ecf5128
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 17:01:11 +0000

    4a68b0f6ab docs: fix code style (#46862)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-a0b4a92'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-4a68b0f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
