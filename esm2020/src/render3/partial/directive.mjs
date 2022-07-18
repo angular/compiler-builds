63a663e7f2719b86723c33c5e9664b4167ce3e1a
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 20:14:34 +0000

    6b8e60c06a fix(compiler-cli): improve the missingControlFlowDirective message (#46846)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0-rc.0+sha-5004b6e'));
+    definitionMap.set('version', o.literal('14.1.0-rc.0+sha-6b8e60c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
