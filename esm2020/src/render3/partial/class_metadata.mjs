7c0c5e3a9778748d96022f6cf59dd5e9a0484689
Andrew Kushnir <akushnir@google.com>
2022-07-19 17:02:08 +0000

    d361eaaf83 test: convert `source-map` core tests to use `async/await` instead of `fakeAsync` (#46888)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.0.6+sha-f42e5db'));
+    definitionMap.set('version', o.literal('14.0.6+sha-d361eaa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
