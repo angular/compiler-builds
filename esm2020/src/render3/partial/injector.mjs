01631ed546022df48e5a74bccbff1968b962d3af
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:05:36 +0000

    0d192e8d0b docs: add info about request timeouts (#46882)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-0dc754a'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-0d192e8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
