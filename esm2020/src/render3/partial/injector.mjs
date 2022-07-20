bac9dbd4bcd74fc3185591be10f284254c37c208
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-07-20 17:56:31 +0000

    6cd5b759aa docs: release notes for the v14.0.7 release

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-3562eb1'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-6cd5b75'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
