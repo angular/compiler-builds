8ba56a12996e7e368e2279c7e4d3c5558eb2f888
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 15:56:01 +0000

    ff7c71653a build: update doc approvers (#46939)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.0+sha-1eb77a2'));
+    definitionMap.set('version', o.literal('14.1.0+sha-ff7c716'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
