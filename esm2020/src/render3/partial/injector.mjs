2163878c495fb693b0057060eebe3ddfc4ff24fc
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 08:42:09 +0000

    f4348360a6 docs(router): remove incorrect deprecation text in `InitialNavigation` (#46916)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-46c44de'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-f434836'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
