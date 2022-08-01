70bf59043d925d87762e125a34eda2759cea83cf
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:09:19 +0000

    99e5fa157d test(service-worker): remove redundant ternary operator from `MockRequest` initialization (#46912)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-49e1912'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-99e5fa1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
