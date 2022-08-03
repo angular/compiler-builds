e029319c87570a2f3a3b6e276fdfa2ef636513f7
Andrew Kushnir <akushnir@google.com>
2022-08-03 18:47:35 +0000

    cb0fe50b1d refactor(common): make NgOptimizedImage directive standalone

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-37ce31f'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-cb0fe50'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
