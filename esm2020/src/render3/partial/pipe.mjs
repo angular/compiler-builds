f346c0d67aebbabe35840b642ed7b38614e26bb6
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 20:24:30 +0000

    1d39ab6e6b build: update io_bazel_rules_sass digest to 70ffe01 (#47023)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.1+sha-d0f4f1d'));
+    definitionMap.set('version', o.literal('14.1.1+sha-1d39ab6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
