a5db6d7322ce45c7467cce8eb59bd8ff2236a133
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 18:02:33 +0000

    44da5a6efe build: add atscott to unavailable users (#47102)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-4e3fc7e'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-44da5a6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
