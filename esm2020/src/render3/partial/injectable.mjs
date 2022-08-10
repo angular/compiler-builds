16536730c60b3028fb74c483f7788f49543494cc
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 00:19:46 +0000

    91954cf20e fix(zone.js): Fix ConsoleTask interface typo (#47090)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-next.0+sha-8546b17'));
+    definitionMap.set('version', o.literal('14.2.0-next.0+sha-91954cf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
