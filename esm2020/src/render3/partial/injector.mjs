f4c6fca2bc7499a6af33733acb20252b54569f2c
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 18:25:43 +0000

    063035f86d docs: fix embedded view description issue (#46957)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.2+sha-0a99130'));
+    definitionMap.set('version', o.literal('14.1.2+sha-063035f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
