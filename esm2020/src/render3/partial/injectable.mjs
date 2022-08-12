1a19cecd2857abd65d3bfa1b82241089be1fa6ca
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 18:34:32 +0000

    970e68ae38 docs: remove duplicated styles (#47143)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.1.2+sha-a5ca049'));
+    definitionMap.set('version', o.literal('14.1.2+sha-970e68a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
