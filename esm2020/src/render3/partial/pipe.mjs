f5fce9c633fdaa52f6edef4c37d8ccec69c6d321
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 17:46:49 +0000

    c467eb78d9 build: update all non-major dependencies (#47162)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.3.0-next.0+sha-d7c0039'));
+    definitionMap.set('version', o.literal('14.3.0-next.0+sha-c467eb7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
