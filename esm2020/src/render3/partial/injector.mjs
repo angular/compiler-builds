440470f01fdf52409dbec13ba31fb4a3cb92790c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 23:33:32 +0000

    b34320128d docs: add Angular Master website to resources list (#47129) PR Close #47129

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.3.0-next.0+sha-54ceed5'));
+    definitionMap.set('version', o.literal('14.3.0-next.0+sha-b343201'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
