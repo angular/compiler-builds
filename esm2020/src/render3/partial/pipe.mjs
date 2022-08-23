700399d32b69f19c72e9b7f61e1baafb28a56f8f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:42:25 +0000

    0dd6102c79 build: update io_bazel_rules_sass digest to 6802532 (#47104)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0-rc.0+sha-e2be526'));
+    definitionMap.set('version', o.literal('14.2.0-rc.0+sha-0dd6102'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
