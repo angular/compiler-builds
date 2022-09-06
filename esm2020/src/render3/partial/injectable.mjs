bf2a9b314fd11adfbfafafe5374ab521885c6b58
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:31:52 +0000

    0d13cc7848 docs: fixing typos and grammatical errors in several documentation files (#47254)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.0+sha-b8fd0da'));
+    definitionMap.set('version', o.literal('15.0.0-next.0+sha-0d13cc7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
