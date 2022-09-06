ddba35ba5fd3d1ea4f50551d6623891bfcb89b7a
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:56:54 +0000

    99c12c92a8 docs: apply suggestions from code review (#47350)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.0+sha-e4f6eed'));
+    definitionMap.set('version', o.literal('15.0.0-next.0+sha-99c12c9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
