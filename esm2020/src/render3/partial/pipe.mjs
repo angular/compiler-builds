e38834a00e14f7e250420f5e05455265063ed198
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:25:45 +0000

    6091786696 fix(service-worker): include headers in requests for assets (#47260)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.0+sha-626340b'));
+    definitionMap.set('version', o.literal('14.2.0+sha-6091786'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
