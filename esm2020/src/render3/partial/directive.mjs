71ebe22f17a11e7d7484322b6a8238418b1c5813
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:45:54 +0000

    ca7bf65933 fix(common): rename `rawSrc` -> `ngSrc` in NgOptimizedImage directive (#47362)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.1+sha-710d1da'));
+    definitionMap.set('version', o.literal('15.0.0-next.1+sha-ca7bf65'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
