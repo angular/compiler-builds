458b7bd1e7a3f9bb4f78dc6d9e47a0bf05b17690
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 19:48:35 +0000

    2d7ce00caa build(docs-infra): introduce new process for generating data for the events page (#45588)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.1+sha-c9bdf9b'));
+    definitionMap.set('version', o.literal('14.2.1+sha-2d7ce00'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
