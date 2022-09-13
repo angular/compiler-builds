76bb23107cbf8867b0f002c62833ab6fac52de5e
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 16:38:17 +0000

    ebc0135239 build: update all non-major dependencies (#47415)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.1+sha-6c7cd6b'));
+    definitionMap.set('version', o.literal('15.0.0-next.1+sha-ebc0135'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
