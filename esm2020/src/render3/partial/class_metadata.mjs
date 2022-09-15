471a977d19b238e4e732e34fd40862ce84fbf0ad
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 03:07:15 +0000

    cc2d32adb9 release: cut the v14.2.2 release

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.1+sha-a0cb501'));
+    definitionMap.set('version', o.literal('14.2.2+sha-cc2d32a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
