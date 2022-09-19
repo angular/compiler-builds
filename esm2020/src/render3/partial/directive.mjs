7061912b6e5d9a7724e5f6e5f24afc2ca6d955c7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 15:39:05 +0000

    c9991ca49d build: update dependency google-closure-compiler to v20220905 (#47437)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.2+sha-53db8ab'));
+    definitionMap.set('version', o.literal('15.0.0-next.2+sha-c9991ca'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
