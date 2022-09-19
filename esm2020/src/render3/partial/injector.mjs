da61a7366044f061f9d8174bbf631b7fc7dbe732
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 11:31:07 +0000

    a192c2de73 fix(docs-infra): apply `text-top` instead of `top` for table alignment (#47457)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.2+sha-14cb9fb'));
+    definitionMap.set('version', o.literal('15.0.0-next.2+sha-a192c2d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
