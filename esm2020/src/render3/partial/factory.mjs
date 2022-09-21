b39a85d95153ea30c765e6298eb3eea37932aa89
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 18:34:37 +0000

    7a5ba93ce2 release: cut the v15.0.0-next.3 release

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.2+sha-0ebd326'));
+    definitionMap.set('version', o.literal('15.0.0-next.3+sha-7a5ba93'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
