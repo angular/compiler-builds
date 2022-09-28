09ad0f8494cf32b0767448d73cffa26faf2464e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-28 21:37:29 +0000

    c8d891ec30 release: cut the v14.2.4 release

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.3+sha-10a3cef'));
+    definitionMap.set('version', o.literal('14.2.4+sha-c8d891e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
