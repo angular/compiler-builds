c4caa1ceb6926ce91eb886da4181caa972d511a4
Andrew Kushnir <akushnir@google.com>
2022-10-04 19:11:15 +0000

    9b32883b74 docs: add Jordan Powell to GDE resources (#47598)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.4+sha-0aed24c'));
+    definitionMap.set('version', o.literal('14.2.4+sha-9b32883'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
