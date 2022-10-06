c60df3787d7794392feef2ff0185e8f419a1f7da
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:21:59 +0000

    c09c1bb033 docs(http): fix copy-paste error in HttpClientJsonpModule docs (#47502)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-16c8f55'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-c09c1bb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
