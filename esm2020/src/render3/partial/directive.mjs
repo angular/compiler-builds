8a0dcb146016cf6d1ae94674062bbed7d1b42c68
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:48:41 +0000

    1095dc5fd1 docs: fix documentation linting errors in ajs-quick-reference.md (#47526)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.5+sha-5782d63'));
+    definitionMap.set('version', o.literal('14.2.5+sha-1095dc5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
