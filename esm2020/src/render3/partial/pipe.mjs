2cd3ffddec120648f9e32140542acae9556f6b1f
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 21:19:48 +0000

    214baeae8b docs: fixed typo in AsyncValidatorFn (#47465)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-bf6679a'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-214baea'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
