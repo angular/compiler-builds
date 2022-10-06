c20f609d70d977791149c2ee7a93d02a8faaa5b2
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 18:32:56 +0000

    db28badfe6 feat(core): enable the new directive composition API (#47642)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-e0dd2c5'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-db28bad'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
