90b324e7bd54a20d14e213cade583e296e334e31
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 20:23:30 +0000

    5782d632f5 ci: cleanup PullApprove config file (#47611)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.5+sha-6f4f162'));
+    definitionMap.set('version', o.literal('14.2.5+sha-5782d63'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
