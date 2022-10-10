c9457426cc6ac42ba5801761cafda8050d89c744
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 20:22:55 +0000

    6bcde0e20a build(docs-infra): upgrade cli command docs sources to 46cb32b90 (#47703)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-c0c7efa'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-6bcde0e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
