e3d1c174b31208d2ce81630705c2b3e323059362
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-10 20:23:06 +0000

    f467c9eabb build: update scorecard action dependencies (#47385)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-6bcde0e'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-f467c9e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
