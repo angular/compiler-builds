9baef1753289ea15ac2d0297858d1efc46f5268e
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 23:05:10 +0000

    a910c8328a build(devtools): migrate to manifest v3 (#47575)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-bbbd4c3'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-a910c83'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
