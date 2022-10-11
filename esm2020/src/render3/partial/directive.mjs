2d78cacbeb1f0d5ece5c9275e608a3d80fab5103
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 17:41:16 +0000

    b618b5aa86 fix(zone.js): cancel tasks only when they are scheduled or running (#46435)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-next.5+sha-96b7fe9'));
+    definitionMap.set('version', o.literal('15.0.0-next.5+sha-b618b5a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
