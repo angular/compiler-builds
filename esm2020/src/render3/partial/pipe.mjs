c70d12fb891b1c86bf1c96800872afe59bcef1ea
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 17:02:07 +0000

    ddbc4931e6 refactor(common): make `provideHttpClient` return `EnvironmentProviders` (#47758)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-6c98ca4'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-ddbc493'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
