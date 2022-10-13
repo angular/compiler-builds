4bd95e0f7b74de1e73a7009e727b5a13cc50fc5a
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 16:56:16 +0000

    2ca213747c docs: update of documentation contrib. guide (#47381)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.6+sha-24950dd'));
+    definitionMap.set('version', o.literal('14.2.6+sha-2ca2137'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
