9f0e9c643557265bbac55a83b3d13ffc3adebf9c
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:41:27 +0000

    bdd4d14db9 refactor(common): drop deprecated selector from the NgOptimizedImage directive (#47798)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-ce5880f'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-bdd4d14'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
