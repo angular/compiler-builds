aac9b114f234b71d1e883d9b4f90961ce6138459
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 15:51:12 +0000

    fb273f6461 docs: fix minor typo in hierarchical-dependency-injection.md (#47812)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-3a18398'));
+    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-fb273f6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
