b84492207bdabb6d2e2504a901e6242585093cc3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:01:58 +0000

    8161b98069 build: update recommended launch vscode config to use proper bazel dist path (#47831)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.7+sha-7c4f77a'));
+    definitionMap.set('version', o.literal('14.2.7+sha-8161b98'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
