bfdd06013e3e1939d16a005c9b7e6b4e63e51269
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:52:24 +0000

    ed48887068 build: update cross-repo angular dependencies (#47871)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-2908eba'));
+    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-ed48887'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
