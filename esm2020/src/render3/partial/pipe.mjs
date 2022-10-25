a978698d7b81f9f16ec4254f1724db5487dea38e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 08:47:47 +0000

    a8ce8f7fc4 docs: update typescript configuration (#47842)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-c0903ac'));
+    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-a8ce8f7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
