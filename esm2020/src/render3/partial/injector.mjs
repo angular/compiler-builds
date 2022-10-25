16b96af68899ed69c39bb290ba9e5e7a93c77e1f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:10:34 +0000

    2908eba59c fix(platform-server): align server renderer interface with base renderer (#47868)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-a8ce8f7'));
+    definitionMap.set('version', o.literal('15.0.0-rc.0+sha-2908eba'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
