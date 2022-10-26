9e42b04e1753b4d3f86ee3ea6c34ea5110931c21
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:41:51 +0000

    8707475341 release: cut the v14.2.8 release

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.7+sha-9d12a07'));
+    definitionMap.set('version', o.literal('14.2.8+sha-8707475'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
