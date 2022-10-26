980839e7ed96ef64053973799774bb46999632a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 14:01:15 +0000

    0984039a8c build: update bazel setup to v5.7.0 (#47436)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-ad5eb5d'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-0984039'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
