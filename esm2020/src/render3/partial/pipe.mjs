8d68c68a2a98369f1e4e32b683a16136c29f86f3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 18:12:35 +0000

    9d12a072b8 Revert "build: add atscott to unavailable list for pullapprove (#47658)" (#47877)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.7+sha-a478ab1'));
+    definitionMap.set('version', o.literal('14.2.7+sha-9d12a07'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
