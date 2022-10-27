b502d156a2717794ec9a0c838ac02599a60b24d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 07:48:18 +0000

    2d085dc037 fix(core): allow readonly arrays for standalone imports (#47851)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-c9a25f8'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-2d085dc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
