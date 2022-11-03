5eb4a92808791235f1c97b24131f3721227368ca
Andrew Kushnir <akushnir@google.com>
2022-11-03 18:37:47 +0000

    13b863a1bf Revert "fix(core): hardening rules related to the attribute order on iframe elements (#47935)" (#47959)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.1.0-next.0+sha-2d08965
+ * @license Angular v15.1.0-next.0+sha-13b863a
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -3674,8 +3674,6 @@ export declare class R3Identifiers {
     static sanitizeUrlOrResourceUrl: outputAst.ExternalReference;
     static trustConstantHtml: outputAst.ExternalReference;
     static trustConstantResourceUrl: outputAst.ExternalReference;
-    static validateIframeAttribute: outputAst.ExternalReference;
-    static validateIframeStaticAttributes: outputAst.ExternalReference;
 }
 
 export declare interface R3InjectableMetadata {
