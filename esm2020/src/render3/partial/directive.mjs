f5d065f4f8a9985b05232099b68a43012cdd31f3
Andrew Kushnir <akushnir@google.com>
2022-11-04 01:13:42 +0000

    6d4f75931b build: add `--keep_going` to components tests. (#47958)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.9+sha-bab5f9a'));
+    definitionMap.set('version', o.literal('14.2.9+sha-6d4f759'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
