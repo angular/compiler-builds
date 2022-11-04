2f994dfbff5de854549b93ea0640b6119311ba9f
Andrew Kushnir <akushnir@google.com>
2022-11-04 16:47:39 +0000

    2ed5aeffd8 fix(router): fix redirectTo on named outlets - resolves #33783 (#47927)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-bc0fc02'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-2ed5aef'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
