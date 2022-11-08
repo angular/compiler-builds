515d75e336f81065170c7ab871f7f4478d67c2c8
Andrew Kushnir <akushnir@google.com>
2022-11-08 19:51:58 +0000

    2efcc26bb6 docs(router): Update router docs to use guard functions instead of @Injectable (#47989)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.2+sha-a93a1a5'));
+    definitionMap.set('version', o.literal('15.0.0-rc.2+sha-2efcc26'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
