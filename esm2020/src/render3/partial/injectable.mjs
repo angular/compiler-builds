08f26ac03f4cb37e83ed8c360a4118020007cdf5
Andrew Kushnir <akushnir@google.com>
2022-11-08 18:52:40 +0000

    28efb06a96 test(router): Move test for redirect under correct describe block (#47993)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('14.2.9+sha-c3bc035'));
+    definitionMap.set('version', o.literal('14.2.9+sha-28efb06'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
