4793401eebf18a6ff106bf54794168aae6365079
Andrew Kushnir <akushnir@google.com>
2022-11-09 19:25:29 +0000

    85a932cc80 docs(router): deprecate setupTestingRouter (#48008)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-f5d0c80'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-85a932c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
