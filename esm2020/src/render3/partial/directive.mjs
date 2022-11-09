a647c4119ebde1a3b05c27d7168ba8f92f32c280
Andrew Kushnir <akushnir@google.com>
2022-11-09 19:13:59 +0000

    f5d0c80622 docs(router): small adjustments to router docs (#48009)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-046ce43'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-f5d0c80'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
