a8d2dec239536cf517abcbc317785874c5199199
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-12 00:03:01 +0000

    629ebad8eb docs(common): Refine NgOptimizedImage docs (#48035)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.3+sha-154ca82'));
+    definitionMap.set('version', o.literal('15.0.0-rc.3+sha-629ebad'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
