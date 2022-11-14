67a06d4816519d017e4bd040b554f62409821610
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2022-11-14 20:26:10 +0000

    38b2ed314d docs: release notes for the v15.0.0-rc.4 release

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-804b855'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-38b2ed3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
