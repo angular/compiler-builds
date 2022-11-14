08b175c09936ed7ef99bb4fe29953fe6db5023c9
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:46:14 +0000

    933d481a0e docs: remove version 12 from actively supported versions (#48041)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0-rc.3+sha-a1b6610'));
+    definitionMap.set('version', o.literal('15.0.0-rc.3+sha-933d481'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
