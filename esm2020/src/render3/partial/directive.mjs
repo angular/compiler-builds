99357a4f397a7fce9c4ac59f15d874b11d20fa58
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 19:45:36 +0000

    804b85554c fix(common): export the IMAGE_CONFIG token (#48051)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-8602f16'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-804b855'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
