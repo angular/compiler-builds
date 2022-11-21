91b57f2c1164e373bb214d65017f686bc2c77fdd
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 22:02:04 +0000

    f376bf8deb docs: fix typo in `cli-builder.mf` (#48129)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-80d0cb5'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-f376bf8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
