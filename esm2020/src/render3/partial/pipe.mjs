de7ace243baf615c0027aebc59ff89136f25d434
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 22:01:06 +0000

    29b198f0c9 docs(forms): add FormRecord class to API summary (#48136)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-ed50032'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-29b198f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
