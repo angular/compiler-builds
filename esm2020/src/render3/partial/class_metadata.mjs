555db7fc38047dde2f1bdd160e7bf051da326a10
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 18:08:24 +0000

    cc8b76ef7c fix(language-service): correctly handle host directive inputs/outputs (#48147)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.0+sha-890feb9'));
+    definitionMap.set('version', o.literal('15.0.0+sha-cc8b76e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
