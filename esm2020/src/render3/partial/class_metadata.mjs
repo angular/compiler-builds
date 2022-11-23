ca8a562fa07e8bcce2f4d71210e6322d2c7032eb
Dylan Hunn <dylhunn@gmail.com>
2022-11-23 20:28:52 +0000

    e864254224 build(bazel): point to correct dist folder in firebase config (#48211)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-2e38af1'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-e864254'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
