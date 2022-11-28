aaef03ee710b65e3e0de3f4fa4a482a0ff3f63d9
Andrew Scott <atscott01@gmail.com>
2022-11-28 19:11:09 +0000

    afc668acd5 refactor(router): Move navigationId handling to the transition manager (#48202)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.0+sha-fcf8572'));
+    definitionMap.set('version', o.literal('15.1.0-next.0+sha-afc668a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
