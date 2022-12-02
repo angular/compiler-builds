3f94410f63181a9e8e7718a769a9c4ed69b3df8b
Andrew Kushnir <akushnir@google.com>
2022-12-02 18:06:08 +0000

    2eccfd412e build: fix unnecessary large runfiles in AIO when composing boilerplate (#48329)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.2+sha-c0fe820'));
+    definitionMap.set('version', o.literal('15.0.2+sha-2eccfd4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
