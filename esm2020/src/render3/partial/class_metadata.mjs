720ac326d9920549c7fa6c9f4b8237d379d7745c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-03 10:39:16 +0000

    c3136d6723 build: setup github actions for preview deployment of AIO (#48302)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-2352c2f'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-c3136d6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
