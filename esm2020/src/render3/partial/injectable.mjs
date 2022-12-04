328f538d2230c0df1e64de8ce8fe7471939888b9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:33:41 +0000

    0d6b33c40f build: check out project before setting AIO preview targets (#48347)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-43e8f47'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-0d6b33c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
