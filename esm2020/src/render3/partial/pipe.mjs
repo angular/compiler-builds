9b20b4a8bfd91f6faa1ab9acb7f94761872bfcab
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 11:38:41 +0000

    011ef66a04 build: set firebase deploy target when deploying AIO preview (#48338)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-09fe75d'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-011ef66'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
