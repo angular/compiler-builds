f4ba5d38e7005ca261fe43db7db38d3839719fa7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 13:37:17 +0000

    43e8f4798f build: use npx for setting firebase deploy targets in AIO preview (#48346)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-011ef66'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-43e8f47'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
