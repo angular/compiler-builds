c9cdd6749fe6790ca32f75665e2ac2b16c80e67a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:32:55 +0000

    38701ce07b build: check out project before setting AIO preview targets (#48347)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.2+sha-09388eb'));
+    definitionMap.set('version', o.literal('15.0.2+sha-38701ce'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
