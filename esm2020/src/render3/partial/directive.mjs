65770cae77969a1a9092ea8cfc96c64214d12311
Andrew Kushnir <akushnir@google.com>
2022-12-05 22:56:50 +0000

    cf51690341 refactor(core): remove several private utils and APIs (#48357)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-8e52ca2'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-cf51690'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
