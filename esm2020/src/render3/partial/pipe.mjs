d10d8faea043e3e7c4d3f41d1ab0d56dbc8b9db9
Andrew Kushnir <akushnir@google.com>
2022-12-06 18:17:59 +0000

    f443ee6af7 docs: wrap json property in double quotes (#48375)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.1+sha-8189b64'));
+    definitionMap.set('version', o.literal('15.1.0-next.1+sha-f443ee6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
