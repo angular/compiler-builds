2988d4ef616eeb06f5cf8a621aa6867033269b3d
Andrew Kushnir <akushnir@google.com>
2022-12-07 22:27:40 +0000

    b55d2dab5d fix(compiler-cli): evaluate const tuple types statically (#48091)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.2+sha-bf44dc2'));
+    definitionMap.set('version', o.literal('15.0.3+sha-b55d2da'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
