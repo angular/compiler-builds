7d57194574959de040a28c5f8f7c2153c0a82a06
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 20:51:51 +0000

    77ba4aac00 build: update cross-repo angular dependencies (#48398)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.3+sha-b55d2da'));
+    definitionMap.set('version', o.literal('15.0.3+sha-77ba4aa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
