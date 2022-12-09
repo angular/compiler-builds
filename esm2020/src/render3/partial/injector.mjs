dcaded1fb23309c4a6913fa6022f8dcef9ae47a0
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 23:25:02 +0000

    c4d17b92f9 docs: Document extracted from the original Introduction document, which is to be retired. (#47913)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.3+sha-2925c6a'));
+    definitionMap.set('version', o.literal('15.0.3+sha-c4d17b9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
