ea4fb50dedde949edd87f9e090e6783e6669b0cb
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 22:24:43 +0000

    4bf271a910 ci: Add jessicajaniuk to code-ownership pullapprove group (#48426)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.2+sha-dcfbe22'));
+    definitionMap.set('version', o.literal('15.1.0-next.2+sha-4bf271a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
