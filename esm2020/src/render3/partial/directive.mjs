5527527a2af8f04070ab8c6c31a466e6c0c41305
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 22:24:38 +0000

    cc5b044cbf docs: fix typos in reviewing content guide (#48441) PR Close #48441

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.2+sha-bf91f8e'));
+    definitionMap.set('version', o.literal('15.1.0-next.2+sha-cc5b044'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
