61b33fc284fec7f682009f1ce829c4acd415ba74
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-13 22:27:22 +0000

    ff84c73603 docs(core): Document invalid multi token (#48267)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.2+sha-279e7d4'));
+    definitionMap.set('version', o.literal('15.1.0-next.2+sha-ff84c73'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
