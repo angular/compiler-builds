a24afef118b6620492e5b1d30cabd2a5b70511c1
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-14 01:16:03 +0000

    9291bd950f refactor(router): Add opt-in provider for upcoming router change (#47988)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.3+sha-aabf8be'));
+    definitionMap.set('version', o.literal('15.0.3+sha-9291bd9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
