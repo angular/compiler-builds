d0b925dcb588dff88c1ccae5fbb9be26fc9c87e3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-03 16:51:24 +0000

    2b8d8f0077 fix(compiler-cli): update `@babel/core` dependency and lock version (#48634)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.4+sha-87d2385'));
+    definitionMap.set('version', o.literal('15.0.4+sha-2b8d8f0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
