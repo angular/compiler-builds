be530dbc5f1282d2db5e46639a339d200c62845a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:07:36 +0000

    10de92c68a refactor(forms): cleanup type any in forms tests (#48624)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.4+sha-c001528'));
+    definitionMap.set('version', o.literal('15.0.4+sha-10de92c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
