47e2b45a89fe946aa8e26adfdd67d0e18f05963d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-05 16:55:35 +0000

    5d1fbbbf83 ci: fix snapshot publish github credential preparation (#48648)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-next.3+sha-eb52d9e'));
+    definitionMap.set('version', o.literal('15.1.0-next.3+sha-5d1fbbb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
