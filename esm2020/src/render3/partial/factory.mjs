cf0fe709f36c41de2fd69d354e11590f2b2342ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-09 18:27:38 +0000

    e615b598ba fix(language-service): ship `/api` entry-point (#48670)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-rc.0+sha-8aa8b4b'));
+    definitionMap.set('version', o.literal('15.1.0-rc.0+sha-e615b59'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
