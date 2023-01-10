92b6735f4e78d6fec8ab4967ab37a5cbe7283e58
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 17:28:58 +0000

    ae34e6cabd fix(language-service): expose `package.json` for vscode extension resolution (#48678)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.0.4+sha-aa31d61'));
+    definitionMap.set('version', o.literal('15.0.4+sha-ae34e6c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
