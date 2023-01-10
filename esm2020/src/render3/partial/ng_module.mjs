1910bc469a294aa83e42aa68d84b38aa9c73a5a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:33:43 +0000

    caf7228f8a fix(compiler): resolve deprecation warning (#48652)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0-rc.0+sha-e615b59'));
+    definitionMap.set('version', o.literal('15.1.0-rc.0+sha-caf7228'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
