41c6e06ad47c6b3e48ffdd8d7b7f8e0b19f02ae4
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:14:22 +0000

    f09b6e4507 docs: add angular devsat q1 2023 (#48683)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-01be0e2'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-f09b6e4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
