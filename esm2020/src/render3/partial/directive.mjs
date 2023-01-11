1b5be6c997c8bed65df1ae35fb9dd162294ecda6
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:10:18 +0000

    b0318f8b39 docs(docs-infra): fix wording inconsistency of the i18n common prepare section references (#47582)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-fbcddb0'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-b0318f8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
