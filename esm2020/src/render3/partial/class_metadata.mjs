2308a6218d3a348cab6d09eb6d1fc9350c68cccc
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:13:19 +0000

    01be0e2874 docs: Add support for previous Chrome release (#48669)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-518cc63'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-01be0e2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
