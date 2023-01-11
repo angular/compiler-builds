98fc806f5162a4a0deac2f0014d806944b418b73
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:15:24 +0000

    8375a8c9a8 docs: Add heading to the router writable properties deprecation (#48684)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-f09b6e4'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-8375a8c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
