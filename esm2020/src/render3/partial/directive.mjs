15bf588498976a7747343c353ff85a20f855788e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-11 15:08:39 +0000

    5ae7ca827f ci: correctly interpolate `ANGULAR_CLI_BUILDS_READONLY_GITHUB_TOKEN` (#48690)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0+sha-1aef7a8'));
+    definitionMap.set('version', o.literal('15.1.0+sha-5ae7ca8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
