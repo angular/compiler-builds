67ebd3fd66c221fe22606a513821d770e5d0c949
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-11 15:08:39 +0000

    5ae7ca827f ci: correctly interpolate `ANGULAR_CLI_BUILDS_READONLY_GITHUB_TOKEN` (#48690)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0+sha-1aef7a8'));
+    definitionMap.set('version', o.literal('15.1.0+sha-5ae7ca8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
