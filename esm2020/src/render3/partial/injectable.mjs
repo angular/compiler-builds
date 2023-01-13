de3a607b6850c45808b3f8946a8a9374d281bd1c
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:48:32 +0000

    914e7b052e build: update actions/cache digest to 58c146c (#48564)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-5b1c7c1'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-914e7b0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
