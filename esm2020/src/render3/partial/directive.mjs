0fb85a5894d4eae38e2e8f434806ae06971a9651
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:45:49 +0000

    f03f20c8ba build: update actions/checkout digest to ac59398 (#48460)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-34dda89'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-f03f20c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
