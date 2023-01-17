58d93b0bc33a92de22382f98cc6b454b8e0f413a
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:32:27 +0000

    7840352dee build: update all non-major dependencies (#48755)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.0+sha-1e30baa'));
+    definitionMap.set('version', o.literal('15.2.0-next.0+sha-7840352'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
