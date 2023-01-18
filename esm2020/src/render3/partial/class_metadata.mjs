857cc5c6762b257399ea4691a18bc0ab0aa69867
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-01-18 18:28:38 +0000

    e3a8a5cc42 release: cut the v15.1.1 release

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.0+sha-0f8a4f9'));
+    definitionMap.set('version', o.literal('15.1.1+sha-e3a8a5c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
