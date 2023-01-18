dc4c43e08ffc4655d6746c9253d4a4e8c770a4a2
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:03:26 +0000

    3a688cb5ec build(devtools): fix issue where esbuild configs were not being set properly in DevTools (#48762)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.1+sha-42ca9c0'));
+    definitionMap.set('version', o.literal('15.2.0-next.1+sha-3a688cb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
