566838bbaa17685224959e0554824acd73b702c4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-23 13:51:36 +0000

    2f83366f58 build: remove unused build optimizer dependency (#48797)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.1+sha-92ab80d'));
+    definitionMap.set('version', o.literal('15.1.1+sha-2f83366'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
