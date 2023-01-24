59463efce93fdfd9af53da1db148a6e75327b5b3
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:06:20 +0000

    64416a6e4d docs: Use of $any() has no runtime incidence. (#48785) Some users are woried about `$any()` as function calls are to be avoided in templates. This sentence clarifies the situation.

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.1+sha-2f83366'));
+    definitionMap.set('version', o.literal('15.1.1+sha-64416a6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
