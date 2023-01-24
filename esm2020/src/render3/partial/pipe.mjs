0f96422e964393f866de644d526af1d887db7a6d
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 16:51:31 +0000

    82c8b7ae9e build(docs-infra): replace base64 images for stackblitz by svgs (#48774)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.1+sha-a63983d'));
+    definitionMap.set('version', o.literal('15.2.0-next.1+sha-82c8b7a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
