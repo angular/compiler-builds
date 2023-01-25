f6930f2f4169cc891b0cd8c1006adb58b27721b6
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:52:20 +0000

    6e197520dd refactor(upgrade): rewrite the downgrade component adapter (#48715)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.2+sha-0861471'));
+    definitionMap.set('version', o.literal('15.1.2+sha-6e19752'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
