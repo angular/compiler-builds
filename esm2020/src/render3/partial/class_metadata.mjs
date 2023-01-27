4ec4fe64c4741aa06f308571986b987de643a718
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 16:38:36 +0000

    2352f8dca3 docs(docs-infra): in navigation put prerequisites page 'guide/property-binding' before 'guide/attribute-binding' (#48766)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.2+sha-e7a4fd3'));
+    definitionMap.set('version', o.literal('15.2.0-next.2+sha-2352f8d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
