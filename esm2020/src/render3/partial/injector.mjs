6ef67d672f730f39e161a1f94b636c2c3ca024b4
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 17:46:48 +0000

    8a9907c23a refactor(common): Remove currency pipe depreciation notice about the default currency (#48872)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.2+sha-5c5d365'));
+    definitionMap.set('version', o.literal('15.2.0-next.2+sha-8a9907c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
