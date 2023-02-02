f0e6f616c6509b2eea5dce44f4ea25f702390555
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:57:46 +0000

    c2bcf0b475 refactor(compiler): Remove strictStyling option for ShadowCss (#48824)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.3+sha-3a9bbad'));
+    definitionMap.set('version', o.literal('15.2.0-next.3+sha-c2bcf0b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
