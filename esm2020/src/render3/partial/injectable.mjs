53e7df97e26baa36474994d2a9cd87e49b60c969
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 17:41:50 +0000

    5c5d36547b docs: fix indentation error in content/guide/build.md (#48893)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.2+sha-33b9cb8'));
+    definitionMap.set('version', o.literal('15.2.0-next.2+sha-5c5d365'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
