bccaf3a793ab32838cb8d20df2bd7e1076c3076a
Andrew Scott <atscott@google.com>
2023-02-03 20:00:53 +0000

    2de6dae16d fix(migrations): migrate RouterModule.forRoot with a config object to use features (#48935)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.3+sha-2ceff3f'));
+    definitionMap.set('version', o.literal('15.2.0-next.3+sha-2de6dae'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
