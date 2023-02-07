600659993137f1bf5e80ab35fea112ecb6b1ddbd
Andrew Scott <atscott@google.com>
2023-02-07 20:03:07 +0000

    4b8b5e3f3c build: update eslint dependencies to v5.51.0 (#48983)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.3+sha-57d0c03'));
+    definitionMap.set('version', o.literal('15.2.0-next.3+sha-4b8b5e3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
