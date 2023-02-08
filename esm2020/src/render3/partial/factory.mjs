dd6c3536e535061ed56e634b46e0eb6e4562dbec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:20:33 +0000

    ff0d48ad92 build: update github/codeql-action action to v2.2.2 (#48889)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.3+sha-110300e'));
+    definitionMap.set('version', o.literal('15.1.3+sha-ff0d48a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
