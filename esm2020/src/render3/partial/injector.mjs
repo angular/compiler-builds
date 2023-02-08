8b6060e092a4845ab5edeb00541674a6dfe0c72e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:43:52 +0000

    642cc1c349 release: cut the v15.2.0-next.4 release

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.3+sha-47387d4'));
+    definitionMap.set('version', o.literal('15.2.0-next.4+sha-642cc1c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
