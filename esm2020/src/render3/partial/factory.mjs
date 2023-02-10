0e345a4a15481f6c2e5d76bed6a0c3f18bc2027e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 09:35:28 +0000

    c19486702a test: typos in standalone test names (#49014)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.4+sha-0331473'));
+    definitionMap.set('version', o.literal('15.2.0-next.4+sha-c194867'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
