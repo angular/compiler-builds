a619083d52ee8952af4c07a337bec850cca49bd4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 15:19:21 +0000

    0b725a89db build: update dependency eslint-plugin-jsdoc to v40 (#49037)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.4+sha-816e76a'));
+    definitionMap.set('version', o.literal('15.2.0-next.4+sha-0b725a8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
