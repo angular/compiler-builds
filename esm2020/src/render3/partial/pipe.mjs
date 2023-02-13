e1a4596a3399dc12f53d30af68122b75aef2afa5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 15:20:25 +0000

    ebae506d89 fix(migrations): use import remapper in root component (#49046)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.4+sha-0b725a8'));
+    definitionMap.set('version', o.literal('15.2.0-next.4+sha-ebae506'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
