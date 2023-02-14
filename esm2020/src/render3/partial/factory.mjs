b97bf73b95a68f2322d428b2640d3c2892330ea3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 19:37:18 +0000

    c7eddc24da refactor(devtools): consolidate devtools global styles into 1 file (#49001)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.4+sha-6534a5e'));
+    definitionMap.set('version', o.literal('15.1.4+sha-c7eddc2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
