24a27d5443f954b1bc11c04c18a9d027bb51aa59
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-15 17:50:13 +0000

    d69ef721d4 release: cut the v15.1.5 release

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.4+sha-c7eddc2'));
+    definitionMap.set('version', o.literal('15.1.5+sha-d69ef72'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
