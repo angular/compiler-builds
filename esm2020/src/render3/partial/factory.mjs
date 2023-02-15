88c52311c96111013d2339c789f20db2d5ae114c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-15 17:49:30 +0000

    44f13e2f2b docs: release notes for the v15.1.5 release

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0-next.4+sha-2ca4972'));
+    definitionMap.set('version', o.literal('15.2.0-next.4+sha-44f13e2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
