b8082920fc017a3fcf403021811805ac9921ca05
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:48:50 +0000

    262895b81a docs: close the developer survey (#49092)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.1.5+sha-d69ef72'));
+    definitionMap.set('version', o.literal('15.1.5+sha-262895b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
