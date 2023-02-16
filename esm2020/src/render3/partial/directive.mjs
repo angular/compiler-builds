7f4d4b48fc4a358ce24dd2cdb6a88f1c6013c59b
Andrew Kushnir <akushnir@google.com>
2023-02-16 19:16:08 +0000

    3a6aebf349 fix(bazel): stop publishing @angular/bazel package to npm (#49093)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-6977403'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-3a6aebf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
