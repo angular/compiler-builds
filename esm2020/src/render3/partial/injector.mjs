a0203aef23309acc2342caf8559a03c5feba6bc4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:50:31 +0000

    0c7b9aab34 docs: close the developer survey (#49092)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-02d5e8d'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-0c7b9aa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
