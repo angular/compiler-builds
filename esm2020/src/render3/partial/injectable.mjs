4a0ba8fdcb49b684241439113606c6a982cecd0e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:22:07 +0000

    48aa96ea13 refactor: remove Angular Compatibility Compiler (ngcc) (#49101)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-9105c41'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-48aa96e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
