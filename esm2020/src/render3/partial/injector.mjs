3418386fe0cad903ec553cdc4b466123b5738c72
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:25:35 +0000

    2d51b5f644 Revert "fix(platform-server): insert transfer state `script` before other `script` tags (#48868)" (#49112)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-ca53ade'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-2d51b5f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
