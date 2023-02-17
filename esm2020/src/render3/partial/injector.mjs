b71e43ae58385c42e3e5cd0a85484d202b66b0a2
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:28:03 +0000

    0b3eacbbcc docs: update Angular CLI help [main] (#49119)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-f12ba1c'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-0b3eacb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
