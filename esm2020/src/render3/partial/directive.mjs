81fa2073796f268b2d9236e4d296b4f45132df53
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:34:38 +0000

    9b9c818f99 perf(core): change `RendererType2.styles` to accept a only a flat array (#49072)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-18c7fac'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-9b9c818'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
