823bdab8fd025662197ba9519d092ce90ec88eee
Andrew Kushnir <akushnir@google.com>
2023-02-24 16:33:48 +0000

    7dd19570e8 fix(migrations): delete barrel exports in standalone migration (#49176)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-79cdfeb'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-7dd1957'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
