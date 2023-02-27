ba8275b71a1bffc820bff02962247b0689855994
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:21:37 +0000

    47ef9e83c7 build: cleanup unused dependencies (#49227)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-d1ac3aa'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-47ef9e8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
