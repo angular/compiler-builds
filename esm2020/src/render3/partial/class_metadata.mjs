0c58f248111ee204df57fc0c5855a055347c927f
Andrew Kushnir <akushnir@google.com>
2023-02-28 20:12:26 +0000

    e9edea363c fix(core): update zone.js peerDependencies ranges (#49244)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-0f5c800'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-e9edea3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
