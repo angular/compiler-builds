15d589dbba2caca44a9a91d0ea8afab33ac1144a
Andrew Kushnir <akushnir@google.com>
2023-02-28 18:19:25 +0000

    87affadb87 fix(migrations): avoid migrating the same class multiple times in standalone migration (#49245)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-dba747f'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-87affad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
