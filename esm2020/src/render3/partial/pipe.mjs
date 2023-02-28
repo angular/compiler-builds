52e451d710a22fa1c0e5ff67922641ffc255418b
Andrew Kushnir <akushnir@google.com>
2023-02-28 17:47:27 +0000

    9553a9a2a1 docs: New doc extract from original HTTP doc to be retired. (#48912)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-557ae66'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-9553a9a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
