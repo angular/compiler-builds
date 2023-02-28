65cf5dcfba8355a7e0a8bb1130a5046bf3c393ab
Andrew Kushnir <akushnir@google.com>
2023-02-28 20:12:58 +0000

    7fd650a898 build: update lint path for @angular/build-tooling rules (#49266)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-e9edea3'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-7fd650a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
