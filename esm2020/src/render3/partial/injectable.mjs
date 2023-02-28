09b20a55270868ec85aed9728bb8c30e6bffe091
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:44:31 +0000

    6d6b76c59f docs(router): Improving linking (#49203)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.0+sha-19a4cde'));
+    definitionMap.set('version', o.literal('15.2.0+sha-6d6b76c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
