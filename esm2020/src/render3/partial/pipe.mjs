615340c9fcbb1b6aff94bda408be48aa99c83fe5
Andrew Kushnir <akushnir@google.com>
2023-02-28 19:17:14 +0000

    f594725951 refactor(core): remove Node.js v14 support (#49255)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.0+sha-0c805f1'));
+    definitionMap.set('version', o.literal('16.0.0-next.0+sha-f594725'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
