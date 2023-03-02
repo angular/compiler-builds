42908689287b44914e79b0f965b75429d6ba6281
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 20:06:39 +0000

    3aec17e11c docs: Add NG8107 entry : Optional chain not nullable extended diagnostic (#49287)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.1+sha-8beb852'));
+    definitionMap.set('version', o.literal('16.0.0-next.1+sha-3aec17e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
