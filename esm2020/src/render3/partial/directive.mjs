451f77f475488e5b6d8eb1ff20a2c1f13458f21c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 21:10:25 +0000

    d42032a380 refactor(core): Remove `isListLikeIterable` from private export. (#49297)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.1+sha-ffc6843'));
+    definitionMap.set('version', o.literal('15.2.1+sha-d42032a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
