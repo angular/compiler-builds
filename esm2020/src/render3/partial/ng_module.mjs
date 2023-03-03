4ecca8e17adc8f6a4ac8f96ca0a78520eb9be296
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 22:19:46 +0000

    e1355e7d16 refactor(migrations): Move ChangeTracker to common utils (#49308)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.1+sha-9737df3'));
+    definitionMap.set('version', o.literal('16.0.0-next.1+sha-e1355e7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
