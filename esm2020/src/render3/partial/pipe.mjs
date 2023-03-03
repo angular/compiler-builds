1ee6cbcc82f31c2f130f4ee076b51848a485ed0d
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 20:02:11 +0000

    b90b3299aa test: fix router testing example breaking due to non-existent stub (#49293)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.1+sha-f321e65'));
+    definitionMap.set('version', o.literal('15.2.1+sha-b90b329'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
