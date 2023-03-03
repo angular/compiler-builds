0265ba013004b393d57d4e785b5162dcce7a29b6
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 22:22:36 +0000

    ac59054cd4 refactor(migrations): Move ChangeTracker to common utils (#49308)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.1+sha-b90b329'));
+    definitionMap.set('version', o.literal('15.2.1+sha-ac59054'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
