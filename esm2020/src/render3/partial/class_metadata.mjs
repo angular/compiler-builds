74ca25ba31d3d212afa759e74125d1b2efb07fc5
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 23:58:53 +0000

    7e23883b5d refactor(dev-infra): remove obsolete todos (#49362)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.2+sha-f099df8'));
+    definitionMap.set('version', o.literal('15.2.2+sha-7e23883'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
