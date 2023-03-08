83465f520f9a6453af81d57b051356cea8bbcf9e
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 02:11:24 +0000

    984d0f49b5 build: update cross-repo angular dependencies (#49188)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.1+sha-8e0a087'));
+    definitionMap.set('version', o.literal('16.0.0-next.1+sha-984d0f4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
