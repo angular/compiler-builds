714f083630fad261f118d44ae2070905af989111
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:30:21 +0000

    5a2b6227b3 fix(zone.js): revert Mocha it.skip, describe.skip method patch (#49329)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.2+sha-daaed89'));
+    definitionMap.set('version', o.literal('16.0.0-next.2+sha-5a2b622'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
