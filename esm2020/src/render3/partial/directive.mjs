43390e3141941ea09f78acc22f4641c5b65b18d4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:44:45 +0000

    0814f20594 feat(core): introduce `runInInjectionContext` and deprecate prior version (#49396)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.2+sha-056d680'));
+    definitionMap.set('version', o.literal('16.0.0-next.2+sha-0814f20'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
