b4aa5a46ccb3dececc730c1b08362a8e49eba387
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:35:02 +0000

    67743340a7 test(common): update async pipe tests to fix test errors (#49433)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.2+sha-bf2a5d5'));
+    definitionMap.set('version', o.literal('16.0.0-next.2+sha-6774334'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
