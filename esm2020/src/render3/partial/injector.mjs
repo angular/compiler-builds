56a2463a377517fb4e3a07d93bd43cf3528d6560
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 16:37:09 +0000

    c934a8e72b fix(platform-browser): only add `ng-app-id` to style on server side (#49465)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-20f7187'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-c934a8e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-20f7187'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-c934a8e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
