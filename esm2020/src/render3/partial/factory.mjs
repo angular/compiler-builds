21d518de6edb6edbbb045c596e1dac16b8c74555
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 09:17:17 +0000

    230345876c fix(core): Allow `TestBed.configureTestingModule` to work with recursive cycle of standalone components. (#49473)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-b4e1bfb'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-2303458'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
