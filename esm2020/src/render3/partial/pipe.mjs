d4e061683dd3d1d2c0e98973cad4ea7b531d2a79
Andrew Scott <atscott@google.com>
2023-03-20 19:43:21 +0000

    84a2ad7b01 refactor(core): expose private setAlternateWeakRefImpl (#49466)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-897a9e5'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-84a2ad7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
