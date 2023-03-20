89dd388e773f6f910bc52205d2ede97fa200be7a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 12:43:26 +0000

    0f0c0a9452 docs(docs-infra): improve docs for @angular/localize package. (#49393)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.3+sha-bae6b5c'));
+    definitionMap.set('version', o.literal('15.2.3+sha-0f0c0a9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
