c660f1f9288a1370deaa0c0077af50a315ca523e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 09:23:34 +0000

    bae6b5ceb1 fix(core): Allow `TestBed.configureTestingModule` to work with recursive cycle of standalone components. (#49473)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.3+sha-868dcab'));
+    definitionMap.set('version', o.literal('15.2.3+sha-bae6b5c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
