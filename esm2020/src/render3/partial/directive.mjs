fed5eb796dc87eeae60f21a5cb9dd003dc3f8ed7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 18:39:16 +0000

    504bd992ce refactor(core): add a note that hydration for i18n blocks is not yet supported (#49497)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-a94b66f'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-504bd99'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
