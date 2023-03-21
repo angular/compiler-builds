9085497cf584c42a3a3b04c1079b5a334ddf1180
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:49:26 +0000

    f0f1e7dfa7 build: update scorecard action dependencies (#49508)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-66f8ed1'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-f0f1e7d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
