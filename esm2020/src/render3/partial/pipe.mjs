20800137086052c0d59b4b63a7aa78d99c9482b9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:52:15 +0000

    24b3e8f042 build: exclude draft PRs from triage queue (#49515)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-f0f1e7d'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-24b3e8f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
