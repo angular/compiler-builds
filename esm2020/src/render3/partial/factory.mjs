80da6a5086f51c0118e7aab8a2ac064acf2cda5e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 16:09:00 +0000

    d1617c449d feat(core): allow removal of previously registered DestroyRef callbacks (#49493)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-24b3e8f'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-d1617c4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
