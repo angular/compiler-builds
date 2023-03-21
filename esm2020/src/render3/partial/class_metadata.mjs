5d5c3b465733d9e19c2c28511be1126d304d38af
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 18:10:12 +0000

    a94b66f432 refactor(core): avoid `ngSkipHydration` on nodes other than component host ones (#49500)

--- esm2020/src/render3/partial/class_metadata.mjs
+++ esm2020/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-a0c289c'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-a94b66f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
