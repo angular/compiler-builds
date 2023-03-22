6933d53eca2764c254ae0ababc026b3a35f9a3c5
Andrew Scott <atscott01@gmail.com>
2023-03-22 20:32:47 +0000

    c6d8e5fef4 docs: release notes for the v15.2.4 release

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.3+sha-0851bd5'));
+    definitionMap.set('version', o.literal('16.0.0-next.3+sha-c6d8e5f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
