f9c92e47d2643a518520ae96ba1f6e7a8e13c831
Andrew Scott <atscott01@gmail.com>
2023-03-22 23:13:41 +0000

    07cbaa3e28 Revert "feat(bazel): (APF) Angular Package Format updates (#49332)" (#49555)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-132c00c'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-07cbaa3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
