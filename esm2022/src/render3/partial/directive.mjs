fbf42fd3dba15e7cd654ddd339ba885824e61279
Andrew Scott <atscott01@gmail.com>
2023-03-23 21:11:59 +0000

    03d1d00ad9 feat(compiler-cli): Add an extended diagnostic for `nSkipHydration` (#49512)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-d43367b'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-03d1d00'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
