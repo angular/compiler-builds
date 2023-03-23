250a3ab31d9e2e45916ac96b63a8e57a7bbcb25c
Andrew Scott <atscott01@gmail.com>
2023-03-23 21:13:06 +0000

    f1d3be3dee refactor(core): only annotate disconnected nodes in content projection (#49549)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-03d1d00'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-f1d3be3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
