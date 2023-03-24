2a54a42e970f7dc743c711c6bb841ce4ceacb601
Andrew Scott <atscott01@gmail.com>
2023-03-24 17:39:41 +0000

    656a38899a build: update actions/checkout digest to 8f4b7f8 (#49577)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.4+sha-29c682c'));
+    definitionMap.set('version', o.literal('15.2.4+sha-656a388'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
