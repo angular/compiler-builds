5cf35ba0f4733e51d28023cfc020003add86091f
Andrew Scott <atscott01@gmail.com>
2023-03-28 17:37:58 +0000

    6d6fc12c88 refactor(core): Remove usage of deprecated `Injector.create()` (#49606)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.4+sha-0fecbab'));
+    definitionMap.set('version', o.literal('15.2.4+sha-6d6fc12'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
