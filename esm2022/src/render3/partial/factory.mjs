b7dc9ba0dc2436bc13c2635192e1a877e91ec906
Andrew Scott <atscott01@gmail.com>
2023-03-28 17:31:18 +0000

    48eff1e945 docs: fixed grammar (#49560) PR Close #49560

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-f305f22'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-48eff1e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
