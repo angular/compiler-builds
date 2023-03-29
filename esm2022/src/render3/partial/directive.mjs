975635fb0cabf014f2f28a9af959f1414856837c
Andrew Scott <atscott01@gmail.com>
2023-03-29 01:57:56 +0000

    ef149defb9 refactor(core): expose `ReactiveNode` privately on signal functions (#49529)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-84a0fa3'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-ef149de'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.4+sha-84a0fa3'));
+    definitionMap.set('version', o.literal('16.0.0-next.4+sha-ef149de'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
