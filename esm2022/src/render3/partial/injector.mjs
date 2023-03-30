f902372e2e92288e9fa9c3af84de926cc1782c5a
Andrew Scott <atscott01@gmail.com>
2023-03-30 16:53:53 +0000

    1c3891cf50 docs: fixed typo error (#49637)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.5+sha-06e74a5'));
+    definitionMap.set('version', o.literal('16.0.0-next.5+sha-1c3891c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
