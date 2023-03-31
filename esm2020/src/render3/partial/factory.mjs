d87aac140952b6948885881a46f81512e88de6d1
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:18:35 +0000

    08979e4589 docs: fix typo in toh-pg6.md (#49141)

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.5+sha-35f4918'));
+    definitionMap.set('version', o.literal('15.2.5+sha-08979e4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
