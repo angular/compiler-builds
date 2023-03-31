5abb5ba84692d8bdafadea26150d36e01f95c0df
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:16:59 +0000

    acff3c6d86 docs: fix typo in toh-pg6.md (#49141)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.5+sha-fda3f67'));
+    definitionMap.set('version', o.literal('16.0.0-next.5+sha-acff3c6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
