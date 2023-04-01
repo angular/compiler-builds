ea23ca2d246b750535419984bdd12ef5df74036d
Dylan Hunn <dylhunn@gmail.com>
2023-04-01 00:21:43 +0000

    fa3909e8b4 fix(router): Ensure initial navigation clears current navigation when blocking (#49572)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.5+sha-acff3c6'));
+    definitionMap.set('version', o.literal('16.0.0-next.5+sha-fa3909e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
