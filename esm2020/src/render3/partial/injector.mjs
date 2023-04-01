b60910982d97b15587b13f6c90176c1a9f52485c
Dylan Hunn <dylhunn@gmail.com>
2023-04-01 00:24:28 +0000

    9b61379096 fix(router): Ensure initial navigation clears current navigation when blocking (#49572)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.5+sha-08979e4'));
+    definitionMap.set('version', o.literal('15.2.5+sha-9b61379'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
