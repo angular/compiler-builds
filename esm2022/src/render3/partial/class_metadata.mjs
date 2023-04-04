1d1059bbd73554a0d8c8a9b56527fe8a8fb240c2
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 22:19:29 +0000

    b35fa73968 feat(core): change the URL sanitization to only block javascript: URLs (#49659)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-f0978ad'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-b35fa73'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
