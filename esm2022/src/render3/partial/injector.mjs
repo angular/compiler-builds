a03ee7fd3bac2fd70d3888c1f2eb7bb17634bd61
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:29:06 +0000

    e7463dc80f docs: tweak hydration guide from feedback (#49705)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-edc3bb1'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-e7463dc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
