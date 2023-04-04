2b40e601dad329b5ba68c98f24a37d0669541ff1
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:32:43 +0000

    aff1512950 feat(http): allow `HttpClient` to cache requests (#49509)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-ba3e9ea'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-aff1512'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
