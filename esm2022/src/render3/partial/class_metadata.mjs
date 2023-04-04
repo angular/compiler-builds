ede64b057b9a3d75a7d7696605d7873dad8c7eeb
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 22:28:36 +0000

    1aef29ceb1 refactor(core): Cleanup for Iterable differs test. (#49598)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-1f3f564'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-1aef29c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
