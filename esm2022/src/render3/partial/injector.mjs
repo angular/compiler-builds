04a1b6410fa693da9d95eb04512bb7b91ef01446
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:28:01 +0000

    85ca1f99b7 build: update dependency concurrently to v8 (#49662)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-7870fb0'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-85ca1f9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
