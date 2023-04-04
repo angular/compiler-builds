fedf93e7182c10ae3bca3959e1adb414df11b255
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:30:29 +0000

    8019fed1f7 docs: fix typo in quick reference (#49655)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-d11f5d7'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-8019fed'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
