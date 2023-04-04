10750f97721483c402554b447a8f2cdb0864f86f
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:28:16 +0000

    d11f5d70d3 build: update dependency google-closure-compiler to v20230228 (#49663)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-85ca1f9'));
+    definitionMap.set('version', o.literal('16.0.0-next.6+sha-d11f5d7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
