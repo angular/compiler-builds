eb8ff58a9b44535e23d9e6369706b4d4e4fcd1ee
Dylan Hunn <github@dylanhunn.com>
2023-04-06 03:16:33 +0000

    726f09c01e docs: release notes for the v16.0.0-next.7 release

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.6+sha-a747954'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-726f09c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
