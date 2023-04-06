9ad1e15103937efb8b67c9bb219623b19be1a3a3
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:22:16 +0000

    c713d9fe66 docs: Improve previous version navigation (#49063) In archive mode the link to the current doc will now point to same page. If the page doesn't exist anymore, it will show a contextualized warning message.

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.7+sha-f148751'));
+    definitionMap.set('version', o.literal('16.0.0-next.7+sha-c713d9f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
