eda02dc189cd3b872c34b4c2d150854272a7cf4b
Dylan Hunn <github@dylanhunn.com>
2023-04-06 03:00:44 +0000

    9a37a7786a release: cut the v15.2.6 release

--- esm2020/src/render3/partial/factory.mjs
+++ esm2020/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.5+sha-da5f316'));
+    definitionMap.set('version', o.literal('15.2.6+sha-9a37a77'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('deps', compileDependencies(meta.deps));
