f3d5cec33112b740705e1dbf70c6accfc5c84591
Andrew Kushnir <akushnir@google.com>
2023-04-11 16:33:17 +0000

    a46b76132e revert "fixup! fix(router): fix = not parsed in router segment name" (#47332)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.7+sha-898eecb'));
+    definitionMap.set('version', o.literal('16.0.0-next.7+sha-a46b761'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
