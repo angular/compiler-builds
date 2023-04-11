c3599ca58e531ce4bbaa1ed116c2bac36ab618b6
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:02:43 +0000

    2eb9b8b402 fix(http): wait for all XHR requests to finish before stabilizing application (#49776)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-next.7+sha-f8e2586'));
+    definitionMap.set('version', o.literal('16.0.0-next.7+sha-2eb9b8b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
