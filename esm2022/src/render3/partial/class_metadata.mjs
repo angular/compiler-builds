70b37d138f404f265aa4bf22e0dc686a6c1a978f
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:02:36 +0000

    079f4bc1ef fix(http): wait for all XHR requests to finish before stabilizing application (#49776)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-b8a4075'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-079f4bc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
