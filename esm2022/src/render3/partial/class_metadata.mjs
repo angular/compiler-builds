df54cb340e3771437bb9d0bd2b6e103eeee21a6f
Andrew Kushnir <akushnir@google.com>
2023-04-12 20:02:47 +0000

    5ac8ca4f55 fix(core): error if document body is null (#49818)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-577374f'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-5ac8ca4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
