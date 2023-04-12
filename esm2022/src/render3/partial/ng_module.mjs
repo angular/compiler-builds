dce4210cef161cc06b6f8b68fe36f7275ecf7547
Andrew Kushnir <akushnir@google.com>
2023-04-12 16:39:59 +0000

    1f7f7598d5 docs: add information on how to run universal in conjunction with service worker (#49795)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-a1e0f86'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-1f7f759'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
