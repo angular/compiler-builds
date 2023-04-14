f057208293dc859e0bd8ded28d8cb6f1ae545208
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 14:27:42 +0000

    8235bc94f7 ci: Remove environment files and EnableProdMode on integration tests (#49746)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-f266a05'));
+    definitionMap.set('version', o.literal('15.2.7+sha-8235bc9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
