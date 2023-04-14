1ff38ff9c1cc722385e553a6dd25db5358131042
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 15:20:29 +0000

    09a42d988e fix(router): canceledNavigationResolution: 'computed' with redirects to the current URL (#49793)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-8235bc9'));
+    definitionMap.set('version', o.literal('15.2.7+sha-09a42d9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
