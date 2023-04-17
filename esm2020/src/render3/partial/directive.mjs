aba718059440f1c424d1d2cf086731882afbd7d2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:47:26 +0000

    1d4cf95a94 docs(docs-infra): add lara newsom to GDE resources (#49620)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-79a8b3c'));
+    definitionMap.set('version', o.literal('15.2.7+sha-1d4cf95'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
