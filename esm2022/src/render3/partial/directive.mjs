10da77dcb78e257c6e47dcae4f6f9f8dd9c3d7e4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:10:59 +0000

    a7dbb23bc8 refactor(compiler-cli): remove `incrementalDriver` on the compiler (#49869)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-3ba5dcc'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-a7dbb23'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
