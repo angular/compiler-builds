bfebcf50c21fff725a90bf6b7615e8a3c0589e7c
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:11:07 +0000

    78c76cecf9 perf(compiler-cli): optimize NgModule emit for standalone components (#49837)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-a7dbb23'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-78c76ce'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
