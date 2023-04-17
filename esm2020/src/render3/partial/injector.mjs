f1ef56949f0f7add67464ba406ed020ffde12a45
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:49:57 +0000

    79a8b3c049 docs: Specify when an app is debuggable by the DevTools. (#48970)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-628c520'));
+    definitionMap.set('version', o.literal('15.2.7+sha-79a8b3c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
