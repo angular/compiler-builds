c6785378ba0a6aae9015dc11049b7895419b7406
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 19:12:41 +0000

    003e7d1c80 Revert "refactor(core): throw an error when `APP_INITIALIZER` token is not an array. (#49860)" (#49893)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-4ee286b'));
+    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-003e7d1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
