9ea44367690ebc56b7c0e9d60b40c287424cb8f2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:36:48 +0000

    3ce1c98b1d refactor: fix typo in windows-chromium-path.js (#49676)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-36ed47f'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-3ce1c98'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
