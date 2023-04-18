ac1f58498fef1ab27f0e57ad6d83a34a9028dcc0
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:50:56 +0000

    27c624e8e7 refactor(compiler): handle #24571 todos. (#49220)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-8219857'));
+    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-27c624e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.1+sha-8219857
+ * @license Angular v16.0.0-rc.1+sha-27c624e
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1369,10 +1369,10 @@ declare class Icu implements Node_3 {
         [k: string]: Node_3;
     };
     sourceSpan: ParseSourceSpan;
-    expressionPlaceholder: string;
+    expressionPlaceholder?: string | undefined;
     constructor(expression: string, type: string, cases: {
         [k: string]: Node_3;
-    }, sourceSpan: ParseSourceSpan);
+    }, sourceSpan: ParseSourceSpan, expressionPlaceholder?: string | undefined);
     visit(visitor: Visitor_2, context?: any): any;
 }
 
