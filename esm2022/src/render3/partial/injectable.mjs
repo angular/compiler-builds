11969aca85584b41b6f78b779c5b03f284a27428
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:56:55 +0000

    ded7ede1d4 test: update platform-server integration tests to use the Angular CLI (#49900)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-5e4797c'));
+    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-ded7ede'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
