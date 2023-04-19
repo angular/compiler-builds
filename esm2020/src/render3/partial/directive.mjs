cb064a5434681569a84032cf09ea79264acd0c31
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-04-19 19:29:11 +0000

    35690fdf6d release: cut the v15.2.8 release

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-3d7b8cd'));
+    definitionMap.set('version', o.literal('15.2.8+sha-35690fd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
