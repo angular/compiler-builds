fd6b7f43a64251a74ebc05c421b45c7cab363b01
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 15:47:59 +0000

    195aaa6b11 refactor(core): Create TestBed injector without a module (#49864)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-38661a6'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-195aaa6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
