9a7e70e75d4eb3203880161a0841e332b903a666
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 15:06:57 +0000

    a175ac5441 test: update platform-server tests to test both ngmodule and standalone application (#49927)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-21b834f'));
+    definitionMap.set('version', o.literal('16.0.0-rc.1+sha-a175ac5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
