106f540eb94438eb18533351071cdee23651468b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 19:02:19 +0000

    3d7b8cdb4d docs: add information on how to use index option in configuration (#49813)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.7+sha-2fff8fa'));
+    definitionMap.set('version', o.literal('15.2.7+sha-3d7b8cd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
