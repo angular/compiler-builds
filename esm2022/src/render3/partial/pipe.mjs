28c6657386cb8694f9f4a7f46814e72e7b0dffd7
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 14:43:50 +0000

    338c7d9125 test(core): adding extra tests for hydration of view containers located after <ng-content> slots (#49920)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-efb3c68'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-338c7d9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
