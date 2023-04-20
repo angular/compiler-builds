c5740b4fa9f1cb4f5d6cb3541c08a8dab7cde31d
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:06:36 +0000

    0f38b98274 docs: replaces "comp: docs" label to "area: docs" (#49952)

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.8+sha-35690fd'));
+    definitionMap.set('version', o.literal('15.2.8+sha-0f38b98'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
