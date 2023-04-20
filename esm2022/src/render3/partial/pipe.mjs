1e89b6c9fd5830d295d88355dfde3d2028bf49fd
Dylan Hunn <dylhunn@gmail.com>
2023-04-20 22:05:06 +0000

    bbc2efcda2 refactor(core): hydrate components of the same type used with and without ngSkipHydration (#49943)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-da1ed98'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-bbc2efc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
