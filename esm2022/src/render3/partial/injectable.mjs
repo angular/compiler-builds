bebf24169c03b8ff4332b2f223f0228f030b0103
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:47:45 +0000

    da681ee712 refactor(core): throw an error when `APP_INITIALIZER` token is not an array. (#49860)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-0ade007'));
+    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-da681ee'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
