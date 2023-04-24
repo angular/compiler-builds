a1bbe4807c9e78262cef383a76e8e984081689d5
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 18:08:13 +0000

    92897b49b0 docs: fix typo in lazy-loading feature modules doc (#49972) PR Close #49972

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-f33abc8'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-92897b4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
