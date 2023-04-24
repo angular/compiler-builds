bd12e9b92e266182d3384ac9dd44666458dc9241
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 18:19:57 +0000

    6d028283d1 docs: fix typo in lazy-loading feature modules doc (#49972) PR Close #49972

--- esm2020/src/render3/partial/directive.mjs
+++ esm2020/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.8+sha-c17b84a'));
+    definitionMap.set('version', o.literal('15.2.8+sha-6d02828'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.internalType);
     if (meta.isStandalone) {
