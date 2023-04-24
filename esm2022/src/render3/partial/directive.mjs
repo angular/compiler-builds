2abfc615869af272ccf2ea3809b89e47fc6bc326
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:13:29 +0000

    9172e63973 test(core): Add hydration symbols test (#49955)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-da681ee'));
+    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-9172e63'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
