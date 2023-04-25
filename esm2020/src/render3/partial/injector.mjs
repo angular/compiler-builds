a2337aa54a6901bcd5f1c1439938ea9ad0377f86
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:18 +0000

    bdf2aa1957 build: bump license year (#49984)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.8+sha-1ef6205'));
+    definitionMap.set('version', o.literal('15.2.8+sha-bdf2aa1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
