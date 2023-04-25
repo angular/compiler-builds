cd661accac395391a7a1eaf8f8067f14adb775ce
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:40 +0000

    f5dd3b4ede refactor(docs-infra): remove `semi` rule from eslintrc. (#49970)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-1090bf8'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-f5dd3b4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
