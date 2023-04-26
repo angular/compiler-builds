fc259e0c07aa2d026f64ac7eccd0b4669a179bf8
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:42:14 +0000

    cc099e7aa0 fix(docs-infra): add `work-break` to `a` tags in `.cli-option` (#50012)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-d26d410'));
+    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-cc099e7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
