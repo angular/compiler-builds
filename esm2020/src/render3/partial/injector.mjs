e9a90e811c5f06dcda0393e1b30cd1ded3d5cc41
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 19:51:40 +0000

    1584b32f09 build: update io_bazel_rules_sass digest to 236d2fc (#50006)

--- esm2020/src/render3/partial/injector.mjs
+++ esm2020/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.8+sha-3599214'));
+    definitionMap.set('version', o.literal('15.2.8+sha-1584b32'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     definitionMap.set('providers', meta.providers);
