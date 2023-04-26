f8db49af5707aad6a8e5a4a0403e19f64f2bffe4
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 19:50:50 +0000

    72ce27115d docs: update Angular CLI help [16.0.x] (#50031)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-2505018'));
+    definitionMap.set('version', o.literal('16.0.0-rc.2+sha-72ce271'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
