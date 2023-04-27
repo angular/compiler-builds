3b699a0c887249c9ab2322c162ed48cb175d5644
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-27 15:36:44 +0000

    80ed0a4868 fix(common): fix incorrectly reported distortion for padded images (#49889)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-3feb9ae'));
+    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-80ed0a4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
