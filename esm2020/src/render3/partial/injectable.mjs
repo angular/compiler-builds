45ac0792b04d7a913d0560973bb2e1cd48e7199f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 09:52:23 +0000

    ea82d3b0e9 ci: update link to start google presubmit (#50060)

--- esm2020/src/render3/partial/injectable.mjs
+++ esm2020/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.8+sha-9107e93'));
+    definitionMap.set('version', o.literal('15.2.8+sha-ea82d3b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // Only generate providedIn property if it has a non-null value
