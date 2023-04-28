b3a7f213e26e3f198ffc0d1e5435bf1587c7cd45
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:25:25 +0000

    d5fb3ad645 refactor(core): finalize rxjs-interop options & docs (#50071)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-cb5c4e0'));
+    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-d5fb3ad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
