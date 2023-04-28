c2e7d1978213170289acd88e9aa55e516219eb2f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 16:36:08 +0000

    cb5c4e0bff build: update cross-repo angular dependencies (#50067)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-6da1054'));
+    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-cb5c4e0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
