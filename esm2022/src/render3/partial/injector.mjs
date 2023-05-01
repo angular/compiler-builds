acf717bcb640726934efa2940f1571117d649860
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 17:46:05 +0000

    1bc3546c7f docs: release notes for the v16.0.0-rc.4 release

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-b83d84a'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-1bc3546'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
