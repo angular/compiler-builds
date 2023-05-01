5b64a3cb250e811e4c4d93b237597234c585284a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 17:19:17 +0000

    952569c2c6 docs: update the intro page (#49980)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-d6d347f'));
+    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-952569c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
