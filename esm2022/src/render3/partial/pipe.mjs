743623bb507cf7ee9be4f452ac30a18b8c2122b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:13:22 +0000

    06452af31f fix(platform-server): remove dependency on `@angular/platform-browser-dynamic` (#50064)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-3c47448'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-06452af'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
