a2c35cad42a6fc07d0337f0a49ddbe5db7db5183
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:12:35 +0000

    d6d347fec5 docs: add developer preview guide for esbuild-based build system (#50068)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-6dc3add'));
+    definitionMap.set('version', o.literal('16.0.0-rc.3+sha-d6d347f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
