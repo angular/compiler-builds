afa1ae633061e87e96ef6df7ee3b416bb257a36b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 11:35:03 +0000

    8080bf0076 docs: Make links out of @see tags (#50098)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0-rc.4+sha-c9d4a69'));
+    definitionMap.set('version', o.literal('16.0.0-rc.4+sha-8080bf0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
