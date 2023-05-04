d9cc20ed297031b3725d057e86bab6c51eb92416
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:12:57 +0000

    cb8cdadd3b refactor(compiler): reflect arrow function definition (#50084)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-2180a69'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-cb8cdad'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
