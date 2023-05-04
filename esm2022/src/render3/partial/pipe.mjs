3ee8ddc736163bf7d71c41023f5519aa61d25c13
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:25:37 +0000

    f28580a42b build: remove the webworker platform. (#50059)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0+sha-f3bb680'));
+    definitionMap.set('version', o.literal('16.0.0+sha-f28580a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
