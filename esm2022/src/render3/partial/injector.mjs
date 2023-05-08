54e56ded65f5ef54120b997ce53b5e09da3eec10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 17:04:28 +0000

    da7ff37dc9 Revert "docs(docs-infra): Remove unused annotation template (#50114)" (#50206)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0+sha-2f03eef'));
+    definitionMap.set('version', o.literal('16.0.0+sha-da7ff37'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
