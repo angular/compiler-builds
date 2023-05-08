ee18c79dd8272e301243ec5ba01449d4dae47202
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:01:52 +0000

    a684888af7 fix(core): only try to retrieve transferred state on the browser (#50144)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-4655fd5'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-a684888'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
