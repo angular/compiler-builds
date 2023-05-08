6d8510a7d44ceab9a3c7825a2691725b22b546ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:01:56 +0000

    fc1d777c69 docs: Fixed Spelling 'servivce' to 'service' (#50204)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-a684888'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-fc1d777'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
