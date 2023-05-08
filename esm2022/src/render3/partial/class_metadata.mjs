337c9cad2a46a8e6f8cadf98860f4f7c93e6915c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:08:45 +0000

    fe653c2141 docs: fix filename in first-app (#50207)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.0+sha-da7ff37'));
+    definitionMap.set('version', o.literal('16.0.0+sha-fe653c2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
