1ef84d4051bd8e89a8f8fa363d6314ab225ca7d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:05:12 +0000

    1c7f690247 docs: update starter lesson to contain example images from the start (#50212)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.0+sha-7ac8f09'));
+    definitionMap.set('version', o.literal('16.1.0-next.0+sha-1c7f690'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
