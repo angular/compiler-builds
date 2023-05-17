ae12b194c0cdae03f9af8101bd54b7565dc623b3
Andrew Kushnir <akushnir@google.com>
2023-05-17 15:33:05 +0000

    656c9537f0 docs: remove plnkr link from markForCheck example (#50328)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.1+sha-3eafb4b'));
+    definitionMap.set('version', o.literal('16.0.1+sha-656c953'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
