59690d2b468892336f4e4a6c1e6df019f4154857
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:36:47 +0000

    b89fae173e docs(language-service): correction of the demonstration translation (#50359)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.2+sha-2d3dc4a'));
+    definitionMap.set('version', o.literal('16.0.2+sha-b89fae1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
