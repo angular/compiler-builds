a5bdc9ac6c931d3840601eac4848498b3ca3e2d8
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 17:51:08 +0000

    f40ecc025b docs: replaced the html tags with the updated ones (#50403)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.2+sha-5574e47'));
+    definitionMap.set('version', o.literal('16.0.2+sha-f40ecc0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
