8401c2ae2224b0adf77b854a9b21f85264271256
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 15:00:16 +0000

    5f3e8a9312 docs: clarify example instructions (#50371)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-97f90e0'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-5f3e8a9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
