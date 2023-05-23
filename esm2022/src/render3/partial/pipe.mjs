3ea7aa5ffa68599345cbd26aa38a80e9c2a83f92
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:33:53 +0000

    e8c51a1840 refactor(core): Add a animation standalone example in the bundling test (#50413)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-f79a5bc'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-e8c51a1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
