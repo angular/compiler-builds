2719228b376cb5d3bd24bf468833570793af7750
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:33:30 +0000

    f7b8fb3c97 build: remove unused gulp helper script (#50428)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-f4393f7'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-f7b8fb3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
