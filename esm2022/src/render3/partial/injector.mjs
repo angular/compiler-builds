3338542f68de23857b5c6a482f8d0fb8c2d5d5a9
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:29:55 +0000

    e516556a8f docs: fix code-example header path in hierarchical-dependency-injection-md file (#50404)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-3152c4d'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-e516556'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
