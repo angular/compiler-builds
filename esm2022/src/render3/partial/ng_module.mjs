9f3446f9d134c52e5dae6ba332013f915f13bddb
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-05-24 17:56:08 +0000

    cbe27990c2 docs: release notes for the v16.0.3 release

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-2cdb4c5'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-cbe2799'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
