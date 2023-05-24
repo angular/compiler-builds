b0ce8cc476b35e05628edd165a041886f1ae7068
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-24 14:18:32 +0000

    75fdbcb8f2 fix(core): fix `Self` flag inside embedded views with custom injectors (#50270)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.1+sha-5fd4bf6'));
+    definitionMap.set('version', o.literal('16.1.0-next.1+sha-75fdbcb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
