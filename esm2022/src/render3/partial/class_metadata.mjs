7827638e6b6082453fa7e4a193d75978bb31ac3f
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:14:14 +0000

    d856be0314 docs: removed the obsolete frameborder Tag and updated the files with using css (#50480)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.2+sha-1ca6362'));
+    definitionMap.set('version', o.literal('16.1.0-next.2+sha-d856be0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
