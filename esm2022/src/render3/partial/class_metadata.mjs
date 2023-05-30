c359be2da5feacc990be97c9544ff89285ac2d63
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 20:13:50 +0000

    0c80349cf6 refactor(http): replace zone.js macrotask creation with `InitialRenderPendingTasks` (#50425)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.2+sha-381cb98'));
+    definitionMap.set('version', o.literal('16.1.0-next.2+sha-0c80349'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
