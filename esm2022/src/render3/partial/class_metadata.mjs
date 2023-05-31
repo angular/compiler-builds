dc9aa82f35157ddda484c1e7a4657233047addee
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:55:24 +0000

    7e468dffce docs: correct Signals documentation (#50518)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.2+sha-ac447ed'));
+    definitionMap.set('version', o.literal('16.1.0-next.2+sha-7e468df'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
