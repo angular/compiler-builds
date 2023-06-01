e67491c2a41d2be1e3a5bceb141e8fcabdeebe84
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 15:59:04 +0000

    019c2d2f14 docs: selector precisions (#50534)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.3+sha-84b6ef0'));
+    definitionMap.set('version', o.literal('16.1.0-next.3+sha-019c2d2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
