90c33cef6df56464ede577d4ce6496476f44bcf5
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:02:44 +0000

    9847085448 refactor(compiler): support property interpolation in the template pipeline (#50118)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.3+sha-57d47b3'));
+    definitionMap.set('version', o.literal('16.1.0-next.3+sha-9847085'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
