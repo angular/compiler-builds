686db1986cf5461698357887c22fbc27d95208e3
Dylan Hunn <github@dylanhunn.com>
2023-06-01 15:55:38 +0000

    84b6ef0262 release: cut the v16.1.0-next.3 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-next.2+sha-7f85d08'));
+    definitionMap.set('version', o.literal('16.1.0-next.3+sha-84b6ef0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
