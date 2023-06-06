b1ca4914e69a1d04695202012086bf07b92c730d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-06 12:41:11 +0000

    21eb5ee840 fix(docs-infra): replace use of turbo on StackBlitz with npm (#50576)

--- esm2020/src/render3/partial/pipe.mjs
+++ esm2020/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.9+sha-2d68de5'));
+    definitionMap.set('version', o.literal('15.2.9+sha-21eb5ee'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.internalType);
