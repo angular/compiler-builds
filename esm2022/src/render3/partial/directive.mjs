cd244e96c216ba264acbf40e7fa1554ad3bf2ecf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:06:08 +0000

    c5facb9433 refactor(compiler-cli): Remove .d.ts files transformer in local compilation mode. (#50486)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.4+sha-7106b2f'));
+    definitionMap.set('version', o.literal('16.0.4+sha-c5facb9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
