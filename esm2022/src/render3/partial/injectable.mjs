699e7b021e7b0c21c0cadd48a0db27995ec61edb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 12:41:04 +0000

    3247a46485 docs: update first-app-lesson-11.md to include info about routerLink (#50348)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-3b2f119'));
+    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-3247a46'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
