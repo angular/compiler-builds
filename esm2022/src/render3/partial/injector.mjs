04d0b43db0d8415044faa5f2360a65c9fd36015e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 16:35:43 +0000

    9429f96f62 test(compiler): Prevent back-sliding on already passing template pipeline tests (#50582)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-d773589'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-9429f96'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
