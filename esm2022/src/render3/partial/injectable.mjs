101186cd3fe3e65803414863a777c0e7f081d9aa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 13:46:27 +0000

    afb7ea4e8c refactor: removed the unused contructor in lazyModule (#50536)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-d5a8ff3'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-afb7ea4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
