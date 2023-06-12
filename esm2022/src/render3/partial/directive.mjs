f2220e8bfc0e69e2e9a95c5b3306c18912176de6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 13:51:10 +0000

    ffdc1304d6 build: update io_bazel_rules_sass digest to 6e0915e (#50605)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-3247a46'));
+    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-ffdc130'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
