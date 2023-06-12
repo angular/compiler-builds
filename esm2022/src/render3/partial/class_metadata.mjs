339668eb1ed13e9a2ad0e994c9b7cf5103fd2d1c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:23:13 +0000

    7c7a4a273a docs: added lang attributes in html files (#50588)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-8234738'));
+    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-7c7a4a2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
