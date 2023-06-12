f93f5db5c03faf12e514a4c732776548c1e247e5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 11:01:18 +0000

    dbadfea67f feat(devtools): create demo application that uses standalone APIs and standalone components/directives/pipes (#48533)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-b1aed48'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-dbadfea'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
