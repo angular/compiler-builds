4f34a278fa7abe1ef20842cffc5517f10abc0172
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 12:59:26 +0000

    fa3b7332a9 build: update actions/checkout action to v3.5.3 (#50689)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-b90f2b1'));
+    definitionMap.set('version', o.literal('16.1.0-rc.0+sha-fa3b733'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
