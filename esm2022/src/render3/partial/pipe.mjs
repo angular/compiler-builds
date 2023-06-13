41f109e2e494c0ceef82a5c194d5d04f22b4e110
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:31:49 +0000

    05ac0868c9 fix(core): avoid duplicated content during hydration while processing a component with i18n (#50644)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.0.5+sha-50e1858'));
+    definitionMap.set('version', o.literal('16.0.5+sha-05ac086'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
