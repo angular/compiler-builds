f5a497fa82d51bc025763f414f78c0b02e9fef08
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 09:09:34 +0000

    1ad4d55d91 fix(core): extend toSignal to accept any Subscribable (#50162)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-88962b7'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-1ad4d55'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
