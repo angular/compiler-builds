54b18ad85b05382215efe88e080a9af2fae7068f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 11:02:11 +0000

    8d42747971 refactor(core): handle #24571 todos. (#49221)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-c31fe76'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-8d42747'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
