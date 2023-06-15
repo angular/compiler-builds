0df1e13fd693ecb0d2c7b083cd950f37c1657284
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-15 14:59:44 +0000

    463e04ca22 refactor: use `queueMicrotask` to schedule micro tasks instead of various helpers (#50485)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.1+sha-1d3e96b'));
+    definitionMap.set('version', o.literal('16.1.1+sha-463e04c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
