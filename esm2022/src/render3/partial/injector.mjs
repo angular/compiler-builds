e139cc643b40a740b587874acb9ee3d42a92507a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-15 13:45:03 +0000

    1d3e96b7d9 test: mark ngtsc test target as flaky (#50718)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.1+sha-fd519b0'));
+    definitionMap.set('version', o.literal('16.1.1+sha-1d3e96b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
