857e5d6088e4d67e230573e4bf170267648097d1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-18 19:43:50 +0000

    d5736d7d50 build: create github action to run benchmark comparisons (#50745)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-9588b11'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-d5736d7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
