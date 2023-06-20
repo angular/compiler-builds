9329ad0043f278b2ee0ab45fe183056a61ebb9a1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 09:08:20 +0000

    43a80ca8f8 build: update eslint dependencies to v5.60.0 (#50690)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-254023a'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-43a80ca'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
