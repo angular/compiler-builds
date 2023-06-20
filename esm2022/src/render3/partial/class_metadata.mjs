97e957184b12a63f17cd3c9fc690ebd2623ca1d6
Andrew Kushnir <akushnir@google.com>
2023-06-20 20:11:07 +0000

    1221b37150 ci: update codeowners for benchpress (#50769)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-4af4910'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-1221b37'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
