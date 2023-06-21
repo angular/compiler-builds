cafa756b82e6ed34c9bff87af8ef446c2642128b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-06-21 18:39:27 +0000

    a99b6ff141 docs: release notes for the v16.1.2 release

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-a126cbc'));
+    definitionMap.set('version', o.literal('16.2.0-next.0+sha-a99b6ff'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
