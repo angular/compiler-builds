c16fe1ae38f7053d9efcb8506bd533d40d8839c1
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-06-28 15:19:51 +0000

    d9d70f0a6f release: cut the v16.1.3 release

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.2+sha-520a1b5'));
+    definitionMap.set('version', o.literal('16.1.3+sha-d9d70f0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
