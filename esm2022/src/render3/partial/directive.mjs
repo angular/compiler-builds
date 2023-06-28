8d1d1fb4d8bec12de6d74812fdc267cd20f47f65
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-06-28 15:26:32 +0000

    fc9da5ed8c release: cut the v16.2.0-next.1 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-d9d1b85'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-fc9da5e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.0+sha-d9d1b85'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-fc9da5e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
