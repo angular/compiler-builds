1bddd7d69465252aed846ad36bf4ebac5ec62197
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-28 20:52:56 +0000

    4073cc04b3 build: lock file maintenance (#50877)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.3+sha-d9d70f0'));
+    definitionMap.set('version', o.literal('16.1.3+sha-4073cc0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
