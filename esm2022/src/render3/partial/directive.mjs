211ec13f9c591834f84658919d3e0cf62efbb73c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:55:17 +0000

    4dcce63665 build: update all non-major dependencies (#50879)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-471cdff'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-4dcce63'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
