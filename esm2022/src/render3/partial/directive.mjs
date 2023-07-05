84011e61589c7aa2fa2cb7129100952afc326778
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:11:53 +0000

    941659f5fd docs: Router tutorial tour of heroes - doc bug fix suggestion #50794 (#50846)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-f9175dd'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-941659f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
