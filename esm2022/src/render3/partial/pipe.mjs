334b46381db14bb861fdffe5e94514557b54cc17
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:07:50 +0000

    e77cb75c54 docs(forms): FormBuilder is not associated with ReactiveFormsModule (#50941)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-e47dfa0'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-e77cb75'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
