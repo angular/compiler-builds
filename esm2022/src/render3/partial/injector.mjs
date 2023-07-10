d8f83e8e251ab1cf25c74323d877d412338354e0
Dylan Hunn <dylhunn@gmail.com>
2023-07-10 14:16:15 +0000

    5528b72979 docs: add link to critters (#50958)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-2b08503'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-5528b72'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
