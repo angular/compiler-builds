e67f5cf0a21f7be671f0c8e25435e813d7c3dd4e
Joey Perrott <josephperrott@gmail.com>
2023-07-11 18:13:40 +0000

    d9f832d55c ci: remove GHA lint job from required statuses (#51011)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-f1b6299'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-d9f832d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
