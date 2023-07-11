6645968ede905d6bd8fe6e9fe2f77cb053916c38
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:45:04 +0000

    a14bdfe859 fix(animations): Ensure elements are removed from the cache after leave animation. (#50929)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-3dafc14'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-a14bdfe'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
