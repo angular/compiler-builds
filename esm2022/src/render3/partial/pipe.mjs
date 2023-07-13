ec50f5ac56e12a8c88a50513f5b34091a3ce6a0f
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:52:21 +0000

    36c5a57a3c docs(docs-infra): typo fix in aot-metadata-errors (#51017)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.1+sha-3b78d06'));
+    definitionMap.set('version', o.literal('16.2.0-next.1+sha-36c5a57'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
