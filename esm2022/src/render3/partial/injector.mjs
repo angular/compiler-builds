bd7bfa99b20dafee718c2c72a8b1a6d45d3eed17
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 16:59:10 +0000

    8d636ad6a7 docs: updated the http tag with https (#50927)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.2+sha-5a5bcd2'));
+    definitionMap.set('version', o.literal('16.2.0-next.2+sha-8d636ad'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
