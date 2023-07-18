941c8966fdd1ee6951426c99c59ca38788013046
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 19:14:46 +0000

    109737b598 docs(common): Add NgOptimizedImage FAQ (#51036) Make minor updatees to NgOptimizedImage docs, and add an FAQ.

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.2+sha-7410d68'));
+    definitionMap.set('version', o.literal('16.2.0-next.2+sha-109737b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
