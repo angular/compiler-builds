575d5f0427f59d6ba1fd37283e9d8d4481b71c75
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-07-19 19:23:57 +0000

    16e132f31f docs: release notes for the v16.1.6 release

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.2+sha-386cb2f'));
+    definitionMap.set('version', o.literal('16.2.0-next.2+sha-16e132f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
