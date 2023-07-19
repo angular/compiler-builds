d21db1b2883df0a06a77f7330b5910a52e9c8809
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-07-19 19:26:30 +0000

    46afa8a91f release: cut the v16.1.6 release

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.5+sha-8c8e721'));
+    definitionMap.set('version', o.literal('16.1.6+sha-46afa8a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
