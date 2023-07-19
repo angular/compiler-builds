88d9acc940823f3a79e3676e6df326475de7d561
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 17:23:01 +0000

    8fb9db9dad docs: Fix path to sample (#51076)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.2+sha-24bf133'));
+    definitionMap.set('version', o.literal('16.2.0-next.2+sha-8fb9db9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
