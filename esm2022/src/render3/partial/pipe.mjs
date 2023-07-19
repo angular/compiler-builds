2b1868bef2731dee4dbb6244d2499f412c19f4f6
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 18:58:31 +0000

    8c8e7210f4 Revert "docs: added the alt attribute in the img tag (#51102)" (#51107)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.5+sha-4176f59'));
+    definitionMap.set('version', o.literal('16.1.5+sha-8c8e721'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
