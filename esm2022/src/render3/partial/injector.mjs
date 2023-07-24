0254495cc82ad564dbba70784d4ddeddd2a969c8
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:36:49 +0000

    de0d3d704c docs: remove orphan migration docs (#51147)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.6+sha-7cfc365'));
+    definitionMap.set('version', o.literal('16.1.6+sha-de0d3d7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
