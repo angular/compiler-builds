cb4d34f81880d5f459168076baaedb42f22412df
Andrew Kushnir <akushnir@google.com>
2023-07-25 17:39:19 +0000

    03f9c9305b docs: updated the tour of heroes doc file (#51151)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.6+sha-de0d3d7'));
+    definitionMap.set('version', o.literal('16.1.6+sha-03f9c93'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
