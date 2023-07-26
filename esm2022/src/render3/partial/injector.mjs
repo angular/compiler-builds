54a54bbce15b32f792960b4fefa69be6d3791ffe
Andrew Kushnir <akushnir@google.com>
2023-07-26 18:30:52 +0000

    21299d95f5 fix(devtools): ensure that inspected component label is always in the viewport (#50656)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.7+sha-07969d1'));
+    definitionMap.set('version', o.literal('16.1.7+sha-21299d9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
