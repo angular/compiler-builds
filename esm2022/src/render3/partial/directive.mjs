352a3e1c66585fc73b7ef5b38e97e831ee8322e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:07:26 +0000

    71e803a868 docs: add new edition of Angular Projects book (#51184) PR Close #51184

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.4+sha-5c6d1bb'));
+    definitionMap.set('version', o.literal('16.2.0-next.4+sha-71e803a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
