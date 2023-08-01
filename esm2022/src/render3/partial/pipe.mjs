5bbe6318f1fa0589af4325c2ed45419f4d1ea815
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:31:13 +0000

    7d579b9db0 docs: add lang attribute to html files (#51235)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.1.7+sha-db0f653'));
+    definitionMap.set('version', o.literal('16.1.7+sha-7d579b9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
