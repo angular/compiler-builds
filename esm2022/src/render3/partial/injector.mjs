dbd2b1e4140f74c7d12c77078dea30619a126803
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 20:19:01 +0000

    8913d3e407 docs: fix warning about an unknown decorator (#51237)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.4+sha-e53d4ec'));
+    definitionMap.set('version', o.literal('16.2.0-next.4+sha-8913d3e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
