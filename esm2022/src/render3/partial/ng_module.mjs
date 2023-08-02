8abb73dbbcbd5c67aed80af4fc1aefd95120f24a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-02 20:31:25 +0000

    1472515d68 release: cut the v16.2.0-rc.0 release

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-next.4+sha-9d88cd8'));
+    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-1472515'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
