75011d58f8fb7d87170bdd54a50b859716801f50
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:46:08 +0000

    cf62063174 docs: add title tag to index file (#51266)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-1472515'));
+    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-cf62063'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
