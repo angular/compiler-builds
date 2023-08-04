44cf84a2ddb9541230e0e3001b804834565d2479
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 20:16:21 +0000

    e01549b26b refactor(core): introduce LView and LContainer utility functions (#51191)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-cf62063'));
+    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-e01549b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
