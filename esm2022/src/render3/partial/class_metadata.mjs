d2fd78bb403f10afbfae03b66bb60d10195bed6b
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:36:35 +0000

    4fe1813c40 refactor(core): remove unnecessary import for custom expect. (#51216)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.3.0-next.0+sha-f12f906'));
+    definitionMap.set('version', o.literal('16.3.0-next.0+sha-4fe1813'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
