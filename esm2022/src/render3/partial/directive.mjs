f4ef79e320b154c0de83dcc826fca12beb0fb1f1
Dylan Hunn <dylhunn@gmail.com>
2023-08-09 17:34:22 +0000

    9998bee5f4 refactor: add `dgp1130` to DevTools PullApprove config (#51301)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-899e51a'));
+    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-9998bee'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-899e51a'));
+    definitionMap.set('version', o.literal('16.2.0-rc.0+sha-9998bee'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
