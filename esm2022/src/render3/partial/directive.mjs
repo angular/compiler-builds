275a4ad3feb7b0e6f2b6d1c9e812d82950981342
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:46:44 +0000

    8f944e0752 ci: migrate framework tests to GHA (#51101)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -35,7 +35,7 @@ export function compileDeclareDirectiveFromMetadata(meta) {
 export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0+sha-af0c8b8'));
+    definitionMap.set('version', o.literal('16.2.0+sha-8f944e0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
