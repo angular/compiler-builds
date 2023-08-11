3ee9afdba653d81b8f591ab730536d5807213701
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-11 13:17:44 +0000

    b2661fe2a0 docs(docs-infra): Hide decorator option row if empty. (#51327)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.3.0-next.0+sha-464b458'));
+    definitionMap.set('version', o.literal('16.3.0-next.0+sha-b2661fe'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
