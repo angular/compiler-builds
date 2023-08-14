5f53e06cb878ed6d0398316fa96ca499705704b5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 21:41:09 +0000

    5a2b7683bc docs: Add CDN Optimization section to hydration guide (#51355)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.0+sha-ebd217d'));
+    definitionMap.set('version', o.literal('17.0.0-next.0+sha-5a2b768'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
