a7ad28a72e3ecb1e1c32bf04e6c1f7b0f3b1b72b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:05:18 +0000

    83b708203e docs: Improve host binding docs (#51326)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.0+sha-3348f88'));
+    definitionMap.set('version', o.literal('17.0.0-next.0+sha-83b7082'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
