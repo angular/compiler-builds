74bbfe4097ed6077b49d166dcdc734b209c19d49
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-16 17:29:48 +0000

    4b46819ed1 release: cut the v16.2.1 release

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.0+sha-7a090ee'));
+    definitionMap.set('version', o.literal('16.2.1+sha-4b46819'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
