2de47381b9cfcfec1a324275cf229b96c4a480a0
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:44:01 +0000

    4fe88cf9af docs: add lang attribute in html files (#51269)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.1+sha-bf0f80e'));
+    definitionMap.set('version', o.literal('16.2.1+sha-4fe88cf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
