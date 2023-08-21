0c724031513ac7d7fb5bd8273f17c206dd0ddaf3
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:26:24 +0000

    a9b3c006f8 fix(core): guard the jasmine hooks (#51394)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.0+sha-5343486'));
+    definitionMap.set('version', o.literal('17.0.0-next.0+sha-a9b3c00'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
