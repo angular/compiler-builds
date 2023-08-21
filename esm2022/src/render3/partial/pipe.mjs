430055ea5884a37c100c2e2622133652111a029e
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:45:46 +0000

    9977727f73 docs: fix typos in inputs-outputs guide (#51428) PR Close #51428

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.0+sha-b7d25bb'));
+    definitionMap.set('version', o.literal('17.0.0-next.0+sha-9977727'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
