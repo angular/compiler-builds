3406f15c0d4ac3e2de71fcb97ade294d30d67482
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-08-23 17:45:28 +0000

    03fc92d448 release: cut the v16.2.2 release

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.1+sha-76860b5'));
+    definitionMap.set('version', o.literal('16.2.2+sha-03fc92d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
