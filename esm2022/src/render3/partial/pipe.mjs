7c76a16263f9afbba52a1187aff18c4b5734e999
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-24 16:19:01 +0000

    6f4a5c325d build: add AIO_PAYLOAD_TOKEN env variable (#51489)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.2+sha-546a8e5'));
+    definitionMap.set('version', o.literal('16.2.2+sha-6f4a5c3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
