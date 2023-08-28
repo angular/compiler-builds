fa2722a003e10fb5f0cac75644c56fa84e2599b6
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 23:14:48 +0000

    3a19d6b743 fix(core): run afterRender callbacks outside of the Angular zone (#51385)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.1+sha-c2d8592'));
+    definitionMap.set('version', o.literal('17.0.0-next.1+sha-3a19d6b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
