6f0ad0d0ed4f230c986b9ba2d832cb3cb08f6efd
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 22:47:20 +0000

    fbdca15a86 docs: migrate attribute-binding code to standalone (#51477)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.1+sha-5d1d3b3'));
+    definitionMap.set('version', o.literal('17.0.0-next.1+sha-fbdca15'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
