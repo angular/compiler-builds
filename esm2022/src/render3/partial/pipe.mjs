6680f319f87c904c016c32906ba4dccb37fc9325
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:00:18 +0000

    dbd761f528 fix(compiler-cli): correct incomplete escaping (#51557)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.2+sha-6e5b865'));
+    definitionMap.set('version', o.literal('16.2.2+sha-dbd761f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
