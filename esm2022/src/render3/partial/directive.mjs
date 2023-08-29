13efe83205f51264569012264bc5e4afa3d71dff
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:42:29 +0000

    fa29eb84a3 docs: fix casing of a11y labels in main page (#51431) PR Close #51431

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.1+sha-8c77240'));
+    definitionMap.set('version', o.literal('17.0.0-next.1+sha-fa29eb8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
