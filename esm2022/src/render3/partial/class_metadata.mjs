477fdf86a5f7f00122c331ccd6ad3964e50eb2c0
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 16:01:24 +0000

    dad47bb720 docs: migrate security guide and example to standalone (#51446)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.1+sha-c340d6e'));
+    definitionMap.set('version', o.literal('17.0.0-next.1+sha-dad47bb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
