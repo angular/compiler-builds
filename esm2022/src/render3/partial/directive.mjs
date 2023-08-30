e57a97b7a9ede44d97e0a8569d52ceb36b1592fb
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 14:53:44 +0000

    c340d6e044 feat(platform-browser): enable removal of styles on component destroy by default (#51571)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.1+sha-0f86a0b'));
+    definitionMap.set('version', o.literal('17.0.0-next.1+sha-c340d6e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
