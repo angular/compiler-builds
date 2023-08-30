13458dd33a1dbd7a1499250892e208b330704ad0
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 19:54:56 +0000

    68d6fae734 build: enable video recording for saucelabs bazel tests (#51533)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.3+sha-587093c'));
+    definitionMap.set('version', o.literal('16.2.3+sha-68d6fae'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
