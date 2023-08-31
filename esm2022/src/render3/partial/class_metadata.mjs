b38a3cab0a00cbe50f114b8dc4137a84f5f8170e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 21:52:49 +0000

    3658e25f2e docs: migrate toh-pt0 to standalone (#51575)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-05e4dbb'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-3658e25'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
