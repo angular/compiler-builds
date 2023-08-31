42662f03ec2b3c92d40eee93ef3ab2f37403038a
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:25:25 +0000

    8a3479b9e4 fix(docs-infra): fix incomplete escaping (#51604)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-acd59ad'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-8a3479b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
