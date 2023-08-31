760fd3f742ed7b5107717a8b9da0fb7221b0e7d5
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 19:07:59 +0000

    0839885a50 refactor(core): make AfterRenderEventManager tree-shakable (#51541)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-a1bad49'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-0839885'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
