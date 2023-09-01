dbd4835fd05e0bac0377296e74d7326a1b4f7b37
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 15:26:46 +0000

    0907f396d5 build: migrate to node 18 (#51609)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-fe1a50c'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-0907f39'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
