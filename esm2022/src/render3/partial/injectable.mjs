fb4f83fa01b6619741a5556cfe6bc5190bd80c7c
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 21:01:12 +0000

    970d68fe76 refactor(core): add `prefetch on idle` support for defer blocks (#51629)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-f61f892'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-970d68f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
