0c680c2c200a551a48523186c427742727e35b50
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:17:43 +0000

    2d96dc27b7 ci: remove test job from circleci in favor of GHA (#51660)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-e804075'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-2d96dc2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
