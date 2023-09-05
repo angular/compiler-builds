36a59bdd42e98e136c3821b01c491de2e696f196
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 20:58:20 +0000

    a734ca3e15 Revert "ci: disable all of the angular-robot settings (#51663)" (#51666)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.3+sha-c2fac37'));
+    definitionMap.set('version', o.literal('16.2.3+sha-a734ca3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
