387ea52999e4210456a5ca7513073df05e0437dd
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 17:03:32 +0000

    3c0c53d4df docs: remove mention of the string based lazy loading (#51651)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.2+sha-7fa17d0'));
+    definitionMap.set('version', o.literal('17.0.0-next.2+sha-3c0c53d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
