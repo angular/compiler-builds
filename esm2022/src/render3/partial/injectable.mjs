7a3b16def37c8f64f1b51c96d6895ea0112acddf
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:40:46 +0000

    4051930b8c docs: Add info about hydration caching. (#51648)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.3+sha-0de3262'));
+    definitionMap.set('version', o.literal('16.2.3+sha-4051930'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
