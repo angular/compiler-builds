856205b9c3fa6839b0f4a51896d21a6e86f7d99a
Andrew Kushnir <akushnir@google.com>
2023-09-11 16:31:21 +0000

    38c6fa1cee fix(docs-infra): Ensure experimental tag shows up on docs (#51712)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.4+sha-c700418'));
+    definitionMap.set('version', o.literal('16.2.4+sha-38c6fa1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
