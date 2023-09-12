386e1a6e51185e8782e5ba792d60e2471a3f3c7a
Andrew Kushnir <akushnir@google.com>
2023-09-12 21:07:15 +0000

    59387ee476 feat(core): support styles and styleUrl as strings (#51715)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.3+sha-f993975'));
+    definitionMap.set('version', o.literal('17.0.0-next.3+sha-59387ee'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
