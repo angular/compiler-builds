549fe49ed572d7faaaffd661ac1c01d7dde6b27a
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:58:23 +0000

    2a1723c945 test(compiler): fix broken integrity check (#51751)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.4+sha-06bbc2f'));
+    definitionMap.set('version', o.literal('17.0.0-next.4+sha-2a1723c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
