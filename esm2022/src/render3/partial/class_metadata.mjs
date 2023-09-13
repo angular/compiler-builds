f9159ca36c31b2ae683baa8c330dd9d1b3044ed3
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:59:21 +0000

    65d69157cb ci: migrate legacy saucelabs job to Github Actions (#51695)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.5+sha-cc57b24'));
+    definitionMap.set('version', o.literal('16.2.5+sha-65d6915'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
