74aa31a643a6c86f8f1a3a8e77ea036bdd7c1803
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-09-13 17:43:34 +0000

    cc57b24881 release: cut the v16.2.5 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.4+sha-f6f3783'));
+    definitionMap.set('version', o.literal('16.2.5+sha-cc57b24'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
