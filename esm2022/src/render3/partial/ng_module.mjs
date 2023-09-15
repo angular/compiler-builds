04fcdd4e739ff41b7b6841822b172c96daf94645
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:19:06 +0000

    5a20a44c64 fix(localize): ng-add schematics for application builder (#51777)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.4+sha-88ac76f'));
+    definitionMap.set('version', o.literal('17.0.0-next.4+sha-5a20a44'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
