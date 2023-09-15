db218b1c233b2f5613761812dcc241ba55d9de5a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:15:02 +0000

    d79db4a9e1 build: update io_bazel_rules_sass digest to 63d77ff (#51243)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.4+sha-25b9b86'));
+    definitionMap.set('version', o.literal('17.0.0-next.4+sha-d79db4a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
