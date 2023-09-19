a4354eb8c535412914b0fcd3f52a84a629c6c406
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 16:51:12 +0000

    0ee0f780e4 build: update dependency jasmine to v5 (#51820)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.4+sha-ba64e9c'));
+    definitionMap.set('version', o.literal('17.0.0-next.4+sha-0ee0f78'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
