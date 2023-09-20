aee757d902e31dab3da4e6d9b42af3b75cf3b638
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 18:00:26 +0000

    6e6452d604 release: cut the v16.2.6 release

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.5+sha-4ccd845'));
+    definitionMap.set('version', o.literal('16.2.6+sha-6e6452d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
