bea74e58d9020038ac7978a825e7a9b3996b3a7f
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 17:11:52 +0000

    e5d327d049 refactor(animations): Use the `Writable` type when overwriting readonly properties. (#49754)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.5+sha-4599642'));
+    definitionMap.set('version', o.literal('17.0.0-next.5+sha-e5d327d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
