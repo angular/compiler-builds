9eea0ba1d85547210c5f8626650063c4e86d56ee
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 16:34:19 +0000

    acfea3cbe3 build: update scorecard action dependencies (#51899)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.5+sha-c52995d'));
+    definitionMap.set('version', o.literal('17.0.0-next.5+sha-acfea3c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
