1f85436306293c0aa861d15cc5d74462ae7b3db6
Dylan Hunn <github@dylanhunn.com>
2023-09-27 20:37:45 +0000

    a16e3b8fe1 release: cut the v16.2.7 release

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.6+sha-39a3e34'));
+    definitionMap.set('version', o.literal('16.2.7+sha-a16e3b8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
