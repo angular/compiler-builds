af3e1db0738f3bc0c93cca99a112eb6fd4eeb997
Dylan Hunn <dylhunn@gmail.com>
2023-09-27 17:39:23 +0000

    39a3e34e03 fix(core): allow toSignal calls in reactive context (#51831) (#51892)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.6+sha-aedd8de'));
+    definitionMap.set('version', o.literal('16.2.6+sha-39a3e34'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
