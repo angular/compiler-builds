85ec2507575a2390a6421e9c4bb4554afd04bf0e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:01:01 +0000

    82e8e5e043 ci: move permissions for google-internal-tests into job definition (#51847)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.7+sha-a16e3b8'));
+    definitionMap.set('version', o.literal('16.2.7+sha-82e8e5e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
