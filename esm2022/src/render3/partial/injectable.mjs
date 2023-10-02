32740e3aa5ac19b3f1eb59af0031cb9e7316544c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:49:09 +0000

    96141c6ad6 ci: add slack messages for failed aio deployments (#51956)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.7+sha-781bb91'));
+    definitionMap.set('version', o.literal('16.2.7+sha-96141c6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
