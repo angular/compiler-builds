24f8a6a6b4fce34f9765df19458c30f95656f55a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 16:12:20 +0000

    9fe08968b8 fix(service-worker): throw a critical error when handleFetch fail (#51989)

--- esm2020/src/render3/partial/ng_module.mjs
+++ esm2020/src/render3/partial/ng_module.mjs
@@ -30,7 +30,7 @@ export function compileDeclareNgModuleFromMetadata(meta) {
 function createNgModuleDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('15.2.9+sha-21eb5ee'));
+    definitionMap.set('version', o.literal('15.2.9+sha-9fe0896'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.internalType);
     // We only generate the keys in the metadata if the arrays contain values.
