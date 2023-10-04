7f8f6954a878f2c6a411e14b10f5d17e2c37c60f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 15:24:52 +0000

    7426948ff6 refactor(common): update `NgTemplateOutlet` to no longer rely on context swapping (#51887)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.6+sha-d77c4b2'));
+    definitionMap.set('version', o.literal('17.0.0-next.6+sha-7426948'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
