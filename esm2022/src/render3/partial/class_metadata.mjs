db2326e1aa769e7d7285dbaa7b27cf89f9098339
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 16:02:09 +0000

    966ce9790a fix(service-worker): throw a critical error when handleFetch fails (#51960)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.7+sha-334fe60'));
+    definitionMap.set('version', o.literal('16.2.7+sha-966ce97'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
