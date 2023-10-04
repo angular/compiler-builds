6a9a299b554086caecc00078a4d78662a61cc40d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 20:34:13 +0000

    c38846292b docs: release notes for the v15.2.10 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.6+sha-a7fa253'));
+    definitionMap.set('version', o.literal('17.0.0-next.6+sha-c388462'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
