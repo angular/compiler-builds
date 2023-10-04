b1ac35ac3fd72bb47a7b20c0e7e66984467ee463
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 21:40:21 +0000

    fe81ff8cd6 release: cut the v17.0.0-next.7 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.6+sha-6441de8'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-fe81ff8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
