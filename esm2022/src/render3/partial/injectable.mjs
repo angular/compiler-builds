8cb5aed72477876269194f64a19e5cb8d6f2ce79
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:40:20 +0000

    f06d97e86d test(core): await all async work to be completed before checking test results (#52064)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-22fa9fe'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-f06d97e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
