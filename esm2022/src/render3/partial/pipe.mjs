7c87c562f18b40d0407808a1206c9460bc624f7f
Andrew Scott <atscott01@gmail.com>
2023-10-06 20:10:21 +0000

    2e16b0e0ea ci: update `@defer` symbol test golden file (#52088)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-048f400'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-2e16b0e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
