6cca45749786d55ace1fbfb81074465159f670ca
Andrew Scott <atscott01@gmail.com>
2023-10-06 19:21:02 +0000

    048f400efc feat(core): add warnings for oversized images and lazy-lcp (#51846)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-b6b9eae'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-048f400'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
