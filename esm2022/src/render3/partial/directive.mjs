03c4af6da70c03a7219682d30a3bec3d13ddabb3
Andrew Scott <atscott01@gmail.com>
2023-10-09 19:10:04 +0000

    e63e3db80a build: update all non-major dependencies (#52024)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.8+sha-5f433f6'));
+    definitionMap.set('version', o.literal('16.2.8+sha-e63e3db'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
