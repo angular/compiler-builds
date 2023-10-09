eef862a9b6b4c8e5eecd762cb542e6617df2e94e
Andrew Scott <atscott01@gmail.com>
2023-10-09 17:16:00 +0000

    28a5925f53 fix(common): use === operator to match NgSwitch cases (#51504)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-e25006b'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-28a5925'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
