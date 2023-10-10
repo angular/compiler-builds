42db8052fbba53f04a3d8bb95205484d837d38bc
Andrew Scott <atscott01@gmail.com>
2023-10-10 21:38:53 +0000

    da056a1 fix(common): add missing types field for @angular/common/locales of exports in package.json (#52080)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-11588a1'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-da056a1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
