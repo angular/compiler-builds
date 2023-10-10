465806d0aa2448cbd05b89d91b10d1cddc5ce046
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:05:00 +0000

    5a4ccdf build: update cross-repo angular dependencies (#52133)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-d487014'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-5a4ccdf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
