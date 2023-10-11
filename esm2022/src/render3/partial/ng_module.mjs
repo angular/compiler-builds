bb0e980eb06c96d9258293ca1f0e16bd06e4b588
Andrew Scott <atscott01@gmail.com>
2023-10-11 17:18:23 +0000

    926db6d release: cut the v17.0.0-next.8 release

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-44a75aa'));
+    definitionMap.set('version', o.literal('17.0.0-next.8+sha-926db6d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
