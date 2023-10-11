e579ff1e3fdc0a2e9a6ac65b636bb26cad518294
Andrew Scott <atscott01@gmail.com>
2023-10-11 16:30:12 +0000

    87340a4 build: update io_bazel_rules_sass digest to 9a327e5 (#52119)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.7+sha-e906942'));
+    definitionMap.set('version', o.literal('17.0.0-next.7+sha-87340a4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
