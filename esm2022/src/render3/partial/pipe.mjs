18f00426cc4f3f86688b1b21098a59228c30877e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 15:50:17 +0000

    25788b2919 docs: add Nicolas Frizzarin to GDE resources (#52003)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.9+sha-f52eee8'));
+    definitionMap.set('version', o.literal('16.2.9+sha-25788b2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
