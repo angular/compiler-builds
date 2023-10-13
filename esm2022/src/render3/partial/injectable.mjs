ff4d94d2b3b3f93c0eb24b8e5387f564169b64d0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 12:15:34 +0000

    2d98726 refactor(core): remove unused helper for setting `LView` for reactive consumer (#52192)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-next.8+sha-030a68b'));
+    definitionMap.set('version', o.literal('17.0.0-next.8+sha-2d98726'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
