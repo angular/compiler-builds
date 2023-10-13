02767dcbc6b75e6c800a81b2c413aa4d3a4c1ef4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 09:25:40 +0000

    e90f5e5 docs: Phase 2 Sample Code Conversions to Standalone (#51918)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.0-next.8+sha-99e7629'));
+    definitionMap.set('version', o.literal('17.0.0-next.8+sha-e90f5e5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
