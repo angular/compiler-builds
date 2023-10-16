7cdc891cd0b6e5bf2739c8464090f46a39128d0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:37:25 +0000

    d6070afdc2 docs: fix typo (#52219)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.9+sha-37e7f2d'));
+    definitionMap.set('version', o.literal('16.2.9+sha-d6070af'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
