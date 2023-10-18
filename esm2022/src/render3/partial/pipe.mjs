a57232aa644006d902ac24ad14a80b5611caf179
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 16:56:38 +0000

    66bc77b docs: release notes for the v16.2.10 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-b0f9b33'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-66bc77b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
