b5438828aa8b497b3316542437e3dcfa5f552d5f
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:34:09 +0000

    a207421 refactor(core): check "skip hydration" condition only when hydration info is present (#52221)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-rc.0+sha-b4c1ffb'));
+    definitionMap.set('version', o.literal('17.0.0-rc.0+sha-a207421'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
