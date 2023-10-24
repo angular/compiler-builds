2a464e48e00b4b3b347c6ce313ea2f7e5b3bfcd2
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:57:23 +0000

    76152a5 fix(core): Ensure backwards-referenced transplanted views are refreshed (#51854)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-c993e9a'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-76152a5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
