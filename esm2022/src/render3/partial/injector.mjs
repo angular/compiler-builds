d97b890801c7c0daf4dffe9342236332a930d053
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:56:35 +0000

    1dd8558 fix(core): Ensure backwards-referenced transplanted views are refreshed (#51854)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-rc.0+sha-c9b1ddf'));
+    definitionMap.set('version', o.literal('17.0.0-rc.0+sha-1dd8558'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
