85e25c4308f39c4884acf1c63118e38a54108bb9
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 16:29:49 +0000

    36873596be docs: remove test.ts file in aio (#52327)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('16.2.10+sha-8239a8f'));
+    definitionMap.set('version', o.literal('16.2.10+sha-3687359'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
