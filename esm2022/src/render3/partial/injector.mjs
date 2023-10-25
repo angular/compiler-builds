8d3ea3a8931c01c471f5e7b8db9a8c56b34cef33
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 16:46:43 +0000

    9692aeb fix(migrations): Add support for nested structures inside a switch statement (#52358)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-76152a5'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-9692aeb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
