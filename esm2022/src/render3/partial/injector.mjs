4bcb5c6814058915fa5e45f6cff774aa580f90c3
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 23:59:07 +0000

    fc9ba39 refactor: fix a number of typos throughout the codebase (#52249)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-31c7bc1'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-fc9ba39'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
