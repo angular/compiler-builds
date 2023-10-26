a61fc7153b767e42a0a835ceb0c88851fd155c87
Dylan Hunn <dylhunn@gmail.com>
2023-10-26 16:33:59 +0000

    8599e18 build: update zone.js version to 0.14.1 (#52385)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-fc9ba39'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-8599e18'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
