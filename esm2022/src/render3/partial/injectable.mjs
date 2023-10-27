1881c0b6a1b5ecb41b3b4441351dba8503d74ee4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:11:57 +0000

    8e17dd7 refactor(localize): mark `TargetMessage` and `MessageId` as public. (#52418)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-rc.1+sha-57b9798'));
+    definitionMap.set('version', o.literal('17.0.0-rc.1+sha-8e17dd7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
