72cc5aca3e0b199f52b20bac9cd1e5459b5d3aa3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:37:18 +0000

    e06b73f docs: add warning for localhost proxy (#51948) PR Close #51948

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-bdd61c7'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-e06b73f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
