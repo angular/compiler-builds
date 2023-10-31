f43c20c24b6b69e2d821232dfabf8c993b2b1ea3
Andrew Scott <atscott01@gmail.com>
2023-10-31 19:03:36 +0000

    e74b578 docs: update home page for live stream event (#52456)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-rc.1+sha-4461cef'));
+    definitionMap.set('version', o.literal('17.0.0-rc.1+sha-e74b578'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
