11c5f403a6a20b11cc5bd3ed1d043e4626f604ba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:29:47 +0000

    a3028e2 refactor(core): Remove RootViewRef<T> because it is the same as ViewRef<T> (#52430)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-bf5bda4'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-a3028e2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
