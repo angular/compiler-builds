7f49a0d1dc16f01427dd44109dd0082b430b2ad0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:49:08 +0000

    64e4835f56 build: update peter-evans/create-or-update-comment digest to 23ff157 (#52279)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.11+sha-03f4050'));
+    definitionMap.set('version', o.literal('16.2.11+sha-64e4835'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
