a6465b705f4e4ecf327f1c7117f92318f3da9fa7
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:14:55 +0000

    499f09ac56 docs: add angular.dev banner (#52535) PR Close #52535

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('16.2.12+sha-ace0a68'));
+    definitionMap.set('version', o.literal('16.2.12+sha-499f09a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
