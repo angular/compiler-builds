3f43241091d2f30aa85688c7309d5f4ddd99d829
Andrew Scott <atscott01@gmail.com>
2023-11-06 17:11:27 +0000

    f2fbe86 fix(migrations): Update regex to better match ng-templates (#52529)

--- fesm2022/compiler.mjs
+++ fesm2022/compiler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v17.1.0-next.0+sha-63777d2
+ * @license Angular v17.1.0-next.0+sha-f2fbe86
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23583,7 +23583,9 @@ function ingestControlFlowInsertionPoint(unit, xref, node) {
         for (const attr of root.attributes) {
             ingestBinding(unit, xref, attr.name, literal(attr.value), 1 /* e.BindingType.Attribute */, null, SecurityContext.NONE, attr.sourceSpan, BindingFlags.TextValue);
         }
-        return root instanceof Element$1 ? root.name : root.tagName;
+        const tagName = root instanceof Element$1 ? root.name : root.tagName;
+        // Don't pass along `ng-template` tag name since it enables directive matching.
+        return tagName === 'ng-template' ? null : tagName;
     }
     return null;
 }
@@ -27423,7 +27425,9 @@ class TemplateDefinitionBuilder {
         // that we don't copy any bound attributes since they don't participate in content projection
         // and they can be used in directive matching (in the case of `Template.templateAttrs`).
         if (root !== null) {
-            tagName = root instanceof Element$1 ? root.name : root.tagName;
+            const name = root instanceof Element$1 ? root.name : root.tagName;
+            // Don't pass along `ng-template` tag name since it enables directive matching.
+            tagName = name === NG_TEMPLATE_TAG_NAME ? null : name;
             attrsExprs =
                 this.getAttributeExpressions(NG_TEMPLATE_TAG_NAME, root.attributes, root.inputs, []);
         }
@@ -30712,7 +30716,7 @@ function publishFacade(global) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('17.1.0-next.0+sha-63777d2');
+const VERSION = new Version('17.1.0-next.0+sha-f2fbe86');
 
 class CompilerConfig {
     constructor({ defaultEncapsulation = ViewEncapsulation.Emulated, preserveWhitespaces, strictInjectionParameters } = {}) {
@@ -32242,7 +32246,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION$6 = '12.0.0';
 function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$6));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
@@ -32350,7 +32354,7 @@ function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION$5 : '14.0.0';
     definitionMap.set('minVersion', literal(minVersion));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
@@ -32627,7 +32631,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION$4 = '12.0.0';
 function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$4));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
@@ -32662,7 +32666,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$3));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
@@ -32713,7 +32717,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$2));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
@@ -32746,7 +32750,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$1));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
@@ -32797,7 +32801,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', literal('17.1.0-next.0+sha-63777d2'));
+    definitionMap.set('version', literal('17.1.0-next.0+sha-f2fbe86'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
