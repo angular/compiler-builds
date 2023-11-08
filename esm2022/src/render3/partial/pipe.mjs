2e6a987b61f65c823f70d2a77b19a373fe56ae3f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 12:28:11 +0000

    fecd141 release: cut the v17.0.0 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0-rc.3+sha-6b83328'));
+    definitionMap.set('version', o.literal('17.0.0+sha-fecd141'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
