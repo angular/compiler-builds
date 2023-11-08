3d5b83470f747d41bb8590151f16773054e97582
Andrew Scott <atscott01@gmail.com>
2023-11-08 18:30:45 +0000

    70d30c2 fix(migrations): Add support for ng-templates with i18n attributes (#52597)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.0+sha-227de98'));
+    definitionMap.set('version', o.literal('17.0.0+sha-70d30c2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
