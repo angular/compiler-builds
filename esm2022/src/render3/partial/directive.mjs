818a691971f4affc20603af5dfece4ee298779c2
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 20:03:37 +0000

    b16fc26 fix(core): limit rate of markers invocations (#52742)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.1+sha-cfb42ee'));
+    definitionMap.set('version', o.literal('17.0.1+sha-b16fc26'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
