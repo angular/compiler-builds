3fb0c26948e892a5f75e63d8d74a37c4c22e232c
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:05:18 +0000

    96d554d docs: fix incorrect tag reference (#52586)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.1+sha-4626691'));
+    definitionMap.set('version', o.literal('17.0.1+sha-96d554d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
