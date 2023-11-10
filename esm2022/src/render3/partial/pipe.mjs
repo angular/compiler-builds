240b79a2fbe993ffe187fdad41cda6d1133d95a7
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-10 18:41:24 +0000

    3cf18bb fix(devtools): check for all new DI debug APIs before trying to determine resolution path providers (#52791)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-70fe5e6'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-3cf18bb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
