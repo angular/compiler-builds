661ffd2146896e188380cceb91b9d4cbf688ff1f
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:01:24 +0000

    f51ce68 docs: update readme links to target angular.dev documentation (#52772)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-da97bbc'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-f51ce68'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
