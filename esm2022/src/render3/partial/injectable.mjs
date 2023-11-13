833e9187c8949d19c6fdef15e6044c2490684463
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 15:27:53 +0000

    e1f84a3 fix(migrations): passed in paths will be respected in nx workspaces (#52796)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.2+sha-54a7b33'));
+    definitionMap.set('version', o.literal('17.0.2+sha-e1f84a3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
