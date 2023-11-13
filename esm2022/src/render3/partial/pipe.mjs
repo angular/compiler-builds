4ca8371a28a055c7c1f85e76d8406581fb52e4e6
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 16:27:04 +0000

    7c066a4 fix(http): Use the response `content-type` to set the blob `type`. (#52840)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.2+sha-e46162d'));
+    definitionMap.set('version', o.literal('17.0.2+sha-7c066a4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
