230194c73101771a5bd708c5b26bbb4a001cbcf1
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-11-15 20:28:26 +0000

    f5307bf release: cut the v17.0.3 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.2+sha-151a544'));
+    definitionMap.set('version', o.literal('17.0.3+sha-f5307bf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
