2407cc1288431f2a9b1b6340329f884765b7c4f8
Andrew Kushnir <akushnir@google.com>
2023-11-17 20:23:32 +0000

    060ea83 docs: fix broken link for view encapsulation (#52995)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-f63c0f6'));
+    definitionMap.set('version', o.literal('17.1.0-next.0+sha-060ea83'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
