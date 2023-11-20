2bd34fa6c9315330b3b34752ef43a446d162e5ab
Andrew Kushnir <akushnir@google.com>
2023-11-20 21:21:51 +0000

    c7c7ea9 fix(core): inherit host directives (#52992)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.1+sha-29e0834'));
+    definitionMap.set('version', o.literal('17.1.0-next.1+sha-c7c7ea9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
