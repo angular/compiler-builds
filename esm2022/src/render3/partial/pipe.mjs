06372c45f2df2a726331dbd4dce04f7ec7b9c316
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-11-20 20:01:10 +0000

    33959f4 release: cut the v17.1.0-next.1 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.0+sha-2ecda9f'));
+    definitionMap.set('version', o.literal('17.1.0-next.1+sha-33959f4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
