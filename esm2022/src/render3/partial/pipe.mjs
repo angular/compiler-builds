412c881600291ca08121faebc630900493b9efc5
Andrew Kushnir <akushnir@google.com>
2023-11-20 17:06:47 +0000

    291deac fix(compiler): generate i18n instructions for blocks (#52958)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.3+sha-6c68f4c'));
+    definitionMap.set('version', o.literal('17.0.3+sha-291deac'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
