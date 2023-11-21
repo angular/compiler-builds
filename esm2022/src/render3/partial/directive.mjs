5fc53145f64cab16be7122930376d808fbde3c30
Andrew Kushnir <akushnir@google.com>
2023-11-21 18:36:14 +0000

    cb2db54 build: update dependency globby to v14 (#52804)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.1+sha-c7c7ea9'));
+    definitionMap.set('version', o.literal('17.1.0-next.1+sha-cb2db54'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
