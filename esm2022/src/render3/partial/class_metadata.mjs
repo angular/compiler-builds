9e5af5b4d805a10b509caa6b44e2c59f62dadff8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 09:53:13 +0000

    6d61f2d build: update dependency karma-jasmine-html-reporter to v2 (#52544)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.4+sha-935fa8f'));
+    definitionMap.set('version', o.literal('17.0.4+sha-6d61f2d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
