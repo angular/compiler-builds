321187acec8c0ad4b3070e7f2d671a362dbdeb96
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:08:14 +0000

    dbd6f38 fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.4+sha-92a28a7'));
+    definitionMap.set('version', o.literal('17.0.4+sha-dbd6f38'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
