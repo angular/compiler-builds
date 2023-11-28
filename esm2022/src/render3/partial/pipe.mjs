cf627d99c4d88b610f1738ddc9ccea80121722d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 18:33:28 +0000

    df01a5c test: refactor platform-server integration tests to use application builder (#53219)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.4+sha-ba6d7fe'));
+    definitionMap.set('version', o.literal('17.0.4+sha-df01a5c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
