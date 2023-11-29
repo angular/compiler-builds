eaa54336e7fe153923aef12ad65013f2274bc2e8
Joey Perrott <josephperrott@gmail.com>
2023-11-29 20:45:06 +0000

    6462ddd build: set up initial adev guide (#53208)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.1+sha-5d15804'));
+    definitionMap.set('version', o.literal('17.1.0-next.2+sha-6462ddd'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
