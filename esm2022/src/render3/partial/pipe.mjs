f3949f83f40bf4909c7515ed5a912b8e67610ab5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 16:39:00 +0000

    6ae4088 fix(migrations): properly handle ngIfThen cases in CF migration (#53256)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.4+sha-5b9f896'));
+    definitionMap.set('version', o.literal('17.0.4+sha-6ae4088'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
