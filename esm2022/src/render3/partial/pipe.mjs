7673878b59da815e3297a0b1cec4c051a004db72
Dylan Hunn <dylhunn@gmail.com>
2023-11-30 17:50:28 +0000

    13ade13 fix(router): Ensure canMatch guards run on wildcard routes (#53239)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.5+sha-81e9489'));
+    definitionMap.set('version', o.literal('17.0.5+sha-13ade13'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
