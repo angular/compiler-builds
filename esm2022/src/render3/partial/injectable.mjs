68d093d563e6085222b1c95854911d11667de25f
Dylan Hunn <dylhunn@gmail.com>
2023-12-05 05:55:08 +0000

    2d2eaec docs: fixed wrong template string placeholder (#53286) PR Close #53286

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.5+sha-dc27bea'));
+    definitionMap.set('version', o.literal('17.0.5+sha-2d2eaec'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
