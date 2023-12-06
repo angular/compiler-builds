e2dac7596ee480796bfd8f7b849ab9fbfdc9ff51
Dylan Hunn <github@dylanhunn.com>
2023-12-06 18:27:08 +0000

    5f73608 release: cut the v17.1.0-next.3 release

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.2+sha-423692a'));
+    definitionMap.set('version', o.literal('17.1.0-next.3+sha-5f73608'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
