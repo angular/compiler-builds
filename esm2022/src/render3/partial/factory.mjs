c4b1af473f15b36a820cf8ddac87916245ed0cdc
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 17:50:18 +0000

    a1e3485 refactor(migrations): Update comments to be more accurate (#53396)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.2+sha-580af8e'));
+    definitionMap.set('version', o.literal('17.1.0-next.2+sha-a1e3485'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
