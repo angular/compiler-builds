ba4139b5c6b1f52cbeb6de31ba61398d014b2a60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-07 20:09:39 +0000

    2565121 fix(core): Avoid refreshing a host view twice when using transplanted views (#53021)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.3+sha-1f5c8bf'));
+    definitionMap.set('version', o.literal('17.1.0-next.3+sha-2565121'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
