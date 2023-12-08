a3430994b70d89ee0a5d24abea5c089c8e268afe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 22:41:42 +0000

    f88b77b build: migrate adev generation to @angular/docs package (#53458)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.6+sha-a398c55'));
+    definitionMap.set('version', o.literal('17.0.6+sha-f88b77b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
