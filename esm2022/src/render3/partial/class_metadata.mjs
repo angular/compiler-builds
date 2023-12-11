68fead375e47b27831d805c2d152d85e4b608c31
Joey Perrott <josephperrott@gmail.com>
2023-12-11 19:27:10 +0000

    1ea8a14 refactor(docs-infra): move versions information into the application (#53501)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.6+sha-24d8aa6'));
+    definitionMap.set('version', o.literal('17.0.6+sha-1ea8a14'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
