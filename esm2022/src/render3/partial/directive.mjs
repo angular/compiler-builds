64defd391f7347eee64c002803bb3dc3f6f3a0b3
Definitely not me <something@something.com>
2023-12-11 21:40:12 +0000

    cc75fab build: drop rxjs_umd_modules as it is unused (#53506)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.3+sha-0ea8864'));
+    definitionMap.set('version', o.literal('17.1.0-next.3+sha-cc75fab'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
