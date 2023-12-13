c109bf502cd1e6dbc6a27202f32c49cc303ff59b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-13 17:34:20 +0000

    fdb9cb7 fix(platform-browser): Get correct base path when using "." as base href when serving from the file:// protocol. (#53547)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.3+sha-d9348be'));
+    definitionMap.set('version', o.literal('17.1.0-next.3+sha-fdb9cb7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
