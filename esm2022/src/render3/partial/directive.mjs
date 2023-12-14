837425718ecd22f248fe93b6ffc3f32751c53cb9
Andrew Scott <atscott01@gmail.com>
2023-12-14 17:23:11 +0000

    606de51 refactor(core): newly created and any dirty views should get refreshed during CD (#53022)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.4+sha-be7beca'));
+    definitionMap.set('version', o.literal('17.1.0-next.4+sha-606de51'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
