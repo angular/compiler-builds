74bce8754955cc037eba8b4b3bba26cf3cf424f2
Andrew Scott <atscott01@gmail.com>
2023-12-14 17:22:58 +0000

    be58dba refactor(core): newly created and any dirty views should get refreshed during CD (#53022)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.7+sha-203a3d5'));
+    definitionMap.set('version', o.literal('17.0.7+sha-be58dba'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
