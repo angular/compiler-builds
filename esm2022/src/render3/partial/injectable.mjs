f8ac047b138b147060f0b2707b61312b6f0a1a5f
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-15 20:03:06 +0000

    658c73d refactor(migrations): update cf migration to handle module imports better (#53575)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.7+sha-e73205f'));
+    definitionMap.set('version', o.literal('17.0.7+sha-658c73d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
