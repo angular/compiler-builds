8d6ac09387ceda3c6b414264215dd25c1cffd04f
Andrew Scott <atscott01@gmail.com>
2023-12-19 21:27:23 +0000

    70de5c5 refactor(core): Use single source of truth for `ApplicationRef.isStable` (#53642)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.7+sha-ed936ba'));
+    definitionMap.set('version', o.literal('17.0.7+sha-70de5c5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
