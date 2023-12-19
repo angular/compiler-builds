195a8a6c397d931e23905f187579a55c5ac6ac51
Andrew Scott <atscott01@gmail.com>
2023-12-19 18:22:10 +0000

    22b95de fix(migrations): cf migration - fix bug in attribute formatting (#53636)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.4+sha-c16e9a7'));
+    definitionMap.set('version', o.literal('17.1.0-next.4+sha-22b95de'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
