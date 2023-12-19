ca6f2aca8d83ccc48ebacc10c58ec56a8f3dfff5
Andrew Scott <atscott01@gmail.com>
2023-12-19 19:06:50 +0000

    eb7c29c fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.7+sha-817dc1b'));
+    definitionMap.set('version', o.literal('17.0.7+sha-eb7c29c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
