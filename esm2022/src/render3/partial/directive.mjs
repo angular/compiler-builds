8bfdbbe8e175eb33325e818f689d56a52136f6e8
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:26:23 +0000

    502f300 fix(router): Should not freeze original object used for route data (#53635)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.4+sha-00cbdc9'));
+    definitionMap.set('version', o.literal('17.1.0-next.4+sha-502f300'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
