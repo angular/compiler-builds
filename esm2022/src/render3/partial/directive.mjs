c30a00130e42eb6eb3f7709e0efb8f6437c673c7
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:38:42 +0000

    d315e2c release: cut the v17.1.0-next.5 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.4+sha-9e2e5d6'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-d315e2c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
