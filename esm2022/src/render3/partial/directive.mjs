6fad01be8363f6a48883b3c06f436f51985994d1
Andrew Scott <atscott01@gmail.com>
2023-12-21 14:33:46 +0000

    9e2e5d6 docs: release notes for the v17.0.8 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.4+sha-c4de4e1'));
+    definitionMap.set('version', o.literal('17.1.0-next.4+sha-9e2e5d6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
