1dc9197737c05ec65c2079b0cf844db25cfee92b
Andrew Scott <atscott@google.com>
2024-01-03 21:06:44 +0000

    5fb199b refactor(core): Remove `LContainerFlags.HasChildViewsToRefresh` (#53715)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -40,7 +40,7 @@ export function createDirectiveDefinitionMap(meta) {
     // in 16.1 is actually used.
     const minVersion = hasTransformFunctions ? MINIMUM_PARTIAL_LINKER_VERSION : '14.0.0';
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.0.8+sha-7bf11ea'));
+    definitionMap.set('version', o.literal('17.0.8+sha-5fb199b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.8+sha-7bf11ea'));
+    definitionMap.set('version', o.literal('17.0.8+sha-5fb199b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
