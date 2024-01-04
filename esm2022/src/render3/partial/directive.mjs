ad262c4fa86f3baf290602083657b25a4d1c19d8
Andrew Scott <atscott@google.com>
2024-01-04 20:55:40 +0000

    f4bd5a3 fix(platform-server): Do not delete global Event (#53659)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-36318db'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-f4bd5a3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
