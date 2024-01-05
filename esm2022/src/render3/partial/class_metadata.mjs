0be438c22a3763838e150fde455021626c64744b
Andrew Scott <atscott@google.com>
2024-01-05 19:33:42 +0000

    a5a9b40 feat(router): Add transient info to RouterLink input (#53784)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-5e84d9c'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-a5a9b40'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
