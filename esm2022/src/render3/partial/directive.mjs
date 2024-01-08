e1ab6abebb9ffc40a3b639ac338e3a4167d63157
Andrew Scott <atscott@google.com>
2024-01-08 19:39:23 +0000

    f7c02e1 fix(docs-infra): include the homepage playground content (#53832)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-a5a9b40'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-f7c02e1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
