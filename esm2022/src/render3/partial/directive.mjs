6a2487d7ad539fed9990e3daa9a7d28e3b7ac5c5
Andrew Scott <atscott@google.com>
2024-01-09 22:35:04 +0000

    5996d3f Revert "refactor(router): Update integration tests to cover navigation and history API (#53799)" (#53860)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-2dedc4a'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-5996d3f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
