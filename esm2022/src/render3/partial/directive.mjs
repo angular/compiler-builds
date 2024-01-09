e7b7d1cef32bd593acbfb461db3c1a3aacd4e9cc
Andrew Scott <atscott@google.com>
2024-01-09 22:51:56 +0000

    c2dd703 refactor(core): Remove internal-only testability features (#53767)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-5996d3f'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-c2dd703'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
