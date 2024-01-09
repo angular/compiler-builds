e27d4ad11812925762d12c2b809a0683b78c0fdf
Andrew Scott <atscott@google.com>
2024-01-09 20:33:05 +0000

    92fd6cc fix(compiler): generate less code for advance instructions (#53845)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.8+sha-c4b266c'));
+    definitionMap.set('version', o.literal('17.0.8+sha-92fd6cc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
