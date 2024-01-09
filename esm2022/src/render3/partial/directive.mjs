1dbee1eb7b5b75c1cc2a5387243146558fd6dd3e
Andrew Scott <atscott@google.com>
2024-01-09 17:14:53 +0000

    a468a5e release: cut the zone.js-0.14.3 release (#53630)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-next.5+sha-e2b5988'));
+    definitionMap.set('version', o.literal('17.1.0-next.5+sha-a468a5e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
