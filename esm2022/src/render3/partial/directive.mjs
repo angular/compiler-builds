92a0ea57df821103d917fe6601eacfa35c1a5604
Andrew Scott <atscott@google.com>
2024-01-11 20:10:24 +0000

    1f8c53c fix(core): TestBed should still use the microtask queue to schedule effects (#53843)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-79fbc4e'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-1f8c53c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
