e62e6dfdee6c12ecf9dece7b4fc2a75ee1a8f562
Dylan Hunn <dylhunn@gmail.com>
2024-01-12 18:32:28 +0000

    2d76a0d docs: remove repeated sentence (#53853)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.9+sha-198e64a'));
+    definitionMap.set('version', o.literal('17.0.9+sha-2d76a0d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
