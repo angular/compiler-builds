0484bd61b5f270d84e50fcacdea097a6dac66def
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:41:51 +0000

    18fa6d7 refactor(compiler-cli): interpolatedSignalNotInvoked diagnostic for input signals (#53883)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-9c00506'));
+    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-18fa6d7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
