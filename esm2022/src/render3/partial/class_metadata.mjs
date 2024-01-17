bcc6ad0035c5e8247353a6e3c79543fcd12ee5c9
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:18:56 +0000

    23f19f8 docs: add `platform-browser/animations/async` entry to AIO (#53914)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.0.9+sha-eb6389e'));
+    definitionMap.set('version', o.literal('17.0.9+sha-23f19f8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
