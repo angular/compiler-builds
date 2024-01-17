e4a564f792ed61da15e12f534c8547ecd95c1032
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:23:45 +0000

    9c00506 ci: use ng-dev-previews project for previews (#53958)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-79e8539'));
+    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-9c00506'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
