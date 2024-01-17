3c098b1dc1502c0aabf53113bccd8a6fcb093f02
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:46:38 +0000

    6616019 refactor: migrate modules to prettier formatting (#53954)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-4d4858f'));
+    definitionMap.set('version', o.literal('17.1.0-rc.0+sha-6616019'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
