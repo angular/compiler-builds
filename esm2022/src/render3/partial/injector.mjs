665292999dc0f0bff81c7ca302e127065bd37691
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:27:26 +0000

    8737544 build(devtools): prevent underscores from being added at build time (#53921)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a566c5c'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-8737544'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
