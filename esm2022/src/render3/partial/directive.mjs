56f132b12bdad3547de3268cb96a8451a3575e04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 17:12:39 +0000

    a057b61 ci: provide proper location for artifacts to allow for firebase to find the artifacts for deployment (#53960)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-c67647b'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a057b61'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
