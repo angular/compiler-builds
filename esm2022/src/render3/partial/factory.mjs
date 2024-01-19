3109849f9899533cc8ce300a5cbd4d2a9c27f93b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:16:38 +0000

    ee68df9 build: update all non-major dependencies (#53979)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a057b61'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-ee68df9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
