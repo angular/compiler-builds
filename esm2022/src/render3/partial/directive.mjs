2530bd1a7457041610bc7d61b60cb805179ccdf4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:21:33 +0000

    424a26b docs: updates defer guide to reflect updated default defer block test behavior (#53975)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0+sha-18bd1a5'));
+    definitionMap.set('version', o.literal('17.1.0+sha-424a26b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
