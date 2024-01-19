747d68e86d51fd178679ec5509a7b557516ea9b5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:26:49 +0000

    62ad2f0 refactor: migrate scripts to prettier formatting (#53976)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0+sha-424a26b'));
+    definitionMap.set('version', o.literal('17.1.0+sha-62ad2f0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
