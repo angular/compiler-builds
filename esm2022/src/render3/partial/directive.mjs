78e41be20e76c50e22e834981911842f89527de1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 18:14:21 +0000

    656549d refactor(devtools): migrate devtools to prettier formatting (#53945)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0+sha-c0f0baa'));
+    definitionMap.set('version', o.literal('17.1.0+sha-656549d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
