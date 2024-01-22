c5feb80a8c168154a57351cd6cd75732b9a2cb38
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-22 13:51:19 +0000

    3e13840 feat(compiler-cli): support host directives for local compilation mode (#53877)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-b774e22'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-3e13840'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
