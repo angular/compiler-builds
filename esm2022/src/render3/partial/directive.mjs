08c92da0ca323295b7bf7ce6bd6251a442489186
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 14:15:26 +0000

    d99bc8a refactor(compiler): ICUs should roll up to the root i18n block for application (#54026)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-f81a436'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-d99bc8a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
