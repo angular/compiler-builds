be1b408e20aa5e3704332585fae7c4c5df08e922
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 20:29:21 +0000

    9e10af4 docs: typo Chat to chart fix (#53892)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a2a0b33'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-9e10af4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
