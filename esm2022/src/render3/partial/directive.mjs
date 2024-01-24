bb5d42092df3f157c761eedcdfc6cc98cd912095
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:01:56 +0000

    8ad1ee7 build: ensure zone.js typing test is hermetic (#54048)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.0+sha-f222bee'));
+    definitionMap.set('version', o.literal('17.1.0+sha-8ad1ee7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
