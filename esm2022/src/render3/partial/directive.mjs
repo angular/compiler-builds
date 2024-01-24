7a999e8ce72524dc4f0b37ccc717633c448637e2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:37:20 +0000

    09f9423 docs: fix typo in Custom directives section (#54044)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-25f91e3'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-09f9423'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
