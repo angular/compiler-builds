3b8deeb19a2bdedd354056e2a039f2484aa24e54
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:23:20 +0000

    a4923f1 ci: add ben to reviewers for adev files (#54078)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-e603450'));
+    definitionMap.set('version', o.literal('17.1.1+sha-a4923f1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
