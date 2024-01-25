48d295bc56f8a1612ed4e15b9caebc5e61b50c22
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 18:08:19 +0000

    e603450 docs: fix & update MDN Shadow API link examples (#54040)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-98d545f'));
+    definitionMap.set('version', o.literal('17.1.1+sha-e603450'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
