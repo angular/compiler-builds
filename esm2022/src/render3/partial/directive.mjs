7d2d27f0f7ce139119e64da24690d5307ba617b9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:23:57 +0000

    d5c0743 ci: add ben to reviewers for adev files (#54078)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-58d4abc'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-d5c0743'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
