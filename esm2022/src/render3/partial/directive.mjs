2e38efe4c766469ac71dd212f6f53a6c56e64fd4
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:44:28 +0000

    d17ba9b ci: update adev approvers list (#54081)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-a4923f1'));
+    definitionMap.set('version', o.literal('17.1.1+sha-d17ba9b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
