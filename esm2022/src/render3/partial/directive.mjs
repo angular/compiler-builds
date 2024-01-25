1acc21e533990b105222fd147036efb5fa05ea4c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 21:40:26 +0000

    ac211d4 docs: bad redirection in core Injector page for providers and injection token links (#52775) PR Close #52775

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-7ac94dd'));
+    definitionMap.set('version', o.literal('17.1.1+sha-ac211d4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
