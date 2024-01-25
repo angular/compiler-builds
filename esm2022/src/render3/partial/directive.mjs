87340a89f2cc3e0422d091822897ff7278d8bfb5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:53:25 +0000

    ccddacf fix: cta clickability issue in adev homepage. (#52905) PR Close #52905

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-b216e76'));
+    definitionMap.set('version', o.literal('17.1.1+sha-ccddacf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
