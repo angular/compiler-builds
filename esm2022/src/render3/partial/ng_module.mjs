0e23a9a523d580c4cf70a59bbaf2396deac9fb80
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:46:29 +0000

    c9c22fa docs: Fix open link in new tab (#53647)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-3e2cd83'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-c9c22fa'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
