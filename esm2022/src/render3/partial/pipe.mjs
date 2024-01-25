941ea2fad123fb68c10a8606fe335ac1ab2393b4
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:53:31 +0000

    ecd0daf docs(docs-infra): move comma in Angular Language Service section (#53759)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-d8d6478'));
+    definitionMap.set('version', o.literal('17.1.1+sha-ecd0daf'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
