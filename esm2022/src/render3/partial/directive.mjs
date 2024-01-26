435cdadf210cf5f6fabcf613df51d511f1149c7b
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:02:54 +0000

    89ab1c1 refactor(common): Use Nullish coalescing assignment when possible. (#53931)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-271755d'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-89ab1c1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
