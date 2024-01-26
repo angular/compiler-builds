a7e82a3f44293e0ab08813c03422a7f571b04824
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:18:53 +0000

    b0632bd docs: Update update-to-version-16.md (#51857)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-16fbe85'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-b0632bd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
