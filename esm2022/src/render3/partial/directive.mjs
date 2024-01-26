52969493abb7ededd2d4cf86b5e3e5bc1f291adb
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 16:08:24 +0000

    f240c70 docs(docs-infra): cleanup repeated scss from typography (#51579)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-0172477'));
+    definitionMap.set('version', o.literal('17.1.1+sha-f240c70'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
