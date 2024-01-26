d1a3aea9eca3dd39f3fcb2b92bca2c2cf68da2fd
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:42:56 +0000

    74567f1 docs: update i18n-common-merge.md (#52400)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-64399b2'));
+    definitionMap.set('version', o.literal('17.1.1+sha-74567f1'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
