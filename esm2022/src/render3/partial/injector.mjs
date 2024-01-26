0e47a65f7457c287c9c3003ee73c863b71d91353
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:07:29 +0000

    16fbe85 docs(router): fix variable reference in text (#54099) PR Close #54099

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-4105ab5'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-16fbe85'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
