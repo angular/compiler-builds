caec822034e919cd9a2ae886113054294a2f43e9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 19:35:19 +0000

    d07ce61 docs: Update component-overview.md (#52282)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-8650f0d'));
+    definitionMap.set('version', o.literal('17.1.1+sha-d07ce61'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
