ff80bc3edf2f8e135f9fd2f927680e3c6e988792
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:05:08 +0000

    f5c520b feat(common): add placeholder to NgOptimizedImage (#53783)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-45288f7'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-f5c520b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
