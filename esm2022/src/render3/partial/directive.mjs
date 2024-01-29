f7f9e38ffa22b605833ec30528242cf065af00fe
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:46:10 +0000

    9571287 docs: correct grammar error on start page (#53993)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-f5c520b'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-9571287'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
