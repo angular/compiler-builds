30efb956763ad5bcfa1f031170376e45756bc9cf
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 20:14:00 +0000

    7001b5d refactor: migrate docs, examples, private, service worker and upgrade to prettier formatting (#54163)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-8d85de7'));
+    definitionMap.set('version', o.literal('17.1.1+sha-7001b5d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
