ce9b95613e38b04791be0c63032d7ebd61ec3e9f
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 15:11:22 +0000

    f3567bb refactor(common): simplify i18n plural and select pipe metadata (#53939)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-7b4d275'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-f3567bb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
