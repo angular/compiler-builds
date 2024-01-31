5fe201a8acf93918a0589812f3be9242ddff0107
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:18:19 +0000

    2e2e8aa test(docs-infra): unit tests in the reference pages (#53529)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-34feb4f'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-2e2e8aa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
