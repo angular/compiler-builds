54fc46394d57407f469cd92c9a5743a2e3855afb
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:09:10 +0000

    34feb4f docs(docs-infra): @input getter setter syntax change (#51023)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-b02cf14'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-34feb4f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
