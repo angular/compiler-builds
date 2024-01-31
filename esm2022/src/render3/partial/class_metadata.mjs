08d67251cdb1a4d4e13df4d71d1fcdb311b8a142
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 19:49:58 +0000

    4883ae6 build: update actions/cache action to v4 (#54179)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.2+sha-57cb448'));
+    definitionMap.set('version', o.literal('17.1.2+sha-4883ae6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
