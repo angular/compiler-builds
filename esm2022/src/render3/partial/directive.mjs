c0a7fd7ec19c2ce66f73e00085cab9293ca24f5b
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:52:43 +0000

    f5ede2a build: update slackapi/slack-github-action digest to 2a8087d (#52000)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a297012'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-f5ede2a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
