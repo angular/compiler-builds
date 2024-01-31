cf1ba346efa42a3134bb364e6f558075c209885a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:01:51 +0000

    b69bf37 fix(zone.js): patch `Response` methods returned by `fetch` (#50653)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-752ebd6'));
+    definitionMap.set('version', o.literal('17.1.1+sha-b69bf37'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
