0f7aa9b4f118424a110ffc409ff1539652ba21f7
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:27:47 +0000

    3b218a7 docs(core): fix example sections (#54121) PR Close #54121

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-4367c44'));
+    definitionMap.set('version', o.literal('17.1.1+sha-3b218a7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
