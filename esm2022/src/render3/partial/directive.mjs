dec8208a8304507abc581ac808b9a09bf2e509de
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 17:41:32 +0000

    0bd668c refactor(router): RouterLink read urlTree only once (#53817)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-948fb5f'));
+    definitionMap.set('version', o.literal('17.1.1+sha-0bd668c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
