18e47b48de38bd86c3abf7d473a6c20e0f715864
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-01-31 19:08:33 +0000

    57cb448 release: cut the v17.1.2 release

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.1+sha-0bd668c'));
+    definitionMap.set('version', o.literal('17.1.2+sha-57cb448'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
