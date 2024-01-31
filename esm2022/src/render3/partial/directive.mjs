8e425312be68d27cbecb9efec10e7259f950f05b
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:12:58 +0000

    4367c44 docs: Update docs about equality. (#54158)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.1+sha-b69bf37'));
+    definitionMap.set('version', o.literal('17.1.1+sha-4367c44'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
