d72024426de40c0900ac6ca3eda4bb16b20ea2ac
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:35:25 +0000

    a297012 docs(docs-infra): add tab titles to home, playground and tutorial (#53026)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.0+sha-2e2e8aa'));
+    definitionMap.set('version', o.literal('17.2.0-next.0+sha-a297012'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
