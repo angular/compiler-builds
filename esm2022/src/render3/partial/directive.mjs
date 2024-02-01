2fe2e42b4ce6c5de6d94b7b6f0d4149eb198baf6
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:12:11 +0000

    0e150c4 docs: correct TypeScript version range for 17.0.x (#53213)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-de777af'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-0e150c4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
