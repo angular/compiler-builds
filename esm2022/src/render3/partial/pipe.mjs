cb34649a6caac9ffc49f2216cfffa8b3fa4ac0d0
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 16:09:04 +0000

    9e3d54c build: update actions/checkout action to v4 (#54183)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.2+sha-924a276'));
+    definitionMap.set('version', o.literal('17.1.2+sha-9e3d54c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
