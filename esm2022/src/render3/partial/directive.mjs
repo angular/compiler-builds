c7ae2a630448d717fe877ce3c86ab45c3eeae28d
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 21:56:14 +0000

    afc72b0 ci: fix size tracking for aio (#54207)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.2+sha-0fb1142'));
+    definitionMap.set('version', o.literal('17.1.2+sha-afc72b0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
