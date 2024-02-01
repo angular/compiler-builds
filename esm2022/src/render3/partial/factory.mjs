487b269f83cc76998b6bab6acb9c97848a04ef65
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 18:59:41 +0000

    bc81440 docs(docs-infra): Remove the edit button from AIO (#54204)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-5d98a02'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-bc81440'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
