a9386fae0bf3534c2b901751abb6b784f22a7f4e
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 23:04:52 +0000

    cd88f14 docs:  remove outdated info about ng serve (#53890)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.2+sha-37a6da3'));
+    definitionMap.set('version', o.literal('17.1.2+sha-cd88f14'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
