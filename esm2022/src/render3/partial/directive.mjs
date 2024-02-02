38a161a86f676bff62311d5644295b7e100a2f19
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 14:55:45 +0000

    74aa8a3 test(core): ExpressionChanged... error does not happen with signals (#54206)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-bdf6927'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-74aa8a3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
