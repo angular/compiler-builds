a3acb54f801f263df854bcb802705ba42c225079
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 20:57:45 +0000

    47c712e refactor(compiler): Show an error when a property binding duplicates an i18n attribute (#54063)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.2+sha-54deaae'));
+    definitionMap.set('version', o.literal('17.1.2+sha-47c712e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
