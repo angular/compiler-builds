8d11d69cdca4fdb28772c329d2348ca777ba5fb3
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-05 16:45:38 +0000

    04a1d50 docs: add Jason Warner to GDE resources (#54237)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.2+sha-fc4d8bb'));
+    definitionMap.set('version', o.literal('17.1.2+sha-04a1d50'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
