afeff9d7916a9ebe782afd90389e46e452831ab4
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:51:00 +0000

    f303838 docs: replace event word with property for binding properties link (#54281)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.1.2+sha-bc4a6a9'));
+    definitionMap.set('version', o.literal('17.1.2+sha-f303838'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
