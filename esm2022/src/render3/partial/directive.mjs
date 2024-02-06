092a5088c880d5fe47acf2e34291b917c3528e7b
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 16:10:26 +0000

    19fae76 fix(zone.js): patch `fs.realpath.native` as macrotask (#54208)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-4851453'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-19fae76'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
