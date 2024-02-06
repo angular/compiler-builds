663e1946a718c824da853243e52a2249854e2ff6
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 18:16:40 +0000

    eeafdb6 build: update dependency firebase-tools to v13 (#54234)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-0763fba'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-eeafdb6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
