6cff145d5ae45b50190bb623558f80d1c9933624
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 19:40:29 +0000

    2de7679 build: update dependency husky to v9 (#54289)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.2+sha-06391d1'));
+    definitionMap.set('version', o.literal('17.1.2+sha-2de7679'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
