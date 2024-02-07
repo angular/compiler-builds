d81a1000891d99a6cdc19bd0b988469b18740317
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 19:00:09 +0000

    38c0084 test: verify signal-based queries in CLI integration test (#54314)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-a08863e'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-38c0084'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
