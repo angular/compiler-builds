b4a080afb6f074c70d5339067322bdf2c092c312
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 22:26:33 +0000

    ca239e8 refactor(core): share refresh logic for cd and signal queries (#54322)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-38c0084'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-ca239e8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
