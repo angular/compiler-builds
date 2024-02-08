91fb94cf964f1bcf3a206e95edbadcdc29d8c3c4
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:16:51 +0000

    ca39926 refactor: migrate router to prettier formatting (#54319)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.3+sha-dd8490f'));
+    definitionMap.set('version', o.literal('17.1.3+sha-ca39926'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
