cdd9d548d070eaa5fc6cc744ba96ec45c60b403b
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-02-08 18:43:22 +0000

    ed41bfc release: cut the v17.2.0-rc.0 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-c47bf63'));
+    definitionMap.set('version', o.literal('17.2.0-rc.0+sha-ed41bfc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
