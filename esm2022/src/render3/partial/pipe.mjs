5708661b2c0c3d5c03a94c180c318374e022d661
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:33:51 +0000

    38b01a3 fix(compiler-cli): interpolatedSignalNotInvoked diagnostic for model signals (#54338)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-next.1+sha-03c3b3e'));
+    definitionMap.set('version', o.literal('17.2.0-next.1+sha-38b01a3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
