c1a2995d0d079029dcfb760159f7234056943621
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 19:23:52 +0000

    9e2c140 refactor(devtools): show tooltip for hydration icon on directive forest (#54326)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-rc.0+sha-ed41bfc'));
+    definitionMap.set('version', o.literal('17.2.0-rc.0+sha-9e2c140'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
