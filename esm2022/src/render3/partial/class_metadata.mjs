2ae85be055a620eb67e09351461abd0e4d005b72
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-09 15:04:23 +0000

    2435291 docs(docs-infra): change deprecated modules' headers' style in the hover state (#54337)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.1.3+sha-375e606'));
+    definitionMap.set('version', o.literal('17.1.3+sha-2435291'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
