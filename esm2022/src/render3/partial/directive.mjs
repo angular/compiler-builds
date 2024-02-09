69224636f254b1e76c051c51c26f0c03c39208ae
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-02-09 10:58:18 +0000

    7aa92fa release: cut the v17.2.0-rc.1 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-rc.0+sha-4b96f37'));
+    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-7aa92fa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
