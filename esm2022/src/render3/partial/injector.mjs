df6c9f3ca2dfa9307b3f88611358a611005cc59b
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-09 15:04:58 +0000

    62b87b4 fix(core): do not crash for signal query that does not have any matches (#54353)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-7aa92fa'));
+    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-62b87b4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
