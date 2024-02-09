e1fa7cdb4f71c9b4ae810c458b2fd303c3637c7f
Andrew Kushnir <akushnir@google.com>
2024-02-09 19:04:04 +0000

    deb9ea0 refactor(docs-infra): create fast mode (#54364)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-62b87b4'));
+    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-deb9ea0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
