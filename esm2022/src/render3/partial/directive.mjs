118c318e0e37af796158c2a5edae44ab8c69faef
Andrew Kushnir <akushnir@google.com>
2024-02-13 19:57:47 +0000

    1c53625 fix(http): Use string body to generate transfer cache key. (#54379)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-501c159'));
+    definitionMap.set('version', o.literal('17.2.0-rc.1+sha-1c53625'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
