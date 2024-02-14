a37ea3f0fe73535dc7f258b9460e2b4f43ef69c8
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:27:06 +0000

    12d4b8e docs: add guide for signal inputs (#54316)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-ed0d5ec'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-12d4b8e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
