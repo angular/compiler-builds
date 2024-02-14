454f0332a26ceb3872ae28c0fca3f5824ea99e71
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:27:27 +0000

    44a0ea4 docs: add guide for signal inputs (#54316)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.1+sha-3eda005'));
+    definitionMap.set('version', o.literal('17.2.1+sha-44a0ea4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
