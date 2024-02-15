f1dc176de8000958d9b64dee990c57ab746842b3
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:58:56 +0000

    ba686cc docs: correct default application builder for new apps (#54386)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.1+sha-a7e4b99'));
+    definitionMap.set('version', o.literal('17.2.1+sha-ba686cc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
