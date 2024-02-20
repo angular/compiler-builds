6416a9f7643508e3744d59030835f092827c176d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:37:50 +0000

    a9741e6 docs(router): deprecate RouterTestingModule (#54466)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-70345b1'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-a9741e6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
