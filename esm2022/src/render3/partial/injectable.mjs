e7c2959c1f563dbad29bbe7ae7b70ce62aadcf24
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:10:08 +0000

    d49558d docs: fix brokens links (#54507)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-55fd7b3'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-d49558d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
