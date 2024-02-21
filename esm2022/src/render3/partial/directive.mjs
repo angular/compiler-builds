5e6377878060f8e5c409faaa57dc6a2b93e390ac
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:46:40 +0000

    554628e docs(docs-infra): Add missing API entries to ADEV (#54530)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-d49558d'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-554628e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
