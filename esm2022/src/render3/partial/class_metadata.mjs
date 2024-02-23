46387295dc4c3fcbaf2edcf331dc0a1398aa3be1
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:36:03 +0000

    dcb9deb fix(core): collect providers from NgModules while rendering `@defer` block (#52881)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-0d95ae5'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-dcb9deb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
