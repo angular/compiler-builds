89845090d64e9f00135d086f2ce9db2bc9db8dca
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 17:24:22 +0000

    707bfc9 perf(common): `AsyncPipe` should not call `markForCheck` on subscription (#54554)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-8e65bdc'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-707bfc9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
