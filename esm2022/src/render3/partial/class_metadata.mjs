3bae572902daf8e99fc858c3e690d2f27f4d0a04
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 19:54:10 +0000

    4efcc74 docs: add a guide for model inputs (#54514)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-next.0+sha-1a6beae'));
+    definitionMap.set('version', o.literal('17.3.0-next.0+sha-4efcc74'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
