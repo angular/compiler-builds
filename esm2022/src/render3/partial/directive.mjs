4f3878ddf574b9b6e2079331fe8f191337a04ba1
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:36:06 +0000

    5712352 fix(core): collect providers from NgModules while rendering `@defer` block (#52881)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.2+sha-dcd6c8c'));
+    definitionMap.set('version', o.literal('17.2.2+sha-5712352'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
