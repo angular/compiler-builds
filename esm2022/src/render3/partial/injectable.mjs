45d1591d7ec1433d989b4fa3ab42dfbee2d60479
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 19:54:12 +0000

    fbb0398 docs: add a guide for model inputs (#54514)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.2+sha-1a526f2'));
+    definitionMap.set('version', o.literal('17.2.2+sha-fbb0398'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
