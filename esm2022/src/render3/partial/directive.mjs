2ab8b94e3641119a19cc960fc1883ee897646167
Dylan Hunn <dylhunn@gmail.com>
2024-02-27 23:25:18 +0000

    79a3281 fix(core): fix typo in injectors.svg file (#54596)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.2+sha-11a0a5d'));
+    definitionMap.set('version', o.literal('17.2.2+sha-79a3281'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
