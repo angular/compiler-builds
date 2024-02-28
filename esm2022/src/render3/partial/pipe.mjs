f59952968d00594fce2b424fb8ecfc42760dee05
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:24:56 +0000

    3f0e892 docs: add updating to v17 guide (#52410)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.3+sha-edc45d5'));
+    definitionMap.set('version', o.literal('17.2.3+sha-3f0e892'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
