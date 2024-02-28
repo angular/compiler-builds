b4cf22e35c063bfd8e6222d9ec2d830fdbc5bf9f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:19:58 +0000

    c1590e7 docs: fix broken link in AIO (#54649)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-1519b5b'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-c1590e7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
