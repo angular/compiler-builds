fec5abd8b9f559fd79cbb3796e0831236768ba1f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:43:45 +0000

    2f875d0 docs: update versioning and deprecation policies (#51423)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-52970e3'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-2f875d0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
