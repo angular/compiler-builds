406c4c4b7838e7ee3832213c1e979445991b661f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:39:42 +0000

    7ca08aa docs: updated setting up Zone.js instructions (#52409)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.3+sha-c19c44d'));
+    definitionMap.set('version', o.literal('17.2.3+sha-7ca08aa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
