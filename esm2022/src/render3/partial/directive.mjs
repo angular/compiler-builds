b8d7e79e6f53a3dab1187b835d9e406f9e7759dd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 14:20:44 +0000

    b0f434a docs: fix broken markdown syntax (#53567)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.3+sha-c05bef0'));
+    definitionMap.set('version', o.literal('17.2.3+sha-b0f434a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
