ab68a32436757e0263c5ba491bf4244c526c4f97
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 10:44:53 +0000

    ffbafc7 fix(core): untrack various core operations (#54614)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.3+sha-917b9bd'));
+    definitionMap.set('version', o.literal('17.2.3+sha-ffbafc7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
