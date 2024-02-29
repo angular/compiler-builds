5d42a605555650e76d236d45dc4f50ad1a476450
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 13:59:15 +0000

    b322079 refactor(core): remove unused `EffectRef` import statement (#53863)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-ffad7b8'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-b322079'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
