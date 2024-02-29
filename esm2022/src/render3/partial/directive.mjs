655c0b14fb1899de22f27b93b3aad85b0d96412a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 10:12:26 +0000

    9a8a544 docs(docs-infra): remove the override for piscina (#54656)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-c1c7384'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-9a8a544'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
