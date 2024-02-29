cf008c801b05f55a1287ca2567182411f5d8c306
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 10:44:44 +0000

    ffad7b8 fix(core): untrack various core operations (#54614)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-9a8a544'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-ffad7b8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
