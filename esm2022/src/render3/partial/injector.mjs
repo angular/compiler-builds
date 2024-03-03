b50c0410c3c4569ae7ec1fe86db57220a4f9f33d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-03 13:03:58 +0000

    0767adc docs: fix broken link on the tutorial page (#54669)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.3+sha-37dcf8d'));
+    definitionMap.set('version', o.literal('17.2.3+sha-0767adc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
