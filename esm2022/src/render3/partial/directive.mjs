a35d80da38426d66a1dd56cd4e20b54faf1874a8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:07:35 +0000

    0d98972 docs(docs-infra): Update tutorial deps (#54677)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-3e703f5'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-0d98972'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
