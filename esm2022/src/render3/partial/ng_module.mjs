5758c31a84616b4282903fe1a1a14cc495ce35e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:42:39 +0000

    07d5e42 docs: revise documentation for @for control flow, 'track $index' performance issues explanation, and confusing 'trackBy' mention (#53806)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-next.1+sha-0ddae16'));
+    definitionMap.set('version', o.literal('17.3.0-next.1+sha-07d5e42'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
