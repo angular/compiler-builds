dfc2e7b49bda414c2a60ed589aa18f078dae2318
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 10:16:18 +0000

    6ca8eca docs: queries as signals adev guide (#54505)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.3+sha-e3bdab1'));
+    definitionMap.set('version', o.literal('17.2.3+sha-6ca8eca'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
