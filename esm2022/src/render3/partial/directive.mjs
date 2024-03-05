c5a6d199936c24b32bd63a054e2df86302694ed0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 08:26:12 +0000

    586cc24 fix(core): apply TestBed provider overrides to `@defer` dependencies (#54667)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.3+sha-b558a01'));
+    definitionMap.set('version', o.literal('17.2.3+sha-586cc24'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
