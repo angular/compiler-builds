92e68807d93cd62c18a1a4a6d28332fc75aea858
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 18:16:15 +0000

    3b50885 docs: add process documentation to CARETAKER.md (#54664)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.3+sha-6148d76'));
+    definitionMap.set('version', o.literal('17.2.3+sha-3b50885'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
