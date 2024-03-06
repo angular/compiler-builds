7f6233d9596ed7aafca79b8d1ac2d8b16a3388d9
Andrew Scott <atscott@google.com>
2024-03-06 21:40:01 +0000

    4d708b2 refactor(core): Fix internal test util types (#54736)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.4+sha-50623ea'));
+    definitionMap.set('version', o.literal('17.2.4+sha-4d708b2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
