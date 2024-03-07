03d01f396409937232efb469970dd6a175dab73e
Andrew Scott <atscott@google.com>
2024-03-07 19:18:00 +0000

    d888da4 fix(core): `ApplicationRef.tick` should respect OnPush for host bindings (#53718)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-8bf842d'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-d888da4'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
