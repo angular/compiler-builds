28acb8bfcf24e5b26b6d2b280c7a4d54baaeba47
Andrew Scott <atscott@google.com>
2024-03-11 19:51:45 +0000

    e27b505 fix(core): prevent infinite loops in clobbered elements check (#54425)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.2.4+sha-eff06ea'));
+    definitionMap.set('version', o.literal('17.2.4+sha-e27b505'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
