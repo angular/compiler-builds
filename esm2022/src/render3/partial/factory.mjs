91a807ebfadc91533c34b2a8f2a45d2d89b65bf7
Andrew Scott <atscott@google.com>
2024-03-11 18:17:50 +0000

    eff06ea refactor(core): add `i18nNodes` in preparation for i18n hydration (#54750)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.2.4+sha-4db321c'));
+    definitionMap.set('version', o.literal('17.2.4+sha-eff06ea'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
