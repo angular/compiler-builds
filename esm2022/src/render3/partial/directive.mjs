6bc111163f77988388b9540b88a46d1f31ff5cfb
Andrew Scott <atscott@google.com>
2024-03-11 16:04:14 +0000

    d269c88 refactor(core): avoid additional closure for queued microtask callback (#54801)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-484ae23'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-d269c88'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
