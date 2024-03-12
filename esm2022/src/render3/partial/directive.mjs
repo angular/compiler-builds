431c02257ccab18c1297e30434204defff6979dc
Andrew Scott <atscott@google.com>
2024-03-12 18:44:59 +0000

    456f18b refactor(core): add internal API to enable i18n hydration (#54784)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-db2f9a9'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-456f18b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
