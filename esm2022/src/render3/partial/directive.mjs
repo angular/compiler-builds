29eba5b995203a2c8b5a61711eb3b9188cb32806
Andrew Scott <atscott@google.com>
2024-03-12 16:23:36 +0000

    8735af0 feat(router): Add ability to return `UrlTree` with `NavigationBehaviorOptions` from guards (#45023)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-018f826'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-8735af0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
