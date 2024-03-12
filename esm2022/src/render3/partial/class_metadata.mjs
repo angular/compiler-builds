8656004bee9641f35ea72a309284d3c09a364d92
Andrew Scott <atscott@google.com>
2024-03-12 18:38:18 +0000

    ac395d0 refactor(compiler-cli): cleanup unused code (#54775)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-rc.0+sha-e75a9a4'));
+    definitionMap.set('version', o.literal('17.3.0-rc.0+sha-ac395d0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
