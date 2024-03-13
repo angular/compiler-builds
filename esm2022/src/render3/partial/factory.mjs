38d6b81dc9d5f38f58c133fcafb711f092839028
Andrew Scott <atscott01@gmail.com>
2024-03-13 21:09:32 +0000

    c22c5c4 release: cut the v17.3.0 release

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0-rc.0+sha-5d41ab9'));
+    definitionMap.set('version', o.literal('17.3.0+sha-c22c5c4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
