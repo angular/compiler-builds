1a9b64ff60adcfdf4819174873eb31b6b53b77e7
Andrew Scott <atscott@google.com>
2024-03-13 22:39:33 +0000

    700c052 fix(core): Update ApplicationRef.tick loop to only throw in dev mode (#54848)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-1b19d2d'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-700c052'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
