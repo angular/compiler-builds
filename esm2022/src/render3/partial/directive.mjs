6ea8d1d2bb027e23717baf5bd4b473946f3ac86f
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:23:19 +0000

    406448a build: update dependency archiver to v7 (#54892)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-9574eda'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-406448a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
