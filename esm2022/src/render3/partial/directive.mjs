2b32e1c569b7aa853a1c9bc668bb29639cbdf213
Joey Perrott <josephperrott@gmail.com>
2024-03-15 17:34:30 +0000

    e56c86f build: update actions/cache digest to ab5e6d0 (#54886)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-0472a92'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-e56c86f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
