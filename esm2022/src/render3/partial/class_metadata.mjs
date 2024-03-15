2a4b6c90a940f102c96be5aeb449ede8a243607d
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:06:21 +0000

    9574eda build: lock file maintenance (#54890)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-e56c86f'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-9574eda'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
