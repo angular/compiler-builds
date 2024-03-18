7845bbb6e7a89f48b5836447c589232b00106b0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 18:08:31 +0000

    860ecce ci: move saucelabs to regular CI job (#54926)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0+sha-e61ccd2'));
+    definitionMap.set('version', o.literal('17.3.0+sha-860ecce'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
