ba9d9b92c5a30ed384fa578a3393657fdc4ca914
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 23:59:43 +0000

    314112d fix(core): Prevent `markForCheck` during change detection from causing infinite loops (#54900)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.0+sha-89eca1d'));
+    definitionMap.set('version', o.literal('18.0.0-next.0+sha-314112d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
