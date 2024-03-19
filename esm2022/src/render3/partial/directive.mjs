fd561766180f18679d7daa7f1b3fce70921aa407
Joey Perrott <josephperrott@gmail.com>
2024-03-19 17:07:06 +0000

    766bdf3 build: update dependency google-closure-compiler to v20240317 (#54931)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.0+sha-e70228a'));
+    definitionMap.set('version', o.literal('17.3.0+sha-766bdf3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
