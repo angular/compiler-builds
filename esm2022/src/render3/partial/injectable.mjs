27c3e0d381afa58a414d004fcc289674ac5e3597
Andrew Scott <atscott01@gmail.com>
2024-03-20 19:54:01 +0000

    c2149df release: cut the v17.3.1 release

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.0+sha-99e9474'));
+    definitionMap.set('version', o.literal('17.3.1+sha-c2149df'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
