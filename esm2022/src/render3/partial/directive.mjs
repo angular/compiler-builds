e8d215a3830a15a5a8f16e0cff6d88f150de8215
Andrew Scott <atscott@google.com>
2024-03-20 20:36:04 +0000

    78d4ad2 ci: temporarily disable failing test (#54970)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.1+sha-c2149df'));
+    definitionMap.set('version', o.literal('17.3.1+sha-78d4ad2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
