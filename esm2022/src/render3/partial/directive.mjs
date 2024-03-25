a0bc1b9c5dbfefa70e70161f648576b107716c42
Dylan Hunn <dylhunn@gmail.com>
2024-03-25 18:22:11 +0000

    ee3bb81 refactor(common): request low quality placeholder images (#54899)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.1+sha-5bd188a'));
+    definitionMap.set('version', o.literal('18.0.0-next.1+sha-ee3bb81'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
