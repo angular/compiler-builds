42f0d478bb28bcfe65d7a761ef392a1bb87f023b
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 18:37:23 +0000

    7663199 docs: link to http security now at best-practices/security (#55060)

--- esm2022/src/render3/partial/class_metadata.mjs
+++ esm2022/src/render3/partial/class_metadata.mjs
@@ -19,7 +19,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.1+sha-519e781'));
+    definitionMap.set('version', o.literal('17.3.1+sha-7663199'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
