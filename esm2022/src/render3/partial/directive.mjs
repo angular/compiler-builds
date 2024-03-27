89133e4b108380c7652e0764cd29bf97c2fa448c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-27 12:04:15 +0000

    9aff43b ci: fix build failures in patch branch (#55064)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.1+sha-fa46844'));
+    definitionMap.set('version', o.literal('17.3.1+sha-9aff43b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
