474f7524b166f004dfb8411196f391ae7032c3cf
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:45:07 +0000

    a893f0b fix(zone.js): should not clear onhandler when remove capture listener (#54602)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.1+sha-93ce4d0'));
+    definitionMap.set('version', o.literal('17.3.1+sha-a893f0b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
