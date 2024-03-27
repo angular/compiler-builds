a615672fc04fdecfffd1aceae1c77d76f59aca35
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 17:02:30 +0000

    14235b7 docs: update compat table with 17.3 (#54882)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.1+sha-adf3ee8'));
+    definitionMap.set('version', o.literal('18.0.0-next.1+sha-14235b7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
