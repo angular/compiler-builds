1f41d258fa13107b3a941e1ee222b34a02764696
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:45:11 +0000

    6368871 fix(migrations): account for variables in imports initializer (#55081)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.1+sha-86a359b'));
+    definitionMap.set('version', o.literal('18.0.0-next.1+sha-6368871'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
