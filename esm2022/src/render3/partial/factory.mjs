170ff3b22d55e083f3e3f93fd673c67527902002
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:28:49 +0000

    86a359b fix(core): establish proper injector resolution order for `@defer` blocks (#55079)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.1+sha-336916c'));
+    definitionMap.set('version', o.literal('18.0.0-next.1+sha-86a359b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
