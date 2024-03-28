6269484e43a50a847f4f931a5c10b4544344ffcc
Andrew Scott <atscott@google.com>
2024-03-28 20:11:26 +0000

    9cef0d2 docs(core): fix toSignal docs (#54964)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.2+sha-4d04399'));
+    definitionMap.set('version', o.literal('17.3.2+sha-9cef0d2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
