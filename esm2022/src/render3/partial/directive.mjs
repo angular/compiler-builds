39e66d1d34e4445a74c7de5bd553293f70f9bd87
Andrew Scott <atscott@google.com>
2024-03-28 19:17:44 +0000

    914e453 fix(core): test cleanup should not throw if Zone is not present (#55096)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-ddbf6bb'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-914e453'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
