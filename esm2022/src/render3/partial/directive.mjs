d7ec515fe4efe03e392053e016442aeaa1b8e386
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:21:36 +0000

    336916c refactor(compiler-cli): allow source file validator rule to produce a single diagnostic (#54993)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.1+sha-e02bcf8'));
+    definitionMap.set('version', o.literal('18.0.0-next.1+sha-336916c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
