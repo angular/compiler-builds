47f7e59070e564960124c26337d69a10b90c749a
Andrew Scott <atscott@google.com>
2024-03-28 22:53:56 +0000

    d839c58 docs(docs-infra): improve styling for deprecated CLI option (#54794)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-fac8d34'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-d839c58'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
