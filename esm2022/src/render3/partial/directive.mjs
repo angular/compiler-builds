8399a9a15716b23536d8373c8cf3f19fca987bdc
Andrew Scott <atscott@google.com>
2024-03-29 19:52:52 +0000

    d3c75f8 Revert "ci: Update workflows to use latest dev infra hash (#55123)" (#55126)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-1ae460c'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-d3c75f8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
