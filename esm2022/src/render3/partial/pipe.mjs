28fc42fbeb04f6c21e712e6f1b3032c147c17f84
Andrew Scott <atscott@google.com>
2024-03-29 16:26:37 +0000

    1c5f158 docs: document why `undefined` is usefull and does not remove classes (#55112)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-860de66'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-1c5f158'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
