821ac735aca9e47391697106a778cd22eb75f49c
Andrew Scott <atscott@google.com>
2024-03-29 17:47:42 +0000

    3974c21 refactor(core): All views attached to application are treated as OnPush in zoneless (#55099)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-a122bd1'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-3974c21'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
