53af5ad3e222b4d52c0a79d8348cd41926063724
Andrew Scott <atscott@google.com>
2024-03-29 19:03:13 +0000

    1ae460c docs: remove unused examples (#55110)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-e4fde98'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-1ae460c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
