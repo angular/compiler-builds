5113ce145ab477d1f9982a125a42a8eb43fc1f0f
Andrew Scott <atscott@google.com>
2024-03-30 17:56:27 +0000

    87f3f27 feat(router): Allow resolvers to return `RedirectCommand` (#54556)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-8a8181a'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-87f3f27'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
