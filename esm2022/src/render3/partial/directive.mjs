99c5674f3c06bb75a7f9a01c6c065ae26109886d
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 20:38:18 +0000

    f4e9768 docs: fix aio job (#55179)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.2+sha-7b67ad2'));
+    definitionMap.set('version', o.literal('17.3.2+sha-f4e9768'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
