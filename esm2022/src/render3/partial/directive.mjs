22bd3c7a0d91cd677acb749b517f6aa5a0a9195a
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:12:26 +0000

    2656fd9 refactor(core): remove unnecessary folder (#55197)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.2+sha-1c736dc'));
+    definitionMap.set('version', o.literal('18.0.0-next.2+sha-2656fd9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
