5575d2d4359d02159bd4cbbfdaa29c8d8b2c7de1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 14:59:21 +0000

    58e2f47 test(common): update ImageKit low quality test (#55193)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.3+sha-8cd1b23'));
+    definitionMap.set('version', o.literal('18.0.0-next.3+sha-58e2f47'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
