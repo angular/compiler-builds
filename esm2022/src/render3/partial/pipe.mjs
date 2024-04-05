5c154a6bd52005a5702a5cca77047a17ba412c7d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-04-05 04:31:25 +0000

    9cb1d27 ci: pullapprove is case sensitive (#55220)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.3+sha-ee0412f'));
+    definitionMap.set('version', o.literal('17.3.3+sha-9cb1d27'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
