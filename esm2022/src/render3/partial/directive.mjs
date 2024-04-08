f8c30f855c6a3968fc91ad4144da38892e2e8331
Andrew Scott <atscott@google.com>
2024-04-08 18:40:44 +0000

    4d2a800 docs(docs-infra): move CLI docs to ADEV (#55242)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.3+sha-80d1094'));
+    definitionMap.set('version', o.literal('17.3.3+sha-4d2a800'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
