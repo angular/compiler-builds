90b39e5261bd9589bcf8c75d36ebc96f9034db1f
Andrew Scott <atscott@google.com>
2024-04-08 17:48:13 +0000

    3583d67 build: update dependency madge to v7 (#55245)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.3+sha-a84cb62'));
+    definitionMap.set('version', o.literal('18.0.0-next.3+sha-3583d67'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
