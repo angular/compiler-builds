1dd97381827fe623c4a0e2af655b18fcab746ed6
Andrew Scott <atscott@google.com>
2024-04-08 21:38:08 +0000

    5587dde docs: add back dynamic form image to fix adev broken link (#55247)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.3+sha-650101e'));
+    definitionMap.set('version', o.literal('17.3.3+sha-5587dde'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
