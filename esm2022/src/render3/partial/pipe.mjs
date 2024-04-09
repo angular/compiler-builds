92f9edc45afded432a35d6bb897cca21fad52076
Andrew Scott <atscott@google.com>
2024-04-09 21:08:32 +0000

    bd236cc fix(language-service): implement getDefinitionAtPosition for Angular templates (#55269)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.3+sha-1c6553e'));
+    definitionMap.set('version', o.literal('18.0.0-next.3+sha-bd236cc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
