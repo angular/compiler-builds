0f349bfd71d798497433f4777e7b857e2eceab93
Andrew Scott <atscott@google.com>
2024-04-09 21:51:14 +0000

    766548c fix(common): skip transfer cache on client (#55012)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.3+sha-545fb0d'));
+    definitionMap.set('version', o.literal('17.3.3+sha-766548c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
