6359aaebfff304e3dcf8c15b87d03304864a73b8
Andrew Scott <atscott@google.com>
2024-04-10 18:03:58 +0000

    92debf4 ci: Merge primitives-shared and primitives-event-dispatch groups (#55273)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.3+sha-66e8cd2'));
+    definitionMap.set('version', o.literal('18.0.0-next.3+sha-92debf4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
