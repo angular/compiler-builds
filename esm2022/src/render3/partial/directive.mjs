263726bf6de8c44fef6a0b3a870ec95d2403e8f1
Andrew Scott <atscott@google.com>
2024-04-10 20:11:10 +0000

    ca8969b Revert "docs(docs-infra): move CLI docs to ADEV (#55242)" (#55291)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.3+sha-ce231fb'));
+    definitionMap.set('version', o.literal('17.3.3+sha-ca8969b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
