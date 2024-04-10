c1c36b116d80d2b32624ed5a33c63359ab9f26f0
Andrew Scott <atscott@google.com>
2024-04-10 17:45:00 +0000

    e742787 docs: add guide for the `output()` function (#54946)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.3+sha-6993146'));
+    definitionMap.set('version', o.literal('18.0.0-next.3+sha-e742787'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
