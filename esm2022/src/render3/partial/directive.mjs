4e35d182f6bc3f8cd70df24dc2a6c17460388352
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 15:30:23 +0000

    c77ebb6 docs(core): add missing space (#55287)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.4+sha-b955999'));
+    definitionMap.set('version', o.literal('17.3.4+sha-c77ebb6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
