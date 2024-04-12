977cc49b1d3513cca946b48612cc6bce9ebe4f34
Andrew Scott <atscott@google.com>
2024-04-12 17:47:38 +0000

    c9abe77 docs: Simply redirect function example for router (#55317)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-11523c1'));
+    definitionMap.set('version', o.literal('18.0.0-next.4+sha-c9abe77'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
