d85b169bdf31f7a7c3572a4544b88c3de19e9686
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 10:30:46 +0000

    5eda3bf docs: add missing usage-notes (#54577)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-3af54d5'));
+    definitionMap.set('version', o.literal('18.0.0-next.4+sha-5eda3bf'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
