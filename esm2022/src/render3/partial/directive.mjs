35ec3190268a13b5065c8eba063fb0bc55fcbede
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 15:33:25 +0000

    43cf239 refactor(migrations): exit developer preview for built-in control flow (#55348)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-3471c41'));
+    definitionMap.set('version', o.literal('18.0.0-next.4+sha-43cf239'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
