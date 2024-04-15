d11c20abc029ff1854acd922612fe5684436ca7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:16:04 +0000

    9f7b9ed refactor(core): warn about duplicated keys when using built-in @for (#55243)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-cfed97d'));
+    definitionMap.set('version', o.literal('18.0.0-next.4+sha-9f7b9ed'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
