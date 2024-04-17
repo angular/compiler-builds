8b95cf1052997721615caa9f32bf32f22a4f4b23
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 15:54:05 +0000

    9d2704c docs: release notes for the v17.3.5 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-a34267b'));
+    definitionMap.set('version', o.literal('18.0.0-next.4+sha-9d2704c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
