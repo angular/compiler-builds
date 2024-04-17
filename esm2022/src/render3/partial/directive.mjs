287f5b42a79d7ef0ea3afb99356ee27dda0d5844
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 16:07:23 +0000

    8ae4783 build: update github/codeql-action action to v3.25.0 (#55358)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.5+sha-cc57d4c'));
+    definitionMap.set('version', o.literal('17.3.5+sha-8ae4783'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
