7e8707b1a721818cc5d61b4ef210e4531b5f5dc8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 15:59:36 +0000

    011cc06 release: cut the v18.0.0-next.5 release

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.4+sha-9d2704c'));
+    definitionMap.set('version', o.literal('18.0.0-next.5+sha-011cc06'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
