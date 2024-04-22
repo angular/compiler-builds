115619ca9bccaabc03ebe10c9615dd6fb149c577
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 18:31:35 +0000

    a19e214 build: update actions/cache digest to 0c45773 (#54950)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.5+sha-39b7bda'));
+    definitionMap.set('version', o.literal('17.3.5+sha-a19e214'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
