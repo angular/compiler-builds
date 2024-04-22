578716ef4b622f12a9f1304d94fddfeb5c487024
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 19:06:35 +0000

    5cf14da fix(core): make `ActivatedRoute` inject correct instance inside `@defer` blocks (#55374)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.5+sha-2c66afe'));
+    definitionMap.set('version', o.literal('17.3.5+sha-5cf14da'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
