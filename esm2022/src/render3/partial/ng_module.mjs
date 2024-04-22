7ec9e86e3d969ad7481a519f25c22cbef6441a73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 18:57:44 +0000

    2c66afe docs: remove performance warning for directive composition (#55448)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.5+sha-a19e214'));
+    definitionMap.set('version', o.literal('17.3.5+sha-2c66afe'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
