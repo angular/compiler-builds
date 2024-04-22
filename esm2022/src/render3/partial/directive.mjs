4439a03f8be11b1f6d658d9473d93b6072ddc612
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 16:53:23 +0000

    74333e6 refactor(core): conditionally read data from apply args (#55465)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.5+sha-b1dffa4'));
+    definitionMap.set('version', o.literal('18.0.0-next.5+sha-74333e6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
