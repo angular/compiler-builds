2812847a948a9c19389cd4952437fdccea188c2f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 20:20:58 +0000

    c773238 docs: Fix typo in waitForAsync example (#55407)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.5+sha-5948193'));
+    definitionMap.set('version', o.literal('18.0.0-next.5+sha-c773238'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
