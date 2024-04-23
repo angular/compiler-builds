a542728bf106b0baeb76a983f55ba54198c6fadf
Andrew Kushnir <akushnir@google.com>
2024-04-23 22:23:00 +0000

    5083d89 build: update scorecard action dependencies (#55473)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.5+sha-9cb003a'));
+    definitionMap.set('version', o.literal('17.3.5+sha-5083d89'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
