cb7a8238cf27e1de02cc82d0961f9aeafd3026fc
Andrew Kushnir <akushnir@google.com>
2024-04-24 16:18:19 +0000

    4bf3d89 refactor(core): Remove stop propagation beahavior. (#55435)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.5+sha-811fe00'));
+    definitionMap.set('version', o.literal('18.0.0-next.5+sha-4bf3d89'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
