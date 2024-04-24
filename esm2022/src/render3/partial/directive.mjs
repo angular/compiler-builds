8d9a47b3a4266153b4fa6d046cd34db882524d7a
Andrew Kushnir <akushnir@google.com>
2024-04-24 18:28:39 +0000

    ac863de feat(core): provide ExperimentalPendingTasks API (#55487)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.5+sha-cf2e1b3'));
+    definitionMap.set('version', o.literal('18.0.0-next.5+sha-ac863de'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
