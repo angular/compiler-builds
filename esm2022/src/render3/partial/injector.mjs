041b21abcb21a31908c736fe4e54ff29ebe4c256
Andrew Kushnir <akushnir@google.com>
2024-04-25 16:02:05 +0000

    5ad2f5f refactor(forms): replace errors with hasError() (#55379)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.6+sha-2a3db67'));
+    definitionMap.set('version', o.literal('18.0.0-next.6+sha-5ad2f5f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
