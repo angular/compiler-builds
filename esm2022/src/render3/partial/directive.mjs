eb6c6bef70828fe552eb5cc64968ac75fcc4508e
Andrew Kushnir <akushnir@google.com>
2024-04-25 18:03:13 +0000

    ae29f04 refactor(core): Remove A11Y_SUPPORT_IN_DISPATCHER behavior. (#55497)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.6+sha-a177abe'));
+    definitionMap.set('version', o.literal('18.0.0-next.6+sha-ae29f04'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
