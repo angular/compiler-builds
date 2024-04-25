4c2087ff9a016c7b36a0442682bb84d22113c439
Andrew Kushnir <akushnir@google.com>
2024-04-25 21:36:13 +0000

    9ec0136 refactor(core): Do not duplicate change detection with run coalescing (part 2) (#55403)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-next.6+sha-a80fa8d'));
+    definitionMap.set('version', o.literal('18.0.0-next.6+sha-9ec0136'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
