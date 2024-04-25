7e7693d0b1c226b18657e93d2d1f2d837168feb7
Andrew Kushnir <akushnir@google.com>
2024-04-25 22:02:15 +0000

    507d83d refactor(devtools): hide hydration error when the component tree is collapsed (#54912)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.6+sha-9258160'));
+    definitionMap.set('version', o.literal('17.3.6+sha-507d83d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
