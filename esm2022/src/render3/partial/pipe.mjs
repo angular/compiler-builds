3ce2ad1cb512d8a8a28f959b9031b771ddea8c52
Andrew Kushnir <akushnir@google.com>
2024-04-25 18:03:12 +0000

    f1d6f86 refactor(core): Remove A11Y_SUPPORT_IN_DISPATCHER behavior. (#55497)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-7e89753'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-f1d6f86'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
