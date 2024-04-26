13a8221606434e921bad321f7d7a4e1338d5e94d
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:37:34 +0000

    f93e518 fix(migrations): resolve multiple structural issues with HttpClient migration (#55557)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-next.6+sha-7a97678'));
+    definitionMap.set('version', o.literal('18.0.0-next.6+sha-f93e518'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
