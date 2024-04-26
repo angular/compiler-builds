035f99c96662ecfcc5fd087983fa4af66ed2f66b
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:46:30 +0000

    be17de5 refactor(core): Permit disabling autoDetect for zoneless fixture (#55494)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-91b1f24'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-be17de5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
