77b0950fd73a318759ee28ad2154e021e1a392d0
Andrew Kushnir <akushnir@google.com>
2024-04-30 16:52:06 +0000

    4261fd3 build: improve incremental rebuilds of compliance tests (#55594)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.6+sha-e5d58d2'));
+    definitionMap.set('version', o.literal('17.3.6+sha-4261fd3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
