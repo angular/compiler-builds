c050b17b676be7fbac8fe8da51572573de185307
Andrew Kushnir <akushnir@google.com>
2024-05-01 22:15:21 +0000

    3818436 fix(core): don't schedule timer triggers on the server (#55605)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.7+sha-4c50e1e'));
+    definitionMap.set('version', o.literal('17.3.7+sha-3818436'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
