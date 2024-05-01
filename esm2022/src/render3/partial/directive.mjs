49151756259ed917e0e8eb1781dfaf840bc24f78
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-01 18:46:10 +0000

    874d332 docs: release notes for the v17.3.7 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-7cea897'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-874d332'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
