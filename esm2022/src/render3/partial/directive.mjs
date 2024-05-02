6976554b01e195eb6aa35e0b8ff48d8c1adbbdd3
Andrew Kushnir <akushnir@google.com>
2024-05-02 21:51:29 +0000

    436111f refactor(devtools): set `key` in `manifest.chrome.json` (#55639)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.7+sha-c21b459'));
+    definitionMap.set('version', o.literal('17.3.7+sha-436111f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
