1acb4729cb6c2a8e285dc4d88f0faef630d624c6
Andrew Kushnir <akushnir@google.com>
2024-05-06 19:45:58 +0000

    3e1d6e9 fix(compiler): maintain multiline CSS selectors during CSS scoping (#55509)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-8f273ce'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-3e1d6e9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
