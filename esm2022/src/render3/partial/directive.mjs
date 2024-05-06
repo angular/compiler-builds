efefac6a1de511c27cf45093365d885db5f03f15
Andrew Kushnir <akushnir@google.com>
2024-05-06 15:21:05 +0000

    6baa3bc refactor(core): Allow the container and the listenable element to be configurable for early event contract. (#55586)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.0+sha-f1dc74f'));
+    definitionMap.set('version', o.literal('18.0.0-rc.0+sha-6baa3bc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
