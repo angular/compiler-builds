dbf2d02b5f5f2ae40cf9fc62482495697c4388a4
Andrew Kushnir <akushnir@google.com>
2024-05-06 23:08:04 +0000

    a5f29f9 refactor(devtools): remove git sha stamping from devtools build (#55694)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-rc.0+sha-8483721'));
+    definitionMap.set('version', o.literal('18.0.0-rc.0+sha-a5f29f9'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
