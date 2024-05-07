4e5d76e4314fd78c01910ccdfc0ba1eb544f82cb
Andrew Kushnir <akushnir@google.com>
2024-05-07 15:20:56 +0000

    67bb310 refactor(core): Fix timing of removal of jsaction attribute to be after event replay. (#55696)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-a9460d0'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-67bb310'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
