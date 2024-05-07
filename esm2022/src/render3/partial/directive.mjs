ab2bc3c1db51cab24e675eb7e48bc71a6c31895c
Andrew Kushnir <akushnir@google.com>
2024-05-07 15:12:32 +0000

    a9460d0 fix(zone.js): remove `abort` listener on a signal when actual event is removed (#55339)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-77ae5a3'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-a9460d0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
