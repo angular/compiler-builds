5b624a72a20d6cc35eb228646d50fbba2c2c342c
Andrew Scott <atscott@google.com>
2024-05-08 22:51:34 +0000

    28a24b6 docs: fix visible code snippets in first-app tutorial (#55742)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-f9aac83'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-28a24b6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
