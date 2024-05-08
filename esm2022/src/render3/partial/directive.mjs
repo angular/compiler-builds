cf675e22e158204817dd1a378207923e444dd643
Andrew Scott <atscott@google.com>
2024-05-08 21:05:23 +0000

    f9aac83 refactor(core): a couple of minor changes to the early-event-contract (#55704)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-84f1639'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-f9aac83'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
