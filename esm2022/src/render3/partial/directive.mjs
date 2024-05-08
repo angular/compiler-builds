f99867c5cd42dbd197e0d3510ad62013412e0034
Andrew Scott <atscott@google.com>
2024-05-08 18:16:42 +0000

    84f70cc release: bump Angular DevTools to version 1.0.16 (#55715)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.8+sha-99bf2cd'));
+    definitionMap.set('version', o.literal('17.3.8+sha-84f70cc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
