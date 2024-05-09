59eded9e1ec1163edb066143b262bc59259b4dbe
Andrew Scott <atscott@google.com>
2024-05-09 21:39:10 +0000

    28fb385 refactor(core): Use early event contract instead of the event contract in bootstrap. (#55587)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-fedeaac'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-28fb385'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
