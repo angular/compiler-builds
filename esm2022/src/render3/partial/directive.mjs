20c6c07654b86961bab522116cfd9e774b447ecc
Andrew Scott <atscott@google.com>
2024-05-13 19:53:29 +0000

    9d1acd6 build: switch from rollup and terser to esbuild for creating contract bundle (#55705)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-dc0c55c'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-9d1acd6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
