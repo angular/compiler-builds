4a7e5d9ff0311dd10447d500c662ffa8ff1529ed
Andrew Scott <atscott@google.com>
2024-05-13 18:25:47 +0000

    71f8bca build: update io_bazel_rules_sass digest to fcce061 (#55755)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('17.3.8+sha-84f70cc'));
+    definitionMap.set('version', o.literal('17.3.8+sha-71f8bca'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
