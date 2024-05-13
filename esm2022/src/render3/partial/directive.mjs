4217a57b7c6f555456476428591ba3f58fb3d3c7
Andrew Scott <atscott@google.com>
2024-05-13 16:41:40 +0000

    eb31f2c refactor(core): Remove custom event and replay behavior. (#55695)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-7c9d37d'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-eb31f2c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
