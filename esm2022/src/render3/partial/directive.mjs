75b0e0b168b0c7607bfb62c41765b088d4ef6036
Andrew Scott <atscott@google.com>
2024-05-14 17:59:27 +0000

    beedb68 build: update cross-repo angular dependencies (#55781)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-bfb5f2b'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-beedb68'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
