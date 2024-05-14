f1c8edac1838c9b029b498ad8fb5cac8d3e6bf79
Andrew Scott <atscott@google.com>
2024-05-14 19:56:32 +0000

    6850ff1 ci: properly rely on adev for the adev-deploy job (#55797)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-55d6942'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-6850ff1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
