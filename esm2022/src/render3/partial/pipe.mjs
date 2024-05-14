15f21a4df99af4fe6cb5752437433d409f9dcea0
Andrew Scott <atscott@google.com>
2024-05-14 19:25:52 +0000

    56fb556 docs: fix production mode broken link (#55794)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-f736bea'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-56fb556'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
