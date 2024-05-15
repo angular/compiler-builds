84cb7d0f39f111c28469d7da72ce8b73c184cdf0
Andrew Scott <atscott@google.com>
2024-05-15 16:03:18 +0000

    88fb946 refactor(core): Add a test case for content projection. (#55801)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-4352598'));
+    definitionMap.set('version', o.literal('18.0.0-rc.1+sha-88fb946'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
