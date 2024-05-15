2e1dc1b1f79be6314a5ffc9b622c45fcef23f073
Andrew Scott <atscott@google.com>
2024-05-15 15:55:44 +0000

    d0d8396 refactor(docs-infra): mark more components as OnPush (#55775)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.8+sha-71f8bca'));
+    definitionMap.set('version', o.literal('17.3.8+sha-d0d8396'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
