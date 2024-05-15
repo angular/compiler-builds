f68093f9d27f22aa92e5430e0f1035b863c5dc2b
Andrew Scott <atscott01@gmail.com>
2024-05-15 20:46:57 +0000

    45badde release: cut the v17.3.9 release

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('17.3.8+sha-d0d8396'));
+    definitionMap.set('version', o.literal('17.3.9+sha-45badde'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
