cdd90423fbe5e136c3ef3c15222a6606f3dad89f
Andrew Scott <atscott@google.com>
2024-05-15 20:32:39 +0000

    84b2351 refactor(core): Update error for both zone and zoneless to be only for apps (#55813)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-99bd9f1'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-84b2351'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
