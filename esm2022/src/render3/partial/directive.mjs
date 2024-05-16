0bf81ef77ef099fcff969db586b6e69843566fc2
Andrew Scott <atscott@google.com>
2024-05-16 16:20:19 +0000

    f3b0e2f refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.0+sha-3d5c3d9'));
+    definitionMap.set('version', o.literal('18.1.0-next.0+sha-f3b0e2f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
