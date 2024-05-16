f5c4fa2a2fb74cb01c8d91916e84c9f0739e6dbd
Andrew Scott <atscott@google.com>
2024-05-16 16:26:44 +0000

    3055b92 fix(zone.js): correctly bundle `zone-patch-rxjs` (#55826)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.0-rc.2+sha-a768c90'));
+    definitionMap.set('version', o.literal('18.0.0-rc.2+sha-3055b92'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
