39ca56899ea41166f58c83c9d66ffd2bcaf71ca1
Andrew Kushnir <akushnir@google.com>
2024-06-11 19:52:54 +0000

    5ec24c9 build: update github/codeql-action action to v3.25.8 (#56374)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.2+sha-65bf5ec'));
+    definitionMap.set('version', o.literal('18.0.2+sha-5ec24c9'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
