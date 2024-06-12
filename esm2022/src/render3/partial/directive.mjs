1d6d7bd3fb5d92946b845a331bdf09f36bd59de1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 23:22:00 +0000

    be09f0b release: cut the v18.0.3 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.2+sha-0a8d30e'));
+    definitionMap.set('version', o.literal('18.0.3+sha-be09f0b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
