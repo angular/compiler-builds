73f174b23593b8d4a819a8bdd638c68d593d109d
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:03:57 +0000

    7c76eb5 refactor: add select scenario to the js-web-frameworks benchmark (#56362)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.3+sha-be09f0b'));
+    definitionMap.set('version', o.literal('18.0.3+sha-7c76eb5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
