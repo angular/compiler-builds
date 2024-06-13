603f514966bcbc612a58796afa0d618d1b55fa6d
Andrew Kushnir <akushnir@google.com>
2024-06-13 19:22:34 +0000

    a620231 Revert "refactor(core): Use `ActionResolver` in `Dispatcher`. (#56369)" (#56440)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.3+sha-ec0d1bf'));
+    definitionMap.set('version', o.literal('18.0.3+sha-a620231'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
