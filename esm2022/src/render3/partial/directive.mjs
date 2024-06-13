6a3d5ed291cefbc62c7b62190f0e9e5194362572
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:28:26 +0000

    34dc700 docs: language-service - Fix quick info URLs (#56416)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.3+sha-7c76eb5'));
+    definitionMap.set('version', o.literal('18.0.3+sha-34dc700'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
