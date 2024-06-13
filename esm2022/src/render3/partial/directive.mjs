79eadc346a3194b30b3149f77b6751ccf474c57f
Andrew Kushnir <akushnir@google.com>
2024-06-13 21:03:58 +0000

    1d3a752 feat(router): Set a different browser URL from the one for route matching (#53318)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.2+sha-fca5764'));
+    definitionMap.set('version', o.literal('18.1.0-next.2+sha-1d3a752'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
