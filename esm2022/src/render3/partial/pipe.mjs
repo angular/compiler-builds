3e9115d565856ac94fc7541b4a8d6affd5934841
Andrew Kushnir <akushnir@google.com>
2024-06-14 17:10:43 +0000

    34e7d14 docs: fix broken link in ng-container API reference (#56445)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.3+sha-a620231'));
+    definitionMap.set('version', o.literal('18.0.3+sha-34e7d14'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
