4b97ced379b1c45d2457f1886a18239920c82247
Andrew Kushnir <akushnir@google.com>
2024-06-14 20:54:23 +0000

    4c0882a ci: begin checking licenses of upstream dependencies to ensure they are approved (#56460)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.3+sha-34e7d14'));
+    definitionMap.set('version', o.literal('18.0.3+sha-4c0882a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
