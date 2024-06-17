aae08610533c120d90c55c35a842cb74e5b85f2b
Andrew Kushnir <akushnir@google.com>
2024-06-17 15:59:03 +0000

    01b82a3 docs: update security guide link to adev (#56469)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.3+sha-4c0882a'));
+    definitionMap.set('version', o.literal('18.0.3+sha-01b82a3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
