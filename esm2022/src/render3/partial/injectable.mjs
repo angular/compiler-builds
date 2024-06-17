b5214d91253033c66dc8f3077427036777801639
Andrew Kushnir <akushnir@google.com>
2024-06-17 19:53:13 +0000

    67b2c33 fix(language-service): import the default exported component correctly (#56432)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.2+sha-89ec195'));
+    definitionMap.set('version', o.literal('18.1.0-next.2+sha-67b2c33'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
