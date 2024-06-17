0113a8de35accae5c257cc598d7c0fff2e0f45cc
Andrew Kushnir <akushnir@google.com>
2024-06-17 16:11:21 +0000

    1d48e72 refactor(core): handle incomplete `DOCUMENT` instances during cleanup in tests (#56422)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.3+sha-01b82a3'));
+    definitionMap.set('version', o.literal('18.0.3+sha-1d48e72'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
