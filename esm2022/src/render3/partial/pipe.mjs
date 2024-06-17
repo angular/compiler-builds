eb28ed8ec0e55d7c23ed6c28ebfd5b415c5b6b15
Andrew Kushnir <akushnir@google.com>
2024-06-17 15:59:03 +0000

    08038e8 docs: update security guide link to adev (#56469)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.2+sha-5a45d2b'));
+    definitionMap.set('version', o.literal('18.1.0-next.2+sha-08038e8'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
