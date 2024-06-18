b218448ac993a7112f96260eb747686fd37c5059
Andrew Kushnir <akushnir@google.com>
2024-06-18 19:30:20 +0000

    0bd55a6 refactor(docs-infra): complete removal of aio directory (#56496)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.2+sha-00bde8b'));
+    definitionMap.set('version', o.literal('18.1.0-next.2+sha-0bd55a6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
