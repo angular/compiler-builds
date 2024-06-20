5672959915862be42f8e52ea657beb8a6a599eef
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-06-20 19:04:22 +0000

    2e58620 release: cut the v18.0.4 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.3+sha-5f8f18d'));
+    definitionMap.set('version', o.literal('18.0.4+sha-2e58620'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
