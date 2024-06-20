31c2c74810a273d113f9505e8b0a3e2ed0fec6d0
Andrew Kushnir <akushnir@google.com>
2024-06-20 22:05:19 +0000

    be9e489 fix(core): improve support for i18n hydration of projected content (#56192)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.4+sha-2e58620'));
+    definitionMap.set('version', o.literal('18.0.4+sha-be9e489'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
