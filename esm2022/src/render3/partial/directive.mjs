615c465ab8b5823d9e4921622f0c84c0b2fe743f
Andrew Kushnir <akushnir@google.com>
2024-06-21 17:09:47 +0000

    75c3921 docs: fix file structure link (#56542)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.4+sha-be9e489'));
+    definitionMap.set('version', o.literal('18.0.4+sha-75c3921'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
