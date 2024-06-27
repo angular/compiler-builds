a44d3fa203f2df450a53371cd6f9f60ff40d77a2
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 19:59:35 +0000

    fa9da56 docs: fix indentation on control flow documentation page (#56748)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.5+sha-be6d9c5'));
+    definitionMap.set('version', o.literal('18.0.5+sha-fa9da56'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
