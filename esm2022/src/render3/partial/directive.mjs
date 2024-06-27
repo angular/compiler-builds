42e1b90f21dc97a5888579d548690c19bf4427c3
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:02:38 +0000

    033dfc3 docs: Add mention of `picture` support in FAQ (#56692)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-next.3+sha-cc4cb96'));
+    definitionMap.set('version', o.literal('18.1.0-next.3+sha-033dfc3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
