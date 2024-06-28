4b972e01816ae2d74f4da863282756ec61e8547b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-28 21:12:19 +0000

    387ca41 ci: update pullapprove primitives list (#56761)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.5+sha-2dcb698'));
+    definitionMap.set('version', o.literal('18.0.5+sha-387ca41'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
