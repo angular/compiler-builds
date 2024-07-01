f132bc40f6299071b400593897ba8722e4c9bb2c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 13:48:05 +0000

    deedc47 build: update docs infra package (#56758)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.5+sha-387ca41'));
+    definitionMap.set('version', o.literal('18.0.5+sha-deedc47'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
