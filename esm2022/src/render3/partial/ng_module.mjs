9f4bfd65fb3676e0854ba55675651a388fa0259a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 17:36:49 +0000

    5d52819 refactor(core): Set the jsaction cache directly instead of using jsaction attribute. (#56747)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.5+sha-4267082'));
+    definitionMap.set('version', o.literal('18.0.5+sha-5d52819'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
