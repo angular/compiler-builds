23b963775e2982648099960c14ac0d612486f54c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-07-01 21:08:13 +0000

    e111e31 docs: release notes for the v18.1.0-rc.0 release

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.4+sha-3d1bc5a'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-e111e31'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
