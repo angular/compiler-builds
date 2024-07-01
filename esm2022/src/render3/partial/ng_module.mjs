35600a7d9c9fa5742dbb719deb1c9b8421c8f88f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 18:53:33 +0000

    b4cc8d4 ci: upload build artifacts on each pr run (#56787)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-next.4+sha-f407e3c'));
+    definitionMap.set('version', o.literal('18.1.0-next.4+sha-b4cc8d4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
