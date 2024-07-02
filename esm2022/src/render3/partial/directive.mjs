dfd4bafdb60bfde560bb4d782bddaf0dbf474e3e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:02:04 +0000

    8fa6f40 docs: update playground app to use hello world for easier onboarding (#56813)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-c229916'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-8fa6f40'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
