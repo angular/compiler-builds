a299ca0c7b724966a710e447ed5ce25c7a247742
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 14:21:07 +0000

    b5c2e91 refactor(common): remove remaining code for NgSwitch equality patch (#56806)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-e111e31'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-b5c2e91'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
