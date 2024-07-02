109dde2f2dd7701da389d32d5ee301c335d9c69e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 15:31:38 +0000

    4d81fdc build: update all non-major dependencies (#56801)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-b5c2e91'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-4d81fdc'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
