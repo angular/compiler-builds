ea5418056e912931fbebb0d6af9b996e30642cd4
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:37:47 +0000

    331b30e fix(core): support injection of object with null constructor. (#56553)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-4343cd2'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-331b30e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
