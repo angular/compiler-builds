e923e50c0fdd7cba67f8368bbcacbad4dd6eb41a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:09:02 +0000

    52852ab build: update github/codeql-action action to v3.25.11 (#56803)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-0f612dc'));
+    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-52852ab'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
