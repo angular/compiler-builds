11a0e10ece5a3b95db2015e936091ac509abe6af
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:45:32 +0000

    d9fe17a refactor(common): Fire priority check on stable. (#56776)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-00d9cd2'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-d9fe17a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
