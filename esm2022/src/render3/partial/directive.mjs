d5825ac19915b1bae908911200253b9c30db6751
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 15:41:26 +0000

    c8e2885 feat(compiler): Add extended diagnostic to warn when there are uncalled functions in event bindings (#56295) (#56295)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-d9fe17a'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-c8e2885'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
