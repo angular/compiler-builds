4264d8ebc0062b12a15f28af1f68b9f0d41a6954
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 10:14:51 +0000

    6e1f78f perf(http): execute `fetch` outside of Angular zone (#56820)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-34e236a'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-6e1f78f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
