29ae97a5c384fbeb025b80fdf7db954375463b79
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 09:36:44 +0000

    85da4b3 docs: update app-shell docs to reflect v17 (#56836)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-3fa21cb'));
+    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-85da4b3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
