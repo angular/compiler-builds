d96f35c8d83376a758c758f693c26f2d6154772f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 08:00:19 +0000

    b5500f8 docs: Update image-optimization.md (#56819)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.6+sha-4ceb853'));
+    definitionMap.set('version', o.literal('18.0.6+sha-b5500f8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
