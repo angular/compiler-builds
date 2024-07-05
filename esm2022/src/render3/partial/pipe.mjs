26dce481a32f8879b46e4a5853de1c8b339f9a77
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-05 10:32:10 +0000

    1efe907 docs: fix broken link (#56848)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-1074134'));
+    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-1efe907'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
