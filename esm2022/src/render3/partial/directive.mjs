962392ea7dbfb4d0bf481b2bbf8db5e968f3c946
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 07:58:39 +0000

    0d3ea33 docs: remove private symbol from docs (#56851)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.6+sha-cea3dfa'));
+    definitionMap.set('version', o.literal('18.0.6+sha-0d3ea33'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
