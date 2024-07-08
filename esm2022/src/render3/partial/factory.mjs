e6ef16c8fdb76cc95bcf87425c17f6245e542efc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 14:31:27 +0000

    86478e4 refactor(compiler-cli): do not truncate/reduce types in API docs (#56572)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.6+sha-0d3ea33'));
+    definitionMap.set('version', o.literal('18.0.6+sha-86478e4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
