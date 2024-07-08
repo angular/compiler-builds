b09d0f1294afbfdd03aaacb3576623fb0e84b704
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 16:08:03 +0000

    46f2680 docs(docs-infra): add missing context with link (#56896)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.6+sha-86478e4'));
+    definitionMap.set('version', o.literal('18.0.6+sha-46f2680'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
