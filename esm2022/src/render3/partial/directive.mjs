6f12c0ebfc2df8e164ccad45ac117eef543bdafd
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 10:51:48 +0000

    ea86754 refactor(platform-server): Add private profiler. (#56274)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.6+sha-85f77b5'));
+    definitionMap.set('version', o.literal('18.0.6+sha-ea86754'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
