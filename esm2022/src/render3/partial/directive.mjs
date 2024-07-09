a79bf8d22e8bb02b4fce5e6fcb38823bd471e6d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 16:21:55 +0000

    d6fff45 fix(migrations): Fix cf migration let condition semicolon order (#56913)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.0.6+sha-5d8cfc6'));
+    definitionMap.set('version', o.literal('18.0.6+sha-d6fff45'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
