9f674343d6acdc7bde1cf22b1a2653c7c6b36d09
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:10:25 +0000

    ae9505b docs: update dependency-injection-context.md (#56736)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-0f4219c'));
+    definitionMap.set('version', o.literal('18.1.0-rc.0+sha-ae9505b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
