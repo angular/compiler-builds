74bf307abc248ee228cdd1c03cc1583a2a1f7ee6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:10:00 +0000

    fe52d8f docs: update dependency-injection-context.md (#56736)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-31b235b'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-fe52d8f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
