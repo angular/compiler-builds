d6e98b3006f18f1fd5994b9c83fba546688bb15d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 12:54:15 +0000

    37dcc8d release: cut the v18.0.7 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.0.6+sha-1c2415e'));
+    definitionMap.set('version', o.literal('18.0.7+sha-37dcc8d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
