2949356046ba5bb3251c3b29780f6391ee5fa760
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:55:40 +0000

    6271a16 refactor(core): Remove unused A11Y_CLICK_SUPPORT define. (#56901)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-b6fb5b7'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-6271a16'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
