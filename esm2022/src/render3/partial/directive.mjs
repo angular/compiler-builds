c14733a23c19fa2f7ea4e0e56142e0931d1ee892
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 16:21:30 +0000

    8df13e1 docs: add overview page for performance section (#56828)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-7f550ea'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-8df13e1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
