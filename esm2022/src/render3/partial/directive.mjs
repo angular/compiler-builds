bf1161097abb653a5ac5c3769c2cd84443af0994
Andrew Scott <atscott@google.com>
2024-07-11 16:00:47 +0000

    e5f11dd build: fix rate limits when deploying angular.dev site (#56929)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-dd6e548'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-e5f11dd'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
