fa74cec9619a7948e9e78ad33df0a24632684d05
Andrew Scott <atscott@google.com>
2024-07-15 18:54:44 +0000

    ac9d9db build: update dependency inquirer to v10 (#56883)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-e958fa8'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-ac9d9db'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
