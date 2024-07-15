087ec5352c5861c535f7002ba19a2c60c0fe7dc1
Andrew Scott <atscott@google.com>
2024-07-15 19:10:56 +0000

    2a71f05 docs: change code formatting for better readability (#56974)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.0+sha-ac9d9db'));
+    definitionMap.set('version', o.literal('18.2.0-next.0+sha-2a71f05'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
