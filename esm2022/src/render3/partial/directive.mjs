d9b3cbf98ed7c9828cc88bf59ed333960c2ef130
Andrew Scott <atscott@google.com>
2024-07-16 19:53:45 +0000

    4f11d6e fix(zone.js): support `Timeout.refresh` in Node.js (#56852)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.0+sha-a1cb9df'));
+    definitionMap.set('version', o.literal('18.1.0+sha-4f11d6e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
