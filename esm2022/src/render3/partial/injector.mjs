41e04a292546ca0c76354c42ea056974e70be675
Andrew Scott <atscott@google.com>
2024-07-18 18:11:47 +0000

    c8fa616 docs: fix headings and code font in `@for` docs (#57007)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-5e2b29c'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-c8fa616'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
