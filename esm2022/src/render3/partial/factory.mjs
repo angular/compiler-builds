718d2c742b14f034aba532b6c4a85278ff2b4228
Andrew Scott <atscott@google.com>
2024-07-18 19:09:54 +0000

    425f44c refactor(core): Move logic into early event contract files. (#56994)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-658c7fe'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-425f44c'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
