240bd9d97784cd89addd3d75c38ebde9d185876b
Andrew Scott <atscott@google.com>
2024-07-18 22:32:12 +0000

    e088c80 docs(docs-infra): update tutorial deps to 18.1 (#57024)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.1+sha-14d442a'));
+    definitionMap.set('version', o.literal('18.1.1+sha-e088c80'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
