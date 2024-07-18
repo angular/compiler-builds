7d961b29a661ab217279e7d991632603c62b9e9d
Andrew Scott <atscott@google.com>
2024-07-18 18:17:28 +0000

    658c7fe docs: fix changelog entry (#57038)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-c8fa616'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-658c7fe'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
