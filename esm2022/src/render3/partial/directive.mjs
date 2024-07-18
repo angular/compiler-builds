9228fdac3d0212313d6d034756c6de366dc261cc
Andrew Scott <atscott@google.com>
2024-07-18 22:31:44 +0000

    f3a88ad docs(docs-infra): update tutorial deps to 18.1 (#57024)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-425f44c'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-f3a88ad'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
