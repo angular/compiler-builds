1e4f054877c52638cace59ca3e3f732d35eab0ec
Andrew Scott <atscott@google.com>
2024-07-19 01:09:10 +0000

    04c504a docs: fix pipes link (#52979)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.1+sha-e088c80'));
+    definitionMap.set('version', o.literal('18.1.1+sha-04c504a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
