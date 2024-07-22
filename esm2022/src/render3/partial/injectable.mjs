34aa76c20b004f8b9b33bdc6b9bfdab882e192c5
Andrew Scott <atscott@google.com>
2024-07-22 17:57:15 +0000

    13bc8d7 docs: Update packages/router/src/router.ts (#57053)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-aeaf07d'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-13bc8d7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
