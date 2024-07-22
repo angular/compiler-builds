9cae1131475083ab4ac2d679ef390eee7480f9ad
Andrew Scott <atscott@google.com>
2024-07-22 22:33:21 +0000

    915b0e0 Revert "fix(compiler-cli): add warning for unused let declarations (#57033)" (#57088)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.1+sha-4639450'));
+    definitionMap.set('version', o.literal('18.1.1+sha-915b0e0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
