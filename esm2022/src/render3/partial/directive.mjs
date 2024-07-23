b37882aced3b0de8c75e1bbcbf4334b6f1f5985c
Andrew Scott <atscott@google.com>
2024-07-23 22:50:11 +0000

    288a4fa test(docs-infra): fix test with slight adjustment to error handling (#57104)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.1+sha-889a02b'));
+    definitionMap.set('version', o.literal('18.1.1+sha-288a4fa'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
