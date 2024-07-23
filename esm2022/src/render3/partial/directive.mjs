aa42a81706ec6867a7dfdeb93d73b39636410d7d
Andrew Scott <atscott@google.com>
2024-07-23 19:25:06 +0000

    d7860a5 build: update all non-major dependencies (#57092)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-a50a81c'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-d7860a5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
