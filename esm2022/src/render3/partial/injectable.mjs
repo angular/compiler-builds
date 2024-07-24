ced85653681dbc764fd46e6c7da02c830bac25b5
Andrew Scott <atscott@google.com>
2024-07-24 17:22:42 +0000

    d5fbe11 docs(docs-infra): disable experimental decorators (#56257)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-0e439d7'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-d5fbe11'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
