ff1c3952d96753115ffc7ff7bc2eeea80558abbd
Andrew Scott <atscott@google.com>
2024-07-24 17:22:49 +0000

    8bb5937 docs(docs-infra): disable experimental decorators (#56257)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.1+sha-c969422'));
+    definitionMap.set('version', o.literal('18.1.1+sha-8bb5937'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
