4d76f3d02229f00fe107e5a54f17b5e4e470d45a
Andrew Scott <atscott@google.com>
2024-07-24 17:09:10 +0000

    0e439d7 Revert "fix(router): Scroller should scroll as soon as change detection completes (#55105)" (#57115)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-83117f1'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-0e439d7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
