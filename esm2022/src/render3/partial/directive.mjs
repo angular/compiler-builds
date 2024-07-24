e5b77d6d32821ec919214655bf179f4f4b785db9
Andrew Scott <atscott@google.com>
2024-07-24 17:29:57 +0000

    a7b973e docs(docs-infra): Use shiki for code highlighting (#57059)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.1+sha-d5fbe11'));
+    definitionMap.set('version', o.literal('18.2.0-next.1+sha-a7b973e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
