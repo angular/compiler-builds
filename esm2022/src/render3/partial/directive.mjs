25800528d47114dfdbad2330378917d7d1e63b3c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 15:56:15 +0000

    81b30dc refactor(docs-infra): migrate @angular/docs from dev-infra into adev directory (#57132)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.2+sha-9304c45'));
+    definitionMap.set('version', o.literal('18.1.2+sha-81b30dc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
