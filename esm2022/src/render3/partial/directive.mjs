a458e5079b99627c330144ef4a33b86f19eb442d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 17:00:52 +0000

    0fbcd6e docs: broken HammerLoader reference fix (#55836)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.2+sha-130ff94'));
+    definitionMap.set('version', o.literal('18.1.2+sha-0fbcd6e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
