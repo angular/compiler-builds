f22f443de275bf6c40729138b4ac5bbd978516e7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 18:10:07 +0000

    62ade76 refactor(core): Update callback schedulers to cancel pending timers (#57186)

--- esm2022/src/render3/partial/factory.mjs
+++ esm2022/src/render3/partial/factory.mjs
@@ -21,7 +21,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION = '12.0.0';
 export function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.2+sha-0fbcd6e'));
+    definitionMap.set('version', o.literal('18.1.2+sha-62ade76'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
