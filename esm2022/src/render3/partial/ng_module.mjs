db88c6522d07a04c31c2b744a546f64ba4f09713
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 14:19:14 +0000

    e11c0c4 fix(compiler-cli): run JIT transforms on `@NgModule` classes with `jit: true` (#57212)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.2+sha-3afd7f0'));
+    definitionMap.set('version', o.literal('18.2.0-next.2+sha-e11c0c4'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
