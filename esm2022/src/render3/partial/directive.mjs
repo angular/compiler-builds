0f7fa780f89ff8c65b98e20440a5fb9df4f5f853
Dylan Hunn <dylhunn@gmail.com>
2024-07-31 17:29:25 +0000

    89f28b6 release: cut the v18.2.0-next.3 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.2+sha-414d985'));
+    definitionMap.set('version', o.literal('18.2.0-next.3+sha-89f28b6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
