0ce508679b3d4d80fa1a8b0004e82d841b2f5692
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 19:01:20 +0000

    de7f5ac refactor(docs-infra): Use new APIs in search dialog (#57215)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.3+sha-be6c246'));
+    definitionMap.set('version', o.literal('18.1.3+sha-de7f5ac'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
