6b2dd80081156176cb10f021c03534fc388f5e65
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 14:05:52 +0000

    3afd7f0 build: update scorecard action dependencies (#57150)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-next.2+sha-147eee4'));
+    definitionMap.set('version', o.literal('18.2.0-next.2+sha-3afd7f0'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
