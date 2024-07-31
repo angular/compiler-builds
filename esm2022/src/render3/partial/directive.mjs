d98df65cf1003f57953185dd55628f82c197f0c6
Dylan Hunn <dylhunn@gmail.com>
2024-07-31 17:23:31 +0000

    414d985 docs: release notes for the v18.1.3 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.2+sha-d26a91f'));
+    definitionMap.set('version', o.literal('18.2.0-next.2+sha-414d985'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
