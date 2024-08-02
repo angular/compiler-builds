1fbb0a26f162e5c52c199fc79d5d9499c13e0e7c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 16:57:34 +0000

    523f639 release: bump Angular DevTools to 1.0.17 (#57221)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.1.3+sha-6ac209c'));
+    definitionMap.set('version', o.literal('18.1.3+sha-523f639'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
