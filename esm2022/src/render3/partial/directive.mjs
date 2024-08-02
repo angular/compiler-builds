3b0f3197d733165767f79594f2f73b44777aa049
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 16:57:22 +0000

    9aa94eb release: bump Angular DevTools to 1.0.17 (#57221)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.3+sha-827070e'));
+    definitionMap.set('version', o.literal('18.2.0-next.3+sha-9aa94eb'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
