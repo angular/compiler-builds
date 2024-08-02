a2ddfa92c4fe529609dc23b97776ee3dbbc4f631
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:15:53 +0000

    6c76c91 feat(router): Add defaultQueryParamsHandling to router configuration (#57198)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.3+sha-2a915d1'));
+    definitionMap.set('version', o.literal('18.2.0-next.3+sha-6c76c91'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
