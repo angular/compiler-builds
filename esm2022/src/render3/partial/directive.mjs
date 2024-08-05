80e07669088d48c0f009ffa206e1d7347a2a40e1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 17:13:10 +0000

    3bdead1 refactor(docs-infra): migrate api-gen from dev-infra into the repo (#57241)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.3+sha-de99d3e'));
+    definitionMap.set('version', o.literal('18.2.0-next.3+sha-3bdead1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
