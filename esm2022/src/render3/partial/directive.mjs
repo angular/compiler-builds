71ac9ad4ef72f93f4da628d713be2370faae2117
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-05 17:39:49 +0000

    4d0842d release: cut the v18.2.0-next.4 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.3+sha-3bdead1'));
+    definitionMap.set('version', o.literal('18.2.0-next.4+sha-4d0842d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
