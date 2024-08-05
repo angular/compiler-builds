9a76a758fba8ceaaf26373716311ae93b721de30
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 21:16:07 +0000

    9109f31 Revert "refactor(docs-infra): Use new APIs in search dialog (#57215)" (#57268)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.4+sha-70e8b40'));
+    definitionMap.set('version', o.literal('18.2.0-next.4+sha-9109f31'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
