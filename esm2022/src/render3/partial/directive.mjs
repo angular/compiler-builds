31edab87309d6d9e65568569a088b2ed2cb9aa50
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 16:52:44 +0000

    0b1dd39 perf(compiler-cli): improve performance of `interpolatedSignalNotInvoked` extended diagnostic (#57291)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-next.4+sha-7919982'));
+    definitionMap.set('version', o.literal('18.2.0-next.4+sha-0b1dd39'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
