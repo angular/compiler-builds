fa46ddd9e81d4da9b24e8102f30edf9c5f8d54dd
Andrew Kushnir <akushnir@google.com>
2024-08-08 15:38:03 +0000

    e39b22a fix(core): Account for addEventListener to be passed a Window or Document. (#57282)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.1.4+sha-bdf8450'));
+    definitionMap.set('version', o.literal('18.1.4+sha-e39b22a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
