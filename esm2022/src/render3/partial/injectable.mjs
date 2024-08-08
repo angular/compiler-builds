321469dca4035250967f607d6d19afc6a686d7fd
Andrew Kushnir <akushnir@google.com>
2024-08-08 15:37:30 +0000

    3439cc2 fix(core): Account for addEventListener to be passed a Window or Document. (#57282)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.3.0-next.0+sha-0761e9a'));
+    definitionMap.set('version', o.literal('18.3.0-next.0+sha-3439cc2'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
