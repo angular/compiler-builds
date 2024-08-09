cb4fa28828bc3549d2fee691193ff93ecc721ab4
Andrew Kushnir <akushnir@google.com>
2024-08-09 15:13:37 +0000

    f271021 feat(router): Add `routerOutletData` input to `RouterOutlet` directive (#57051)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.3.0-next.0+sha-769b6e1'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-f271021'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
