9433f6f356dc9d2f4dbd6015e9232f0e3b2f88b1
Andrew Kushnir <akushnir@google.com>
2024-08-12 16:32:30 +0000

    b279081 fix(router): Align RouterModule.forRoot errorHandler with provider error handler (#57050)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-37bf7de'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-b279081'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
