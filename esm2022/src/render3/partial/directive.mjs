3662db1cb4d79e567942e023ff32813934961596
Andrew Kushnir <akushnir@google.com>
2024-08-12 19:16:53 +0000

    5d16c28 refactor(migrations): handle safe property reads in signal input migration (#57318)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-cea3e4b'));
+    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-5d16c28'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
