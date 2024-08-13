caf769b64d23f225aba645075b860d77a8dbee6b
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:47:40 +0000

    de85979 fix(core): skip hydration for i18n nodes that were not projected (#57356)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-8475206'));
+    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-de85979'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
