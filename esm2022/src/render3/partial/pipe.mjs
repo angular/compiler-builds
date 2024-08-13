8bdb83a2f053312db6d3af05ee816a62b0bdaaf4
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:58:52 +0000

    b1a9d0f fix(migrations): avoid duplicating comments when generating properties (#57367)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-de85979'));
+    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-b1a9d0f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
