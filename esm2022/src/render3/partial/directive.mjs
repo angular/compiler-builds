66168e49948782de7baf70b3c0e008257aee087b
Andrew Kushnir <akushnir@google.com>
2024-08-13 19:15:34 +0000

    e30c60e refactor(core): Add experimental support to have one event contract when there are multiple apps on the page. (#57355)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-b1a9d0f'));
+    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-e30c60e'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
