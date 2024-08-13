8eacb63f4c7396a0314344c7d9adb299dbcb52b9
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:41:12 +0000

    8475206 docs(docs-infra): Update marked to 14 (#57363)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-b8c82fa'));
+    definitionMap.set('version', o.literal('18.2.0-rc.0+sha-8475206'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
