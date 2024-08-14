75e6c7c9edb25dd0abbfb9aab7be922dfe65b9c1
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-08-14 16:47:58 +0000

    c1f1c02 docs: release notes for the v18.2.0 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-c5b598e'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-c1f1c02'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-c5b598e'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-c1f1c02'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
