43c77988f32d81552ab8385a9599c26d3ca48a98
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 14:50:42 +0000

    9155f75 docs(common): fix example code (#57406)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0+sha-d49e083'));
+    definitionMap.set('version', o.literal('18.2.0+sha-9155f75'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
