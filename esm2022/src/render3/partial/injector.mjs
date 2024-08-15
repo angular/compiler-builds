a7a548a4e42dc10352519d493e6ce6ba60b1c01c
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 16:36:29 +0000

    3b0dca7 fix(core): Allow zoneless scheduler to run inside `fakeAsync` (#56932)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-58a79b6'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-3b0dca7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-58a79b6'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-3b0dca7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
