ccd20e1725e36120fa842cbeb941b9f1acc8e3f0
Dylan Hunn <dylhunn@gmail.com>
2024-08-16 15:25:24 +0000

    3e6ee7e docs: update CHANGELOG to drop a breaking change note from 18.2.0 version (#57420)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.0+sha-086b754'));
+    definitionMap.set('version', o.literal('18.2.0+sha-3e6ee7e'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
