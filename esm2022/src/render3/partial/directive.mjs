802575f6ae1a6a200062b7bab6c73edcd4b28312
Dylan Hunn <dylhunn@gmail.com>
2024-08-17 13:58:16 +0000

    368f36d refactor(migrations): framework to build batchable migrations (#57396)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('19.0.0-next.0+sha-baa1254'));
+    definitionMap.set('version', o.literal('19.0.0-next.0+sha-368f36d'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
