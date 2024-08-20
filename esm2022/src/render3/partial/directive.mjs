862646f257ec75b63d0fff6d94fff7a8de6aa926
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-20 20:25:12 +0000

    1a5f5ee build: update github/codeql-action action to v3.26.3 (#57455)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.0+sha-980c64a'));
+    definitionMap.set('version', o.literal('18.2.0+sha-1a5f5ee'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
