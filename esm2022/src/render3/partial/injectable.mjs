96e2040d23caa1fe4c3a63b99d6989ff65d062a8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-08-23 12:06:28 +0000

    87c594b refactor(migrations): add support for simpler variant of tsurge migration (#57484)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.1+sha-09deb24'));
+    definitionMap.set('version', o.literal('18.2.1+sha-87c594b'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
