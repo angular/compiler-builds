d872eca503093392087e8eeef2128a00476650e6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 21:51:25 +0000

    6d3a2af fix(core): Do not bubble capture events. (#57476)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.1+sha-223b785'));
+    definitionMap.set('version', o.literal('18.2.1+sha-6d3a2af'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
