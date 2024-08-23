9c28fe630a1161336ee86c1101b21343c25c4537
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 20:54:07 +0000

    aacef07 refactor(migrations): replacements in tsurge are always serializable (#57501)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('19.0.0-next.1+sha-17f7b25'));
+    definitionMap.set('version', o.literal('19.0.0-next.1+sha-aacef07'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
