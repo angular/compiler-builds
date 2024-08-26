a797df10765931a13e99c77d0cf0711090bc3f18
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-26 16:22:39 +0000

    03b3527 docs: fix typo in recommendations.ts (#57521) PR Close #57521

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.1+sha-03ec620'));
+    definitionMap.set('version', o.literal('18.2.1+sha-03b3527'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
