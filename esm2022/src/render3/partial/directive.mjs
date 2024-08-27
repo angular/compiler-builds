e42505950192c885659854d3f44289ec04949684
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 21:07:39 +0000

    76b9e2b Revert "refactor(migrations): initial migration logic for converting to signal queries (#57525)" (#57555)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('19.0.0-next.1+sha-21445a2'));
+    definitionMap.set('version', o.literal('19.0.0-next.1+sha-76b9e2b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
