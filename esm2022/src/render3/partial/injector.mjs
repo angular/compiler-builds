ed7a1e1b7f2a4af9855792ad06a6f7825d464331
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 22:12:41 +0000

    226a67d fix(core): Schedulers run in zone above Angular rather than root (#57553)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('19.0.0-next.1+sha-76b9e2b'));
+    definitionMap.set('version', o.literal('19.0.0-next.1+sha-226a67d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
