16f72127f31266664f99424be86176024e3758f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 20:02:35 +0000

    3067633 docs(docs-infra): Add query as an url query param to the api reference (#57062)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.1+sha-03b3527'));
+    definitionMap.set('version', o.literal('18.2.1+sha-3067633'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
