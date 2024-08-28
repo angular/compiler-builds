2bf50a40a50138b5b9ef27e86457a024bdf6ed42
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 21:36:53 +0000

    1853bbb release: cut the v18.2.2 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.1+sha-3ecf620'));
+    definitionMap.set('version', o.literal('18.2.2+sha-1853bbb'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
