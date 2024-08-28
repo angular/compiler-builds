0ff5cf9a2cd9a2e28be373c15f399897a01dab64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-28 15:54:45 +0000

    adf3274 fix(devtools): correctly set environment injector path in the case where there are no element injectors (#57442)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.1+sha-eba3a0a'));
+    definitionMap.set('version', o.literal('18.2.1+sha-adf3274'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
