ac03f2c04d70d63c32663e23baa4766dd12dffc1
Andrew Kushnir <akushnir@google.com>
2024-08-29 22:24:32 +0000

    614c311 docs: update common-router-tasks to include sample dynamic fragment (#57590)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.2+sha-5007970'));
+    definitionMap.set('version', o.literal('18.2.2+sha-614c311'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
