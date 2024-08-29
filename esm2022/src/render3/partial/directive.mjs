661e19d0871a2150b2bd9f67664c99fbac9a1d39
Andrew Kushnir <akushnir@google.com>
2024-08-29 14:57:59 +0000

    d9c1004 refactor(migrations): ensure control flow analysis of signal input migration works with all TS versions (#57566)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.2+sha-98c6c05'));
+    definitionMap.set('version', o.literal('18.2.2+sha-d9c1004'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
