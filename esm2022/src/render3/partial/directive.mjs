9b2335263ff077b569acd5aa7f73c18e1bf913e4
Andrew Kushnir <akushnir@google.com>
2024-08-29 17:23:20 +0000

    93bdbbc docs(docs-infra): Add dev-mode only mention for `core/global` (#57365)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.1+sha-5e9661d'));
+    definitionMap.set('version', o.literal('18.2.2+sha-93bdbbc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
