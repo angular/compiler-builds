9fd5a7f08bed2a97a65a406d0600c1a6b5e95c14
Andrew Kushnir <akushnir@google.com>
2024-09-03 16:15:25 +0000

    de68e04 fix(http): Dynamicaly call the global fetch implementation (#57531)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.2+sha-2324d9b'));
+    definitionMap.set('version', o.literal('18.2.2+sha-de68e04'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
