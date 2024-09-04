4936eb25a570831903aa154474daca7188e5983b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 21:04:25 +0000

    004110c refactor(devtools): use signal apis for directive forest (#56998)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.3+sha-69ca33f'));
+    definitionMap.set('version', o.literal('18.2.3+sha-004110c'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
