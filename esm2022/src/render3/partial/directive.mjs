26fd5da8d1a0af5b6309f074c1c0b86700eaceee
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:08:43 +0000

    4cb07e6 docs: extraneous backslashes removed (#57725)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.3+sha-0843f31'));
+    definitionMap.set('version', o.literal('18.2.3+sha-4cb07e6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
