e0c11a8515e53ff5fb4883396406853201080870
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:21:25 +0000

    b619d69 fix(compiler): produce less noisy errors when parsing control flow (#57711)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.3+sha-4cb07e6'));
+    definitionMap.set('version', o.literal('18.2.3+sha-b619d69'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
