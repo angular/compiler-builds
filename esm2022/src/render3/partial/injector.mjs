5bab638dff2cd53fe99cbaf9015bdc3d38150c4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:44:32 +0000

    8d27839 docs: fix broken link in roadmap page (#57722)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.3+sha-b619d69'));
+    definitionMap.set('version', o.literal('18.2.3+sha-8d27839'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
