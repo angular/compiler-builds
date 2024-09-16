c8ca0de384b5562964de12169f3349cc4e7908cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:30:31 +0000

    e16b8a3 docs: correct key usage in sectionStyles and update template binding (#57820) PR Close #57820

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.4+sha-d5a27b8'));
+    definitionMap.set('version', o.literal('18.2.4+sha-e16b8a3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
