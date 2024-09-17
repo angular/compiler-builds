fae62b9e845b4b3c02b75c02fc5a76af11a011f4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 14:35:01 +0000

    76709d5 fix(core): Handle @let declaration with array when `preparingForHydration` (#57816)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.4+sha-dbd697c'));
+    definitionMap.set('version', o.literal('18.2.4+sha-76709d5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
