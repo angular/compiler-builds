18f98262e85ceb54b1d8f0bea71656cf93526ba5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 14:57:59 +0000

    0365f38 docs: adapt the NgModule to the new default : standalone (#57532)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.4+sha-e685ed8'));
+    definitionMap.set('version', o.literal('18.2.4+sha-0365f38'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
