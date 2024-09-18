ae1205a72499ca64a6a2065a2466371654f8ce0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 07:54:40 +0000

    e685ed8 fix(compiler-cli): extended diagnostics not validating ICUs (#57845)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.4+sha-76709d5'));
+    definitionMap.set('version', o.literal('18.2.4+sha-e685ed8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
