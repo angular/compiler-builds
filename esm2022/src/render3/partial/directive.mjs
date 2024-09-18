28b81ccfc7fa09a404fe2fe605299547397ca7d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 15:33:54 +0000

    2f96a34 release: cut the v18.2.5 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.4+sha-0365f38'));
+    definitionMap.set('version', o.literal('18.2.5+sha-2f96a34'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
