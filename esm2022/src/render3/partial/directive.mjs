ed42d9a79fe5a3abab4a4c3be655a3c5671c4549
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 10:55:37 +0000

    47d5341 docs: fix closing tag slash location (#57405)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.5+sha-ad9ba9a'));
+    definitionMap.set('version', o.literal('18.2.5+sha-47d5341'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
