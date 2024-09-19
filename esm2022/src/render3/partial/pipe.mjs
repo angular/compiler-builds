5024060298fcc6394403352a2980cc30b250337c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 10:49:38 +0000

    ad9ba9a build(docs-infra): use chunk optimizer for adev builds (#56830)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.5+sha-1701c40'));
+    definitionMap.set('version', o.literal('18.2.5+sha-ad9ba9a'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
