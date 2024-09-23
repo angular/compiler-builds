8b43c0fcf52f205662f5731b4fc206dcc415f63a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:13:34 +0000

    3cc40f3 refactor(devtools): provide user with suggestions on why their angular application is not being detected in dev mode (#57861)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.5+sha-6092015'));
+    definitionMap.set('version', o.literal('18.2.5+sha-3cc40f3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
