3aa9507e921ce3d0198d87b986f58da17f100270
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 15:25:45 +0000

    1825460 fix(docs-infra): resolve Mermaid from Bazel root path (#57924)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.5+sha-3cc40f3'));
+    definitionMap.set('version', o.literal('18.2.5+sha-1825460'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
