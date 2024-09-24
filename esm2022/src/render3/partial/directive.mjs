c8947eaf5ce706631902722050ff1de9b89ec825
Andrew Scott <atscott@google.com>
2024-09-24 20:07:40 +0000

    dbf15c8 fix(docs-infra): extend the timeout for jasmine tests of mermaid (#57948)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.5+sha-03ac3c2'));
+    definitionMap.set('version', o.literal('18.2.5+sha-dbf15c8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
