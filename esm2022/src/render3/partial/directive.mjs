cc8da565f043893f0304b5fdff713c7531172024
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:12:00 +0000

    b7b0052 refactor(router): Move view transitions to developer preview (#55600)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.5+sha-997fe96'));
+    definitionMap.set('version', o.literal('18.2.5+sha-b7b0052'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
