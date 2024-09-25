4410e07a80c4ac61b9dbabc483c0a06217a2499b
Andrew Scott <atscott01@gmail.com>
2024-09-25 17:05:11 +0000

    1391928 release: cut the v18.2.6 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.5+sha-dbf15c8'));
+    definitionMap.set('version', o.literal('18.2.6+sha-1391928'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
