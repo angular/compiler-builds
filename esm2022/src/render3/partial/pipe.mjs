81322240156d7a2bb04a434078d199e3e3b82eec
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 07:12:17 +0000

    b84e2d3 docs: add deferrable views tutorial (#57934)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.6+sha-b9d846d'));
+    definitionMap.set('version', o.literal('18.2.6+sha-b84e2d3'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
