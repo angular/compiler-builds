d4cd10a7984a681bd1535e63bc9628124c450f8c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 16:15:11 +0000

    70591e7 docs: use proper workdir for tutorial and playground (#56063)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.6+sha-a21867d'));
+    definitionMap.set('version', o.literal('18.2.6+sha-70591e7'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
