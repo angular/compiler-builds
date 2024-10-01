55f604716836b849a35c6dbbb58a6504f6bbf122
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:37:17 +0000

    8d8fb44 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.6+sha-6b67cb5'));
+    definitionMap.set('version', o.literal('18.2.6+sha-8d8fb44'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
