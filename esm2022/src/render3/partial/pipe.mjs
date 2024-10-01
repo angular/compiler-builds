0a65764021e738400dbdaa7b4109595f4f8f1fc7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 13:55:48 +0000

    a21867d fix(docs-infra): allow tutorial component to be GCed (#58028)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.6+sha-8d8fb44'));
+    definitionMap.set('version', o.literal('18.2.6+sha-a21867d'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
