94ce88c61b61772f69b1a6885730ee700ed7740a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 11:51:13 +0000

    46a2ad3 fix(common): prevent warning about oversize image twice (#58021)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.6+sha-c48816e'));
+    definitionMap.set('version', o.literal('18.2.6+sha-46a2ad3'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
