336eff049306efff1ff8f10f83dfd5e0f688ab60
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 18:07:12 +0000

    dc911c6 refactor(docs-infra): cleanup `AfterRenderSequence` for reference list (#58030)

--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.7+sha-46b088a'));
+    definitionMap.set('version', o.literal('18.2.7+sha-dc911c6'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
