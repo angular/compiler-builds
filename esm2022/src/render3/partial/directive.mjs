775c2b236e96fc7b7cb8acced5207f63c4d6f833
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-03 16:48:21 +0000

    694fe3f fix(docs-infra): fix calculation of height editor (#54128) Apply the min() function for set the smallest height

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.7+sha-dc911c6'));
+    definitionMap.set('version', o.literal('18.2.7+sha-694fe3f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
