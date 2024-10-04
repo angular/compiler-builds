5a3c7f903dcdde480a9ade9fa4c69c52f39a05e1
Andrew Kushnir <akushnir@google.com>
2024-10-04 18:04:38 +0000

    39a851f refactor: change security issue redirect to angular.dev (#58070)

--- esm2022/src/render3/partial/ng_module.mjs
+++ esm2022/src/render3/partial/ng_module.mjs
@@ -33,7 +33,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.7+sha-de03e93'));
+    definitionMap.set('version', o.literal('18.2.7+sha-39a851f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
