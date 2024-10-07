03c30bee412f4ae3d48ffbbe1e27bd0c9f8ea85e
Andrew Kushnir <akushnir@google.com>
2024-10-07 15:33:46 +0000

    02de7fc fix(zone.js): remove `abort` listener once fetch is settled (#57882)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.7+sha-10081a9'));
+    definitionMap.set('version', o.literal('18.2.7+sha-02de7fc'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
