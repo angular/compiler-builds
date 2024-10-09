c8a52dfbe6b7ea28c0bd28497aa0fac983b080a2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 13:55:28 +0000

    31fb9d0 docs: mention autoDetectChanges parameter default value (#58092)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.7+sha-59394ee'));
+    definitionMap.set('version', o.literal('18.2.7+sha-31fb9d0'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
