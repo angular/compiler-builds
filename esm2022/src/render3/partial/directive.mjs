6d50bf8bc74966d16e620890bf915d5ac2b10db6
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:29:24 +0000

    da0e6b1 docs(core): fix description about signal value equality (#56472)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.8+sha-2532ffa'));
+    definitionMap.set('version', o.literal('18.2.8+sha-da0e6b1'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
