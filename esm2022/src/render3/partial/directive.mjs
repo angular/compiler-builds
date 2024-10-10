7a80ad86483886172df33d50b80e3243411c99c9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 12:43:18 +0000

    4746794 docs: move JSDoc below constructor (#57187)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.8+sha-a9e440d'));
+    definitionMap.set('version', o.literal('18.2.8+sha-4746794'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
