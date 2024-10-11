c4513c60aa7b66e222cf18e69c85320b33999769
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 10:43:01 +0000

    d9bb74f build: update io_bazel_rules_sass digest to 583fc8c (#58155)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.8+sha-4746794'));
+    definitionMap.set('version', o.literal('18.2.8+sha-d9bb74f'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
