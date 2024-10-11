964790b1989e84317836a674f5008a62102180f2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 14:14:06 +0000

    6cd2028 ci: disable bazel-saucelabs job (#58162)

--- esm2022/src/render3/partial/injector.mjs
+++ esm2022/src/render3/partial/injector.mjs
@@ -29,7 +29,7 @@ export function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.8+sha-d9bb74f'));
+    definitionMap.set('version', o.literal('18.2.8+sha-6cd2028'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
