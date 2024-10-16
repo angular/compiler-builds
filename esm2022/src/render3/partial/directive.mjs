5a700b8eab3c50e6da7b1367ba3e45e4d5db7369
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-16 16:51:00 +0000

    e60d016 docs(docs-infra): change mermaid diagram colors (#58223)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.8+sha-532bd11'));
+    definitionMap.set('version', o.literal('18.2.8+sha-e60d016'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
