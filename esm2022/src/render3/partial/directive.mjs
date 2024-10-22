ce82247b9e3875c897eb112efa61bfc1c13c7d06
Andrew Kushnir <akushnir@google.com>
2024-10-22 18:36:21 +0000

    2fb88a5 ci: temporarily disable adev tests (#58309)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.8+sha-713a14f'));
+    definitionMap.set('version', o.literal('18.2.8+sha-2fb88a5'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
--- esm2022/src/render3/partial/injectable.mjs
+++ esm2022/src/render3/partial/injectable.mjs
@@ -34,7 +34,7 @@ export function compileDeclareInjectableFromMetadata(meta) {
 export function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.8+sha-713a14f'));
+    definitionMap.set('version', o.literal('18.2.8+sha-2fb88a5'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
