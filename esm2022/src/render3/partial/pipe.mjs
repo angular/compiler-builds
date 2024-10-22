96d3282670243b31348c449bca1980e7a2c1ce96
Andrew Kushnir <akushnir@google.com>
2024-10-22 16:49:42 +0000

    713a14f build: update actions/cache digest to 6849a64 (#58305)

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.8+sha-7e933d8'));
+    definitionMap.set('version', o.literal('18.2.8+sha-713a14f'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
