95552fe2019bd469cfdfcf963f5b3a73c2fca911
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:47:09 +0000

    3b989ac fix(localize): Adding  arb format to the list of valid formats in the localization extractor cli (#58287)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.9+sha-e638b29'));
+    definitionMap.set('version', o.literal('18.2.9+sha-3b989ac'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
