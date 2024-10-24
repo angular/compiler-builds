4ad48dd6d13ae41881a2ae404ec2d23fd52701a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 19:46:43 +0000

    e638b29 docs(router): document segment consumption during route evaluation (#58069)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.9+sha-fae3c83'));
+    definitionMap.set('version', o.literal('18.2.9+sha-e638b29'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
