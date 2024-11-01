b6c792886c85ea2b0dd41f04be80af219983e553
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 14:46:58 +0000

    9d9abf2 build: Update publish-build-artifacts to honor ORG (#58408)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.10+sha-a249d0b'));
+    definitionMap.set('version', o.literal('18.2.10+sha-9d9abf2'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
