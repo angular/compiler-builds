efbc7549df516bb929cd7a331a575736f5f1799f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-06 12:07:45 +0000

    5f2d98a fix(core): avoid slow stringification when checking for duplicates in dev mode (#58521)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.10+sha-6c80778'));
+    definitionMap.set('version', o.literal('18.2.10+sha-5f2d98a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
