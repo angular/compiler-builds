a00932378f27478cd7b5d392f0be43548888335d
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 14:53:48 +0000

    caeeee7 docs(docs-infra): Fixed grammatical error (#58611)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.11+sha-2fbc7f0'));
+    definitionMap.set('version', o.literal('18.2.11+sha-caeeee7'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
