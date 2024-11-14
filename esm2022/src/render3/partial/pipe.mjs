4c0dd1d2caba86dec4b6f8ee145c99aa44d9ea62
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-14 16:17:24 +0000

    7672145 release: cut the v18.2.12 release

--- esm2022/src/render3/partial/pipe.mjs
+++ esm2022/src/render3/partial/pipe.mjs
@@ -32,7 +32,7 @@ export function compileDeclarePipeFromMetadata(meta) {
 export function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', o.literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', o.literal('18.2.11+sha-d9009c1'));
+    definitionMap.set('version', o.literal('18.2.12+sha-7672145'));
     definitionMap.set('ngImport', o.importExpr(R3.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
