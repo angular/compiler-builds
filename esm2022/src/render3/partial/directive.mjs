b0eb03af45858b8921a4b36009f3bd2ee30d30ca
Andrew Kushnir <akushnir@google.com>
2024-11-18 20:54:06 +0000

    88072c8 docs: add v19 developer event banner (#58721)

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.12+sha-6fef9b5'));
+    definitionMap.set('version', o.literal('18.2.12+sha-88072c8'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
