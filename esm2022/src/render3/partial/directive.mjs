883a3e19c517215502e18f31fd695685cf277907
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-26 18:09:35 +0000

    e1e419a release: cut the v18.2.13 release

--- esm2022/src/render3/partial/directive.mjs
+++ esm2022/src/render3/partial/directive.mjs
@@ -28,7 +28,7 @@ export function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', o.literal(minVersion));
-    definitionMap.set('version', o.literal('18.2.12+sha-c6cec5e'));
+    definitionMap.set('version', o.literal('18.2.13+sha-e1e419a'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone) {
