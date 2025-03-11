106b9f347bd68a35c8e18195a82875493da64131
Andrew Kushnir <akushnir@google.com>
2025-03-11 00:08:06 +0000

    c73520b refactor(compiler): remove empty empty string suffix from interpolation instructions (#60066)

--- fesm2022/compiler.mjs
+++ fesm2022/compiler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.1+sha-e3afa24
+ * @license Angular v20.0.0-next.1+sha-c73520b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -23178,7 +23178,17 @@ const PURE_FUNCTION_CONFIG = {
     mapping: (n) => n,
 };
 function callVariadicInstructionExpr(config, baseArgs, interpolationArgs, extraArgs, sourceSpan) {
+    // mapping need to be done before potentially dropping the last interpolation argument
     const n = config.mapping(interpolationArgs.length);
+    // In the case the interpolation instruction ends with a empty string we drop it
+    // And the runtime will take care of it.
+    const lastInterpolationArg = interpolationArgs.at(-1);
+    if (extraArgs.length === 0 &&
+        interpolationArgs.length > 1 &&
+        lastInterpolationArg instanceof LiteralExpr &&
+        lastInterpolationArg.value === '') {
+        interpolationArgs.pop();
+    }
     if (n < config.constant.length) {
         // Constant calling pattern.
         return importExpr(config.constant[n])
@@ -32868,7 +32878,7 @@ const MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION = '18.0.0';
 function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$5));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
@@ -32886,7 +32896,7 @@ function compileComponentDeclareClassMetadata(metadata, dependencies) {
     callbackReturnDefinitionMap.set('ctorParameters', metadata.ctorParameters ?? literal(null));
     callbackReturnDefinitionMap.set('propDecorators', metadata.propDecorators ?? literal(null));
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('resolveDeferredDeps', compileComponentMetadataAsyncResolver(dependencies));
@@ -32981,7 +32991,7 @@ function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', literal(minVersion));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone !== undefined) {
@@ -33397,7 +33407,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION$4 = '12.0.0';
 function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$4));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
@@ -33432,7 +33442,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$3));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
@@ -33483,7 +33493,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$2));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
@@ -33516,7 +33526,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$1));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
@@ -33567,7 +33577,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', literal('20.0.0-next.1+sha-e3afa24'));
+    definitionMap.set('version', literal('20.0.0-next.1+sha-c73520b'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
@@ -33725,7 +33735,7 @@ function compileHmrUpdateCallback(definitions, constantStatements, meta) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.0.0-next.1+sha-e3afa24');
+const VERSION = new Version('20.0.0-next.1+sha-c73520b');
 
 //////////////////////////////////////
 // This file only reexports content of the `src` folder. Keep it that way.
