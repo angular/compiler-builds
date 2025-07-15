7853faea3f10eb223cf80c93743f3aa3bc34754d
Andrew Kushnir <akushnir@google.com>
2025-07-15 15:06:50 +0000

    80da073 docs: fix missing 'of' in Style Guide sentence (#62628)

--- fesm2022/compiler.mjs
+++ fesm2022/compiler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-e8c5603
+ * @license Angular v20.2.0-next.0+sha-80da073
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -19498,12 +19498,13 @@ class _ParseAST {
         else {
             if (this.isAssignmentOperator(this.next)) {
                 const operation = this.next.strValue;
-                this.advance();
                 if (!(this.parseFlags & 1 /* ParseFlags.Action */)) {
+                    this.advance();
                     this.error('Bindings cannot contain assignments');
                     return new EmptyExpr$1(this.span(start), this.sourceSpan(start));
                 }
                 const receiver = new PropertyRead(this.span(start), this.sourceSpan(start), nameSpan, readReceiver, id);
+                this.advance();
                 const value = this.parseConditional();
                 return new Binary(this.span(start), this.sourceSpan(start), operation, receiver, value);
             }
@@ -19622,12 +19623,13 @@ class _ParseAST {
             this.expectCharacter($RBRACKET);
             if (this.isAssignmentOperator(this.next)) {
                 const operation = this.next.strValue;
-                this.advance();
                 if (isSafe) {
+                    this.advance();
                     this.error("The '?.' operator cannot be used in the assignment");
                 }
                 else {
                     const binaryReceiver = new KeyedRead(this.span(start), this.sourceSpan(start), receiver, key);
+                    this.advance();
                     const value = this.parseConditional();
                     return new Binary(this.span(start), this.sourceSpan(start), operation, binaryReceiver, value);
                 }
@@ -33714,7 +33716,7 @@ const MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION = '18.0.0';
 function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$5));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
@@ -33732,7 +33734,7 @@ function compileComponentDeclareClassMetadata(metadata, dependencies) {
     callbackReturnDefinitionMap.set('ctorParameters', metadata.ctorParameters ?? literal(null));
     callbackReturnDefinitionMap.set('propDecorators', metadata.propDecorators ?? literal(null));
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('resolveDeferredDeps', compileComponentMetadataAsyncResolver(dependencies));
@@ -33827,7 +33829,7 @@ function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', literal(minVersion));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone !== undefined) {
@@ -34243,7 +34245,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION$4 = '12.0.0';
 function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$4));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
@@ -34278,7 +34280,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$3));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
@@ -34329,7 +34331,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$2));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
@@ -34362,7 +34364,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$1));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
@@ -34413,7 +34415,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', literal('20.2.0-next.0+sha-e8c5603'));
+    definitionMap.set('version', literal('20.2.0-next.0+sha-80da073'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
@@ -34569,7 +34571,7 @@ function compileHmrUpdateCallback(definitions, constantStatements, meta) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.2.0-next.0+sha-e8c5603');
+const VERSION = new Version('20.2.0-next.0+sha-80da073');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
