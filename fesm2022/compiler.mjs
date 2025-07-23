d6c012a8c03bd268bf329c4ac8e81fc094d457ef
Kirill Cherkashin <kirjs@google.com>
2025-07-23 15:16:49 +0000

    7767aa6 fix(compiler): allow more characters in square-bracketed attribute names (#62742)

--- fesm2022/compiler.mjs
+++ fesm2022/compiler.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.1+sha-882522c
+ * @license Angular v20.2.0-next.1+sha-7767aa6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -16694,6 +16694,23 @@ class _Tokenizer {
                     }
                 }
                 return isNameEnd(code);
+            };
+        }
+        else if (attrNameStart === $LBRACKET) {
+            let openBrackets = 0;
+            // Be more permissive for which characters are allowed inside square-bracketed attributes,
+            // because they usually end up being bound as attribute values. Some third-party packages
+            // like Tailwind take advantage of this.
+            nameEndPredicate = (code) => {
+                if (code === $LBRACKET) {
+                    openBrackets++;
+                }
+                else if (code === $RBRACKET) {
+                    openBrackets--;
+                }
+                // Only check for name-ending characters if the brackets are balanced or mismatched.
+                // Also interrupt the matching on new lines.
+                return openBrackets <= 0 ? isNameEnd(code) : isNewLine(code);
             };
         }
         else {
@@ -34231,7 +34248,7 @@ const MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION = '18.0.0';
 function compileDeclareClassMetadata(metadata) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$5));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('decorators', metadata.decorators);
@@ -34249,7 +34266,7 @@ function compileComponentDeclareClassMetadata(metadata, dependencies) {
     callbackReturnDefinitionMap.set('ctorParameters', metadata.ctorParameters ?? literal(null));
     callbackReturnDefinitionMap.set('propDecorators', metadata.propDecorators ?? literal(null));
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_DEFER_SUPPORT_VERSION));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', metadata.type);
     definitionMap.set('resolveDeferredDeps', compileComponentMetadataAsyncResolver(dependencies));
@@ -34344,7 +34361,7 @@ function createDirectiveDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     const minVersion = getMinimumVersionForPartialOutput(meta);
     definitionMap.set('minVersion', literal(minVersion));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     // e.g. `type: MyDirective`
     definitionMap.set('type', meta.type.value);
     if (meta.isStandalone !== undefined) {
@@ -34760,7 +34777,7 @@ const MINIMUM_PARTIAL_LINKER_VERSION$4 = '12.0.0';
 function compileDeclareFactoryFunction(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$4));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('deps', compileDependencies(meta.deps));
@@ -34795,7 +34812,7 @@ function compileDeclareInjectableFromMetadata(meta) {
 function createInjectableDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$3));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // Only generate providedIn property if it has a non-null value
@@ -34846,7 +34863,7 @@ function compileDeclareInjectorFromMetadata(meta) {
 function createInjectorDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$2));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     definitionMap.set('providers', meta.providers);
@@ -34879,7 +34896,7 @@ function createNgModuleDefinitionMap(meta) {
         throw new Error('Invalid path! Local compilation mode should not get into the partial compilation path');
     }
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION$1));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     definitionMap.set('type', meta.type.value);
     // We only generate the keys in the metadata if the arrays contain values.
@@ -34930,7 +34947,7 @@ function compileDeclarePipeFromMetadata(meta) {
 function createPipeDefinitionMap(meta) {
     const definitionMap = new DefinitionMap();
     definitionMap.set('minVersion', literal(MINIMUM_PARTIAL_LINKER_VERSION));
-    definitionMap.set('version', literal('20.2.0-next.1+sha-882522c'));
+    definitionMap.set('version', literal('20.2.0-next.1+sha-7767aa6'));
     definitionMap.set('ngImport', importExpr(Identifiers.core));
     // e.g. `type: MyPipe`
     definitionMap.set('type', meta.type.value);
@@ -35086,7 +35103,7 @@ function compileHmrUpdateCallback(definitions, constantStatements, meta) {
  * @description
  * Entry point for all public APIs of the compiler package.
  */
-const VERSION = new Version('20.2.0-next.1+sha-882522c');
+const VERSION = new Version('20.2.0-next.1+sha-7767aa6');
 
 //////////////////////////////////////
 // THIS FILE HAS GLOBAL SIDE EFFECT //
