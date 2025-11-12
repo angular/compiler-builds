e92c6ed570a2cc0e1d3857912438c50f2dd93426
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-12 21:17:18 +0000

    f7e5857 refactor(compiler-cli): rework type checking for signal forms

--- types/compiler.d.ts
+++ types/compiler.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-48d3954
+ * @license Angular v21.1.0-next.0+sha-f7e5857
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -5326,10 +5326,6 @@ declare class Identifiers {
         moduleName: string;
     };
     static UnwrapDirectiveSignalInputs: {
-        name: string;
-        moduleName: string;
-    };
-    static ExtractFormControlValue: {
         name: string;
         moduleName: string;
     };
