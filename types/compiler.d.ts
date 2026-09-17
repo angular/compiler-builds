646388569d9120ba5332262771f317f67218dd42
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 21:15:23 +0000

    a783c4e refactor(platform-browser): type shadow DOM renderer fields

--- types/compiler.d.ts
+++ types/compiler.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-7e678e0
+ * @license Angular v22.3.0-next.0+sha-a783c4e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -1708,7 +1708,7 @@ declare class ConstantPool {
     constructor(isClosureCompilerEnabled?: boolean);
     getConstLiteral(literal: Expression, forceShared?: boolean): Expression;
     getSharedConstant(def: SharedConstantDefinition, expr: Expression): Expression;
-    getSharedFunctionReference(fn: Expression, prefix: string, useUniqueName?: boolean): Expression;
+    getSharedFunctionReference(fn: Expression, prefix: string, useUniqueName?: boolean): ReadVarExpr;
     /**
      * Produce a unique name in the context of this pool.
      *
